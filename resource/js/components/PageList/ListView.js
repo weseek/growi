6d13b92b5afda514698f6c3beec02359faadbdfa
Sotaro KARASAWA <sotaro.k@gmail.com>
2016-05-10 00:07:33 +0900

    Fix nav structure

--- resource/js/components/PageList/ListView.js
+++ resource/js/components/PageList/ListView.js
@@ -11,15 +11,11 @@ export default class ListView extends React.Component {
 
       return (
         <li className="page-list-li" key={page._id}>
-          <div className="picture-outer">
-            <UserPicture user={page.revision.author} />
-          </div>
-          <div className="page-link-outer">
-            <a className="page-list-link" href={page.path}>
-              <PagePath page={page} />
-            </a>
-            <PageListMeta page={page} />
-          </div>
+          <UserPicture user={page.revision.author} />
+          <a className="page-list-link" href={page.path}>
+            <PagePath page={page} />
+          </a>
+          <PageListMeta page={page} />
         </li>
       );
     });
