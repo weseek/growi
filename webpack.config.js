fa9928a5dbde8414088c80215ac49298e56f4be2
Sotaro KARASAWA <sotaro.k@gmail.com>
2016-05-13 00:16:11 +0900

    Should be separate file on dev/prod webpack config for real

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,11 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    })
     //new webpack.ProvidePlugin({
     //  jQuery: "jquery",
     //  $: "jquery",
