b1def13c8522d8fe88d337cb1ba7775de65623f2
Sotaro KARASAWA <sotaro.k@gmail.com>
2016-06-09 17:44:34 +0900

    webpack.config.js: NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       "process.env": {
-        NODE_ENV: JSON.stringify("production")
+        NODE_ENV: process.env.NODE_ENV || "production"
       }
     })
     //new webpack.ProvidePlugin({
