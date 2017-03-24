9cbc7d312a48600c1abdfdec623baa8cb4ebeaad
Yuki Takei <yuki@weseek.co.jp>
2017-03-24 09:28:10 +0900

    add comments

--- bin/generate-plugin-definitions-source.js
+++ bin/generate-plugin-definitions-source.js
@@ -1,11 +1,20 @@
+/**
+ * the tool for genetion of plugin definitions source code
+ *
+ * @author Yuki Takei <yuki@weseek.co.jp>
+ */
 const fs = require('graceful-fs');
 const slash = require('slash');
 const swig = require('swig');
 const helpers = require('../config/helpers');
 
+const TEMPLATE = helpers.root('bin/templates/plugin-definitions.js.swig');
+const OUT = helpers.root('resource/js/plugins/plugin-definitions.js');
+
 const PluginUtils = require('../lib/plugins/plugin-utils');
 const pluginUtils = new PluginUtils();
 
+// get definitions
 const definitions = pluginUtils.listPluginNames(helpers.root())
   .map((name) => {
     return pluginUtils.generatePluginDefinition(name, true);
@@ -18,9 +27,8 @@ const definitions = pluginUtils.listPluginNames(helpers.root())
     return definition;
   });
 
-var template = swig.compileFile(helpers.root('bin/templates/plugin-definitions.js.swig'));
-var output = template({definitions});
+var compiledTemplate = swig.compileFile(TEMPLATE);
+var code = compiledTemplate({definitions});
 
-fs.writeFileSync(
-  helpers.root('resource/js/plugins/plugin-definitions.js'),
-  output);
+// write
+fs.writeFileSync(OUT, code);
