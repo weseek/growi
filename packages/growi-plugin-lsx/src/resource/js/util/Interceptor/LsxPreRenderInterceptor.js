165a4d33cc447f0745a63423b6f3d9e4aedc6331
Yuki Takei <yuki@weseek.co.jp>
2017-03-28 06:15:55 +0900

    use crowi-pluginkit from npm

--- packages/growi-plugin-lsx/src/resource/js/util/Interceptor/LsxPreRenderInterceptor.js
+++ packages/growi-plugin-lsx/src/resource/js/util/Interceptor/LsxPreRenderInterceptor.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import BasicInterceptor from '../../../../lib/util/BasicInterceptor';
+import { BasicInterceptor } from 'crowi-pluginkit';
 
 import { Lsx } from '../../components/Lsx';
 import { LsxContext } from '../LsxContext';
