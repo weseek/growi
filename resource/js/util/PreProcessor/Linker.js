12a9ea36ccd78798fb26e4e43876ed1a60230c8a
Yuki Takei <yuki@weseek.co.jp>
2017-04-01 01:31:10 +0900

    BugFix original Linker
    * omit the '\s' of head

--- resource/js/util/PreProcessor/Linker.js
+++ resource/js/util/PreProcessor/Linker.js
@@ -4,7 +4,7 @@ export default class Linker {
 
     return markdown
       // process angle branckets like '</Level1/Level2>'
-      .replace(/\s<((\/[^>]+?){2,})>/g, ' <a href="$1">$1</a>') // ページ間リンク: <> でかこまれてて / から始まり、 / が2個以上
+      .replace(/<((\/[^>]+?){2,})>/g, '<a href="$1">$1</a>') // ページ間リンク: <> でかこまれてて / から始まり、 / が2個以上
       // process square branckets like '[/Level1]'
       // see: https://regex101.com/r/QSt1yu/1
       .replace(/\[(\/[^\]]+?)\]/g, ' <a href="$1">$1</a>')
