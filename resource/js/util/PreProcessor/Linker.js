97e28b716fb4cc6af61fc13b9a7d5bec6e0d009a
Yuki Takei <yuki@weseek.co.jp>
2017-04-01 01:32:23 +0900

    BugFix square brackets linker
    * ensure not to process default markdown linker syntax

--- resource/js/util/PreProcessor/Linker.js
+++ resource/js/util/PreProcessor/Linker.js
@@ -6,8 +6,8 @@ export default class Linker {
       // process angle branckets like '</Level1/Level2>'
       .replace(/<((\/[^>]+?){2,})>/g, '<a href="$1">$1</a>') // ページ間リンク: <> でかこまれてて / から始まり、 / が2個以上
       // process square branckets like '[/Level1]'
-      // see: https://regex101.com/r/QSt1yu/1
-      .replace(/\[(\/[^\]]+?)\]/g, ' <a href="$1">$1</a>')
+      // see: https://regex101.com/r/QSt1yu/3
+      .replace(/\[(\/[^\]]+?)\](?!\()/g, '<a href="$1">$1</a>')
       ;
   }
 }
