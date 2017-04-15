72f22b54f10d44d3bb7b459d2cde442d2fda8a91
Yuki Takei <yuki@weseek.co.jp>
2017-04-15 09:38:12 +0900

    BugFix when url starts with '/'

--- packages/growi-plugin-pukiwiki-like-linker/src/resource/js/util/PreProcessor/PukiwikiLikeLinker.js
+++ packages/growi-plugin-pukiwiki-like-linker/src/resource/js/util/PreProcessor/PukiwikiLikeLinker.js
@@ -8,7 +8,7 @@ export class PukiwikiLikeLinker {
       .replace(/\[\[(([^(\]\])]+)>)?(.+?)\]\]/g, function(all, group1, group2, group3) {
         // create url
         // use 'group3' as is if starts from 'http(s)', otherwise join to 'window.location.pathname'
-        var url = (group3.match(/^https?:\/\//)) ? group3 : path.join(window.location.pathname, group3);
+        var url = (group3.match(/^(\/|https?:\/\/)/)) ? group3 : path.join(window.location.pathname, group3);
         // determine alias string
         // if 'group2' is undefined, use group3
         var alias = group2 ? group2 : group3;
