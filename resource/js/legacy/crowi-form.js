3fefd39117037c9df8d5a37fe09aaa69ad251610
Yuki Takei <yuki@weseek.co.jp>
2017-05-01 02:37:41 +0900

    change inline-attachment setting according to the type of uploading file

--- resource/js/legacy/crowi-form.js
+++ resource/js/legacy/crowi-form.js
@@ -466,9 +466,16 @@ $(function() {
       urlText: "\n![file]({filename})\n"
     };
 
+    attachmentOption.onFileReceived = function(file) {
+      // if not image
+      if (!file.type.match(/^image\/.+$/)) {
+        // modify urlText with 'a' tag
+        this.settings.urlText = `<a href="{filename}">${file.name}</a>\n`;
+      }
+    }
+
     attachmentOption.onFileUploadResponse = function(res) {
       var result = JSON.parse(res.response);
-      console.log(result);
 
       if (result.ok && result.pageCreated) {
         var page = result.page,
