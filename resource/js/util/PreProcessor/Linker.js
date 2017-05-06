3c0c2b4ce9bd60437bbc8396b0bd68649d9f54a8
Sotaro KARASAWA <sotaro.k@gmail.com>
2017-05-06 23:39:34 +0900

    Added link pattern of [/xxx]

--- resource/js/util/PreProcessor/Linker.js
+++ resource/js/util/PreProcessor/Linker.js
@@ -3,7 +3,7 @@ export default class Linker {
   process(markdown) {
 
     return markdown
-      //.replace(/\s(https?:\/\/[\S]+)/g, ' <a href="$1">$1</a>') // リンク
+      .replace(/\s\[(\/[^\]]+?)\](?!\()/g, ' <a href="$1">$1</a>') // ページ間リンク: [] でかこまれてて / から始まる
       .replace(/\s<((\/[^>]+?){2,})>/g, ' <a href="$1">$1</a>') // ページ間リンク: <> でかこまれてて / から始まり、 / が2個以上
       ;
   }
