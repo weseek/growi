cc887b6b6e4a0892bdef24ce1280b03cd2daf155
Yuki Takei <yuki@weseek.co.jp>
2017-06-07 04:53:07 +0900

    ensure to use https for getting Mathjax.js

--- resource/js/util/PostProcessor/Mathjax.js
+++ resource/js/util/PostProcessor/Mathjax.js
@@ -3,7 +3,7 @@ export default class Mathjax {
 
   constructor(crowi) {
     this.crowi = crowi;
-    this.defaultUrl = 'https//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?skipStartupTypeset=true';
+    this.defaultUrl = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?skipStartupTypeset=true';
 
     this.mathJaxConfigured = false;
 
