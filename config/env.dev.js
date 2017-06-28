315878f892aafecb34511b954ec50d69888f4eba
Yuki Takei <yuki@weseek.co.jp>
2017-06-29 02:24:46 +0900

    apply pino only when production

--- config/env.dev.js
+++ config/env.dev.js
@@ -1,10 +1,11 @@
 module.exports = {
+  // DEBUG: '*',
   NODE_ENV: 'development',
-  // MATHJAX: 1,
-  // REDIS_URL: 'redis://localhost:6379/crowi',
-  // ELASTICSEARCH_URI: 'http://localhost:9200/crowi',
+  MATHJAX: 1,
+  REDIS_URL: 'redis://localhost:6379/crowi',
+  ELASTICSEARCH_URI: 'http://localhost:9200/crowi',
   PLUGIN_NAMES_TOBE_LOADED: [
-    // 'crowi-plugin-lsx',
-    // 'crowi-plugin-pukiwiki-like-linker',
+    'crowi-plugin-lsx',
+    'crowi-plugin-pukiwiki-like-linker',
   ]
 }
