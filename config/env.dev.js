4959c6f33b39921f99779043d52a549fb2e7b274
Yuki Takei <yuki@weseek.co.jp>
2017-06-29 03:15:24 +0900

    improve env.dev.js for debug

--- config/env.dev.js
+++ config/env.dev.js
@@ -1,11 +1,17 @@
 module.exports = {
-  // DEBUG: '*',
   NODE_ENV: 'development',
   MATHJAX: 1,
-  REDIS_URL: 'redis://localhost:6379/crowi',
-  ELASTICSEARCH_URI: 'http://localhost:9200/crowi',
+  // REDIS_URL: 'redis://localhost:6379/crowi',
+  // ELASTICSEARCH_URI: 'http://localhost:9200/crowi',
   PLUGIN_NAMES_TOBE_LOADED: [
-    'crowi-plugin-lsx',
-    'crowi-plugin-pukiwiki-like-linker',
-  ]
+    // 'crowi-plugin-lsx',
+    // 'crowi-plugin-pukiwiki-like-linker',
+  ],
+  // filters for debug
+  DEBUG: [
+    // 'express:*',
+    // 'crowi:*',
+    // 'crowi:plugins:*',
+    // 'crowi:InterceptorManager',
+  ].join(),
 }
