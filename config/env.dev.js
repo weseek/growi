7e955aa8ccfcf811dfecc477625648bbb076000a
Yuki Takei <yuki@weseek.co.jp>
2017-09-24 01:04:46 +0900

    add config "security:isEnabledPassport"

--- config/env.dev.js
+++ config/env.dev.js
@@ -1,16 +1,19 @@
 module.exports = {
   NODE_ENV: 'development',
-  // FILE_UPLOAD: 'local',
+  FILE_UPLOAD: 'local',
   // MATHJAX: 1,
-  // REDIS_URL: 'redis://localhost:6379/crowi',
-  // ELASTICSEARCH_URI: 'http://localhost:9200/crowi',
+  REDIS_URL: 'redis://localhost:6379/crowi',
+  ELASTICSEARCH_URI: 'http://localhost:9200/crowi',
   PLUGIN_NAMES_TOBE_LOADED: [
-    // 'crowi-plugin-lsx',
-    // 'crowi-plugin-pukiwiki-like-linker',
+    'crowi-plugin-lsx',
+    'crowi-plugin-pukiwiki-like-linker',
   ],
   // filters for debug
   DEBUG: [
     // 'express:*',
+    'crowi:routes:login',
+    'crowi:routes:login-passport',
+    'crowi:service:PassportService',
     // 'crowi:*',
     // 'crowi:routes:page',
     // 'crowi:plugins:*',
