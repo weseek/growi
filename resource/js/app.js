5e300b497431949f52cbaf7409121adff0bfc0b0
Yuki Takei <yuki@weseek.co.jp>
2018-03-23 23:35:52 +0900

    add comment

--- resource/js/app.js
+++ resource/js/app.js
@@ -224,7 +224,7 @@ if (customTitleEditorElem != null) {
   )
 }
 
-// うわーもうー
+// うわーもうー (commented by Crowi team -- 2018.03.23 Yuki Takei)
 $('a[data-toggle="tab"][href="#revision-history"]').on('show.bs.tab', function() {
   ReactDOM.render(<PageHistory pageId={pageId} crowi={crowi} />, document.getElementById('revision-history'));
 });
