8c1fbeabf3377eea59a9c616586e29617adb8117
Yuki Takei <yuki@weseek.co.jp>
2018-03-24 06:47:38 +0900

    correspond growi plugins

--- config/env.dev.js
+++ config/env.dev.js
@@ -5,8 +5,8 @@ module.exports = {
   // REDIS_URL: 'redis://localhost:6379/crowi',
   // ELASTICSEARCH_URI: 'http://localhost:9200/crowi',
   PLUGIN_NAMES_TOBE_LOADED: [
-    // 'crowi-plugin-lsx',
-    // 'crowi-plugin-pukiwiki-like-linker',
+    // 'growi-plugin-lsx',
+    // 'growi-plugin-pukiwiki-like-linker',
   ],
   // filters for debug
   DEBUG: [
