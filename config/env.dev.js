b1e0706923e903193b16e962decb93c7675e9c39
sou <sou@weseek.co.jp>
2018-05-07 19:54:50 +0900

    editconfig, defaulted some configs

--- config/env.dev.js
+++ config/env.dev.js
@@ -2,7 +2,7 @@ module.exports = {
   NODE_ENV: 'development',
   FILE_UPLOAD: 'local',
   // MATHJAX: 1,
-  ELASTICSEARCH_URI: 'http://localhost:9200/growi',
+  // ELASTICSEARCH_URI: 'http://localhost:9200/growi',
   PLUGIN_NAMES_TOBE_LOADED: [
     // 'growi-plugin-lsx',
     // 'growi-plugin-pukiwiki-like-linker',
