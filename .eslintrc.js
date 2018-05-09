b3e2294461ff1ec7836fa504e1ce3fd0839775e8
Yuki Takei <yuki@weseek.co.jp>
2018-05-10 01:21:01 +0900

    upgrade libs
    * eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,8 +37,8 @@ module.exports = {
       2,
       {
         "SwitchCase": 1,
+        "ignoredNodes": ['JSXElement *', 'JSXElement'],
         "FunctionExpression": {"parameters": 2},
-        "CallExpression": {"parameters": 2}
       }
     ],
     "key-spacing": [
@@ -55,11 +55,11 @@ module.exports = {
       "error",
       "single"
     ],
-    "react/jsx-indent": [
+    "react/jsx-indent-props": [
       "error",
-      4,
-      { "ignoredNodes": ["JSXElement *"] }
+      2
     ],
+    "react/no-string-refs": 'off',
     "semi": [
       "error",
       "always"
