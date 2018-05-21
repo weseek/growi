75e4ec8468c04657b5f2cbd3548bd081ebf5a601
yusuketk <yusuketk@weseek.co.jp>
2018-05-21 15:56:35 +0900

    (page-comment-form) replace formcontrol with textarea

--- resource/js/components/PageComment/CommentForm.js
+++ resource/js/components/PageComment/CommentForm.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import FormControl from 'react-bootstrap/es/FormControl';
 import Button from 'react-bootstrap/es/Button';
 import UserPicture from '../User/UserPicture';
+
 /**
  *
  * @author Yuki Takei <yuki@weseek.co.jp>
@@ -38,17 +39,11 @@ export default class CommentForm extends React.Component {
           </a>
         </div>
         <div className="comment-form-main">
-          <div className="comment-form-editor">
-            <FormControl
-            type="text"
-            className="comment-form-comment form-control"
-            value={this.state.value}
-            required
-            placeholder="Write comments here..."
-            onChange={this.handleChange}
-          />
+          <div className="comment-write comment-form-editor">
+            <textarea className="comment-form-comment form-control" id="comment-form-comment" name="commentForm[comment]" required placeholder="Write comments here..." >
+            </textarea>
             <div className="form-check">
-            <input type="checkbox" className="form-check-input" id="checkbox-markdown"></input>
+            <input type="checkbox" className="form-check-input" id="checkbox-markdown" />
               <label className="form-check-label" htmlFor="checkbox-markdown">Markdown</label>
             </div>
           </div>
