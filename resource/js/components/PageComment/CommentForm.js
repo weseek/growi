02464626c3bba965ff8e2834ff5f1fd9ada17aa8
yusuketk <yusuketk@weseek.co.jp>
2018-05-28 18:03:41 +0900

    change file for fixing fail

--- resource/js/components/PageComment/CommentForm.js
+++ resource/js/components/PageComment/CommentForm.js
@@ -5,8 +5,8 @@ import CommentPreview from '../PageComment/CommentPreview';
 
 import Button from 'react-bootstrap/es/Button';
 import FormControl from 'react-bootstrap/es/FormControl';
-import Tab from 'react-bootstrap/lib/tab';
-import Tabs from 'react-bootstrap/lib/tabs';
+import Tab from './node_modules/react-bootstrap/es/tab';
+import Tabs from './node_modules/react-bootstrap/es/tabs';
 import UserPicture from '../User/UserPicture';
 
 /**
