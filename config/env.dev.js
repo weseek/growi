21e0d840fb323215082f871099279e74f3733d27
Yuki Takei <yuki@weseek.co.jp>
2018-06-19 05:39:33 +0900

    add HACKMD_URI env var

--- config/env.dev.js
+++ config/env.dev.js
@@ -3,6 +3,7 @@ module.exports = {
   FILE_UPLOAD: 'local',
   // MATHJAX: 1,
   ELASTICSEARCH_URI: 'http://localhost:9200/growi',
+  HACKMD_URI: 'http://localhost:3100',
   PLUGIN_NAMES_TOBE_LOADED: [
     // 'growi-plugin-lsx',
     // 'growi-plugin-pukiwiki-like-linker',
