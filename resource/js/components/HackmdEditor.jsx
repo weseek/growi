4af4786a6f9a29fce2a4dffebde6aa30af7752f5
Yuki Takei <yuki@weseek.co.jp>
2018-06-23 04:47:09 +0900

    WIP: impl HackmdEditor.jsx

--- resource/js/components/HackmdEditor.jsx
+++ resource/js/components/HackmdEditor.jsx
@@ -7,23 +7,32 @@ export default class HackmdEditor extends React.PureComponent {
     super(props);
 
     this.state = {
-
     };
 
+    this.loadHandler = this.loadHandler.bind(this);
   }
 
   componentWillMount() {
   }
 
+  loadHandler() {
+
+  }
+
   render() {
     const envVars = this.props.crowi.config.env;
     const hackMdUri = envVars.HACKMD_URI;
+
     if (hackMdUri == null) {
       return <React.Fragment></React.Fragment>;
     }
 
     return (
-      <iframe src={hackMdUri}>
+      <iframe id='iframe-hackmd'
+        ref='iframe'
+        src={hackMdUri}
+        onLoad={this.loadHandler}
+      >
       </iframe>
     );
   }
