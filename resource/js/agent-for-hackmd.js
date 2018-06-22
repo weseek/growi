ebcfb590d21d03a8a229ee83b3438aa1880df321
Yuki Takei <yuki@weseek.co.jp>
2018-06-23 05:55:19 +0900

    WIP: impl agent-for-hackmd.js

--- resource/js/agent-for-hackmd.js
+++ resource/js/agent-for-hackmd.js
@@ -1,4 +1,22 @@
-console.log('GROWI agent for HackMD loaded.');
+/* eslint-disable no-console  */
+console.log('Loading GROWI agent for HackMD...');
 
-const origin = '{{origin}}';
-console.log('origin:', origin);
+const allowedOrigin = '{{origin}}';
+
+/**
+ * Validate origin
+ * @param {object} event
+ */
+function validateOrigin(event) {
+  if (event.origin !== allowedOrigin) {
+    console.error('Rejected');
+    return;
+  }
+}
+
+window.addEventListener('message', (event) => {
+  validateOrigin(event);
+  console.log('getValue called');
+});
+
+console.log('GROWI agent for HackMD has successfully loaded.');
