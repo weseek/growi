ffec1228c185ff246026414b7bc80e9eec745e70
Yuki Takei <yuki@weseek.co.jp>
2018-06-23 00:26:05 +0900

    set SESSION_NAME to env.dev.js to avoid batting with HackMD

--- config/env.dev.js
+++ config/env.dev.js
@@ -1,6 +1,7 @@
 module.exports = {
   NODE_ENV: 'development',
   FILE_UPLOAD: 'local',
+  SESSION_NAME: 'connect.growi-dev.sid',
   // MATHJAX: 1,
   ELASTICSEARCH_URI: 'http://localhost:9200/growi',
   HACKMD_URI: 'http://localhost:3100',
