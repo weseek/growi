00cdb147ef9ae1d57952aa0b66216f35e5a59fbb
sou <sou@weseek.co.jp>
2018-06-28 12:19:43 +0900

    xssOption

--- resource/js/util/PreProcessor/XssFilter.js
+++ resource/js/util/PreProcessor/XssFilter.js
@@ -1,9 +1,11 @@
 import Xss from '../../../../lib/util/xss';
+import xssOption from '../../../../lib/util/xssOption';
 
 export default class XssFilter {
 
   constructor(crowi) {
-    this.xss = new Xss(crowi);
+    this.xssOption = new xssOption(crowi.config);
+    this.xss = new Xss(this.xssOption);
   }
 
   process(markdown) {
