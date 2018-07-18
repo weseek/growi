816e31da862e99d7c865746f10088bd19b35acd5
Yuki Takei <yuki@weseek.co.jp>
2018-07-18 18:01:24 +0900

    GC-590: use switchery
    add official files

--- resource/js/legacy/crowi-admin.js
+++ resource/js/legacy/crowi-admin.js
@@ -1,6 +1,9 @@
 require('bootstrap-select');
 require('./thirdparty-js/jQuery.style.switcher');
 
+const Switchery = require('./thirdparty-js/switchery/switchery');
+require('./thirdparty-js/switchery/switchery.css');
+
 $(function() {
   $('#slackNotificationForm').on('submit', function(e) {
     $.post('/_api/admin/notification.add', $(this).serialize(), function(res) {
@@ -105,6 +108,14 @@ $(function() {
 
   // style switcher
   $('#styleOptions').styleSwitcher();
+
+  // switchery
+  const elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+  elems.forEach(function(elem) {
+    const color = elem.dataset.color;
+    const size = elem.dataset.size;
+    new Switchery(elem, { color, size });
+  });
 });
 
 
