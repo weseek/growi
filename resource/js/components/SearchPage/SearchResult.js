41fb0ca0100eb18fabd602354ae025a923de2c7a
Yuki Takei <yuki@weseek.co.jp>
2018-07-23 02:11:01 +0900

    clean code

--- resource/js/components/SearchPage/SearchResult.js
+++ resource/js/components/SearchPage/SearchResult.js
@@ -121,9 +121,7 @@ export default class SearchResult extends React.Component {
           isDeleted = false;
         }
       }).catch(err => {
-        /* eslint-disable no-console */
-        console.log(err.message);
-        /* eslint-enable */
+        console.error(err.message);   // eslint-disable-line no-console
         isDeleted = false;
         this.setState({errorMessageForDeleting: err.message});
       });
