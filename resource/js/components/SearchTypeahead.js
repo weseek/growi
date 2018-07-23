b9478af43f089ac05da0409a199b1aae91070629
mayu morita <mayu@weseek.co.jp>
2018-07-23 13:42:23 +0900

    add style

--- resource/js/components/SearchTypeahead.js
+++ resource/js/components/SearchTypeahead.js
@@ -173,23 +173,25 @@ export default class SearchTypeahead extends React.Component {
   }
 
   getHelpElement() {
-    return <table className="table table-borderd search-help">
-              <caption className="text-center">Search Help</caption>
+    return <table className="table m-1">
+              <caption className="text-left text-primary p-2 pb-2">
+                <h5 className="m-1"><i className="icon-magnifier pr-2"/>Search Help</h5>
+              </caption>
               <tr>
-                <td className="text-center">keyword</td>
-                <th>記事名 or カテゴリ or 本文にkeywordを含む</th>
+                <td className="text-right mt-0 p-1 "><code>keyword</code></td>
+                <th><h6 className="m-0 pt-1">記事名 or 本文に<samp>"keyword"</samp>を含む</h6></th>
               </tr>
               <tr>
-                <td className="text-center">title:keyword</td>
-                <th>記事名にkeywordを含む</th>
+                <td className="text-right mt-0 p-1"><code>title:keyword</code></td>
+                <th><h6 className="m-0 pt-1">記事名に<samp>"keyword"</samp>を含む</h6></th>
               </tr>
               <tr>
-                <td className="text-center">a b</td>
-                <th>文字列aとbを含む(スペース区切り)</th>
+                <td className="text-right mt-0 p-1"><code>a b</code></td>
+                <th><h6 className="m-0 pt-1">文字列<samp>"a"</samp>と<samp>"b"</samp>を含む (スペース区切り)</h6></th>
               </tr>
               <tr>
-                <td className="text-center">-keyword</td>
-                <th>文字列keywordを含まない</th>
+                <td className="text-right mt-0 p-1"><code>-keyword</code></td>
+                <th><h6 className="m-0 pt-1">文字列<samp>"keyword"</samp>を含まない</h6></th>
               </tr>
             </table>;
   }
