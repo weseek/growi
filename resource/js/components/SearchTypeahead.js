874122d8c121e6e08259d7e9b791931f4349fb67
mayu morita <mayu@weseek.co.jp>
2018-07-23 16:40:53 +0900

    restore

--- resource/js/components/SearchTypeahead.js
+++ resource/js/components/SearchTypeahead.js
@@ -170,30 +170,6 @@ export default class SearchTypeahead extends React.Component {
       </div>
     );
   }
-
-  getHelpElement() {
-    return <table className="table m-1">
-              <caption className="text-left text-primary p-2 pb-2">
-                <h5 className="m-1"><i className="icon-magnifier pr-2"/>Search Help</h5>
-              </caption>
-              <tr>
-                <td className="text-right mt-0 p-1 "><code>keyword</code></td>
-                <th><h6 className="m-0 pt-1">記事名 or 本文に<samp>"keyword"</samp>を含む</h6></th>
-              </tr>
-              <tr>
-                <td className="text-right mt-0 p-1"><code>title:keyword</code></td>
-                <th><h6 className="m-0 pt-1">記事名に<samp>"keyword"</samp>を含む</h6></th>
-              </tr>
-              <tr>
-                <td className="text-right mt-0 p-1"><code>a b</code></td>
-                <th><h6 className="m-0 pt-1">文字列<samp>"a"</samp>と<samp>"b"</samp>を含む (スペース区切り)</h6></th>
-              </tr>
-              <tr>
-                <td className="text-right mt-0 p-1"><code>-keyword</code></td>
-                <th><h6 className="m-0 pt-1">文字列<samp>"keyword"</samp>を含まない</h6></th>
-              </tr>
-            </table>;
-  }
 }
 
 /**
