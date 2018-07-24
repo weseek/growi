8ea46a92d84da6fe415d7c1cdf53a524390bc8e8
Yuki Takei <yuki@weseek.co.jp>
2018-07-24 20:02:52 +0900

    remove SESSION_NAME from env.dev.js

--- config/env.dev.js
+++ config/env.dev.js
@@ -1,7 +1,6 @@
 module.exports = {
   NODE_ENV: 'development',
   FILE_UPLOAD: 'local',
-  SESSION_NAME: 'connect.growi-dev.sid',
   // MATHJAX: 1,
   ELASTICSEARCH_URI: 'http://localhost:9200/growi',
   HACKMD_URI: 'http://localhost:3010',
