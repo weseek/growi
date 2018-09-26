4ec79d3a4f76ae73cec8caec6739a59b206ef9b8
Yuki Takei <yuki@weseek.co.jp>
2018-09-26 23:41:44 +0900

    clean code

--- config/migrate.js
+++ config/migrate.js
@@ -1,10 +1,17 @@
-const getMongoUri = (env) => {
+/**
+ * Configuration file for migrate-mongo
+ * @see https://github.com/seppevs/migrate-mongo
+ *
+ * @author Yuki Takei <yuki@weseek.co.jp>
+ */
+
+function getMongoUri(env) {
   return env.MONGOLAB_URI || // for B.C.
     env.MONGODB_URI || // MONGOLAB changes their env name
     env.MONGOHQ_URL ||
     env.MONGO_URI ||
     ((env.NODE_ENV === 'test') ? 'mongodb://localhost/growi_test' : 'mongodb://localhost/growi');
-};
+}
 
 const mongoUri = getMongoUri(process.env);
 const match = mongoUri.match(/^(.+)\/([^/]+)$/);
