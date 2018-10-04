a6ea01fb95070e1c480c4448ad099eca6bf47b1a
utsushiiro <ozeki.haru@gmail.com>
2018-10-04 20:15:33 +0900

    feedback

--- src/client/js/util/markdown-it/table-with-handsontable-button.js
+++ src/client/js/util/markdown-it/table-with-handsontable-button.js
@@ -8,7 +8,7 @@ export default class TableWithHandsontableButtonConfigurer {
     md.renderer.rules.table_open = (tokens, idx) => {
       const beginLine = tokens[idx].map[0] + 1;
       const endLine  = tokens[idx].map[1];
-      return `<div class="editable-with-handsontable"><button onClick="crowi.launchHandsontableModal('page', ${beginLine}, ${endLine})"><i class="icon-note"></i></button><table class="table table-bordered">`;
+      return `<div class="editable-with-handsontable"><button class="handsontable-modal-trigger" onClick="crowi.launchHandsontableModal('page', ${beginLine}, ${endLine})"><i class="icon-note"></i></button><table class="table table-bordered">`;
     };
 
     md.renderer.rules.table_close = (tokens, idx) => {
