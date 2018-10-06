fc266b16a79ac634d0ba6a6171cb0adf5d56029a
utsushiiro <ozeki.haru@gmail.com>
2018-10-06 17:24:47 +0900

    wip

--- src/client/js/components/PageEditor/HandsontableModal.jsx
+++ src/client/js/components/PageEditor/HandsontableModal.jsx
@@ -18,10 +18,110 @@ export default class HandsontableModal extends React.Component {
     this.state = {
       show: false,
       markdownTableOnInit: HandsontableModal.getDefaultMarkdownTable(),
-      markdownTable: HandsontableModal.getDefaultMarkdownTable()
+      markdownTable: HandsontableModal.getDefaultMarkdownTable(),
+      handsontableSetting: HandsontableModal.getDefaultHandsotableSetting()
     };
 
-    this.settings = {
+    this.init = this.init.bind(this);
+    this.reset = this.reset.bind(this);
+    this.cancel = this.cancel.bind(this);
+    this.save = this.save.bind(this);
+  }
+
+  init(markdownTable) {
+    const initMarkdownTable = markdownTable || HandsontableModal.getDefaultMarkdownTable();
+    const initCallback = function() {
+      const align = initMarkdownTable.options.align;
+      for (let i = 0; i < align.length; i++) {
+        const mapping = {
+          'r': 'htRight',
+          'c': 'htCenter',
+          'l': 'htLeft',
+          '': ''
+        };
+        HandsontableModal.alignColumns(this, i, i, mapping[align[i]]);
+      }
+    };
+    this.setState(
+      {
+        markdownTableOnInit: initMarkdownTable,
+        markdownTable: initMarkdownTable.clone(),
+        handsontableSetting: Object.assign({}, this.state.handsontableSetting, { init: initCallback })
+      }
+    );
+  }
+
+  show(markdownTable) {
+    this.init(markdownTable);
+    this.setState({ show: true });
+  }
+
+  reset() {
+    this.setState({ markdownTable: this.state.markdownTableOnInit.clone() });
+  }
+
+  cancel() {
+    this.setState({ show: false });
+  }
+
+  save() {
+    if (this.props.onSave != null) {
+      this.props.onSave(this.state.markdownTable);
+    }
+    // console.log(this.refs.hotTable.hotInstance.getCellMetaAtRow(0));
+    // console.log(this.refs.hotTable.hotInstance.getCellMeta(0, 0));
+    console.log(this.state.handsontableSetting);
+    this.setState({ show: false });
+  }
+
+  render() {
+    return (
+      <Modal show={this.state.show} onHide={this.cancel} bsSize="large">
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Table</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="p-0">
+          <Navbar>
+            <Navbar.Form>
+              <ButtonGroup>
+                <Button><i className="ti-align-left"></i></Button>
+                <Button><i className="ti-align-center"></i></Button>
+                <Button><i className="ti-align-right"></i></Button>
+              </ButtonGroup>
+            </Navbar.Form>
+          </Navbar>
+          <div className="p-4">
+            <HotTable ref='hotTable' data={this.state.markdownTable.table} settings={this.state.handsontableSetting} />
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <div className="d-flex justify-content-between">
+            <Button bsStyle="danger" onClick={this.reset}>Reset</Button>
+            <div className="d-flex">
+              <Button bsStyle="default" onClick={this.cancel}>Cancel</Button>
+              <Button bsStyle="primary" onClick={this.save}>Done</Button>
+            </div>
+          </div>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+  static getDefaultMarkdownTable() {
+    return new MarkdownTable(
+      [
+        ['col1', 'col2', 'col3'],
+        ['', '', ''],
+        ['', '', ''],
+      ],
+      {
+        align: ['', '', '']
+      }
+    );
+  }
+
+  static getDefaultHandsotableSetting() {
+    return {
       height: 300,
       rowHeaders: true,
       colHeaders: true,
@@ -57,80 +157,8 @@ export default class HandsontableModal extends React.Component {
         }
       },
       stretchH: 'all',
-      selectionMode: 'multiple',
+      selectionMode: 'multiple'
     };
-
-    this.init = this.init.bind(this);
-    this.reset = this.reset.bind(this);
-    this.cancel = this.cancel.bind(this);
-    this.save = this.save.bind(this);
-  }
-
-  init(markdownTable) {
-    const initMarkdownTable = markdownTable || HandsontableModal.getDefaultMarkdownTable();
-    this.setState({ markdownTableOnInit: initMarkdownTable });
-    this.setState({ markdownTable: initMarkdownTable.clone() });
-  }
-
-  show(markdownTable) {
-    this.init(markdownTable);
-    this.setState({ show: true });
-  }
-
-  reset() {
-    this.setState({ markdownTable: this.state.markdownTableOnInit.clone() });
-  }
-
-  cancel() {
-    this.setState({ show: false });
-  }
-
-  save() {
-    if (this.props.onSave != null) {
-      this.props.onSave(this.state.markdownTable);
-    }
-    this.setState({ show: false });
-  }
-
-  render() {
-    return (
-      <Modal show={this.state.show} onHide={this.cancel} bsSize="large">
-        <Modal.Header closeButton>
-          <Modal.Title>Edit Table</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="p-0">
-          <Navbar>
-            <Navbar.Form>
-              <ButtonGroup>
-                <Button><i className="ti-align-left"></i></Button>
-                <Button><i className="ti-align-center"></i></Button>
-                <Button><i className="ti-align-right"></i></Button>
-              </ButtonGroup>
-            </Navbar.Form>
-          </Navbar>
-          <div className="p-4">
-            <HotTable data={this.state.markdownTable.table} settings={this.settings} />
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <div className="d-flex justify-content-between">
-            <Button bsStyle="danger" onClick={this.reset}>Reset</Button>
-            <div className="d-flex">
-              <Button bsStyle="default" onClick={this.cancel}>Cancel</Button>
-              <Button bsStyle="primary" onClick={this.save}>Done</Button>
-            </div>
-          </div>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
-
-  static getDefaultMarkdownTable() {
-    return new MarkdownTable([
-      ['col1', 'col2', 'col3'],
-      ['', '', ''],
-      ['', '', ''],
-    ]);
   }
 
   static alignColumns(core, startCol, endCol, className) {
