1b80fc9ec0b8b94ff7611c96a12f31ecd7a22350
utsushiiro <ozeki.haru@gmail.com>
2018-10-09 18:01:28 +0900

    move the edit link embedding rule to another configurer

--- src/client/js/util/markdown-it/header.js
+++ src/client/js/util/markdown-it/header.js
@@ -22,14 +22,6 @@ export default class HeaderConfigurer {
         return self.renderToken(tokens, idx, options, env, self);
       }
     };
-
-    rules.heading_close = (tokens, idx) => {
-      return `<span class="revision-head-edit-button">
-                <a href="#edit" onClick="Crowi.setCaretLineData(parseInt(this.parentNode.parentNode.dataset.line, 10))">
-                  <i class="icon-note"></i>
-                </a>
-              </span></${tokens[idx].tag}>`;
-    };
   }
 
   /**
