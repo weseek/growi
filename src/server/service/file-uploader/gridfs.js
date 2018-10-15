3d20a4576dc7ad5e2df774e4b436e1c5775952c7
yusueketk <yusuketk@weseek.co.jp>
2018-10-15 13:40:38 +0900

    add method for file uploading to gridfs

--- src/server/service/file-uploader/gridfs.js
+++ src/server/service/file-uploader/gridfs.js
@@ -6,33 +6,57 @@ module.exports = function(crowi) {
   var debug = require('debug')('growi:service:fileUploaderLocal')
   var fs = require('fs');
   var mongoose = require('mongoose');
+  var gridfs = require('gridfs-stream');
+  var path = require('path');
   var lib = {};
-  var Attachment = {};
 
+  mongoose.connect('mongodb://localhost/growi', {
+    useNewUrlParser: true
+  });
+  mongoose.Promise = global.Promise;
+  gridfs.mongo = mongoose.mongo;
+  var connection = mongoose.connection;
+  connection.on('error', console.error.bind(console, 'connection error:'));
+
+  lib.uploadFile = function (filePath, contentType, fileStream, options) {
+    debug('File uploading: ' + filePath);
+    return new Promise(function (resolve, reject) {
+      // connection.once('open', function () {
+        var gfs = gridfs(connection.db);
+
+        // Writing a file from local to MongoDB
+        var writestream = gfs.createWriteStream({ filename: 'test.jpg' });
+        fs.createReadStream("public/uploads/attachment/5bb73b688ea417589dbd503f/14e456e38cea76bbca34dfdab712b909.jpg").pipe(writestream);
+        writestream.on('close', function (file) {
+          resolve(file);
+        });
+      });
+    // });
+  };
   // // mongoose connect
   // mongoose.connect('mongodb://localhost/test');
 
-  // instantiate mongoose-gridfs
-  var gridfs = require('mongoose-gridfs')({
-    collection: 'attachments',
-    model: 'Attachment',
-    mongooseConnection: mongoose.connection
-  });
+  // // instantiate mongoose-gridfs
+  // var gridfs = require('mongoose-gridfs')({
+  //   collection: 'attachments',
+  //   model: 'Attachment',
+  //   mongooseConnection: mongoose.connection
+  // });
 
-  // obtain a model
-  Attachment = gridfs.model;
+  // // obtain a model
+  // Attachment = gridfs.model;
 
-  // create or save a file
-  lib.uploadFile = function (filePath, contentType, fileStream, options) {
-    Attachment.write({
-      filePath: filePath,
-      contentType: contentType
-    },
-      fs.createReadStream(fileStream.path),
-      function (error, createdFile) {
-        debug('Failed to upload ' + createdFile + 'to gridFS', error);
-      });
-  };
+  // // create or save a file
+  // lib.uploadFile = function (filePath, contentType, fileStream, options) {
+  //   Attachment.write({
+  //     filePath: filePath,
+  //     contentType: contentType
+  //   },
+  //     fs.createReadStream(fileStream.path),
+  //     function (error, createdFile) {
+  //       debug('Failed to upload ' + createdFile + 'to gridFS', error);
+  //     });
+  // };
 
   // for larger file size
   // read a file and receive a stream
@@ -48,7 +72,9 @@ module.exports = function(crowi) {
   // Attachment.unlinkById(objectid, function (error, unlinkedAttachment) {
   //   debug('Failed to remove ' + unlinkedAttachment + 'in gridFS', error);
   // });
+  lib.generateUrl = function (filePath) {
+    return path.posix.join('/uploads', filePath);
+  };
 
   return lib;
 };
-
