7501867fcfe4f57421deeceff0405755486d3f4d
Yuki Takei <yuki@weseek.co.jp>
2018-10-19 22:47:45 +0900

    add comment

--- src/migrations/20181019114028-abolish-page-group-relation.js
+++ src/migrations/20181019114028-abolish-page-group-relation.js
@@ -6,6 +6,20 @@ const logger = require('@alias/logger')('growi:migrate:abolish-page-group-relati
 const mongoose = require('mongoose');
 const config = require('@root/config/migrate');
 
+/**
+ * BEFORE
+ *   - 'pagegrouprelations' collection exists (related to models/page-group-relation.js)
+ *     - schema:
+ *       {
+ *         "_id" : ObjectId("5bc9de4d745e137e0424ed89"),
+ *         "targetPage" : ObjectId("5b028f13c1f7ba2e58d2fd21"),
+ *         "relatedGroup" : ObjectId("5b07e6e6929bad5d3cce9995"),
+ *         "__v" : 0
+ *       }
+ * AFTER
+ *   - 'pagegrouprelations' collection is dropped and models/page-group-relation.js is removed
+ *   - Page model has 'grantedGroup' field newly
+ */
 module.exports = {
 
   async up(db) {
