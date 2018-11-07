be91cd4d6a957574b9fb79a170b58e3e60e63422
utsushiiro <ozeki.haru@gmail.com>
2018-11-08 00:40:56 +0900

    merge resolve.modules of webpack.dev with that of webpack.common

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -24,7 +24,6 @@ module.exports = require('./webpack.common')({
     'js/dev': './src/client/js/dev',
   },
   resolve: {
-    // TODO merge in webpack.common.js
     modules: [path.join(process.env.HOME, '.node_modules')],
   },
   module: {
