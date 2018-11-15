78fdd48a8e4f32c9651615ef40b3cadac0d81c63
Yuto Iwata <jyuchan415@gmail.com>
2018-11-15 17:20:59 +0900

    Add React Component for Installer Form.

---
+++ src/client/js/components/InstallerForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class InstallerForm extends React.Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+InstallerForm.propTypes = {
+};
