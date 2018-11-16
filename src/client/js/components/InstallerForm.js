c457516f1bee9fbfe30685505e2a35d97692e5f4
Yuto Iwata <jyuchan415@gmail.com>
2018-11-16 14:07:34 +0900

    Enabled translation in installer.

--- src/client/js/components/InstallerForm.js
+++ src/client/js/components/InstallerForm.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { translate } from 'react-i18next';
 
-export default class InstallerForm extends React.Component {
+class InstallerForm extends React.Component {
   render() {
     return (
       <div>
         <div className="input-group" id="input-group-username">
           <span className="input-group-addon"><i className="icon-user"></i></span>
-          <input type="text" className="form-control" placeholder="{{ t('User ID') }}" name="registerForm[username]" value="{{ req.body.registerForm.username }}" required />
+          <input type="text" className="form-control" placeholder={ this.props.t('User ID') } name="registerForm[username]" value="{{ req.body.registerForm.username }}" required />
         </div>
         <p className="help-block">
           <span id="help-block-username"></span>
@@ -15,24 +16,24 @@ export default class InstallerForm extends React.Component {
 
         <div className="input-group">
           <span className="input-group-addon"><i className="icon-tag"></i></span>
-          <input type="text" className="form-control" placeholder="{{ t('Name') }}" name="registerForm[name]" value="{{ googleName|default(req.body.registerForm.name) }}" required />
+          <input type="text" className="form-control" placeholder={ this.props.t('Name') } name="registerForm[name]" value="{{ googleName|default(req.body.registerForm.name) }}" required />
         </div>
 
         <div className="input-group">
           <span className="input-group-addon"><i className="icon-envelope"></i></span>
-          <input type="email" className="form-control" placeholder="{{ t('Email') }}" name="registerForm[email]" value="{{ googleEmail|default(req.body.registerForm.email) }}" required />
+          <input type="email" className="form-control" placeholder={ this.props.t('Email') } name="registerForm[email]" value="{{ googleEmail|default(req.body.registerForm.email) }}" required />
         </div>
 
         <div className="input-group">
           <span className="input-group-addon"><i className="icon-lock"></i></span>
-          <input type="password" className="form-control" placeholder="{{ t('Password') }}" name="registerForm[password]" required />
+          <input type="password" className="form-control" placeholder={ this.props.t('Password') } name="registerForm[password]" required />
         </div>
 
         <input type="hidden" name="_csrf" value="{{ csrf() }}" />
         <div className="input-group m-t-30 m-b-20 d-flex justify-content-center">
           <button type="submit" className="fcbtn btn btn-success btn-1b btn-register">
             <span className="btn-label"><i className="icon-user-follow"></i></span>
-            {{ t('Create') }}
+            { this.props.t('Create') }
           </button>
         </div>
       </div>
@@ -41,4 +42,7 @@ export default class InstallerForm extends React.Component {
 }
 
 InstallerForm.propTypes = {
+  t: PropTypes.func.isRequired,               // i18next
 };
+
+export default translate()(InstallerForm);
