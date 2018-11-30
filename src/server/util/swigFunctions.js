ddaef74a9160acdb446f1e78b82568aa00b5d1de
utsushiiro <ozeki.haru@gmail.com>
2018-11-30 18:06:01 +0900

    wip

--- src/server/util/swigFunctions.js
+++ src/server/util/swigFunctions.js
@@ -41,8 +41,24 @@ module.exports = function(crowi, app, req, locals) {
     return fontSize;
   };
 
+  /**
+   * @see ConfigManager#getConfig
+   */
   locals.getConfig = function(namespace, key) {
     return crowi.configManager.getConfig(namespace, key);
+  };
+
+  /**
+   * **Do not use this unless absolutely necessary. Use getConfig instead.**
+   */
+  locals.getConfigFromDB = function(namespace, key) {
+    return crowi.configManager.getConfigFromDB(namespace, key);
+  };
+  /**
+   * **Do not use this unless absolutely necessary. Use getConfig instead.**
+   */
+  locals.getConfigFromEnvVars = function(namespace, key) {
+    return crowi.configManager.getConfigFromEnvVars(namespace, key);
   };
 
   /**
