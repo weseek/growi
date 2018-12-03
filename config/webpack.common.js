0c1d9ffd2bb87483b9fc48920bbb9dacb36509c1
Yuki Takei <yuki@weseek.co.jp>
2018-12-03 23:28:39 +0900

    ensure to babelify growi plugins in node_modules

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -77,6 +77,7 @@ module.exports = (options) => {
           exclude: {
             test:    helpers.root('node_modules'),
             exclude: [  // include as a result
+              { test: helpers.root('node_modules', 'growi-plugin-') },
               helpers.root('node_modules/codemirror/src'),
               helpers.root('node_modules/string-width'),
               helpers.root('node_modules/is-fullwidth-code-point'), // depends from string-width
