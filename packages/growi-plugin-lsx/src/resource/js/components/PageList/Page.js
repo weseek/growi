3a05dc821e49c531ca2dc2e56a7f62bed9322695
Yuki Takei <yuki@weseek.co.jp>
2018-12-04 20:05:49 +0900

    remove PageListMeta component

--- packages/growi-plugin-lsx/src/resource/js/components/PageList/Page.js
+++ packages/growi-plugin-lsx/src/resource/js/components/PageList/Page.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import PageListMeta from '@client/js/components/PageList/PageListMeta';
+
 import { LsxContext } from '../../util/LsxContext';
-import { PageListMeta } from './PageListMeta';
 import { PagePath } from './PagePath';
 import { PageNode } from '../PageNode';
 
