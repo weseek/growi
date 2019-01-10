7926a99ad9c4f9cd4681806619d1ae2242c2966c
Yuto Iwata <yuto@weseek.co.jp>
2019-01-10 20:56:32 +0900

    call growiRenderer(WIP), and add comment.

--- src/client/js/util/reveal/plugins/growi-renderer.js
+++ src/client/js/util/reveal/plugins/growi-renderer.js
@@ -2,12 +2,14 @@
  * reveal.js growi-renderer plugin.
  */
 (function(root) {
-  // const GrowiRenderer = require('../../GrowiRenderer');
-  // let growiRenderer = new GrowiRenderer(root.crowi, root.crowiRenderer, {mode: 'editor'});
-  // growiRenderer.setup();
+  // put 'hoge' on each slide.
   let sections = document.querySelectorAll( '[data-markdown]'), section;
   for (let i = 0, len = sections.length; i < len; i++ ) {
     section = sections[i];
     section.innerHTML = '<h1>hoge</h1>';
   }
+  const GrowiRenderer = require('../../GrowiRenderer');
+  let growiRenderer = new GrowiRenderer(root.crowi, root.crowiRenderer, {mode: 'editor'});
+  growiRenderer.setup();
+  // TODO: retract code block by GrowiRenderer in GC-1354.
 }(this));
