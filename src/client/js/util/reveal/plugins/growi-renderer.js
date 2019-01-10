efcdb472348317b548ccf2bd392f139a15c649d9
Yuto Iwata <yuto@weseek.co.jp>
2019-01-10 18:58:16 +0900

    disabled reveal marked/markdown plugin, and change growi-renderer plugin to make slide reveal hoge.

--- src/client/js/util/reveal/plugins/growi-renderer.js
+++ src/client/js/util/reveal/plugins/growi-renderer.js
@@ -5,4 +5,9 @@
   // const GrowiRenderer = require('../../GrowiRenderer');
   // let growiRenderer = new GrowiRenderer(root.crowi, root.crowiRenderer, {mode: 'editor'});
   // growiRenderer.setup();
+  let sections = document.querySelectorAll( '[data-markdown]'), section;
+  for (let i = 0, len = sections.length; i < len; i++ ) {
+    section = sections[i];
+    section.innerHTML = '<h1>hoge</h1>';
+  }
 }(this));
