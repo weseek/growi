bf5dd581ca9658248cb261b2b8f94ecc2824ec56
Yuto Iwata <yuto@weseek.co.jp>
2019-01-17 18:17:29 +0900

    access crowi instance in parent window.

--- src/client/js/util/reveal/plugins/growi-renderer.js
+++ src/client/js/util/reveal/plugins/growi-renderer.js
@@ -1,7 +1,7 @@
 /**
  * reveal.js growi-renderer plugin.
  */
-(function(root) {
+(function() {
   // put 'hoge' on each slide.
   let sections = document.querySelectorAll( '[data-markdown]'), section;
   for (let i = 0, len = sections.length; i < len; i++ ) {
@@ -13,8 +13,10 @@
     h1.appendChild(document.createTextNode('Hoge'));
     section.appendChild(h1);
   }
-  const GrowiRenderer = require('../../GrowiRenderer');
-  let growiRenderer = new GrowiRenderer(root.crowi, root.crowiRenderer, {mode: 'editor'});
+  const GrowiRenderer = require('../../GrowiRenderer').default;
+  // parent window DOM (crowi.js) of presentation window.
+  let parentWindow = window.parent;
+  let growiRenderer = new GrowiRenderer(parentWindow.crowi, parentWindow.crowiRenderer, {mode: 'editor'});
   growiRenderer.setup();
   // TODO: retract code block by GrowiRenderer in GC-1354.
-}(this));
+}());
