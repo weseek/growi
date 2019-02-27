93abb2e8ffc074c59988461bbb0378e833328b52
yusuketk <yusuketk@weseek.co.jp>
2019-02-27 20:19:40 +0900

    remove unnecessary methods

--- src/server/models/tag.js
+++ src/server/models/tag.js
@@ -12,46 +12,5 @@ module.exports = function(crowi) {
     },
   });
 
-  /**
-   * create a tag (Promise wrapper)
-   */
-  tagSchema.statics.createTag = function(tag) {
-    debug('create new tag:', tag);
-    return new Promise((resolve, reject) => {
-      this.create({name: tag}, function(err, createdTag) {
-        if (err) {
-          reject(err);
-        }
-        resolve(createdTag);
-      });
-    });
-  };
-
-  /**
-   * get a tag by id(Promise wrpper)
-   */
-  tagSchema.statics.getOneById = function(id) {
-    return new Promise((resolve, reject) => {
-      this.find({
-        _id: id
-      }, function(err, tags) {
-        if (err) {
-          reject(err);
-        }
-        resolve(tags[0]);
-      });
-    });
-  };
-
-  tagSchema.statics.removeById = function(tagId) {
-    const Tag = this;
-    Tag.remove({_id: tagId}, function(err, done) {
-      if (err) {
-        throw new Error(err);
-      }
-      debug('deleta tag:', tagId);
-    });
-  };
-
   return mongoose.model('Tag', tagSchema);
 };
