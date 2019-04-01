926b053301dc736ce40beebd41dcc23915fe9847
yusuketk <yusuketk@weseek.co.jp>
2019-04-01 15:31:28 +0900

    adjust

--- src/client/js/components/TagsListPage.jsx
+++ src/client/js/components/TagsListPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 // SearchTypeahead wrapper
 export default class TagsListPage extends React.Component {
