1cb44846c769c9036669056dd5130ecc69873664
Yuki Takei <yuki@weseek.co.jp>
2019-04-16 14:34:28 +0900

    add test

--- packages/growi-commons/src/test/plugin/util/option-parser.test.js
+++ packages/growi-commons/src/test/plugin/util/option-parser.test.js
@@ -15,10 +15,20 @@ describe('option-parser', () => {
     ${'aaa'}
     ${'5++2'}
     ${'5:+2'}
-  `.describe.only('.parseRange(\'$arg\')', ({ arg }) => {
-    test('returns null', () => {
-      expect(OptionParser.parseRange(arg)).toBeNull();
-    });
+  `.test('.parseRange(\'$arg\') returns null', ({ arg }) => {
+    expect(OptionParser.parseRange(arg)).toBeNull();
+  });
+
+  each`
+    arg       | start | end
+    ${'1'}    | ${1} | ${1}
+    ${'2:1'}  | ${2} | ${1}
+    ${'2:'}   | ${2} | ${-1}
+    ${'10:-3'}   | ${10} | ${-3}
+    ${'5+2'}   | ${5} | ${7}
+    ${'5+'}   | ${5} | ${5}
+  `.test('.parseRange(\'$arg\') returns { start: $start, end : $end }', ({ arg, start, end }) => {
+    expect(OptionParser.parseRange(arg)).toEqual({ start, end });
   });
 
 });
