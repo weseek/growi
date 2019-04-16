ee74e533658724fbe213b9549c3011bc73e055f4
Yuki Takei <yuki@weseek.co.jp>
2019-04-16 14:49:24 +0900

    add OptionParser to index.js

--- packages/growi-commons/src/index.js
+++ packages/growi-commons/src/index.js
@@ -1,4 +1,7 @@
 module.exports = {
   BasicInterceptor: require('./util/basic-interceptor'),
   pathUtils: require('./util/path-utils'),
+  plugin: {
+    OptionParser: require('./plugin/util/option-parser'),
+  },
 };
