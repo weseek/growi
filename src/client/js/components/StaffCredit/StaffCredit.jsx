d650be596b984f73f3586a91313dddd9fcc33930
Kazuya_Nagase <al15065@shibaura-it.ac.jp>
2019-04-26 17:29:22 +0900

    :new: StaffCredit componentの雛形を作成

---
+++ src/client/js/components/StaffCredit/StaffCredit.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withTranslation } from 'react-i18next';
+
+import FormGroup from 'react-bootstrap/es/FormGroup';
+import FormControl from 'react-bootstrap/es/FormControl';
+import ListGroup from 'react-bootstrap/es/ListGroup';
+import ListGroupItem from 'react-bootstrap/es/ListGroupItem';
+import Modal from 'react-bootstrap/es/Modal';
+
+const SPECIFIED_GROUP_VALUE = 'specifiedGroup';
+
+/**
+ * Page staff credit component
+ *
+ * @export
+ * @class StaffCredit
+ * @extends {React.Component}
+ */
+class StaffCredit extends React.Component {
+
+  constructor() {
+
+  }
+
+    render() {
+        return (
+            <h1>ここに</h1>
+        );
+    }
+}
