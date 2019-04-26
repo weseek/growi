c1c7da49adf6bdb5cb89f9c67e9b79fdcc5ad836
Kazuya_Nagase <al15065@shibaura-it.ac.jp>
2019-04-26 17:53:43 +0900

    :syringe: CIのエラーの指摘箇所リファクタ

--- src/client/js/components/StaffCredit/StaffCredit.jsx
+++ src/client/js/components/StaffCredit/StaffCredit.jsx
@@ -1,14 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withTranslation } from 'react-i18next';
-
-import FormGroup from 'react-bootstrap/es/FormGroup';
-import FormControl from 'react-bootstrap/es/FormControl';
-import ListGroup from 'react-bootstrap/es/ListGroup';
-import ListGroupItem from 'react-bootstrap/es/ListGroupItem';
-import Modal from 'react-bootstrap/es/Modal';
-
-const SPECIFIED_GROUP_VALUE = 'specifiedGroup';
 
 /**
  * Page staff credit component
@@ -19,13 +9,13 @@ const SPECIFIED_GROUP_VALUE = 'specifiedGroup';
  */
 class StaffCredit extends React.Component {
 
-  constructor() {
-
+  render() {
+    return (
+      <p>スタッフロール</p>
+    );
   }
 
-    render() {
-        return (
-            <h1>スタッフロール</h1>
-        );
-    }
 }
+
+StaffCredit.propTypes = {
+};
