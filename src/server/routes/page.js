4e87d389c8af1dcb4562f09eb92891f50721c13a
yusuketk <yusuketk@weseek.co.jp>
2019-04-26 10:12:24 +0900

    remove console log

--- src/server/routes/page.js
+++ src/server/routes/page.js
@@ -436,7 +436,7 @@ module.exports = function(crowi, app) {
     const limit = 50;
     const offset = parseInt(req.query.offset) || 0;
     await addRenderVarsForDescendants(renderVars, path, req.user, offset, limit, true);
-console.log(renderVars);
+
     return res.render(view, renderVars);
   };
 
