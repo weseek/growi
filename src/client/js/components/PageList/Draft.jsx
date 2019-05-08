58d2839df2d3f19ffdbdcc10efe18c9d99fb67c9
mizozobu <hamc.m.so.j12@gmail.com>
2019-05-08 18:48:29 +0900

    draft style

--- src/client/js/components/PageList/Draft.jsx
+++ src/client/js/components/PageList/Draft.jsx
@@ -6,13 +6,17 @@ export default class Draft extends React.Component {
   render() {
     return (
       <li className="page-list-li d-flex align-items-center">
-        <a className="page-list-link" href={this.props.draft.path}>
+        <a href={`${this.props.draft.path}#edit`} target="_blank" rel="noopener noreferrer">
+          <button type="button" className="btn-primary p-0">
+            <span className="icon-note"></span>
+          </button>
+        </a>
+        <button type="button" className="btn-danger p-0" onClick={() => { return this.props.clearDraft(this.props.draft.path) }}>
+          <span className="icon-trash"></span>
+        </button>
+        <a className="page-list-link px-3" href={this.props.draft.path}>
           {this.props.draft.path}
         </a>
-        <a href={`${this.props.draft.path}#edit`} target="_blank" rel="noopener noreferrer">
-          <button type="button">Resume</button>
-        </a>
-        <button type="button" onClick={() => { return this.props.clearDraft(this.props.draft.path) }}>Delete</button>
       </li>
     );
   }
