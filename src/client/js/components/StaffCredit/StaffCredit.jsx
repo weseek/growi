8371b02a458b94c45b3453e3e0b174ffab48acce
Kazuya_Nagase <al15065@shibaura-it.ac.jp>
2019-05-17 13:38:26 +0900

    :new: staff creditにデザインを当てる

--- src/client/js/components/StaffCredit/StaffCredit.jsx
+++ src/client/js/components/StaffCredit/StaffCredit.jsx
@@ -11,7 +11,15 @@ export default class StaffCredit extends React.Component {
 
   render() {
     return (
-      <div className="text-center">staff credit</div>
+      <div className="text-center credit-curtain">
+        <div className="credit-body">
+          <p className="title my-5">Growi Soncho</p>
+          <span className="dev-position">1st</span>
+          <p className="dev-name mb-5">Sou Mizobuchi</p>
+          <span className="dev-position">2nd</span>
+          <p className="dev-name mb-5">Yusuke Takizawa</p>
+        </div>
+      </div>
     );
   }
 
