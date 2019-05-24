52c6d06d815018f40da7fdebd20f67064552e6ea
Kazuya_Nagase <al15065@shibaura-it.ac.jp>
2019-05-24 13:38:22 +0900

    :syringe:fix FB

--- src/client/js/components/StaffCredit/StaffCredit.jsx
+++ src/client/js/components/StaffCredit/StaffCredit.jsx
@@ -15,30 +15,23 @@ export default class StaffCredit extends React.Component {
     super(props);
 
     this.state = {
-      isShow: false,
+      isShown: false,
       userCommand: [],
     };
     this.konamiCommand = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', '5', '7', '3'];
+    this.deleteCredit = this.deleteCredit.bind(this);
   }
 
   @keydown('enter', 'up', 'down', 'right', 'left', '5', '7', '3')
   check(event) {
     if (this.konamiCommand[this.state.userCommand.length] === event.key) {
       const nextValue = this.state.userCommand.concat(event.key);
-      // 最後のコナミコマンドならuserCommandをカラにしてコンポーネントを表示
       if (nextValue.length === this.konamiCommand.length) {
         this.setState({
-          isShow: true,
+          isShown: true,
           userCommand: [],
         });
-        // クレジットが流れ終わるタイミングでコンポーネントを削除
-        (async() => {
-          const delay = (time) => { return new Promise((res) => { return setTimeout(() => { return res() }, time) }) };
-          await delay(15 * 1000);
-          this.deleteCredit();
-        })();
       }
-      // 入力されたコマンドがコナミコマンドの次のコマンドならuserCommandに追加
       else {
         this.setState({ userCommand: nextValue });
       }
@@ -49,15 +42,15 @@ export default class StaffCredit extends React.Component {
   }
 
   deleteCredit() {
-    if (this.state.isShow) {
-      this.setState({ isShow: false });
+    if (this.state.isShown) {
+      this.setState({ isShown: false });
     }
   }
 
   render() {
-    if (this.state.isShow) {
+    if (this.state.isShown) {
       return (
-        <div className="text-center credit-curtain">
+        <div className="text-center credit-curtain" onClick={this.deleteCredit}>
           <div className="credit-body">
             <p className="title my-5">Growi Soncho</p>
             <span className="dev-position">1st</span>
