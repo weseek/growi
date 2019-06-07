e27d677e682a5873e8e8d87966b6b5879bdb3e71
Daisuke Takahashi <daisuke@extendwings.com>
2019-06-07 13:22:03 +0900

    Remove unused method

--- src/server/models/config.js
+++ src/server/models/config.js
@@ -347,11 +347,6 @@ module.exports = function(crowi) {
     return getValueForCrowiNS(config, key);
   };
 
-  configSchema.statics.isSameUsernameTreatedAsIdenticalUser = function(config, providerType) {
-    const key = `security:passport-${providerType}:isSameUsernameTreatedAsIdenticalUser`;
-    return getValueForCrowiNS(config, key);
-  };
-
   configSchema.statics.isUploadable = function(config) {
     const method = process.env.FILE_UPLOAD || 'aws';
 
