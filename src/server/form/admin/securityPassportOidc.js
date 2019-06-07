89cf395671ed0862f46576aa9a09a99fd1e5f058
Daisuke Takahashi <daisuke@extendwings.com>
2019-06-07 10:44:17 +0900

    Some OIDC provider returns display name separately.

--- src/server/form/admin/securityPassportOidc.js
+++ src/server/form/admin/securityPassportOidc.js
@@ -9,6 +9,7 @@ module.exports = form(
   field('settingForm[security:passport-oidc:clientSecret]').trim(),
   field('settingForm[security:passport-oidc:attrMapId]').trim(),
   field('settingForm[security:passport-oidc:attrMapUserName]').trim(),
+  field('settingForm[security:passport-oidc:attrMapName]').trim(),
   field('settingForm[security:passport-oidc:attrMapMail]').trim(),
   field('settingForm[security:passport-oidc:isSameEmailTreatedAsIdenticalUser]').trim().toBooleanStrict(),
   field('settingForm[security:passport-oidc:isSameUsernameTreatedAsIdenticalUser]').trim().toBooleanStrict(),
