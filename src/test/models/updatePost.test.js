200fb6fea743ce2302801186f4a1b0699cce5b12
Yuki Takei <yuki@weseek.co.jp>
2019-06-19 23:19:21 +0900

    normalize updatePost.test.js

--- src/test/models/updatePost.test.js
+++ src/test/models/updatePost.test.js
@@ -1,23 +1,31 @@
-const chai = require('chai');
-const sinonChai = require('sinon-chai');
+const mongoose = require('mongoose');
 
-const expect = chai.expect;
-const utils = require('../utils.js');
-
-chai.use(sinonChai);
+const { getInstance } = require('../setup-crowi');
 
 describe('UpdatePost', () => {
-  const UpdatePost = utils.models.UpdatePost;
+  // eslint-disable-next-line no-unused-vars
+  let crowi;
+  let UpdatePost;
+
+  beforeAll(async(done) => {
+    crowi = await getInstance();
+    done();
+  });
+
+  beforeEach(async(done) => {
+    UpdatePost = mongoose.model('UpdatePost');
+    done();
+  });
 
   describe('.createPrefixesByPathPattern', () => {
     describe('with a path', () => {
       test('should return right patternPrfixes', (done) => {
-        expect(UpdatePost.createPrefixesByPathPattern('/*')).to.deep.equal(['*', '*']);
-        expect(UpdatePost.createPrefixesByPathPattern('/user/*/日報*')).to.deep.equal(['user', '*']);
-        expect(UpdatePost.createPrefixesByPathPattern('/project/hoge/*')).to.deep.equal(['project', 'hoge']);
-        expect(UpdatePost.createPrefixesByPathPattern('/*/MTG/*')).to.deep.equal(['*', 'MTG']);
-        expect(UpdatePost.createPrefixesByPathPattern('自己紹介')).to.deep.equal(['*', '*']);
-        expect(UpdatePost.createPrefixesByPathPattern('/user/aoi/メモ/2016/02/10/xxx')).to.deep.equal(['user', 'aoi']);
+        expect(UpdatePost.createPrefixesByPathPattern('/*')).toEqual(['*', '*']);
+        expect(UpdatePost.createPrefixesByPathPattern('/user/*/日報*')).toEqual(['user', '*']);
+        expect(UpdatePost.createPrefixesByPathPattern('/project/hoge/*')).toEqual(['project', 'hoge']);
+        expect(UpdatePost.createPrefixesByPathPattern('/*/MTG/*')).toEqual(['*', 'MTG']);
+        expect(UpdatePost.createPrefixesByPathPattern('自己紹介')).toEqual(['*', '*']);
+        expect(UpdatePost.createPrefixesByPathPattern('/user/aoi/メモ/2016/02/10/xxx')).toEqual(['user', 'aoi']);
 
         done();
       });
@@ -27,12 +35,12 @@ describe('UpdatePost', () => {
   describe('.getRegExpByPattern', () => {
     describe('with a pattern', () => {
       test('should return right regexp', (done) => {
-        expect(UpdatePost.getRegExpByPattern('/*')).to.deep.equal(/^\/.*/);
-        expect(UpdatePost.getRegExpByPattern('/user/*/日報*')).to.deep.equal(/^\/user\/.*\/日報.*/);
-        expect(UpdatePost.getRegExpByPattern('/project/hoge/*')).to.deep.equal(/^\/project\/hoge\/.*/);
-        expect(UpdatePost.getRegExpByPattern('/*/MTG/*')).to.deep.equal(/^\/.*\/MTG\/.*/);
-        expect(UpdatePost.getRegExpByPattern('自己紹介')).to.deep.equal(/^\/.*自己紹介.*/);
-        expect(UpdatePost.getRegExpByPattern('/user/aoi/メモ/2016/02/10/xxx')).to.deep.equal(/^\/user\/aoi\/メモ\/2016\/02\/10\/xxx/);
+        expect(UpdatePost.getRegExpByPattern('/*')).toEqual(/^\/.*/);
+        expect(UpdatePost.getRegExpByPattern('/user/*/日報*')).toEqual(/^\/user\/.*\/日報.*/);
+        expect(UpdatePost.getRegExpByPattern('/project/hoge/*')).toEqual(/^\/project\/hoge\/.*/);
+        expect(UpdatePost.getRegExpByPattern('/*/MTG/*')).toEqual(/^\/.*\/MTG\/.*/);
+        expect(UpdatePost.getRegExpByPattern('自己紹介')).toEqual(/^\/.*自己紹介.*/);
+        expect(UpdatePost.getRegExpByPattern('/user/aoi/メモ/2016/02/10/xxx')).toEqual(/^\/user\/aoi\/メモ\/2016\/02\/10\/xxx/);
         done();
       });
     });
@@ -41,8 +49,8 @@ describe('UpdatePost', () => {
   describe('.normalizeChannelName', () => {
     describe('with a channel name', () => {
       test('should return true', (done) => {
-        expect(UpdatePost.normalizeChannelName('#pj-hoge')).to.be.equal('pj-hoge');
-        expect(UpdatePost.normalizeChannelName('pj-hoge')).to.be.equal('pj-hoge');
+        expect(UpdatePost.normalizeChannelName('#pj-hoge')).toEqual('pj-hoge');
+        expect(UpdatePost.normalizeChannelName('pj-hoge')).toEqual('pj-hoge');
 
         done();
       });
