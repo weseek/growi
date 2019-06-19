b0bd1a4567d3553e4686a5eee5194c14b14b2333
itizawa <itizawa@weseek.co.jp>
2019-06-19 14:55:41 +0900

    create migration file

---
+++ src/migrations/20190619055421-adjust-page-grant.js
@@ -0,0 +1,14 @@
+module.exports = {
+  up(db) {
+    // TODO write your migration here. Return a Promise (and/or use async & await).
+    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
+    // Example:
+    // return db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
+  },
+
+  down(db) {
+    // TODO write the statements to rollback your migration (if possible)
+    // Example:
+    // return db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+  },
+};
