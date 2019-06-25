65755ccf81c9484b98e55e356c31d6fc4e31a3be
Yuki Takei <yuki@weseek.co.jp>
2019-06-25 16:33:16 +0900

    impl migration file

--- src/migrations/20190624110950-fill-last-update-user.js
+++ src/migrations/20190624110950-fill-last-update-user.js
@@ -4,32 +4,40 @@ const logger = require('@alias/logger')('growi:migrate:abolish-page-group-relati
 const mongoose = require('mongoose');
 const config = require('@root/config/migrate');
 
+/**
+ * FIX https://github.com/weseek/growi/issues/1067
+ */
 module.exports = {
 
   async up(db) {
     logger.info('Apply migration');
     mongoose.connect(config.mongoUri, config.mongodb.options);
 
-    /*
-    await db.collection('pages').aggregate([
-      {
-        $match: {
-          $or: [
-            { lastUpdateUser: { $exists: false } },
-            { lastUpdateUser: { $eq: null } },
-          ],
-        },
-      },
-      {
-        $addFields: {
-          lastUpdateUser: '$creator',
-        },
-      },
-    ])
-      .out('pages')
-      .toArray();
+    const Page = require('@server/models/page')();
 
-    */
+    // see https://stackoverflow.com/questions/3974985/update-mongodb-field-using-value-of-another-field/37280419#37280419
+
+    // retrieve target data
+    const pages = await Page.find({
+      $or: [
+        { lastUpdateUser: { $exists: false } },
+        { lastUpdateUser: { $eq: null } },
+      ],
+    }).select('_id creator');
+
+    // create requests for bulkWrite
+    const requests = pages.map((page) => {
+      return {
+        updateOne: {
+          filter: { _id: page._id },
+          update: { $set: { lastUpdateUser: page.creator } },
+        },
+      };
+    });
+
+    if (requests.length > 0) {
+      await db.collection('pages').bulkWrite(requests);
+    }
 
     logger.info('Migration has successfully applied');
   },
