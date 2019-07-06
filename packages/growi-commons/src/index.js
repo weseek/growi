00ed729645fcf7ce08440cd5a1f3579acc0b2532
Yuki Takei <yuki@weseek.co.jp>
2019-07-07 03:12:04 +0900

    add env-utils

--- packages/growi-commons/src/index.js
+++ packages/growi-commons/src/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   BasicInterceptor: require('./util/basic-interceptor'),
   pathUtils: require('./util/path-utils'),
+  envUtils: require('./util/env-utils'),
   // plugin
   customTagUtils: require('./plugin/util/custom-tag-utils'),
   // service
