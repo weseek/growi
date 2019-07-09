4f045cce15219cef99acb8f9069064d8a7c442d6
熊谷洸介(Kousuke Kumagai) <kumagaikousuke1995@gmail.com>
2019-07-09 17:54:26 +0900

    htmlの必要な部分を移植した

--- src/client/js/components/Admin/Importer.jsx
+++ src/client/js/components/Admin/Importer.jsx
@@ -1,14 +1,22 @@
 import React, { Fragment } from 'react';
+import axios from 'axios';
 
 class Importer extends React.Component {
 
   constructor(props) {
     super();
+    this.state = {
+      userDatas: [],
+    };
+  }
+
+  componentWillMount() {
   }
 
   render() {
     return (
       <Fragment>
+
       </Fragment>
     );
   }
