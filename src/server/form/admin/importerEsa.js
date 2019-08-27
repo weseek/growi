2dd9c449237cadd955d9af83184a80204a491ffd
熊谷洸介(Kousuke Kumagai) <kumagaikousuke1995@gmail.com>
2019-08-27 12:00:02 +0900

    Commit parent branch

--- src/server/form/admin/importerEsa.js
+++
@@ -1,8 +0,0 @@
-const form = require('express-form');
-
-const field = form.field;
-
-module.exports = form(
-  field('settingForm[importer:esa:access_token]').required(),
-  field('settingForm[importer:esa:team_name]').required(),
-);
--- src/server/form/admin/importerQiita.js
+++
@@ -1,8 +0,0 @@
-const form = require('express-form');
-
-const field = form.field;
-
-module.exports = form(
-  field('settingForm[importer:qiita:access_token]').required(),
-  field('settingForm[importer:qiita:team_name]').required(),
-);
