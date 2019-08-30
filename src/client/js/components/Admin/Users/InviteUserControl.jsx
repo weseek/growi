ff411a18c1b6712a8ab0718b67d29bdbbf265d5a
itizawa <itizawa@weseek.co.jp>
2019-08-30 18:06:00 +0900

    add function

--- src/client/js/components/Admin/Users/InviteUserControl.jsx
+++ src/client/js/components/Admin/Users/InviteUserControl.jsx
@@ -7,12 +7,16 @@ import AppContainer from '../../../services/AppContainer';
 
 class InviteUserControl extends React.Component {
 
+  onUserInviteClicked() {
+    console.log('hello');
+  }
+
   render() {
     const { t } = this.props;
 
     return (
       <Fragment>
-        <button type="button" data-toggle="collapse" className="btn btn-default">
+        <button type="button" className="btn btn-default" onClick={this.onUserInviteClicked}>
           { t('user_management.invite_users') }
         </button>
       </Fragment>
