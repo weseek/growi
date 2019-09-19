6dcb468f48048d857092142d21adf9d74025d691
itizawa <itizawa@weseek.co.jp>
2019-09-19 14:27:08 +0900

    create base component

--- src/client/js/components/Admin/MarkdownSetting/MarkDownSetting.jsx
+++ src/client/js/components/Admin/MarkdownSetting/MarkDownSetting.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withTranslation } from 'react-i18next';
 
-import { createSubscribedElement } from '../UnstatedUtils';
-import WebsocketContainer from '../../services/WebsocketContainer';
+import { createSubscribedElement } from '../../UnstatedUtils';
+
+import AppContainer from '../../../services/AppContainer';
 
 class MarkdownSetting extends React.Component {
 
@@ -10,7 +12,6 @@ class MarkdownSetting extends React.Component {
     super(props);
 
     this.state = {
-      hoge:0
     };
   }
 
@@ -20,3 +21,15 @@ class MarkdownSetting extends React.Component {
   }
 
 }
+
+const MarkdownSettingWrapper = (props) => {
+  return createSubscribedElement(MarkdownSetting, props, [AppContainer]);
+};
+
+MarkdownSetting.propTypes = {
+  t: PropTypes.func.isRequired, // i18next
+  appContainer: PropTypes.instanceOf(AppContainer).isRequired,
+
+};
+
+export default withTranslation()(MarkdownSettingWrapper);
