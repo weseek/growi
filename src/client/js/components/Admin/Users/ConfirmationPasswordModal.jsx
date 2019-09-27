1810412ee7350d249379bc81048f274d4151bd92
itizawa <itizawa@weseek.co.jp>
2019-09-27 12:00:38 +0900

    show invited list

--- src/client/js/components/Admin/Users/ConfirmationPasswordModal.jsx
+++ src/client/js/components/Admin/Users/ConfirmationPasswordModal.jsx
@@ -9,16 +9,30 @@ import AppContainer from '../../../services/AppContainer';
 
 class ConfirmationPasswordModal extends React.Component {
 
+  renderCreatedEmail(userList) {
+    return userList.map((user) => {
+      return (
+        <tr key={user.email}>
+          <td>
+            <strong>{user.email}</strong>
+          </td>
+          <td>{user.password}</td>
+        </tr>
+      );
+    });
+  }
+
+  renderExistingEmail(emailList) {
+    return emailList.map((user) => {
+      return (
+        <p key={user}><strong>{user}</strong></p>
+      );
+    });
+  }
+
   render() {
     const { t } = this.props;
-
-    const renderCreatedEmail = () => {
-      return <p>helllo</p>;
-    };
-
-    const renderExistedEmail = () => {
-      return <p>helllo</p>;
-    };
+    const { createdUserList, existingEmailList } = this.props.invitedEmailList;
 
     return (
       <Modal show={this.props.show}>
@@ -31,26 +45,8 @@ class ConfirmationPasswordModal extends React.Component {
           <p>{ t('user_management.temporary_password') }</p>
           <p>{ t('user_management.send_new_password') }</p>
           <p className="text-danger">{ t('user_management.send_temporary_password') }</p>
-          {/* {this.props.invitedEmailList.map((email) => {
-              return (
-                <tr key={email._id}>
-                  <td>
-                    <strong>{email}</strong>
-                  </td>
-                  <td>{user.name}</td>
-                  <td>{user.email}</td>
-                  <td>{dateFnsFormat(new Date(user.createdAt), 'YYYY-MM-DD')}</td>
-                  <td>
-                    { user.lastLoginAt && <span>{dateFnsFormat(new Date(user.lastLoginAt), 'YYYY-MM-DD HH:mm')}</span> }
-                  </td>
-                  <td>
-                    <UserMenu user={user} onPasswordResetClicked={this.props.onPasswordResetClicked} />
-                  </td>
-                </tr>
-              );
-            })} */}
-          {renderCreatedEmail()}
-          {renderExistedEmail()}
+          {createdUserList && this.renderCreatedEmail(createdUserList)}
+          {existingEmailList && this.renderExistingEmail(existingEmailList)}
         </Modal.Body>
         <Modal.Footer className="d-flex">
         </Modal.Footer>
@@ -73,7 +69,7 @@ ConfirmationPasswordModal.propTypes = {
   appContainer: PropTypes.instanceOf(AppContainer).isRequired,
 
   show: PropTypes.bool.isRequired,
-  invitedEmailList: PropTypes.string,
+  invitedEmailList: PropTypes.object,
 };
 
 export default withTranslation()(ConfirmationPasswordModalWrapper);
