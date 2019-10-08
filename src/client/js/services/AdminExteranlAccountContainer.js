0ca1501ef666118bd378827c856a969b5a2fe383
WESEEK Kaito <kaito@weseek.co.jp>
2019-10-08 18:53:10 +0900

    fix class name

--- src/client/js/services/AdminExteranlAccountContainer.js
+++ src/client/js/services/AdminExteranlAccountContainer.js
@@ -9,7 +9,7 @@ const logger = loggerFactory('growi:services:UserGroupDetailContainer');
  * Service container for admin users page (Users.jsx)
  * @extends {Container} unstated Container
  */
-export default class AdminUsersContainer extends Container {
+export default class AdminExternalAccountContainer extends Container {
 
   constructor(appContainer) {
     super();
