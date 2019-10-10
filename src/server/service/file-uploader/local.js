a0c32df4adbfdd7adca596656a14038262c8aa49
Otani Haruhiko <haruhiko@weseek.co.jp>
2019-10-10 11:42:31 +0900

    Revert "Revert "refs GW-321: refacter""
    This reverts commit 049d1dc0240c68e31791838436d2dc4796b6dcfc.

--- src/server/service/file-uploader/local.js
+++ src/server/service/file-uploader/local.js
@@ -88,7 +88,8 @@ module.exports = function(crowi) {
    */
   lib.checkLimit = async(uploadFileSize) => {
     const maxFileSize = crowi.configManager.getConfig('crowi', 'app:maxFileSize');
-    return { isUploadable: uploadFileSize <= maxFileSize, errorMessage: 'File size exceeds the size limit per file' };
+    const totalLimit = crowi.configManager.getConfig('crowi', 'app:fileUploadTotalLimit');
+    return lib.doCheckLimit(uploadFileSize, maxFileSize, totalLimit);
   };
 
   return lib;
