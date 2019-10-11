ce4fa6f4078ed4db734feca21d8ed6a2363db4ff
itizawa <itizawa@weseek.co.jp>
2019-10-11 12:51:05 +0900

    adjust error

--- src/client/js/components/Admin/MarkdownSetting/XssForm.jsx
+++ src/client/js/components/Admin/MarkdownSetting/XssForm.jsx
@@ -95,8 +95,15 @@ class XssForm extends React.Component {
         <form className="row">
           <div className="form-group">
             <div className="col-xs-4 text-right">
-              <div className="checkbox checkbox-success" onChange={markDownSettingContainer.switchEnableXss}>
-                <input type="checkbox" id="XssEnable" className="form-check-input" name="isEnabledXss" checked={isEnabledXss} />
+              <div className="checkbox checkbox-success">
+                <input
+                  type="checkbox"
+                  id="XssEnable"
+                  className="form-check-input"
+                  name="isEnabledXss"
+                  checked={isEnabledXss}
+                  onChange={markDownSettingContainer.switchEnableXss}
+                />
                 <label htmlFor="XssEnable">
                   { t('markdown_setting.Enable XSS prevention') }
                 </label>
