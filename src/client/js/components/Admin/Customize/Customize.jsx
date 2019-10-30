7bd630943c2541f87b9b4d103e46ce0d90f9d80c
itizawa <itizawa@weseek.co.jp>
2019-10-30 12:58:39 +0900

    show header

--- src/client/js/components/Admin/Customize/Customize.jsx
+++ src/client/js/components/Admin/Customize/Customize.jsx
@@ -14,25 +14,24 @@ class Customize extends React.Component {
 
     return (
       <Fragment>
-        {/* fieldset + legend ではなく、row + header + フォームコンポーネントに書き換える(GC244着手時に対応) */}
-        <fieldset>
-          <legend>{t('customize_page.Layout')}</legend>
-          {/* レイアウトフォームの react componentをここで呼ぶ(GW-244) */}
-          <legend>{t('customize_page.Theme')}</legend>
-          {/* テーマフォームの react componentをここで呼ぶ(GW-245) */}
-          <legend>{t('customize_page.Behavior')}</legend>
-          {/* 挙動フォームの react componentをここで呼ぶ(GW-246) */}
-          <legend>{t('customize_page.Function')}</legend>
-          {/* 機能フォームの react componentをここで呼ぶ(GW-276) */}
-          <legend>{t('customize_page.Code Highlight')}</legend>
-          {/* コードハイライトフォームの react componentをここで呼ぶ(GW-277) */}
-          <legend>{t('customize_page.custom_title')}</legend>
-          {/* カスタムタイトルフォームの react componentをここで呼ぶ(GW-278) */}
-          <legend>{t('customize_page.Custom CSS')}</legend>
-          {/* カスタムCSSフォームの react componentをここで呼ぶ(GW-279) */}
-          <legend>{t('customize_page.Custom script')}</legend>
-          {/* カスタムスクリプトフォームの react componentをここで呼ぶ(GW-280) */}
-        </fieldset>
+        <div className="row my-3">
+          <h2>{t('customize_page.Layout')}</h2>
+          {/* レイアウトコンポーネントを設置する */}
+        </div>
+        <legend>{t('customize_page.Theme')}</legend>
+        {/* テーマフォームの react componentをここで呼ぶ(GW-245) */}
+        <legend>{t('customize_page.Behavior')}</legend>
+        {/* 挙動フォームの react componentをここで呼ぶ(GW-246) */}
+        <legend>{t('customize_page.Function')}</legend>
+        {/* 機能フォームの react componentをここで呼ぶ(GW-276) */}
+        <legend>{t('customize_page.Code Highlight')}</legend>
+        {/* コードハイライトフォームの react componentをここで呼ぶ(GW-277) */}
+        <legend>{t('customize_page.custom_title')}</legend>
+        {/* カスタムタイトルフォームの react componentをここで呼ぶ(GW-278) */}
+        <legend>{t('customize_page.Custom CSS')}</legend>
+        {/* カスタムCSSフォームの react componentをここで呼ぶ(GW-279) */}
+        <legend>{t('customize_page.Custom script')}</legend>
+        {/* カスタムスクリプトフォームの react componentをここで呼ぶ(GW-280) */}
       </Fragment>
     );
   }
