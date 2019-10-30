a0f0486c314b54136110c27a43c56bd7c5e5ba4b
itizawa <itizawa@weseek.co.jp>
2019-10-30 14:29:45 +0900

    remane

--- src/client/js/services/AdminCustomizeContainer.js
+++ src/client/js/services/AdminCustomizeContainer.js
@@ -4,7 +4,7 @@ import { Container } from 'unstated';
  * Service container for admin customize setting page (Customize.jsx)
  * @extends {Container} unstated Container
  */
-export default class CustomizeContainer extends Container {
+export default class AdminCustomizeContainer extends Container {
 
   constructor(appContainer) {
     super();
