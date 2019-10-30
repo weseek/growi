0c5623d1a798b726ee0ca5725dcdd4ff70fb30a2
itizawa <itizawa@weseek.co.jp>
2019-10-30 13:33:04 +0900

    Crowi Classic Layout

--- src/client/js/components/Admin/Customize/CustomizeLayoutSetting.jsx
+++ src/client/js/components/Admin/Customize/CustomizeLayoutSetting.jsx
@@ -51,6 +51,25 @@ class CustomizeLayoutSetting extends React.Component {
             <li>Affix Table-of-contents</li>
           </ul>
         </div>
+        <div className="col-sm-4">
+          <h4>
+            <div className="radio radio-primary">
+              <input type="radio" id="radioLayoutCrowi" name="settingForm[customize:layout]" value="crowi" onClick="selectableTheme(event)" />
+              <label htmlFor="radioLayoutCrowi">
+                  Crowi Classic Layout
+              </label>
+            </div>
+          </h4>
+          <a href="/images/admin/customize/layout-classic.gif" className="ss-container">
+            <img src="/images/admin/customize/layout-classic-thumb.gif" width="240px" />
+          </a>
+          <h4>Separated Functions</h4>
+          <ul>
+            <li>Collapsible Sidebar</li>
+            <li>Show and post comments in Sidebar</li>
+            <li>Collapsible Table-of-contents</li>
+          </ul>
+        </div>
       </form>
     );
   }
