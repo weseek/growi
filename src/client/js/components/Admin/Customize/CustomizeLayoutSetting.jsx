e25989a17819753679da6340f2f0c216322d9911
itizawa <itizawa@weseek.co.jp>
2019-10-30 13:27:15 +0900

    GROWI Enhanced Layout

--- src/client/js/components/Admin/Customize/CustomizeLayoutSetting.jsx
+++ src/client/js/components/Admin/Customize/CustomizeLayoutSetting.jsx
@@ -4,7 +4,34 @@ class CustomizeLayoutSetting extends React.Component {
 
   render() {
     return (
-      <p>test hoge</p>
+      <form>
+        <div className="col-sm-4">
+          <h4>
+            <div className="radio radio-primary">
+              <input
+                type="radio"
+                id="radioLayoutGrowi"
+                name="settingForm[customize:layout]"
+                value="growi"
+                onClick="selectableTheme(event)"
+              />
+              <label htmlFor="radioLayoutGrowi">
+                  GROWI Enhanced Layout <small className="text-success">(Recommended)</small>
+              </label>
+            </div>
+          </h4>
+          <a href="/images/admin/customize/layout-crowi-plus.gif" className="ss-container">
+            <img src="/images/admin/customize/layout-crowi-plus-thumb.gif" width="240px" />
+          </a>
+          <h4>Simple and Clear</h4>
+          <ul>
+            {/* TODO i18n */}
+            <li>Full screen layout and thin margins/paddings</li>
+            <li>Show and post comments at the bottom of the page</li>
+            <li>Affix Table-of-contents</li>
+          </ul>
+        </div>
+      </form>
     );
   }
 
