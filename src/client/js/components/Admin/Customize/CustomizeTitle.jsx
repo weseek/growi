a0ee3560eb4ddd99ea4bd3cc0e7a850b447b6f95
sooouh <souh@weseek.co.jp>
2019-11-13 13:07:24 +0900

    create customtitle jsx

--- src/client/js/components/Admin/Customize/CustomizeTitle.jsx
+++ src/client/js/components/Admin/Customize/CustomizeTitle.jsx
@@ -2,31 +2,78 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 
+import loggerFactory from '@alias/logger';
+
+import AppContainer from '../../../services/AppContainer';
 import AdminCustomizeContainer from '../../../services/AdminCustomizeContainer';
+import AdminUpdateButtonRow from '../Common/AdminUpdateButtonRow';
+import { createSubscribedElement } from '../../UnstatedUtils';
+import { toastSuccess, toastError } from '../../../util/apiNotification';
+
+const logger = loggerFactory('growi:customizeTitle');
 
 class CustomizeTitle extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.onClickSubmit = this.onClickSubmit.bind(this);
+  }
+
+  async onClickSubmit() {
+    const { t, adminCustomizeContainer } = this.props;
+
+    try {
+      await adminCustomizeContainer.updateCustomizeTitle();
+      toastSuccess(t('customize_page.update_title_success'));
+    }
+    catch (err) {
+      toastError(err);
+      logger.error(err);
+    }
+  }
+
   render() {
-    const { adminCustomizeContainer } = this.props;
+    const { t, appContainer, adminCustomizeContainer } = this.props;
 
     return (
       <div className="row">
         <div className="col-sm-4">
-          <CustomizeTitle>
+          <React.Fragment>
+            <h2>{t('customize_page.custom title')}</h2>
+            <p className="well">
+              { t('customize_page.custom title') }<br />
+              { t('customize_page.custom title') }
+            </p>
             {/* TODO i18n */}
-            <h4>Simple and Clear</h4>
+            <h4>title</h4>
+
             <ul>
-              <li>Full screen layout and thin margins/paddings</li>
-              <li>Show and post comments at the bottom of the page</li>
-              <li>Affix Table-of-contents</li>
+              <li>aaaaaaaaaaaaaaa</li>
+              <li>bbbbbbbbbbbbbbb</li>
+              <li>ccccccccccccccc</li>
             </ul>
-          </CustomizeTitle>
+            <div className="form-group">
+              <div className="col-xs-12">
+                <input
+                  id="customTitleSettingForm"
+                  value={appContainer.config.customizeTitle}
+                  onChange={(inputValue) => { adminCustomizeContainer.changeCustomizeTitle(inputValue) }}
+                />
+              </div>
+            </div>
+            <AdminUpdateButtonRow onClick={this.onClickSubmit} />
+          </React.Fragment>
         </div>
       </div>
     );
   }
 
 }
+
+const CustomizeTitleWrapper = (props) => {
+  return createSubscribedElement(CustomizeTitle, props, [AppContainer, AdminCustomizeContainer]);
+};
 
 CustomizeTitle.propTypes = {
   t: PropTypes.func.isRequired, // i18next
