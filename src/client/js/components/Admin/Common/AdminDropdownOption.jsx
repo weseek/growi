015b8cf397795a1429a861531a77df2e75b9aadb
itizawa <itizawa@weseek.co.jp>
2019-11-13 23:08:36 +0900

    adjust dropdown

--- src/client/js/components/Admin/Common/AdminDropdownOption.jsx
+++ src/client/js/components/Admin/Common/AdminDropdownOption.jsx
@@ -18,9 +18,11 @@ class AdminDropdownOption extends React.PureComponent {
       <div className="my-0 btn-group">
         <label>{this.props.label}</label>
         <div className="dropdown">
-          <button className="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            {this.props.selectedValue}
-            <span className="ml-2 caret"></span>
+          <button className="btn btn-default dropdown-toggle w-75" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span className="pull-left">{this.props.selectedValue}</span>
+            <span className="bs-caret pull-right">
+              <span className="caret" />
+            </span>
           </button>
           {/* TODO adjust dropdown after BS4 */}
           <ul className="dropdown-menu" role="menu">
