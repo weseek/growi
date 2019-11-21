7315cec556349614ec184d61fe07a4b74617d2c6
itizawa <itizawa@weseek.co.jp>
2019-11-21 16:09:59 +0900

    remove comment

--- src/server/routes/apiv3/customize-setting.js
+++ src/server/routes/apiv3/customize-setting.js
@@ -68,7 +68,6 @@ module.exports = (crowi) => {
 
   const { ApiV3FormValidator } = crowi.middlewares;
 
-  // TODO GW-533 implement accurate validation
   const validator = {
     layoutTheme: [
       body('layoutType').isString().isIn(['growi', 'kibela', 'crowi']),
