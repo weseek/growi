905434004d6879ec081ca1f087e76a2bfd044133
Yuki Takei <yuki@weseek.co.jp>
2019-11-25 19:44:48 +0900

    fix lint error

--- docker/nocdn/env.prod.js
+++ docker/nocdn/env.prod.js
@@ -1,6 +1,5 @@
 module.exports = {
-    NODE_ENV: 'production',
-    NO_CDN: true,
-    // FORMAT_NODE_LOG: false,
-  };
-  
+  NODE_ENV: 'production',
+  NO_CDN: true,
+  // FORMAT_NODE_LOG: false,
+};
