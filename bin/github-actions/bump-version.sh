083d5aca2764dd4eca871aef689e60d0c8530c4e
Yuki Takei <yuki@weseek.co.jp>
2019-11-25 20:12:01 +0900

    transplant files from growi-docker

---
+++ bin/github-actions/bump-version.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd docker
+
+sed -i -e "s/^\(ENV APP_VERSION v\).\+$/\1${RELEASE_VERSION}/" Dockerfile
+sed -i -e "s/^\([*] \[\`\)[^\`]\+\(\`, \`3\.5\`, .\+\]\)\(.\+\/blob\/v\).\+\(\/Dockerfile.\+\)$/\1${RELEASE_VERSION}\2\3${RELEASE_VERSION}\4/" README.md
+sed -i -e "s/^\([*] \[\`\)[^\`]\+\(\`, \`3\.5-nocdn\`, .\+\]\)\(.\+\/blob\/v\).\+\(\/nocdn\/Dockerfile.\+\)$/\1${RELEASE_VERSION}-nocdn\2\3${RELEASE_VERSION}\4/" README.md
