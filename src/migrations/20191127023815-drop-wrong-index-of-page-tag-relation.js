b3544bab064a111a8cfa3fe12dd8c689674d2888
Yuki Takei <yuki@weseek.co.jp>
2019-11-27 03:50:45 +0900

    fix migration script

--- src/migrations/20191127023815-drop-wrong-index-of-page-tag-relation.js
+++ src/migrations/20191127023815-drop-wrong-index-of-page-tag-relation.js
@@ -4,7 +4,14 @@ const logger = require('@alias/logger')('growi:migrate:drop-wrong-index-of-page-
 const mongoose = require('mongoose');
 const config = require('@root/config/migrate');
 
-async function dropIndexIfExists(collection, indexName) {
+async function dropIndexIfExists(db, collectionName, indexName) {
+  // check existence of the collection
+  const items = await db.listCollections({ name: collectionName }, { nameOnly: true }).toArray();
+  if (items.length === 0) {
+    return;
+  }
+
+  const collection = await db.collection(collectionName);
   if (await collection.indexExists(indexName)) {
     await collection.dropIndex(indexName);
   }
@@ -15,8 +22,7 @@ module.exports = {
     logger.info('Apply migration');
     mongoose.connect(config.mongoUri, config.mongodb.options);
 
-    const collection = db.collection('pagetagrelations');
-    await dropIndexIfExists(collection, 'page_1_user_1');
+    await dropIndexIfExists(db, 'pagetagrelations', 'page_1_user_1');
 
     logger.info('Migration has successfully applied');
   },
