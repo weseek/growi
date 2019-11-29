e0e26c7fee54c940655775cc424da0c5ecec0781
Yuki Takei <yuki@weseek.co.jp>
2019-11-29 19:25:06 +0900

    use su-exec

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-# Corresponds to `FILE_UPLOAD=local`
+# Support `FILE_UPLOAD=local`
 mkdir -p /data/uploads
-if [ ! -e "$APP_DIR/public/uploads" ]; then
-  ln -s /data/uploads $APP_DIR/public/uploads
+if [ ! -e "$appDir/public/uploads" ]; then
+  ln -s /data/uploads $appDir/public/uploads
 fi
 
 chown node:node /data/uploads
 chown -h node:node $appDir/public/uploads
 
-exec "/sbin/tini -e 143 -- $@"
+su-exec node $@
