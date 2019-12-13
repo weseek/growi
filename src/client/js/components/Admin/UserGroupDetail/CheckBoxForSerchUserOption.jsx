8a2a854a5be2af027608ac7102f11828e8c4fee3
itizawa <itizawa@weseek.co.jp>
2019-12-13 11:53:45 +0900

    remove di appContainer

--- src/client/js/components/Admin/UserGroupDetail/CheckBoxForSerchUserOption.jsx
+++ src/client/js/components/Admin/UserGroupDetail/CheckBoxForSerchUserOption.jsx
@@ -3,9 +3,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 
-import { createSubscribedElement } from '../../UnstatedUtils';
-import AppContainer from '../../../services/AppContainer';
-
 class CheckBoxForSerchUserOption extends React.Component {
 
   render() {
@@ -31,18 +28,10 @@ class CheckBoxForSerchUserOption extends React.Component {
 
 CheckBoxForSerchUserOption.propTypes = {
   t: PropTypes.func.isRequired, // i18next
-  appContainer: PropTypes.instanceOf(AppContainer).isRequired,
 
   option: PropTypes.string.isRequired,
   checked: PropTypes.bool.isRequired,
   onChange: PropTypes.bool.isRequired,
 };
 
-/**
- * Wrapper component for using unstated
- */
-const CheckBoxForSerchUserOptionWrapper = (props) => {
-  return createSubscribedElement(CheckBoxForSerchUserOption, props, [AppContainer]);
-};
-
-export default withTranslation()(CheckBoxForSerchUserOptionWrapper);
+export default withTranslation()(CheckBoxForSerchUserOption);
--- src/client/js/components/Admin/UserGroupDetail/RadioButtonForSerchUserOption.jsx
+++ src/client/js/components/Admin/UserGroupDetail/RadioButtonForSerchUserOption.jsx
@@ -3,9 +3,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 
-import { createSubscribedElement } from '../../UnstatedUtils';
-import AppContainer from '../../../services/AppContainer';
-
 class RadioButtonForSerchUserOption extends React.Component {
 
   render() {
@@ -31,18 +28,10 @@ class RadioButtonForSerchUserOption extends React.Component {
 
 RadioButtonForSerchUserOption.propTypes = {
   t: PropTypes.func.isRequired, // i18next
-  appContainer: PropTypes.instanceOf(AppContainer).isRequired,
 
   searchType: PropTypes.string.isRequired,
   checked: PropTypes.bool.isRequired,
   onChange: PropTypes.bool.isRequired,
 };
 
-/**
- * Wrapper component for using unstated
- */
-const RadioButtonForSerchUserOptionWrapper = (props) => {
-  return createSubscribedElement(RadioButtonForSerchUserOption, props, [AppContainer]);
-};
-
-export default withTranslation()(RadioButtonForSerchUserOptionWrapper);
+export default withTranslation()(RadioButtonForSerchUserOption);
