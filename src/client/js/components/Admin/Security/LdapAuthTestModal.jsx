8fd36dd3597cef197e4057f2e2c1db590249aa94
itizawa <itizawa@weseek.co.jp>
2019-12-16 12:29:59 +0900

    add input

--- src/client/js/components/Admin/Security/LdapAuthTestModal.jsx
+++ src/client/js/components/Admin/Security/LdapAuthTestModal.jsx
@@ -9,22 +9,38 @@ import { createSubscribedElement } from '../../UnstatedUtils';
 import AdminLdapSecurityContainer from '../../../services/AdminLdapSecurityContainer';
 
 class LdapAuthTestModal extends React.Component {
+
   render() {
+    const { t } = this.props;
+
     return (
       <Modal show={this.props.isOpen} onHide={this.props.onClose}>
         <Modal.Header className="modal-header" closeButton>
           <Modal.Title>
+            {t('Test LDAP Account')}
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          hoge
+          <div className="row p-3">
+            <label htmlFor="username" className="col-xs-3 text-right">{t('username')}</label>
+            <div className="col-xs-6">
+              <input className="form-control" name="username" />
+            </div>
+          </div>
+          <div className="row p-3">
+            <label htmlFor="password" className="col-xs-3 text-right">{t('Password')}</label>
+            <div className="col-xs-6">
+              <input className="form-control" type="password" name="password" />
+            </div>
+          </div>
         </Modal.Body>
         <Modal.Footer className="d-flex">
 
         </Modal.Footer>
       </Modal>
-    )
+    );
   }
+
 }
 
 
