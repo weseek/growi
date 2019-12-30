b5a5c6c3324c45e025efb20578e51e1e8e65501d
Ryu Sato <ryu@weseek.co.jp>
2019-12-30 13:52:54 +0900

    modify package.json script

--- config/swagger-definition.js
+++ config/swagger-definition.js
@@ -1,14 +1,16 @@
 const pkg = require('../package.json');
 
+const apiVersion = process.env.API_VERSION || 3;
+
 module.exports = {
   openapi: '3.0.1',
   info: {
-    title: 'GROWI REST API v3',
+    title: `GROWI REST API v${apiVersion}`,
     version: pkg.version,
   },
   servers: [
     {
-      url: 'https://demo.growi.org/_api/v3/',
+      url: `https://demo.growi.org/_api/v${apiVersion}/`,
     },
   ],
 };
