65759325e150207f02a32ffd728e8bada17585fc
itizawa <itizawa@weseek.co.jp>
2020-02-03 15:28:33 +0900

    FB

--- src/server/routes/apiv3/personal-setting.js
+++ src/server/routes/apiv3/personal-setting.js
@@ -42,16 +42,13 @@ module.exports = (crowi) => {
    *              application/json:
    *                schema:
    *                  properties:
-   *                    personalParams:
+   *                    currentUser:
    *                      type: object
    *                      description: personal params
    */
   router.get('/', loginRequiredStrictly, async(req, res) => {
-    const personalParams = {
-      currentUser: await User.findUserByUsername(req.user.username),
-      registrationWhiteList: await crowi.configManager.getConfig('crowi', 'security:registrationWhiteList'),
-    };
-    return res.apiv3({ personalParams });
+    const currentUser = await User.findUserByUsername(req.user.username);
+    return res.apiv3({ currentUser });
   });
 
   return router;
