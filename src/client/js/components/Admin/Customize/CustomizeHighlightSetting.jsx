644d99897e324fe56131429d1a4402daec55dbb1
Yuki Takei <yuki@weseek.co.jp>
2020-02-09 01:04:07 +0900

    fix role of dropdown menu item

--- src/client/js/components/Admin/Customize/CustomizeHighlightSetting.jsx
+++ src/client/js/components/Admin/Customize/CustomizeHighlightSetting.jsx
@@ -65,7 +65,7 @@ class CustomizeHighlightSetting extends React.Component {
 
       menuItem.push(
         <li key={styleId} role="presentation" type="button" onClick={() => adminCustomizeContainer.switchHighlightJsStyle(styleId, styleName, isBorderEnable)}>
-          <a role="menuitem">{styleName}</a>
+          <a role="button">{styleName}</a>
         </li>,
       );
     });
