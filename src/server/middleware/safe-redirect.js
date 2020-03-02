db919ee130cf22eaa5f8c2b415e220e51fa646ac
Yuki Takei <yuki@weseek.co.jp>
2020-03-03 03:16:37 +0900

    impl safeRedirect with whitelist mechanism

--- src/server/middleware/safe-redirect.js
+++ src/server/middleware/safe-redirect.js
@@ -2,34 +2,63 @@ const loggerFactory = require('@alias/logger');
 
 const logger = loggerFactory('growi:middleware:safe-redirect');
 
+
+/**
+ * Check whether the redirect url host is in specified whitelist
+ * @param {Array<string>} whitelistOfHosts
+ * @param {string} redirectToFqdn
+ */
+function isInWhitelist(whitelistOfHosts, redirectToFqdn) {
+  if (whitelistOfHosts == null || whitelistOfHosts.length === 0) {
+    return false;
+  }
+
+  const redirectUrl = new URL(redirectToFqdn);
+  return whitelistOfHosts.includes(redirectUrl.hostname) || whitelistOfHosts.includes(redirectUrl.host);
+}
+
 /**
  * Redirect with prevention from Open Redirect
  *
  * Usage: app.use(require('middleware/safe-redirect'))
  */
-module.exports = function(req, res, next) {
+module.exports = (whitelistOfHosts) => {
 
-  // extends res object
-  res.safeRedirect = function(redirectTo) {
-    if (redirectTo == null) {
-      return res.redirect('/');
-    }
+  return function(req, res, next) {
 
-    // prevention from open redirect
-    try {
-      const redirectUrl = new URL(redirectTo, `${req.protocol}://${req.get('host')}`);
-      if (redirectUrl.hostname === req.hostname) {
-        return res.redirect(redirectUrl);
+    // extend res object
+    res.safeRedirect = function(redirectTo) {
+      if (redirectTo == null) {
+        return res.redirect('/');
       }
-      logger.warn('Requested redirect URL is invalid, redirect to root page');
-    }
-    catch (err) {
-      logger.warn('Requested redirect URL is invalid, redirect to root page', err);
-    }
 
-    return res.redirect('/');
+      try {
+        // check inner redirect
+        const redirectUrl = new URL(redirectTo, `${req.protocol}://${req.get('host')}`);
+        if (redirectUrl.hostname === req.hostname) {
+          logger.debug(`Requested redirect URL (${redirectTo}) is local.`);
+          return res.redirect(redirectUrl.href);
+        }
+        logger.debug(`Requested redirect URL (${redirectTo}) is NOT local.`);
+
+        // check whitelisted redirect
+        const isWhitelisted = isInWhitelist(whitelistOfHosts, redirectTo);
+        if (isWhitelisted) {
+          logger.debug(`Requested redirect URL (${redirectTo}) is in whitelist.`, `whitelist=${whitelistOfHosts}`);
+          return res.redirect(redirectTo);
+        }
+        logger.debug(`Requested redirect URL (${redirectTo}) is NOT in whitelist.`, `whitelist=${whitelistOfHosts}`);
+      }
+      catch (err) {
+        logger.warn(`Requested redirect URL (${redirectTo}) is invalid.`, err);
+      }
+
+      logger.warn(`Requested redirect URL (${redirectTo}) is UNSAFE, redirecting to root page.`);
+      return res.redirect('/');
+    };
+
+    next();
+
   };
-
-  next();
 
 };
