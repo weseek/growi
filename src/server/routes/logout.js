c0d4d80d5023b750bf3812777bd2cb3006d16e5d
Yuki Takei <yuki@weseek.co.jp>
2020-03-02 20:54:31 +0900

    redirect to referer url when logout

--- src/server/routes/logout.js
+++ src/server/routes/logout.js
@@ -1,8 +1,14 @@
+const { URL } = require('url');
+
 module.exports = function(crowi, app) {
   return {
     logout(req, res) {
       req.session.destroy();
-      return res.redirect('/');
+
+      // parse referer url
+      const referer = new URL(req.headers.referer);
+      // redirect
+      return res.redirect(`${referer.pathname}${referer.search}${referer.hash}`);
     },
   };
 };
