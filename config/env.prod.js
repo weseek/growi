a687cd390dedb32c75ed4f4439c0621ce7b27100
Yuki Takei <yuki@weseek.co.jp>
2020-03-13 16:25:57 +0900

    modify logger settings

--- config/env.prod.js
+++ config/env.prod.js
@@ -1,7 +1,4 @@
 module.exports = {
   NODE_ENV: 'production',
   // FORMAT_NODE_LOG: false, // default: true
-
-  'growi:routes:login-passport': 'debug',
-  'growi:service:PassportService': 'debug',
 };
--- config/logger/config.prod.js
+++ config/logger/config.prod.js
@@ -1,36 +1,6 @@
 module.exports = {
   default: 'info',
 
-  // 'express-session': 'debug',
-
-  /*
-   * configure level for server
-   */
-  // 'express:*': 'debug',
-  // 'growi:*': 'debug',
-  'growi:crowi': 'debug',
-  // 'growi:crow:dev': 'debug',
-  'growi:crowi:express-init': 'debug',
-  'growi:models:external-account': 'debug',
-  // 'growi:routes:login': 'debug',
   'growi:routes:login-passport': 'debug',
-  'growi:middleware:safe-redirect': 'debug',
   'growi:service:PassportService': 'debug',
-  // 'growi:service:ConfigManager': 'debug',
-  'growi:lib:search': 'debug',
-  // 'growi:service:GlobalNotification': 'debug',
-  // 'growi:lib:importer': 'debug',
-  // 'growi:routes:page': 'debug',
-  'growi-plugin:*': 'debug',
-  // 'growi:InterceptorManager': 'debug',
-
-  // email
-  // 'growi:lib:mailer': 'debug',
-
-  /*
-   * configure level for client
-   */
-  'growi:app': 'debug',
-  'growi:services:*': 'debug',
-  // 'growi:StaffCredit': 'debug',
 };
