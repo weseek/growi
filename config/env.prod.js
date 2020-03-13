135ca3a13f2fcd0b2e7790f4ec476a620c93adfb
Yuki Takei <yuki@weseek.co.jp>
2020-03-13 15:12:37 +0900

    modify logger settings for prod

--- config/env.prod.js
+++ config/env.prod.js
@@ -1,4 +1,7 @@
 module.exports = {
   NODE_ENV: 'production',
-  // FORMAT_NODE_LOG: false,
+  FORMAT_NODE_LOG: true,
+
+  'growi:routes:login-passport': 'debug',
+  'growi:service:PassportService': 'debug',
 };
