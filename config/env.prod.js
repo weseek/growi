1a1be89d560a8279cb994f339ab64abbcb3b5ead
Yuki Takei <yuki@weseek.co.jp>
2020-03-13 15:19:06 +0900

    update env.prod.js

--- config/env.prod.js
+++ config/env.prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: 'production',
-  FORMAT_NODE_LOG: true,
+  // FORMAT_NODE_LOG: false, // default: true
 
   'growi:routes:login-passport': 'debug',
   'growi:service:PassportService': 'debug',
