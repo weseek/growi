11b336398b7e2257e72f07bcfd48238dc074d173
itizawa <itizawa@weseek.co.jp>
2020-03-26 11:02:35 +0900

    add i18n key for env_var_priority

--- src/client/js/components/Admin/AdminHome/AdminHome.jsx
+++ src/client/js/components/Admin/AdminHome/AdminHome.jsx
@@ -57,6 +57,7 @@ class AdminHome extends React.Component {
         <div className="row mb-5">
           <div className="col-md-12">
             <h2 className="admin-setting-header">{t('admin:admin_top.list_of_env_vars')}</h2>
+            <p>{t('admin:admin_top.env_var_priority')}</p>
             {/* eslint-disable-next-line react/no-danger */}
             <p dangerouslySetInnerHTML={{ __html: t('admin:admin_top.about_security') }} />
             {adminHomeContainer.state.envVars && <EnvVarsTable envVars={adminHomeContainer.state.envVars} />}
