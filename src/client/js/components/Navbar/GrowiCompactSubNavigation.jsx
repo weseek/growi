e003429196bbd7162c97d0ae302e4e2bcb282d93
ryohek <ryohei@weseek.co.jp>
2020-04-02 17:26:44 +0900

    delete diff and fix name

--- src/client/js/components/Navbar/GrowiCompactSubNavigation.jsx
+++ src/client/js/components/Navbar/GrowiCompactSubNavigation.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 
 import { withTranslation } from 'react-i18next';
 
-import { isTrashPage } from '../../../../lib/util/path-utils';
 import { createSubscribedElement } from '../UnstatedUtils';
 import AppContainer from '../../services/AppContainer';
 import RevisionPath from '../Page/RevisionPath';
@@ -11,23 +10,16 @@ import PageContainer from '../../services/PageContainer';
 import TagLabels from '../Page/TagLabels';
 import LikeButton from '../LikeButton';
 import BookmarkButton from '../BookmarkButton';
-// import PageCreator from './PageCreator';
 import ReducedPageCreator from './ReducedPageCreator';
 import ReducedRevisionAuthor from './ReducedRevisionAuthor';
-// import RevisionAuthor from './RevisionAuthor';
 
 const GrowiCompactSubNavigation = (props) => {
   const { appContainer, pageContainer } = props;
   const {
-    path, createdAt, creator, updatedAt, revisionAuthor,
+    createdAt, creator, updatedAt, revisionAuthor,
   } = pageContainer.state;
-  // const isVisible = (window.pageYOffset > 122);
-  // // const positionY = document.getElementById('#grw-navbar');
-  // const invisiblePosition = 122;
-  // window.addEventListener('scroll', () => {
-  //   if (isVisible) {
   return (
-    <div className="d-flex align-items-center fixed-top grw-compactsubnavbar-color px-3">
+    <div className="d-flex align-items-center fixed-top grw-compact-subnavbar px-3">
 
       {/* Page Path */}
       <div className="title-container mr-auto">
@@ -53,10 +45,6 @@ const GrowiCompactSubNavigation = (props) => {
 
     </div>
   );
-  //   }
-  // }, true);
-  // return null;
-
 };
 
 /**
