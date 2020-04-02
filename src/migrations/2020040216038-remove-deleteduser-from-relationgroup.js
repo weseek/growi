40ffd4a06bde20f4b5449e1f82718cc1e98d34a5
ryuichi-e <ryuichi-e@weseek.co.jp>
2020-04-02 21:26:02 +0900

    added getModelSafety

--- src/migrations/2020040216038-remove-deleteduser-from-relationgroup.js
+++ src/migrations/2020040216038-remove-deleteduser-from-relationgroup.js
@@ -4,13 +4,15 @@ const logger = require('@alias/logger')('growi:migrate:adjust-pages-path');
 const mongoose = require('mongoose');
 const config = require('@root/config/migrate');
 
+const { getModelSafely } = require('@commons/util/mongoose-utils');
+
 module.exports = {
   async up(db) {
     logger.info('Apply migration');
     mongoose.connect(config.mongoUri, config.mongodb.options);
 
-    const User = require('@server/models/user')();
-    const UserGroupRelation = require('@server/models/user-group-relation')();
+    const User = getModelSafely('User') || require('@server/models/user')();
+    const UserGroupRelation = getModelSafely('UserGroupRelation') || require('@server/models/user-group-relation')();
 
     const deletedUsers = await User.find({ status: 4 }); // deleted user
     await UserGroupRelation.remove({ relatedUser: deletedUsers });
