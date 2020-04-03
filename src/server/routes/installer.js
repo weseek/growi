08d88e6de825925c06bcfbf9837c9fc2d903f4b2
Yoshida Takuto <takuto.yoshida@gmail.com>
2020-04-04 03:02:24 +0900

    use en-US ver. of sandbox-bootstrap3.md

--- src/server/routes/installer.js
+++ src/server/routes/installer.js
@@ -33,7 +33,7 @@ module.exports = function(crowi, app) {
 
     // create /Sandbox/*
     promises.push(createPage(path.join(crowi.localeDir, lang, 'sandbox.md'), '/Sandbox', owner, lang));
-    promises.push(createPage(path.join(crowi.localeDir, lang, 'sandbox-bootstrap3.md'), '/Sandbox/Bootstrap3', owner, lang));
+    promises.push(createPage(path.join(crowi.localeDir, 'en-US', 'sandbox-bootstrap3.md'), '/Sandbox/Bootstrap3', owner, lang));
     promises.push(createPage(path.join(crowi.localeDir, lang, 'sandbox-diagrams.md'), '/Sandbox/Diagrams', owner, lang));
     promises.push(createPage(path.join(crowi.localeDir, lang, 'sandbox-math.md'), '/Sandbox/Math', owner, lang));
 
