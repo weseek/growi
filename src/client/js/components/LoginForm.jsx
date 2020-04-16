5bbcd88b4b2606097948e70fabcc73b876eed572
yusuketk <yusuketk@weseek.co.jp>
2020-04-16 17:42:53 +0900

    fix render

--- src/client/js/components/LoginForm.jsx
+++ src/client/js/components/LoginForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { createSubscribedElement } from './UnstatedUtils';
-import AppContainer from '../services/AppContainer';
+
+import { withTranslation } from 'react-i18next';
 
 class LoginForm extends React.Component {
 
@@ -10,43 +10,20 @@ class LoginForm extends React.Component {
 
     this.state = {
     };
-
-  }
-
-  componentDidMount() {
-  }
-
-  componentWillUnmount() {
   }
 
   render() {
 
     return (
-      <div className="main container-fluid">
-      </div>
+      <div className="aaaaa"></div>
     );
   }
 
 }
 
-/**
- * Wrapper component for using unstated
- */
-const LoginFormWrapper = (props) => {
-  return createSubscribedElement(LoginForm, props, [AppContainer]);
-};
-
 LoginForm.propTypes = {
-  t: PropTypes.func.isRequired, // i18next
-  appContainer: PropTypes.instanceOf(AppContainer).isRequired,
-
-  keyword: PropTypes.string,
-  onSubmit: PropTypes.func.isRequired,
-  onInputChange: PropTypes.func,
+  // i18next
+  t: PropTypes.func.isRequired,
 };
 
-LoginForm.defaultProps = {
-  onInputChange: () => { },
-};
-
-export default LoginFormWrapper;
+export default withTranslation()(LoginForm);
