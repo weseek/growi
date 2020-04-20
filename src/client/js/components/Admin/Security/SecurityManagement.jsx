f82f14c52222765f54f6899abc04ce4ca7ea272e
kaoritokashiki <kaori-t@weseek.co.jp>
2020-04-20 15:37:58 +0900

    delete dropdown and disabled navs

--- src/client/js/components/Admin/Security/SecurityManagement.jsx
+++ src/client/js/components/Admin/Security/SecurityManagement.jsx
@@ -65,23 +65,11 @@ class SecurityManagement extends React.Component {
             <li className="nav-item">
               <a className="nav-link active" href="#">Active</a>
             </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-              <div className="dropdown-menu">
-                <a className="dropdown-item" href="#">Action</a>
-                <a className="dropdown-item" href="#">Another action</a>
-                <a className="dropdown-item" href="#">Something else here</a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="#">Separated link</a>
-              </div>
-            </li>
             <li className="nav-item">
               <a className="nav-link" href="#">Link</a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#">Disabled</a>
-            </li>
           </ul>
+
           <Nav tabs>
             <NavItem>
               <NavLink
