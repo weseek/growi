1fa3ea3d8e8b3c07b68b46fab2f4284b5771021f
ryuichi-e <ryuichi-e@weseek.co.jp>
2020-04-20 22:14:07 +0900

    added chekbox-all class

--- src/client/js/components/Admin/UserManagement.jsx
+++ src/client/js/components/Admin/UserManagement.jsx
@@ -155,7 +155,7 @@ class UserManagement extends React.Component {
             </div>
 
             <div className="mx-5 form-inline">
-              <div className="custom-control custom-checkbox custom-checkbox-primary mr-2">
+              <div className="custom-control custom-checkbox custom-checkbox-primary mr-2 checkbox-all">
                 <input
                   className="custom-control-input"
                   type="checkbox"
