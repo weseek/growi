00cc06bfd848c4d4ccfd2e0270e72f6bfcf7c742
kaoritokashiki <kaori-t@weseek.co.jp>
2020-04-20 15:33:42 +0900

    add bootstrap nav

--- src/client/js/components/Admin/Security/SecurityManagement.jsx
+++ src/client/js/components/Admin/Security/SecurityManagement.jsx
@@ -60,6 +60,28 @@ class SecurityManagement extends React.Component {
 
         <div className="auth-mechanism-configurations m-t-10">
           <h2 className="border-bottom">{t('security_setting.Authentication mechanism settings')}</h2>
+
+          <ul className="nav nav-tabs">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Active</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+              <div className="dropdown-menu">
+                <a className="dropdown-item" href="#">Action</a>
+                <a className="dropdown-item" href="#">Another action</a>
+                <a className="dropdown-item" href="#">Something else here</a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">Separated link</a>
+              </div>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#">Disabled</a>
+            </li>
+          </ul>
           <Nav tabs>
             <NavItem>
               <NavLink
