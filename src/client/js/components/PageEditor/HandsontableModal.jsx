dad825015e89c891da9da9adefb62a82b6ddddf3
ryohek <ryohei@weseek.co.jp>
2020-04-21 16:17:25 +0900

    remove reactstrap

--- src/client/js/components/PageEditor/HandsontableModal.jsx
+++ src/client/js/components/PageEditor/HandsontableModal.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import {
-  Modal, ModalHeader, ModalBody, ModalFooter,
+  Modal,
 } from 'reactstrap';
 
 import Handsontable from 'handsontable';
