412c5e8d12a0e54a9584b5024af774c2fe0b410c
ryuichi-e <ryuichi-e@weseek.co.jp>
2020-04-28 19:18:02 +0900

    edited at oidc client secret

--- src/client/js/components/Admin/Security/OidcSecuritySetting.jsx
+++ src/client/js/components/Admin/Security/OidcSecuritySetting.jsx
@@ -154,9 +154,9 @@ class OidcSecurityManagement extends React.Component {
               </div>
             </div>
 
-            <div className="row mb-5">
-              <label htmlFor="oidcClientSecret" className="col-3 text-right py-2">{t('security_setting.client_secret')}</label>
-              <div className="col-6">
+            <div className="row mb-5 form-group">
+              <label htmlFor="oidcClientSecret" className="text-left text-md-right col-md-3 col-form-label">{t('security_setting.client_secret')}</label>
+              <div className="col-md-6">
                 <input
                   className="form-control"
                   type="text"
