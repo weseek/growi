73d7425d499254092540af520e542fb493bdc24d
ryuichi-e <ryuichi-e@weseek.co.jp>
2020-04-28 16:29:10 +0900

    edited search filter p2

--- src/client/js/components/Admin/Security/LdapSecuritySetting.jsx
+++ src/client/js/components/Admin/Security/LdapSecuritySetting.jsx
@@ -220,8 +220,8 @@ class LdapSecuritySetting extends React.Component {
             <div className="form-group row">
               <label className="text-left text-md-right col-md-3 col-form-label">
                 <strong>{t('security_setting.ldap.search_filter')}</strong>
-              </div>
-              <div className="col-6">
+              </label>
+              <div className="col-md-6">
                 <input
                   className="form-control"
                   type="text"
