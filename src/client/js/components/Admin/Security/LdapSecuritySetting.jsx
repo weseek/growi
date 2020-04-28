56d970c953de260ca2cbff709cb84a8bdc865960
ryuichi-e <ryuichi-e@weseek.co.jp>
2020-04-28 16:29:37 +0900

    edited username p2

--- src/client/js/components/Admin/Security/LdapSecuritySetting.jsx
+++ src/client/js/components/Admin/Security/LdapSecuritySetting.jsx
@@ -256,8 +256,8 @@ class LdapSecuritySetting extends React.Component {
             <div className="form-group row">
               <label className="text-left text-md-right col-md-3 col-form-label">
                 <strong htmlFor="attrMapUsername">{t('username')}</strong>
-              </div>
-              <div className="col-6">
+              </label>
+              <div className="col-md-6">
                 <input
                   className="form-control"
                   type="text"
