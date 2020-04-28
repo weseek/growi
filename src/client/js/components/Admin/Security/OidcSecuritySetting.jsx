c9391f55198f4a6c2f085d540367910144021a02
ryuichi-e <ryuichi-e@weseek.co.jp>
2020-04-28 19:17:40 +0900

    edited at oidc client id

--- src/client/js/components/Admin/Security/OidcSecuritySetting.jsx
+++ src/client/js/components/Admin/Security/OidcSecuritySetting.jsx
@@ -138,9 +138,9 @@ class OidcSecurityManagement extends React.Component {
               </div>
             </div>
 
-            <div className="row mb-5">
-              <label htmlFor="oidcClientId" className="col-3 text-right py-2">{t('security_setting.clientID')}</label>
-              <div className="col-6">
+            <div className="row mb-5 form-group">
+              <label htmlFor="oidcClientId" className="text-left text-md-right col-md-3 col-form-label">{t('security_setting.clientID')}</label>
+              <div className="col-md-6">
                 <input
                   className="form-control"
                   type="text"
