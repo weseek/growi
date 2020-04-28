9aae6ef9a9b259df689569727972e003ae3f6c59
ryuichi-e <ryuichi-e@weseek.co.jp>
2020-04-28 16:27:25 +0900

    edited bind dn part 1

--- src/client/js/components/Admin/Security/LdapSecuritySetting.jsx
+++ src/client/js/components/Admin/Security/LdapSecuritySetting.jsx
@@ -151,8 +151,8 @@ class LdapSecuritySetting extends React.Component {
               </div>
             </div>
 
-            <div className="row mb-5">
-              <div className="col-3 text-right py-2">
+            <div className="form-group row">
+              <label className="text-left text-md-right col-md-3 col-form-label">
                 <strong>Bind DN</strong>
               </div>
               <div className="col-6">
