b9cc7acdf95ed96d6d3181aac41763289ec07e83
kaoritokashiki <kaori-t@weseek.co.jp>
2020-04-29 16:22:17 +0900

    col adjustment for a table

--- src/client/js/components/Admin/Security/SecuritySetting.jsx
+++ src/client/js/components/Admin/Security/SecuritySetting.jsx
@@ -58,8 +58,8 @@ class SecuritySetting extends React.Component {
         </div>
           )}
 
-        <table className="table col-md-8 m-3">
         <h4>{ t('Page list and search results') }</h4>
+        <table className="table col-md-8 m-md-3">
           <thead>
             <tr>
               <th scope="col">{ t('Scope of page showing') }</th>
