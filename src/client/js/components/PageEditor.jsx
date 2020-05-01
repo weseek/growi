783b6a6a0bb7eb64daaac90566d772a6cdcbfbdf
itizawa <itizawa@weseek.co.jp>
2020-05-01 16:30:05 +0900

    adjust

--- src/client/js/components/PageEditor.jsx
+++ src/client/js/components/PageEditor.jsx
@@ -288,8 +288,8 @@ class PageEditor extends React.Component {
     const emojiStrategy = this.props.appContainer.getEmojiStrategy();
 
     return (
-      <div className="row">
-        <div className="page-editor-editor-container col-6">
+      <div className="d-flex">
+        <div className="page-editor-editor-container" style={{ flex: 1 }}>
           <Editor
             ref={(c) => { this.editor = c }}
             value={this.state.markdown}
@@ -305,7 +305,7 @@ class PageEditor extends React.Component {
             onSave={this.onSaveWithShortcut}
           />
         </div>
-        <div className="d-none d-xl-block page-editor-preview-container col-6">
+        <div className="d-none d-xl-block page-editor-preview-container" style={{ flex: 1 }}>
           <Preview
             markdown={this.state.markdown}
             // eslint-disable-next-line no-return-assign
