537e43690a21a22f88b8937255398320bd1142fc
ryuichi-e <ryuichi-e@weseek.co.jp>
2020-05-11 17:50:45 +0900

    remove lint error

--- src/client/js/components/Admin/ImportData/GrowiArchive/ImportForm.jsx
+++ src/client/js/components/Admin/ImportData/GrowiArchive/ImportForm.jsx
@@ -456,7 +456,6 @@ class ImportForm extends React.Component {
             </button>
           </div>
         </form>
-        
         <div className="well well-sm small my-4">
           <ul>
             <li>{t('admin:importer_management.growi_settings.description_of_import_mode.about')}</li>
