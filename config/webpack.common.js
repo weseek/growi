f8ccb2a89c30725a1909fde7bc77e6a840b0cb5c
ryohek <ryohei@weseek.co.jp>
2020-05-12 15:12:09 +0900

    wip adjust island theme color

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -42,7 +42,7 @@ module.exports = (options) => {
       'styles/theme-halloween':       './src/client/styles/scss/theme/halloween.scss',
       // 'styles/theme-wood':          './src/client/styles/scss/theme/wood.scss',
       // 'styles/theme-christmas':          './src/client/styles/scss/theme/christmas.scss',
-      // 'styles/theme-island':      './src/client/styles/scss/theme/island.scss',
+      'styles/theme-island':      './src/client/styles/scss/theme/island.scss',
       'styles/theme-antarctic':      './src/client/styles/scss/theme/antarctic.scss',
       'styles/theme-spring':         './src/client/styles/scss/theme/spring.scss',
       // styles for external services
