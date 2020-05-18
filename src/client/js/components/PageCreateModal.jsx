24726b4ec50b88b5358e499451bb29b47f3f0afc
itizawa <itizawa@weseek.co.jp>
2020-05-18 11:15:12 +0900

    add modal body

--- src/client/js/components/PageCreateModal.jsx
+++ src/client/js/components/PageCreateModal.jsx
@@ -15,12 +15,27 @@ const PageCreateModal = (props) => {
   const { t, appContainer } = props;
 
   return (
-    <Modal isOpen={appContainer.state.isPageCreateModalShown} toggle={appContainer.closePageCreateModal}>
+    <Modal size="lg" isOpen={appContainer.state.isPageCreateModalShown} toggle={appContainer.closePageCreateModal}>
       <ModalHeader tag="h4" toggle={appContainer.closePageCreateModal} className="bg-primary text-light">
         { t('New Page') }
       </ModalHeader>
       <ModalBody>
-        hoge
+        <div className="row form-group">
+          <fieldset className="col-12 mb-4">
+            <h3 className="grw-modal-head pb-2">{ t("Create today's") }</h3>
+            <div className="d-flex create-page-input-container">
+              <div className="create-page-input-row d-flex align-items-center">
+                {/* <span class="page-today-prefix">{ userPageRoot(user) }}/</span>
+                  <input type="text" data-prefix="{{ userPageRoot(user) }}/" class="page-today-input1 form-control text-center" value="{{ t('Memo') }}" id="" name="">
+                  <span class="page-today-suffix">/{ now|datetz('Y/m/d') }}/</span>
+                  <input type="text" data-prefix="/{{ now|datetz('Y/m/d') }}/" class="page-today-input2 form-control" id="page-today-input2" name="" placeholder="{{ t('Input page name (optional)') }}"> */}
+              </div>
+              <div className="create-page-button-container">
+                <button type="submit" className="btn btn-outline-primary rounded-pill"><i className="icon-fw icon-doc"></i>{ t('Create') }</button>
+              </div>
+            </div>
+          </fieldset>
+        </div>
       </ModalBody>
       <ModalFooter>
       </ModalFooter>
