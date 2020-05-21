fa244fcd5ab18c3a67ac44ba0f4a0293d96534f6
itizawa <itizawa@weseek.co.jp>
2020-05-22 00:34:56 +0900

    add object

--- src/client/js/components/PageDeleteModal.jsx
+++ src/client/js/components/PageDeleteModal.jsx
@@ -16,16 +16,28 @@ const PageDeleteModal = (props) => {
   const {
     t, isOpen, toggle, isDeleteCompletely, path,
   } = props;
+  const deleteMode = isDeleteCompletely ? 'completely' : 'temporary';
+
+  const deleteIconAndKey = {
+    completely: {
+      icon: 'fire',
+      translationKey: 'completely',
+    },
+    temporary: {
+      icon: 'trash',
+      translationKey: 'page',
+    },
+  };
 
   return (
     <Modal isOpen={isOpen} toggle={toggle} className="grw-create-page">
       <ModalHeader tag="h4" toggle={toggle} className="bg-danger text-light">
-        {isDeleteCompletely && <span><i className="icon-fw icon-fire"></i>{ t('modal_delete.delete_completely') }</span>}
-        {!isDeleteCompletely && <span><i className="icon-fw icon-trash"></i>{ t('modal_delete.delete_page') }</span>}
+        <i className={`icon-fw icon-${deleteIconAndKey[deleteMode].icon}`}></i>
+        { t(`modal_delete.delete_${deleteIconAndKey[deleteMode].translationKey}`) }
       </ModalHeader>
       <ModalBody>
         <div className="form-group">
-          <label htmlFor="">{ t('modal_delete.deleting_page') }:</label><br />
+          <label>{ t('modal_delete.deleting_page') }:</label><br />
           <code>{ path }</code>
         </div>
       </ModalBody>
@@ -54,6 +66,7 @@ PageDeleteModal.propTypes = {
   toggle: PropTypes.func.isRequired,
   onClickSubmit: PropTypes.func.isRequired,
 
+  path: PropTypes.string.isRequired,
   isDeleteCompletely: PropTypes.bool,
 };
 
