9cef51698658d16c9f70ad4d82492f3b483b1bcb
itizawa <itizawa@weseek.co.jp>
2020-05-21 14:30:30 +0900

    fix lint error

--- src/client/js/components/Page/TrashPageAlert.jsx
+++ src/client/js/components/Page/TrashPageAlert.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { withTranslation } from 'react-i18next';
-import { toastSuccess, toastError } from '../../util/apiNotification';
+import { toastError } from '../../util/apiNotification';
 
 import { createSubscribedElement } from '../UnstatedUtils';
 import AppContainer from '../../services/AppContainer';
