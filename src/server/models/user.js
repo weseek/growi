d70ecd716096893cc6d25be32a0e8576f1594c6d
yusuketk <yusuketk@weseek.co.jp>
2020-05-27 11:54:33 +0900

    fix calling updateImageUrlCached

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -189,7 +189,7 @@ module.exports = function(crowi) {
 
   userSchema.methods.updateIsGravatarEnabled = async function(isGravatarEnabled) {
     this.isGravatarEnabled = isGravatarEnabled;
-    this.imageUrlCached = await this.updateImageUrlCached();
+    await this.updateImageUrlCached();
     const userData = await this.save();
     return userData;
   };
@@ -225,7 +225,7 @@ module.exports = function(crowi) {
 
   userSchema.methods.updateImage = async function(attachment) {
     this.imageAttachment = attachment;
-    this.imageUrlCached = await this.updateImageUrlCached();
+    await this.updateImageUrlCached();
     return this.save();
   };
 
@@ -241,7 +241,7 @@ module.exports = function(crowi) {
     }
 
     this.imageAttachment = undefined;
-    this.imageUrlCached = await this.updateImageUrlCached();
+    this.updateImageUrlCached();
     return this.save();
   };
 
