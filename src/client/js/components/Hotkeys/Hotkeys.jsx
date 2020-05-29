9306451e103b06fa13ba74d9390f81a5f5cf06e9
makoto-s <makoto-s@weseek.co.jp>
2020-05-29 12:04:07 +0900

    made some corrections by the help of yarn lint

--- src/client/js/components/Hotkeys/Hotkeys.jsx
+++ src/client/js/components/Hotkeys/Hotkeys.jsx
@@ -15,7 +15,7 @@ export default class Hotkeys extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <HotkeysDetector onDetected={this.onDetected} />
+        <HotkeysDetector onDetected={() => this.onDetected()} />
       </React.Fragment>
     );
   }
