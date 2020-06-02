8b20fbac3ffb7e60b6df8b3341653e474eb50760
ryuichi-e <ryuichi-e@weseek.co.jp>
2020-06-02 16:52:27 +0900

    modal file

--- src/client/js/components/OutsideShareLinkModal.jsx
+++ src/client/js/components/OutsideShareLinkModal.jsx
@@ -9,13 +9,19 @@ import { withTranslation } from 'react-i18next';
 
 import { createSubscribedElement } from './UnstatedUtils';
 
+
 const OutsideShareLinkModal = (props) => {
 
+  const { t } = props;
+
+
   return (
-    <Modal size="lg" className="grw-create-page">
-      <ModalHeader tag="h4" className="bg-primary text-light">
+    <Modal size="lg" isOpen={props.isOpen} toggle={props.onClose} className="grw-create-page">
+      <ModalHeader tag="h4" toggle={props.onClose} className="bg-primary text-light">Hi there!
+        { t('New Page') }
       </ModalHeader>
       <ModalBody>
+        <h1>Hi there</h1>
       </ModalBody>
       <ModalFooter>
       </ModalFooter>
@@ -26,13 +32,16 @@ const OutsideShareLinkModal = (props) => {
 /**
  * Wrapper component for using unstated
  */
-const OutsideShareLinkModalWrapper = (props) => {
-  return createSubscribedElement(OutsideShareLinkModal, [props]);
+const ModalControlWrapper = (props) => {
+  return createSubscribedElement(OutsideShareLinkModal, props);
 };
 
 
 OutsideShareLinkModal.propTypes = {
   t: PropTypes.func.isRequired, //  i18next
+
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
 };
 
-export default withTranslation()(OutsideShareLinkModalWrapper);
+export default withTranslation()(ModalControlWrapper);
