54e69a0e5d888127642d856ac0d33ca8f2090eeb
ryuichi-e <ryuichi-e@weseek.co.jp>
2020-06-02 16:52:42 +0900

    drop down list

--- src/client/js/components/Page/PageShareManagement.jsx
+++ src/client/js/components/Page/PageShareManagement.jsx
@@ -2,42 +2,59 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 
+import { isTopPage } from '@commons/util/path-utils';
 import { createSubscribedElement } from '../UnstatedUtils';
 import AppContainer from '../../services/AppContainer';
 import PageContainer from '../../services/PageContainer';
-import OutsideShareLinkModal from '../OutsideShareLinkModal';
+/* import OutsideShareLinkModal from '../OutsideShareLinkModal'; */
 
 
 const PageShareManagement = (props) => {
-  /* const { t } = props; */
+  const { t, appContainer, pageContainer } = props;
+  const { path } = pageContainer.state;
 
+  const { currentUser } = appContainer;
+/*   const isTopPagePath = isTopPage(path); */
 
-  const [isOutsideShareLinkModalShown, setIsOutsideShareLinkModalShown] = useState(false);
+/*   const [isOutsideShareLinkModalShown, setIsOutsideShareLinkModalShown] = useState(false);
 
   function openOutsideShareLinkModalHandler() {
     setIsOutsideShareLinkModalShown(true);
   }
 
   function closeOutsideShareLinkModalHandler() {
-    setIsOutsideShareLinkModalShown(false);
+    setIsOutsideShareLinkModalShown(false); */
   }
 
   function renderModals() {
     return (
       <>
-        <OutsideShareLinkModal
+        {/* <OutsideShareLinkModal
           isOpen={isOutsideShareLinkModalShown}
           onClose={closeOutsideShareLinkModalHandler}
-        />
+        /> */}
       </>
     );
   }
 
   return (
     <>
-      <a className="nav-link" href="#" role="tab" data-toggle="tab" onClick={openOutsideShareLinkModalHandler}>
+      <a
+        role="button"
+        className={`nav-link dropdown-toggle dropdown-toggle-no-caret ${currentUser == null && 'dropdown-toggle-disabled'}`}
+        href="#"
+        data-toggle={`${currentUser == null ? 'tooltip' : 'dropdown'}`}
+        data-placement="top"
+        data-container="body"
+        title={t('Not available for guest')}
+      >
         <i className="fa fa-share-alt"></i>
       </a>
+      <div className="dropdown-menu dropdown-menu-right">
+        <button className="dropdown-item" type="button" onClick={openOutsideShareLinkModalHandler}>
+          <i className="icon-fw icon-magic-wand"></i> { t('template.option_label.create/edit') }
+        </button>
+      </div>
       {renderModals()}
     </>
   );
