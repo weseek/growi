f585ebc6342132895b176831ada044b99244311a
ryohek <ryohei@weseek.co.jp>
2020-06-03 16:26:02 +0900

    adjust for SearchTypeahead

--- src/client/js/components/SearchTypeahead.jsx
+++ src/client/js/components/SearchTypeahead.jsx
@@ -164,7 +164,7 @@ class SearchTypeahead extends React.Component {
     return (
       <span>
         <UserPicture user={page.lastUpdateUser} size="sm" noLink />
-        <span className="ml-1"><PagePathLabel page={page} /></span>
+        <span className="ml-1 text-break text-wrap"><PagePathLabel page={page} /></span>
         <PageListMeta page={page} />
       </span>
     );
