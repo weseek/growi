71bca58037559da3d43a9c13e04a7bdfdbe2751d
itizawa <itizawa@weseek.co.jp>
2020-06-03 15:08:37 +0900

    create route

---
+++ src/server/routes/apiv3/share-links.js
@@ -0,0 +1,42 @@
+// const loggerFactory = require('@alias/logger');
+
+// const logger = loggerFactory('growi:routes:apiv3:share-links');
+
+const express = require('express');
+
+const router = express.Router();
+
+/**
+ * @swagger
+ *  tags:
+ *    name: ShareLinks
+ */
+
+module.exports = (crowi) => {
+  const accessTokenParser = require('../../middleware/access-token-parser')(crowi);
+  const loginRequired = require('../../middleware/login-required')(crowi);
+
+  // TDOO write swagger
+  router.get('/', accessTokenParser, loginRequired, async(req, res) => {
+    // TODO GW-2616
+  });
+
+
+  // TDOO write swagger
+  router.post('/', accessTokenParser, loginRequired, async(req, res) => {
+    // TODO GW-2609
+  });
+
+  // TDOO write swagger
+  router.delete('/all', accessTokenParser, loginRequired, async(req, res) => {
+    // TODO GW-2694
+  });
+
+  // TDOO write swagger
+  router.delete('/:id', accessTokenParser, loginRequired, async(req, res) => {
+    // TODO GW-2610
+  });
+
+
+  return router;
+};
