f638c9b3e84322f44fbd4b2e46f0db4827ae60ac
itizawa <itizawa@weseek.co.jp>
2020-06-03 15:10:31 +0900

    remove access token parser

--- src/server/routes/apiv3/share-links.js
+++ src/server/routes/apiv3/share-links.js
@@ -13,27 +13,26 @@ const router = express.Router();
  */
 
 module.exports = (crowi) => {
-  const accessTokenParser = require('../../middleware/access-token-parser')(crowi);
   const loginRequired = require('../../middleware/login-required')(crowi);
 
   // TDOO write swagger
-  router.get('/', accessTokenParser, loginRequired, async(req, res) => {
+  router.get('/', loginRequired, async(req, res) => {
     // TODO GW-2616
   });
 
 
   // TDOO write swagger
-  router.post('/', accessTokenParser, loginRequired, async(req, res) => {
+  router.post('/', loginRequired, async(req, res) => {
     // TODO GW-2609
   });
 
   // TDOO write swagger
-  router.delete('/all', accessTokenParser, loginRequired, async(req, res) => {
+  router.delete('/all', loginRequired, async(req, res) => {
     // TODO GW-2694
   });
 
   // TDOO write swagger
-  router.delete('/:id', accessTokenParser, loginRequired, async(req, res) => {
+  router.delete('/:id', loginRequired, async(req, res) => {
     // TODO GW-2610
   });
 
