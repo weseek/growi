11a04a3fad3f466d3abc3e9a2be8394a62859871
itizawa <itizawa@weseek.co.jp>
2020-06-08 19:04:29 +0900

    path share link length

--- src/client/js/services/PageContainer.js
+++ src/client/js/services/PageContainer.js
@@ -54,6 +54,7 @@ export default class PageContainer extends Container {
       tags: [],
       hasChildren: JSON.parse(mainContent.getAttribute('data-page-has-children')),
       templateTagData: mainContent.getAttribute('data-template-tags') || null,
+      shareLinksLength:  JSON.parse(mainContent.getAttribute('data-share-links-length')),
 
       // latest(on remote) information
       remoteRevisionId: revisionId,
