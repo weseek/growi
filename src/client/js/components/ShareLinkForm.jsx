bd047f7744c95dc7c7f7ca62750e769e734e23c9
itizawa <itizawa@weseek.co.jp>
2020-06-17 00:36:39 +0900

    refactor

--- src/client/js/components/ShareLinkForm.jsx
+++ src/client/js/components/ShareLinkForm.jsx
@@ -105,7 +105,7 @@ class ShareLinkForm extends React.Component {
   }
 
   async handleIssueShareLink() {
-    const { t, pageContainer } = this.props;
+    const { t, appContainer, pageContainer } = this.props;
     const { pageId } = pageContainer.state;
     const { description } = this.state;
 
@@ -119,7 +119,7 @@ class ShareLinkForm extends React.Component {
     }
 
     try {
-      await this.props.appContainer.apiv3.post('/share-links/', { relatedPage: pageId, expiredAt, description });
+      await appContainer.apiv3Delete('/share-links/', { relatedPage: pageId, expiredAt, description });
       this.closeForm();
       toastSuccess(t('toaster.issue_share_link'));
     }
