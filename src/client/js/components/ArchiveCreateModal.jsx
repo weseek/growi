78aeba8cb3e8ece0d9ce54fb0f793d1140f56ea3
zahmis <meda829ratteru86@gmail.com>
2020-06-17 03:56:28 +0000

    delete comment out

--- src/client/js/components/ArchiveCreateModal.jsx
+++ src/client/js/components/ArchiveCreateModal.jsx
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 
 const ArchiveCreateModal = (props) => {
-  // const [isArchiveCreateModalShown, setIsArchiveCreateModalShown] = useState(false);
-  // const [isDownloadComment, isDownloadFile,isDownloadSubordinatedPages] = props;
 
   return (
     <Modal size="lg" isOpen={props.isOpen} toggle={props.onClose}>
@@ -13,73 +11,16 @@ const ArchiveCreateModal = (props) => {
       </ModalHeader>
       <ModalBody>
         <input type="checkbox" />
+        <label>コメントもダウンロードする</label>
         <input type="checkbox" />
+        <label>ファイルもダウンロードする</label>
         <input type="checkbox" />
-
-        試作
+        <label>配下ページもダウンロードする</label>
       </ModalBody>
     </Modal>
   );
 };
 
-// export default class ArchiveCreateModal extends React.PureComponet {
-
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       show: false,
-//       isIncludeComment: false,
-//       isIncludeFile: false,
-//       isCreateAllSubordinatedPage: false,
-//     };
-
-//     this.show = this.show.bind(this);
-//     this.cancel = this.cancel.bind(this);
-//     this.save = this.save.bind(this);
-
-//   }
-
-//   show() {
-
-//   }
-
-//   cancel() {
-//     this.hide();
-//   }
-
-//   hide() {
-//     this.setState({ show: false });
-//   }
-
-//   save() {
-
-//   }
-
-
-//   render() {
-//     return (
-//       <Modal isOpen={this.state.show} toggle={this.cancel} size="lg">
-//         <ModalHeader tag="h4" toggle={this.cancel} className="bg-primary text-light">
-//           アーカイブ作成
-//         </ModalHeader>
-
-//         <ModalBody className="container">
-//           <div className="custom-control">
-//             <button className="btn btn-outline-secondary d-block mx-auto px-5 mb-3" type="button">アーカイブを作成する</button>
-
-//             <input className="custom-control-input" type="checkbox" checked={this.state.isIncludeComment} />
-//             <label className="custom-control-label" onClick="this.">コメントもダウンロードする</label>
-
-//             <input className="custom-control-input" type="checkbox" checked={this.isIncludeFile} />
-//             <label className="custom-control-label" onClick="this.">添付ファイルもダウンロードする</label>
-
-//             <input className="custom-control-input" type="checkbox" checked={this.isCreateAllSubordinatedPage} />
-//             <label className="custom-control-label" onClick="this.">配下ページもダウンロードする</label>
-//           </div>
-//         </ModalBody>
-//       </Modal>
-//     );
-//   }
 ArchiveCreateModal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   onClose: PropTypes.func.isRequired,
