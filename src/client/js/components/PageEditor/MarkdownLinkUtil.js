f227d8ada12f84cf4307ced413dc6d659e95879a
ryohek <ryohei@weseek.co.jp>
2020-06-19 15:39:39 +0900

    WIP

--- src/client/js/components/PageEditor/MarkdownLinkUtil.js
+++ src/client/js/components/PageEditor/MarkdownLinkUtil.js
@@ -3,12 +3,22 @@
  */
 class MarkdownLinkUtil {
 
+  constructor() {
+    this.linePartOfTableRE = /^([^\r\n|]*)\|(([^\r\n|]*\|)+)$/;
+    this.isInTable = this.isInTable.bind(this);
+  }
+
   getSelectedTextInEditor(editor) {
     return editor.getDoc().getSelection();
   }
 
   replaceFocusedMarkdownLinkWithEditor(editor, link) {
     editor.getDoc().replaceSelection(link);
+  }
+
+  isInTable(editor) {
+    const curPos = editor.getCursor();
+    return this.linePartOfTableRE.test(editor.getDoc().getLine(curPos.line));
   }
 
 }
