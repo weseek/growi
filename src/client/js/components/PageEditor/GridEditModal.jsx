ee7485859212f0a9de936ba92809456c8b5be468
ryohek <ryohei@weseek.co.jp>
2020-06-23 10:33:13 +0900

    add method

--- src/client/js/components/PageEditor/GridEditModal.jsx
+++ src/client/js/components/PageEditor/GridEditModal.jsx
@@ -30,6 +30,30 @@ export default class GridEditModal extends React.PureComponent {
     this.hide();
   }
 
+  pasteCodedGrid() {
+    // dummy data
+    const pasteCode = `
+      <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-md-5 col-lg-12"></div>
+            <div class="col-xs-3"></div>
+            <div class="col-lg-1"></div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12"></div>
+        </div>
+        <div class="row">
+            <div class="col-md-11"></div>
+        </div>
+        <div class="row">
+            <div class="col-xs-4"></div>
+        </div>
+      </div>
+    `;
+    this.cancel();
+    return pasteCode;
+  }
+
   render() {
 
     return (
@@ -42,7 +66,7 @@ export default class GridEditModal extends React.PureComponent {
         <ModalFooter className="grw-modal-footer">
           <div className="ml-auto">
             <button type="button" className="mr-2 btn btn-secondary" onClick={this.cancel}>Cancel</button>
-            <button type="button" className="btn btn-primary">Done</button>
+            <button type="button" className="btn btn-primary" onClick={this.pasteCodedGrid}>Done</button>
           </div>
         </ModalFooter>
       </Modal>
