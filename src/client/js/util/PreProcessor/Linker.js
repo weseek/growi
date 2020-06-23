71a16ae4b319bd9de44da2f49c196e989606046e
白石誠 <makoto-s@weseek.co.jp>
2020-06-23 14:33:41 +0900

    minor change

--- src/client/js/util/PreProcessor/Linker.js
+++ src/client/js/util/PreProcessor/Linker.js
@@ -5,10 +5,10 @@ export default class Linker {
     return markdown
       // process angle branckets like '</Level1/Level2>'
       // see: https://regex101.com/r/rxAy4F/2
-      .replace(/<((\/[^>\n]+?){2,})>/g, '<a href="$1">$1</a><a><i class="icon-note"></i></a>') // ページ間リンク: <> でかこまれてて / から始まり、 / が2個以上
+      .replace(/<((\/[^>\n]+?){2,})>/g, '<a href="$1" class="handson-link-modal-trigger">$1</a><a><i class="icon-note"></i></a>') // ページ間リンク: <> でかこまれてて / から始まり、 / が2個以上
       // process square branckets like '[/Level1]'
       // see: https://regex101.com/r/QSt1yu/5
-      .replace(/\[(\/[^\]\n]+?)\](?!\()/g, '<a href="$1">$1</a><a><i class="icon-note"></i></a>');
+      .replace(/\[(\/[^\]\n]+?)\](?!\()/g, '<a href="$1" class="handson-link-modal-trigger">$1</a><a><i class="icon-note"></i></a>');
   }
 
 }
