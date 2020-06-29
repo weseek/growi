616b3938459443fe8e90407c8ffbbd83b773bf75
ryohek <ryohei@weseek.co.jp>
2020-06-29 19:16:26 +0900

    add reg exp

--- src/client/js/components/PageEditor/GridEditorUtil.js
+++ src/client/js/components/PageEditor/GridEditorUtil.js
@@ -2,9 +2,19 @@
  * Utility for grid editor
  */
 class GridEditorUtil {
-
   constructor() {
+    // regex101 の url
+    this.linePartOfTableRE = /(<div>.*)(</div>)$/;
+    this.isInRow = this.isInRow.bind(this);
     this.replaceGridWithHtmlWithEditor = this.replaceGridWithHtmlWithEditor.bind(this);
+  }
+  /**
+   * return boolean value whether the cursor position is in a row
+   */
+  isInRow(editor) {
+    const curPos = editor.getCursor();
+    // return this.linePartOfTableRE.test(editor.getDoc().getLine(curPos.line));
+    return this.linePartOfRow.test(editor.getDoc().getLine(curPos.line));
   }
 
   replaceGridWithHtmlWithEditor(editor) {
@@ -16,9 +26,7 @@ class GridEditorUtil {
     );
     editor.getDoc().setCursor(curPos.line + 1, 2);
   }
-
 }
-
 // singleton pattern
 const instance = new GridEditorUtil();
 Object.freeze(instance);
