7914fd7cad8befed1ff260ece77173534b93b4ea
kaoritokashiki <kaori-t@weseek.co.jp>
2020-07-03 09:35:56 +0900

    undisplayed "t" inside of each col

--- src/client/js/components/PageEditor/GridEditModal.jsx
+++ src/client/js/components/PageEditor/GridEditModal.jsx
@@ -34,7 +34,7 @@ export default class GridEditModal extends React.PureComponent {
     const cols = [];
     for (let i = 0; i < 12; i++) {
       // [bg-light:TODO support dark mode by GW-3037]
-      cols.push(<div className="bg-light grid-bg-col col-1">t</div>);
+      cols.push(<div className="bg-light grid-bg-col col-1"></div>);
     }
     return cols;
   }
