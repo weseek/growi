182d1d98eddee8caa904ebeef522b517891bb0f1
kaoritokashiki <kaori-t@weseek.co.jp>
2020-07-08 15:59:10 +0900

    add some methods to get  arrays' info

--- src/client/js/models/BootstrapGrid.js
+++ src/client/js/models/BootstrapGrid.js
@@ -12,4 +12,46 @@ export default class BootstrapGrid {
   }
 
 
+  getDisplaySize() {
+    const displaySize = ['sm', 'md', 'lg', 'xl'];
+    return displaySize;
+  }
+
+  getColsize() {
+    const colSize = [];
+    for (let i = 0; i < 12; i++) {
+      colSize.push(i);
+    }
+    return colSize;
+  }
+
+  getOffsetSize() {
+    const offsetSize = [];
+    for (let i = 0; i < 12; i++) {
+      offsetSize.push(i);
+    }
+    return offsetSize;
+  }
+
+  getGridInfo(disSz, offSz, colSz) {
+    const gridInfo = { displaySize: disSz, offsetSize: offSz, colSize: colSz };
+    return gridInfo;
+  }
+
+  getGridInfos(gridInfo) {
+    const gridInfos = [];
+    for (let i = 0; i < gridInfo.length; i++) {
+      gridInfos.push(i);
+    }
+    return gridInfos;
+  }
+
+  getRowInfo(gridInfos) {
+    const rowInfo = [];
+    for (let i = 0; i < gridInfos.length; i++) {
+      rowInfo.push(i);
+    }
+    return rowInfo;
+  }
+
 }
