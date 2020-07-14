1a62ff52155d5f7e2674913b0125004c249a33eb
ryohek <ryohei@weseek.co.jp>
2020-07-14 14:48:35 +0900

    delete

--- src/client/js/components/PageEditor/GridEditModal.jsx
+++ src/client/js/components/PageEditor/GridEditModal.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import {
   Modal, ModalHeader, ModalBody, ModalFooter,
 } from 'reactstrap';
-import BootstrapGrid from '../../models/BootstrapGrid';
 
 export default class GridEditModal extends React.PureComponent {
 
