a26fa7b20854c35350b12b16257eda69ede3df30
ryuichi-e <ryuichi-e@weseek.co.jp>
2020-07-22 17:00:55 +0900

    fix error

--- src/client/js/components/MyBookmarkList/MyBookmarkList.jsx
+++ src/client/js/components/MyBookmarkList/MyBookmarkList.jsx
@@ -6,7 +6,7 @@ import { withUnstatedContainers } from '../UnstatedUtils';
 
 import AppContainer from '../../services/AppContainer';
 import PageContainer from '../../services/PageContainer';
-import { toastError } from '../../util/apiNotification';
+// import { toastError } from '../../util/apiNotification';
 // import PaginationWrapper from '../PaginationWrapper';
 import Page from '../PageList/Page';
 
@@ -17,53 +17,12 @@ class MyBookmarkList extends React.Component {
 
     this.state = {
       pages: [],
-      activePage: 1,
-      totalPages: 0,
-      pagingLimit: Infinity,
     };
 
-    // this.handlePage = this.handlePage.bind(this);
   }
 
-  /* componentWillMount() {
-    this.getMyBookmarkList(1);
-  } */
 
-  /* async handlePage(selectedPage) {
-    await this.getMyBookmarkList(selectedPage);
-  } */
-
-  // async getMyBookmarkList(selectPageNumber) {
-  /* const { appContainer, pageContainer } = this.props; */
-
-  /* const userId = appContainer.currentUserId; */
-  /* TODO GW-3255 get config from customize settings */
-  /* const limit = appContainer.getConfig().recentCreatedLimit; */
-  /* const limit = 10;
-    const offset = (selectPageNumber - 1) * limit; */
-
-  // This block is cited from MyDraftList
-  /* await appContainer.api.bookmark(limit, offset)
-      .then((res) => {
-        const totalPages = res.totalCount;
-        const pages = res.pages;
-        const activePage = selectPageNumber;
-        this.setState({
-          pages,
-          activePage,
-          totalPages,
-          pagingLimit: limit,
-        });
-      }); */
-  /* try {
-      await pageContainer.retrieveMyBookmarkList(userId, limit, offset);
-    }
-    catch (error) {
-      logger.error('failed to fetch data', error);
-      toastError(error, 'Error occurred in updating History');
-    } */
-  // }
-  async getMyBookmarkList(selectPageNumber) {
+  /* async getMyBookmarkList(selectPageNumber) {
     const { appContainer } = this.props;
     const userId = appContainer.currentUserId;
     const limit = 10;
@@ -74,15 +33,12 @@ class MyBookmarkList extends React.Component {
       const { docs: pages, totalDocs: totalPages, limit: pagingLimit } = data.paginateResult;
       this.setState({
         pages,
-        totalPages,
-        pagingLimit,
-        activePage: selectPageNumber,
       });
     }
     catch (error) {
       toastError(error, 'Error occurred in updating History');
     }
-  }
+  } */
 
 
   /**
