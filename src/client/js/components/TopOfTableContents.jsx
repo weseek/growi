44460b694622e188af2009f09d80382e11355551
kaoritokashiki <kaori-t@weseek.co.jp>
2020-07-27 16:05:23 +0900

    change padding bottom size

--- src/client/js/components/TopOfTableContents.jsx
+++ src/client/js/components/TopOfTableContents.jsx
@@ -11,7 +11,7 @@ const TopOfTableContents = (props) => {
 
   return (
     <>
-      <div className="top-of-table-contents d-flex align-items-end pb-2">
+      <div className="top-of-table-contents d-flex align-items-end pb-1">
         <button type="button" className="bg-transparent border-0">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" className="table-top-icon" width="20" height="20">
             <rect className="cls-1" width="14" height="14" />
