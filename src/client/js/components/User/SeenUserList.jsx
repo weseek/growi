618399264ab41ef1286e0c9e8519a00142af6f76
oshikishintaro <oshikishin@gmail.com>
2020-07-27 16:40:28 +0900

    -Removed liker list from toc-container. -Changed the look of the seen user list.

--- src/client/js/components/User/SeenUserList.jsx
+++ src/client/js/components/User/SeenUserList.jsx
@@ -12,12 +12,12 @@ class SeenUserList extends React.Component {
   render() {
     const { pageContainer } = this.props;
     return (
-      <div className="user-list-content text-truncate text-muted text-right">
-        <span className="text-danger">
-          <span className="seen-user-count">{pageContainer.state.sumOfSeenUsers}</span>
+      <div className="dropdown text-right">
+        <button className="btn btn-link dropdown-toggle text-danger" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <i className="fa fa-fw fa-paw"></i>
-        </span>
-        <span className="mr-1">
+          <span className="seen-user-count">{pageContainer.state.sumOfSeenUsers}</span>
+        </button>
+        <span className="dropdown-menu dropdown-menu-right px-2 col user-list-content text-truncate text-muted">
           <UserPictureList users={pageContainer.state.seenUsers} />
         </span>
       </div>
@@ -25,6 +25,44 @@ class SeenUserList extends React.Component {
   }
 
 }
+
+
+// class SeenUserList extends React.Component {
+
+//   render() {
+//     const { pageContainer } = this.props;
+//     return (
+//       <div className="user-list-content text-truncate text-muted text-right">
+//         <button type="button" className="btn link" data-toggle="tooltip" data-placement="bottom" title="a">
+//           <span className="text-danger">
+//             <span className="seen-user-count">{pageContainer.state.sumOfSeenUsers}</span>
+//             <i className="fa fa-fw fa-paw"></i>
+//           </span>
+//         </button>
+//       </div>
+//     );
+//   }
+
+// }
+
+// class SeenUserList extends React.Component {
+
+//   render() {
+//     const { pageContainer } = this.props;
+//     return (
+//       <div className="user-list-content text-truncate text-muted text-right">
+//         <span className="text-danger">
+//           <span className="seen-user-count">{pageContainer.state.sumOfSeenUsers}</span>
+//           <i className="fa fa-fw fa-paw"></i>
+//         </span>
+//         <span className="mr-1">
+//           <UserPictureList users={pageContainer.state.seenUsers} />
+//         </span>
+//       </div>
+//     );
+//   }
+
+// }
 
 SeenUserList.propTypes = {
   pageContainer: PropTypes.instanceOf(PageContainer).isRequired,
