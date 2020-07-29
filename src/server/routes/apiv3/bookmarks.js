39096b61cea3fcb2daf00d081f8da6cfba065d3d
ryuichi-e <ryuichi-e@weseek.co.jp>
2020-07-29 16:31:58 +0900

    remove csrf

--- src/server/routes/apiv3/bookmarks.js
+++ src/server/routes/apiv3/bookmarks.js
@@ -105,7 +105,7 @@ module.exports = (crowi) => {
 
   // select page from bookmark where userid = userid
 
-  router.get('/:userId', accessTokenParser, loginRequired, csrf, apiV3FormValidator, async(req, res) => {
+  router.get('/:userId', accessTokenParser, loginRequired, apiV3FormValidator, async(req, res) => {
     const { userId } = req.params;
     try {
       const bookmarks = await Bookmark
