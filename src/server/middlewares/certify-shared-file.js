a4f149c7a898e51f39b3cb85a11a9ffad67d1c1d
itizawa <itizawa@weseek.co.jp>
2020-07-30 04:42:55 +0000

    fix lint error

--- src/server/middlewares/certify-shared-file.js
+++ src/server/middlewares/certify-shared-file.js
@@ -1,5 +1,4 @@
 const loggerFactory = require('@alias/logger');
-const shareLinks = require('../routes/apiv3/share-links');
 
 const logger = loggerFactory('growi:middleware:certify-shared-fire');
 
@@ -10,10 +9,8 @@ module.exports = (crowi) => {
     const fileId = req.params.id || null;
 
     if (fileId == null) {
-      return next();
+      next();
     }
-
-    console.log('hoge');
 
     // No need to check shared page if user exists
     if (req.user != null) {
@@ -26,22 +23,23 @@ module.exports = (crowi) => {
     const attachment = await Attachment.findOne({ _id: fileId });
 
     if (attachment == null) {
-      return next();
+      next();
     }
 
     const shareLinks = await ShareLink.find({ relatedPage: attachment.page });
 
     // If sharelinks don't exist, skip it
     if (shareLinks.length === 0) {
-      return next();
+      next();
     }
 
     // Is there a valid share link
     await Promise.all(shareLinks.map((sharelink) => {
       if (!sharelink.isExpired()) {
         logger.debug('Confirmed target file belong to a share page');
-        return req.isSharedPage = true;
+        req.isSharedPage = true;
       }
+      return;
     }));
 
     next();
