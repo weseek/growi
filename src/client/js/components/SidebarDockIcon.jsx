0b87a8a4ff535657d65ea638111a133701dc2391
oshikishintaro <oshikishin@gmail.com>
2020-07-31 18:55:53 +0900

    Refactoring.

--- src/client/js/components/SidebarDockIcon.jsx
+++ src/client/js/components/SidebarDockIcon.jsx
@@ -9,14 +9,14 @@ const SidebarDockIcon = () => (
     className="sidebar-mode-icon"
   >
     <rect className="icon-collision" width="23" height="23" />
-    <path
-      d="M20.86,3.92a.64.64,0,0,1,.64.63v13.9a.64.64,0,0,1-.64.63H2.14a.64.64,0,0,
-      1-.64-.63V4.55a.64.64,0,0,1,.64-.63H20.86m0-1.5H2.14A2.13,2.13,0,0,0,0,4.55v13.9a2.13,
-      2.13,0,0,0,2.14,2.13H20.86A2.13,2.13,0,0,0,23,18.45V4.55a2.13,2.13,0,0,0-2.14-2.13Z"
-      className="personal-dropdown-icons"
-    >
-    </path>
-    <rect x="7.49" y="3.05" width="1.2" height="16.91" className="personal-dropdown-icons"></rect>
+    <g className="personal-dropdown-icons">
+      <path
+        d="M20.86,3.92a.64.64,0,0,1,.64.63v13.9a.64.64,0,0,1-.64.63H2.14a.64.64,0,0,
+        1-.64-.63V4.55a.64.64,0,0,1,.64-.63H20.86m0-1.5H2.14A2.13,2.13,0,0,0,0,4.55v13.9a2.13,
+        2.13,0,0,0,2.14,2.13H20.86A2.13,2.13,0,0,0,23,18.45V4.55a2.13,2.13,0,0,0-2.14-2.13Z"
+      />
+      <rect x="7.49" y="3.05" width="1.2" height="16.91" />
+    </g>
   </svg>
 
 );
