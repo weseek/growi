cd1d77e711bbce247227a60713993d5bcf715b45
oshikishintaro <oshikishin@gmail.com>
2020-07-31 14:42:32 +0900

    Try and error popover.

--- src/client/js/components/User/SeenUserList.jsx
+++ src/client/js/components/User/SeenUserList.jsx
@@ -14,20 +14,19 @@ class SeenUserList extends React.Component {
   render() {
     const { pageContainer } = this.props;
     return (
-      <div className="dropdown text-right ">
-        {/* <button className="btn btn-seen-user-list border-0 px-1 py-0 dropdown-toggle " type="button"> */}
-        <button type="button" className="btn btn-seen-user-list border-0 px-1 py-0" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
-          <span className="mr-2 svg footstamp-icon"><FootstampIcon /></span>
-          <span className="seen-user-count">{pageContainer.state.countOfSeenUsers}</span>
-        </button>
-        <span className="dropdown-menu dropdown-menu-right px-2 text-right user-list-content text-truncate text-muted">
+      <button type="button" className="btn btn-seen-user-list border-0 px-1 py-0" data-container="body" data-toggle="popover" data-placement="bottom" data-html="true" data-content_div_id="seen-user-content">
+        <span className="mr-2 svg footstamp-icon"><FootstampIcon /></span>
+        <span className="seen-user-count">{pageContainer.state.countOfSeenUsers}</span>
+        <div id="seen-user-content" className="hide px-2 text-right user-list-content text-truncate text-muted">
           <UserPictureList users={pageContainer.state.seenUsers} />
-        </span>
-      </div>
+        </div>
+      </button>
     );
+
   }
 
 }
+
 
 SeenUserList.propTypes = {
   pageContainer: PropTypes.instanceOf(PageContainer).isRequired,
