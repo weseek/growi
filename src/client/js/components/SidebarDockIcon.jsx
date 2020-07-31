132f7aaac3bf9757231ddfea7a6dc2d2eeb65bcb
oshikishintaro <oshikishin@gmail.com>
2020-07-31 15:31:53 +0900

    Display test : Changed the contents of Sidebar-icon  to Growilogo

--- src/client/js/components/SidebarDockIcon.jsx
+++ src/client/js/components/SidebarDockIcon.jsx
@@ -2,25 +2,52 @@ import React from 'react';
 
 const SidebarDockIcon = () => (
   <svg
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 23 23"
-  className="sidebar-mode-icon"
-  width="14"
-  height="14"
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+    viewBox="0 0 226.44 196.11"
   >
-  <defs>
-    <style>.cls-1{fill:none;}</style>
-  </defs>
-  <g id="レイヤー_2" data-name="レイヤー 2">
-    <g id="レイヤー_1-2" data-name="レイヤー 1">
-      <rect class="cls-1" width="23" height="23"/>
-      <path d="M20.86,3.92a.64.64,0,0,1,.64.63v13.9a.64.64,0,0,1-.64.63H2.14a.64.64,0,0,1-.64-.63V4.55a.64.64,0,0,1,.64-.63H20.86m0-1.5H2.14A2.13,2.13,0,0,0,0,4.55v13.9a2.13,2.13,0,0,0,2.14,2.13H20.86A2.13,2.13,0,0,0,23,18.45V4.55a2.13,2.13,0,0,0-2.14-2.13Z"/>
-      <rect x="7.49" y="3.05" width="1.2" height="16.91"/>
-    </g>
-  </g>
-</svg>
-
+    <path
+      d="M56.61 196.11L169.83 196.11 226.44 98.06 188.7 98.06 150.96 163.43 75.48 163.43 56.61 196.11z"
+      className="group2"
+    >
+    </path>
+    <path
+      // eslint-disable-next-line max-len
+      d="M75.48 98.05L94.35 65.37 150.96 65.38 207.57 65.37 207.57 65.38 226.44 98.06 169.83 98.06 113.22 98.06 94.39 130.66 94.3 130.66 84.92 114.4 75.48 98.05z"
+      className="group1"
+    >
+    </path>
+    <path
+      d="M0 98.06L56.6 0 113.22 0.01 169.83 0.01 169.83 0.01 188.69 32.68 132.09 32.69 75.47 32.69 18.86 130.74 0 98.06z"
+      className="group1"
+    >
+    </path>
+    <path
+      d="M75.48 163.43L56.61 130.74 37.71 163.46 47.15 179.81 56.54 196.07 56.63 196.07 75.48 163.43z"
+      className="group1"
+    >
+    </path>
+  </svg>
 );
 
-export default SidebarDockIcon;
+//   <svg
+//   xmlns="http://www.w3.org/2000/svg"
+//   viewBox="0 0 23 23"
+//   className="sidebar-mode-icon"
+//   width="14"
+//   height="14"
+//   >
+//   <defs>
+//     <style>.cls-1{fill:none;}</style>
+//   </defs>
+//   <g id="レイヤー_2" data-name="レイヤー 2">
+//     <g id="レイヤー_1-2" data-name="レイヤー 1">
+//       <rect class="cls-1" width="23" height="23"/>
+//       <path d="M20.86,3.92a.64.64,0,0,1,.64.63v13.9a.64.64,0,0,1-.64.63H2.14a.64.64,0,0,1-.64-.63V4.55a.64.64,0,0,1,.64-.63H20.86m0-1.5H2.14A2.13,2.13,0,0,0,0,4.55v13.9a2.13,2.13,0,0,0,2.14,2.13H20.86A2.13,2.13,0,0,0,23,18.45V4.55a2.13,2.13,0,0,0-2.14-2.13Z"/>
+//       <rect x="7.49" y="3.05" width="1.2" height="16.91"/>
+//     </g>
+//   </g>
+// </svg>
 
+export default SidebarDockIcon;
