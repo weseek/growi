5da4cdc7d898076c168ca3a5797cf0c0b6d2c808
itizawa <itizawa@weseek.co.jp>
2020-08-05 11:04:02 +0000

    add comment

--- src/server/routes/page.js
+++ src/server/routes/page.js
@@ -1348,6 +1348,7 @@ module.exports = function(crowi, app) {
    * @apiParam {String} new_path New path name.
    * @apiParam {Bool} create_redirect
    */
+  // TODO remove after GW-3429 and GW-3430
   api.rename = async function(req, res) {
     const pageId = req.body.page_id;
     const previousRevision = req.body.revision_id || null;
