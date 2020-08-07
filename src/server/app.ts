077dcb823fd9ff140dee9095dfb53f8d603c6f24
Yuki Takei <yuki@weseek.co.jp>
2020-08-07 14:21:34 +0000

    transplant index.ts -> app.ts

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,21 +1,16 @@
-/**
- * Growi::app.js
- *
- * @package growi
- * @author  Yuki Takei <yuki@weseek.co.jp>
- */
+import path from 'path';
+import Logger from 'bunyan';
 
-require('module-alias/register');
+import loggerFactory from '~/utils/logger';
+import { hasProcessFlag } from '~/utils/process-utils';
 
-const logger = require('@alias/logger')('growi');
-const helpers = require('@commons/util/helpers');
-const growi = new (require('./crowi'))(helpers.root());
+const logger: Logger = loggerFactory('growi');
 
 
 /** **********************************
  *          Main Process
  ********************************** */
-process.on('uncaughtException', (err) => {
+process.on('uncaughtException', (err?: Error) => {
   logger.error('Uncaught Exception: ', err);
 });
 
@@ -23,17 +18,25 @@ process.on('unhandledRejection', (reason, p) => {
   logger.error('Unhandled Rejection: Promise:', p, 'Reason:', reason);
 });
 
-growi.start()
-  .then((server) => {
-    if (helpers.hasProcessFlag('ci')) {
+async function main() {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const Crowi = require('./crowi');
+    const growi = new Crowi(path.resolve(__dirname, '../'));
+    const server = await growi.start();
+
+    if (hasProcessFlag('ci')) {
       logger.info('"--ci" flag is detected. Exit process.');
       server.close(() => {
         process.exit();
       });
     }
-  })
-  .catch((err) => {
+  }
+  catch (err) {
     logger.error('An error occurred, unable to start the server');
     logger.error(err);
     process.exit(1);
-  });
+  }
+}
+
+main();
