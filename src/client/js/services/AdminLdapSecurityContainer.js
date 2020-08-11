15670dd14838b804d6cc9e417cf448c06df645a7
yusuketk <yusuketk@weseek.co.jp>
2020-08-11 16:48:51 +0900

    ldap

--- src/client/js/services/AdminLdapSecurityContainer.js
+++ src/client/js/services/AdminLdapSecurityContainer.js
@@ -15,10 +15,12 @@ export default class AdminLdapSecurityContainer extends Container {
     super();
 
     this.appContainer = appContainer;
+    this.dummyServerUrl = 0;
 
     this.state = {
       retrieveError: null,
-      serverUrl: '',
+      // set dummy value tile for using suspense
+      serverUrl: this.dummyServerUrl,
       isUserBind: false,
       ldapBindDN: '',
       ldapBindDNPassword: '',
