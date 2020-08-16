2bbb08caa7f75170c20b65e1aee913bf0a615bb1
Yuki Takei <yuki@weseek.co.jp>
2020-08-16 17:37:15 +0000

    load boot.js for all pages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,11 @@
-import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
+import Document, {
+  DocumentContext, DocumentInitialProps,
+  Html, Head, Main, NextScript,
+} from 'next/document';
+
+import path from 'path';
+
+import customManifest from '^/.next/custom-manifest.json';
 
 class GrowiDocument extends Document {
 
@@ -9,6 +16,22 @@ class GrowiDocument extends Document {
     console.log(configManager.getConfig('crowi', 'customize:layout'));
 
     return initialProps;
+  }
+
+  render() {
+    const bootJsPath = path.join('/_next', customManifest['boot.js']);
+
+    return (
+      <Html>
+        <Head>
+          <script src={bootJsPath}></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 
 }
