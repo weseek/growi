e4f7a17d1a1a701ea56ff89f951323ca4ec2d580
Yuki Takei <yuki@weseek.co.jp>
2020-08-16 20:41:07 +0000

    set layout class with GrowiDocumentProps

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,28 +5,39 @@ import Document, {
 
 import path from 'path';
 
-import customManifest from '^/.next/custom-manifest.json';
+interface GrowiDocumentProps {
+  bootJsPath: string;
+  layout: string;
+}
+declare type GrowiDocumentInitialProps = DocumentInitialProps & GrowiDocumentProps;
 
-class GrowiDocument extends Document {
+class GrowiDocument extends Document<GrowiDocumentProps> {
 
-  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-    const initialProps = await Document.getInitialProps(ctx);
+  static i = 0;
+
+  static async getInitialProps(ctx: DocumentContext): Promise<GrowiDocumentInitialProps> {
+    const initialProps: DocumentInitialProps = await Document.getInitialProps(ctx);
+
+    const customManifest: any = await import('^/.next/custom-manifest.json');
+    const bootJsPath = path.join('/_next', customManifest['boot.js']);
 
     const { configManager } = ctx.req?.crowi;
-    console.log(configManager.getConfig('crowi', 'customize:layout'));
 
-    return initialProps;
+    const layout = configManager.getConfig('crowi', 'customize:layout');
+
+    return { ...initialProps, bootJsPath, layout };
   }
 
-  render() {
-    const bootJsPath = path.join('/_next', customManifest['boot.js']);
+  render(): JSX.Element {
+
+    const { bootJsPath, layout } = this.props;
 
     return (
       <Html>
         <Head>
           <script src={bootJsPath}></script>
         </Head>
-        <body>
+        <body className={layout}>
           <Main />
           <NextScript />
         </body>
