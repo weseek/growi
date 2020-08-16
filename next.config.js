f85047e517c66e72423408ba1ed06dab067050b5
Yuki Takei <yuki@weseek.co.jp>
2020-08-16 17:36:51 +0000

    add webpack entry point for boot.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,40 @@
 const path = require('path');
 
+const WebpackAssetsManifest = require('webpack-assets-manifest');
+
+// define additional entries
+const additionalWebpackEntries = {
+  boot: './src/client/js/boot',
+};
+
 module.exports = {
   poweredByHeader: false,
   webpack(config) {
+
+    // configure additional entries
+    const orgEntry = config.entry;
+    config.entry = () => {
+      return orgEntry().then((entry) => {
+        return { ...entry, ...additionalWebpackEntries };
+      });
+    };
+
+    // configure alias
     config.resolve.alias = {
       ...config.resolve.alias,
       '~': path.resolve(__dirname, './src'), // src
       '^': path.resolve(__dirname, './'), // project root
       '@alias/logger': path.resolve(__dirname, './src/utils/logger'), // alias for logger
     };
+
+    // configure plugins
+    config.plugins.push(
+      new WebpackAssetsManifest({
+        publicPath: true,
+        output: 'custom-manifest.json',
+      }),
+    );
+
     return config;
   },
 };
