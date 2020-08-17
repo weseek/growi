98bf8d8ea8321785dbc2c3c4689b3e3cbaba4472
kaoritokashiki <kaori-t@weseek.co.jp>
2020-08-17 11:20:32 +0900

    adjust responsive for the Preview

--- src/client/js/components/PageEditor/GridEditModal.jsx
+++ src/client/js/components/PageEditor/GridEditModal.jsx
@@ -180,15 +180,15 @@ export default class GridEditModal extends React.Component {
             </div>
             <div className="row">
               <h1 className="pl-3 w-100">Preview</h1>
-              <div className="col-6">
+              <div className="col-md-6">
                 <label className="d-block"><i className="pr-2 icon-screen-desktop"></i>Desktop</label>
                 <div className="desktop-preview border d-block"></div>
               </div>
-              <div className="col-3">
+              <div className="col-md-3">
                 <label className="d-block"><i className="pr-2 icon-screen-tablet"></i>Tablet</label>
                 <div className="tablet-preview border d-block"></div>
               </div>
-              <div className="col-3">
+              <div className="col-md-3">
                 <label className="d-block"><i className="pr-2 icon-screen-smartphone"></i>Mobile</label>
                 <div className="mobile-preview border d-block"></div>
               </div>
