1ba84c6e753a8227e0614a7d802dfd7ae864e9fe
oshikishintaro <oshikishin@gmail.com>
2020-08-18 21:13:11 +0900

    -Removed the size specification from the icon component used in the fab. Renamed "NewPageIcon" to "CreatePageIcon".

--- src/client/js/components/Icons/CreatePageIcon.jsx
+++ src/client/js/components/Icons/CreatePageIcon.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 
-const NewPageIcon = () => (
+const CreatePageIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="27"
-    height="27"
     viewBox="0 0 27 30"
   >
     <path
@@ -25,4 +23,4 @@ const NewPageIcon = () => (
 );
 
 
-export default NewPageIcon;
+export default CreatePageIcon;
