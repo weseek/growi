b52755b1a5dbfb14183c0ef2b3ff8a06830d8098
zahmis <meda829ratteru86@gmail.com>
2020-08-20 09:33:11 +0000

    add pageDummyDataError

--- src/client/js/components/PageList.jsx
+++ src/client/js/components/PageList.jsx
@@ -1,49 +1,52 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 import Page from './PageList/Page';
 import { withUnstatedContainers } from './UnstatedUtils';
+import { toastError } from '../util/apiNotification';
 
 import { withLoadingSppiner } from './SuspenseUtils';
 
-
 import AppContainer from '../services/AppContainer';
 import PageContainer from '../services/PageContainer';
+
+// let retrieveError = null;
+const pageDummyData = 0;
+const pageDummyDataError = 1;
+let pages = pageDummyData;
 
 const PageList = (props) => {
   const { appContainer, pageContainer } = props;
   const { path } = pageContainer.state;
 
-  const [pages, setPages] = useState(null);
+  // const [pages, setPages] = useState(pageDummyData);
   // const [spinnerError, setSpinnerError] = useState(null);
+
+  if (pages === pageDummyDataError) {
+    throw new Error('hoge');
+  }
 
   const getPageList = useCallback(async() => {
     const res = await appContainer.apiv3Get('/pages/list', { path });
-    setPages(res.data.pages);
+    pages = res.data.pages;
   }, [appContainer, path]);
 
   useEffect(() => {
     getPageList();
   }, [getPageList]);
 
-  if (pages == null) {
-    // throw (async() => {
-    //   try {
-    //     await hoge;
-    //   }
-    //   catch (err) {
-    //     setSpinnerError('hoge');
-    //     return spinnerError;
-    //   }
-    // });
-    throw new Promise(async() => {
-      try { await [hoge] }
-      catch (err) {
-        console.log('hoge');
-      }
-    });
-  }
 
+  if (pages === pageDummyData) {
+    throw (async() => {
+      try {
+        await getPageList();
+      }
+      catch (err) {
+        pages = 1;
+        toastError(err);
+      }
+    })();
+  }
 
   return pages.map(page => (
     <li key={page._id}>
