603e989f7d82626e24080ce9c58c5cd3d51c21ca
Yuki Takei <yuki@weseek.co.jp>
2020-08-22 18:07:38 +0000

    use useTranslation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,29 @@
 import Link from 'next/link';
-import PropTypes from 'prop-types';
 
 import nextI18Next from '~/i18n';
 
 import Layout from '../components/Layout';
 
-const IndexPage = ({ t }) => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      {t('Help')}
-    </p>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-);
+const IndexPage = () => {
+  const { t } = nextI18Next.useTranslation();
+
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <h1>Hello Next.js 👋</h1>
+      <p>
+        {t('Help')}
+      </p>
+      <p>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </p>
+    </Layout>
+  );
+};
 
 IndexPage.getInitialProps = async() => ({
   namespacesRequired: ['translation'],
 });
-
-IndexPage.propTypes = {
-  t: PropTypes.func.isRequired,
-};
 
 export default nextI18Next.withTranslation()(IndexPage);
