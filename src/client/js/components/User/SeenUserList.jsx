4e0eb9858d372f8fdd6e6958b0221454e5005fb8
Yuki Takei <yuki@weseek.co.jp>
2020-08-25 13:19:01 +0000

    adjust layout

--- src/client/js/components/User/SeenUserList.jsx
+++ src/client/js/components/User/SeenUserList.jsx
@@ -21,7 +21,7 @@ const SeenUserList = (props) => {
   const { pageContainer } = props;
   return (
     <div className="grw-seen-user-list pl-2 ml-2">
-      <Button id="po-seen-user" color="link" className="px-2 py-0">
+      <Button id="po-seen-user" color="link" className="px-2">
         <span className="mr-1 footstamp-icon"><FootstampIcon /></span>
         <span className="seen-user-count">{pageContainer.state.countOfSeenUsers}</span>
       </Button>
