bada034b01569ca69e408fe762a683b1d7a2d77e
kaoritokashiki <kaori-t@weseek.co.jp>
2020-08-26 17:14:12 +0900

    add arguments in method

--- src/client/js/components/Navbar/ThreeStrandedButton.jsx
+++ src/client/js/components/Navbar/ThreeStrandedButton.jsx
@@ -6,23 +6,23 @@ const ThreeStrandedButton = (props) => {
 
   const { t } = props;
 
-  function threeStrandedButtonClickedHandler() {
+  function threeStrandedButtonClickedHandler(clickedButton) {
     if (props.onThreeStrandedButtonClicked != null) {
-      props.onThreeStrandedButtonClicked();
+      props.onThreeStrandedButtonClicked(clickedButton);
     }
   }
 
   return (
     <div className="btn-group grw-three-stranded-button" role="group " aria-label="three-stranded-button">
-      <button type="button" className="btn btn-outline-primary view-button" onClick={() => { threeStrandedButtonClickedHandler() }}>
+      <button type="button" className="btn btn-outline-primary view-button" onClick={() => { threeStrandedButtonClickedHandler('view') }}>
         <i className="icon-control-play icon-fw" />
         { t('view') }
       </button>
-      <button type="button" className="btn btn-outline-primary edit-button" onClick={() => { threeStrandedButtonClickedHandler() }}>
+      <button type="button" className="btn btn-outline-primary edit-button" onClick={() => { threeStrandedButtonClickedHandler('edit') }}>
         <i className="icon-note icon-fw" />
         { t('Edit') }
       </button>
-      <button type="button" className="btn btn-outline-primary hackmd-button" onClick={() => { threeStrandedButtonClickedHandler() }}>
+      <button type="button" className="btn btn-outline-primary hackmd-button" onClick={() => { threeStrandedButtonClickedHandler('hackmd') }}>
         <i className="fa fa-fw fa-file-text-o" />
         { t('hackmd.hack_md') }
       </button>
