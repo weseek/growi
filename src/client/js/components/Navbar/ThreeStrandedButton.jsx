dc525cf7bd42800e0883278ae0d14518c6216edb
kaoritokashiki <kaori-t@weseek.co.jp>
2020-08-26 16:17:37 +0900

    using eventHandler when buttons are clicked

--- src/client/js/components/Navbar/ThreeStrandedButton.jsx
+++ src/client/js/components/Navbar/ThreeStrandedButton.jsx
@@ -14,15 +14,15 @@ const ThreeStrandedButton = (props) => {
 
   return (
     <div className="btn-group grw-three-stranded-button" role="group " aria-label="three-stranded-button">
-      <button type="button" className="btn btn-outline-primary view-button">
+      <button type="button" className="btn btn-outline-primary view-button" onClick={() => { threeStrandedButtonClickedHandler() }}>
         <i className="icon-control-play icon-fw" />
         { t('view') }
       </button>
-      <button type="button" className="btn btn-outline-primary edit-button">
+      <button type="button" className="btn btn-outline-primary edit-button" onClick={() => { threeStrandedButtonClickedHandler() }}>
         <i className="icon-note icon-fw" />
         { t('Edit') }
       </button>
-      <button type="button" className="btn btn-outline-primary hackmd-button">
+      <button type="button" className="btn btn-outline-primary hackmd-button" onClick={() => { threeStrandedButtonClickedHandler() }}>
         <i className="fa fa-fw fa-file-text-o" />
         { t('hackmd.hack_md') }
       </button>
