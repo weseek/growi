df44bf8f852d7a1c8b56fa1c360cfd42a560486a
itizawa <itizawa@weseek.co.jp>
2020-08-28 03:51:24 +0000

    copy config

--- src/migrations/20200828024025-copy-aws-setting.js
+++ src/migrations/20200828024025-copy-aws-setting.js
@@ -4,12 +4,34 @@ const logger = require('@alias/logger')('growi:migrate:remove-layout-setting');
 const mongoose = require('mongoose');
 const config = require('@root/config/migrate');
 
-// const { getModelSafely } = require('@commons/util/mongoose-utils');
+const { getModelSafely } = require('@commons/util/mongoose-utils');
 
 module.exports = {
   async up(db, client) {
     logger.info('Apply migration');
     mongoose.connect(config.mongoUri, config.mongodb.options);
+
+    const Config = getModelSafely('Config') || require('@server/models/config')();
+
+    const [accessKeyId, secretAccessKey] = await Promise.all([
+      Config.findOne({ key: 'aws:accessKeyId' }),
+      Config.findOne({ key: 'aws:secretAccessKey' }),
+    ]);
+
+    const promise = [
+      Config.create({
+        key: 'mail:sesAccessKeyId',
+        ns: 'crowi',
+        value: accessKeyId.value,
+      }),
+      Config.create({
+        key: 'mail:sesSecretAccessKey',
+        ns: 'crowi',
+        value: secretAccessKey.value,
+      }),
+    ];
+
+    await Promise.all(promise);
 
     logger.info('Migration has successfully applied');
   },
@@ -17,6 +39,10 @@ module.exports = {
   async down(db, client) {
     logger.info('Rollback migration');
     mongoose.connect(config.mongoUri, config.mongodb.options);
+
+    const Config = getModelSafely('Config') || require('@server/models/config')();
+
+    await Config.deleteMany({ key: { $in: ['mail:sesAccessKeyId', 'mail:sesSecretAccessKey'] } });
 
     logger.info('Migration has been successfully rollbacked');
   },
