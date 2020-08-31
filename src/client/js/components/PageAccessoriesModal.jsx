78994abc53385c3edbf4fe5853134e3b642bd2c3
白石誠 <makoto-s@weseek.co.jp>
2020-08-31 15:12:43 +0900

    modified a little

--- src/client/js/components/PageAccessoriesModal.jsx
+++ src/client/js/components/PageAccessoriesModal.jsx
@@ -83,9 +83,7 @@ const PageAccessoriesModal = (props) => {
               {pageAccessoriesContainer.state.activeComponents.has('pagelist') && <PageList />}
             </TabPane>
             <TabPane tabId="timeline" className="p-4">
-              {
-                pageAccessoriesContainer.state.activeComponents.has('timeline') && <PageTimeline fromModal />
-              }
+              {pageAccessoriesContainer.state.activeComponents.has('timeline') && <PageTimeline fromModal /> }
             </TabPane>
             <TabPane tabId="page-history">
               <div className="overflow-auto">
