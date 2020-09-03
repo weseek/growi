35e6317979d09d5cf231ef5a20606e305ca9c5fc
takeru0001 <takerudunk@gmail.com>
2020-09-03 17:12:08 +0900

    add LikeCount component

---
+++ src/client/js/components/LikeCount.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class LikeCount extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  appContainer.apiv3.get('/page/likeCount', { pageId });
+
+
+  render() {
+
+    return ();
+  }
+}
