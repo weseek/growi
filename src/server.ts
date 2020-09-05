bd79244897b603863185757d2f22545c30c8cfee
Yuki Takei <yuki@weseek.co.jp>
2020-09-05 19:37:15 +0000

    fix server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,24 @@
 import { Configuration, Inject, PlatformApplication } from '@tsed/common';
 import { GlobalAcceptMimesMiddleware } from '@tsed/platform-express';
-import * as express from 'express';
-import * as cookieParser from 'cookie-parser';
-import * as methodOverride from 'method-override';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import methodOverride from 'method-override';
 
 const rootDir = __dirname;
 
 @Configuration({
   rootDir,
   port: process.env.PORT || 3000,
+  httpsPort: false,
   acceptMimes: ['application/json'],
 })
 export class Server {
 
   @Inject()
-  app: PlatformApplication<Express.Application>;
+  app!: PlatformApplication<Express.Application>;
 
-  // @Configuration()
-  // settings: Configuration;
+  @Configuration()
+  settings!: Configuration;
 
   /**
    * This method let you configure the express middleware required by your application to works.
