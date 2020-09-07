02091d9f4bead90b9ecf85e2c6e8d84f8c5cb39e
Yuki Takei <yuki@weseek.co.jp>
2020-09-07 18:35:52 +0000

    initialize mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
-import { Configuration, Inject, PlatformApplication } from '@tsed/common';
+import {
+  Configuration, Inject, PlatformApplication, Value,
+} from '@tsed/common';
 import { GlobalAcceptMimesMiddleware } from '@tsed/platform-express';
+
 import express from 'express';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import methodOverride from 'method-override';
 
+import mongoose from 'mongoose';
+
 import loggerFactory from '~/utils/logger';
 import { SafeRedirectMiddleware } from './server/middlewares/safe-redirect';
+import { getMongoUri, mongoOptions } from './server/util/mongoose-utils';
 
 const rootDir = __dirname;
 const logger = loggerFactory('growi:Server');
@@ -22,14 +28,32 @@ const logger = loggerFactory('growi:Server');
     '${rootDir}/server/service/**/*.ts',
     /* eslint-enable no-template-curly-in-string */
   ],
+  mongoose: [ // @tsed/mongoose format configuration
+    {
+      id: 'default',
+      url: getMongoUri(),
+      connectionOptions: mongoOptions,
+    },
+  ],
 })
 export class Server {
 
   @Inject()
   app!: PlatformApplication<Express.Application>;
 
-  @Configuration()
-  settings!: Configuration;
+  @Value('mongoose')
+  mongooseConfig!: any[];
+
+  $beforeInit(): void | Promise<any> {
+    return this.initMongoose();
+  }
+
+  private initMongoose(): Promise<typeof mongoose> {
+    // initialize mongoose without @tsed/mongoose
+    //  because mongoose.model() does not work when using @tsed/mongoose
+    const { url, connectionOptions } = this.mongooseConfig[0];
+    return mongoose.connect(url, connectionOptions);
+  }
 
   /**
    * This method let you configure the express middleware required by your application to works.
