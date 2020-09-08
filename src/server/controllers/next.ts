c85d15cf13674fc1622036a7f4b47b8ac34228ba
Yuki Takei <yuki@weseek.co.jp>
2020-09-08 12:32:18 +0000

    setup hmr route

--- src/server/controllers/next.ts
+++ src/server/controllers/next.ts
@@ -1,25 +1,49 @@
-import { Controller, PlatformRouter } from '@tsed/common';
+import {
+  Controller, Get, Constant,
+  Req, Res, Request, Response,
+} from '@tsed/common';
 import next from 'next';
+import Server from 'next/dist/next-server/server/next-server';
+import { Env } from '@tsed/core';
 
 @Controller('/')
 export class NextCtrl {
 
-  constructor(private router: PlatformRouter) {
+  @Constant('env')
+  private env!: Env;
+
+  private nextApp!: Server;
+
+  constructor() {
     this.init();
   }
 
   async init(): Promise<void> {
     const dev = process.env.NODE_ENV !== 'production';
-    const nextApp = next({ dev });
+    this.nextApp = next({ dev });
 
-    await nextApp.prepare();
+    return this.nextApp.prepare();
+  }
 
-    const handle = nextApp.getRequestHandler();
+  @Get('/*')
+  get(@Req() req: Request, @Res() res: Response): Promise<void> {
+    const handle = this.nextApp.getRequestHandler();
+    return handle(req, res);
+  }
 
-    this.router.get('/*', (req, res) => {
-      // req.crowi = this;
-      return handle(req, res);
-    });
+  /**
+   * For HMR
+   * @param req
+   * @param res
+   */
+  @Get('/_next/webpack-hmr')
+  hmr(@Req() req: Request, @Res() res: Response): Promise<void> {
+    if (this.env !== Env.DEV) {
+      return res.status(403).send('webpack-hmr is enabled only when development');
+    }
+
+    const handle = this.nextApp.getRequestHandler();
+    return handle(req, res);
   }
 
 }
