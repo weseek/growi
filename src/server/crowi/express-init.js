d02c97317586739f34ba372fbaf134619b6b9728
Yuki Takei <yuki@weseek.co.jp>
2020-09-09 11:38:36 +0000

    omit unnecessary lines

--- src/server/crowi/express-init.js
+++ src/server/crowi/express-init.js
@@ -6,7 +6,7 @@ import registerSafeRedirect from '../middlewares/safe-redirect';
 
 module.exports = function(crowi, app) {
   const debug = require('debug')('growi:crowi:express-init');
-  const path = require('path');
+  // const path = require('path');
   const express = require('express');
   const helmet = require('helmet');
   const cookieParser = require('cookie-parser');
@@ -14,51 +14,50 @@ module.exports = function(crowi, app) {
   const passport = require('passport');
   const expressSession = require('express-session');
   const flash = require('connect-flash');
-  const swig = require('swig-templates');
-  const webpackAssets = require('express-webpack-assets');
+  // const swig = require('swig-templates');
+  // const webpackAssets = require('express-webpack-assets');
 
   const injectCurrentuserToLocalvars = require('../middlewares/inject-currentuser-to-localvars')();
   const autoReconnectToS2sMsgServer = require('../middlewares/auto-reconnect-to-s2s-msg-server')(crowi);
 
   const avoidSessionRoutes = require('../routes/avoid-session-routes');
 
-  const env = crowi.node_env;
+  // const env = crowi.node_env;
 
   app.use(helmet());
 
-  app.use((req, res, next) => {
-    const now = new Date();
-    // for datez
+  // app.use((req, res, next) => {
+  //   const now = new Date();
+  //   // for datez
 
-    const Page = crowi.model('Page');
-    const Config = crowi.model('Config');
-    app.set('tzoffset', crowi.appService.getTzoffset());
+  //   const Page = crowi.model('Page');
+  //   app.set('tzoffset', crowi.appService.getTzoffset());
 
-    req.csrfToken = null;
+  //   req.csrfToken = null;
 
-    res.locals.req = req;
-    res.locals.baseUrl = crowi.appService.getSiteUrl();
-    res.locals.env = env;
-    res.locals.now = now;
-    res.locals.consts = {
-      pageGrants: Page.getGrantLabels(),
-    };
-    res.locals.local_config = Config.getLocalconfig(); // config for browser context
+  //   res.locals.req = req;
+  //   res.locals.baseUrl = crowi.appService.getSiteUrl();
+  //   res.locals.env = env;
+  //   res.locals.now = now;
+  //   res.locals.consts = {
+  //     pageGrants: Page.getGrantLabels(),
+  //   };
+  //   res.locals.local_config = Config.getLocalconfig(); // config for browser context
 
-    next();
-  });
+  //   next();
+  // });
 
   app.set('port', crowi.port);
   const staticOption = (crowi.node_env === 'production') ? { maxAge: '30d' } : {};
   app.use(express.static(crowi.publicDir, staticOption));
-  app.engine('html', swig.renderFile);
-  app.use(webpackAssets(
-    path.join(crowi.publicDir, 'manifest.json'),
-    { devMode: (crowi.node_env === 'development') },
-  ));
+  // app.engine('html', swig.renderFile);
+  // app.use(webpackAssets(
+  //   path.join(crowi.publicDir, 'manifest.json'),
+  //   { devMode: (crowi.node_env === 'development') },
+  // ));
   // app.set('view cache', false);  // Default: true in production, otherwise undefined. -- 2017.07.04 Yuki Takei
-  app.set('view engine', 'html');
-  app.set('views', crowi.viewsDir);
+  // app.set('view engine', 'html');
+  // app.set('views', crowi.viewsDir);
   app.use(methodOverride());
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ extended: true, limit: '50mb' }));
@@ -88,10 +87,10 @@ module.exports = function(crowi, app) {
   app.use(injectCurrentuserToLocalvars);
   app.use(autoReconnectToS2sMsgServer);
 
-  const middlewares = require('../util/middlewares')(crowi, app);
-  app.use(middlewares.swigFilters(swig));
-  app.use(middlewares.swigFunctions());
-  app.use(middlewares.csrfKeyGenerator());
+  // const middlewares = require('../util/middlewares')(crowi, app);
+  // app.use(middlewares.swigFilters(swig));
+  // app.use(middlewares.swigFunctions());
+  // app.use(middlewares.csrfKeyGenerator());
 
   app.use(nextI18NextMiddleware(nextI18next));
 };
