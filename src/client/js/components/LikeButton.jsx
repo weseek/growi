ddde4798f157a9b10740ee81489473a526b44222
ryuichi-e <ryuichi-e@weseek.co.jp>
2020-09-10 15:53:17 +0900

    remove d-edit-none class from LikeButton.jsx

--- src/client/js/components/LikeButton.jsx
+++ src/client/js/components/LikeButton.jsx
@@ -43,7 +43,7 @@ class LikeButton extends React.Component {
       <button
         type="button"
         onClick={this.handleClick}
-        className={`btn rounded-circle btn-like border-0 d-edit-none
+        className={`btn rounded-circle btn-like border-0
         ${this.state.isLiked ? 'active' : ''}`}
       >
         <i className="icon-like"></i>
