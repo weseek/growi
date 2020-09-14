bf7775fd216948fbc793b9e372a420f160d4171f
Yuki Takei <yuki@weseek.co.jp>
2020-09-14 18:02:50 +0000

    refactor GrowiNavbar to functional component

--- src/client/js/components/Navbar/GrowiNavbar.jsx
+++ src/client/js/components/Navbar/GrowiNavbar.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 
 import {
-  useAppTitle, useSiteUrl, useConfidential,
+  useAppTitle, useConfidential, useCurrentUser,
 } from '~/stores/context';
 
 import { withUnstatedContainers } from '../UnstatedUtils';
@@ -39,70 +39,74 @@ const Confidential = () => {
   );
 };
 
-class GrowiNavbar extends React.Component {
+const NavbarRight = (props) => {
+  const { t, navigationContainer } = props;
+  const { data: currentUser } = useCurrentUser();
 
-  renderNavbarRight() {
-    const { t, appContainer, navigationContainer } = this.props;
-    const { currentUser } = appContainer;
-
-    // render login button
-    if (currentUser == null) {
-      return <li id="login-user" className="nav-item"><a className="nav-link" href="/login">Login</a></li>;
-    }
-
-    return (
-      <>
-        <li className="nav-item d-none d-md-block">
-          <button className="px-md-2 nav-link btn-create-page border-0 bg-transparent" type="button" onClick={navigationContainer.openPageCreateModal}>
-            <i className="icon-pencil mr-2"></i>
-            <span className="d-none d-lg-block">{ t('New') }</span>
-          </button>
-        </li>
-
-        <li className="grw-personal-dropdown nav-item dropdown dropdown-toggle dropdown-toggle-no-caret">
-          {/* <PersonalDropdown /> */}
-        </li>
-      </>
-    );
+  // render login button
+  if (currentUser == null) {
+    return <li id="login-user" className="nav-item"><a className="nav-link" href="/login">Login</a></li>;
   }
 
-  render() {
-    const { appContainer, navigationContainer } = this.props;
-    // const { crowi, isSearchServiceConfigured } = appContainer.config;
-    // const { isDeviceSmallerThanMd } = navigationContainer.state;
+  return (
+    <>
+      <li className="nav-item d-none d-md-block">
+        <button className="px-md-2 nav-link btn-create-page border-0 bg-transparent" type="button" onClick={navigationContainer.openPageCreateModal}>
+          <i className="icon-pencil mr-2"></i>
+          <span className="d-none d-lg-block">{ t('New') }</span>
+        </button>
+      </li>
 
-    return (
-      <nav className="navbar grw-navbar navbar-expand navbar-dark sticky-top mb-0 px-0">
+      <li className="grw-personal-dropdown nav-item dropdown dropdown-toggle dropdown-toggle-no-caret">
+        {/* <PersonalDropdown /> */}
+      </li>
+    </>
+  );
+};
+NavbarRight.propTypes = {
+  t: PropTypes.func.isRequired, //  i18next
 
-        {/* Brand Logo  */}
-        <div className="navbar-brand mr-0">
-          <a className="grw-logo d-block" href="/">
-            <GrowiLogo />
-          </a>
+  navigationContainer: PropTypes.instanceOf(NavigationContainer).isRequired,
+};
+
+
+const GrowiNavbar = (props) => {
+
+  // const { crowi, isSearchServiceConfigured } = appContainer.config;
+  // const { isDeviceSmallerThanMd } = navigationContainer.state;
+
+  return (
+
+    <nav className="navbar grw-navbar navbar-expand navbar-dark sticky-top mb-0 px-0">
+
+      {/* Brand Logo  */}
+      <div className="navbar-brand mr-0">
+        <a className="grw-logo d-block" href="/">
+          <GrowiLogo />
+        </a>
+      </div>
+
+      <div className="grw-app-title d-none d-md-block">
+        <AppTilte />
+      </div>
+
+
+      {/* Navbar Right  */}
+      <ul className="navbar-nav ml-auto">
+        <NavbarRight {...props} />
+      </ul>
+
+      <Confidential />
+
+      {/* { isSearchServiceConfigured && !isDeviceSmallerThanMd && (
+        <div className="grw-global-search grw-global-search-top position-absolute">
+          <GlobalSearch />
         </div>
+      ) } */}
+    </nav>
+  );
 
-        <div className="grw-app-title d-none d-md-block">
-          <AppTilte />
-        </div>
-
-
-        {/* Navbar Right  */}
-        <ul className="navbar-nav ml-auto">
-          {/* {this.renderNavbarRight()} */}
-        </ul>
-
-        <Confidential />
-
-        {/* { isSearchServiceConfigured && !isDeviceSmallerThanMd && (
-          <div className="grw-global-search grw-global-search-top position-absolute">
-            <GlobalSearch />
-          </div>
-        ) } */}
-      </nav>
-    );
-  }
-
-}
+};
 
 /**
  * Wrapper component for using unstated
