92a20bc0beac9bc39b4bd7d84e90ed73e8b9cfca
Yuki Takei <yuki@weseek.co.jp>
2020-09-14 16:02:12 +0000

    clean code

--- src/stores/context.tsx
+++ src/stores/context.tsx
@@ -2,17 +2,17 @@ import useSWR, { responseInterface } from 'swr';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const useCurrentUser = (initialData?: any): responseInterface<any, any> => {
-  return useSWR('static/app/currentUser', { initialData });
+  return useSWR('currentUser', { initialData });
 };
 
 export const useAppTitle = (initialData?: string): responseInterface<string, any> => {
-  return useSWR('static/app/appTitle', { initialData });
+  return useSWR('appTitle', { initialData });
 };
 
 export const useSiteUrl = (initialData?: string): responseInterface<string, any> => {
-  return useSWR('static/app/siteUrl', { initialData });
+  return useSWR('siteUrl', { initialData });
 };
 
 export const useConfidential = (initialData?: string): responseInterface<string, any> => {
-  return useSWR('static/app/confidential', { initialData });
+  return useSWR('confidential', { initialData });
 };
