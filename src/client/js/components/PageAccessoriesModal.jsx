5f71aa89a859210bd273fd397a961944a70b4408
zahmis <meda829ratteru86@gmail.com>
2020-09-14 18:22:06 +0900

    Revert "3rem m-5"
    This reverts commit 45d649ea4f7f2d1c7608032f8ad10a5bfd930288.

--- src/client/js/components/PageAccessoriesModal.jsx
+++ src/client/js/components/PageAccessoriesModal.jsx
@@ -89,7 +89,7 @@ const PageAccessoriesModal = (props) => {
               </NavLink>
             </NavItem>
           </Nav>
-          <TabContent activeTab={activeTab} className="m-5">
+          <TabContent activeTab={activeTab}>
 
             <TabPane tabId="pagelist">
               {pageAccessoriesContainer.state.activeComponents.has('pagelist') && <PageList />}
