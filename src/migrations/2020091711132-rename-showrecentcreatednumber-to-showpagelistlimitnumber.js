5a6d318d863775c919a5884baede443ca2b82f80
yusuketk <yusuketk@weseek.co.jp>
2020-09-17 12:14:34 +0900

    create migrate file

---
+++ src/migrations/2020091711132-rename-showrecentcreatednumber-to-showpagelistlimitnumber.js
@@ -0,0 +1,36 @@
+require('module-alias/register');
+const logger = require('@alias/logger')('growi:migrate:rename-showrecentcreatednumber-to-showpagelistlimitnumber');
+
+const mongoose = require('mongoose');
+const config = require('@root/config/migrate');
+
+const { getModelSafely } = require('@commons/util/mongoose-utils');
+
+module.exports = {
+  async up(db) {
+    logger.info('Apply migration');
+    mongoose.connect(config.mongoUri, config.mongodb.options);
+
+    const Config = getModelSafely('Config') || require('@server/models/config')();
+
+    await Config.findOneAndUpdate(
+      { key: 'customize:showRecentCreatedNumber' },
+      { key: 'customize:showPageListLimitNumber' },
+      { upsert: true },
+    );
+
+    logger.info('Migration has successfully applied');
+  },
+
+  async down(db) {
+    mongoose.connect(config.mongoUri, config.mongodb.options);
+
+    const Config = getModelSafely('Config') || require('@server/models/config')();
+
+    await Config.findOneAndUpdate(
+      { key: 'customize:showPageListLimitNumber' },
+      { key: 'customize:showRecentCreatedNumber' },
+      { upsert: true },
+    );
+  },
+};
