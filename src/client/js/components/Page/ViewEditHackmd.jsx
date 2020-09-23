6af49a28d326d63dcecb3c1b77e0599f297815f4
zahmis <meda829ratteru86@gmail.com>
2020-09-23 16:35:03 +0000

    add unstatedContainer

--- src/client/js/components/Page/ViewEditHackmd.jsx
+++ src/client/js/components/Page/ViewEditHackmd.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import PageEditor from '../PageEditor';
 import Page from '../Page';
 import PageEditorByHackmd from '../PageEditorByHackmd';
+import { withUnstatedContainers } from '../UnstatedUtils';
+import NavigationContainer from '../../services/NavigationContainer';
 
 
-function ViewEditHackmd() {
+const ViewEditHackmd = (props) => {
+  const { navigationContainer } = props;
 
   return (
     <div>
-      <Page />
-      <PageEditor />
-      <PageEditorByHackmd />
+      {navigationContainer.state.editorMode === 'view' && 'view'}
+      {navigationContainer.state.editorMode === 'edit' && 'edit'}
+      {navigationContainer.state.editorMode === 'hackmd' && 'hackmd'}
     </div>
   );
-}
 
-export default ViewEditHackmd;
+};
+
+ViewEditHackmd.propTypes = {
+  navigationContainer: propTypes.instanceOf(NavigationContainer).isRequired,
+};
+
+
+export default withUnstatedContainers(ViewEditHackmd, [NavigationContainer]);
