6d834f4a4eb524c77835363bc8bb97dbf910ba83
zahmis <meda829ratteru86@gmail.com>
2020-09-23 18:32:34 +0000

    add component

--- src/client/js/components/Page/DisplaySwitcher.jsx
+++ src/client/js/components/Page/DisplaySwitcher.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import propTypes from 'prop-types';
 import { withUnstatedContainers } from '../UnstatedUtils';
 import NavigationContainer from '../../services/NavigationContainer';
+import Editor from '../PageEditor/Editor';
+import Page from '../Page';
+import PageEditorByHackmd from '../PageEditorByHackmd';
 
 
 const DisplaySwitcher = (props) => {
   const { navigationContainer } = props;
 
   return (
-    <>
-      {navigationContainer.state.editorMode === 'view' && 'view'}
-      {navigationContainer.state.editorMode === 'edit' && 'edit'}
-      {navigationContainer.state.editorMode === 'hackmd' && 'hackmd'}
-    </>
+    <div className="">
+      {navigationContainer.state.editorMode === 'view' && <Page />}
+      {/* {navigationContainer.state.editorMode === 'edit' && <Editor />} */}
+      {navigationContainer.state.editorMode === 'hackmd' && <PageEditorByHackmd />}
+    </div>
   );
 
 };
