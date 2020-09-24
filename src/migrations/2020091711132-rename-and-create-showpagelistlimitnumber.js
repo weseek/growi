b38c17cdcd1a302a80ca6f46c186568849db172f
yusuketk <yusuketk@weseek.co.jp>
2020-09-24 17:31:34 +0900

    rename migration file

--- src/migrations/2020091711132-rename-and-create-showpagelistlimitnumber.js
+++ src/migrations/2020091711132-rename-and-create-showpagelistlimitnumber.js
@@ -11,13 +11,21 @@ module.exports = {
     logger.info('Apply migration');
     mongoose.connect(config.mongoUri, config.mongodb.options);
 
-    const Config = getModelSafely('Config') || require('@server/models/config')();
+    // const Config = getModelSafely('Config') || require('@server/models/config')();
 
-    await Config.findOneAndUpdate(
-      { ns: 'crowi', key: 'customize:showRecentCreatedNumber' },
-      { ns: 'crowi', key: 'customize:showPageListLimitNumber' },
-      { upsert: true },
-    );
+    // await Config.findOneAndUpdate(
+    //   { ns: 'crowi', key: 'customize:showRecentCreatedNumber' },
+    //   { ns: 'crowi', key: 'customize:A' },
+    //   { upsert: true },
+    // );
+
+    // Config.save([
+    //   { ns: 'crowi', key: 'customize:B', value: 10 },
+    //   { ns: 'crowi', key: 'customize:B', value: 10 },
+    // ]);
+    // Config.update();
+    // Config.update({ ns: 'crowi', key: 'customize:C' }, { value: 10 }, { upsert: true });
+    // Config.update({ ns: 'crowi', key: 'customize:D' }, { value: 10 }, { upsert: true });
 
     logger.info('Migration has successfully applied');
   },
@@ -28,9 +36,11 @@ module.exports = {
     const Config = getModelSafely('Config') || require('@server/models/config')();
 
     await Config.findOneAndUpdate(
-      { ns: 'crowi', key: 'customize:showPageListLimitNumber' },
+      { ns: 'crowi', key: 'customize:A' },
       { ns: 'crowi', key: 'customize:showRecentCreatedNumber' },
       { upsert: true },
     );
+
+    logger.info('Migration has successfully applied');
   },
 };
