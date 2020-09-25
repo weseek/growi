92728f02ee924886b548bfc51a9985e17b5dd658
Yuki Takei <yuki@weseek.co.jp>
2020-09-25 14:11:45 +0000

    fix module path

--- src/migrations/20200903080025-remove-timeline-type.js.js
+++ src/migrations/20200903080025-remove-timeline-type.js.js
@@ -1,17 +1,18 @@
-require('module-alias/register');
-const logger = require('@alias/logger')('growi:migrate:remove-behavior-type');
+import loggerFactory from '~/utils/logger';
+
+const logger = loggerFactory('growi:migrate:remove-timeline-type');
 
 const mongoose = require('mongoose');
-const config = require('@root/config/migrate');
+const config = require('^/config/migrate');
 
-const { getModelSafely } = require('@commons/util/mongoose-utils');
+const { getModelSafely } = require('~/server/util/mongoose-utils');
 
 module.exports = {
   async up(db, client) {
     logger.info('Apply migration');
     mongoose.connect(config.mongoUri, config.mongodb.options);
 
-    const Config = getModelSafely('Config') || require('@server/models/config')();
+    const Config = getModelSafely('Config') || require('~/server/models/config')();
 
     await Config.findOneAndDelete({ key: 'customize:timeline' }); // remove timeline
 
@@ -23,7 +24,7 @@ module.exports = {
     logger.info('Rollback migration');
     mongoose.connect(config.mongoUri, config.mongodb.options);
 
-    const Config = getModelSafely('Config') || require('@server/models/config')();
+    const Config = getModelSafely('Config') || require('~/server/models/config')();
 
     const insertConfig = new Config({
       ns: 'crowi',
--- src/server/routes/apiv3/attachment.js
+++ src/server/routes/apiv3/attachment.js
@@ -1,4 +1,4 @@
-const loggerFactory = require('@alias/logger');
+import loggerFactory from '@alias/logger';
 
 const logger = loggerFactory('growi:routes:apiv3:attachment'); // eslint-disable-line no-unused-vars
 
