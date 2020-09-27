9252fbf1f9dd020cd3dedac2e8eac6bdbf24cb0e
zahmis <meda829ratteru86@gmail.com>
2020-09-27 19:16:17 +0000

    put back button

--- src/client/js/components/Navbar/ThreeStrandedButton.jsx
+++ src/client/js/components/Navbar/ThreeStrandedButton.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
-import { Nav, NavItem, NavLink } from 'reactstrap';
 
 const ThreeStrandedButton = (props) => {
   const { t } = props;
