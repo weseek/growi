da0527cdf08c0194f5f69adc1a2fd43f89b1e39a
Yuki Takei <yuki@weseek.co.jp>
2020-09-27 20:12:46 +0000

    reorganize layout file

--- src/components/RawLayout.tsx
+++ src/components/RawLayout.tsx
@@ -1,28 +1,12 @@
 import React, { ReactNode } from 'react';
-import Link from 'next/link';
 import Head from 'next/head';
 
-import { withUnstatedContainers } from '~/client/js/components/UnstatedUtils';
-import GrowiNavbar from '~/client/js/components/Navbar/GrowiNavbar';
-import GrowiNavbarBottom from '~/client/js/components/Navbar/GrowiNavbarBottom';
-
-// import AppContainer from '~/client/js/services/AppContainer';
-import CounterContainer from '~/client/js/services/CounterContainer';
-
-
-import { useCurrentUser } from '~/stores/context';
-
 type Props = {
-  // appContainer: AppContainer,
-  counterContainer: CounterContainer,
-
   title: string
   children?: ReactNode
 }
 
-const Layout = ({ counterContainer, children, title }: Props) => {
-
-  const { data: currentUser } = useCurrentUser();
+const RawLayout = ({ children, title }: Props): JSX.Element => {
 
   return (
     <>
@@ -32,45 +16,10 @@ const Layout = ({ counterContainer, children, title }: Props) => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className="wrapper">
-        <GrowiNavbar />
-
-        <div className="page-wrapper d-flex d-print-block">
-          <div id="grw-sidebar-wrapper"></div>
-
-          <div className="flex-fill mw-0">
-            {children}
-          </div>
-        </div>
-
-        <GrowiNavbarBottom />
+        {children}
       </div>
-
-      <header>
-        <nav>
-          <Link href="/[[...path]]" as="/" shallow>
-            <a>Home</a>
-          </Link>{' '}
-          |{' '}
-          <Link href="/about">
-            <a>About</a>
-          </Link>{' '}
-          |{' '}
-          <Link href="/users">
-            <a>Users List</a>
-          </Link>{' '}
-          | <a href="/api/users">Users API</a>
-        </nav>
-      </header>
-
-      <footer>
-        <hr />
-        <span>I&apos;m here to stay (Footer)</span>
-        <hr />
-        <p>count: {counterContainer.state.count}</p>
-        <p>currentUser.username: {currentUser?.username}</p>
-      </footer>
     </>
   );
 };
 
-export default withUnstatedContainers(Layout, [CounterContainer]);
+export default RawLayout;
