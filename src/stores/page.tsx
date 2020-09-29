d58a3bd317e2899daa1f5639696ffd2ada6db1e7
Yuki Takei <yuki@weseek.co.jp>
2020-09-29 09:30:53 +0000

    support ConfigInterface in useRecentlyUpdatedSWR

--- src/stores/page.tsx
+++ src/stores/page.tsx
@@ -1,10 +1,12 @@
 import useSWR, { responseInterface } from 'swr';
+import { ConfigInterface } from 'swr/dist/types';
 import { apiv3Get } from '~/client/js/util/apiv3-client';
 
-export const useRecentlyUpdatedSWR = <Data, Error>(): responseInterface<Data, Error> => {
+export const useRecentlyUpdatedSWR = <Data, Error>(config?: ConfigInterface): responseInterface<Data, Error> => {
   const endpoint = '/pages/recent';
   return useSWR(
     endpoint,
     () => apiv3Get(endpoint).then(response => response.data),
+    config,
   );
 };
