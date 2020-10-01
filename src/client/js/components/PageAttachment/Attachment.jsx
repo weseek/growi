ef001d5cd410e621688e65da194f5f51bfe185d8
zahmis <meda829ratteru86@gmail.com>
2020-10-01 07:56:43 +0000

    fix

--- src/client/js/components/PageAttachment/Attachment.jsx
+++ src/client/js/components/PageAttachment/Attachment.jsx
@@ -54,7 +54,7 @@ export default class Attachment extends React.Component {
       : '';
 
     return (
-      <div className="attachment">
+      <li className="attachment">
         <span className="mr-1 attachment-userpicture">
           <UserPicture user={attachment.creator} size="sm"></UserPicture>
         </span>
@@ -67,7 +67,7 @@ export default class Attachment extends React.Component {
 
         {btnDownload}
         {btnTrash}
-      </div>
+      </li>
     );
   }
 
