8e4b564f70ee4373dbe5b7a2124c832fef44d163
Yuki Takei <yuki@weseek.co.jp>
2020-10-05 16:39:11 +0000

    fix invalid attribute type

--- src/client/js/components/PageAttachment.jsx
+++ src/client/js/components/PageAttachment.jsx
@@ -109,7 +109,7 @@ class PageAttachment extends React.Component {
       deleteAttachmentModal = (
         <DeleteAttachmentModal
           isOpen={showModal}
-          animation={false}
+          animation="false"
           toggle={deleteModalClose}
 
           attachmentToDelete={attachmentToDelete}
