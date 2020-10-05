b6564035733d505edf2275d50f869be4668a9c79
Yuki Takei <yuki@weseek.co.jp>
2020-10-05 14:37:28 +0000

    refactor LabeledProgressBar

--- src/client/js/components/Admin/Common/LabeledProgressBar.jsx
+++ src/client/js/components/Admin/Common/LabeledProgressBar.jsx
@@ -2,38 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 
-class LabeledProgressBar extends React.Component {
+const LabeledProgressBar = (props) => {
 
+  const {
+    header, currentCount, totalCount, isInProgress,
+  } = props;
 
-  render() {
-    const {
-      header, currentCount, totalCount, isInProgress,
-    } = this.props;
+  const percentage = currentCount / totalCount * 100;
+  const isActive = (isInProgress != null)
+    ? isInProgress //                         apply props.isInProgress if set
+    : (currentCount !== totalCount); //       otherwise, set true when currentCount does not equal totalCount
 
-    const percentage = currentCount / totalCount * 100;
-    const isActive = (isInProgress != null)
-      ? isInProgress //                         apply props.isInProgress if set
-      : (currentCount !== totalCount); //       otherwise, set true when currentCount does not equal totalCount
-
-    return (
-      <>
-        <h6 className="my-1">
-          {header}
-          <div className="float-right">{currentCount} / {totalCount}</div>
-        </h6>
-        <div className="progress">
-          <div
-            className={`progress-bar ${isActive ? 'bg-info progress-bar-striped active' : 'bg-success'}`}
-            style={{ width: `${percentage}%` }}
-          >
-            <span className="sr-only">{percentage.toFixed(0)}% Complete</span>
-          </div>
+  return (
+    <>
+      <h6 className="my-1">
+        {header}
+        <div className="float-right">{currentCount} / {totalCount}</div>
+      </h6>
+      <div className="progress">
+        <div
+          className={`progress-bar ${isActive ? 'bg-info progress-bar-striped active' : 'bg-success'}`}
+          style={{ width: `${percentage}%` }}
+        >
+          <span className="sr-only">{percentage.toFixed(0)}% Complete</span>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 
-}
+};
 
 LabeledProgressBar.propTypes = {
   header: PropTypes.string.isRequired,
