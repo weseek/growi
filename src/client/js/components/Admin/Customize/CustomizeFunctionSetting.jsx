5b52ab9b4d187ff25eb32319d380bc4fc7efbf00
kaori <kaori@weseek.co.jp>
2020-10-07 11:24:59 +0900

    success to show each label and description of the Dropdowns

--- src/client/js/components/Admin/Customize/CustomizeFunctionSetting.jsx
+++ src/client/js/components/Admin/Customize/CustomizeFunctionSetting.jsx
@@ -114,7 +114,7 @@ class CustomizeFunctionSetting extends React.Component {
                 <div className="form-group row">
                   <div className="offset-md-3 col-md-6 text-left">
                     <div className="my-0 w-100">
-                      {/* <label>{t(value.label)}</label> */}
+                      <label>{t(value.label)}</label>
                     </div>
                     <UncontrolledDropdown>
                       <DropdownToggle className="text-right col-6" caret>
@@ -133,7 +133,7 @@ class CustomizeFunctionSetting extends React.Component {
                       </DropdownMenu>
                     </UncontrolledDropdown>
                     <p className="form-text text-muted">
-                      {/* {t(value.desc)} */}
+                      {t(value.desc)}
                     </p>
                   </div>
                 </div>
