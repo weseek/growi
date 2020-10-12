c7b15f5fddca493dccdee8baa36e0bcc926bbc38
zahmis <meda829ratteru86@gmail.com>
2020-10-12 05:54:53 +0000

    fix mydraftlist

--- src/client/js/components/MyDraftList/MyDraftList.jsx
+++ src/client/js/components/MyDraftList/MyDraftList.jsx
@@ -16,7 +16,6 @@ class MyDraftList extends React.Component {
 
   constructor(props) {
     super(props);
-    const { appContainer } = this.props;
 
     this.state = {
       drafts: [],
@@ -24,7 +23,7 @@ class MyDraftList extends React.Component {
       activePage: 1,
       totalDrafts: 0,
       // [TODO: rename pageLimitationM to pageLimitationL]
-      pagingLimit: appContainer.getConfig().pageLimitationM || 30,
+      pagingLimit: Infinity,
     };
 
     this.handlePage = this.handlePage.bind(this);
