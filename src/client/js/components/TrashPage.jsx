0401a9485f643e4909a2d2db94c866372d336539
kaori <kaori@weseek.co.jp>
2020-10-13 11:39:44 +0900

    remove unnecessary cotainers

--- src/client/js/components/TrashPage.jsx
+++ src/client/js/components/TrashPage.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 import PageListIcon from './Icons/PageListIcon';
-
-import { withUnstatedContainers } from './UnstatedUtils';
-
-import AppContainer from '../services/AppContainer';
-import PageContainer from '../services/PageContainer';
 import CustomNavigation from './CustomNavigation';
 
 
@@ -30,13 +25,8 @@ const TrashPage = (props) => {
   );
 };
 
-const PageListWrapper = withUnstatedContainers(TrashPage, [AppContainer, PageContainer]);
-
-
 TrashPage.propTypes = {
   t: PropTypes.func.isRequired, //  i18next
-  appContainer: PropTypes.instanceOf(AppContainer),
-  pageContainer: PropTypes.instanceOf(PageContainer),
 };
 
-export default withTranslation()(PageListWrapper);
+export default withTranslation()(TrashPage);
