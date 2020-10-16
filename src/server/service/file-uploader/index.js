aa92222e9a0e246dff5fbeee3c6d03c673140a0c
yusuketk <yusuketk@weseek.co.jp>
2020-10-16 15:58:14 +0900

    add todo comment

--- src/server/service/file-uploader/index.js
+++ src/server/service/file-uploader/index.js
@@ -14,7 +14,9 @@ const envToModuleMappings = {
 class FileUploadServiceFactory {
 
   initializeUploader(crowi) {
+    // temporarily use envToModuleMappings [remove in GW-4136]
     const fileUplodeTypeInConfig = envToModuleMappings[crowi.configManager.getConfig('crowi', 'app:fileUploadType')];
+
     const method = envToModuleMappings[process.env.FILE_UPLOAD] || fileUplodeTypeInConfig || 'ppp';
     const modulePath = `./${method}`;
     this.uploader = require(modulePath)(crowi);
