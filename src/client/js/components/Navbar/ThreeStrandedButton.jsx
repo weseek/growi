d64c248713d548bc8975e68d86cdf9e4f6e97065
itizawa <itizawa@weseek.co.jp>
2020-10-16 07:41:46 +0000

    adjust

--- src/client/js/components/Navbar/ThreeStrandedButton.jsx
+++ src/client/js/components/Navbar/ThreeStrandedButton.jsx
@@ -12,7 +12,6 @@ const ThreeStrandedButton = (props) => {
     if (isBtnDisabled) {
       return;
     }
-
     if (props.onThreeStrandedButtonClicked != null) {
       props.onThreeStrandedButtonClicked(viewType);
     }
