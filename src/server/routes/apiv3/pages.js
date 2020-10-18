eb0277a344466d6fb5d76f256eed1094bcecf62e
kaori <kaori@weseek.co.jp>
2020-10-18 23:01:18 +0900

    define "false" as default bool and remove the else block

--- src/server/routes/apiv3/pages.js
+++ src/server/routes/apiv3/pages.js
@@ -90,13 +90,10 @@ module.exports = (crowi) => {
     const offset = +req.query.offset || 0;
     const { isTrashPage } = require('@commons/util/path-utils');
 
-    let includeTrashed;
+    let includeTrashed = false;
 
     if (isTrashPage(path)) {
       includeTrashed = true;
-    }
-    else {
-      includeTrashed = false;
     }
 
     const queryOptions = {
