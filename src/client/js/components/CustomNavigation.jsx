e81b6536f008ec4dc8a17e1c6700a8f135ea7262
kaori <kaori@weseek.co.jp>
2020-10-20 20:22:38 +0900

    imported useRef

--- src/client/js/components/CustomNavigation.jsx
+++ src/client/js/components/CustomNavigation.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import {
   Nav, NavItem, NavLink, TabContent, TabPane,
