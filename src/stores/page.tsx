157d81029b05ad2ed4eb4ee0aae69f4e9b4ad719
Yuki Takei <yuki@weseek.co.jp>
2020-10-21 20:39:21 +0000

    clean swr code

--- src/stores/page.tsx
+++ src/stores/page.tsx
@@ -3,10 +3,9 @@ import { ConfigInterface } from 'swr/dist/types';
 import { apiv3Get } from '~/client/js/util/apiv3-client';
 
 export const useRecentlyUpdatedSWR = <Data, Error>(config?: ConfigInterface): responseInterface<Data, Error> => {
-  const endpoint = '/pages/recent';
   return useSWR(
-    endpoint,
-    () => apiv3Get(endpoint).then(response => response.data),
+    '/pages/recent',
+    endpoint => apiv3Get(endpoint).then(response => response.data),
     config,
   );
 };
