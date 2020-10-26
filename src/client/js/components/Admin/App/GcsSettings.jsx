0662b4bcb656517f599bdf218edc2801214d1ca4
yusuketk <yusuketk@weseek.co.jp>
2020-10-26 16:46:14 +0900

    rename

--- src/client/js/components/Admin/App/GcsSettings.jsx
+++ src/client/js/components/Admin/App/GcsSettings.jsx
@@ -18,7 +18,7 @@ function GcsSetting(props) {
       {gcsUseOnlyEnvVars && (
         <p
           className="alert alert-info"
-          dangerouslySetInnerHTML={{ __html: t('admin:app_setting.note_for_the_only_env_option', { env: 'IS_GCS_ENV_PRIORITIZED' }) }}
+          dangerouslySetInnerHTML={{ __html: t('admin:app_setting.note_for_the_only_env_option', { env: 'GCS_PRIORITIZES_ENV_VARS_FOR_SOME_OPTIONS' }) }}
         />
       )}
       <table className={`table settings-table ${gcsUseOnlyEnvVars && 'use-only-env-vars'}`}>
