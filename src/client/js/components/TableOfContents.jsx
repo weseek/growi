d5dced7e817c4ed8e81ec84df420d221263ce1f2
zahmis <meda829ratteru86@gmail.com>
2020-10-29 06:36:43 +0000

    fix name

--- src/client/js/components/TableOfContents.jsx
+++ src/client/js/components/TableOfContents.jsx
@@ -67,11 +67,11 @@ const TableOfContents = (props) => {
 
       { isUserPage && (
       <div className="mt-3 d-flex justify-content-around">
-        <a className="btn btn-outline-secondary btn-sm" href="#Bookmarks">
+        <a className="btn btn-outline-secondary btn-sm" href="#bookmarks-list">
           <i className="mr-2 icon-star"></i>
           <span>Bookmarks</span>
         </a>
-        <a className="btn btn-outline-secondary btn-sm" href="#RecentlyCreated">
+        <a className="btn btn-outline-secondary btn-sm" href="#recently-created-list">
           <i className="grw-icon-container-recently-created mr-2"><RecentlyCreatedIcon /></i>
           <span>Recently Created</span>
         </a>
