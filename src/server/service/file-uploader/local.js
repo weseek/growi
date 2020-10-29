bf2531c515f8258ee93dabd7e7d5d5d369958bde
yusuketk <yusuketk@weseek.co.jp>
2020-10-29 15:48:54 +0900

    adjust

--- src/server/service/file-uploader/local.js
+++ src/server/service/file-uploader/local.js
@@ -99,7 +99,9 @@ module.exports = function(crowi) {
    */
   lib.canRespond = () => {
     // Check whether to use internal redirect of nginx or Apache.
-    return process.env.FILE_UPLOAD === 'local' && lib.configManager.getConfig('crowi', 'fileUpload:local:useInternalRedirect');
+    return (
+      crowi.configManager.getConfig('crowi', 'app:fileUploadType') === 'local' && lib.configManager.getConfig('crowi', 'fileUpload:local:useInternalRedirect')
+    );
   };
 
   /**
