cd4e50673160f26752af66c5fd0f2ddadbed9620
kaori <kaori@weseek.co.jp>
2020-11-06 14:13:18 +0900

    rename  functions to show icons

--- src/client/js/components/Me/PersonalSettings.jsx
+++ src/client/js/components/Me/PersonalSettings.jsx
@@ -10,74 +10,43 @@ import ApiSettings from './ApiSettings';
 
 class PersonalSettings extends React.Component {
 
-
   render() {
     const { t } = this.props;
 
-    // const Icons = [
-    //   <i className="icon-fw icon-user"></i>,
-    //   <i className="icon-fw icon-share-alt"></i>,
-    //   <i className="icon-fw icon-lock"></i>,
-    //   <i className="icon-fw icon-paper-plane"></i>,
-    // ];
-    // console.log(`Icons = ${Icons}`);
-
-    // const setIcons = () => {
-    //   Icons.map((icon) => {
-    //     console.log(icon);
-    //     return (
-    //       <>
-    //         { icon }
-    //       </>
-    //     );
-    //   });
-    // };
-    // const setIcons = () => {
-    //   Icons.map(icon => icon);
-    // };
-
-    const setIcon1 = () => {
-      return (
-        <i className="icon-fw icon-user"></i>
-      );
+    const UserIcon = () => {
+      return <i className="icon-fw icon-user"></i>;
     };
-    const setIcon2 = () => {
-      return (
-        <i className="icon-fw icon-share-alt"></i>
-      );
+    const shereAltIcon = () => {
+      return <i className="icon-fw icon-share-alt"></i>;
     };
-    const setIcon3 = () => {
-      return (
-        <i className="icon-fw icon-lock"></i>
-      );
+    const lockIcon = () => {
+      return <i className="icon-fw icon-lock"></i>;
     };
-    const setIcon4 = () => {
-      return (
-        <i className="icon-fw icon-paper-plan"></i>
-      );
+    const paperPlaneIcon = () => {
+      return <i className="icon-fw icon-paper-plane"></i>;
     };
 
     const navTabMapping = {
       user_infomation: {
-        Icon: setIcon1,
+        Icon: UserIcon,
         Content: UserSettings,
         i18n: t('User Information'),
         index: 0,
       },
       external_accounts: {
-        Icon: setIcon2,
+        Icon: shereAltIcon,
         Content: ExternalAccountLinkedMe,
         i18n: t('admin:user_management.external_accounts'),
         index: 1,
       },
       password_settings: {
-        Icon: setIcon3,
+        Icon: lockIcon,
         Content: PasswordSettings,
         i18n: t('Password Settings'),
         index: 2,
       },
       api_settings: {
-        Icon: setIcon4,
+        Icon: paperPlaneIcon,
         Content: ApiSettings,
         i18n: t('API Settings'),
         index: 3,
