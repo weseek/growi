6ff67b8f71cc3860232581df46a98c153251cc5b
kaori <kaori@weseek.co.jp>
2020-11-11 22:03:42 +0900

    add container-fluied and remove duplicated content-main class

--- src/client/js/components/Me/PersonalSettings.jsx
+++ src/client/js/components/Me/PersonalSettings.jsx
@@ -49,10 +49,15 @@ class PersonalSettings extends React.Component {
     return (
       <>
         <header className="py-3">
-          <h1 className="title">{ t('User Settings') }</h1>
+          <div className="container-fluid">
+            <h1 className="title">{ t('User Settings') }</h1>
+          </div>
         </header>
-        <div id="grw-fav-sticky-trigger" className="sticky-top"></div>
-        <CustomNavigation navTabMapping={navTabMapping} tabContentClasses={['px-0']} />
+        <div id="main" className="main">
+          <div id="content-main" className="content-main container-lg">
+            <CustomNavigation navTabMapping={navTabMapping} tabContentClasses={['px-0']} />
+          </div>
+        </div>
       </>
     );
   }
