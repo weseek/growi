416b58543a1bad8be0cc73567b04e92642657028
zamis <meda829ratteru86@gmail.com>
2020-11-11 16:34:32 +0900

    fix

--- src/client/js/components/ForbiddenPage.jsx
+++ src/client/js/components/ForbiddenPage.jsx
@@ -11,7 +11,7 @@ const ForbiddenPage = (props) => {
         <div className="col-lg-12">
           <h2 className="text-muted">
             <i className="icon-ban" aria-hidden="true"></i>
-      Forbidden
+            Forbidden
           </h2>
         </div>
       </div>
