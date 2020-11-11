5c0c6a33b29db01a9a759d85d1989c3680530567
itizawa <itizawa@weseek.co.jp>
2020-11-11 22:44:01 +0900

    add isAbleToKnowAttachedPageInformation

--- src/client/js/services/PageContainer.js
+++ src/client/js/services/PageContainer.js
@@ -105,9 +105,10 @@ export default class PageContainer extends Container {
     this.checkAndUpdateImageUrlCached(this.state.likerUsers);
 
     const { currentUser } = this.appContainer;
+    // see https://dev.growi.org/5fabddf8bbeb1a0048bcb9e9
+    const isAbleToKnowAttachedPageInformation = this.state.pageId != null || !(currentUser == null && this.state.isSharedPage);
 
-    // retrieve information when the page exists and the user is logged in
-    if (currentUser != null && this.state.pageId != null) {
+    if (isAbleToKnowAttachedPageInformation) {
       this.retrieveSeenUsers();
       this.retrieveLikeInfo();
       this.retrieveBookmarkInfo();
