53e6965ef7d9fc702c634a6ed2ecdd45a8e3df81
zamis <meda829ratteru86@gmail.com>
2020-11-17 08:27:30 +0000

    fix

--- src/client/js/components/Page/DisplaySwitcher.jsx
+++ src/client/js/components/Page/DisplaySwitcher.jsx
@@ -18,13 +18,13 @@ const DisplaySwitcher = (props) => {
     <>
       <TabContent activeTab={editorMode}>
         <TabPane tabId="view">
-          <div className="d-lg-none d-md-block ">
-            <div className="d-flex pull-right">
-              <PageAccessories />
-            </div>
-            <div>&nbsp;</div>
-            <div className="border-bottom">&nbsp;</div>
+          {/* <div className="d-lg-none d-md-block "> */}
+          <div className="d-flex justify-content-end border-bottom">
+            <PageAccessories />
           </div>
+          {/* <div>&nbsp;</div>
+            <div className="border-bottom">&nbsp;</div> */}
+          {/* </div> */}
 
           {/* <div className="d-lg-none d-md-block border-bottom">&nbsp; <br /></div> */}
 
