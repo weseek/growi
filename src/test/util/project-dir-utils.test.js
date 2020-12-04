fd2d0f45ed45d3097933cec3353c5f78c1fbe3aa
haruhiko <haruhiko@weseek.co.jp>
2020-12-04 17:26:51 +0900

    refs GW-4652 utils/project-dir-utils.ts

---
+++ src/test/util/project-dir-utils.test.js
@@ -0,0 +1,34 @@
+
+import path from 'path';
+import { projectRoot, resolveFromRoot } from '~/utils/project-dir-utils';
+
+jest.mock('path');
+jest.mock('fs');
+// TODO: Make mock changes for each test.
+jest.mock('process', () => {
+  return {
+    cwd: jest.fn().mockImplementation(() => {
+      return 'cwd';
+    }),
+  };
+});
+
+// TODO: Make mock changes for each test.
+jest.mock('fs', () => {
+  return {
+    existsSync: jest.fn().mockImplementation(() => {
+      return true;
+    }),
+  };
+});
+
+describe('.resolveFromRoot', () => {
+  beforeAll(() => {
+    path.resolve.mockImplementation(jest.fn());
+  });
+  test('should intended functions coll when ', () => {
+    resolveFromRoot('relativePath');
+    expect(path.resolve.mock.calls[0]).toMatchObject(['cwd', 'relativePath']);
+  });
+});
+// TODO: Add test of when isServer false and fs.existsSync false
