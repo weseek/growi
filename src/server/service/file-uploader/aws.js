63ef9f717a63cda71ce468ae387993b6c1819015
itizawa <itizawa@weseek.co.jp>
2020-12-07 04:00:23 +0000

    adjust

--- src/server/service/file-uploader/aws.js
+++ src/server/service/file-uploader/aws.js
@@ -73,7 +73,7 @@ module.exports = function(crowi) {
   };
 
   lib.canRespond = function() {
-    return this.configManager.getConfig('crowi', 'aws:isEnabledInternalStreamSystem');
+    return !this.configManager.getConfig('crowi', 'aws:isEnabledInternalStreamSystem');
   };
 
   lib.respond = async function(res, attachment) {
