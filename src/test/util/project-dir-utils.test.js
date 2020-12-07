f3950d6b44492f1c579f6ae312265cda83959c6d
haruhiko <haruhiko@weseek.co.jp>
2020-12-07 17:48:03 +0900

    - Response feedback

--- src/test/util/project-dir-utils.test.js
+++ src/test/util/project-dir-utils.test.js
@@ -1,7 +1,6 @@
 import path from 'path';
 import { projectRoot, resolveFromRoot } from '~/utils/project-dir-utils';
 
-jest.mock('path');
 jest.mock('fs');
 // TODO: Make mock changes for each test.
 jest.mock('process', () => {
@@ -22,12 +21,9 @@ jest.mock('fs', () => {
 });
 
 describe('.resolveFromRoot', () => {
-  beforeAll(() => {
-    path.resolve.mockImplementation(jest.fn());
-  });
-  test('should intended functions coll when ', () => {
-    resolveFromRoot('relativePath');
-    expect(path.resolve.mock.calls[0]).toMatchObject(['cwd', 'relativePath']);
+  test('should intended return path when isServr true and fs.existsSync true', () => {
+    const result = resolveFromRoot('relativePath');
+    expect(result).toBe(`${path.resolve()}/cwd/relativePath`);
   });
 });
 // TODO: Add test of when isServer false and fs.existsSync false
