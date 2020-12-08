d50d8dbf3734977af1c8e7987759e106ed1db408
haruhiko <haruhiko@weseek.co.jp>
2020-12-08 17:32:47 +0900

    - Add test of getServerSideCommonProps

--- src/test/util/nextjs-page-utils.test.js
+++ src/test/util/nextjs-page-utils.test.js
@@ -2,6 +2,32 @@ import { getServerSideCommonProps, useCustomTitle, useCustomTitleForPage } from 
 import DevidedPagePath from '~/models/devided-page-path';
 jest.mock('~/models/devided-page-path');
 
+describe('.getServerSideCommonProps', () => {
+  test('should be return replaced text', async() => {
+    const result = await getServerSideCommonProps(
+      {
+        req: {
+          crowi: {
+            appService: { getAppTitle: jest.fn().mockImplementation(() => { return 'appTitle' }) },
+            customizeService: { customTitleTemplate: 'customTitleTemplate' }
+          }
+        },
+        resolvedUrl: 'resolvedUrl',
+      }
+    )
+    expect(result).toMatchObject(
+      {
+        props: {
+          namespacesRequired: [ 'translation' ],
+          currentPagePath: '/resolvedUrl',
+          appTitle: 'appTitle',
+          customTitleTemplate: 'customTitleTemplate'
+        }
+      }
+    )
+  });
+});
+
 describe('.useCustomTitle', () => {
   test('should be return replaced text', () => {
     const result = useCustomTitle(
