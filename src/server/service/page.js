c4ae8277f5c9a349a4d106486abfc6a69cf4d475
zamis <meda829ratteru86@gmail.com>
2020-12-16 08:00:13 +0000

    adjust

--- src/server/service/page.js
+++ src/server/service/page.js
@@ -161,7 +161,6 @@ class PageService {
 
   async handlePrivatePagesForDeletedGroup(deletedGroup, action, transferToUserGroupId) {
     const Page = this.crowi.model('Page');
-
     const pages = await Page.find({ grantedGroup: deletedGroup });
 
     switch (action) {
