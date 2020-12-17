70514a43f0192f98dd8dfae2752f8c1035fe1917
kaori <kaori@weseek.co.jp>
2020-12-17 16:43:06 +0900

    replace escape sequence to a valiable

--- src/client/js/components/PageEditor/GridEditModal.jsx
+++ src/client/js/components/PageEditor/GridEditModal.jsx
@@ -68,7 +68,8 @@ class GridEditModal extends React.Component {
   pasteCodedGrid() {
     const { colsRatios, responsiveSize } = this.state;
     const convertedHTML = geu.convertRatiosAndSizeToHTML(colsRatios, responsiveSize);
-    const pastedGridData = `::: editable-row\n<div class="container">\n\t<div class="row">\n${convertedHTML}\n\t</div>\n</div>\n:::`;
+    const spaceTab = '    ';
+    const pastedGridData = `::: editable-row\n<div class="container">\n${spaceTab}<div class="row">\n${convertedHTML}\n${spaceTab}</div>\n</div>\n:::`;
     // display converted html on console
     console.log(convertedHTML);
 
