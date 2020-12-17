6368e3d852432b50dcc1953fe2824c8280cb6dae
yusuketk <yusuketk@weseek.co.jp>
2020-12-17 19:16:08 +0900

    fix

--- src/client/js/util/PreProcessor/EasyGrid.js
+++ src/client/js/util/PreProcessor/EasyGrid.js
@@ -2,7 +2,7 @@ export default class EasyGrid {
 
   process(markdown) {
     // see: https://regex101.com/r/WR6IvX/3
-    return markdown.replace(/:::\s*editable-row[\r\n]((.|[\r\n])*):::/, (all, group) => {
+    return markdown.replace(/:::\s*editable-row[\r\n]((.|[\r\n])*?)[\r\n]:::/gm, (all, group) => {
       return group;
     });
   }
