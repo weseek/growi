0fc5d27ad886da4288cfc336b49de793015d9d4e
itizawa <itizawa@weseek.co.jp>
2020-12-24 05:00:57 +0000

    create likeButton

---
+++ src/components/Atoms/LikeButton.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+
+type Props= {
+  onCLick?: () => void;
+  count?: number;
+  isLiked?: boolean;
+}
+
+export const LikeButton:FC<Props> = (props:Props) => {
+  // defaultValue
+  const { onCLick, count = 0, isLiked = false } = props;
+
+  const handleClick = () => {
+    if (onCLick == null) {
+      return;
+    }
+    onCLick();
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={handleClick}
+      className={`btn btn-like border-0 ${isLiked ? 'active' : ''}`}
+    >
+      <i className="icon-like mr-3" />
+      <span className="total-likes">
+        {count}
+      </span>
+    </button>
+  );
+};
