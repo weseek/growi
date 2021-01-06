69d5791baa18a3ad3d4f31ad4a2987fb0ca69fe7
itizawa <itizawa@weseek.co.jp>
2021-01-06 09:50:57 +0000

    create swr

--- src/stores/context.tsx
+++ src/stores/context.tsx
@@ -27,6 +27,14 @@ export const useNotFound = (initialData?: boolean): responseInterface<boolean, a
   return useStaticSWR('isNotFound', initialData);
 };
 
+export const useTrash = (initialData?: boolean): responseInterface<boolean, any> => {
+  return useStaticSWR('isTrash', initialData);
+};
+
+export const useShared = (initialData?: boolean): responseInterface<boolean, any> => {
+  return useStaticSWR('isShared', initialData);
+};
+
 export const useIsAbleToDeleteCompletely = (initialData?: boolean): responseInterface<boolean, any> => {
   return useStaticSWR('isAbleToDeleteCompletely', initialData);
 };
