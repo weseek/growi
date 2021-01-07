3bcbfc6ee7d4707fb709e6a3f6e63f44f1418b35
takeru0001 <ironman.jarvis0426@gmail.com>
2021-01-07 13:55:50 +0900

    fix

--- src/server/models/page.js
+++ src/server/models/page.js
@@ -1230,8 +1230,9 @@ module.exports = function(crowi) {
 
     pages.forEach((page) => {
       const newPagePath = page.path.replace(pathRegExp, newPagePathPrefix);
+      const updateAt = new Date().toISOString();
       if (updateMetadata) {
-        unorderedBulkOp.find({ _id: page._id }).update({ $set: { path: newPagePath, lastUpdateUser: user._id, updatedAt: Date.now() } });
+        unorderedBulkOp.find({ _id: page._id }).update([{ $set: { path: newPagePath, lastUpdateUser: user._id, updatedAt: { $toDate: updateAt } } }]);
       }
       else {
         unorderedBulkOp.find({ _id: page._id }).update({ $set: { path: newPagePath } });
