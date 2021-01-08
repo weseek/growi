efa2ad177d81b848121cde2fa29d822ba74cb8c6
itizawa <itizawa@weseek.co.jp>
2021-01-08 03:22:59 +0000

    FB

--- src/components/Organisms/GrowiSubnavButtons.tsx
+++ src/components/Organisms/GrowiSubnavButtons.tsx
@@ -1,32 +1,31 @@
 import { FC } from 'react';
-import { mutate } from 'swr';
 import { LikeButton } from '~/components/Atoms/LikeButton';
 import { BookmarkButton } from '~/components/Atoms/BookmarkButton';
-import { useCurrentPageSWR, useIsBookmarkInfoSWR, useLikeInfoSWR } from '~/stores/page';
+import { useCurrentPageSWR, useBookmarkInfoSWR, useLikeInfoSWR } from '~/stores/page';
 import { Page as IPage, BookmarkInfo as IBookmarkInfo, LikeInfo as ILikeInfo } from '~/interfaces/page';
-import { apiv3Put } from '~/lib/util/apiv3-client';
+import { apiv3Put } from '~/utils/apiv3-client';
 
 export const PageReactionButtons:FC = () => {
   const { data: page } = useCurrentPageSWR();
   const { id } = page as IPage;
 
-  const { data: likeInfo } = useLikeInfoSWR(id);
+  const { data: likeInfo, mutate: likeInfoMutate } = useLikeInfoSWR(id);
   const { sumOfLikers, isLiked } = likeInfo as ILikeInfo;
 
-  const { data: bookmarkInfo } = useIsBookmarkInfoSWR(id);
+  const { data: bookmarkInfo, mutate: bookmarkInfoMutate } = useBookmarkInfoSWR(id);
   const { sumOfBookmarks, isBookmarked } = bookmarkInfo as IBookmarkInfo;
 
 
   const handleClickLikeButton = async() => {
     const bool = !isLiked;
     await apiv3Put('/page/likes', { pageId: id, bool });
-    mutate('/page/like-info');
+    likeInfoMutate();
   };
 
   const handleClickBookmarkButton = async() => {
     const bool = !isBookmarked;
     await apiv3Put('/bookmarks', { pageId: id, bool });
-    mutate('/bookmarks/info');
+    bookmarkInfoMutate();
   };
 
   return (
