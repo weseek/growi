34f3fc236a932dbe8e4782a24d09e98a0a070b61
zamis <meda829ratteru86@gmail.com>
2021-01-13 19:48:06 +0000

    modify aws

--- src/server/service/file-uploader/aws.js
+++ src/server/service/file-uploader/aws.js
@@ -110,9 +110,11 @@ module.exports = function(crowi) {
 
   };
 
-  lib.deleteFiles = async function(attachment) {
-    const filePath = getFilePathOnStorage(attachment);
-    return lib.deleteFileByFilePath(filePath);
+  lib.deleteFiles = async function(attachments) {
+    attachments.map((attachment) => {
+      const filePath = getFilePathOnStorage(attachment);
+      return lib.deleteFileByFilePath(filePath);
+    });
   };
 
   lib.deleteFileByFilePath = async function(filePath) {
