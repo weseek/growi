22cb9864aa6928f7e0c3d967a30aea0e25cbc112
yusuketk <yusuketk@weseek.co.jp>
2021-01-14 16:40:22 +0900

    get basic info

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,18 +1,41 @@
-import { NextPage } from 'next';
+import {
+  NextPage, GetServerSideProps, GetServerSidePropsContext,
+} from 'next';
 import dynamic from 'next/dynamic';
 
 import BasicLayout from '../../components/BasicLayout';
 
 import { useTranslation } from '~/i18n';
+import { CrowiRequest } from '~/interfaces/crowi-request';
+import { CommonProps, getServerSideCommonProps } from '~/utils/nextjs-page-utils';
 import AdminHome from '~/client/js/components/Admin/AdminHome/AdminHome';
 
-type Props = {
+import {
+  useCurrentUser,
+  useAppTitle, useConfidential,
+  useSearchServiceConfigured, useSearchServiceReachable,
+} from '../../stores/context';
+
+type Props = CommonProps & {
+  currentUser: any,
+
+  appTitle: string,
+  confidential: string,
+  isSearchServiceConfigured: boolean,
+  isSearchServiceReachable: boolean,
 };
 
 const AdminHomePage: NextPage<Props> = (props: Props) => {
   const { t } = useTranslation();
   const title = t('Wiki Management Home Page');
   const AdminNavigation = dynamic(() => import('~/client/js/components/Admin/common/AdminNavigation'), { ssr: false });
+
+  useCurrentUser(props.currentUser != null ? JSON.parse(props.currentUser) : null);
+
+  useAppTitle(props.appTitle);
+  useConfidential(props.confidential);
+  useSearchServiceConfigured(props.isSearchServiceConfigured);
+  useSearchServiceReachable(props.isSearchServiceReachable);
 
   return (
     <>
@@ -35,6 +58,30 @@ const AdminHomePage: NextPage<Props> = (props: Props) => {
       </BasicLayout>
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async(context: GetServerSidePropsContext) => {
+  const req: CrowiRequest = context.req as CrowiRequest;
+  const { crowi } = req;
+  const {
+    appService, searchService,
+  } = crowi;
+
+  const { user } = req;
+
+  const result = await getServerSideCommonProps(context);
+  const props: Props = result.props as Props;
+  if (user != null) {
+    props.currentUser = JSON.stringify(user.toObject());
+  }
+
+  props.confidential = appService.getAppConfidential();
+  props.isSearchServiceConfigured = searchService.isConfigured;
+  props.isSearchServiceReachable = searchService.isReachable;
+
+  return {
+    props,
+  };
 };
 
 export default AdminHomePage;
