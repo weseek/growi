b4d985ed5d4bb1399f9b64faa397e8b96e36fc33
zamis <meda829ratteru86@gmail.com>
2021-01-18 12:13:31 +0000

    add await

--- src/server/service/attachment.js
+++ src/server/service/attachment.js
@@ -57,7 +57,7 @@ class AttachmentService {
     });
     await unorderAttachmentsBulkOp.execute();
 
-    fileUploadService.deleteFiles(attachments);
+    await fileUploadService.deleteFiles(attachments);
 
     return;
   }
