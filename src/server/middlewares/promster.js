fe00a66e9a007fc6a3f84135adf3e3398c5fbd63
Yuki Takei <yuki@weseek.co.jp>
2021-01-19 09:40:24 +0000

    BugFix

--- src/server/middlewares/promster.js
+++ src/server/middlewares/promster.js
@@ -1,8 +1,9 @@
 module.exports = (crowi, app) => {
   const { configManager } = crowi;
 
+  // when disabled
   if (!configManager.getConfig('crowi', 'promister:isEnabled')) {
-    return null;
+    return () => {};
   }
 
   const { createMiddleware } = require('@promster/express');
