102df43fdca56c44860a9daae0cda5fc4aacb399
kaori <kaori@weseek.co.jp>
2021-01-19 12:11:42 +0900

    create growi-bot file in routes directory

---
+++ src/server/routes/apiv3/growi-bot.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+module.exports = (crowi) => {
+  router.get('/', async(req, res) => {
+
+    return;
+  });
+
+  return router;
+};
