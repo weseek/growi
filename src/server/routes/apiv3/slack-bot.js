f05f65c560fcbe66fcc9372200894251ac8e5189
kaori <kaori@weseek.co.jp>
2021-01-19 17:32:17 +0900

    required bolt in slack-bot.js

--- src/server/routes/apiv3/slack-bot.js
+++ src/server/routes/apiv3/slack-bot.js
@@ -1,6 +1,12 @@
 const express = require('express');
+// const { App } = require('@slack/bolt');
 
 const router = express.Router();
+
+// const app = new App({
+//   token: process.env.SLACK_BOT_TOKEN,
+//   signingSecret: process.env.SLACK_SIGNING_SECRET,
+// });
 
 module.exports = (crowi) => {
   router.get('/', async(req, res) => {
