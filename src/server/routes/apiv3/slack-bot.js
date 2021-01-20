e38ef86c05b361baa04913ef9fb132b45a27d8dd
kaori <kaori@weseek.co.jp>
2021-01-20 17:31:55 +0900

    add example codes with expressReceiver

--- src/server/routes/apiv3/slack-bot.js
+++ src/server/routes/apiv3/slack-bot.js
@@ -1,21 +1,32 @@
 const express = require('express');
-// const { App } = require('@slack/bolt');
+const { App, ExpressReceiver } = require('@slack/bolt');
 
 const router = express.Router();
-
-// const app = new App({
-//   token: process.env.SLACK_BOT_TOKEN,
-//   signingSecret: process.env.SLACK_SIGNING_SECRET,
-// });
+const receiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_SECRET });
 
 module.exports = (crowi) => {
+
   router.get('/', async(req, res) => {
+
+    const app = new App({
+      token: process.env.SLACK_BOT_TOKEN,
+      receiver,
+    });
+
+    app.event('message', async({ event, client }) => {
+      await client.chat.postMessage('...');
+    });
+
+    receiver.router.post('/secret-page', (req, res) => {
+      res.send('yay!');
+    });
 
     // TODO: use res.apiv3
     return res.json({
       ok: true,
     });
   });
+
 
   return router;
 };
