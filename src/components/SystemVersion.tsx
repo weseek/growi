29b22d82511aac45669835d724be2834d0d3a8c9
yusuketk <yusuketk@weseek.co.jp>
2021-01-21 16:20:38 +0900

    create shortcuts modal and delete legacy method

---
+++ src/components/SystemVersion.tsx
@@ -0,0 +1,170 @@
+import React, { useState } from 'react';
+import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+
+import { useTranslation } from '~/i18n';
+
+import KeyboardReturnEnterIcon from '~/client/js/components/Icons/KeyboardReturnEnterIcon';
+
+type Props = {
+  growiVersion: string,
+};
+
+const ShortcutsModal = (props: Props): JSX.Element => {
+  const { t } = useTranslation();
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  // add classes to cmd-key by OS
+  const platform = window.navigator.platform.toLowerCase();
+  const isMac = (platform.indexOf('mac') > -1);
+  const additionalClassByOs = isMac ? 'mac' : 'key-longer win';
+
+  function toggleIsOpen() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <>
+      <Modal id="shortcuts-modal" size="lg" isOpen={isOpen} toggle={toggleIsOpen} className="grw-create-page">
+        <ModalHeader tag="h4" toggle={toggleIsOpen} className="bg-primary text-light">
+          {t('Shortcuts')}
+        </ModalHeader>
+        <ModalBody>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6">
+                <h3>
+                  <strong>{t('modal_shortcuts.global.title')}</strong>
+                </h3>
+
+                <table className="table">
+                  <tr>
+                    <th>
+                      {/* eslint-disable-next-line react/no-danger */}
+                      <span dangerouslySetInnerHTML={{ __html: t('modal_shortcuts.global.Open/Close shortcut help') }} />:
+                    </th>
+                    <td>
+                      <span className={`key cmd-key ${additionalClassByOs}`}></span> + <span className="key">/</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>{t('modal_shortcuts.global.Create Page')}:</th>
+                    <td>
+                      <span className="key">C</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>{t('modal_shortcuts.global.Edit Page')}:</th>
+                    <td>
+                      <span className="key">E</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>
+                      {/* eslint-disable-next-line react/no-danger */}
+                      <span dangerouslySetInnerHTML={{ __html: t('modal_shortcuts.global.Show Contributors') }} />:
+                    </th>
+                    <td>
+                      <a href="{ t('modal_shortcuts.global.konami_code_url') }" target="_blank">
+                        {t('modal_shortcuts.global.Konami Code')}
+                      </a>
+                      <br />
+                      <span className="key key-small">&uarr;</span>&nbsp;<span className="key key-small">&uarr;</span>
+                      <span className="key key-small">&darr;</span>&nbsp;<span className="key key-small">&darr;</span>
+                      <span className="key key-small">&larr;</span>
+                      <br />
+                      <span className="key key-small">&rarr;</span>
+                      <span className="key key-small">&larr;</span>&nbsp;<span className="key key-small">&rarr;</span>
+                      <span className="key key-small">B</span>&nbsp;<span className="key key-small">A</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>{t('modal_shortcuts.global.MirrorMode')}:</th>
+                    <td>
+                      <a href="{ t('modal_shortcuts.global.konami_code_url') }" target="_blank">
+                        {t('modal_shortcuts.global.Konami Code')}
+                      </a>
+                      <br />
+                      <span className="key key-small">X</span>&nbsp;<span className="key key-small">X</span>
+                      <span className="key key-small">B</span>&nbsp;<span className="key key-small">B</span>
+                      <span className="key key-small">A</span>
+                      <br />
+                      <span className="key key-small">Y</span>
+                      <span className="key key-small">A</span>&nbsp;<span className="key key-small">Y</span>
+                      <span className="key key-small">&darr;</span>&nbsp;<span className="key key-small">&larr;</span>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+
+              <div className="col-lg-6">
+                <h3>
+                  <strong>{t('modal_shortcuts.editor.title')}</strong>
+                </h3>
+                <table className="table">
+                  <tr>
+                    <th>{t('modal_shortcuts.editor.Indent')}:</th>
+                    <td>
+                      <span className="key key-longer">Tab</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>{t('modal_shortcuts.editor.Outdent')}:</th>
+                    <td className="text-nowrap">
+                      <span className="key key-long">Shift</span> + <span className="key key-longer">Tab</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>{t('modal_shortcuts.editor.Save Page')}:</th>
+                    <td>
+                      <span className={`key cmd-key ${additionalClassByOs}`}></span> + <span className="key">S</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>{t('modal_shortcuts.editor.Delete Line')}:</th>
+                    <td>
+                      <span className={`key cmd-key ${additionalClassByOs}`}></span> + <span className="key">D</span>
+                    </td>
+                  </tr>
+                </table>
+
+                <h3>
+                  <strong>{t('modal_shortcuts.commentform.title')}</strong>
+                </h3>
+
+                <table className="table">
+                  <tr>
+                    <th>{t('modal_shortcuts.commentform.Post')}:</th>
+                    <td>
+                      <span className={`key cmd-key ${additionalClassByOs}`}></span> +
+                      <span className="key key-longer">
+                        <KeyboardReturnEnterIcon />
+                      </span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>{t('modal_shortcuts.editor.Delete Line')}:</th>
+                    <td>
+                      <span className={`key cmd-key ${additionalClassByOs}`}></span> + <span className="key">D</span>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+          </div>
+        </ModalBody>
+      </Modal>
+      <div className="system-version d-none d-md-block d-edit-none d-print-none">
+        <span>
+          <a href="https://growi.org">GROWI</a> {props.growiVersion}
+        </span>
+        <button type="button" className="btn btn-link p-0" onClick={() => toggleIsOpen()}>
+          <i className="fa fa-keyboard-o"></i>&nbsp;<span className={`cmd-key ${additionalClassByOs}`}></span>-/
+        </button>
+      </div>
+
+    </>
+  );
+};
+
+export default ShortcutsModal;
