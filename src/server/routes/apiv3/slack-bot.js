2c1b9f576262dca2f206aefa6d1bbaf586158aec
kaori <kaori@weseek.co.jp>
2021-01-26 14:12:53 +0900

    add codes refer to 'Customizing a receiver' in docs

--- src/server/routes/apiv3/slack-bot.js
+++ src/server/routes/apiv3/slack-bot.js
@@ -1,4 +1,8 @@
+
+const { EventEmitter } = require('events');
+const { createServer } = require('http');
 const express = require('express');
+
 const { App, ExpressReceiver } = require('@slack/bolt');
 
 const router = express.Router();
@@ -7,19 +11,70 @@ const receiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_
 module.exports = (crowi) => {
 
   router.get('/', async(req, res) => {
+
+    class simpleReceiver extends EventEmitter {
+
+      constructor(signingSecret, endpoints) {
+        super();
+        this.app = express();
+        this.server = createServer(this.app);
+
+        for (const endpoint of endpoints) {
+          this.app.post(endpoint, this.requestHandler.bind(this));
+        }
+      }
+
+      init(app) {
+        this.bolt = app;
+      }
+
+      // start(port) {
+      //   return new Promise((resolve, reject) => {
+      //     try {
+      //       this.server.listen(port, () => {
+      //         resolve(this.server);
+      //       });
+      //     }
+      //     catch (error) {
+      //       reject(error);
+      //     }
+      //   });
+      // }
+
+      stop() {
+        return new Promise((resolve, reject) => {
+          this.server.close((error) => {
+            if (error) {
+              reject(error);
+              return;
+            }
+            resolve();
+          });
+        });
+      }
+
+    }
+
+    res.send('iii');
+
+    const app = new App({
+      token: process.env.SLACK_BOT_TOKEN,
+      receiver,
+    });
+
+    // app.post('/postMessage', async(req, res) => {
+    //   res.send('iii');
+    // });
+
+    // app.event('message', async({ event, client }) => {
+    //   // Do some slack-specific stuff here
+    //   await client.chat.postMessage({
+    //     channel: 'growi_bot',
+    //     text: 'hiiii',
+    //   });
+    // });
   });
 
-  const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    receiver,
-  });
-
-  console.log('bbb');
-
-  app.message('あ', async({ message, say }) => {
-    await say('aaaaaa');
-  });
-  console.log('ccc');
 
   return router;
 };
