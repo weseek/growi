3dcd2fca8318f382d1d854b56e3e7fdebb49cd94
kaori <kaori@weseek.co.jp>
2021-01-28 16:24:09 +0900

    remove 'parseBody' because it's already used in the main server script

--- src/server/service/bolt-reciever.js
+++ src/server/service/bolt-reciever.js
@@ -12,10 +12,9 @@ class BoltRecieverService extends EventEmitter {
   // This is a very simple implementation. Look at the ExpressReceiver source for more detail
   async requestHandler(req, res) {
     let ackCalled = false;
-    // Assume parseBody function exists to parse incoming requests
-    const parsedReq = parseBody(req);
+
     const event = {
-      body: parsedReq.body,
+      body: req.body,
       // Receivers are responsible for handling acknowledgements
       // `ack` should be prepared to be called multiple times and
       // possibly with `response` as an error
