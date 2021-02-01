473d88132128040378b87cf8dfdcc4379705317d
zahmis <meda829ratteru86@gmail.com>
2021-02-01 04:36:54 +0000

    add currentTagsMutate()

--- src/client/js/components/Page/TagLabels.tsx
+++ src/client/js/components/Page/TagLabels.tsx
@@ -1,9 +1,9 @@
 import React, { useCallback, useState } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 import { toastSuccess, toastError } from '../../util/apiNotification';
 
-import { withUnstatedContainers } from '../UnstatedUtils';
+// import { withUnstatedContainers } from '../UnstatedUtils';
 import AppContainer from '../../services/AppContainer';
 
 import RenderTagLabels from './RenderTagLabels';
@@ -22,8 +22,9 @@ const TagLabels = (props: Props): JSX.Element => {
   const [isTagEditModalShown, setIsTagEditModalShown] = useState(false);
 
   const { data: currentUser } = useCurrentUser();
-  const { data: tags, error } = useCurrentPageTagsSWR();
+  const { data: tags, error, mutate: currentPageTagsMutate } = useCurrentPageTagsSWR();
   const { data: currentPage } = useCurrentPageSWR();
+  // console.log(tags);
 
   const openEditorModal = useCallback(() => {
     setIsTagEditModalShown(true);
@@ -42,9 +43,7 @@ const TagLabels = (props: Props): JSX.Element => {
 
     try {
       await apiPost('/tags.update', { pageId, tags: newTags });
-
-      // update pageContainer.state
-      // pageContainer.setState({ tags });
+      currentPageTagsMutate();
 
       // TODO impl this after editorMode becomes available.
       // update editorContainer.state
@@ -54,7 +53,7 @@ const TagLabels = (props: Props): JSX.Element => {
     catch (err) {
       toastError(err, 'fail to update tags');
     }
-  }, []);
+  }, [currentPageTagsMutate, currentPage.id]);
 
   const isLoading = !error && !tags;
 
@@ -98,28 +97,28 @@ export default TagLabels;
 // TODO: remove old code (https://youtrack.weseek.co.jp/issue/GW-4961)
 class DeprecatedTagLabels extends React.Component {
 
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    // this.state = {
-    //   isTagEditModalShown: false,
-    // };
+  // this.state = {
+  //   isTagEditModalShown: false,
+  // };
 
-    // this.openEditorModal = this.openEditorModal.bind(this);
-    // this.closeEditorModal = this.closeEditorModal.bind(this);
-    this.tagsUpdatedHandler = this.tagsUpdatedHandler.bind(this);
-  }
+  // this.openEditorModal = this.openEditorModal.bind(this);
+  // this.closeEditorModal = this.closeEditorModal.bind(this);
+  // this.tagsUpdatedHandler = this.tagsUpdatedHandler.bind(this);
+  // }
 
   /**
    * @return tags data
    *   1. pageContainer.state.tags if editorMode is view
    *   2. editorContainer.state.tags if editorMode is edit
    */
-  getTagData() {
-    // const { editorContainer, editorMode } = this.props;
-    // return (editorMode === 'edit') ? editorContainer.state.tags : pageContainer.state.tags;
-    return [];
-  }
+  // getTagData() {
+  // const { editorContainer, editorMode } = this.props;
+  // return (editorMode === 'edit') ? editorContainer.state.tags : pageContainer.state.tags;
+  //   return [];
+  // }
 
   // openEditorModal() {
   //   this.setState({ isTagEditModalShown: true });
@@ -129,53 +128,53 @@ class DeprecatedTagLabels extends React.Component {
   //   this.setState({ isTagEditModalShown: false });
   // }
 
-  async tagsUpdatedHandler(newTags) {
-    const {
-      appContainer, editorMode,
-    } = this.props;
+  // async tagsUpdatedHandler(newTags) {
+  //   const {
+  //     appContainer, editorMode,
+  //   } = this.props;
 
-    // const { pageId } = pageContainer.state;
+  // const { pageId } = pageContainer.state;
 
-    // // It will not be reflected in the DB until the page is refreshed
-    // if (editorMode === 'edit') {
-    //   return editorContainer.setState({ tags: newTags });
-    // }
+  // // It will not be reflected in the DB until the page is refreshed
+  // if (editorMode === 'edit') {
+  //   return editorContainer.setState({ tags: newTags });
+  // }
 
-    // try {
-    //   const { tags } = await appContainer.apiPost('/tags.update', { pageId, tags: newTags });
+  // try {
+  //   const { tags } = await appContainer.apiPost('/tags.update', { pageId, tags: newTags });
 
-    //   // update pageContainer.state
-    //   pageContainer.setState({ tags });
-    //   // update editorContainer.state
-    //   editorContainer.setState({ tags });
+  //   // update pageContainer.state
+  //   pageContainer.setState({ tags });
+  //   // update editorContainer.state
+  //   editorContainer.setState({ tags });
 
-    //   toastSuccess('updated tags successfully');
-    // }
-    // catch (err) {
-    //   toastError(err, 'fail to update tags');
-    // }
-  }
+  //   toastSuccess('updated tags successfully');
+  // }
+  // catch (err) {
+  //   toastError(err, 'fail to update tags');
+  // }
+  // }
 
 
-  render() {
-    return null;
-  }
+  // render() {
+  //   return null;
+  // }
 
 }
 
 /**
  * Wrapper component for using unstated
  */
-const TagLabelsWrapper = withUnstatedContainers(DeprecatedTagLabels, [AppContainer]);
+// const TagLabelsWrapper = withUnstatedContainers(DeprecatedTagLabels, [AppContainer]);
 
-DeprecatedTagLabels.propTypes = {
-  t: PropTypes.func.isRequired, // i18next
+// DeprecatedTagLabels.propTypes = {
+// t: PropTypes.func.isRequired, // i18next
 
-  appContainer: PropTypes.instanceOf(AppContainer).isRequired,
-  // pageContainer: PropTypes.instanceOf(PageContainer).isRequired,
-  // editorContainer: PropTypes.instanceOf(EditorContainer).isRequired,
+// appContainer: PropTypes.instanceOf(AppContainer).isRequired,
+// pageContainer: PropTypes.instanceOf(PageContainer).isRequired,
+// editorContainer: PropTypes.instanceOf(EditorContainer).isRequired,
 
-  editorMode: PropTypes.string.isRequired,
-};
+// editorMode: PropTypes.string.isRequired,
+// };
 
 // export default withTranslation()(TagLabelsWrapper);
