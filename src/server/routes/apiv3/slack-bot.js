c77def56bbaf6d861fdc2cf5145519f9d653300b
yusuketk <yusuketk@weseek.co.jp>
2021-02-01 18:10:11 +0900

    clean code

--- src/server/routes/apiv3/slack-bot.js
+++ src/server/routes/apiv3/slack-bot.js
@@ -7,10 +7,8 @@ const router = express.Router();
 module.exports = (crowi) => {
   this.app = crowi.express;
   const { boltService } = crowi;
-  const endpoints = ['/'];
-  for (const endpoint of endpoints) {
-    router.post(endpoint, boltService.receiver.requestHandler.bind(this));
-  }
+
+  router.post('/', boltService.receiver.requestHandler.bind(this));
 
   return router;
 };
