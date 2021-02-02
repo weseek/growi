a6c591d1c6729a7c86fa504d1d30d3609bb376f4
zahmis <meda829ratteru86@gmail.com>
2021-02-02 16:36:43 +0000

    use AsyncTypeahead directly

--- src/client/js/components/Page/TagsInput.tsx
+++ src/client/js/components/Page/TagsInput.tsx
@@ -17,9 +17,9 @@ const TagsInput : FC<Props> = (props: Props) => {
   const [selected, setSelected] = useState(props.tags);
   const [defaultPageTags, setDefaultPageTags] = useState(props.tags);
 
-  // useEffect(() => {
-  //   this.typeahead.getInstance().focus();
-  // });
+  useEffect(() => {
+    AsyncTypeahead.typeahead.getInstance().focus();
+  });
 
   function handleChange(selected) {
     setSelected(selected);
@@ -35,24 +35,24 @@ const TagsInput : FC<Props> = (props: Props) => {
     setIsLoading(false);
   }
 
-  // function handleSelect(e) {
-  //   if (e.keyCode === 32) { // '32' means ASCII code of 'space'
-  //     e.preventDefault();
-  //     const instance = this.typeahead.getInstance();
-  //     const { initialItem } = instance.state;
+  function handleSelect(e) {
+    if (e.keyCode === 32) { // '32' means ASCII code of 'space'
+      e.preventDefault();
+      const instance = AsyncTypeahead.typeahead.getInstance();
+      const { initialItem } = instance.state;
 
-  //     if (initialItem) {
-  //       instance._handleMenuItemSelect(initialItem, e);
-  //     }
-  //   }
-  // }
+      if (initialItem) {
+        instance._handleMenuItemSelect(initialItem, e);
+      }
+    }
+  }
 
 
   return (
     <div className="tag-typeahead">
       <AsyncTypeahead
         id="tag-typeahead-asynctypeahead"
-        // ref={(typeahead) => { this.typeahead = typeahead }}
+        ref={(typeahead) => { AsyncTypeahead.typeahead = typeahead }}
         caseSensitive={false}
         defaultSelected={defaultPageTags}
         isLoading={isLoading}
@@ -61,7 +61,7 @@ const TagsInput : FC<Props> = (props: Props) => {
         newSelectionPrefix=""
         onChange={handleChange}
         onSearch={handleSearch}
-        // onKeyDown={handleSelect}
+        onKeyDown={handleSelect}
         options={resultTags} // Search result (Some tag names)
         placeholder="tag name"
         selectHintOnEnter
@@ -71,81 +71,5 @@ const TagsInput : FC<Props> = (props: Props) => {
   );
 
 };
-
-// class DeprecatedTagsInput extends React.Component {
-
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       resultTags: [],
-//       isLoading: false,
-//       selected: this.props.tags,
-//       defaultPageTags: this.props.tags,
-//     };
-
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handleSearch = this.handleSearch.bind(this);
-//     this.handleSelect = this.handleSelect.bind(this);
-//   }
-
-//   componentDidMount() {
-//     this.typeahead.getInstance().focus();
-//   }
-
-//   handleChange(selected) {
-//     // send tags to TagLabel Component when user add tag to form everytime
-//     this.setState({ selected }, () => {
-//       this.props.onTagsUpdated(this.state.selected);
-//     });
-//   }
-
-//   async handleSearch(query) {
-//     this.setState({ isLoading: true });
-//     const res = await apiGet('/tags.search', { q: query });
-//     res.tags.unshift(query); // selectable new tag whose name equals query
-//     this.setState({
-//       resultTags: Array.from(new Set(res.tags)), // use Set for de-duplication
-//       isLoading: false,
-//     });
-//   }
-
-//   handleSelect(e) {
-//     if (e.keyCode === 32) { // '32' means ASCII code of 'space'
-//       e.preventDefault();
-//       const instance = this.typeahead.getInstance();
-//       const { initialItem } = instance.state;
-
-//       if (initialItem) {
-//         instance._handleMenuItemSelect(initialItem, e);
-//       }
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <div className="tag-typeahead">
-//         <AsyncTypeahead
-//           id="tag-typeahead-asynctypeahead"
-//           ref={(typeahead) => { this.typeahead = typeahead }}
-//           caseSensitive={false}
-//           defaultSelected={this.state.defaultPageTags}
-//           isLoading={this.state.isLoading}
-//           minLength={1}
-//           multiple
-//           newSelectionPrefix=""
-//           onChange={this.handleChange}
-//           onSearch={this.handleSearch}
-//           onKeyDown={this.handleSelect}
-//           options={this.state.resultTags} // Search result (Some tag names)
-//           placeholder="tag name"
-//           selectHintOnEnter
-//           autoFocus={this.props.autoFocus}
-//         />
-//       </div>
-//     );
-//   }
-
-// }
 
 export default TagsInput;
