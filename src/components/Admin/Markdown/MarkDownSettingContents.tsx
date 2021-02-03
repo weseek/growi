a4a7f1d21880cf29cfb940d38d034088107bd301
yusuketk <yusuketk@weseek.co.jp>
2021-02-03 13:40:30 +0900

    not use props

--- src/components/Admin/Markdown/MarkDownSettingContents.tsx
+++ src/components/Admin/Markdown/MarkDownSettingContents.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Card, CardBody } from 'reactstrap';
 
 import { useTranslation } from '~/i18n';
-import { useMarkdownSettingsSWR } from '~/stores/admin';
 import LineBreakForm from '~/client/js/components/Admin/MarkdownSetting/LineBreakForm';
 import PresentationForm from '~/client/js/components/Admin/MarkdownSetting/PresentationForm';
 import XssForm from '~/client/js/components/Admin/MarkdownSetting/XssForm';
@@ -10,19 +9,6 @@ import XssForm from '~/client/js/components/Admin/MarkdownSetting/XssForm';
 
 const MarkDownSettingContents = (): JSX.Element => {
   const { t } = useTranslation();
-  const { data: markdownSettingParams, error, isValidating } = useMarkdownSettingsSWR();
-
-  if (error) {
-    return <></>;
-  }
-
-  if (isValidating) {
-    return (
-      <div className="my-5 text-center">
-        <i className="fa fa-lg fa-spinner fa-pulse mx-auto text-muted"></i>
-      </div>
-    );
-  }
   return (
     <React.Fragment>
       {/* Line Break Setting */}
@@ -30,32 +16,21 @@ const MarkDownSettingContents = (): JSX.Element => {
       <Card className="card well my-3">
         <CardBody className="px-0 py-2">{ t('admin:markdown_setting.lineBreak_desc') }</CardBody>
       </Card>
-      <LineBreakForm
-        isEnabledLinebreaks={markdownSettingParams?.isEnabledLinebreaks}
-        isEnabledLinebreaksInComments={markdownSettingParams?.isEnabledLinebreaksInComments}
-      />
+      <LineBreakForm />
 
       {/* Presentation Setting */}
       <h2 className="admin-setting-header">{ t('admin:markdown_setting.presentation_header') }</h2>
       <Card className="card well my-3">
         <CardBody className="px-0 py-2">{ t('admin:markdown_setting.presentation_desc') }</CardBody>
       </Card>
-      <PresentationForm
-        pageBreakSeparator={markdownSettingParams?.pageBreakSeparator}
-        pageBreakCustomSeparator={markdownSettingParams?.pageBreakCustomSeparator}
-      />
+      <PresentationForm />
 
       {/* XSS Setting */}
       <h2 className="admin-setting-header">{ t('admin:markdown_setting.xss_header') }</h2>
       <Card className="card well my-3">
         <CardBody className="px-0 py-2">{ t('admin:markdown_setting.xss_desc') }</CardBody>
       </Card>
-      <XssForm
-        isEnabledXss={markdownSettingParams?.isEnabledXss}
-        xssOption={markdownSettingParams?.xssOption}
-        tagWhiteList={markdownSettingParams?.tagWhiteList}
-        attrWhiteList={markdownSettingParams?.attrWhiteList}
-      />
+      <XssForm />
     </React.Fragment>
   );
 };
