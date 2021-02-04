11a9e3a2469a85e32016ff2f89a9be742bfac359
yusuketk <yusuketk@weseek.co.jp>
2021-02-04 10:49:44 +0900

    define type

--- src/stores/admin.tsx
+++ src/stores/admin.tsx
@@ -1,8 +1,11 @@
 import useSWR, { responseInterface } from 'swr';
 import { apiv3Get } from '~/client/js/util/apiv3-client';
-import { customizeParams as ICustomizeParams } from '~/interfaces/admin';
+import {
+  markdownParams as IMarkdownParams,
+  customizeParams as ICustomizeParams,
+} from '~/interfaces/admin';
 
-export const useMarkdownSettingsSWR = (): responseInterface<any, Error> => {
+export const useMarkdownSettingsSWR = (): responseInterface<IMarkdownParams, Error> => {
   return useSWR(
     '/markdown-setting',
     (endpoint, path) => apiv3Get(endpoint, { path }).then(result => result.data.markdownParams),
