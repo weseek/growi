34d3aad74e66a905066a5ab8f76b0551a5aaf5b5
yusuketk <yusuketk@weseek.co.jp>
2021-02-04 10:45:03 +0900

    useswr

--- src/client/js/components/Admin/App/AppSetting.tsx
+++ src/client/js/components/Admin/App/AppSetting.tsx
@@ -1,91 +1,102 @@
-import React from 'react';
-import { WithTranslation } from 'next-i18next';
+import React, { useEffect } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { useTranslation } from '~/i18n';
 
-import { config as nextI18NextConfig, withTranslation } from '~/i18n';
+
 import loggerFactory from '~/utils/logger';
+import { useAppSettingsSWR } from '~/stores/admin';
 
-import { withUnstatedContainers } from '../../UnstatedUtils';
 import { toastSuccess, toastError } from '../../../util/apiNotification';
-
-import AdminAppContainer from '../../../services/AdminAppContainer';
-// import { AdminUpdateButtonRow } from '~/components/Admin/Common/AdminUpdateButtonRow';
+import { apiv3Put } from '~/utils/apiv3-client';
 
 const logger = loggerFactory('growi:appSettings');
 
-interface Props extends WithTranslation {
-  adminAppContainer: AdminAppContainer,
-}
+const AppSetting = ():JSX.Element => {
+  const { t } = useTranslation();
+  const { data, mutate } = useAppSettingsSWR();
 
-class AppSetting extends React.Component<Props> {
+  const appSettingMethods = useForm({
+    defaultValues: {
+      title: data?.title,
+      confidential: data?.confidential,
+      globalLang: data?.globalLang,
+      fileUpload: data?.fileUpload,
+      siteUrl: data?.envSiteUrl,
+      envSiteUrl: data?.envSiteUrl,
+      isMailerSetup: data?.isMailerSetup,
+      fromAddress: data?.fromAddress,
+    },
+  });
 
-  constructor(props) {
-    super(props);
-
-    this.submitHandler = this.submitHandler.bind(this);
-  }
-
-  async submitHandler() {
-    const { t, adminAppContainer } = this.props;
-
+  const submitHandler: SubmitHandler<FormValues> = async(formValues) => {
     try {
-      await adminAppContainer.updateAppSettingHandler();
+      await apiv3Put('/app-setting/app', formValues);
+      mutate();
       toastSuccess(t('toaster.update_successed', { target: t('App Settings') }));
     }
     catch (err) {
       toastError(err);
       logger.error(err);
     }
-  }
+  };
 
-  render() {
-    const { t, adminAppContainer } = this.props;
+  useEffect(() => {
+    appSettingMethods.setValue('title', data?.title);
+    appSettingMethods.setValue('confidential', data?.confidential);
+    appSettingMethods.setValue('globalLang', data?.globalLang);
+    appSettingMethods.setValue('fileUpload', data?.fileUpload);
+    appSettingMethods.setValue('siteUrl', data?.siteUrl);
+    appSettingMethods.setValue('isMailerSetup', data?.isMailerSetup);
+    appSettingMethods.setValue('fromAddress', data?.fromAddress);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data?.themeType]);
 
-    return (
-      <React.Fragment>
-        <div className="form-group row">
-          <label className="text-left text-md-right col-md-3 col-form-label">{t('admin:app_setting.site_name')}</label>
-          <div className="col-md-6">
-            <input
-              className="form-control"
-              type="text"
-              defaultValue={adminAppContainer.state.title || ''}
-              onChange={(e) => {
+  return (
+    <form role="form" onSubmit={appSettingMethods.handleSubmit(submitHandler)}>
+      <div className="form-group row">
+        <label className="text-left text-md-right col-md-3 col-form-label">{t('admin:app_setting.site_name')}</label>
+        <div className="col-md-6">
+          <input
+            className="form-control"
+            type="text"
+            defaultValue={adminAppContainer.state.title || ''}
+            onChange={(e) => {
                 adminAppContainer.changeTitle(e.target.value);
               }}
-              placeholder="GROWI"
-            />
-            <p className="form-text text-muted">{t('admin:app_setting.sitename_change')}</p>
-          </div>
+            placeholder="GROWI"
+          />
+          <p className="form-text text-muted">{t('admin:app_setting.sitename_change')}</p>
         </div>
+      </div>
 
-        <div className="row form-group mb-5">
-          <label
-            className="text-left text-md-right col-md-3 col-form-label"
-          >
-            {t('admin:app_setting.confidential_name')}
-          </label>
-          <div className="col-md-6">
-            <input
-              className="form-control"
-              type="text"
-              defaultValue={adminAppContainer.state.confidential || ''}
-              onChange={(e) => {
+      <div className="row form-group mb-5">
+        <label
+          className="text-left text-md-right col-md-3 col-form-label"
+        >
+          {t('admin:app_setting.confidential_name')}
+        </label>
+        <div className="col-md-6">
+          <input
+            className="form-control"
+            type="text"
+            defaultValue={adminAppContainer.state.confidential || ''}
+            onChange={(e) => {
                 adminAppContainer.changeConfidential(e.target.value);
               }}
-              placeholder={t('admin:app_setting.confidential_example')}
-            />
-            <p className="form-text text-muted">{t('admin:app_setting.header_content')}</p>
-          </div>
+            placeholder={t('admin:app_setting.confidential_example')}
+          />
+          <p className="form-text text-muted">{t('admin:app_setting.header_content')}</p>
         </div>
+      </div>
 
-        <div className="row form-group mb-5">
-          <label
-            className="text-left text-md-right col-md-3 col-form-label"
-          >
-            {t('admin:app_setting.default_language')}
-          </label>
-          <div className="col-md-6 py-2">
-            {
+      <div className="row form-group mb-5">
+        <label
+          className="text-left text-md-right col-md-3 col-form-label"
+        >
+          {t('admin:app_setting.default_language')}
+        </label>
+        <div className="col-md-6 py-2">
+          {
               nextI18NextConfig.allLanguages.map(lang => (
                 <div key={lang} className="custom-control custom-radio custom-control-inline">
                   <input
@@ -103,49 +114,46 @@ class AppSetting extends React.Component<Props> {
                 </div>
               ))
             }
-          </div>
         </div>
+      </div>
 
-        <div className="row form-group mb-5">
-          <label
-            className="text-left text-md-right col-md-3 col-form-label"
-          >
-            {t('admin:app_setting.file_uploading')}
-          </label>
-          <div className="col-md-6">
-            <div className="custom-control custom-checkbox custom-checkbox-info">
-              <input
-                type="checkbox"
-                id="cbFileUpload"
-                className="custom-control-input"
-                name="fileUpload"
-                checked={adminAppContainer.state.fileUpload != null}
-                onChange={(e) => {
+      <div className="row form-group mb-5">
+        <label
+          className="text-left text-md-right col-md-3 col-form-label"
+        >
+          {t('admin:app_setting.file_uploading')}
+        </label>
+        <div className="col-md-6">
+          <div className="custom-control custom-checkbox custom-checkbox-info">
+            <input
+              type="checkbox"
+              id="cbFileUpload"
+              className="custom-control-input"
+              name="fileUpload"
+              checked={adminAppContainer.state.fileUpload != null}
+              onChange={(e) => {
                   adminAppContainer.changeFileUpload(e.target.checked);
                 }}
-              />
-              <label
-                className="custom-control-label"
-                htmlFor="cbFileUpload"
-              >
-                {t('admin:app_setting.enable_files_except_image')}
-              </label>
-            </div>
-
-            <p className="form-text text-muted">
-              {t('admin:app_setting.attach_enable')}
-            </p>
+            />
+            <label
+              className="custom-control-label"
+              htmlFor="cbFileUpload"
+            >
+              {t('admin:app_setting.enable_files_except_image')}
+            </label>
           </div>
+
+          <p className="form-text text-muted">
+            {t('admin:app_setting.attach_enable')}
+          </p>
         </div>
+      </div>
+      <div className="d-flex justify-content-center">
+        <input type="submit" value={t('Update')} className="btn btn-primary" />
+      </div>
+    </form>
+  );
 
-        {/* <AdminUpdateButtonRow onClick={this.submitHandler} disabled={adminAppContainer.state.retrieveError != null} /> */}
-      </React.Fragment>
-    );
-  }
+};
 
-}
-
-/**
- * Wrapper component for using unstated
- */
-export default withUnstatedContainers(withTranslation()(AppSetting), [AdminAppContainer]);
+export default AppSetting;
--- src/stores/admin.tsx
+++ src/stores/admin.tsx
@@ -2,6 +2,14 @@ import useSWR, { responseInterface } from 'swr';
 import { apiv3Get } from '~/client/js/util/apiv3-client';
 import { customizeParams as ICustomizeParams } from '~/interfaces/admin';
 
+export const useAppSettingsSWR = (): responseInterface<ICustomizeParams, Error> => {
+  return useSWR(
+    '/app-setting',
+    (endpoint, path) => apiv3Get(endpoint, { path }).then(result => result.data.appParams),
+    { revalidateOnFocus: false },
+  );
+};
+
 export const useCustomizeSettingsSWR = (): responseInterface<ICustomizeParams, Error> => {
   return useSWR(
     '/customize-setting',
