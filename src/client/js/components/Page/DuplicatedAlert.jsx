c44104e0a7b8d4d305847271664dc9f098332123
zahmis <meda829ratteru86@gmail.com>
2021-02-09 08:05:11 +0000

    fix duplicatedAlert

--- src/client/js/components/Page/DuplicatedAlert.jsx
+++ src/client/js/components/Page/DuplicatedAlert.jsx
@@ -6,10 +6,13 @@ import { withTranslation } from 'react-i18next';
 const DuplicatedAlert = (props) => {
   const { t } = props;
 
+  const urlParams = new URLSearchParams(window.location.search);
+  const fromPath = urlParams.get('duplicated');
+
   return (
     <div className="alert alert-success py-3 px-4">
       <strong>
-        { t('Duplicated') }:{t('page_page.notice.duplicated')}
+        { t('Duplicated') }:{t('page_page.notice.duplicated', { fromPath })}
       </strong>
     </div>
   );
