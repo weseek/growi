cbfcb26ba0f4d06b4deb983780e5b2952179456b
itizawa <itizawa@weseek.co.jp>
2021-02-12 08:05:16 +0000

    display spinner

--- src/components/PageAccessory/PageHistory.tsx
+++ src/components/PageAccessory/PageHistory.tsx
@@ -1,25 +1,18 @@
 import React, {
   useCallback, useState, FC, useEffect,
 } from 'react';
-import loggerFactory from '@alias/logger';
 
-// import { toastError } from '../util/apiNotification';
-
-// import { withLoadingSppiner } from './SuspenseUtils';
 // import PageRevisionList from './PageHistory/PageRevisionList';
-
 import { PaginationWrapper } from '~/components/PaginationWrapper';
 
 import { useCurrentPageHistorySWR } from '~/stores/page';
-
-const logger = loggerFactory('growi:PageHistory');
 
 export const PageHistory:FC = () => {
   const [activePage, setActivePage] = useState(1);
   const [totalItemsCount, setTotalItemsCount] = useState(100);
   const [limit, setLimit] = useState(10);
 
-  const { data: paginationResult } = useCurrentPageHistorySWR(activePage, limit);
+  const { data: paginationResult, error } = useCurrentPageHistorySWR(activePage, limit);
 
   const handlePage = useCallback(async(selectedPage) => {
     setActivePage(selectedPage);
@@ -34,8 +27,31 @@ export const PageHistory:FC = () => {
     setLimit(paginationResult.limit);
   }, [paginationResult]);
 
+  if (paginationResult == null) {
+    return (
+      <div className="my-5 text-center">
+        <i className="fa fa-lg fa-spinner fa-pulse mx-auto text-muted" />
+      </div>
+    );
+  }
+
+  if (error != null) {
+    return (
+      <div className="my-5">
+        <div className="text-danger">{error}</div>
+      </div>
+    );
+  }
+
   return (
     <>
+      {/* <PageRevisionList
+        pageHistoryContainer={pageHistoryContainer}
+        revisions={revisions}
+        diffOpened={diffOpened}
+        getPreviousRevision={getPreviousRevision}
+        onDiffOpenClicked={onDiffOpenClicked}
+      /> */}
       <PaginationWrapper
         activePage={activePage}
         changePage={handlePage}
@@ -46,71 +62,3 @@ export const PageHistory:FC = () => {
     </>
   );
 };
-
-// function DeprecatePageHistory(props) {
-//   const { pageHistoryContainer } = props;
-//   const { getPreviousRevision, onDiffOpenClicked } = pageHistoryContainer;
-//   const {
-//     activePage, totalPages, pagingLimit, revisions, diffOpened,
-//   } = pageHistoryContainer.state;
-
-//   const handlePage = useCallback(async(selectedPage) => {
-//     try {
-//       await props.pageHistoryContainer.retrieveRevisions(selectedPage);
-//     }
-//     catch (err) {
-//       toastError(err);
-//       props.pageHistoryContainer.setState({ errorMessage: err.message });
-//       logger.error(err);
-//     }
-//   }, [props.pageHistoryContainer]);
-
-//   if (pageHistoryContainer.state.errorMessage != null) {
-//     return (
-//       <div className="my-5">
-//         <div className="text-danger">{pageHistoryContainer.state.errorMessage}</div>
-//       </div>
-//     );
-//   }
-
-//   if (pageHistoryContainer.state.revisions === pageHistoryContainer.dummyRevisions) {
-//     throw new Promise(async() => {
-//       try {
-//         await props.pageHistoryContainer.retrieveRevisions(1);
-//       }
-//       catch (err) {
-//         toastError(err);
-//         pageHistoryContainer.setState({ errorMessage: err.message });
-//         logger.error(err);
-//       }
-//     });
-//   }
-
-//   return (
-//     <div>
-//       <PageRevisionList
-//         pageHistoryContainer={pageHistoryContainer}
-//         revisions={revisions}
-//         diffOpened={diffOpened}
-//         getPreviousRevision={getPreviousRevision}
-//         onDiffOpenClicked={onDiffOpenClicked}
-//       />
-//       <PaginationWrapper
-//         activePage={activePage}
-//         changePage={handlePage}
-//         totalItemsCount={totalPages}
-//         pagingLimit={pagingLimit}
-//         align="center"
-//       />
-//     </div>
-//   );
-
-// }
-
-// const RenderPageHistoryWrapper = withUnstatedContainers(withLoadingSppiner(PageHistory), [PageHistroyContainer]);
-
-// PageHistory.propTypes = {
-//   pageHistoryContainer: PropTypes.instanceOf(PageHistroyContainer).isRequired,
-// };
-
-// export default RenderPageHistoryWrapper;
