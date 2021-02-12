75d8cc8bc7d2c97a20725ec9400933aa61887d81
itizawa <itizawa@weseek.co.jp>
2021-02-12 03:45:20 +0000

    jsx to tsx

---
+++ src/components/PageAccessory/PageHistory.tsx
@@ -0,0 +1,87 @@
+import React, { useCallback } from 'react';
+import PropTypes from 'prop-types';
+import loggerFactory from '@alias/logger';
+
+import { withUnstatedContainers } from './UnstatedUtils';
+import { toastError } from '../util/apiNotification';
+
+import { withLoadingSppiner } from './SuspenseUtils';
+import PageRevisionList from './PageHistory/PageRevisionList';
+
+import PageHistroyContainer from '../services/PageHistoryContainer';
+import PaginationWrapper from './PaginationWrapper';
+
+
+const logger = loggerFactory('growi:PageHistory');
+
+export const PageHistory = () => {
+  return <p>hoge</p>;
+};
+
+// function DeprecatePageHistory(props) {
+//   const { pageHistoryContainer } = props;
+//   const { getPreviousRevision, onDiffOpenClicked } = pageHistoryContainer;
+//   const {
+//     activePage, totalPages, pagingLimit, revisions, diffOpened,
+//   } = pageHistoryContainer.state;
+
+//   const handlePage = useCallback(async(selectedPage) => {
+//     try {
+//       await props.pageHistoryContainer.retrieveRevisions(selectedPage);
+//     }
+//     catch (err) {
+//       toastError(err);
+//       props.pageHistoryContainer.setState({ errorMessage: err.message });
+//       logger.error(err);
+//     }
+//   }, [props.pageHistoryContainer]);
+
+//   if (pageHistoryContainer.state.errorMessage != null) {
+//     return (
+//       <div className="my-5">
+//         <div className="text-danger">{pageHistoryContainer.state.errorMessage}</div>
+//       </div>
+//     );
+//   }
+
+//   if (pageHistoryContainer.state.revisions === pageHistoryContainer.dummyRevisions) {
+//     throw new Promise(async() => {
+//       try {
+//         await props.pageHistoryContainer.retrieveRevisions(1);
+//       }
+//       catch (err) {
+//         toastError(err);
+//         pageHistoryContainer.setState({ errorMessage: err.message });
+//         logger.error(err);
+//       }
+//     });
+//   }
+
+//   return (
+//     <div>
+//       <PageRevisionList
+//         pageHistoryContainer={pageHistoryContainer}
+//         revisions={revisions}
+//         diffOpened={diffOpened}
+//         getPreviousRevision={getPreviousRevision}
+//         onDiffOpenClicked={onDiffOpenClicked}
+//       />
+//       <PaginationWrapper
+//         activePage={activePage}
+//         changePage={handlePage}
+//         totalItemsCount={totalPages}
+//         pagingLimit={pagingLimit}
+//         align="center"
+//       />
+//     </div>
+//   );
+
+// }
+
+// const RenderPageHistoryWrapper = withUnstatedContainers(withLoadingSppiner(PageHistory), [PageHistroyContainer]);
+
+// PageHistory.propTypes = {
+//   pageHistoryContainer: PropTypes.instanceOf(PageHistroyContainer).isRequired,
+// };
+
+// export default RenderPageHistoryWrapper;
