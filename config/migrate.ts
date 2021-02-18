306783d7facfa3582452a9cd7012d24f9e3cf5ca
itizawa <itizawa@weseek.co.jp>
2021-02-18 02:17:21 +0000

    use mongoose-util value

--- config/migrate.ts
+++ config/migrate.ts
@@ -1,17 +1,21 @@
+import { getMongoUri } from '../src/server/util/mongoose-utils';
 /**
  * Configuration file for migrate-mongo
  * @see https://github.com/seppevs/migrate-mongo
  *
  * @author Yuki Takei <yuki@weseek.co.jp>
  */
-// todo get url from mongoose-util
-export const mongoUri = 'mongodb://mongo/growi';
+export const mongoUri = getMongoUri();
+
+// parse url
+const url = new URL(mongoUri);
+
 export const migrationFileExtension = '.ts';
 export const changelogCollectionName = 'migrations';
 export const migrationsDir = 'src/migrations/';
 export const mongodb = {
-  url: 'mongodb://mongo/growi',
-  databaseName: 'growi', // omit heading slash
+  url: mongoUri,
+  databaseName: url.pathname.substring(1), // omit heading slash
   options: {
     useNewUrlParser: true, // removes a deprecation warning when connecting
     useUnifiedTopology: true,
