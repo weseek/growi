03c09dec6e272a067d589d7d708632a12675eae0
kaori <kaori@weseek.co.jp>
2021-02-24 13:24:57 +0900

    removed unused loggar module

--- src/server/routes/apiv3/comment.js
+++ src/server/routes/apiv3/comment.js
@@ -1,7 +1,3 @@
-import loggerFactory from '~/utils/logger';
-
-const logger = loggerFactory('growi:routes:apiv3:comment'); // eslint-disable-line no-unused-vars
-
 const express = require('express');
 
 const router = express.Router();
