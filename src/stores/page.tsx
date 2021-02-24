3cf7ee57952a699a683c616614ff88e9aad99f78
kaori <kaori@weseek.co.jp>
2021-02-24 15:02:43 +0900

    change apiGet to apiv3Get

--- src/stores/page.tsx
+++ src/stores/page.tsx
@@ -67,7 +67,7 @@ export const useCurrentPageCommentsSWR = (): responseInterface<Comment[], Error>
   return useSWR(
     ['/comments.get', currentPage],
     // TODO GW-5149 implement apiv3
-    (endpoint, page) => apiGet(endpoint, { page_id: page.id }).then(response => response.comments),
+    (endpoint, page) => apiv3Get(endpoint, { page_id: page.id }).then(response => response.comments),
     {
       revalidateOnFocus: false,
       revalidateOnReconnect: false,
