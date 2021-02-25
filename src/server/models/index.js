c45efe30aae7c2f4aab5ddc4364f9587cc2a710e
itizawa <itizawa@weseek.co.jp>
2021-02-25 08:15:47 +0000

    disable

--- src/server/models/index.js
+++ src/server/models/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Config: require('./config'),
-  Page: require('./page'),
+  // Page: require('./page'),
   // TODO GW-2746 bulk export pages
   // PageArchive: require('./page-archive'),
   PageTagRelation: require('./page-tag-relation'),
