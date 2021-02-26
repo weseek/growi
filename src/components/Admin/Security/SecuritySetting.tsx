92f5b64fa5b368ab1c7ee03093a488a1e67e349b
zahmis <meda829ratteru86@gmail.com>
2021-02-26 07:51:56 +0000

    temp: add variable

--- src/components/Admin/Security/SecuritySetting.tsx
+++ src/components/Admin/Security/SecuritySetting.tsx
@@ -1,132 +1,156 @@
 /* eslint-disable react/no-danger */
-import React, { FC, useCallback } from 'react';
+import React, { FC } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { useTranslation } from '~/i18n';
 import { toastSuccess, toastError } from '~/client/js/util/apiNotification';
+import { apiv3Put } from '~/utils/apiv3-client';
 
-type Props ={
 
+// const retrieveError = null
+const currentRestrictGuestMode = 'security:restrictGuestMode';
+const currentPageCompleteDeletionAuthority = 'security:pageCompleteDeletionAuthority';
+const isShowRestrictedByOwner = 'security:list-policy:hideRestrictedByOwner';
+const isShowRestrictedByGroup = 'security:list-policy:hideRestrictedByGroup';
+
+type FormValues ={
+[currentRestrictGuestMode]: string,
+[currentPageCompleteDeletionAuthority]: string,
+[isShowRestrictedByOwner]: boolean,
+[isShowRestrictedByGroup]:boolean,
+// [wikiMode]: string,
 };
 
-export const SecuritySetting: FC<Props> = () => {
+export const SecuritySetting: FC<FormValues> = () => {
   const { t } = useTranslation();
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      [currentRestrictGuestMode]: 'Deny',
+      [currentPageCompleteDeletionAuthority]: 'adminOnly',
+      [isShowRestrictedByOwner]: false,
+      [isShowRestrictedByGroup]: false,
+      // [wikiMode]: 'bar',
+    },
+  });
 
-  const putSecuritySetting = useCallback(async() => {
-    // const { t, adminGeneralSecurityContainer } = this.props;
+  const submitHandler: SubmitHandler<FormValues> = async(formValues) => {
     try {
-      // await adminGeneralSecurityContainer.updateGeneralSecuritySetting();
+      await apiv3Put('/security-setting/general-setting', {
+        [currentRestrictGuestMode]: formValues[currentRestrictGuestMode],
+        [currentPageCompleteDeletionAuthority]: formValues[currentPageCompleteDeletionAuthority],
+        [isShowRestrictedByOwner]: formValues[isShowRestrictedByOwner],
+        [isShowRestrictedByGroup]: formValues[isShowRestrictedByGroup],
+
+      });
       toastSuccess(t('security_setting.updated_general_security_setting'));
     }
     catch (err) {
       toastError(err);
     }
-  }, [t]);
+  };
 
   return (
-    <React.Fragment>
-      <h2 className="alert-anchor border-bottom">
-        {t('security_settings')}
-      </h2>
-      {/* {adminGeneralSecurityContainer.retrieveError != null && (
+    <div className="row">
+      <form role="form" className="col-md-12" onSubmit={handleSubmit(submitHandler)}>
+        <h2 className="alert-anchor border-bottom">
+          {t('security_settings')}
+        </h2>
+        {/* {adminGeneralSecurityContainer.retrieveError != null && (
       <div className="alert alert-danger">
         <p>{t('Error occurred')} : {adminGeneralSecurityContainer.retrieveError}</p>
       </div>
         )} */}
 
-      <h4 className="mt-4">
-        { t('page_list_and_search_results') }
-      </h4>
-      <table className="table table-bordered col-lg-9 mb-5">
-        <thead>
-          <tr>
-            <th scope="col">{ t('scope_of_page_disclosure') }</th>
-            <th scope="col">{ t('set_point') }</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">{ t('Public') }</th>
-            <td>{ t('always_displayed') }</td>
-          </tr>
-          <tr>
-            <th scope="row">{ t('Anyone with the link') }</th>
-            <td>{ t('always_hidden') }</td>
-          </tr>
-          <tr>
-            <th scope="row">{ t('Only me') }</th>
-            <td>
-              <div className="custom-control custom-switch custom-checkbox-success">
-                <input
-                  type="checkbox"
-                  className="custom-control-input"
-                  id="isShowRestrictedByOwner"
-                  // checked={adminGeneralSecurityContainer.state.isShowRestrictedByOwner}
-                  // onChange={() => { adminGeneralSecurityContainer.switchIsShowRestrictedByOwner() }}
-                />
-                <label className="custom-control-label" htmlFor="isShowRestrictedByOwner">
-                  {t('displayed_or_hidden')}
-                </label>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">{ t('Only inside the group') }</th>
-            <td>
-              <div className="custom-control custom-switch custom-checkbox-success">
-                <input
-                  type="checkbox"
-                  className="custom-control-input"
-                  id="isShowRestrictedByGroup"
-                  // checked={adminGeneralSecurityContainer.state.isShowRestrictedByGroup}
-                  // onChange={() => { adminGeneralSecurityContainer.switchIsShowRestrictedByGroup() }}
-                />
-                <label className="custom-control-label" htmlFor="isShowRestrictedByGroup">
-                  {t('displayed_or_hidden')}
-                </label>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <h4>{t('page_access_and_delete_rights')}</h4>
-      <div className="row mb-4">
-        <div className="col-md-3 text-md-right py-2">
-          <strong>{t('security_setting.Guest Users Access')}</strong>
-        </div>
-        <div className="col-md-9">
-          <div className="dropdown">
-            {/* TODO: show dropdown text byGW-5142 */}
-            <button
+        <h4 className="mt-4">
+          { t('page_list_and_search_results') }
+        </h4>
+        <table className="table table-bordered col-lg-9 mb-5">
+          <thead>
+            <tr>
+              <th scope="col">{ t('scope_of_page_disclosure') }</th>
+              <th scope="col">{ t('set_point') }</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">{ t('Public') }</th>
+              <td>{ t('always_displayed') }</td>
+            </tr>
+            <tr>
+              <th scope="row">{ t('Anyone with the link') }</th>
+              <td>{ t('always_hidden') }</td>
+            </tr>
+            <tr>
+              <th scope="row">{ t('Only me') }</th>
+              <td>
+                <div className="custom-control custom-switch custom-checkbox-success">
+                  <input
+                    type="checkbox"
+                    className="custom-control-input"
+                    id="isShowRestrictedByOwner"
+                    ref={register}
+                  />
+                  <label className="custom-control-label" htmlFor="isShowRestrictedByOwner">
+                    {t('displayed_or_hidden')}
+                  </label>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">{ t('Only inside the group') }</th>
+              <td>
+                <div className="custom-control custom-switch custom-checkbox-success">
+                  <input
+                    type="checkbox"
+                    className="custom-control-input"
+                    id="isShowRestrictedByGroup"
+                    ref={register}
+                  />
+                  <label className="custom-control-label" htmlFor="isShowRestrictedByGroup">
+                    {t('displayed_or_hidden')}
+                  </label>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <h4>{t('page_access_and_delete_rights')}</h4>
+        <div className="row mb-4">
+          <div className="col-md-3 text-md-right py-2">
+            <strong>{t('security_setting.Guest Users Access')}</strong>
+          </div>
+          <div className="col-md-9">
+            <div className="dropdown">
+              {/* TODO: show dropdown text byGW-5142 */}
+              <button
               // className={`btn btn-outline-secondary dropdown-toggle text-right col-12
               //             col-md-auto ${adminGeneralSecurityContainer.isWikiModeForced && 'disabled'}`}
-              type="button"
-              id="dropdownMenuButton"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="true"
-            >
-              <span className="float-left">
-                {/* {currentRestrictGuestMode === 'Deny' && t('security_setting.guest_mode.deny')}
+                type="button"
+                id="dropdownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="true"
+              >
+                <span className="float-left">
+                  {/* {currentRestrictGuestMode === 'Deny' && t('security_setting.guest_mode.deny')}
                 {currentRestrictGuestMode === 'Readonly' && t('security_setting.guest_mode.readonly')} */}
-              </span>
-            </button>
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <button
-                className="dropdown-item"
-                type="button"
-                // onClick={() => { adminGeneralSecurityContainer.changeRestrictGuestMode('Deny') }}
-              >
-                {t('security_setting.guest_mode.deny')}
+                </span>
               </button>
-              <button
-                className="dropdown-item"
-                type="button"
-                // onClick={() => { adminGeneralSecurityContainer.changeRestrictGuestMode('Readonly') }}
-              >
-                {t('security_setting.guest_mode.readonly')}
-              </button>
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <button
+                  className="dropdown-item"
+                  type="button"
+                >
+                  {t('security_setting.guest_mode.deny')}
+                </button>
+                <button
+                  className="dropdown-item"
+                  type="button"
+                >
+                  {t('security_setting.guest_mode.readonly')}
+                </button>
+              </div>
             </div>
-          </div>
-          {/* {adminGeneralSecurityContainer.isWikiModeForced && (
+            {/* {adminGeneralSecurityContainer.isWikiModeForced && (
             <p className="alert alert-warning mt-2 text-left offset-3 col-6">
               <i className="icon-exclamation icon-fw">
               </i><b>FIXED</b><br />
@@ -138,72 +162,68 @@ export const SecuritySetting: FC<Props> = () => {
               />
             </p>
           )} */}
+          </div>
         </div>
-      </div>
 
-      <div className="row mb-4">
-        <div className="col-md-3 text-md-right mb-2">
-          <strong>{t('security_setting.complete_deletion')}</strong>
-        </div>
-        <div className="col-md-6">
-          <div className="dropdown">
-            <button
-              className="btn btn-outline-secondary dropdown-toggle text-right col-12 col-md-auto"
-              type="button"
-              id="dropdownMenuButton"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="true"
-            >
-              {/* <span className="float-left">
+        <div className="row mb-4">
+          <div className="col-md-3 text-md-right mb-2">
+            <strong>{t('security_setting.complete_deletion')}</strong>
+          </div>
+          <div className="col-md-6">
+            <div className="dropdown">
+              <button
+                className="btn btn-outline-secondary dropdown-toggle text-right col-12 col-md-auto"
+                type="button"
+                id="dropdownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="true"
+              >
+                {/* <span className="float-left">
                 {currentPageCompleteDeletionAuthority === 'anyOne' && t('security_setting.anyone')}
                 {currentPageCompleteDeletionAuthority === 'adminOnly' && t('security_setting.admin_only')}
                 {(currentPageCompleteDeletionAuthority === 'adminAndAuthor' || currentPageCompleteDeletionAuthority == null)
                     && t('security_setting.admin_and_author')}
               </span> */}
-            </button>
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <button
-                className="dropdown-item"
-                type="button"
-                // onClick={() => { adminGeneralSecurityContainer.changePageCompleteDeletionAuthority('anyOne') }}
-              >
-                {t('security_setting.anyone')}
               </button>
-              <button
-                className="dropdown-item"
-                type="button"
-                // onClick={() => { adminGeneralSecurityContainer.changePageCompleteDeletionAuthority('adminOnly') }}
-              >
-                {t('security_setting.admin_only')}
-              </button>
-              <button
-                className="dropdown-item"
-                type="button"
-                // onClick={() => { adminGeneralSecurityContainer.changePageCompleteDeletionAuthority('adminAndAuthor') }}
-              >
-                {t('security_setting.admin_and_author')}
-              </button>
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <button
+                  className="dropdown-item"
+                  type="button"
+                >
+                  {t('security_setting.anyone')}
+                </button>
+                <button
+                  className="dropdown-item"
+                  type="button"
+                >
+                  {t('security_setting.admin_only')}
+                </button>
+                <button
+                  className="dropdown-item"
+                  type="button"
+                >
+                  {t('security_setting.admin_and_author')}
+                </button>
+              </div>
+              <p className="form-text text-muted small">
+                {t('security_setting.complete_deletion_explain')}
+              </p>
             </div>
-            <p className="form-text text-muted small">
-              {t('security_setting.complete_deletion_explain')}
-            </p>
           </div>
         </div>
-      </div>
-      <div className="row my-3">
-        <div className="text-center text-md-left offset-md-3 col-md-5">
-          <button
-            type="button"
-            className="btn btn-primary"
-            // disabled={adminGeneralSecurityContainer.retrieveError != null}
-            onClick={putSecuritySetting}
-          >
-            {t('Update')}
-          </button>
+        <div className="row my-3">
+          <div className="text-center text-md-left offset-md-3 col-md-5">
+            <div className="row my-3">
+              <div className="mx-auto">
+                <button type="submit" className="btn btn-primary">{ t('Update') }</button>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </React.Fragment>
+
+      </form>
+    </div>
   );
 };
 
