56cc85210d298214225970b7fbd07eed78f81126
itizawa <itizawa@weseek.co.jp>
2021-03-11 09:32:37 +0000

    use useCurrentPageList

--- src/components/PageAccessory/PageList.tsx
+++ src/components/PageAccessory/PageList.tsx
@@ -9,46 +9,40 @@ import { apiv3Get } from '../../client/js/util/apiv3-client';
 import Page from '../../client/js/components/PageList/Page';
 import { Page as IPage } from '~/interfaces/page';
 
-import { useCurrentPageSWR } from '~/stores/page';
+import { useCurrentPageSWR, useCurrentPageList } from '~/stores/page';
 import { useTranslation } from '~/i18n';
 
-type Props ={
+type Props = {
   liClasses?: string[],
 }
 
 export const PageList:VFC<Props> = (props:Props) => {
   const { t } = useTranslation();
 
-  const { data: currentPage } = useCurrentPageSWR();
-
   const { liClasses = ['mb-3'] } = props;
   const [pages, setPages] = useState<IPage[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
 
   const [activePage, setActivePage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
+  const [totalItemsCount, setTotalItemsCount] = useState(0);
   const [limit, setLimit] = useState(Infinity);
 
-  function setPageNumber(selectedPageNumber) {
-    setActivePage(selectedPageNumber);
-  }
+  const { data: paginationResult } = useCurrentPageList(activePage);
 
-  const updatePageList = useCallback(async() => {
-    const page = activePage;
-    const res = await apiv3Get('/pages/list', { path: currentPage?.path, page });
-
-    setPages(res.data.pages);
-    setIsLoading(false);
-    setTotalPages(res.data.totalCount);
-    setLimit(res.data.limit);
-  }, [currentPage?.path, activePage]);
+  const handlePage = useCallback(async(selectedPage) => {
+    setActivePage(selectedPage - 1);
+  }, []);
 
   useEffect(() => {
-    updatePageList();
-  }, [updatePageList]);
+    if (paginationResult == null) {
+      return;
+    }
+    setActivePage(paginationResult.offset + 1);
+    setTotalItemsCount(paginationResult.totalCount);
+    setLimit(paginationResult.limit);
+    setPages(paginationResult.pages);
+  }, [paginationResult]);
 
-
-  if (isLoading) {
+  if (paginationResult == null) {
     return (
       <div className="wiki">
         <div className="text-muted text-center">
@@ -58,13 +52,7 @@ export const PageList:VFC<Props> = (props:Props) => {
     );
   }
 
-
-  const pageList = pages.map(page => (
-    <li key={page._id} className={liClasses.join(' ')}>
-      <Page page={page} />
-    </li>
-  ));
-  if (pageList.length === 0) {
+  if (pages.length === 0) {
     return (
       <div className="mt-2">
         {/* eslint-disable-next-line react/no-danger */}
@@ -76,12 +64,16 @@ export const PageList:VFC<Props> = (props:Props) => {
   return (
     <div className="page-list">
       <ul className="page-list-ul page-list-ul-flat">
-        {pageList}
+        {pages.map(page => (
+          <li key={page._id} className={liClasses.join(' ')}>
+            <Page page={page} />
+          </li>
+        ))}
       </ul>
       <PaginationWrapper
         activePage={activePage}
-        changePage={setPageNumber}
-        totalItemsCount={totalPages}
+        changePage={handlePage}
+        totalItemsCount={totalItemsCount}
         pagingLimit={limit}
         align="center"
       />
