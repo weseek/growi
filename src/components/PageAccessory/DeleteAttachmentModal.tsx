b34c36d6d3ebf1a2b8f62dbd8c8ba55314e1e9cc
itizawa <itizawa@weseek.co.jp>
2021-03-16 09:03:14 +0000

    display originalName

--- src/components/PageAccessory/DeleteAttachmentModal.tsx
+++ src/components/PageAccessory/DeleteAttachmentModal.tsx
@@ -34,9 +34,10 @@ export const DeleteAttachmentModal:VFC<Props> = (props: Props) => {
       </ModalHeader>
       <ModalBody>
         <div className="attachment-delete-image">
-          {/* <p>
-            <i className={this.iconNameByFormat(attachmentToDelete.fileFormat)}></i> {attachmentToDelete.originalName}
-          </p> */}
+          <p>
+            <i className={(attachmentToDelete.fileFormat.match(/image\/.+/i)) ? 'icon-picture' : 'icon-doc'} />{' '}
+            {attachmentToDelete.originalName}
+          </p>
           <p>
             uploaded by{' '}
             <UserPicture user={attachmentToDelete.creator} size="sm" />{' '}
@@ -73,71 +74,3 @@ export const DeleteAttachmentModal:VFC<Props> = (props: Props) => {
 //   _onDeleteConfirm() {
 //     this.props.onAttachmentDeleteClickedConfirm(this.props.attachmentToDelete);
 //   }
-
-//   iconNameByFormat(format) {
-//     if (format.match(/image\/.+/i)) {
-//       return 'icon-picture';
-//     }
-
-//     return 'icon-doc';
-//   }
-
-//   renderByFileFormat(attachment) {
-//     const content = (attachment.fileFormat.match(/image\/.+/i))
-//       ? <img src={attachment.filePathProxied} alt="deleting image" />
-//       : '';
-
-
-//     return (
-//       <div className="attachment-delete-image">
-//         <p>
-//           <i className={this.iconNameByFormat(attachment.fileFormat)}></i> {attachment.originalName}
-//         </p>
-//         <p>
-//           uploaded by <UserPicture user={attachment.creator} size="sm"></UserPicture> <Username user={attachment.creator}></Username>
-//         </p>
-//         {content}
-//       </div>
-//     );
-//   }
-
-//   render() {
-//     const attachment = this.props.attachmentToDelete;
-//     if (attachment === null) {
-//       return null;
-//     }
-
-//     const props = Object.assign({}, this.props);
-//     delete props.onAttachmentDeleteClickedConfirm;
-//     delete props.attachmentToDelete;
-//     delete props.inUse;
-//     delete props.deleting;
-//     delete props.deleteError;
-
-
-//     const renderAttachment = this.renderByFileFormat(attachment);
-
-//     return (
-//       <Modal {...props} className="attachment-delete-modal" bssize="large" aria-labelledby="contained-modal-title-lg">
-//         <ModalHeader tag="h4" toggle={this.props.toggle} className="bg-danger text-light">
-//           <span id="contained-modal-title-lg">Delete attachment?</span>
-//         </ModalHeader>
-//         <ModalBody>
-//           {renderAttachment}
-//         </ModalBody>
-//         <ModalFooter>
-//           <div className="mr-3 d-inline-block">
-//             {deletingIndicator}
-//           </div>
-//           <Button
-//             color="danger"
-//             onClick={this._onDeleteConfirm}
-//             disabled={this.props.deleting}
-//           >Delete!
-//           </Button>
-//         </ModalFooter>
-//       </Modal>
-//     );
-//   }
-
-// }
