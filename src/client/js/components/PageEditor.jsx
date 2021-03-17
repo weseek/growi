b540f611d05e9259d5486dab163f0ed51beb2091
yusuketk <yusuketk@weseek.co.jp>
2021-03-17 15:58:46 +0900

    get page info

--- src/client/js/components/PageEditor.jsx
+++ src/client/js/components/PageEditor.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import { throttle, debounce } from 'throttle-debounce';
 import { envUtils } from 'growi-commons';
@@ -7,87 +7,158 @@ import loggerFactory from '~/utils/logger';
 import Editor from './PageEditor/Editor';
 import Preview from './PageEditor/Preview';
 import scrollSyncHelper from './PageEditor/ScrollSyncHelper';
+import { useCurrentPageSWR } from '~/stores/page';
+import { useIsMobile } from '~/stores/ui';
 import { useEditorConfig } from '~/stores/context';
 
 const logger = loggerFactory('growi:PageEditor');
 
 const PageEditor = (props) => {
+  const editor = useRef();
+  const previewElement = useRef();
 
-  // this.previewElement = React.createRef();
+  const { data: currentPage } = useCurrentPageSWR();
+  const { data: isMobile } = useIsMobile();
   const { data: config } = useEditorConfig();
 
-  const [markdown, setMarkdown] = useState('');
-  // markdown: this.props.pageContainer.state.markdown,
+  const [markdown, setMarkdown] = useState(currentPage?.revision?.body);
   const [isUploadable, setIsUploadable] = useState(config.upload.image || config.upload.file);
   const [isUploadableFile, setIsUploadableFile] = useState(config.upload.file);
   const [isMathJaxEnabled, setIsMathJaxEnabled] = useState(!!config.env.MATHJAX);
 
   // // for scrolling
-  // this.lastScrolledDateWithCursor = null;
-  // this.isOriginOfScrollSyncEditor = false;
-  // this.isOriginOfScrollSyncEditor = false;
+  let lastScrolledDateWithCursor = null;
+  let isOriginOfScrollSyncEditor = false;
+  let isOriginOfScrollSyncPreview = false;
 
-  //   // create throttled function
-  //   this.scrollPreviewByEditorLineWithThrottle = throttle(20, this.scrollPreviewByEditorLine);
-  //   this.scrollPreviewByCursorMovingWithThrottle = throttle(20, this.scrollPreviewByCursorMoving);
-  //   this.scrollEditorByPreviewScrollWithThrottle = throttle(20, this.scrollEditorByPreviewScroll);
-  //   this.setMarkdownStateWithDebounce = debounce(50, throttle(100, (value) => {
-  //     this.setState({ markdown: value });
-  //   }));
-  //   this.saveDraftWithDebounce = debounce(800, this.saveDraft);
-  // }
+
+  /**
+   * scroll Preview element by scroll event
+   * @param {number} line
+   */
+  const scrollPreviewByEditorLine = (line) => {
+    if (previewElement == null) {
+      return;
+    }
+
+    // prevent circular invocation
+    if (isOriginOfScrollSyncPreview) {
+      isOriginOfScrollSyncPreview = false; // turn off the flag
+      return;
+    }
+
+    // turn on the flag
+    isOriginOfScrollSyncEditor = true;
+    scrollSyncHelper.scrollPreview(previewElement, line);
+  };
+
+
+  /**
+   * scroll Preview element by cursor moving
+   * @param {number} line
+   */
+  const scrollPreviewByCursorMoving = (line) => {
+    if (previewElement == null) {
+      return;
+    }
+
+    // prevent circular invocation
+    if (isOriginOfScrollSyncPreview) {
+      isOriginOfScrollSyncPreview = false; // turn off the flag
+      return;
+    }
+
+    // turn on the flag
+    isOriginOfScrollSyncEditor = true;
+    scrollSyncHelper.scrollPreviewToRevealOverflowing(previewElement, line);
+  };
+
+  /**
+   * scroll Editor component by scroll event of Preview component
+   * @param {number} offset
+   */
+  const scrollEditorByPreviewScroll = (offset) => {
+    if (previewElement == null) {
+      return;
+    }
+
+    // prevent circular invocation
+    if (isOriginOfScrollSyncEditor) {
+      isOriginOfScrollSyncEditor = false; // turn off the flag
+      return;
+    }
+
+    // turn on the flag
+    isOriginOfScrollSyncPreview = true;
+    scrollSyncHelper.scrollEditor(editor, previewElement, offset);
+  };
+
+  const saveDraft = () => {
+    // const { pageContainer, editorContainer } = this.props;
+    // editorContainer.saveDraft(pageContainer.state.path, this.state.markdown);
+  };
+
+  // create throttled function
+  const scrollPreviewByEditorLineWithThrottle = throttle(20, scrollPreviewByEditorLine);
+  const scrollPreviewByCursorMovingWithThrottle = throttle(20, scrollPreviewByCursorMoving);
+  const scrollEditorByPreviewScrollWithThrottle = throttle(20, scrollEditorByPreviewScroll);
+  const setMarkdownStateWithDebounce = debounce(50, throttle(100, (value) => {
+    setMarkdown(value);
+  }));
+  const saveDraftWithDebounce = debounce(800, saveDraft);
+
 
   const getMarkdown = () => {
     return markdown;
   };
 
-  // updateEditorValue(markdown) {
-  //   this.editor.setValue(markdown);
-  // }
+  const updateEditorValue = (markdown) => {
+    editor.setValue(markdown);
+  };
 
-  // focusToEditor() {
-  //   this.editor.forceToFocus();
-  // }
+  const focusToEditor = () => {
+    editor.forceToFocus();
+  };
 
   /**
    * set caret position of editor
    * @param {number} line
    */
-  // setCaretLine(line) {
-  //   this.editor.setCaretLine(line);
-  //   scrollSyncHelper.scrollPreview(this.previewElement, line);
-  // }
+  const setCaretLine = (line) => {
+    editor.setCaretLine(line);
+    scrollSyncHelper.scrollPreview(previewElement, line);
+  };
 
   /**
    * the change event handler for `markdown` state
    * @param {string} value
    */
-  // onMarkdownChanged(value) {
-  //   // const { pageContainer, editorContainer } = this.props;
-  //   this.setMarkdownStateWithDebounce(value);
-  //   // only when the first time to edit
-  //   // if (!pageContainer.state.revisionId) {
-  //   //   this.saveDraftWithDebounce();
-  //   // }
-  //   // editorContainer.enableUnsavedWarning();
-  // }
+  const onMarkdownChanged = (value) => {
+    // const { pageContainer, editorContainer } = this.props;
+    setMarkdownStateWithDebounce(value);
+    // only when the first time to edit
+    // if (!pageContainer.state.revisionId) {
+    //   this.saveDraftWithDebounce();
+    // }
+    // editorContainer.enableUnsavedWarning();
+  };
 
   /**
    * save and update state of containers
    */
-  // async onSaveWithShortcut() {
+  const onSaveWithShortcut = async() => {
   // const { pageContainer, editorContainer } = this.props;
   // const optionsToSave = editorContainer.getCurrentOptionsToSave();
 
-  // try {
-  //   // disable unsaved warning
-  //   editorContainer.disableUnsavedWarning();
+    // try {
+    //   // disable unsaved warning
+    //   editorContainer.disableUnsavedWarning();
 
-  //   // eslint-disable-next-line no-unused-vars
-  //   const { page, tags } = await pageContainer.save(this.state.markdown, optionsToSave);
-  //   logger.debug('success to save');
+    //   // eslint-disable-next-line no-unused-vars
+    //   const { page, tags } = await pageContainer.save(this.state.markdown, optionsToSave);
+    //   logger.debug('success to save');
 
-  //   pageContainer.showSuccessToastr();
+    //   pageContainer.showSuccessToastr();
 
   //   // update state of EditorContainer
   //   editorContainer.setState({ tags });
@@ -96,44 +167,44 @@ const PageEditor = (props) => {
   //   logger.error('failed to save', error);
   //   pageContainer.showErrorToastr(error);
   // }
-  // }
+  };
 
   /**
    * the upload event handler
    * @param {any} file
    */
-  // async onUpload(file) {
+  const onUpload = (file) => {
   // const { appContainer, pageContainer, editorContainer } = this.props;
 
-  // try {
-  //   let res = await appContainer.apiGet('/attachments.limit', {
-  //     fileSize: file.size,
-  //   });
+    // try {
+    //   let res = await appContainer.apiGet('/attachments.limit', {
+    //     fileSize: file.size,
+    //   });
 
-  //   if (!res.isUploadable) {
-  //     throw new Error(res.errorMessage);
-  //   }
+    //   if (!res.isUploadable) {
+    //     throw new Error(res.errorMessage);
+    //   }
 
-  //   const formData = new FormData();
-  //   const { pageId, path } = pageContainer.state;
-  //   formData.append('_csrf', appContainer.csrfToken);
-  //   formData.append('file', file);
-  //   formData.append('path', path);
-  //   if (pageId != null) {
-  //     formData.append('page_id', pageContainer.state.pageId);
-  //   }
+    //   const formData = new FormData();
+    //   const { pageId, path } = pageContainer.state;
+    //   formData.append('_csrf', appContainer.csrfToken);
+    //   formData.append('file', file);
+    //   formData.append('path', path);
+    //   if (pageId != null) {
+    //     formData.append('page_id', pageContainer.state.pageId);
+    //   }
 
-  //   res = await appContainer.apiPost('/attachments.add', formData);
-  //   const attachment = res.attachment;
-  //   const fileName = attachment.originalName;
+    //   res = await appContainer.apiPost('/attachments.add', formData);
+    //   const attachment = res.attachment;
+    //   const fileName = attachment.originalName;
 
-  //   let insertText = `[${fileName}](${attachment.filePathProxied})`;
-  //   // when image
-  //   if (attachment.fileFormat.startsWith('image/')) {
-  //     // modify to "![fileName](url)" syntax
-  //     insertText = `!${insertText}`;
-  //   }
-  //   this.editor.insertText(insertText);
+    //   let insertText = `[${fileName}](${attachment.filePathProxied})`;
+    //   // when image
+    //   if (attachment.fileFormat.startsWith('image/')) {
+    //     // modify to "![fileName](url)" syntax
+    //     insertText = `!${insertText}`;
+    //   }
+    //   this.editor.insertText(insertText);
 
   //   // when if created newly
   //   if (res.pageCreated) {
@@ -149,7 +220,7 @@ const PageEditor = (props) => {
   // finally {
   //   this.editor.terminateUploadingState();
   // }
-  // }
+  };
 
   /**
    * the scroll event handler from codemirror
@@ -158,104 +229,40 @@ const PageEditor = (props) => {
    *                    And data.line is also available that is added by Editor component
    * @see https://codemirror.net/doc/manual.html#events
    */
-  // onEditorScroll(data) {
-  //   // prevent scrolling
-  //   //   if the elapsed time from last scroll with cursor is shorter than 40ms
-  //   const now = new Date();
-  //   if (now - this.lastScrolledDateWithCursor < 40) {
-  //     return;
-  //   }
+  const onEditorScroll = (data) => {
+    // prevent scrolling
+    // if the elapsed time from last scroll with cursor is shorter than 40ms
+    const now = new Date();
+    if (now - lastScrolledDateWithCursor < 40) {
+      return;
+    }
 
-  //   this.scrollPreviewByEditorLineWithThrottle(data.line);
-  // }
+    scrollPreviewByEditorLineWithThrottle(data.line);
+  };
 
   /**
    * the scroll event handler from codemirror
    * @param {number} line
    * @see https://codemirror.net/doc/manual.html#events
    */
-  // onEditorScrollCursorIntoView(line) {
-  //   // record date
-  //   this.lastScrolledDateWithCursor = new Date();
-  //   this.scrollPreviewByCursorMovingWithThrottle(line);
-  // }
+  const onEditorScrollCursorIntoView = (line) => {
+    // record date
+    lastScrolledDateWithCursor = new Date();
+    scrollPreviewByCursorMovingWithThrottle(line);
+  };
 
-  /**
-   * scroll Preview element by scroll event
-   * @param {number} line
-   */
-  // scrollPreviewByEditorLine(line) {
-  //   if (this.previewElement == null) {
-  //     return;
-  //   }
-
-  //   // prevent circular invocation
-  //   if (this.isOriginOfScrollSyncPreview) {
-  //     this.isOriginOfScrollSyncPreview = false; // turn off the flag
-  //     return;
-  //   }
-
-  //   // turn on the flag
-  //   this.isOriginOfScrollSyncEditor = true;
-  //   scrollSyncHelper.scrollPreview(this.previewElement, line);
-  // }
-
-  /**
-   * scroll Preview element by cursor moving
-   * @param {number} line
-   */
-  // scrollPreviewByCursorMoving(line) {
-  //   if (this.previewElement == null) {
-  //     return;
-  //   }
-
-  //   // prevent circular invocation
-  //   if (this.isOriginOfScrollSyncPreview) {
-  //     this.isOriginOfScrollSyncPreview = false; // turn off the flag
-  //     return;
-  //   }
-
-  //   // turn on the flag
-  //   this.isOriginOfScrollSyncEditor = true;
-  //   scrollSyncHelper.scrollPreviewToRevealOverflowing(this.previewElement, line);
-  // }
 
   /**
    * the scroll event handler from Preview component
    * @param {number} offset
    */
-  // onPreviewScroll(offset) {
-  //   this.scrollEditorByPreviewScrollWithThrottle(offset);
-  // }
+  const onPreviewScroll = (offset) => {
+    scrollEditorByPreviewScrollWithThrottle(offset);
+  };
 
-  /**
-   * scroll Editor component by scroll event of Preview component
-   * @param {number} offset
-   */
-  // scrollEditorByPreviewScroll(offset) {
-  //   if (this.previewElement == null) {
-  //     return;
-  //   }
-
-  //   // prevent circular invocation
-  //   if (this.isOriginOfScrollSyncEditor) {
-  //     this.isOriginOfScrollSyncEditor = false; // turn off the flag
-  //     return;
-  //   }
-
-  //   // turn on the flag
-  //   this.isOriginOfScrollSyncPreview = true;
-  //   scrollSyncHelper.scrollEditor(this.editor, this.previewElement, offset);
-  // }
-
-  // saveDraft() {
-  //   const { pageContainer, editorContainer } = this.props;
-  //   editorContainer.saveDraft(pageContainer.state.path, this.state.markdown);
-  // }
-
-  // clearDraft() {
-  //   this.props.editorContainer.clearDraft(this.props.pageContainer.state.path);
-  // }
+  const constclearDraft = () => {
+    // this.props.editorContainer.clearDraft(this.props.pageContainer.state.path);
+  };
 
   const noCdn = envUtils.toBoolean(config.env.NO_CDN);
   // const emojiStrategy = this.props.appContainer.getEmojiStrategy();
@@ -263,30 +270,28 @@ const PageEditor = (props) => {
   return (
     <div className="d-flex flex-wrap">
       <div className="page-editor-editor-container flex-grow-1 flex-basis-0 mw-0">
-        hoge
         <Editor
-          // ref={(c) => { this.editor = c }}
+          ref={editor}
           value={markdown}
           noCdn={noCdn}
-            // isMobile={this.props.appContainer.isMobile}
+          isMobile={isMobile}
           isUploadable={isUploadable}
           isUploadableFile={isUploadableFile}
             // emojiStrategy={emojiStrategy}
-          // onScroll={this.onEditorScroll}
-          // onScrollCursorIntoView={this.onEditorScrollCursorIntoView}
-          // onChange={this.onMarkdownChanged}
-          // onUpload={this.onUpload}
-          // onSave={this.onSaveWithShortcut}
+          onScroll={onEditorScroll}
+          onScrollCursorIntoView={onEditorScrollCursorIntoView}
+          onChange={onMarkdownChanged}
+          onUpload={onUpload}
+          onSave={onSaveWithShortcut}
         />
       </div>
       <div className="d-none d-lg-block page-editor-preview-container flex-grow-1 flex-basis-0 mw-0">
         {/* <Preview
-          markdown={this.state.markdown}
-            // eslint-disable-next-line no-return-assign
-          inputRef={(el) => { return this.previewElement = el }}
-          isMathJaxEnabled={this.state.isMathJaxEnabled}
+          markdown={markdown}
+          inputRef={previewElement}
+          isMathJaxEnabled={isMathJaxEnabled}
           renderMathJaxOnInit={false}
-          onScroll={this.onPreviewScroll}
+          onScroll={onPreviewScroll}
         /> */}
       </div>
     </div>
