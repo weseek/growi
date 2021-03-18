70387bba8daece6eb1ff696f0d2dac9aa919c505
zahmis <meda829ratteru86@gmail.com>
2021-03-18 05:25:38 +0000

    add accessTokenParser

--- src/server/routes/apiv3/slack-bot-setting.js
+++ src/server/routes/apiv3/slack-bot-setting.js
@@ -4,6 +4,7 @@ const loggerFactory = require('@alias/logger');
 const logger = loggerFactory('growi:routes:apiv3:notification-setting');
 
 const express = require('express');
+const apiv3FormValidator = require('../../middlewares/apiv3-form-validator');
 
 const router = express.Router();
 
@@ -17,17 +18,33 @@ const router = express.Router();
 
 /**
  * @swagger
- *  slack-bot-setting:
- *    name: SlackBotSetting
+ *  tags:
+ *    name: SlackBot
  */
 
 module.exports = (crowi) => {
-  // const loginRequiredStrictly = require('../../middlewares/login-required')(crowi);
-  // const adminRequired = require('../../middlewares/admin-required')(crowi);
-  // const csrf = require('../../middlewares/csrf')(crowi);
+  const accessTokenParser = require('../../middlewares/access-token-parser')(crowi);
+
+  const loginRequiredStrictly = require('../../middlewares/login-required')(crowi);
+
+  const adminRequired = require('../../middlewares/admin-required')(crowi);
+  const csrf = require('../../middlewares/csrf')(crowi);
   // const apiV3FormValidator = require('../../middlewares/apiv3-form-validator')(crowi);
 
-  router.get('/', async(req, res) => {
+  /**
+   * @swagger
+   *
+   *  paths:
+   *    /custom-bot-setting/:
+   *      get:
+   *        tags:
+   *        description: get SingingSecret and slackBotToken
+   *        parameters:
+   *        responses:
+   *          200:
+   *            description: Succeeded to get SigningSecret and SlackBotToken
+   */
+  router.get('/custom-bot-setting', accessTokenParser, loginRequiredStrictly, adminRequired, async(req, res) => {
 
     const slackBotSettingParams = {
       slackSigningSecret: await crowi.configManager.getConfig('crowi', 'slackbot:signingSecret'),
@@ -36,11 +53,12 @@ module.exports = (crowi) => {
     return res.apiv3({ slackBotSettingParams });
   });
 
-  router.put('/', async(req, res) => {
+  router.put('/custom-bot-setting', accessTokenParser, loginRequiredStrictly, adminRequired, csrf, async(req, res) => {
 
     const slackBotSettingParams = {
-      slackSigningSecret: req.body.slackSigningSecret,
-      slackBotToken: req.body.slackBotSettingParams,
+      // temp data
+      slackSigningSecret: 1234567890,
+      slackBotToken: 'asdfghjkkl1234567890',
     };
     return res.apiv3({ slackBotSettingParams });
   });
