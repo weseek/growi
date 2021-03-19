e10a93f5ffdbb1e408140e8db879fd94ac9677c0
itizawa <itizawa@weseek.co.jp>
2021-03-19 10:47:40 +0000

    refactor

--- src/server/models/revision.ts
+++ src/server/models/revision.ts
@@ -1,34 +1,44 @@
-// disable no-return-await for model functions
-/* eslint-disable no-return-await */
-import loggerFactory from '~/utils/logger';
+import { Schema, Model } from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const logger = loggerFactory('growi:models:revision');
+import { getOrCreateModel } from '../util/mongoose-utils';
+import { Revision as IRevision } from '../../interfaces/page';
 
-module.exports = function(crowi) {
+// import User from '~/server/models/user';
 
+/*
+ * define methods type
+ */
+interface ModelMethods {
+  updateName(name:string): Promise<void>;
+}
 
-  const mongoose = require('mongoose');
-  const mongoosePaginate = require('mongoose-paginate-v2');
+const schema = new Schema<IRevision>({
 
-  const ObjectId = mongoose.Schema.Types.ObjectId;
-  const revisionSchema = new mongoose.Schema({
-    path: { type: String, required: true, index: true },
-    body: {
-      type: String,
-      required: true,
-      get: (data) => {
+  path: { type: String, required: true, index: true },
+  body: {
+    type: String,
+    required: true,
+    get: (data) => {
       // replace CR/CRLF to LF above v3.1.5
       // see https://github.com/weseek/growi/issues/463
-        return data ? data.replace(/\r\n?/g, '\n') : '';
-      },
+      return data ? data.replace(/\r\n?/g, '\n') : '';
     },
-    format: { type: String, default: 'markdown' },
-    author: { type: ObjectId, ref: 'User' },
-    createdAt: { type: Date, default: Date.now },
-    hasDiffToPrev: { type: Boolean },
-  });
-  revisionSchema.plugin(mongoosePaginate);
+  },
+  format: { type: String, default: 'markdown' },
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  createdAt: { type: Date, default: Date.now },
+  hasDiffToPrev: { type: Boolean },
+});
+
+schema.plugin(mongoosePaginate);
+
+/**
+ * Revision Class
+ *
+ * @class Revision
+ */
+class Revision extends Model {
 
   /*
    * preparation for https://github.com/weseek/growi/issues/216
@@ -42,52 +52,50 @@ module.exports = function(crowi) {
   //   next();
   // });
 
-  revisionSchema.statics.findRevisions = function(ids) {
-    const User = crowi.model('User');
+  static findRevisions(ids) {
 
     if (!Array.isArray(ids)) {
       return Promise.reject(new Error('The argument was not Array.'));
     }
 
-    return new Promise(((resolve, reject) => {
-      this.find({ _id: { $in: ids } })
-        .sort({ createdAt: -1 })
-        .populate('author', User.USER_PUBLIC_FIELDS)
-        .exec((err, revisions) => {
-          if (err) {
-            return reject(err);
-          }
+    // return new Promise(((resolve, reject) => {
+    //   this.find({ _id: { $in: ids } })
+    //     .sort({ createdAt: -1 })
+    //     .populate('author', User.USER_PUBLIC_FIELDS)
+    //     .exec((err, revisions) => {
+    //       if (err) {
+    //         return reject(err);
+    //       }
 
-          return resolve(revisions);
-        });
-    }));
-  };
+    //       return resolve(revisions);
+    //     });
+    // }));
+  }
 
-  revisionSchema.statics.findRevisionIdList = function(path) {
+  static findRevisionIdList(path) {
     return this.find({ path })
       .select('_id author createdAt hasDiffToPrev')
       .sort({ createdAt: -1 })
       .exec();
-  };
+  }
 
-  revisionSchema.statics.findRevisionList = function(path, options) {
-    const User = crowi.model('User');
+  static findRevisionList(path, options) {
 
-    return new Promise(((resolve, reject) => {
-      this.find({ path })
-        .sort({ createdAt: -1 })
-        .populate('author', User.USER_PUBLIC_FIELDS)
-        .exec((err, data) => {
-          if (err) {
-            return reject(err);
-          }
+    // return new Promise(((resolve, reject) => {
+    //   this.find({ path })
+    //     .sort({ createdAt: -1 })
+    //     .populate('author', User.USER_PUBLIC_FIELDS)
+    //     .exec((err, data) => {
+    //       if (err) {
+    //         return reject(err);
+    //       }
 
-          return resolve(data);
-        });
-    }));
-  };
+    //       return resolve(data);
+    //     });
+    // }));
+  }
 
-  revisionSchema.statics.updateRevisionListByPath = function(path, updateData, options) {
+  static updateRevisionListByPath(path, updateData, options) {
     return new Promise(((resolve, reject) => {
       this.update({ path }, { $set: updateData }, { multi: true }, (err, data) => {
         if (err) {
@@ -97,9 +105,9 @@ module.exports = function(crowi) {
         return resolve(data);
       });
     }));
-  };
+  }
 
-  revisionSchema.statics.prepareRevision = function(pageData, body, previousBody, user, options) {
+  static prepareRevision(pageData, body, previousBody, user, options) {
     if (!options) {
       // eslint-disable-next-line no-param-reassign
       options = {};
@@ -121,19 +129,21 @@ module.exports = function(crowi) {
     }
 
     return newRevision;
-  };
+  }
 
-  revisionSchema.statics.removeRevisionsByPath = function(path) {
-    return new Promise(((resolve, reject) => {
-      this.remove({ path }, (err, data) => {
-        if (err) {
-          return reject(err);
-        }
+static removeRevisionsByPath = function(path) {
+  return new Promise(((resolve, reject) => {
+    this.remove({ path }, (err, data) => {
+      if (err) {
+        return reject(err);
+      }
 
-        return resolve(data);
-      });
-    }));
-  };
-
-  return mongoose.model('Revision', revisionSchema);
+      return resolve(data);
+    });
+  }));
 };
+
+}
+
+schema.loadClass(Revision);
+export default getOrCreateModel<IRevision, ModelMethods>('Revision', schema);
