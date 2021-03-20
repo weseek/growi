e2afc7f5a074bf084f107916d08bf038783ef0ea
Yuki Takei <yuki@weseek.co.jp>
2021-03-20 22:42:57 +0000

    support --ci option

--- packages/growi-bot-proxy/src/index.ts
+++ packages/growi-bot-proxy/src/index.ts
@@ -1,5 +1,8 @@
 import { $log } from '@tsed/common';
 import { PlatformExpress } from '@tsed/platform-express';
+
+import helpers from '^/../../src/lib/util/helpers';
+
 import { Server } from './Server';
 
 async function bootstrap() {
@@ -9,6 +12,11 @@ async function bootstrap() {
 
     await platform.listen();
     $log.debug('Server initialized');
+
+    if (helpers.hasProcessFlag('ci')) {
+      $log.info('"--ci" flag is detected. Exit process.');
+      process.exit();
+    }
   }
   catch (er) {
     $log.error(er);
