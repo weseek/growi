e0b0bc6771a891ec38dd0254322c15613f42c488
itizawa <itizawa@weseek.co.jp>
2021-03-23 07:08:54 +0000

    display DuplicatedAlert

--- src/components/PageAlert/PageAlerts.tsx
+++ src/components/PageAlert/PageAlerts.tsx
@@ -1,11 +1,13 @@
 import { VFC } from 'react';
 import { RenameAlert } from '~/components/PageAlert/RenameAlert';
+import { DuplicatedAlert } from '~/components/PageAlert/DuplicatedAlert';
 
 export const PageAlerts:VFC = () => {
   return (
     <div className="row row-alerts d-edit-none">
       <div className="col-sm-12">
         <RenameAlert />
+        <DuplicatedAlert />
       </div>
     </div>
   );
