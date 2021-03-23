9fc62b701de9a1d72337f3f9aa8199db29729e9a
itizawa <itizawa@weseek.co.jp>
2021-03-23 05:56:59 +0000

    create StaleAlert component

--- src/components/PageAlert/PageAlerts.tsx
+++ src/components/PageAlert/PageAlerts.tsx
@@ -1,10 +1,12 @@
 import { VFC } from 'react';
+import { StaleAlert } from '~/components/PageAlert/StaleAlert';
 import { RenameAlert } from '~/components/PageAlert/RenameAlert';
 
 export const PageAlerts:VFC = () => {
   return (
     <div className="row row-alerts d-edit-none">
       <div className="col-sm-12">
+        <StaleAlert />
         <RenameAlert />
       </div>
     </div>
---
+++ src/components/PageAlert/StaleAlert.tsx
@@ -0,0 +1,12 @@
+import { VFC } from 'react';
+
+type Props = {
+
+};
+export const StaleAlert:VFC<Props> = (props: Props) => {
+  return (
+    <div>
+
+    </div>
+  );
+};
