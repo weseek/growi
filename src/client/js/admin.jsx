42b90336281191861fa4913224928ecb0bab8966
Steven Fukase <fukasesteven@gmail.com>
2021-03-23 12:27:21 +0000

    Trying to add Slack Integration page

--- src/client/js/admin.jsx
+++ src/client/js/admin.jsx
@@ -10,6 +10,7 @@ import ErrorBoundary from './components/ErrorBoudary';
 import AdminHome from './components/Admin/AdminHome/AdminHome';
 import UserGroupDetailPage from './components/Admin/UserGroupDetail/UserGroupDetailPage';
 import NotificationSetting from './components/Admin/Notification/NotificationSetting';
+import SlackIntegration from './components/Admin/SlackIntegration/SlackIntegration';
 import ManageGlobalNotification from './components/Admin/Notification/ManageGlobalNotification';
 import MarkdownSetting from './components/Admin/MarkdownSetting/MarkDownSetting';
 import UserManagement from './components/Admin/UserManagement';
@@ -96,6 +97,7 @@ Object.assign(componentMappings, {
   'admin-importer': <ImportDataPage />,
   'admin-export-page': <ExportArchiveDataPage />,
   'admin-notification-setting': <NotificationSetting />,
+  'admin-slack-integration': <SlackIntegration />,
   'admin-global-notification-setting': <ManageGlobalNotification />,
   'admin-user-page': <UserManagement />,
   'admin-external-account-setting': <ManageExternalAccount />,
---
+++ src/client/js/components/Admin/SlackIntegration/SlackIntegration.jsx
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+// import PropTypes from 'prop-types';
+
+// import { withUnstatedContainers } from '../../UnstatedUtils';
+// import AppContainer from '../../../services/AppContainer';
+
+class SlackIntegration extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>AAA</h1>
+      </Fragment>
+    );
+  }
+}
+
+// const SlackIntegrationWrapper = withUnstatedContainers(SlackIntegration, [AppContainer]);
+
+// SlackIntegration.propTypes = {
+//   t: PropTypes.func.isRequired, // i18next
+//   appContainer: PropTypes.instanceOf(AppContainer).isRequired,
+// slackIntegrationContainer: PropTypes.instanceOf(SlackIntegrationContainer).isRequired,
+// };
+
+export default SlackIntegration;
