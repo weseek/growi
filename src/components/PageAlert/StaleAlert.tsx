813e2224b8dfe90c154af5ac2aa4d2cd48e93e85
itizawa <itizawa@weseek.co.jp>
2021-03-23 06:23:21 +0000

    implement StaleAlert

--- src/components/PageAlert/StaleAlert.tsx
+++ src/components/PageAlert/StaleAlert.tsx
@@ -1,12 +1,39 @@
 import { VFC } from 'react';
+import { differenceInYears } from 'date-fns';
+import { useTranslation } from '~/i18n';
+import { useCurrentPageSWR } from '~/stores/page';
 
-type Props = {
+export const StaleAlert:VFC = () => {
+  const { t } = useTranslation();
+  const { data: currentPage } = useCurrentPageSWR();
 
-};
-export const StaleAlert:VFC<Props> = (props: Props) => {
+  if (currentPage == null) {
+    return null;
+  }
+
+  const countAge:number = differenceInYears(new Date(), new Date(currentPage.updatedAt));
+
+  if (countAge === 0) {
+    return null;
+  }
+
+  let alertColor;
+  switch (countAge) {
+    case 1:
+      alertColor = 'info';
+      break;
+    case 2:
+      alertColor = 'info';
+      break;
+    default:
+      alertColor = 'danger';
+      break;
+  }
+
   return (
-    <div>
-
+    <div className={`alert alert-${alertColor}`}>
+      <i className="icon-fw icon-hourglass" />
+      <strong>{ t('page_page.notice.stale', { count: countAge }) }</strong>
     </div>
   );
 };
