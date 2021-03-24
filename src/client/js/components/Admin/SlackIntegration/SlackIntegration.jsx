f2311bfc41ac1b230948b3d2fbec96b0fa0a4979
Steven Fukase <fukasesteven@gmail.com>
2021-03-24 05:30:58 +0000

    commented out proptypes and i18next

--- src/client/js/components/Admin/SlackIntegration/SlackIntegration.jsx
+++ src/client/js/components/Admin/SlackIntegration/SlackIntegration.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 // import { withUnstatedContainers } from '../../UnstatedUtils';
 // import AppContainer from '../../../services/AppContainer';
@@ -7,14 +7,14 @@ import PropTypes from 'prop-types';
 class SlackIntegration extends React.Component {
 
   render() {
-    const { t } = this.props;
+    // const { t } = this.props;
 
     return (
       <Fragment>
         <div className="row">
           <div className="col-lg-12">
-            <h2 className="admin-setting-header">{t('Access Token')}</h2>
-            {/* <h2 className="admin-setting-header">Access Token</h2> */}
+            {/* <h2 className="admin-setting-header">{t('Access Token')}</h2> */}
+            <h2 className="admin-setting-header">Access Token</h2>
             <div className="form-group row">
               {/* <label className="text-left text-md-right col-md-3 col-form-label">{t('admin:app_setting.site_name')}</label> */}
               <label className="text-left text-md-right col-md-3 col-form-label">Access Token</label>
@@ -33,10 +33,10 @@ class SlackIntegration extends React.Component {
 
 // const SlackIntegrationWrapper = withUnstatedContainers(SlackIntegration, [AppContainer]);
 
-SlackIntegration.propTypes = {
-  t: PropTypes.func.isRequired, // i18next
+// SlackIntegration.propTypes = {
+//   t: PropTypes.func.isRequired, // i18next
 //   appContainer: PropTypes.instanceOf(AppContainer).isRequired,
 // slackIntegrationContainer: PropTypes.instanceOf(SlackIntegrationContainer).isRequired,
-};
+// };
 
 export default SlackIntegration;
