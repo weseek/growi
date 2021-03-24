b038cab60e5d348f44d0753707fd9ef64781bfb7
Steven Fukase <fukasesteven@gmail.com>
2021-03-24 07:07:20 +0000

    Access Token screen added (Without localization)

--- src/client/js/components/Admin/SlackIntegration/SlackIntegration.jsx
+++ src/client/js/components/Admin/SlackIntegration/SlackIntegration.jsx
@@ -3,8 +3,15 @@ import React, { Fragment } from 'react';
 
 // import { withUnstatedContainers } from '../../UnstatedUtils';
 // import AppContainer from '../../../services/AppContainer';
-
 class SlackIntegration extends React.Component {
+
+  updateHandler() {
+    console.log('Update button pressed');
+  }
+
+  discardHandler() {
+    console.log('Discard button pressed');
+  }
 
   render() {
     // const { t } = this.props;
@@ -15,13 +22,25 @@ class SlackIntegration extends React.Component {
           <div className="col-lg-12">
             {/* <h2 className="admin-setting-header">{t('Access Token')}</h2> */}
             <h2 className="admin-setting-header">Access Token</h2>
+
             <div className="form-group row">
               {/* <label className="text-left text-md-right col-md-3 col-form-label">{t('admin:app_setting.site_name')}</label> */}
               <label className="text-left text-md-right col-md-3 col-form-label">Access Token</label>
-
               <div className="col-md-6">
                 <input className="form-control" type="text" placeholder="access-token" />
               </div>
+            </div>
+
+            <div className="row my-3">
+              <div className="offset-4 col-5">
+                <button type="button" className="btn btn-primary text-nowrap mx-1" onClick={this.updateHandler}>
+                  Update
+                </button>
+                <button type="button" className="btn btn-primary text-nowrap mx-1" onClick={this.discardHandler}>
+                  Discard
+                </button>
+              </div>
+
             </div>
           </div>
         </div>
