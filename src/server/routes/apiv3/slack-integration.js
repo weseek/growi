6eb7b34b8e05e20c8f291303a40e6235b33aa061
itizawa <itizawa@weseek.co.jp>
2021-03-26 02:11:32 +0000

    require crypto

--- src/server/routes/apiv3/slack-integration.js
+++ src/server/routes/apiv3/slack-integration.js
@@ -3,6 +3,7 @@ const loggerFactory = require('@alias/logger');
 const logger = loggerFactory('growi:routes:apiv3:notification-setting');
 const express = require('express');
 const { body } = require('express-validator');
+const crypto = require('crypto');
 const ErrorV3 = require('../../models/vo/error-apiv3');
 
 const router = express.Router();
