295946d1ba40b92082a5ab568feb24da63592ab1
Steven Fukase <fukasesteven@gmail.com>
2021-03-30 01:59:45 +0000

    Toggle button

--- src/client/js/components/Admin/SlackIntegration/SlackIntegration.jsx
+++ src/client/js/components/Admin/SlackIntegration/SlackIntegration.jsx
@@ -16,6 +16,18 @@ function SlackIntegration() {
         </div>
       </div>
 
+      <div className="row mx-auto my-5 btn-group btn-group-toggle" data-toggle="buttons">
+        <label className="btn btn-secondary active">
+          <input type="radio" name="options" id="option1" checked /> Official Bot
+        </label>
+        <label className="btn btn-secondary">
+          <input type="radio" name="options" id="option2" /> Custom Bot (Non-Proxy)
+        </label>
+        <label className="btn btn-secondary">
+          <input type="radio" name="options" id="option3" /> Custom Bot (With-Proxy)
+        </label>
+      </div>
+
       <div className="row">
         <div className="col-lg-12">
           <h2 className="admin-setting-header">{t('slack_integration.custom_bot_non_proxy_settings')}</h2>
