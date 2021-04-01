02f9d4d3c53e091be9af2dcee7b27e2138354c45
zahmis <meda829ratteru86@gmail.com>
2021-04-01 15:31:57 +0000

    create checkSlackGrowiCommunication

--- src/client/js/components/Admin/SlackIntegration/SlackGrowiBridging.jsx
+++ src/client/js/components/Admin/SlackIntegration/SlackGrowiBridging.jsx
@@ -7,22 +7,37 @@ import { toastError } from '../../../util/apiNotification';
 import AppContainer from '../../../services/AppContainer';
 import AdminAppContainer from '../../../services/AdminAppContainer';
 
-const SlackGrowiBridging = (props) => {
-  const { appContainer, adminAppContainer } = props;
+
+const SlackGrowiBridging = (crowi, props) => {
+  const { WebClient, LogLevel } = require('@slack/web-api');
+  const { adminAppContainer } = props;
   const [siteName, setSiteName] = useState('');
+
+  const checkSlackGrowiCommunication = function() {
+    return new Promise((resolve, reject) => {
+      const client = new WebClient('xoxb-1399660543842-1848670292404-I6OLidkKzn4WPF34ezWmE56r', {
+        logLevel: LogLevel.DEBUG,
+      });
+      client.chat.postMessage({ text: 'hoge', channel: 'C01BAT2LXHV' }, (err, res) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(res);
+      });
+    });
+  };
 
   const fetchData = useCallback(async() => {
     try {
-      await adminAppContainer.retrieveAppSettingsData();
-      setSiteName(adminAppContainer.state.title);
-      const res = await appContainer.apiv3.get('/slack-bot/');
-      console.log(res);
+      // await adminAppContainer.retrieveAppSettingsData();
+      // setSiteName(adminAppContainer.state.title);
+      checkSlackGrowiCommunication();
 
     }
     catch (err) {
       toastError(err);
     }
-  }, [appContainer, adminAppContainer]);
+  }, [adminAppContainer]);
 
   useEffect(() => {
     fetchData();
