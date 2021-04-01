85d8acf191390d253af0ec0e49e108ef7f82e1d8
zahmis <meda829ratteru86@gmail.com>
2021-04-01 11:41:07 +0000

    add fetchdata, useEffect

--- src/client/js/components/Admin/SlackIntegration/SlackGrowiBridging.jsx
+++ src/client/js/components/Admin/SlackIntegration/SlackGrowiBridging.jsx
@@ -1,17 +1,44 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { withUnstatedContainers } from '../../UnstatedUtils';
+import { toastError } from '../../../util/apiNotification';
+
 
 import AppContainer from '../../../services/AppContainer';
 import AdminAppContainer from '../../../services/AdminAppContainer';
 
 const SlackGrowiBridging = (props) => {
-  return 'hoge';
+  const { appContainer, adminAppContainer } = props;
+  const [siteName, setSiteName] = useState('');
+
+  const fetchData = useCallback(async() => {
+    try {
+      await adminAppContainer.retrieveAppSettingsData();
+      setSiteName(adminAppContainer.state.title);
+      const res = await appContainer.apiv3.get('/slack-bot/');
+      console.log(res);
+
+    }
+    catch (err) {
+      toastError(err);
+    }
+  }, [appContainer, adminAppContainer]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+
+  return (
+    <>
+      {siteName}
+    </>
+  );
 };
 const SlackGrowiBridgingWrapper = withUnstatedContainers(SlackGrowiBridging, [AppContainer, AdminAppContainer]);
 
-SlackGrowiBridgingWrapper.propTypes = {
-  appContainer: PropTypes.instanceOf(AppContainer).isRequired,
-  adminAppContainer: PropTypes.instanceOf(AdminAppContainer).isRequired,
+SlackGrowiBridging.propTypes = {
+  appContainer: PropTypes.instanceOf(AppContainer),
+  adminAppContainer: PropTypes.instanceOf(AdminAppContainer),
 };
 export default SlackGrowiBridgingWrapper;
