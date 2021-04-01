7aed411c78f066e1489f69d0816eda37994a82ad
Steven Fukase <fukasesteven@gmail.com>
2021-04-01 07:42:18 +0000

    Deleted fragment

--- src/client/js/components/Admin/SlackIntegration/ConfirmBotChangeModal.jsx
+++ src/client/js/components/Admin/SlackIntegration/ConfirmBotChangeModal.jsx
@@ -24,29 +24,27 @@ const ConfirmBotChangeModal = (props) => {
   }
 
   return (
-    <>
-      <Modal isOpen={isOpen} centered>
-        <ModalHeader toggle={onCancelClick}>
-          {t('slack_integration.modal.warning')}
-        </ModalHeader>
-        <ModalBody>
-          <div>
-            <h4>{t('slack_integration.modal.sure_change_bot_type')}</h4>
-          </div>
-          <div>
-            <p>{t('slack_integration.modal.changes_will_be_deleted')}</p>
-          </div>
-        </ModalBody>
-        <ModalFooter>
-          <button type="button" className="btn btn-secondary" onClick={onCancelClick}>
-            {t('slack_integration.modal.cancel')}
-          </button>
-          <button type="button" className="btn btn-primary" onClick={onConfirmClick}>
-            {t('slack_integration.modal.change')}
-          </button>
-        </ModalFooter>
-      </Modal>
-    </>
+    <Modal isOpen={isOpen} centered>
+      <ModalHeader toggle={onCancelClick}>
+        {t('slack_integration.modal.warning')}
+      </ModalHeader>
+      <ModalBody>
+        <div>
+          <h4>{t('slack_integration.modal.sure_change_bot_type')}</h4>
+        </div>
+        <div>
+          <p>{t('slack_integration.modal.changes_will_be_deleted')}</p>
+        </div>
+      </ModalBody>
+      <ModalFooter>
+        <button type="button" className="btn btn-secondary" onClick={onCancelClick}>
+          {t('slack_integration.modal.cancel')}
+        </button>
+        <button type="button" className="btn btn-primary" onClick={onConfirmClick}>
+          {t('slack_integration.modal.change')}
+        </button>
+      </ModalFooter>
+    </Modal>
   );
 };
 
