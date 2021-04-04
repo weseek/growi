92bde4b9a601bac2731aaaf705fd2ae428d480ec
zahmis <meda829ratteru86@gmail.com>
2021-04-04 15:22:11 +0000

    create IndexService

--- packages/slack/src/services/index.ts
+++ packages/slack/src/services/index.ts
@@ -1,15 +1,21 @@
 import { parse } from '../utils/slash-command-parser';
-
+import { RegisterService } from './register';
 
 export class IndexService {
 
-  // constructor(crowi) {
-  //   this.crowi = crowi;
-  // }
   receiveBody(parseBody) {
     const body = parse(parseBody);
+    const commandType = body.growiCommandArgs;
+    const commandArgs = body.growiCommandType;
 
-    console.log(body);
+    if (commandType === 'register') {
+      this.openModal();
+      return;
+    }
+  }
+
+  openModal() {
+    console.log('open');
   }
 
 
