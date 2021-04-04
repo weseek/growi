4958aae6d0bf9ec8101945b12fdca2d2a294db2f
zahmis <meda829ratteru86@gmail.com>
2021-04-04 15:43:58 +0000

    fix register.ts in slack

--- packages/slack/src/services/register.ts
+++ packages/slack/src/services/register.ts
@@ -1,11 +1,21 @@
+import { parse } from '../utils/slash-command-parser';
+
 export class RegisterService {
 
-  // constructor(crowi) {
-  //   this.crowi = crowi;
-  // }
+  receiveBody(parseBody) {
+    const body = parse(parseBody);
+    const commandType = body.growiCommandArgs;
+    const commandArgs = body.growiCommandType;
+
+    if (commandType === 'register') {
+      this.openModal();
+      return;
+    }
+  }
 
   openModal() {
-    console.log('waimatu');
+    console.log('open');
   }
+
 
 }
