e844638aa36fa81f61ebabc2ffde5d8d38efeab0
zahmis <meda829ratteru86@gmail.com>
2021-04-04 16:45:59 +0000

    delete commentout

--- packages/slackbot-proxy/src/services/RecieveService.ts
+++ packages/slackbot-proxy/src/services/RecieveService.ts
@@ -3,12 +3,6 @@ import { Service } from '@tsed/di';
 @Service()
 export class ReceiveService {
 
-  // body:any
-
-  // constructor(body) {
-  //   this.body = body;
-  // }
-
   receive(body) {
 
     console.log('Receive Class', body);
