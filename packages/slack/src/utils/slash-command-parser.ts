89117495acecc7fabe5a83b745861a920763a562
zahmis <meda829ratteru86@gmail.com>
2021-04-05 06:29:40 +0000

    fix slash-command-parse.ts

--- packages/slack/src/utils/slash-command-parser.ts
+++ packages/slack/src/utils/slash-command-parser.ts
@@ -1,17 +1,18 @@
 import { SlashCommand } from '@slack/bolt';
 
-import { GrowiCommand } from '~/interfaces/growi-command';
-import { InvalidGrowiCommandError } from '~/models/errors';
+import { GrowiCommand } from '../interfaces/growi-command';
+import { InvalidGrowiCommandError } from '../models/errors';
 
 export const parse = (slashCommand: SlashCommand): GrowiCommand => {
-  const splitted = slashCommand.text.split(' ');
-  if (splitted.length < 2) {
+  console.log(slashCommand);
+  const splitted = slashCommand.command.split(' ');
+  if (splitted.length < 1) {
     throw new InvalidGrowiCommandError('The SlashCommand.text does not specify GrowiCommand type');
   }
 
   return {
     text: slashCommand.text,
-    growiCommandType: splitted[0],
-    growiCommandArgs: splitted.slice(1),
+    growiCommandType: 'hoge',
+    growiCommandArgs: ['fugo'],
   };
 };
