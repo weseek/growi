fafb9f7f4e788415e2438644df52d554f79dffa1
zahmis <meda829ratteru86@gmail.com>
2021-04-05 06:29:57 +0000

    fix type

--- packages/slackbot-proxy/src/controllers/slack.ts
+++ packages/slackbot-proxy/src/controllers/slack.ts
@@ -1,3 +1,4 @@
+import { SlashCommand } from '@slack/bolt';
 import {
   BodyParams, Controller, Get, Inject, Post, Req, Res,
 } from '@tsed/common';
@@ -58,7 +59,7 @@ export class SlackCtrl {
   }
 
   @Post('/events')
-  handleEvent(@BodyParams() body:{[key:string]:string}, @Res() res: Res): string {
+  handleEvent(@BodyParams() body:SlashCommand, @Res() res: Res): string {
     // Send response immediately to avoid opelation_timeout error
     // See https://api.slack.com/apis/connections/events-api#the-events-api__responding-to-events
 
