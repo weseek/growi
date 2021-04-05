3829449441124314bd3059a86fef5c2cd15017ff
zahmis <meda829ratteru86@gmail.com>
2021-04-05 03:42:12 +0000

    delete register.ts

--- packages/slackbot-proxy/src/services/RecieveService.ts
+++ packages/slackbot-proxy/src/services/RecieveService.ts
@@ -1,19 +1,13 @@
 import { Service } from '@tsed/di';
-import { openModal } from '../../../slack/src/services/register';
 
 @Service()
 export class ReceiveService {
 
-  receive(body) {
+  receiveContentsFromSlack(body) {
     if (body.text === 'register') {
       console.log(body);
-      return this.register(body);
     }
     return;
-  }
-
-  register(body) {
-    openModal(body);
   }
 
 }
