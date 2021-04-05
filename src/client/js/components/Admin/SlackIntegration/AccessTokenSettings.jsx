0d8c4b0f24332fb31f9ba192160ba6d6df9accde
深瀬スティーヴン <fukasesteven@gmail.com>
2021-04-05 17:07:39 +0900

    Update src/client/js/components/Admin/SlackIntegration/AccessTokenSettings.jsx
    Co-authored-by: itizawa <48426654+itizawa@users.noreply.github.com>

--- src/client/js/components/Admin/SlackIntegration/AccessTokenSettings.jsx
+++ src/client/js/components/Admin/SlackIntegration/AccessTokenSettings.jsx
@@ -32,8 +32,13 @@ const AccessTokenSettings = (props) => {
         <div className="form-group row my-5">
           <label className="text-left text-md-right col-md-3 col-form-label">Access Token</label>
           <div className="col-md-6">
-            <CopyToClipboard text={props.accessToken} onCopy={props.accessToken ? showCopiedToaster : null}>
-              <input className="form-control" type="text" value={props.accessToken} readOnly />
+{props.accessToken == null ? (
+  <input className="form-control" type="text" value={props.accessToken} readOnly />
+): (
+<CopyToClipboard text={props.accessToken} onCopy={showCopiedToaster}>
+<input className="form-control" type="text" value={props.accessToken} readOnly />
+</CopyToClipboard>
+)}
             </CopyToClipboard>
           </div>
         </div>
