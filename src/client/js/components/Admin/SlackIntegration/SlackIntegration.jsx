1ef88bec83427af0a1b0c8f3536ba6a9330a2f6e
Steven Fukase <fukasesteven@gmail.com>
2021-04-05 04:35:12 +0000

    Deleted console log

--- src/client/js/components/Admin/SlackIntegration/SlackIntegration.jsx
+++ src/client/js/components/Admin/SlackIntegration/SlackIntegration.jsx
@@ -22,7 +22,6 @@ const SlackIntegration = (props) => {
   const fetchData = useCallback(async() => {
     try {
       const response = await appContainer.apiv3.get('slack-integration/');
-      console.log(response.data.slackBotSettingParams);
       const { currentBotType, accessToken } = response.data.slackBotSettingParams;
       setCurrentBotType(currentBotType);
       setAccessToken(accessToken);
