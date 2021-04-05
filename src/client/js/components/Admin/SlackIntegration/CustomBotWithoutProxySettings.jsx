212e16454a1f1d84d7ff7217aaa3c42c1f9c4796
Shun Miyazawa <shun-m@weseek.co.jp>
2021-04-05 17:24:39 +0900

    Add isBoltSetup to state

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxySettings.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxySettings.jsx
@@ -21,6 +21,7 @@ const CustomBotWithoutProxySettings = (props) => {
   // get site name from this GROWI
   // eslint-disable-next-line no-unused-vars
   const [siteName, setSiteName] = useState('');
+  const [isBoltSetup, setIsBoltSetup] = useState(null);
   const currentBotType = 'custom-bot-without-proxy';
 
   const getSlackWSInWithoutProxy = useCallback(async() => {
@@ -45,12 +46,13 @@ const CustomBotWithoutProxySettings = (props) => {
       setSlackSigningSecretEnv(slackSigningSecretEnvVars);
       setSlackBotTokenEnv(slackBotTokenEnvVars);
       setSiteName(adminAppContainer.state.title);
-      console.log('isBoltSetup', isBoltSetup);
+      setIsBoltSetup(isBoltSetup);
     }
     catch (err) {
       toastError(err);
     }
   }, [appContainer, adminAppContainer]);
+
 
   useEffect(() => {
     fetchData();
