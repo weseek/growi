32c2098e4c41f6b56e39034c5fa08ff10764ea5c
zahmis <meda829ratteru86@gmail.com>
2021-04-06 11:58:59 +0000

    fix type

--- packages/slackbot-proxy/src/services/RecieveService.ts
+++ packages/slackbot-proxy/src/services/RecieveService.ts
@@ -1,11 +1,10 @@
 import { Service } from '@tsed/di';
-import { SlashCommand } from '@slack/bolt';
 import { parse } from '@growi/slack/src/utils/slash-command-parser';
 
 @Service()
 export class ReceiveService {
 
-  receiveContentsFromSlack(body:SlashCommand) : string {
+  receiveContentsFromSlack(body:{[key:string]:string}) : string {
     const parseBody = parse(body);
     if (parseBody.growiCommandType === 'register') {
       console.log('register action occured');
