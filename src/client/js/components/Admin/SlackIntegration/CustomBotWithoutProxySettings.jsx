3c2328d3768223340c68348a200540b92806023f
Steven Fukase <fukasesteven@gmail.com>
2021-04-06 09:32:10 +0000

    Fixed invalid accordion vars

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxySettings.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxySettings.jsx
@@ -168,11 +168,11 @@ const CustomBotWithoutProxySettings = (props) => {
                 className="btn btn-link btn-block d-flex justify-content-between"
                 type="button"
                 data-toggle="collapse"
-                data-target="#collapseOne"
-                aria-expanded="true"
-                aria-controls="collapseOne"
+                data-target="#collapseTwo"
+                aria-expanded="false"
+                aria-controls="collapseTwo"
               >
-                ① Botを作成する
+                ② BotをSlackにインストールする
                 <i className="fa fa-chevron-down"></i>
               </button>
             </h2>
@@ -198,39 +198,9 @@ const CustomBotWithoutProxySettings = (props) => {
                 className="btn btn-link btn-block d-flex justify-content-between"
                 type="button"
                 data-toggle="collapse"
-                data-target="#collapseOne"
-                aria-expanded="true"
-                aria-controls="collapseOne"
-              >
-                ② BotをSlackにインストールする
-                <i className="fa fa-chevron-down"></i>
-              </button>
-            </h2>
-          </div>
-
-          <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-            <div className="card-body">
-              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard
-              dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-              assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
-              butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably havent heard of them accusamus
-              labore sustainable VHS.
-            </div>
-          </div>
-
-        </div>
-
-        <div className="card mb-0">
-
-          <div className="card-header" id="headingThree">
-            <h2 className="mb-0">
-              <button
-                className="btn btn-link btn-block d-flex justify-content-between"
-                type="button"
-                data-toggle="collapse"
-                data-target="#collapseOne"
-                aria-expanded="true"
-                aria-controls="collapseOne"
+                data-target="#collapseThree"
+                aria-expanded="false"
+                aria-controls="collapseThree"
               >
                 ③ Signing SecretとBot Tokenを登録する
                 <i className="fa fa-chevron-down"></i>
@@ -252,15 +222,15 @@ const CustomBotWithoutProxySettings = (props) => {
 
         <div className="card mb-0">
 
-          <div className="card-header" id="headingThree">
+          <div className="card-header" id="headingFour">
             <h2 className="mb-0">
               <button
                 className="btn btn-link btn-block d-flex justify-content-between"
                 type="button"
                 data-toggle="collapse"
-                data-target="#collapseOne"
-                aria-expanded="true"
-                aria-controls="collapseOne"
+                data-target="#collapseFour"
+                aria-expanded="false"
+                aria-controls="collapseFour"
               >
                 ④ 連携状況のテストをする
                 <i className="fa fa-chevron-down"></i>
@@ -268,7 +238,7 @@ const CustomBotWithoutProxySettings = (props) => {
             </h2>
           </div>
 
-          <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+          <div id="collapseFour" className="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
             <div className="card-body">
               Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard
               dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
