b3b4cb1949446fd8374d3e4105421398a8fd1455
Yuki Takei <yuki@weseek.co.jp>
2021-04-08 13:42:09 +0000

    fix slack package build settings

--- packages/slackbot-proxy/src/services/RecieveService.ts
+++ packages/slackbot-proxy/src/services/RecieveService.ts
@@ -1,15 +1,15 @@
 import { Service } from '@tsed/di';
-// import { parse } from '@growi/slack/src/utils/slash-command-parser';
+import { parse } from '@growi/slack/lib/utils/slash-command-parser';
 
 @Service()
 export class ReceiveService {
 
   receiveContentsFromSlack(body:{[key:string]:string}) : string {
-    // const parseBody = parse(body);
-    // if (parseBody.growiCommandType === 'register') {
-    //   console.log('register action occured');
-    //   return 'register action occurd';
-    // }
+    const parseBody = parse(body);
+    if (parseBody.growiCommandType === 'register') {
+      console.log('register action occured');
+      return 'register action occurd';
+    }
     return 'return receiveContentsFromSlack';
   }
 
