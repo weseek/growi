c80023c5a3dd53fe43edbcc1a292bec084c90235
kaori <kaori@weseek.co.jp>
2021-04-12 22:03:21 +0900

    rename file and apply marks

--- src/client/js/components/Admin/SlackIntegration/SlackIntegration.jsx
+++ src/client/js/components/Admin/SlackIntegration/SlackIntegration.jsx
@@ -167,25 +167,19 @@ const SlackIntegration = (props) => {
               <div className="card-text">
                 <div className="my-2">
                   <div className="d-flex justify-content-between mb-3">
-                    <span>{t('admin:slack_integration.selecting_bot_types.set_up')}</span>
+                    {/* TODO change image of difficulties */}
+                    {/* <span>{t('admin:slack_integration.selecting_bot_types.set_up')}</span> */}
                     {/* <span className={`bot-type-disc-${value.setUp}`}>{t(`admin:slack_integration.selecting_bot_types.${value.setUp}`)}</span> */}
-                    <img className="bot-type-disc-easy" src="/images/slack-integration/batten-basic-red.png" alt="" />
 
 
                   </div>
                   <div className="d-flex justify-content-between mb-3">
                     <span>{t('admin:slack_integration.selecting_bot_types.multiple_workspaces_integration')}</span>
-                    {/* <span className={`bot-type-disc-${value.multiWSIntegration}`}>
-                      {t(`admin:slack_integration.selecting_bot_types.${value.multiWSIntegration}`)}
-                    </span> */}
-                    <img className="bot-type-disc-easy" src="/images/slack-integration/batten-basic-red.png" alt="" />
+                    <img className="bot-type-disc-easy" src={`/images/slack-integration/${value.multiWSIntegration}.png`} alt="" />
                   </div>
                   <div className="d-flex justify-content-between">
                     <span>{t('admin:slack_integration.selecting_bot_types.security_control')}</span>
-                    {/* <span className={`bot-type-disc-${value.securityControl}`}>
-                      {t(`admin:slack_integration.selecting_bot_types.${value.securityControl}`)}
-                    </span> */}
-                    <img className="bot-type-disc-easy" src="/images/slack-integration/batten-basic-red.png" alt="" />
+                    <img className="bot-type-disc-easy" src={`/images/slack-integration/${value.securityControl}.png`} alt="" />
                   </div>
                 </div>
               </div>
