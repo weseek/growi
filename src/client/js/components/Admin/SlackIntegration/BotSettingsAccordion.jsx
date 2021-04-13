453c8e069e630fe9e83544d99055fd93d2dffa2b
Steven Fukase <fukasesteven@gmail.com>
2021-04-13 09:33:45 +0000

    Added wrapper component

--- src/client/js/components/Admin/SlackIntegration/BotSettingsAccordion.jsx
+++ src/client/js/components/Admin/SlackIntegration/BotSettingsAccordion.jsx
@@ -3,44 +3,53 @@ import { Collapse } from 'reactstrap';
 import PropTypes from 'prop-types';
 
 const BotSettingsAccordion = (props) => {
-  if (props.onToggleAccordionHandler != null) {
-    onToggleAccordionHandler = props.onToggleAccordionHandler;
-  }  
-
   return (
     <div className="card border-0 rounded-lg shadow overflow-hidden">
-
-      <div className="card border-0 rounded-lg mb-0">
-        <div
-          className="card-header font-weight-normal py-3 d-flex justify-content-between"
-          role="button"
-          onClick={onToggleAccordionHandler}
-        >
-          <p className="mb-0 text-primary"><span className="mr-2">①</span>{props.title}</p>
-          {props.isActive
-            ? <i className="fa fa-chevron-up" />
-            : <i className="fa fa-chevron-down" />
-          }
-        </div>
-        <Collapse isOpen={props.isActive}>
-          <div className="card-body">
-            {props.children}
-          </div>
-        </Collapse>
-      </div>
-
-
-
+      {props.children}
     </div>
   );
-
 };
 
-BotSettingsAccordion.propTypes = {
+const BotSettingsAccordionItem = (props) => {
+  const onToggleAccordionHandler = () => {
+    if (props.onToggleAccordionHandler != null) {
+      props.onToggleAccordionHandler();
+    }
+  };
+
+  return (
+    <div className="card border-0 rounded-lg mb-0">
+      <div
+        className="card-header font-weight-normal py-3 d-flex justify-content-between"
+        role="button"
+        onClick={onToggleAccordionHandler}
+      >
+        <p className="mb-0 text-primary">{props.title}</p>
+        {props.isActive
+          ? <i className="fa fa-chevron-up" />
+          : <i className="fa fa-chevron-down" />
+        }
+      </div>
+      <Collapse isOpen={props.isActive}>
+        <div className="card-body">
+          {props.children}
+        </div>
+      </Collapse>
+    </div>
+  );
+};
+
+BotSettingsAccordion.item = BotSettingsAccordionItem;
+
+BotSettingsAccordionItem.propTypes = {
   isActive: PropTypes.bool,
   title: PropTypes.string.isRequired,
   children: PropTypes.element.isRequired,
   onToggleAccordionHandler: PropTypes.func,
+};
+
+BotSettingsAccordion.propTypes = {
+  children: PropTypes.element.isRequired,
 };
 
 export default BotSettingsAccordion;
