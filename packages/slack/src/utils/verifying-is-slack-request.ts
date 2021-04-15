c59f303d6d07fae08a5bbdfb62305f157dcc99ba
zahmis <meda829ratteru86@gmail.com>
2021-04-15 12:58:34 +0000

    add return type

--- packages/slack/src/utils/verifying-is-slack-request.ts
+++ packages/slack/src/utils/verifying-is-slack-request.ts
@@ -1,15 +1,12 @@
 import { createHmac, timingSafeEqual } from 'crypto';
 import { stringify } from 'qs';
-import {Request, Response, NextFunction} from 'express'
+import { Request, Response, NextFunction } from 'express';
 /**
    * Verify if the request came from slack
    * See: https://api.slack.com/authentication/verifying-requests-from-slack
    */
-interface signingSecret extends Request{
-  signingSecret: string
-}
 
-export const verifyingIsSlackRequest = (req: Request & {signingSecret?:string}, res: Response, next: NextFunction) => {
+export const verifyingIsSlackRequest = (req: Request & {signingSecret?:string}, res: Response, next: NextFunction):Record<string, any> | void => {
 
   if (req.signingSecret == null) {
     return res.send('No signing secret.');
