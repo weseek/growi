18fd00f89846df45d33a6e882af9c429fddc1699
Shun Miyazawa <shun-m@weseek.co.jp>
2021-04-15 13:53:23 +0900

    init

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithProxySettings.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithProxySettings.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import PropTypes from 'prop-types';
+import AppContainer from '../../../services/AppContainer';
+import AdminAppContainer from '../../../services/AdminAppContainer';
+import { withUnstatedContainers } from '../../UnstatedUtils';
 
-const CustomBotWithProxySettings = () => {
+const CustomBotWithProxySettings = (props) => {
+  // eslint-disable-next-line no-unused-vars
+  const { appContainer, adminAppContainer } = props;
+  const { t } = useTranslation();
 
   return (
     <div className="row my-5">
@@ -9,4 +17,12 @@ const CustomBotWithProxySettings = () => {
   );
 };
 
-export default CustomBotWithProxySettings;
+
+const CustomBotWithProxySettingsWrapper = withUnstatedContainers(CustomBotWithProxySettings, [AppContainer, AdminAppContainer]);
+
+CustomBotWithProxySettings.propTypes = {
+  appContainer: PropTypes.instanceOf(AppContainer).isRequired,
+  adminAppContainer: PropTypes.instanceOf(AdminAppContainer).isRequired,
+};
+
+export default CustomBotWithProxySettingsWrapper;
