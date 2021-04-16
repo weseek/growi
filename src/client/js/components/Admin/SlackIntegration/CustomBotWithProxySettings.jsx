680ba7e25f6321e0f118584b4e7c0a1098ded055
Shun Miyazawa <shun-m@weseek.co.jp>
2021-04-16 12:57:22 +0900

    cleanup

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithProxySettings.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithProxySettings.jsx
@@ -18,57 +18,46 @@ const CustomBotWithProxySettings = (props) => {
       <h2 className="admin-setting-header">{t('admin:slack_integration.custom_bot_with_proxy_integration')}</h2>
 
       <div className="d-flex justify-content-center my-5 bot-integration">
+
+        {/* card-slack */}
         <div className="card rounded shadow border-0 w-50 admin-bot-card">
           <h5 className="card-title font-weight-bold mt-3 ml-4">Slack</h5>
           <div className="card-body p-4"></div>
         </div>
 
-        <div className="text-center w-50 row">
 
-          {/* border */}
-          <hr className="border-danger align-self-center admin-border col"></hr>
+        {/* border */}
+        <hr className="border-danger align-self-center admin-border col"></hr>
 
-          {/* circle */}
-          <div className="align-self-center col">
-            <div className="circle-back">
-              <div className="circle-front mt-3">
-                <p className="text-light font-weight-bold m-0 pt-3 mt-2">Proxy</p>
-                <p className="text-light font-weight-bold">Server</p>
-              </div>
-            </div>
-          </div>
 
-          {/* border */}
-          <hr className="border-danger align-self-center admin-border col"></hr>
+        {/* sentence */}
+        {/* <p className="text-secondary m-0"><small>{t('admin:slack_integration.integration_sentence.integration_is_not_complete')}</small></p> */}
+        {/* <p className="text-secondary"><small>{t('admin:slack_integration.integration_sentence.proceed_with_the_following_integration_procedure')}</small></p> */}
 
-          {/* <p className="text-secondary m-0"><small>{t('admin:slack_integration.integration_sentence.integration_is_not_complete')}</small></p> */}
-          {/* <p className="text-secondary"><small>{t('admin:slack_integration.integration_sentence.proceed_with_the_following_integration_procedure')}</small></p> */}
-        </div>
 
+        {/* card-growi-app */}
         <div className="card rounded-lg shadow border-0 w-50 admin-bot-card">
           <h5 className="card-title font-weight-bold mt-3 ml-4">GROWI App</h5>
           <div className="card-body p-4 text-center">
             <a className="btn btn-primary mb-5">WESEEK Inner Wiki</a>
           </div>
         </div>
+
       </div>
 
-
-
-
+      
       <div className="row text-center bot-integration">
 
+        {/* border */}
         <hr className="border-danger align-self-center admin-border col"></hr>
 
-        <div className="align-self-center col">
-          <div className="circle-back">
-            <div className="circle-front mt-3">
-              <p className="text-light font-weight-bold m-0 pt-3 mt-2">Proxy</p>
-              <p className="text-light font-weight-bold">Server</p>
-            </div>
-          </div>
+        {/* cicle */}
+        <div className="circle text-center">
+          <p className="text-light font-weight-bold m-0 pt-3 mt-2">Proxy</p>
+          <p className="text-light font-weight-bold">Server</p>
         </div>
 
+        {/* border */}
         <hr className="border-danger align-self-center admin-border col"></hr>
       </div>
 
