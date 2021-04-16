b3100872d63f34a3ef33eccf2d20993a0701caf6
Steven Fukase <fukasesteven@gmail.com>
2021-04-16 04:30:43 +0000

    Init

---
+++ src/client/js/components/Admin/SlackIntegration/BotSettingsAccordion.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Collapse } from 'reactstrap';
+import PropTypes from 'prop-types';
+
+const BotSettingsAccordion = (props) => {
+  return (
+    <div className="card border-0 rounded-lg shadow overflow-hidden">
+      {props.children}
+    </div>
+  );
+};
+
+const BotSettingsAccordionItem = (props) => {
+  const onToggleAccordionHandler = () => {
+    if (props.onToggleAccordionHandler != null) {
+      props.onToggleAccordionHandler();
+    }
+  };
+
+  return (
+    <div className="card border-0 rounded-lg mb-0">
+      <div
+        className="card-header font-weight-normal py-3 d-flex justify-content-between"
+        role="button"
+        onClick={onToggleAccordionHandler}
+      >
+        <p className="mb-0 text-primary"><span className="mr-2">{props.itemNumber}</span>{props.title}</p>
+        {props.isActive
+          ? <i className="fa fa-chevron-up" />
+          : <i className="fa fa-chevron-down" />
+        }
+      </div>
+      <Collapse isOpen={props.isActive}>
+        <div className="card-body">
+          {props.children}
+        </div>
+      </Collapse>
+    </div>
+  );
+};
+
+BotSettingsAccordion.Item = BotSettingsAccordionItem;
+
+BotSettingsAccordionItem.propTypes = {
+  isActive: PropTypes.bool,
+  itemNumber: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  onToggleAccordionHandler: PropTypes.func,
+  children: PropTypes.node.isRequired,
+};
+
+BotSettingsAccordion.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default BotSettingsAccordion;
