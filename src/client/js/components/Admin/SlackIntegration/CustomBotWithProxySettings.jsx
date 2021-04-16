7cf7268d821ce8efe6d88c81ec890f218aba439f
Shun Miyazawa <shun-m@weseek.co.jp>
2021-04-16 16:25:03 +0900

    fix

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithProxySettings.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithProxySettings.jsx
@@ -25,42 +25,40 @@ const CustomBotWithProxySettings = (props) => {
           <div className="card-body p-4"></div>
         </div>
 
+        {/* center-content */}
+        <div className="text-center w-25 mb-5">
 
-        {/* border */}
-        <hr className="border-danger align-self-center admin-border col"></hr>
+          {/* sentence */}
+          <p className="text-secondary m-0"><small>{t('admin:slack_integration.integration_sentence.integration_is_not_complete')}</small></p>
+          <p className="text-secondary"><small>{t('admin:slack_integration.integration_sentence.proceed_with_the_following_integration_procedure')}</small></p>
 
+          <div className="row m-0">
 
-        {/* sentence */}
-        {/* <p className="text-secondary m-0"><small>{t('admin:slack_integration.integration_sentence.integration_is_not_complete')}</small></p> */}
-        {/* <p className="text-secondary"><small>{t('admin:slack_integration.integration_sentence.proceed_with_the_following_integration_procedure')}</small></p> */}
+            {/* border */}
+            <hr className="border-danger align-self-center admin-border col"></hr>
 
+            {/* circle */}
+            <div className="circle text-center">
+              <p className="text-light font-weight-bold m-0 pt-3 mt-2 col">Proxy</p>
+              <p className="text-light font-weight-bold">Server</p>
+            </div>
+
+            {/* border */}
+            <hr className="border-danger align-self-center admin-border col"></hr>
+
+          </div>
+
+        </div>
 
         {/* card-growi-app */}
         <div className="card rounded-lg shadow border-0 w-50 admin-bot-card">
           <h5 className="card-title font-weight-bold mt-3 ml-4">GROWI App</h5>
           <div className="card-body p-4 text-center">
-            <a className="btn btn-primary mb-5">WESEEK Inner Wiki</a>
+            <a className="btn btn-primary mt-3">WESEEK Inner Wiki</a>
           </div>
         </div>
 
       </div>
-
-      
-      <div className="row text-center bot-integration">
-
-        {/* border */}
-        <hr className="border-danger align-self-center admin-border col"></hr>
-
-        {/* cicle */}
-        <div className="circle text-center">
-          <p className="text-light font-weight-bold m-0 pt-3 mt-2">Proxy</p>
-          <p className="text-light font-weight-bold">Server</p>
-        </div>
-
-        {/* border */}
-        <hr className="border-danger align-self-center admin-border col"></hr>
-      </div>
-
 
       {/* ---------------  end  ---------------*/}
 
