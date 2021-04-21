665f402a4c4eb30c9c9e88bca7c913f46e5db6a4
Shun Miyazawa <shun-m@weseek.co.jp>
2021-04-21 17:42:14 +0900

    cleanup

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxySettings.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxySettings.jsx
@@ -58,15 +58,6 @@ const CustomBotWithoutProxySettings = (props) => {
           </div>
         </div>
 
-        {/* <div className="text-center w-25 mt-4"> */}
-          {/* <p className="text-secondary m-0"><small>{t('admin:slack_integration.integration_sentence.integration_is_not_complete')}</small></p> */}
-          {/* <p className="text-secondary"><small>{t('admin:slack_integration.integration_sentence.proceed_with_the_following_integration_procedure')}</small></p> */}
-          {/* <p className="text-secondary"><small>{t('admin:slack_integration.integration_sentence.integration_sucessed')}</small></p> */}
-          {/* <hr className="border-danger align-self-center admin-border"></hr> */}
-        {/* </div> */}
-
-        {/* ----------------- start ----------------- */}
-
         <div className="text-center w-25">
           {props.isSetupSlackBot && (
             <div className="mt-5">
@@ -82,8 +73,6 @@ const CustomBotWithoutProxySettings = (props) => {
             </div>
           )}
         </div>
-
-        {/* -----------------  end  ----------------- */}
 
         <div className="card rounded-lg shadow border-0 w-50 admin-bot-card">
           <h5 className="card-title font-weight-bold mt-3 ml-4">GROWI App</h5>
