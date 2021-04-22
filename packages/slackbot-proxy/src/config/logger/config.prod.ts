400ced826879e21a174868a252f41bb48e06ae13
Yuki Takei <yuki@weseek.co.jp>
2021-04-22 17:25:04 +0000

    setup universal-bunyan

---
+++ packages/slackbot-proxy/src/config/logger/config.prod.ts
@@ -0,0 +1,16 @@
+import { UniversalBunyanConfig } from 'universal-bunyan';
+
+const config: UniversalBunyanConfig = {
+  default: 'info',
+
+  // 'express-session': 'debug',
+
+  /*
+   * configure level for server
+   */
+  // 'express:*': 'debug',
+  // 'lunar:*': 'debug',
+
+};
+
+export default config;
