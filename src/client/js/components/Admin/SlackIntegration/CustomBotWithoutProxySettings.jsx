5edc9b9efaa09fbc9074fb8ee117fe74878b96cd
Shun Miyazawa <shun-m@weseek.co.jp>
2021-04-22 14:09:48 +0900

    fix lint

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxySettings.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxySettings.jsx
@@ -66,7 +66,7 @@ const CustomBotWithoutProxySettings = (props) => {
           )}
           {!props.isSetupSlackBot && (
             <div className="mt-4">
-              <p className="text-secondary m-0" dangerouslySetInnerHTML={{__html: `<small> ${t('admin:slack_integration.integration_sentence.integration_is_not_complete')} </small>`}}></p>
+              <p className="text-secondary m-0" dangerouslySetInnerHTML={{ __html: `<small> ${t('admin:slack_integration.integration_sentence.integration_is_not_complete')} </small>` }}></p>
               <hr className="align-self-center admin-border-danger border-danger"></hr>
             </div>
           )}
