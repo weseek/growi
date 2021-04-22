f8124306e292a1acbae40f269c7e0e7355a57809
zahmis <meda829ratteru86@gmail.com>
2021-04-22 09:44:02 +0000

    add log

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxySettingsAccordion.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxySettingsAccordion.jsx
@@ -39,6 +39,8 @@ const CustomBotWithoutProxySettingsAccordion = ({
         currentBotType,
       });
 
+      console.log('update secret', isConnectedToSlack);
+
       if (isConnectedToSlack) {
         onSetIsRegisterSlackCredentials(true);
       }
