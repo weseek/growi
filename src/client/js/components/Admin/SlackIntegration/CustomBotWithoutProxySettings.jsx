22c9f7823a9ba0d2bf9376a96762f91d172ad53f
Shun Miyazawa <shun-m@weseek.co.jp>
2021-04-22 19:34:07 +0900

    add card component, remove card

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxySettings.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxySettings.jsx
@@ -6,6 +6,7 @@ import AdminAppContainer from '../../../services/AdminAppContainer';
 import { withUnstatedContainers } from '../../UnstatedUtils';
 import { toastError } from '../../../util/apiNotification';
 import CustomBotWithoutProxySettingsAccordion, { botInstallationStep } from './CustomBotWithoutProxySettingsAccordion';
+import CustomBotWithouProxySettomgSlackCard from './CustomBotWithouProxySettingsCard';
 
 const CustomBotWithoutProxySettings = (props) => {
   const { appContainer } = props;
@@ -39,44 +40,13 @@ const CustomBotWithoutProxySettings = (props) => {
     <>
 
       <h2 className="admin-setting-header">{t('admin:slack_integration.custom_bot_without_proxy_integration')}</h2>
-
-      <div className="d-flex justify-content-center my-5 bot-integration">
-        <div className="card rounded shadow border-0 w-50 admin-bot-card">
-          <h5 className="card-title font-weight-bold mt-3 ml-4">Slack</h5>
-          <div className="card-body p-2 w-50 mx-auto">
-            {slackWSNameInWithoutProxy && (
-              <div className="card p-20 slack-work-space-name-card">
-                <div className="m-2 text-center">
-                  <h5 className="font-weight-bold">{ slackWSNameInWithoutProxy }</h5>
-                  <img width={20} height={20} src="/images/slack-integration/growi-bot-kun-icon.png" />
-                </div>
-              </div>
-            )}
-          </div>
-        </div>
-
-        <div className="text-center w-25">
-          {props.isSetupSlackBot && (
-            <div className="mt-5">
-              <p className="text-success"><small className="fa fa-check"> {t('admin:slack_integration.integration_sentence.integration_sucessed')}</small></p>
-              <hr className="align-self-center admin-border-success border-success"></hr>
-            </div>
-          )}
-          {!props.isSetupSlackBot && (
-            <div className="mt-4">
-              <small className="text-secondary m-0" dangerouslySetInnerHTML={{ __html: t('admin:slack_integration.integration_sentence.integration_is_not_complete') }} />
-              <hr className="align-self-center admin-border-danger border-danger"></hr>
-            </div>
-          )}
-        </div>
-
-        <div className="card rounded-lg shadow border-0 w-50 admin-bot-card">
-          <h5 className="card-title font-weight-bold mt-3 ml-4">GROWI App</h5>
-          <div className="card-body p-4 mb-5 text-center">
-            <div className="btn btn-primary">{ siteName }</div>
-          </div>
-        </div>
-      </div>
+      
+      <CustomBotWithouProxySettomgSlackCard
+        currentBotType={props.currentBotType}
+        siteName={siteName}
+        slackWSNameInWithoutProxy={slackWSNameInWithoutProxy}
+        isSetupSlackBot={props.isSetupSlackBot}
+      />
 
       <h2 className="admin-setting-header">{t('admin:slack_integration.custom_bot_without_proxy_settings')}</h2>
 
