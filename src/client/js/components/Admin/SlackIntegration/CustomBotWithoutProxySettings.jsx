771328594ea71e7e95ca66feef48c20e82cd4598
Yuki Takei <yuki@weseek.co.jp>
2021-04-22 17:52:58 +0000

    fix lint errors

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxySettings.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxySettings.jsx
@@ -64,7 +64,11 @@ const CustomBotWithoutProxySettings = (props) => {
           )}
           {!props.isSetupSlackBot && (
             <div className="mt-4">
-              <small className="text-secondary m-0" dangerouslySetInnerHTML={{ __html: t('admin:slack_integration.integration_sentence.integration_is_not_complete') }} />
+              <small
+                className="text-secondary m-0"
+                // eslint-disable-next-line react/no-danger
+                dangerouslySetInnerHTML={{ __html: t('admin:slack_integration.integration_sentence.integration_is_not_complete') }}
+              />
               <hr className="align-self-center admin-border-danger border-danger"></hr>
             </div>
           )}
