38bfbe93bbf54862d9f52a5a0a3ea72b49ecc877
Shun Miyazawa <shun-m@weseek.co.jp>
2021-04-25 22:24:47 +0900

    fix lint

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxySettingsAccordion.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxySettingsAccordion.jsx
@@ -63,11 +63,6 @@ const CustomBotWithoutProxySettingsAccordion = ({
     }
   };
 
-  const submitForm = (e)=> {
-    e.preventDefault();
-    onTestConnectionHandler();
-  };
-
   const onTestConnectionHandler = async() => {
     setConnectionErrorCode(null);
     setConnectionErrorMessage(null);
@@ -84,6 +79,11 @@ const CustomBotWithoutProxySettingsAccordion = ({
       setConnectionErrorCode(err[0].code);
       setConnectionErrorMessage(err[0].message);
     }
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    onTestConnectionHandler();
   };
 
   const inputTestChannelHandler = (channel) => {
@@ -164,7 +164,7 @@ const CustomBotWithoutProxySettingsAccordion = ({
       >
         <p className="text-center m-4">{t('admin:slack_integration.without_proxy.test_connection_by_pressing_button')}</p>
         <div className="d-flex justify-content-center">
-          <form className="form-row align-items-center w-25" onSubmit={ e => { submitForm(e) }}>
+          <form className="form-row align-items-center w-25" onSubmit={e => submitForm(e)}>
             <div className="col-8 input-group-prepend">
               <span className="input-group-text" id="slack-channel-addon"><i className="fa fa-hashtag" /></span>
               <input
