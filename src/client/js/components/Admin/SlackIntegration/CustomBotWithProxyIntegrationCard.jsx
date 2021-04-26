4122fde22b3f3fcf6612d96adc747951b7eb24a7
Shun Miyazawa <shun-m@weseek.co.jp>
2021-04-26 11:59:44 +0900

    add component

---
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithProxyIntegrationCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CustomBotWithProxyIntegrationCard = (props) => {
+
+  return (
+    <>
+
+      <p>CustomBotWithProxyIntegrationCard</p>
+
+    </>
+  );
+};
+
+export default CustomBotWithProxyIntegrationCard;
