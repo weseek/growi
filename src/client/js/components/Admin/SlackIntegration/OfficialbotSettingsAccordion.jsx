5f421cd8985d674357fc855c5dc8a52be85f61f3
kaori <kaori@weseek.co.jp>
2021-04-30 11:29:25 +0900

    add a todo comment

--- src/client/js/components/Admin/SlackIntegration/OfficialbotSettingsAccordion.jsx
+++ src/client/js/components/Admin/SlackIntegration/OfficialbotSettingsAccordion.jsx
@@ -10,18 +10,26 @@ const OfficialBotSettingsAccordion = () => {
       <Accordion
         title={<><span className="mr-2">①</span>{t('admin:slack_integration.accordion.install_bot_to_slack')}</>}
       >
+        {/* TODO: GW-5824 add accordion contents  */}
+        hoge
       </Accordion>
       <Accordion
         title={<><span className="mr-2">②</span>{t('admin:slack_integration.accordion.register_official_bot_proxy_service')}</>}
       >
+        {/* TODO: GW-5824 add accordion contents  */}
+        hoge
       </Accordion>
       <Accordion
         title={<><span className="mr-2">③</span>{t('admin:slack_integration.accordion.register_proxy_url')}</>}
       >
+        {/* TODO: GW-5824 add accordion contents  */}
+        hoge
       </Accordion>
       <Accordion
         title={<><span className="mr-2">④</span>{t('admin:slack_integration.accordion.test_connection')}</>}
       >
+        {/* TODO: GW-5824 add accordion contents  */}
+        hoge
       </Accordion>
     </div>
   );
