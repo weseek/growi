c467c9fa1384b5d49e7d1271d29a10be6a79c9aa
Steven Fukase <fukasesteven@gmail.com>
2021-04-30 09:31:11 +0000

    lint fix

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxyIntegrationCard.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxyIntegrationCard.jsx
@@ -18,7 +18,7 @@ const CustomBotWithoutProxyIntegrationCard = (props) => {
               <img width={20} height={20} src="/images/slack-integration/growi-bot-kun-icon.png" />
             </div>
           </div>
-          )}
+            )}
         </div>
       </div>
 
@@ -38,7 +38,7 @@ const CustomBotWithoutProxyIntegrationCard = (props) => {
         <div className="mt-4">
           <small
             className="text-secondary m-0"
-              // eslint-disable-next-line react/no-danger
+                // eslint-disable-next-line react/no-danger
             dangerouslySetInnerHTML={{ __html: t('admin:slack_integration.integration_sentence.integration_is_not_complete') }}
           />
           <hr className="align-self-center admin-border-danger border-danger"></hr>
