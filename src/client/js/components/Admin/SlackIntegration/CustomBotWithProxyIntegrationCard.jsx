70681429cd2fb808f7e5547508235e4ad21837ef
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-06 13:50:45 +0900

    fix padding

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithProxyIntegrationCard.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithProxyIntegrationCard.jsx
@@ -12,7 +12,7 @@ const CustomBotWithProxyIntegrationCard = (props) => {
 
         <div className="card rounded shadow border-0 w-50 admin-bot-card">
           <h5 className="card-title font-weight-bold mt-3 ml-4">Slack</h5>
-          <div className="card-body p-4">
+          <div className="card-body px-5">
             {props.slackWSNameInWithProxy != null && (
               <div className="card slack-work-space-name-card">
                 <div className="m-2 text-center">
