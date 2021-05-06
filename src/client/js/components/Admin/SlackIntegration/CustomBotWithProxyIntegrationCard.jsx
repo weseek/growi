a1948888cfa207bd6202fc9da2dcbbfc02f31a67
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-06 18:02:51 +0900

    change process

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithProxyIntegrationCard.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithProxyIntegrationCard.jsx
@@ -11,7 +11,7 @@ const CustomBotWithProxyIntegrationCard = (props) => {
       <div className="card rounded shadow border-0 w-50 admin-bot-card">
         <h5 className="card-title font-weight-bold mt-3 ml-4">Slack</h5>
         <div className="card-body px-5">
-          {props.slackWSNameInWithProxy[0] != null && (
+          {props.slackWSNameInWithProxy.length !== 0 && (
             <div className="card slack-work-space-name-card">
               <div className="m-2 text-center">
                 <h5 className="font-weight-bold">{props.slackWSNameInWithProxy[0]}</h5>
