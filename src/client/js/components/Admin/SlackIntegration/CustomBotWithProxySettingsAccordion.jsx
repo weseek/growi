9f4b50ae9d83c150131355b67c7e0ca5f996243a
Steven Fukase <fukasesteven@gmail.com>
2021-05-06 02:15:24 +0000

    Added translation

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithProxySettingsAccordion.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithProxySettingsAccordion.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import 
+import { useTranslation } from 'react-i18next';
 import Accordion from '../Common/Accordion';
 
 const CustomBotWithProxySettingsAccordion = () => {
-
+  const { t } = useTranslation();
   return (
     <div className="card border-0 rounded-lg shadow overflow-hidden">
       <Accordion
-        title={<><span className="mr-2">①</span>First Accordion</>}
+        title={<><span className="mr-2">①</span>{t('admin:slack_integration.accordion.create_bot')}</>}
       >
         <div className="row my-5">
           <div className="mx-auto">
@@ -30,13 +30,14 @@ const CustomBotWithProxySettingsAccordion = () => {
         </div>
       </Accordion>
       <Accordion
-        title={<><span className="mr-2">②</span>Second Accordion</>}
+        title={<><span className="mr-2">②</span>{t('admin:slack_integration.accordion.install_bot_to_slack')}</>}
       >
         <div className="row my-5">
           <div className="mx-auto">
             <div>
+              {/* TODO: Insert install link */}
               <button type="button" className="btn btn-primary text-nowrap mx-1" onClick={() => window.open('https://api.slack.com/apps', '_blank')}>
-                {t('admin:slack_integration.accordion.create_bot')}
+                {t('admin:slack_integration.accordion.install_now')}
                 <i className="fa fa-external-link ml-2" aria-hidden="true" />
               </button>
             </div>
@@ -44,7 +45,7 @@ const CustomBotWithProxySettingsAccordion = () => {
             <a href="#">
               <p className="text-center mt-1">
                 <small>
-                  {t('admin:slack_integration.accordion.how_to_create_a_bot')}
+                  {t('admin:slack_integration.accordion.how_to_install')}
                   <i className="fa fa-external-link ml-2" aria-hidden="true" />
                 </small>
               </p>
