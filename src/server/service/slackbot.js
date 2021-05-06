52599c646b118af300dd1a13da2337248e6d2b46
zahmis <meda829ratteru86@gmail.com>
2021-05-06 02:24:38 +0000

    restore isSetupSlackBot

--- src/server/service/slackbot.js
+++ src/server/service/slackbot.js
@@ -19,6 +19,7 @@ class SlackBotService extends S2sMessageHandlable {
     this.client = null;
     this.searchService = null;
 
+    this.isSetupSlackBot = false;
     this.isConnectedToSlack = false;
 
     this.lastLoadedAt = null;
@@ -27,12 +28,14 @@ class SlackBotService extends S2sMessageHandlable {
   }
 
   async initialize() {
+    this.isSetupSlackBot = false;
     this.isConnectedToSlack = false;
     const token = this.crowi.configManager.getConfig('crowi', 'slackbot:token');
 
     if (token != null) {
       this.client = new WebClient(token, { logLevel: LogLevel.DEBUG });
       logger.debug('SlackBot: setup is done');
+      this.isSetupSlackBot = true;
       await this.sendAuthTest();
     }
 
