89f8be009e96b00ef8a4c077f71b2cd8e7db6cc3
Steven Fukase <fukasesteven@gmail.com>
2021-05-07 09:40:24 +0000

    Commented out for testing

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxySettingsAccordion.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxySettingsAccordion.jsx
@@ -181,9 +181,11 @@ const CustomBotWithoutProxySettingsAccordion = ({
             </div>
           </form>
         </div>
-        {connectionErrorMessage != null
+        {/* {connectionErrorMessage != null */}
+        {connectionErrorMessage == null
           && <p className="text-danger text-center my-4">{t('admin:slack_integration.accordion.error_check_logs_below')}</p>}
-        {connectionSuccessMessage != null
+        {/* {connectionSuccessMessage != null */}
+        {connectionSuccessMessage == null
           && <p className="text-info text-center my-4">{t('admin:slack_integration.accordion.send_message_to_slack_work_space')}</p>}
         <form>
           <div className="row my-3 justify-content-center">
