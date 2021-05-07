846846b1ac1e4d572ceba4492eb7183cdcc6cd9f
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-07 18:41:33 +0900

    add header

--- src/client/js/components/Admin/SlackIntegration/OfficialBotSettings.jsx
+++ src/client/js/components/Admin/SlackIntegration/OfficialBotSettings.jsx
@@ -7,6 +7,8 @@ const OfficialBotSettings = () => {
 
   return (
     <>
+      <h2 className="admin-setting-header">{t('admin:slack_integration.official_bot_integration')}</h2>
+
       <h2 className="admin-setting-header">{t('admin:slack_integration.official_bot_settings')}</h2>
 
       <div className="my-5 mx-3">
