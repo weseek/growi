302fa882ff027a5d49891943f745079f7eeaca02
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-07 18:43:41 +0900

    success show integration ui

--- src/client/js/components/Admin/SlackIntegration/OfficialBotSettings.jsx
+++ src/client/js/components/Admin/SlackIntegration/OfficialBotSettings.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import OfficialBotSettingsAccordion from './OfficialbotSettingsAccordion';
+import CustomBotWithProxyIntegrationCard from './CustomBotWithProxyIntegrationCard';
 
 const OfficialBotSettings = () => {
   const { t } = useTranslation();
@@ -8,6 +9,13 @@ const OfficialBotSettings = () => {
   return (
     <>
       <h2 className="admin-setting-header">{t('admin:slack_integration.official_bot_integration')}</h2>
+
+        {/* TODO delete tmp props */}
+        <CustomBotWithProxyIntegrationCard
+          siteNames={['siteName1', 'siteName2', 'siteName3']}
+          slackWSNameInWithProxy={['wsName1', 'wsName2']}
+          isSlackScopeSet
+        />
 
       <h2 className="admin-setting-header">{t('admin:slack_integration.official_bot_settings')}</h2>
 
