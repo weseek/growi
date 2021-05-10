1e9f66ca23439718dbd39fcfc83c26a294e290a7
Steven Fukase <fukasesteven@gmail.com>
2021-05-10 08:49:56 +0000

    copied from old branch

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithProxySettings.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithProxySettings.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 import AppContainer from '../../../services/AppContainer';
@@ -12,9 +12,16 @@ const CustomBotWithProxySettings = (props) => {
   const { appContainer, adminAppContainer } = props;
   const { t } = useTranslation();
 
+  // TODO: Multiple accordion logic
+  const [accordionComponentsArray, setAccordionComponentsArray] = useState(0);
+  const addAccordionHandler = () => {
+    setAccordionComponentsArray(
+      prevState => prevState + 1,
+    );
+  };
+
   return (
     <>
-
       <h2 className="admin-setting-header mb-2">{t('admin:slack_integration.custom_bot_with_proxy_integration')}</h2>
 
       {/* TODO delete tmp props */}
@@ -33,8 +40,23 @@ const CustomBotWithProxySettings = (props) => {
           </button>
         </div>
 
-        <div className="d-flex flex-column my-5 w-100">
-          <CustomBotWithProxySettingsAccordion />
+        {/* // TODO: Multiple accordion logic */}
+        {Array(...Array(accordionComponentsArray)).map(i => (
+          <div className="d-flex flex-column my-5 w-100">
+            <CustomBotWithProxySettingsAccordion />
+          </div>
+        ))}
+
+        {/* TODO: Disable button when integration is incomplete */}
+        {/* TODO: i18n */}
+        <div className="row justify-content-center my-5">
+          <button
+            type="button"
+            className="btn btn-outline-primary"
+            onClick={addAccordionHandler}
+          >
+            + Slackワークスペースを追加
+          </button>
         </div>
       </div>
     </>
