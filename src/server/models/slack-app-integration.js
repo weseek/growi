924fdc0d2ff1ab07ed8e14ea1a4fb90bd209fcf6
Yuki Takei <yuki@weseek.co.jp>
2021-05-10 12:21:37 +0000

    refactor slack-app-integration.js

--- src/server/models/slack-app-integration.js
+++ src/server/models/slack-app-integration.js
@@ -2,9 +2,9 @@ module.exports = function(crowi) {
   const mongoose = require('mongoose');
 
   const slackAppIntegrationSchema = new mongoose.Schema({
-    accessTokenForGrowi: { type: String, required: true, unique: true },
-    accessTokenForProxy: { type: String, required: true, unique: true },
+    tokenGtoP: { type: String, required: true, unique: true },
+    tokenPtoG: { type: String, required: true, unique: true },
   });
 
-  return mongoose.model('SlackAppIntegrationSchema', slackAppIntegrationSchema);
+  return mongoose.model('SlackAppIntegration', slackAppIntegrationSchema);
 };
