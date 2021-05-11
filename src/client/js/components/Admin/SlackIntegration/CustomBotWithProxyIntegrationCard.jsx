68573127765caac614170c36c393d991e84f145b
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-11 15:11:49 +0900

    rename

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithProxyIntegrationCard.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithProxyIntegrationCard.jsx
@@ -11,7 +11,7 @@ const CustomBotWithProxyIntegrationCard = (props) => {
       <div className="card rounded shadow border-0 w-50 admin-bot-card">
         <h5 className="card-title font-weight-bold mt-3 ml-4">Slack</h5>
         <div className="card-body px-5">
-          {props.slackWorkSpaceNames.map((slackWorkSpaceName) => {
+          {props.slackWorkSpaces.map((slackWorkSpaceName) => {
             return (
               <div key={slackWorkSpaceName.name} className={slackWorkSpaceName.active ? 'card slack-work-space-name-card' : ''}>
                 <div className="m-2 text-center">
@@ -76,7 +76,7 @@ const CustomBotWithProxyIntegrationCard = (props) => {
 
 CustomBotWithProxyIntegrationCard.propTypes = {
   siteNames: PropTypes.array.isRequired,
-  slackWorkSpaceNames: PropTypes.array,
+  slackWorkSpaces: PropTypes.array,
   isSlackScopeSet: PropTypes.bool,
 };
 
