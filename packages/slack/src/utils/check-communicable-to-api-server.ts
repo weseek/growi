5ea04aa0d38b040d09638d5201ffe97865f2071d
Yuki Takei <yuki@weseek.co.jp>
2021-05-11 11:10:12 +0000

    set timeout to checkCommunicableToApiServer

--- packages/slack/src/utils/check-communicable-to-api-server.ts
+++ packages/slack/src/utils/check-communicable-to-api-server.ts
@@ -1,8 +1,14 @@
 import axios, { AxiosError } from 'axios';
 
+/**
+ * Check whether the HTML server responds or not.
+ *
+ * @param serverUri Server URI to connect
+ * @returns AxiosError when error is occured
+ */
 export const checkCommunicableToApiServer = async(serverUri = 'https://slack.com/api/'): Promise<void|AxiosError> => {
   try {
-    await axios.get(serverUri, { maxRedirects: 0 });
+    await axios.get(serverUri, { maxRedirects: 0, timeout: 3000 });
   }
   catch (err) {
     return err as AxiosError;
