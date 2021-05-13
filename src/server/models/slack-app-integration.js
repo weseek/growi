4c9c0f059200d4d8b5a31c7307810a462b17f574
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-13 18:51:26 +0900

    fix generateAccessToken func

--- src/server/models/slack-app-integration.js
+++ src/server/models/slack-app-integration.js
@@ -7,11 +7,11 @@ const schema = new mongoose.Schema({
 });
 class SlackAppIntegration {
 
-  static generateAccessToken(user) {
+  static generateAccessToken() {
     const hasher1 = crypto.createHash('sha512');
     const hasher2 = crypto.createHash('sha512');
-    const tokenGtoP = hasher1.update(new Date().getTime() + user._id).digest('base64');
-    const tokenPtoG = hasher2.update(new Date().getTime() + user.username).digest('base64');
+    const tokenGtoP = hasher1.update(`${new Date().getTime().toString()}proxy`).digest('base64');
+    const tokenPtoG = hasher2.update(`${new Date().getTime().toString()}growi`).digest('base64');
     return [tokenGtoP, tokenPtoG];
   }
 
