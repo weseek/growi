d52ecedb55b370130871d595c3696b095a3190b5
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-13 12:18:05 +0900

    defined class

--- src/server/models/slack-app-integration.js
+++ src/server/models/slack-app-integration.js
@@ -1,6 +1,19 @@
+const crypto = require('crypto');
+
+class SlackAppIntegration {
+
+  static generateAccessToken(user) {
+    const hasher1 = crypto.createHash('sha512');
+    const hasher2 = crypto.createHash('sha512');
+    const growi = hasher1.update(new Date().getTime() + user._id);
+    const proxy = hasher2.update(new Date().getTime() + user.username);
+    return [growi.digest('base64'), proxy.digest('base64')];
+  }
+
+}
+
 module.exports = function(crowi) {
   const mongoose = require('mongoose');
-  const crypto = require('crypto');
 
   const slackAppIntegrationSchema = new mongoose.Schema({
     tokenGtoP: { type: String, required: true, unique: true },
