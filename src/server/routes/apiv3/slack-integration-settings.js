7aa10b5ef1bb258d0466acf0fcb665b64313ddb6
zahmis <meda829ratteru86@gmail.com>
2021-05-14 06:29:09 +0000

    del: function

--- src/server/routes/apiv3/slack-integration-settings.js
+++ src/server/routes/apiv3/slack-integration-settings.js
@@ -82,14 +82,6 @@ module.exports = (crowi) => {
     return configManager.updateConfigsInTheSameNamespace('crowi', params, true);
   }
 
-  // eslint-disable-next-line no-unused-vars
-  function generateAccessToken(user) {
-    const hasher = crypto.createHash('sha512');
-    hasher.update(new Date().getTime() + user._id);
-
-    return hasher.digest('base64');
-  }
-
   async function getConnectionStatusesFromProxy(tokens) {
     const csv = tokens.join(',');
 
