5fa87ebcf1113bae1e09c1f269a7f0ef9a31b7a4
zahmis <meda829ratteru86@gmail.com>
2021-05-17 06:07:48 +0000

    add midleware

--- src/server/routes/apiv3/slack-integration-settings.js
+++ src/server/routes/apiv3/slack-integration-settings.js
@@ -352,7 +352,7 @@ module.exports = (crowi) => {
    *          200:
    *            description: Succeeded to delete access tokens for slack
    */
-  router.delete('/access-tokens', async(req, res) => {
+  router.delete('/access-tokens', accessTokenParser, loginRequiredStrictly, adminRequired, csrf, async(req, res) => {
     const SlackAppIntegration = mongoose.model('SlackAppIntegration');
     const { tokenGtoP, tokenPtoG } = req.body;
     try {
