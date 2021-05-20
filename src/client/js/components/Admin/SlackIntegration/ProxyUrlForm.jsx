2e42ff6f7282c22ce88e3d2cba947dc0b16cfeb7
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-20 14:17:10 +0900

    delete fragment

--- src/client/js/components/Admin/SlackIntegration/ProxyUrlForm.jsx
+++ src/client/js/components/Admin/SlackIntegration/ProxyUrlForm.jsx
@@ -25,23 +25,21 @@ const ProxyUrlFrom = (props) => {
   };
 
   return (
-    <>
-      <div className="form-group row my-4">
-        <label className="text-left text-md-right col-md-3 col-form-label mt-3">Proxy URL</label>
-        <div className="col-md-6 mt-3">
-          <input
-            className="form-control"
-            type="text"
-            name="settingForm[proxyUrl]"
-            defaultValue={props.proxyUri}
-            onChange={(e) => { props.setProxyUri(e.target.value) }}
-          />
-        </div>
-        <div className="col-md-2 mt-3 text-center text-md-left">
-          <button type="button" className="btn btn-primary" onClick={updateProxyUri} disabled={false}>{ t('Update') }</button>
-        </div>
+    <div className="form-group row my-4">
+      <label className="text-left text-md-right col-md-3 col-form-label mt-3">Proxy URL</label>
+      <div className="col-md-6 mt-3">
+        <input
+          className="form-control"
+          type="text"
+          name="settingForm[proxyUrl]"
+          defaultValue={props.proxyUri}
+          onChange={(e) => { props.setProxyUri(e.target.value) }}
+        />
       </div>
-    </>
+      <div className="col-md-2 mt-3 text-center text-md-left">
+        <button type="button" className="btn btn-primary" onClick={updateProxyUri} disabled={false}>{ t('Update') }</button>
+      </div>
+    </div>
   );
 };
 
