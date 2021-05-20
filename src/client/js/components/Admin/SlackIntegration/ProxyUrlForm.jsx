343e7c7c623a1ced83186636278395c1aeb74144
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-20 12:09:18 +0900

    add props

--- src/client/js/components/Admin/SlackIntegration/ProxyUrlForm.jsx
+++ src/client/js/components/Admin/SlackIntegration/ProxyUrlForm.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ProxyUrlFrom = (props) => {
   return (
-    <>
-      <span>ProxyUrlForm</span>
-    </>
+    <span>ProxyUrlForm</span>
   );
+};
+
+
+ProxyUrlFrom.propTypes = {
+  proxyUri: PropTypes.string.isRequired,
+  setProxyUrl: PropTypes.func.isRequired,
+  updateProxyUri: PropTypes.func.isRequired,
 };
 
 export default ProxyUrlFrom;
