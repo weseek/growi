ac6e3be582fffb68f2be457572de578e53944a31
Shun Miyazawa <shun-m@weseek.co.jp>
2021-05-24 13:00:17 +0900

    fix

--- src/client/js/components/Admin/SlackIntegration/WithProxyAccordions.jsx
+++ src/client/js/components/Admin/SlackIntegration/WithProxyAccordions.jsx
@@ -54,25 +54,18 @@ export const BotInstallProcess = () => {
 export const RegisteringProxyUrlProcess = () => {
   const { t } = useTranslation();
   return (
-    <div className="p-4 d-flex flex-column align-items-center">
-      <ol>
-        <li>
-          <p
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{ __html: t('admin:slack_integration.accordion.copy_proxy_url') }}
-          />
-          <img className="mb-3 border border-light img-fluid" src="/images/slack-integration/growi-register-sentence.png" />
-        </li>
-
-        <li>
-          <span
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{ __html: t('admin:slack_integration.accordion.enter_proxy_url_and_update') }}
-          />
-          <p className="text-danger">{t('admin:slack_integration.accordion.dont_need_update')}</p>
-          <img className="mb-3 w-75 border border-light img-fluid" src="/images/slack-integration/growi-set-proxy-url.png" />
-        </li>
-      </ol>
+    <div className="container w-75 py-5">
+      <p
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: t('admin:slack_integration.accordion.copy_proxy_url') }}
+      />
+      <img className="mb-5 border border-light img-fluid" src="/images/slack-integration/growi-register-sentence.png" />
+      <span
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: t('admin:slack_integration.accordion.enter_proxy_url_and_update') }}
+      />
+      <p className="text-danger">{t('admin:slack_integration.accordion.dont_need_update')}</p>
+      <img className="mb-3 border border-light img-fluid" src="/images/slack-integration/growi-set-proxy-url.png" />
     </div>
   );
 };
