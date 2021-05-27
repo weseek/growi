2b5af878c95347a97143d83bcb542e12010d6672
Yuki Takei <yuki@weseek.co.jp>
2021-05-27 09:43:42 +0000

    fix postResult for building docker image

--- packages/slackbot-proxy/src/controllers/growi-to-slack.ts
+++ packages/slackbot-proxy/src/controllers/growi-to-slack.ts
@@ -3,7 +3,7 @@ import {
 } from '@tsed/common';
 import axios from 'axios';
 
-import { WebAPICallResult } from '@slack/web-api';
+import { WebAPICallOptions, WebAPICallResult } from '@slack/web-api';
 
 import {
   verifyGrowiToSlackRequest, getConnectionStatuses, testToSlack, generateWebClient,
@@ -190,10 +190,17 @@ export class GrowiToSlackCtrl {
     }
 
     const client = generateWebClient(token);
-    await client.chat.postMessage({
-      channel: req.body.channel,
-      blocks: req.body.blocks,
-    });
+
+    try {
+      // TODO: GW-6133
+      const opt = req.body as WebAPICallOptions;
+      await client.apiCall('put', opt);
+    }
+    catch (err) {
+      // TODO: GW-6133
+      // logger.error()
+      return res.status(500).send({ message: err.message });
+    }
 
     logger.debug('postMessage is success');
 
