0a55218efab75c2d6f6eb1736ab4b713220eb1ed
Steven Fukase <fukasesteven@gmail.com>
2021-05-27 18:51:26 +0900

    revert "Merge branch 'imprv/6083-show-accordion' of https://github.com/weseek/growi into feat/growi-bot"
    This reverts commit cb2eb51ea503b8734c86bc5b42ecc6ed61aa1475, reversing
    changes made to dfe4d8b4c7bd2a60baef90e1e0a53192976cdfb6.

--- src/client/js/components/Admin/SlackIntegration/SlackIntegration.jsx
+++ src/client/js/components/Admin/SlackIntegration/SlackIntegration.jsx
@@ -122,12 +122,7 @@ const SlackIntegration = (props) => {
 
   switch (currentBotType) {
     case 'officialBot':
-      settingsComponent = (
-        <OfficialBotSettings
-          slackAppIntegrations={slackAppIntegrations}
-          proxyServerUri={proxyServerUri}
-        />
-      );
+      settingsComponent = <OfficialBotSettings slackAppIntegrations={slackAppIntegrations} proxyServerUri={proxyServerUri} />;
       break;
     case 'customBotWithoutProxy':
       settingsComponent = (
