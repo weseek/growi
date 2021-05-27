98006022980a410251860a20bff4da227204dd80
kaori <kaori@weseek.co.jp>
2021-05-27 20:21:00 +0900

    improved toastSuccess

--- src/client/js/components/Admin/SlackIntegration/SlackIntegration.jsx
+++ src/client/js/components/Admin/SlackIntegration/SlackIntegration.jsx
@@ -82,7 +82,7 @@ const SlackIntegration = (props) => {
     try {
       await appContainer.apiv3.put('/slack-integration-settings/slack-app-integrations');
       fetchSlackIntegrationData();
-      // toastSuccess(t('admin:slack_integration.bot_reset_successful'));
+      toastSuccess(t('admin:slack_integration.bot_reset_successful'));
     }
     catch (error) {
       toastError(error);
