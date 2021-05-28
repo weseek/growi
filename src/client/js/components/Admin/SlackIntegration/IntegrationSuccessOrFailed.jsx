bac5c10b3b548bb86f027204f2174adc2e205d76
zahmis <meda829ratteru86@gmail.com>
2021-05-28 08:48:11 +0000

    adjust

--- src/client/js/components/Admin/SlackIntegration/IntegrationSuccessOrFailed.jsx
+++ src/client/js/components/Admin/SlackIntegration/IntegrationSuccessOrFailed.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
-
 import { UncontrolledTooltip } from 'reactstrap';
 
 import ConductionStatusHr from './ConductionStatusHr';
