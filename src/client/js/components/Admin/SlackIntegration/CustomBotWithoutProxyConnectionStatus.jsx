edd2db6a20ae3371ee9bc8940ba833488928a1d1
zahmis <meda829ratteru86@gmail.com>
2021-05-31 07:44:24 +0000

    fix

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxyConnectionStatus.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxyConnectionStatus.jsx
@@ -20,7 +20,7 @@ const CustomBotWithoutProxyConnectionStatus = (props) => {
       <div className="card rounded shadow border-0 w-50 admin-bot-card mb-0">
         <h5 className="card-title font-weight-bold mt-3 ml-4">Slack</h5>
         <div className="card-body p-2 w-50 mx-auto">
-          {totalCount > 0 ? '' : (
+          {totalCount > 0 ? (
             <div className="card slack-work-space-name-card">
               <div className="m-2 text-center">
                 <h5 className="font-weight-bold">
@@ -29,7 +29,7 @@ const CustomBotWithoutProxyConnectionStatus = (props) => {
                 <img width={20} height={20} src="/images/slack-integration/growi-bot-kun-icon.png" />
               </div>
             </div>
-         )}
+         ) : ''}
         </div>
       </div>
 
