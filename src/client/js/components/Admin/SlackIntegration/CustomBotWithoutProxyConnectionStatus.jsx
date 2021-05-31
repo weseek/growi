ac3aa001e1f3f2531d1455dd901a962d50151462
zahmis <meda829ratteru86@gmail.com>
2021-05-31 09:37:26 +0000

    fix

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxyConnectionStatus.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxyConnectionStatus.jsx
@@ -8,7 +8,7 @@ const CustomBotWithoutProxyConnectionStatus = (props) => {
   const connectionStatusValues = Object.values(connectionStatuses); // type: ConnectionStatus[]
 
   const totalCount = connectionStatusValues.length;
-  const errorCount = connectionStatusValues.filter(connectionStatusValue => connectionStatusValue.error != null).length;
+  const errorCount = connectionStatusValues.filter(connectionStatus => connectionStatus.error != null).length;
 
   let workspaceName;
   if (totalCount > 0) {
