5febe1e8b4a4f72531dbbdf9e34620f6d01fec33
itizawa <itizawa@weseek.co.jp>
2021-06-01 01:28:42 +0000

    put back

--- config/env.dev.js
+++ config/env.dev.js
@@ -4,7 +4,7 @@ module.exports = {
   // MONGO_GRIDFS_TOTAL_LIMIT: 10485760,   // 10MB
   MATHJAX: 1,
   // NO_CDN: true,
-  MONGO_URI: 'mongodb://mongo:27017/growi11',
+  MONGO_URI: 'mongodb://mongo:27017/growi',
   // REDIS_URI: 'http://redis:6379',
   // NCHAN_URI: 'http://nchan',
   ELASTICSEARCH_URI: 'http://elasticsearch:9200/growi',
