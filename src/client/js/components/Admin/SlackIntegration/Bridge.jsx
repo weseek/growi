576d11479582c20623ed2e024367e0a935fdaa82
Shun Miyazawa <shun-m@weseek.co.jp>
2021-06-02 03:49:52 +0000

    Replace border-radius: 50% and rounded-circle

--- src/client/js/components/Admin/SlackIntegration/Bridge.jsx
+++ src/client/js/components/Admin/SlackIntegration/Bridge.jsx
@@ -5,7 +5,7 @@ import { UncontrolledTooltip } from 'reactstrap';
 
 const ProxyCircle = () => (
   <div className="grw-bridge-proxy-circle">
-    <div className="circle position-absolute bg-primary border-light">
+    <div className="circle position-absolute bg-primary border-light rounded-circle">
       <p className="circle-inner text-light font-weight-bold d-none d-lg-inline">Proxy Server</p>
       <p className="circle-inner mt-5 d-block d-lg-none">ProxyServer</p>
     </div>
