3dac472e874161d41d00ca31316b082c599d49c1
Shun Miyazawa <shun-m@weseek.co.jp>
2021-06-03 08:06:25 +0000

    fix margin

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxySettings.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithoutProxySettings.jsx
@@ -61,11 +61,11 @@ const CustomBotWithoutProxySettings = (props) => {
       />
 
       <h2 className="admin-setting-header">{t('admin:slack_integration.integration_procedure')}</h2>
-      <div className={(props.slackSigningSecret != null || props.slackBotToken != null) ? 'px-3 mb-5' : 'px-3 my-5'}>
+      <div className="px-3 my-5">
         {(props.slackSigningSecret != null || props.slackBotToken != null) && (
-          <div className="d-flex justify-content-end">
+          <div className="d-flex justify-content-end　my-3">
             <button
-              className="my-3 pull-right btn text-danger border-danger"
+              className="pull-right btn text-danger border-danger"
               type="button"
               onClick={() => setIsDeleteConfirmModalShown(true)}
             >{t('admin:slack_integration.reset')}
