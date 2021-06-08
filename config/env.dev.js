e06276919c0b4e86044c3dfcc98ee6182cf3e27b
Steven Fukase <fukasesteven@gmail.com>
2021-06-08 06:10:08 +0000

    Added test env

--- config/env.dev.js
+++ config/env.dev.js
@@ -22,4 +22,6 @@ module.exports = {
   // DEV_HTTPS: true,
   // FORCE_WIKI_MODE: 'private', // 'public', 'private', undefined
   // PROMSTER_ENABLED: true,
+  GROWI_CLOUD_URI: 'http://growi.cloud',
+  GROWI_APP_ID_FOR_GROWI_CLOUD: 'cloudid123',
 };
