3861c866a5fd436d7ebfa85e40e6a4ca5ab098a9
Yuki Takei <yuki@weseek.co.jp>
2021-06-08 18:33:09 +0000

    commonize the repetitions of XSS processing

--- src/client/js/util/PreProcessor/XssFilter.js
+++ src/client/js/util/PreProcessor/XssFilter.js
@@ -14,17 +14,7 @@ export default class XssFilter {
 
   process(markdown) {
     if (this.crowi.config.isEnabledXssPrevention) {
-      let count = 0;
-      let tempValue = markdown;
-      let currValue = '';
-      while (true) {
-        count += 1;
-        currValue = this.xss.process(tempValue);
-        if(count > 50) return '--filtered--';
-        if(currValue == tempValue) break;
-        tempValue = currValue;
-      }
-      return currValue;
+      return this.xss.process(markdown);
     }
 
     return markdown;
