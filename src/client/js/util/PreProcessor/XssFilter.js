149d7b69726bacf4538719580b532832ba7b8b54
Harold Kim <root@stypr.com>
2021-06-08 01:15:15 -0700

    Improve XSS filter

--- src/client/js/util/PreProcessor/XssFilter.js
+++ src/client/js/util/PreProcessor/XssFilter.js
@@ -14,7 +14,17 @@ export default class XssFilter {
 
   process(markdown) {
     if (this.crowi.config.isEnabledXssPrevention) {
-      return this.xss.process(markdown);
+      let count = 0;
+      let tempValue = markdown;
+      let currValue = '';
+      while (true) {
+        count += 1;
+        currValue = this.xss.process(tempValue);
+        if(count > 50) return '--filtered--';
+        if(currValue == tempValue) break;
+        tempValue = currValue;
+      }
+      return currValue;
     }
 
     return markdown;
