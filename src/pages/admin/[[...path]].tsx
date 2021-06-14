6b31f39a8d7dec8d048375a77f080e6a728d892e
itizawa <itizawa@weseek.co.jp>
2021-06-14 01:06:16 +0000

    clean code

--- src/pages/admin/[[...path]].tsx
+++ src/pages/admin/[[...path]].tsx
@@ -163,8 +163,6 @@ export const getServerSideProps: GetServerSideProps = async(context: GetServerSi
   props.growiCloudUri = configManager.getConfig('crowi', 'app:growiCloudUri');
   props.growiAppIdForCloud = configManager.getConfig('crowi', 'app:growiAppIdForCloud');
 
-  console.log(props.growiAppIdForCloud);
-
   return {
     props,
   };
