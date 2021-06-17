336067301e45c77e9651c409688eb63ef1461732
itizawa <itizawa@weseek.co.jp>
2021-06-17 16:59:30 +0000

    early return

--- packages/slackbot-proxy/src/services/growi-uri-injector/GrowiUriInjectionModalDelegator.ts
+++ packages/slackbot-proxy/src/services/growi-uri-injector/GrowiUriInjectionModalDelegator.ts
@@ -5,18 +5,20 @@ export class GrowiUriInjectionModalDelegator implements GrowiUriInjector {
 
 
   inject(body: any, growiUri:string): void {
-    if (body.view != null) {
-      const parsedView = JSON.parse(body.view as string);
-      parsedView.private_metadata = JSON.stringify({ growiUri });
-      body.view = JSON.stringify(parsedView);
+    if (body.view == null) {
+      return;
     }
+    const parsedView = JSON.parse(body.view as string);
+    parsedView.private_metadata = JSON.stringify({ growiUri });
+    body.view = JSON.stringify(parsedView);
   }
 
   extract(body: any): string|void {
     const payload = JSON.parse(body.payload);
-    if (payload.view != null) {
-      return JSON.parse(payload.view.private_metadata).growiUri;
+    if (payload?.view?.private_metadata == null) {
+      return;
     }
+    return JSON.parse(payload.view.private_metadata).growiUri;
   }
 
 }
