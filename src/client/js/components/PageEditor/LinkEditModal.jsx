c947e04d8e7a39c929422dcbaf0e13288e90031f
Steven Fukase <fukasesteven@gmail.com>
2021-06-21 02:16:53 +0000

    bug fix

--- src/client/js/components/PageEditor/LinkEditModal.jsx
+++ src/client/js/components/PageEditor/LinkEditModal.jsx
@@ -224,11 +224,14 @@ class LinkEditModal extends React.PureComponent {
   }
 
   handleChangeLinkInput(link) {
+    this.setState({ linkInputValue: link, isUseRelativePath: false, isUsePermanentLink: false });
     let isUseRelativePath = this.state.isUseRelativePath;
     if (!this.state.linkInputValue.startsWith('/') || this.state.linkerType === Linker.types.growiLink) {
       isUseRelativePath = false;
     }
-    this.setState({ linkInputValue: link, isUseRelativePath, isUsePermanentLink: false });
+    this.setState({
+      linkInputValue: link, isUseRelativePath, isUsePermanentLink: false, permalink: '',
+    });
   }
 
   handleSelecteLinkerType(linkerType) {
