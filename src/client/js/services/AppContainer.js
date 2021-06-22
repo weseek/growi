906f4faca8d32178d019db254b3adb2284ed0fef
Taichi Masuyama <montanha.masu536@gmail.com>
2021-06-22 10:34:01 +0000

    Modified a comment just for a note for the future dev

--- src/client/js/services/AppContainer.js
+++ src/client/js/services/AppContainer.js
@@ -54,7 +54,7 @@ export default class AppContainer extends Container {
     this.isSharedUser = isSharedPageElem != null && this.currentUser == null;
 
     // TAICHI: check if Link Sharing is enabled or not
-    // this.canShareLink = 
+    // this.isLinkSharingForbidden =
 
     const userLocaleId = this.currentUser?.lang;
     this.i18n = i18nFactory(userLocaleId);
