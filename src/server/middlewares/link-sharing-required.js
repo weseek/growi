8c655c4a1c0f2848c3b8490ee3f2f08a48e89e61
hakumizuki <montanha.masu536@gmail.com>
2021-06-24 06:15:32 +0000

    Fixed linting

--- src/server/middlewares/link-sharing-required.js
+++ src/server/middlewares/link-sharing-required.js
@@ -4,11 +4,11 @@ const ErrorV3 = require('../models/vo/error-apiv3');
 const logger = loggerFactory('growi:middleware:link-sharing-required');
 
 module.exports = (crowi) => (req, res, next) => {
-    const isLinkSharingDisabled = crowi.configManager.getConfig('crowi', 'security:disableLinkSharing');
-    logger.debug(`isLinkSharingDisabled: ${isLinkSharingDisabled}`);
+  const isLinkSharingDisabled = crowi.configManager.getConfig('crowi', 'security:disableLinkSharing');
+  logger.debug(`isLinkSharingDisabled: ${isLinkSharingDisabled}`);
 
-    if (isLinkSharingDisabled) {
-        return res.apiv3Err(new ErrorV3('Link sharing is disabled', 'link-sharing-disabled'));
-    }
-    next();
-}
+  if (isLinkSharingDisabled) {
+    return res.apiv3Err(new ErrorV3('Link sharing is disabled', 'link-sharing-disabled'));
+  }
+  next();
+};
