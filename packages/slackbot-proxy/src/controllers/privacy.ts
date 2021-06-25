88976a6328ec81872683ccdaf18df8fee45becd8
itizawa <itizawa@weseek.co.jp>
2021-06-25 06:32:39 +0000

    implement route

--- packages/slackbot-proxy/src/controllers/privacy.ts
+++ packages/slackbot-proxy/src/controllers/privacy.ts
@@ -1,13 +1,20 @@
-import { Controller, Get } from '@tsed/common';
+import { Controller, PlatformRouter } from '@tsed/common';
+import { Request, Response } from 'express';
 
 const isOfficialMode = process.env.OFFICIAL_MODE === 'true';
 
 @Controller('/privacy')
 export class SlackCtrl {
 
-  @Get('/')
-  async install(): Promise<string> {
-    return 'Privary Policy';
+  constructor(router: PlatformRouter) {
+    if (isOfficialMode) {
+      router.get('/', this.getPrivacy);
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  getPrivacy(req: Request, res: Response): string|void {
+    res.send('Privary Policy');
   }
 
 }
