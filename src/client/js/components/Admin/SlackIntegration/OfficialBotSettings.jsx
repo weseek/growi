78b18782759585b0e3ce51f7ee9446690d256936
zahmis <meda829ratteru86@gmail.com>
2021-07-01 04:39:29 +0000

    comment out official bot

--- src/client/js/components/Admin/SlackIntegration/OfficialBotSettings.jsx
+++ src/client/js/components/Admin/SlackIntegration/OfficialBotSettings.jsx
@@ -67,8 +67,8 @@ const OfficialBotSettings = (props) => {
   return (
     <>
       <h2 className="admin-setting-header">{t('admin:slack_integration.official_bot_integration')}
-        {/* TODO: add an appropriate links by GW-5614 */}
-        <i className="fa fa-external-link btn-link ml-2" aria-hidden="true"></i>
+        {/* TODO: If Bot-manual section of docs is merged into master, show links and add an appropriate links by GW-5614. */}
+        {/* <i className="fa fa-external-link btn-link ml-2" aria-hidden="true"></i> */}
       </h2>
 
       {slackAppIntegrations.length !== 0 && (
