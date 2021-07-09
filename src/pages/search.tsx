d23e796bc04f36ae967f2e47cc47421c6d42d8cf
Yohei-Shiina <yohei0125@weseek.co.jp>
2021-07-09 17:58:30 +0900

    6702 change titles

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link';
 import RawLayout from '../components/RawLayout';
 
 const AboutPage = () => (
-  <RawLayout title="About | Next.js + TypeScript Example">
-    <h1>About</h1>
-    <p>This is the about page</p>
+  <RawLayout title="Search Results | Next.js + TypeScript Example">
+    <h1>Search Results</h1>
+    <p>This is the search results page</p>
     <p>
       <Link href="/">
         <a>Go home</a>
