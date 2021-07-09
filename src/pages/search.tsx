2f6f98898e8e5038658ffe398c977e4cb1eacb02
Yohei-Shiina <yohei0125@weseek.co.jp>
2021-07-09 18:14:51 +0900

    change variable name

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import RawLayout from '../components/RawLayout';
 
-const AboutPage = () => (
+const SearchPage = () => (
   <RawLayout title="Search Results | Next.js + TypeScript Example">
     <h1>Search Results</h1>
     <p>This is the search results page</p>
@@ -13,4 +13,4 @@ const AboutPage = () => (
   </RawLayout>
 );
 
-export default AboutPage;
+export default SearchPage;
