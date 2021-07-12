f1ad09943034533d424bdaad3d253a99de219a80
kaori <kaori@weseek.co.jp>
2021-07-12 15:59:12 +0900

    added a password setting link

--- src/client/js/components/LoginForm.jsx
+++ src/client/js/components/LoginForm.jsx
@@ -266,13 +266,16 @@ class LoginForm extends React.Component {
                 {isLocalOrLdapStrategiesEnabled && this.renderLocalOrLdapLoginForm()}
                 {isSomeExternalAuthEnabled && this.renderExternalAuthLoginForm()}
                 {isRegistrationEnabled && (
-                  <div className="row">
-                    <div className="col-12 text-right py-2">
-                      <a href="#register" id="register" className="link-switch" onClick={this.switchForm}>
-                        <i className="ti-check-box"></i> {t('Sign up is here')}
-                      </a>
-                    </div>
+                <div className="row">
+                  <div className="col-12 text-right py-2">
+                    <a href="#reset-password" id="reset-password" className="d-block link-switch mb-1" onClick={this.switchForm}>
+                      <i className="icon-key"></i> {t('forgot_password')}
+                    </a>
+                    <a href="#register" id="register" className="link-switch" onClick={this.switchForm}>
+                      <i className="ti-check-box"></i> {t('Sign up is here')}
+                    </a>
                   </div>
+                </div>
               )}
               </div>
               <div className="back">
