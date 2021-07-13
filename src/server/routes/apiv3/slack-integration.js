cf252bd48581f2eb3ff190b272980aab95df298b
itizawa <itizawa.pen@gmail.com>
2021-07-13 04:12:36 +0000

    create command

--- src/server/routes/apiv3/slack-integration.js
+++ src/server/routes/apiv3/slack-integration.js
@@ -114,6 +114,9 @@ module.exports = (crowi) => {
         case 'help':
           await crowi.slackBotService.helpCommand(client, body);
           break;
+        case 'togetter':
+          await crowi.slackBotService.togetterCommand(client, body, args);
+          break;
         default:
           await crowi.slackBotService.notCommand(client, body);
           break;
@@ -159,6 +162,14 @@ module.exports = (crowi) => {
         const { body, args, offset } = parsedValue;
         const newOffset = offset + 10;
         await crowi.slackBotService.showEphemeralSearchResults(client, body, args, newOffset);
+        break;
+      }
+      case 'showMoreTogetterResults': {
+        const parsedValue = JSON.parse(payload.actions[0].value);
+
+        const { body, args, limit } = parsedValue;
+        const newLimit = limit + 10;
+        await crowi.slackBotService.togetterCommand(client, body, args, newLimit);
         break;
       }
       default:
