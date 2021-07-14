acc3c421549c5d56e986247a3c5a6ed2901250e6
kaori <kaori@weseek.co.jp>
2021-07-14 20:14:57 +0900

    removed id

--- src/client/js/components/LoginForm.jsx
+++ src/client/js/components/LoginForm.jsx
@@ -268,7 +268,7 @@ class LoginForm extends React.Component {
                 {isRegistrationEnabled && (
                 <div className="row">
                   <div className="col-12 text-right py-2">
-                    <a href="/forgot-password" id="reset-password" className="d-block link-switch mb-1">
+                    <a href="/forgot-password" className="d-block link-switch mb-1">
                       <i className="icon-key"></i> {t('forgot_password')}
                     </a>
                     <a href="#register" id="register" className="link-switch" onClick={this.switchForm}>
