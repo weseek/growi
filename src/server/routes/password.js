7f8c0c2b241eb36eaf75cd8cc39404991fc53ec4
kaori <kaori@weseek.co.jp>
2021-07-14 17:28:19 +0900

    success to render forgot-password page

--- src/server/routes/password.js
+++ src/server/routes/password.js
@@ -1,22 +1,9 @@
 module.exports = function(crowi, app) {
-  const ApiResponse = require('../util/apiResponse');
-
   const actions = {};
-  const api = {};
-
-  /*
-   TODO: add swagger
-  */
-
-  // api.get = async function(req, res) {
-  //   return res.json(ApiResponse.success('hoge'));
-  // };
 
   actions.forgotpassword = async function(req, res) {
-    return res.json(ApiResponse.success('hogesss'));
-    // return res.redirect('/password');
+    return res.render('forgot-password');
   };
 
-  actions.api = api;
   return actions;
 };
