85da522e8e605331a31c6f723ab9f4da87cb956b
kaori <kaori@weseek.co.jp>
2021-07-15 21:09:22 +0900

    use card not panel

--- src/client/js/components/PasswordResetRequestForm.jsx
+++ src/client/js/components/PasswordResetRequestForm.jsx
@@ -11,24 +11,24 @@ const PasswordResetRequestForm = (props) => {
       <div className="container">
         <div className="row">
           <div className="col-md-4 col-md-offset-4">
-            <div className="panel panel-default">
-              <div className="panel-body">
+            <div className="card">
+              <div className="card-body">
                 <div className="text-center">
                   <h3><i className="fa fa-lock fa-4x"></i></h3>
                   <h2 className="text-center">Forgot Password?</h2>
                   <p>You can reset your password here.</p>
-                  <div className="panel-body">
+                  <div className="card-body">
                     <form id="register-form" role="form" autoComplete="off" className="form" method="post">
                       <div className="form-group">
                         <div className="input-group">
                           <span className="input-group-addon">
                             <i className="glyphicon glyphicon-envelope" />
                           </span>
-                          <input id="email" name="email" placeholder="email address" className="form-control" type="email" />
+                          <input name="email" placeholder="email address" className="form-control" type="email" />
                         </div>
                       </div>
                       <div className="form-group">
-                        <input name="recover-submit" className="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit" />
+                        <input name="reset-password-btn" className="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit" />
                       </div>
                     </form>
                   </div>
