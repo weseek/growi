0c3d42932053ff3fd05e6d7d74b6f71e060cf9d9
Yohei-Shiina <yohei0125@weseek.co.jp>
2021-07-15 20:28:33 +0900

    6752 show navbar top

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -3,27 +3,35 @@ import {
 } from 'next';
 import Link from 'next/link';
 import { i18n, config, useTranslation } from '~/i18n';
-
+import { CrowiRequest } from '~/interfaces/crowi-request';
 
 import { CommonProps, getServerSideCommonProps, useCustomTitle } from '~/utils/nextjs-page-utils';
-
 import BasicLayout from '../components/BasicLayout'
 
-type Props = CommonProps & {
+import {
+  useCurrentUser
+} from '../stores/context';
 
+type Props = CommonProps & {
+  currentUser: any,
+
+  page: any,
+  isAbleToDeleteCompletely: boolean,
 }
 const SearchPage: NextPage<Props> = (props: Props) => {
 
   const { t } = useTranslation();
   // Todo: 検索結果ページ用の翻訳追加
-  const title = useCustomTitle(props, t('installer.setup'));
+  const title = useCustomTitle(props, t('SearchResult'));
+
+  useCurrentUser(props.currentUser != null ? JSON.parse(props.currentUser) : null);
 
   // load all languages
   i18n.loadLanguages(config.allLanguages);
 
   return (
     <>
-      <BasicLayout title="Search Results">
+      <BasicLayout title={title}>
         <h1>Search Results</h1>
         <p>This is the search results page</p>
         <p>
@@ -37,14 +45,22 @@ const SearchPage: NextPage<Props> = (props: Props) => {
 };
 
 export const getServerSideProps: GetServerSideProps = async(context: GetServerSidePropsContext) => {
+  const req: CrowiRequest = context.req as CrowiRequest;
+
+  const { user } = req;
+
   const result = await getServerSideCommonProps(context);
+
   // check for presence
   // see: https://github.com/vercel/next.js/issues/19271#issuecomment-730006862
   if (!('props' in result)) {
     throw new Error('invalid getSSP result');
   }
 
-  const { props } = result;
+  const props: Props = result.props as Props;
+  if (user != null) {
+    props.currentUser = JSON.stringify(user.toObject());
+  }
 
   return {
     props: {
