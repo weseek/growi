c4cd8427e248f8e03f22b694ed173ab774604d67
Shun Miyazawa <shun-m@weseek.co.jp>
2021-07-16 07:58:58 +0000

    comment out

--- src/client/js/components/Admin/Users/StatusSuspendedButton.jsx
+++ src/client/js/components/Admin/Users/StatusSuspendedButton.jsx
@@ -6,62 +6,62 @@ import { withUnstatedContainers } from '../../UnstatedUtils';
 import { toastSuccess, toastError } from '../../../util/apiNotification';
 import AdminUsersContainer from '../../../services/AdminUsersContainer';
 
-class StatusSuspendedButton extends React.Component {
+// class StatusSuspendedButton extends React.Component {
 
-  constructor(props) {
-    super(props);
+//   constructor(props) {
+//     super(props);
 
-    this.onClickDeactiveBtn = this.onClickDeactiveBtn.bind(this);
-  }
+//     this.onClickDeactiveBtn = this.onClickDeactiveBtn.bind(this);
+//   }
 
-  async onClickDeactiveBtn() {
-    const { t } = this.props;
+//   async onClickDeactiveBtn() {
+//     const { t } = this.props;
 
-    try {
-      const username = await this.props.adminUsersContainer.deactivateUser(this.props.user._id);
-      toastSuccess(t('toaster.deactivate_user_success', { username }));
-    }
-    catch (err) {
-      toastError(err);
-    }
-  }
+//     try {
+//       const username = await this.props.adminUsersContainer.deactivateUser(this.props.user._id);
+//       toastSuccess(t('toaster.deactivate_user_success', { username }));
+//     }
+//     catch (err) {
+//       toastError(err);
+//     }
+//   }
 
-  renderSuspendedBtn() {
-    const { t } = this.props;
+//   renderSuspendedBtn() {
+//     const { t } = this.props;
 
-    return (
-      <button className="dropdown-item" type="button" onClick={() => { this.onClickDeactiveBtn() }}>
-        <i className="icon-fw icon-ban"></i> {t('admin:user_management.user_table.deactivate_account')}
-      </button>
-    );
-  }
+//     return (
+//       <button className="dropdown-item" type="button" onClick={() => { this.onClickDeactiveBtn() }}>
+//         <i className="icon-fw icon-ban"></i> {t('admin:user_management.user_table.deactivate_account')}
+//       </button>
+//     );
+//   }
 
-  renderSuspendedAlert() {
-    const { t } = this.props;
+//   renderSuspendedAlert() {
+//     const { t } = this.props;
 
-    return (
-      <div className="px-4">
-        <i className="icon-fw icon-ban mb-2"></i>{t('admin:user_management.user_table.deactivate_account')}
-        <p className="alert alert-danger">{t('admin:user_management.user_table.your_own')}</p>
-      </div>
-    );
-  }
+//     return (
+//       <div className="px-4">
+//         <i className="icon-fw icon-ban mb-2"></i>{t('admin:user_management.user_table.deactivate_account')}
+//         <p className="alert alert-danger">{t('admin:user_management.user_table.your_own')}</p>
+//       </div>
+//     );
+//   }
 
-  render() {
-    const { user } = this.props;
-    // TODO: GW-5303 retrieve from SWR
-    // const { currentUsername } = this.props.appContainer;
-    const currentUsername = '';
+//   render() {
+//     const { user } = this.props;
+//     // TODO: GW-5303 retrieve from SWR
+//     // const { currentUsername } = this.props.appContainer;
+//     const currentUsername = '';
 
-    return (
-      <Fragment>
-        {user.username !== currentUsername ? this.renderSuspendedBtn()
-          : this.renderSuspendedAlert()}
-      </Fragment>
-    );
-  }
+//     return (
+//       <Fragment>
+//         {user.username !== currentUsername ? this.renderSuspendedBtn()
+//           : this.renderSuspendedAlert()}
+//       </Fragment>
+//     );
+//   }
 
-}
+// }
 
 /**
  * Wrapper component for using unstated
