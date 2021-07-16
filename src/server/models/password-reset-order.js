278c450f2c729a6170c8130746bdd327ff335329
kaori <kaori@weseek.co.jp>
2021-07-16 21:11:04 +0900

    removed isExpired from scheme and added static method instead

--- src/server/models/password-reset-order.js
+++ src/server/models/password-reset-order.js
@@ -3,26 +3,24 @@ const uniqueValidator = require('mongoose-unique-validator');
 
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
-/*
- * define schema
- */
 const schema = new mongoose.Schema({
   token: { type: String, required: true, unique: true },
   email: { type: String, required: true },
   relatedUser: { type: ObjectId, ref: 'User' },
-  isExpired: { type: Boolean, default: false },
 });
 schema.plugin(uniqueValidator);
 
-/**
- * PasswordResetOrder Class
- *
- * @class PasswordResetOrder
- */
 class PasswordResetOrder {
 
   static generateOneTimeToken() {
     // TODO: generate unique token by GW-6802
+  }
+
+  static isExpired() {
+    const now = Date.now();
+    const expiredAt = this.createdAt.getTime() + 600000;
+
+    return expiredAt < now;
   }
 
 }
