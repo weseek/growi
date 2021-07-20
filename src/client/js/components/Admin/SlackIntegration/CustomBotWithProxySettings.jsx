008aaa0f315acce587f671f23f210e96c5c1ec36
Shun Miyazawa <shun-m@weseek.co.jp>
2021-07-20 15:38:39 +0900

    ok

--- src/client/js/components/Admin/SlackIntegration/CustomBotWithProxySettings.jsx
+++ src/client/js/components/Admin/SlackIntegration/CustomBotWithProxySettings.jsx
@@ -66,11 +66,12 @@ const CustomBotWithProxySettings = (props) => {
   return (
     <>
       <h2 className="admin-setting-header mb-2">{t('admin:slack_integration.custom_bot_with_proxy_integration')}
-        <i
-          className="fa fa-external-link btn-link ml-2"
-          aria-hidden="true"
-          onClick={() => window.open(`${t('admin:slack_integration.docs_url.custom_bot_with_proxy')}`, '_blank')}
-        />
+        <a href={t('admin:slack_integration.docs_url.custom_bot_with_proxy')} target="_blank" rel="noopener noreferrer">
+          <i
+            className="fa fa-external-link btn-link ml-2"
+            aria-hidden="true"
+          />
+        </a>
       </h2>
 
       {slackAppIntegrations.length !== 0 && (
