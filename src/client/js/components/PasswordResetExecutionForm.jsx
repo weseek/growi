614f471c61d898833b744bf84312ae2ddd3bac2f
kaori <kaori@weseek.co.jp>
2021-07-26 22:37:21 +0900

    FB: separated row from components

--- src/client/js/components/PasswordResetExecutionForm.jsx
+++ src/client/js/components/PasswordResetExecutionForm.jsx
@@ -8,36 +8,24 @@ const PasswordResetExecutionForm = (props) => {
   // const { t } = props;
 
   return (
-    <div className="container">
-      <div className="row justify-content-md-center">
-        <div className="col-md-6 mt-5">
-          <div className="text-center">
-            <h1><i className="icon-lock large"></i></h1>
-            <h2 className="text-center">Reset Password</h2>
-            {/* TODO: show user email by GW-6778 */}
-            <p>Enter a new password for foo@example.com.</p>
-            <form role="form" className="form" method="post">
-              <div className="form-group">
-                <div className="input-group">
-                  <input name="password" placeholder="New Password" className="form-control" type="password" />
-                </div>
-              </div>
-              <div className="form-group">
-                <div className="input-group">
-                  <input name="password" placeholder="Confirm the new password" className="form-control" type="password" />
-                </div>
-              </div>
-              <div className="form-group">
-                <input name="reset-password-btn" className="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit" />
-              </div>
-              <a href="/login">
-                <i className="icon-login mr-1"></i>Sign in instead
-              </a>
-            </form>
-          </div>
+    <form role="form" className="form" method="post">
+      <div className="form-group">
+        <div className="input-group">
+          <input name="password" placeholder="New Password" className="form-control" type="password" />
         </div>
       </div>
-    </div>
+      <div className="form-group">
+        <div className="input-group">
+          <input name="password" placeholder="Confirm the new password" className="form-control" type="password" />
+        </div>
+      </div>
+      <div className="form-group">
+        <input name="reset-password-btn" className="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit" />
+      </div>
+      <a href="/login">
+        <i className="icon-login mr-1"></i>Sign in instead
+      </a>
+    </form>
   );
 };
 
