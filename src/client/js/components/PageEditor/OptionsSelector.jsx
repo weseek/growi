032cff261d8a2432598c1e5bb6134f5004102602
zahmis <meda829ratteru86@gmail.com>
2021-07-26 11:36:48 +0000

    add comment

--- src/client/js/components/PageEditor/OptionsSelector.jsx
+++ src/client/js/components/PageEditor/OptionsSelector.jsx
@@ -115,6 +115,7 @@ class OptionsSelector extends React.Component {
     this.setState({ isCddMenuOpened: !this.state.isCddMenuOpened });
   }
 
+  // TODO: useIndentSize after EditorNavbnavBottom is shown
   onChangeIndentSize(newValue) {
     const { editorContainer } = this.props;
     editorContainer.setState({ indentSize: newValue });
