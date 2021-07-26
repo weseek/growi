9de9ed9b587212cbc69a0fcf557b6ad67e667f02
Shun Miyazawa <shun-m@weseek.co.jp>
2021-07-26 16:22:39 +0900

    restore

--- src/client/js/components/Admin/Users/StatusSuspendedButton.jsx
+++ src/client/js/components/Admin/Users/StatusSuspendedButton.jsx
@@ -6,13 +6,17 @@ import { withUnstatedContainers } from '../../UnstatedUtils';
 import { toastSuccess, toastError } from '../../../util/apiNotification';
 import AdminUsersContainer from '../../../services/AdminUsersContainer';
 
-const StatusSuspendedButton = (props) => {
-  const { t, user, appContainer } = props;
+class StatusSuspendedButton extends React.Component {
 
-  // TODO: GW-5303 retrieve from SWR
-  const { currentUsername } = appContainer;
+  constructor(props) {
+    super(props);
 
-  const onClickDeactiveBtn = async() => {
+    this.onClickDeactiveBtn = this.onClickDeactiveBtn.bind(this);
+  }
+
+  async onClickDeactiveBtn() {
+    const { t } = this.props;
+
     try {
       const username = await this.props.adminUsersContainer.deactivateUser(this.props.user._id);
       toastSuccess(t('toaster.deactivate_user_success', { username }));
@@ -20,88 +24,44 @@ const StatusSuspendedButton = (props) => {
     catch (err) {
       toastError(err);
     }
-  };
+  }
 
-  const renderSuspendedBtn = () => {
+  renderSuspendedBtn() {
+    const { t } = this.props;
+
     return (
-      <button className="dropdown-item" type="button" onClick={() => { onClickDeactiveBtn() }}>
+      <button className="dropdown-item" type="button" onClick={() => { this.onClickDeactiveBtn() }}>
         <i className="icon-fw icon-ban"></i> {t('admin:user_management.user_table.deactivate_account')}
       </button>
     );
-  };
+  }
 
-  const renderSuspendedAlert = () => {
+  renderSuspendedAlert() {
+    const { t } = this.props;
+
     return (
       <div className="px-4">
         <i className="icon-fw icon-ban mb-2"></i>{t('admin:user_management.user_table.deactivate_account')}
         <p className="alert alert-danger">{t('admin:user_management.user_table.your_own')}</p>
       </div>
     );
-  };
+  }
 
-  return (
-    <>
-      {user.username !== currentUsername ? renderSuspendedBtn(): renderSuspendedAlert()}
-    </>
-  );
-};
+  render() {
+    const { user } = this.props;
+    // TODO: GW-5303 retrieve from SWR
+    // const { currentUsername } = this.props.appContainer;
+    const currentUsername = '';
 
-// class StatusSuspendedButton extends React.Component {
+    return (
+      <Fragment>
+        {user.username !== currentUsername ? this.renderSuspendedBtn()
+          : this.renderSuspendedAlert()}
+      </Fragment>
+    );
+  }
 
-//   constructor(props) {
-//     super(props);
-
-//     this.onClickDeactiveBtn = this.onClickDeactiveBtn.bind(this);
-//   }
-
-//   async onClickDeactiveBtn() {
-//     const { t } = this.props;
-
-//     try {
-//       const username = await this.props.adminUsersContainer.deactivateUser(this.props.user._id);
-//       toastSuccess(t('toaster.deactivate_user_success', { username }));
-//     }
-//     catch (err) {
-//       toastError(err);
-//     }
-//   }
-
-//   renderSuspendedBtn() {
-//     const { t } = this.props;
-
-//     return (
-//       <button className="dropdown-item" type="button" onClick={() => { this.onClickDeactiveBtn() }}>
-//         <i className="icon-fw icon-ban"></i> {t('admin:user_management.user_table.deactivate_account')}
-//       </button>
-//     );
-//   }
-
-//   renderSuspendedAlert() {
-//     const { t } = this.props;
-
-//     return (
-//       <div className="px-4">
-//         <i className="icon-fw icon-ban mb-2"></i>{t('admin:user_management.user_table.deactivate_account')}
-//         <p className="alert alert-danger">{t('admin:user_management.user_table.your_own')}</p>
-//       </div>
-//     );
-//   }
-
-//   render() {
-//     const { user } = this.props;
-//     // TODO: GW-5303 retrieve from SWR
-//     // const { currentUsername } = this.props.appContainer;
-//     const currentUsername = '';
-
-//     return (
-//       <Fragment>
-//         {user.username !== currentUsername ? this.renderSuspendedBtn()
-//           : this.renderSuspendedAlert()}
-//       </Fragment>
-//     );
-//   }
-
-// }
+}
 
 /**
  * Wrapper component for using unstated
