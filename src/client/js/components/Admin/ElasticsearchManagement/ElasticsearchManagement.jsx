da626f1f8cc719037d1e9d32e2b5af83e484e292
Steven Fukase <fukasesteven@gmail.com>
2021-07-27 06:46:39 +0000

    lint revert

--- src/client/js/components/Admin/ElasticsearchManagement/ElasticsearchManagement.jsx
+++ src/client/js/components/Admin/ElasticsearchManagement/ElasticsearchManagement.jsx
@@ -122,16 +122,20 @@ class ElasticsearchManagementBody extends React.Component {
         <hr />
 
         <div className="row">
-          <label className="col-md-3 col-form-label text-left text-md-right">{ t('full_text_search_management.reconnect') }</label>
+          <label className="col-md-3 col-form-label text-left text-md-right">{t('full_text_search_management.reconnect')}</label>
           <div className="col-md-6">
-            <ReconnectControls isEnabled={isReconnectBtnEnabled} isProcessing={isReconnectingProcessing} onReconnectingRequested={this.reconnect} />
+            <ReconnectControls
+              isEnabled={isReconnectBtnEnabled}
+              isProcessing={isReconnectingProcessing}
+              onReconnectingRequested={this.reconnect}
+            />
           </div>
         </div>
 
         <hr />
 
         <div className="row">
-          <label className="col-md-3 col-form-label text-left text-md-right">{ t('full_text_search_management.normalize') }</label>
+          <label className="col-md-3 col-form-label text-left text-md-right">{t('full_text_search_management.normalize')}</label>
           <div className="col-md-6">
             <NormalizeIndicesControls
               isRebuildingProcessing={isRebuildingProcessing}
@@ -145,7 +149,7 @@ class ElasticsearchManagementBody extends React.Component {
         <hr />
 
         <div className="row">
-          <label className="col-md-3 col-form-label text-left text-md-right">{ t('full_text_search_management.rebuild') }</label>
+          <label className="col-md-3 col-form-label text-left text-md-right">{t('full_text_search_management.rebuild')}</label>
           <div className="col-md-6">
             {/* TODO: GW-5134 Migrate SocketIoContainer to SWR */}
             {/* <RebuildIndexControls
