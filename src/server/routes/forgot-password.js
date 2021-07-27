5a82fc741c537b6a4b71b30290fd4e3a35073ffb
kaori <kaori@weseek.co.jp>
2021-07-27 16:02:29 +0900

    using user email

--- src/server/routes/forgot-password.js
+++ src/server/routes/forgot-password.js
@@ -18,10 +18,10 @@ module.exports = function(crowi, app) {
   };
 
 
-  async function sendPasswordResetEmail(i18n) {
+  async function sendPasswordResetEmail(email, i18n) {
 
     return mailService.send({
-      to: 'hoge@example.com',
+      to: email,
       subject: 'forgotPasswordMailTest',
       template: path.join(crowi.localeDir, `${i18n}/notifications/passwordReset.txt`),
       // TODO: need to set appropriate values by GW-6828
@@ -48,7 +48,7 @@ module.exports = function(crowi, app) {
     const grobalLang = configManager.getConfig('crowi', 'app:globalLang');
     const i18n = req.language || grobalLang;
 
-    await sendPasswordResetEmail(i18n);
+    await sendPasswordResetEmail(email, i18n);
     return;
   };
 
