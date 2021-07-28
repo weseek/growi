f621d2c753b5297b0c3100f96ececa616b0c9aa3
Yuki Takei <yuki@weseek.co.jp>
2021-07-28 11:51:25 +0000

    devide utils

--- packages/app/src/utils/process-utils.ts
+++ packages/app/src/utils/process-utils.ts
@@ -1,18 +1,4 @@
-/**
- * @author: @AngularClass
- * @author: Yuki Takei <yuki@weseek.co.jp>
- */
-
-const path = require('path');
-
-// Helper functions
-const ROOT = path.resolve(__dirname, '../../..');
-
-function hasProcessFlag(flag) {
+/* eslint-disable import/prefer-default-export */
+export function hasProcessFlag(flag: string): boolean {
   return process.argv.join('').indexOf(flag) > -1;
 }
-
-const root = path.join.bind(path, ROOT);
-
-exports.hasProcessFlag = hasProcessFlag;
-exports.root = root;
