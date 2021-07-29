1b81711c69210f407855e1b4afe8981c3e510a12
Yuki Takei <yuki@weseek.co.jp>
2021-07-29 18:49:05 +0000

    typescriptize app.ts

--- packages/app/src/server/app.ts
+++ packages/app/src/server/app.ts
@@ -1,21 +1,17 @@
-/**
- * Growi::app.js
- *
- * @package growi
- * @author  Yuki Takei <yuki@weseek.co.jp>
- */
+import Logger from 'bunyan';
 
-require('module-alias/register');
+import loggerFactory from '~/utils/logger';
+import { hasProcessFlag } from '~/utils/process-utils';
 
-const logger = require('@alias/logger')('growi');
-const helpers = require('@commons/util/helpers');
-const growi = new (require('./crowi'))(helpers.root());
+import Crowi from './crowi';
+
+const logger: Logger = loggerFactory('growi');
 
 
 /** **********************************
  *          Main Process
  ********************************** */
-process.on('uncaughtException', (err) => {
+process.on('uncaughtException', (err?: Error) => {
   logger.error('Uncaught Exception: ', err);
 });
 
@@ -23,17 +19,24 @@ process.on('unhandledRejection', (reason, p) => {
   logger.error('Unhandled Rejection: Promise:', p, 'Reason:', reason);
 });
 
-growi.start()
-  .then((server) => {
-    if (helpers.hasProcessFlag('ci')) {
+async function main() {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const growi = new Crowi();
+    const server = await growi.start();
+
+    if (hasProcessFlag('ci')) {
       logger.info('"--ci" flag is detected. Exit process.');
       server.close(() => {
         process.exit();
       });
     }
-  })
-  .catch((err) => {
+  }
+  catch (err) {
     logger.error('An error occurred, unable to start the server');
     logger.error(err);
     process.exit(1);
-  });
+  }
+}
+
+main();
