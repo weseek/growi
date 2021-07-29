2e9ed11f083eb68ac353762a77e39813e056bf7b
kaori <kaori@weseek.co.jp>
2021-07-29 16:23:21 +0900

    success to redirect to another page

--- src/server/middlewares/password-reset.js
+++ src/server/middlewares/password-reset.js
@@ -1,9 +1,14 @@
 module.exports = (crowi, app) => {
 
   return (req, res, next) => {
+    const { value } = req.query;
     // check the oneTimeToken is valid
-    if (req.token == null /* || token.isExpired() */) {
-      return res.redirect('/login');
+
+    // http://localhost:3000/forgot-password/token?value=hoge
+    if (value === 'hoge' /* || token.isExpired() */) {
+      console.log('req.queryがほげ');
+      // return res.redirect('/login');
+      return res.redirect('/login/error/registered');
     }
     return next();
   };
