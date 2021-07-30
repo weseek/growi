53e3c2a162abf1c0fa80a8591a773da29f93d91b
kaori <kaori@weseek.co.jp>
2021-07-30 16:49:19 +0900

    added default config

--- src/server/models/config.js
+++ src/server/models/config.js
@@ -49,6 +49,7 @@ module.exports = function(crowi) {
       'security:pageCompleteDeletionAuthority' : undefined,
 
       'security:passport-local:isEnabled' : true,
+      'security:passport-local:isEnabledPasswordReset'  : true,
       'security:passport-ldap:isEnabled' : false,
       'security:passport-ldap:serverUrl' : undefined,
       'security:passport-ldap:isUserBind' : undefined,
