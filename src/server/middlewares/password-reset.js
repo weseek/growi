4cae922747447577a4a7a725863c62082dd82f9a
kaori <kaori@weseek.co.jp>
2021-07-31 22:58:06 +0900

    removed unnecessary 'if'

--- src/server/middlewares/password-reset.js
+++ src/server/middlewares/password-reset.js
@@ -4,10 +4,6 @@ module.exports = (crowi, app) => {
   return async(req, res, next) => {
     const { token } = req.params;
 
-    if (token == null) {
-      return res.redirect('/login');
-    }
-
     const passwordResetOrder = await PasswordResetOrder.findOne({ token });
     // check the oneTimeToken is valid
     if (passwordResetOrder == null || passwordResetOrder.isExpired()) {
