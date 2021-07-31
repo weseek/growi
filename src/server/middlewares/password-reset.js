c92ee05f523f8a0df33deddb1ec9c1791a8fc84b
kaori <kaori@weseek.co.jp>
2021-07-31 23:39:19 +0900

    improved null check

--- src/server/middlewares/password-reset.js
+++ src/server/middlewares/password-reset.js
@@ -4,6 +4,10 @@ module.exports = (crowi, app) => {
   return async(req, res, next) => {
     const { token, email } = req.query;
 
+    if (token == null || email == null) {
+      return res.redirect('/login');
+    }
+
     const passwordResetOrder = await PasswordResetOrder.findOne({ token, email });
     // check the oneTimeToken is valid
     if (passwordResetOrder == null || passwordResetOrder.isExpired()) {
