76d498f7d98d63e79cb0940301122bec3468c3fc
kaori <kaori@weseek.co.jp>
2021-08-01 22:22:57 +0900

    change default state

--- src/client/js/services/AdminLocalSecurityContainer.js
+++ src/client/js/services/AdminLocalSecurityContainer.js
@@ -22,7 +22,7 @@ export default class AdminLocalSecurityContainer extends Container {
       registrationMode: this.dummyRegistrationMode,
       registrationWhiteList: [],
       useOnlyEnvVars: false,
-      isPasswordResetEnabled: true,
+      isPasswordResetEnabled: false,
     };
 
   }
