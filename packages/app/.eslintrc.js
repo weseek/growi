52ba17a5c050464135b25c334a02974649b3a121
Yuki Takei <yuki@weseek.co.jp>
2021-08-02 14:51:00 +0000

    update .eslintrc

--- packages/app/.eslintrc.js
+++ packages/app/.eslintrc.js
@@ -18,5 +18,9 @@ module.exports = {
       name: 'axios',
       message: 'Please use src/utils/axios instead.',
     }],
+    // set 'warn' temporarily -- 2021.08.02 Yuki Takei
+    '@typescript-eslint/explicit-module-boundary-types': ['warn'],
+    '@typescript-eslint/no-use-before-define': ['warn'],
+    '@typescript-eslint/no-var-requires': ['warn'],
   },
 };
