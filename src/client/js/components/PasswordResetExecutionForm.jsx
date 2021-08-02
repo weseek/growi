ea491236e8a76303100caab3f8341f32eac4cc26
kaori <kaori@weseek.co.jp>
2021-08-03 07:01:23 +0900

    added onSubmit

--- src/client/js/components/PasswordResetExecutionForm.jsx
+++ src/client/js/components/PasswordResetExecutionForm.jsx
@@ -1,21 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
+import { toastSuccess, toastError } from '../util/apiNotification';
 
 
 const PasswordResetExecutionForm = (props) => {
-  const { t } = props;
+  const { t /* appContainer, personalContainer */ } = props;
+
+  const [newPassword, setNewPassword] = useState('');
+  const [newPasswordConfirm, setNewPasswordConfirm] = useState('');
+
+  // console.log(newPassword);
+  // console.log(newPasswordConfirm);
+
+  const onClickSubmit = async(e) => {
+    e.preventDefault();
+
+    if (newPassword === '' || newPasswordConfirm === '') {
+      toastError('err', t('personal_settings.password_is_not_set'));
+      return;
+    }
+
+    if (newPassword !== newPasswordConfirm) {
+      toastError('err', t('forgot_password.new_password_and_confirmation_are_not_same'));
+      return;
+    }
+
+    try {
+      // await appContainer.apiv3Put('/personal-setting/password', {
+      //   oldPassword, newPassword, newPasswordConfirm,
+      // });
+
+      setNewPassword();
+      setNewPasswordConfirm();
+      // this.setState({ oldPassword: '', newPassword: '', newPasswordConfirm: '' });
+      // await personalContainer.retrievePersonalData();
+      toastSuccess(t('toaster.update_successed', { target: t('Password') }));
+    }
+    catch (err) {
+      toastError(err);
+    }
+
+  };
 
   return (
-    <form role="form" className="form" method="post">
+    <form role="form" className="form" onSubmit={onClickSubmit}>
       <div className="form-group">
         <div className="input-group">
-          <input name="password" placeholder={t('forgot_password.new_password')} className="form-control" type="password" />
+          <input
+            name="password"
+            placeholder={t('forgot_password.new_password')}
+            className="form-control"
+            type="password"
+            onChange={e => setNewPassword(e.target.value)}
+          />
         </div>
       </div>
       <div className="form-group">
         <div className="input-group">
-          <input name="password" placeholder={t('forgot_password.confirm_new_password')} className="form-control" type="password" />
+          <input
+            name="password"
+            placeholder={t('forgot_password.confirm_new_password')}
+            className="form-control"
+            type="password"
+            onChange={e => setNewPasswordConfirm(e.target.value)}
+          />
         </div>
       </div>
       <div className="form-group">
