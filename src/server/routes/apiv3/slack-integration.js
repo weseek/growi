1ea40dec7a8362cea33433a9ffac8ffb7de6ee67
hakumizuki <montanha.masu536@gmail.com>
2021-08-04 11:19:47 +0000

    Modified a message

--- src/server/routes/apiv3/slack-integration.js
+++ src/server/routes/apiv3/slack-integration.js
@@ -75,7 +75,7 @@ module.exports = (crowi) => {
 
     // validate
     if (!supportedCommands.includes(command)) {
-      return res.status(403).send(`You are not allowed to run '${command}' command to this GROWI.`);
+      return res.status(403).send(`It is not allowed to run '${command}' command from this channel.`);
     }
 
     next();
