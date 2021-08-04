628364dd288bc9fc9000bab7d4c2187070fe53c4
Yuki Takei <yuki@weseek.co.jp>
2021-08-04 14:21:47 +0000

    mv logger config

--- packages/app/src/utils/logger/index.ts
+++ packages/app/src/utils/logger/index.ts
@@ -1,8 +1,8 @@
 import Logger from 'bunyan';
 import { createLogger } from 'universal-bunyan';
 
-import configForDev from '^/config/logger/config.dev';
-import configForProd from '^/config/logger/config.prod';
+import configForDev from '~/config/logger/config.dev';
+import configForProd from '~/config/logger/config.prod';
 
 const isProduction = process.env.NODE_ENV === 'production';
 const config = isProduction ? configForProd : configForDev;
