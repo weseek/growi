da0b4d91dfe8f1ad7f90abc578b2ab5c0f12b32d
Yuki Takei <yuki@weseek.co.jp>
2021-08-07 16:30:58 +0000

    fix module exporting of core

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,15 +1,14 @@
 import * as _pathUtils from './utils/path-utils';
 
+// module.exports = {
+//   BasicInterceptor: require('./utils/basic-interceptor'),
+//   envUtils: require('./utils/env-utils'),
+//   // plugin
+//   customTagUtils: require('./plugin/util/custom-tag-utils'),
+//   TagCacheManager: require('./plugin/service/tag-cache-manager'),
+//   // service
+//   LocalStorageManager: require('./service/localstorage-manager'),
+// };
+
 // export utils
 export const pathUtils = _pathUtils;
-
-
-module.exports = {
-  BasicInterceptor: require('./utils/basic-interceptor'),
-  envUtils: require('./utils/env-utils'),
-  // plugin
-  customTagUtils: require('./plugin/util/custom-tag-utils'),
-  TagCacheManager: require('./plugin/service/tag-cache-manager'),
-  // service
-  LocalStorageManager: require('./service/localstorage-manager'),
-};
