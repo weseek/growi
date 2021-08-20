6a7db70ed511bb5d35977fac0298d545d5a46d9f
Shun Miyazawa <shun-m@weseek.co.jp>
2021-08-20 06:16:12 +0000

    update

--- packages/app/src/components/WatchButton.jsx
+++ packages/app/src/components/WatchButton.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import { withTranslation } from 'react-i18next';
 
-const WatchButton = () => {
+
+const WatchButton = (props) => {
+
+  const handleClick = () => {
+    console.log('watch button clicked!');
+  };
 
   return (
     <>
-      <p>Hello Watch Button</p>
+      <button
+        type="button"
+        id="bookmark-button"
+        onClick={handleClick}
+        className={`btn btn-bookmark border-0 ${`btn-${props.size}`}`}
+      >
+        <i className="fa fa-eye"></i>
+      </button>
+
     </>
   );
 
 };
 
+WatchButton.propTypes = {
+  size: PropTypes.string,
+};
+
+WatchButton.defaultProps = {
+  size: 'md',
+};
 
 export default withTranslation()(WatchButton);
