94472b3d98d2522b19fb8d2bbcecf914bfbc6e1e
kaori <kaori@weseek.co.jp>
2021-08-24 14:35:45 +0900

    redirected to another view with error

--- packages/app/src/server/routes/forgot-password.js
+++ packages/app/src/server/routes/forgot-password.js
@@ -9,7 +9,22 @@ module.exports = function(crowi, app) {
 
   actions.resetPassword = async function(req, res) {
 
+    if (req.error != null) {
+      // if (req.error === 'Token not found') {
+      //   return res.json({ status: 404, error: req.error });
+      // }
+      const error = 'password-reset-order';
+      return res.render('forgot-password/error', { reason: error });
+
+      // if (req.error === 'passwordResetOrder is null or expired or revoked') {
+      //   return res.json({ status: 400, error: req.error });
+      // }
+    }
+
+    console.log('req.passwordResetOrder', req.passwordResetOrder);
+
     const { email } = req.passwordResetOrder;
+
     return res.render('reset-password', { email });
   };
 
