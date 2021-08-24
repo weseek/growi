5cdbba6aca589427a4e5914a0302c8ec6cba6c22
kaori <kaori@weseek.co.jp>
2021-08-25 01:30:38 +0900

    create activity model and activityDefine util

---
+++ packages/app/src/server/util/activityDefine.ts
@@ -0,0 +1,43 @@
+const MODEL_PAGE = 'Page';
+const MODEL_COMMENT = 'Comment';
+
+const ACTION_CREATE = 'CREATE'; // Not support yet
+const ACTION_MODIFY = 'MODIFY'; // Not support yet
+const ACTION_DELETE = 'DELETE'; // Not support yet
+const ACTION_COMMENT = 'COMMENT';
+const ACTION_LIKE = 'LIKE';
+
+const getSupportTargetModelNames = () => {
+  return [MODEL_PAGE];
+};
+
+const getSupportEventModelNames = () => {
+  return [MODEL_COMMENT];
+};
+
+const getSupportActionNames = () => {
+  return [
+    // ACTION_CREATE,
+    // ACTION_MODIFY,
+    // ACTION_DELETE,
+    ACTION_COMMENT,
+    ACTION_LIKE,
+  ];
+};
+
+const activityDefine = {
+  MODEL_PAGE,
+  MODEL_COMMENT,
+
+  ACTION_CREATE, // Not support yet
+  ACTION_MODIFY, // Not support yet
+  ACTION_DELETE, // Not support yet
+  ACTION_COMMENT,
+  ACTION_LIKE,
+
+  getSupportTargetModelNames,
+  getSupportEventModelNames,
+  getSupportActionNames,
+};
+
+export default activityDefine;
