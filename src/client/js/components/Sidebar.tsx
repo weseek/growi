06c4d4988ba582d8777134fbb771b0f07642fd0b
Shinichi Kondo <shinichi@weseek.co.jp>
2021-08-24 20:58:05 +0900

    refs GW-7081 unused NavigationUIController

--- src/client/js/components/Sidebar.tsx
+++ src/client/js/components/Sidebar.tsx
@@ -4,10 +4,6 @@ import React, {
 } from 'react';
 
 import {
-  ThemeProvider,
-} from '@atlaskit/navigation-next';
-
-import {
   useCurrentSidebarContents, useDrawerMode, useDrawerOpened, usePreferDrawerModeByUser,
   useCurrentProductNavWidth, useSidebarCollapsed, useSidebarResizeDisabled
 } from '~/stores/ui';
@@ -277,75 +273,68 @@ const Sidebar = (props: Props) => {
   return (
     <>
       <div className={`grw-sidebar d-print-none ${isDrawerMode ? 'grw-sidebar-drawer' : ''} ${isDrawerOpened ? 'open' : ''}`}>
-        <ThemeProvider
-          theme={theme => ({
-          ...theme,
-          context: 'product',
-        })}
-        >
-          <div className="data-layout-container">
-            <div className="navigation">
-              <div className="grw-navigation-wrap">
-                <div className="grw-global-navigation">
-                { isMounted ? <GlobalNavigation></GlobalNavigation> : <GlobalNavigationSkelton></GlobalNavigationSkelton> }
-                </div>
-                <div
-                  ref={resizableContainer}
-                  className="grw-contextual-navigation"
-                  onMouseEnter={() => hoverHandler(true)}
-                  onMouseLeave={() => hoverHandler(false)}
-                  onMouseMove={draggableAreaMoveHandler}
-                  onMouseUp={dragableAreaMouseUpHandler}
-                  style={{ width: isCollapsed ? sidebarMinimizeWidth : currentProductNavWidth }}
-                >
-                  <div className="grw-contextual-navigation-child">
-                    <div role="group" className="grw-contextual-navigation-sub"></div>
-                  </div>
-                  <div className="grw-contextual-navigation-child2">
-                    <div role="group" className={`grw-contextual-navigation-sub ${!isHover && isCollapsed ? 'collapsed' : ''}`}>
-                    { isMounted ? <SidebarContents></SidebarContents> : <SidebarSkeltonContents></SidebarSkeltonContents> }
-                    </div>
-                  </div>
-                </div>
+        <div className="data-layout-container">
+          <div className="navigation">
+            <div className="grw-navigation-wrap">
+              <div className="grw-global-navigation">
+              { isMounted ? <GlobalNavigation></GlobalNavigation> : <GlobalNavigationSkelton></GlobalNavigationSkelton> }
               </div>
-              <div className="grw-navigation-draggable">
-                <div className="grw-navigation-draggable-sub"></div>
-                <div
-                  className={`${!isDrawerMode ? 'resizable' : ''} grw-navigation-draggable-hitarea`}
-                  onMouseDown={dragableAreaClickHandler}
-                >
-                  <div className="grw-navigation-draggable-hitarea-child"></div>
+              <div
+                ref={resizableContainer}
+                className="grw-contextual-navigation"
+                onMouseEnter={() => hoverHandler(true)}
+                onMouseLeave={() => hoverHandler(false)}
+                onMouseMove={draggableAreaMoveHandler}
+                onMouseUp={dragableAreaMouseUpHandler}
+                style={{ width: isCollapsed ? sidebarMinimizeWidth : currentProductNavWidth }}
+              >
+                <div className="grw-contextual-navigation-child">
+                  <div role="group" className="grw-contextual-navigation-sub"></div>
                 </div>
-                <div>
-                  <div>
-                    <button
-                      className={`ak-navigation-resize-button ${!isDrawerMode ? 'resizable' : ''} ${isCollapsed ? 'collapse-state' : 'normal-state'} `}
-                      type="button"
-                      aria-expanded="true"
-                      aria-label="Toggle navigation"
-                      disabled={isDrawerMode}
-                      onClick={toggleNavigationBtnClickHandler}
-                    >
-                      <div className="css-z8pkji"></div>
-                      <span role="presentation" className="sc-AxjAm jMDUxe">
-                        <svg width="24" height="24" viewBox="0 0 24 24" focusable="false" role="presentation">
-                          <path
-                            d="M13.706 9.698a.988.988 0 0 0 0-1.407
-                            1.01 1.01 0 0 0-1.419 0l-2.965 2.94a1.09 1.09 0 0 0 0 1.548l2.955
-                            2.93a1.01 1.01 0 0 0 1.42 0 .988.988 0 0 0 0-1.407l-2.318-2.297 2.327-2.307z"
-                            fill="currentColor"
-                            fillRule="evenodd"
-                          >
-                          </path>
-                        </svg>
-                      </span>
-                    </button>
+                <div className="grw-contextual-navigation-child2">
+                  <div role="group" className={`grw-contextual-navigation-sub ${!isHover && isCollapsed ? 'collapsed' : ''}`}>
+                  { isMounted ? <SidebarContents></SidebarContents> : <SidebarSkeltonContents></SidebarSkeltonContents> }
                   </div>
                 </div>
               </div>
             </div>
+            <div className="grw-navigation-draggable">
+              <div className="grw-navigation-draggable-sub"></div>
+              <div
+                className={`${!isDrawerMode ? 'resizable' : ''} grw-navigation-draggable-hitarea`}
+                onMouseDown={dragableAreaClickHandler}
+              >
+                <div className="grw-navigation-draggable-hitarea-child"></div>
+              </div>
+              <div>
+                <div>
+                  <button
+                    className={`ak-navigation-resize-button ${!isDrawerMode ? 'resizable' : ''} ${isCollapsed ? 'collapse-state' : 'normal-state'} `}
+                    type="button"
+                    aria-expanded="true"
+                    aria-label="Toggle navigation"
+                    disabled={isDrawerMode}
+                    onClick={toggleNavigationBtnClickHandler}
+                  >
+                    <div className="css-z8pkji"></div>
+                    <span role="presentation" className="sc-AxjAm jMDUxe">
+                      <svg width="24" height="24" viewBox="0 0 24 24" focusable="false" role="presentation">
+                        <path
+                          d="M13.706 9.698a.988.988 0 0 0 0-1.407
+                          1.01 1.01 0 0 0-1.419 0l-2.965 2.94a1.09 1.09 0 0 0 0 1.548l2.955
+                          2.93a1.01 1.01 0 0 0 1.42 0 .988.988 0 0 0 0-1.407l-2.318-2.297 2.327-2.307z"
+                          fill="currentColor"
+                          fillRule="evenodd"
+                        >
+                        </path>
+                      </svg>
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
-        </ThemeProvider>
+        </div>
       </div>
 
       { isDrawerOpened && (
