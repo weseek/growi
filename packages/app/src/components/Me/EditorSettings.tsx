55c2da72f5c77fc6f607a70a3394683d3b16e680
kaori <kaori@weseek.co.jp>
2021-08-30 16:49:27 +0900

    created new tsx file

---
+++ packages/app/src/components/Me/EditorSettings.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import PropTypes from 'prop-types';
+
+
+type Props = {
+}
+
+export const EditorSettings: FC<Props> = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div>hge</div>
+  );
+};
