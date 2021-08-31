45618c9be7e42155360acf3de75be1d5d5cc9f24
Shinichi Kondo <shinichi@weseek.co.jp>
2021-08-31 15:14:28 +0900

    refs GW-7078 fix style and tag structure remove unnecessary tags

--- src/client/js/components/Sidebar.tsx
+++ src/client/js/components/Sidebar.tsx
@@ -302,29 +302,27 @@ const Sidebar = (props: Props) => {
               >
                 <div className="grw-navigation-draggable-hitarea-child"></div>
               </div>
-              <div>
-                <button
-                  className={`ak-navigation-resize-button ${!isDrawerMode ? 'resizable' : ''} ${isCollapsed ? 'collapse-state' : 'normal-state'} `}
-                  type="button"
-                  aria-expanded="true"
-                  aria-label="Toggle navigation"
-                  disabled={isDrawerMode}
-                  onClick={toggleNavigationBtnClickHandler}
-                >
-                  <span role="presentation" className="jMDUxe">
-                    <svg width="24" height="24" viewBox="0 0 24 24" focusable="false" role="presentation">
-                      <path
-                        d="M13.706 9.698a.988.988 0 0 0 0-1.407
-                        1.01 1.01 0 0 0-1.419 0l-2.965 2.94a1.09 1.09 0 0 0 0 1.548l2.955
-                        2.93a1.01 1.01 0 0 0 1.42 0 .988.988 0 0 0 0-1.407l-2.318-2.297 2.327-2.307z"
-                        fill="currentColor"
-                        fillRule="evenodd"
-                      >
-                      </path>
-                    </svg>
-                  </span>
-                </button>
-              </div>
+              <button
+                className={`ak-navigation-resize-button ${!isDrawerMode ? 'resizable' : ''} ${isCollapsed ? 'collapse-state' : 'normal-state'} `}
+                type="button"
+                aria-expanded="true"
+                aria-label="Toggle navigation"
+                disabled={isDrawerMode}
+                onClick={toggleNavigationBtnClickHandler}
+              >
+                <span role="presentation" className="jMDUxe">
+                  <svg width="24" height="24" viewBox="0 0 24 24" focusable="false" role="presentation">
+                    <path
+                      d="M13.706 9.698a.988.988 0 0 0 0-1.407
+                      1.01 1.01 0 0 0-1.419 0l-2.965 2.94a1.09 1.09 0 0 0 0 1.548l2.955
+                      2.93a1.01 1.01 0 0 0 1.42 0 .988.988 0 0 0 0-1.407l-2.318-2.297 2.327-2.307z"
+                      fill="currentColor"
+                      fillRule="evenodd"
+                    >
+                    </path>
+                  </svg>
+                </span>
+              </button>
             </div>
           </div>
         </div>
