cac44d12a8b11a470e0045fafa5fec841bce83c7
stevenfukase <fukasesteven@gmail.com>
2021-09-03 15:21:26 +0900

    revert validator

--- packages/app/src/server/routes/apiv3/personal-setting.js
+++ packages/app/src/server/routes/apiv3/personal-setting.js
@@ -1,4 +1,4 @@
-import { body, checkSchema } from 'express-validator';
+import { body } from 'express-validator';
 
 import loggerFactory from '~/utils/logger';
 
@@ -13,7 +13,6 @@ const express = require('express');
 const passport = require('passport');
 
 const router = express.Router();
-
 
 /**
  * @swagger
