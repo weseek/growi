26c7c33491b097d423279bbbe8bbd70661d7ada1
Yuki Takei <yuki@weseek.co.jp>
2021-09-03 10:34:32 +0000

    add files

--- bump-versions.config.js
+++ bump-versions.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   monorepo: {
     mainVersionFile: 'package.json',
-    packagesToBump: ['packages/app', 'packages/common'],
+    packagesToBump: [
+      'packages/app',
+      'packages/core',
+      'packages/slack',
+      'packages/ui',
+      'packages/plugin-*',
+    ],
   },
 };
