936e2c53624cff3ba42d7e8b7f870057575a47ba
kaori <kaori@weseek.co.jp>
2021-09-08 19:14:54 +0900

    init notificationEvent

--- packages/app/src/server/service/notification.ts
+++ packages/app/src/server/service/notification.ts
@@ -2,6 +2,18 @@
 
 class NortificationService {
 
+  crowi!: any;
+
+  notificationEvent!: any;
+
+
+  constructor(crowi) {
+    this.crowi = crowi;
+    this.notificationEvent = crowi.event('notification');
+
+    // init
+    this.notificationEvent.on('update', this.notificationEvent.onUpdate);
+  }
 
 }
 
