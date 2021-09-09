e87586e38f98e921ebeb23da8b0ff83438ca95d3
Shun Miyazawa <shun-m@weseek.co.jp>
2021-09-09 12:47:27 +0000

    cleanup

--- packages/app/src/server/routes/ogp.ts
+++ packages/app/src/server/routes/ogp.ts
@@ -1,13 +1,13 @@
-module.exports = function(crowi, app) {
-  return {
-    ogp(req, res) {
-      console.log('This is the route to display the ogp image');
-      console.log(req.params);
+import {
+  Request, Response,
+} from 'express';
 
-      // TODO: GW-7369 OGP server にリクエスト、レスポンスの画像を pipe して返却できるようにする
-      // TODO: pageId から path を解決、OGP image の表示判断を行う
+export const renderOgp = (req: Request, res: Response): void => {
+  console.log('This is the route to display the ogp image');
+  console.log(req.params);
 
-      return res.end();
-    },
-  };
+  // TODO: GW-7369 OGP server にリクエスト、レスポンスの画像を pipe して返却できるようにする
+  // TODO: pageId から path を解決、OGP image の表示判断を行う
+
+  return res.end();
 };
