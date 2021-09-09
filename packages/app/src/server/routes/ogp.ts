57740366779e344a77f59e69a81336aee218ce52
Shun Miyazawa <shun-m@weseek.co.jp>
2021-09-09 12:56:17 +0000

    returns 400 if the parameter is empty

--- packages/app/src/server/routes/ogp.ts
+++ packages/app/src/server/routes/ogp.ts
@@ -2,12 +2,16 @@ import {
   Request, Response,
 } from 'express';
 
-export const renderOgp = (req: Request, res: Response): void => {
+export const renderOgp = (req: Request, res: Response): Response => {
   console.log('This is the route to display the ogp image');
   console.log(req.params);
+
+  if (req.params.pageId === '') {
+    return res.status(400).send();
+  }
 
   // TODO: GW-7369 OGP server にリクエスト、レスポンスの画像を pipe して返却できるようにする
   // TODO: pageId から path を解決、OGP image の表示判断を行う
 
-  return res.end();
+  return res.status(200).send();
 };
