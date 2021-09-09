6b470707b1f33c4664818ed2913b1b90cfe8cf99
Shun Miyazawa <shun-m@weseek.co.jp>
2021-09-09 03:28:36 +0000

    add TODO

--- packages/app/src/server/routes/ogp.js
+++ packages/app/src/server/routes/ogp.js
@@ -2,6 +2,11 @@ module.exports = function(crowi, app) {
   return {
     ogp(req, res) {
       console.log('This is the route to display the ogp image');
+      console.log(req.params);
+
+      // TODO: GW-7369 OGP server にリクエスト、レスポンスの画像を pipe して返却できるようにする
+      // TODO: pageId から path を解決、OGP image の表示判断を行う
+
       return res.end();
     },
   };
