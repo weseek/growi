5022c0cb1ae3835601218f45302f091c2f0154c1
kaori <kaori@weseek.co.jp>
2021-09-13 13:18:45 +0900

    clean code

--- packages/app/src/server/service/in-app-notification.ts
+++ packages/app/src/server/service/in-app-notification.ts
@@ -22,11 +22,6 @@ class InAppNotificationService {
   updateCommentEvent(): void {
     this.commentEvent.on('update', (user) => {
       this.commentEvent.onUpdate();
-
-      // TODO: socket.on on the client side by GW-7402
-      // if (this.socketIoService.isInitialized) {
-      //   this.socketIoService.getDefaultSocket().emit('comment updated', { user });
-      // }
     });
   }
 
