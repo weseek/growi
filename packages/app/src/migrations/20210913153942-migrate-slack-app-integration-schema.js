64af8d7df0ec62cb9cc52c20e2441eed8dbb0090
Taichi Masuyama <montanha.masu536@gmail.com>
2021-09-13 10:21:16 +0000

    Fixed linting

--- packages/app/src/migrations/20210913153942-migrate-slack-app-integration-schema.js
+++ packages/app/src/migrations/20210913153942-migrate-slack-app-integration-schema.js
@@ -95,7 +95,7 @@ module.exports = {
               },
             },
             {
-              $unset: ['permissionsForBroadcastUseCommands', 'permissionsForSingleUseCommands']
+              $unset: ['permissionsForBroadcastUseCommands', 'permissionsForSingleUseCommands'],
             },
           ],
         },
