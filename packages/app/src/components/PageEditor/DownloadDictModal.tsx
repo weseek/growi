bf40348426848d94684da46c43b2f96d6fe564ce
Steven Fukase <fukasesteven@gmail.com>
2021-09-22 09:32:09 +0000

    wip modal

--- packages/app/src/components/PageEditor/DownloadDictModal.tsx
+++ packages/app/src/components/PageEditor/DownloadDictModal.tsx
@@ -2,10 +2,11 @@ import React, { useState, FC } from 'react';
 import {
   Modal, ModalHeader, ModalBody, ModalFooter,
 } from 'reactstrap';
+import { useTranslation } from 'react-i18next';
 
 export const DownloadDictModal: FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(true);
-
+  const { t } = useTranslation('');
   return (
     <Modal isOpen={isModalOpen} toggle={() => setIsModalOpen(false)} className="">
       <ModalHeader tag="h4" toggle={() => setIsModalOpen(false)} className="bg-warning">
@@ -14,25 +15,23 @@ export const DownloadDictModal: FC = () => {
       </ModalHeader>
       <ModalBody>
         Are you sure you want to download the dictionary file?
-        <ModalFooter>
-          {/* <div className="d-flex justify-content-end"> */}
-          <button
-            type="button"
-            className="btn btn-outline-secondary"
-            onClick={() => console.log('button')}
-          >
-              Cancel
-          </button>
-          <button
-            type="button"
-            className="btn btn-outline-primary ml-3"
-            onClick={() => console.log('button')}
-          >
-              Enable
-          </button>
-          {/* </div> */}
-        </ModalFooter>
       </ModalBody>
+      <ModalFooter>
+        <button
+          type="button"
+          className="btn btn-outline-secondary"
+          onClick={() => console.log('button')}
+        >
+          {t('Cancel')}
+        </button>
+        <button
+          type="button"
+          className="btn btn-outline-primary ml-3"
+          onClick={() => console.log('button')}
+        >
+          {t('Enable')}
+        </button>
+      </ModalFooter>
     </Modal>
   );
 };
