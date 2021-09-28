f1ba0fa0437469543483b0b044aab54243379029
kaori <kaori@weseek.co.jp>
2021-09-28 23:40:59 +0900

    comment out err

--- packages/app/src/components/InAppNotification.tsx/InAppNotificationList.tsx
+++ packages/app/src/components/InAppNotification.tsx/InAppNotificationList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FC } from 'react';
 // import UserPicture from '../User/UserPicture'
 
 // import PageCommentNotification from './ModelAction/PageCommentNotification'
@@ -13,65 +13,70 @@ interface Props {
   onClick: any
 }
 
-export default class InAppNotificationList extends React.Component<Props> {
+// export default class InAppNotificationList extends React.Component<Props> {
+export const InAppNotificationList = (props: Props): JSX.Element => {
 
-  onClick() {
-    this.props.onClick(this.props.notification);
-  }
+  // onClick() {
+  //   props.onClick(props.notification);
+  // }
 
-  getActionUsers() {
-    const notification = this.props.notification;
-    const latestActionUsers = notification.actionUsers.slice(0, 3);
-    const latestUsers = latestActionUsers.map((user) => {
-      return `@${user.username}`;
-    });
+  // const getActionUsers = () => {
+  //   const notification = props.notification;
+  //   const latestActionUsers = notification.actionUsers.slice(0, 3);
+  //   const latestUsers = latestActionUsers.map((user) => {
+  //     return `@${user.username}`;
+  //   });
 
-    let actionedUsers = '';
-    const latestUsersCount = latestUsers.length;
-    if (latestUsersCount === 1) {
-      actionedUsers = latestUsers[0];
-    }
-    else if (notification.actionUsers.length >= 4) {
-      actionedUsers = `${latestUsers.slice(0, 2).join(', ')} and ${notification.actionUsers.length - 2} others`;
-    }
-    else {
-      actionedUsers = latestUsers.join(', ');
-    }
+  //   let actionedUsers = '';
+  //   const latestUsersCount = latestUsers.length;
+  //   if (latestUsersCount === 1) {
+  //     actionedUsers = latestUsers[0];
+  //   }
+  //   else if (notification.actionUsers.length >= 4) {
+  //     actionedUsers = `${latestUsers.slice(0, 2).join(', ')} and ${notification.actionUsers.length - 2} others`;
+  //   }
+  //   else {
+  //     actionedUsers = latestUsers.join(', ');
+  //   }
 
-    return actionedUsers;
-  }
+  //   return actionedUsers;
+  // };
 
-  getUserImage() {
-    const latestActionUsers = this.props.notification.actionUsers.slice(0, 3);
+  const getUserImage = () => {
+    // const latestActionUsers = props.notification.actionUsers.slice(0, 3);
 
-    if (latestActionUsers.length < 1) {
-      // what is this case?
-      return '';
-    }
+    // if (latestActionUsers.length < 1) {
+    // what is this case?
+    //   return '';
+    // }
 
     // return <UserPicture user={latestActionUsers[0]} />;
     return;
-  }
+  };
 
-  render() {
-    const notification = this.props.notification;
-    const componentName = `${notification.targetModel}:${notification.action}`;
-    const props = {
-      actionUsers: this.getActionUsers(),
-      ...this.props,
-    };
+  const Render = () => {
+    // const notification = props.notification;
+    // const componentName = `${notification.targetModel}:${notification.action}`;
+    // const props = {
+    //   actionUsers: this.getActionUsers(),
+    //   ...props,
+    // };
 
-    switch (componentName) {
-      case 'Page:COMMENT':
-        // return <PageCommentNotification {...props} onClick={this.onClick.bind(this)} />;
-        return;
-      case 'Page:LIKE':
-        // return <PageLikeNotification {...props} onClick={this.onClick.bind(this)} />
-        return;
-      default:
-    }
+    // switch (componentName) {
+    //   case 'Page:COMMENT':
+    //     // return <PageCommentNotification {...props} onClick={this.onClick.bind(this)} />;
+    //     return;
+    //   case 'Page:LIKE':
+    //     // return <PageLikeNotification {...props} onClick={this.onClick.bind(this)} />
+    //     return;
+    //   default:
+    // }
 
-    return '';
-  }
+    return;
+  };
 
-}
+  return (
+    <>InAppNotificationList</>
+  );
+
+};
