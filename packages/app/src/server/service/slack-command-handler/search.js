7e77edc2ed16e55d47c88ed5e21676fc8a0721c1
Yuki Takei <yuki@weseek.co.jp>
2021-09-29 20:29:49 +0000

    commonize generating search result blocks

--- packages/app/src/server/service/slack-command-handler/search.js
+++ packages/app/src/server/service/slack-command-handler/search.js
@@ -10,26 +10,55 @@ const SlackbotError = require('../../models/vo/slackbot-error');
 
 const PAGINGLIMIT = 7;
 
+
 module.exports = (crowi) => {
   const BaseSlackCommandHandler = require('./slack-command-handler');
   const handler = new BaseSlackCommandHandler(crowi);
 
-  handler.handleCommand = async function(growiCommand, client, body) {
-    const { responseUrl, growiCommandArgs } = growiCommand;
-    let searchResult;
-    try {
-      searchResult = await this.retrieveSearchResults(responseUrl, client, body, growiCommandArgs);
-    }
-    catch (err) {
-      logger.error('Failed to get search results.', err);
-      throw new SlackbotError({
-        method: 'postEphemeral',
-        to: 'channel',
-        popupMessage: 'Failed To Search',
-        mainMessage: '*Failed to search.*\n Hint\n `/growi search [keyword]`',
-      });
-    }
 
+  function getKeywords(growiCommandArgs) {
+    const keywords = growiCommandArgs.join(' ');
+    return keywords;
+  }
+
+  function appendSpeechBaloon(mrkdwn, commentCount) {
+    return (commentCount != null && commentCount > 0)
+      ? `${mrkdwn}   :speech_balloon: ${commentCount}`
+      : mrkdwn;
+  }
+
+  function generatePageLinkMrkdwn(pathname, href) {
+    return `<${decodeURI(href)} | ${decodeURI(pathname)}>`;
+  }
+
+  function generateLastUpdateMrkdwn(updatedAt, baseDate) {
+    if (updatedAt != null) {
+      // cast to date
+      const date = new Date(updatedAt);
+      return formatDistanceStrict(date, baseDate);
+    }
+    return '';
+  }
+
+  async function retrieveSearchResults(growiCommandArgs, offset = 0) {
+    const keywords = getKeywords(growiCommandArgs);
+
+    const { searchService } = crowi;
+    const options = { limit: PAGINGLIMIT, offset };
+    const results = await searchService.searchKeyword(keywords, null, {}, options);
+    const resultsTotal = results.meta.total;
+
+    const pages = results.data.map((data) => {
+      const { path, updated_at: updatedAt, comment_count: commentCount } = data._source;
+      return { path, updatedAt, commentCount };
+    });
+
+    return {
+      pages, offset, resultsTotal,
+    };
+  }
+
+  function buildRespondBodyForSearchResult(searchResult, growiCommandArgs) {
     const appUrl = crowi.appService.getSiteUrl();
     const appTitle = crowi.appService.getAppTitle();
 
@@ -37,22 +66,9 @@ module.exports = (crowi) => {
       pages, offset, resultsTotal,
     } = searchResult;
 
-    const keywords = this.getKeywords(growiCommandArgs);
-
+    const keywords = getKeywords(growiCommandArgs);
 
     let searchResultsDesc;
-
-    if (resultsTotal === 0 || resultsTotal == null) {
-      if (keywords === '') return;
-      await respond(responseUrl, {
-        text: 'No page found.',
-        blocks: [
-          markdownSectionBlock(`No page found. keyword(s): *"${keywords}"*`),
-          markdownSectionBlock('Please try other keywords.'),
-        ],
-      });
-      return;
-    }
     switch (resultsTotal) {
       case 1:
         searchResultsDesc = `*${resultsTotal}* page is found.`;
@@ -61,7 +77,6 @@ module.exports = (crowi) => {
         searchResultsDesc = `*${resultsTotal}* pages are found.`;
         break;
     }
-
 
     const contextBlock = {
       type: 'context',
@@ -89,8 +104,8 @@ module.exports = (crowi) => {
           type: 'section',
           text: {
             type: 'mrkdwn',
-            text: `${this.appendSpeechBaloon(`*${this.generatePageLinkMrkdwn(pathname, href)}*`, commentCount)}`
-              + `  \`${this.generateLastUpdateMrkdwn(updatedAt, now)}\``,
+            text: `${appendSpeechBaloon(`*${generatePageLinkMrkdwn(pathname, href)}*`, commentCount)}`
+              + `  \`${generateLastUpdateMrkdwn(updatedAt, now)}\``,
           },
           accessory: {
             type: 'button',
@@ -131,16 +146,71 @@ module.exports = (crowi) => {
             text: 'Next',
           },
           action_id: 'search:showNextResults',
-          value: JSON.stringify({ offset, body, growiCommandArgs }),
+          value: JSON.stringify({ offset, growiCommandArgs }),
         },
       );
     }
     blocks.push(actionBlocks);
 
-    await respond(responseUrl, {
+    return {
       text: 'Successed To Search',
       blocks,
-    });
+    };
+  }
+
+
+  async function buildRespondBody(growiCommandArgs) {
+    const firstKeyword = growiCommandArgs[0];
+
+    // enpty keyword
+    if (firstKeyword == null) {
+      return {
+        text: 'Input keywords',
+        blocks: [
+          markdownSectionBlock('*Input keywords.*\n Hint\n `/growi search [keyword]`'),
+        ],
+      };
+    }
+
+    const searchResult = await retrieveSearchResults(growiCommandArgs);
+
+    // no search results
+    if (searchResult.resultsTotal === 0) {
+      const keywords = getKeywords(growiCommandArgs);
+      logger.info(`No page found with "${keywords}"`);
+
+      return {
+        text: `No page found with "${keywords}"`,
+        blocks: [
+          markdownSectionBlock(`*No page matches your keyword(s) "${keywords}".*`),
+          markdownSectionBlock(':mag: *Help: Searching*'),
+          divider(),
+          markdownSectionBlock('`word1` `word2` (divide with space) \n Search pages that include both word1, word2 in the title or body'),
+          divider(),
+          markdownSectionBlock('`"This is GROWI"` (surround with double quotes) \n Search pages that include the phrase "This is GROWI"'),
+          divider(),
+          markdownSectionBlock('`-keyword` \n Exclude pages that include keyword in the title or body'),
+          divider(),
+          markdownSectionBlock('`prefix:/user/` \n Search only the pages that the title start with /user/'),
+          divider(),
+          markdownSectionBlock('`-prefix:/user/` \n Exclude the pages that the title start with /user/'),
+          divider(),
+          markdownSectionBlock('`tag:wiki` \n Search for pages with wiki tag'),
+          divider(),
+          markdownSectionBlock('`-tag:wiki` \n Exclude pages with wiki tag'),
+        ],
+      };
+    }
+
+    return buildRespondBodyForSearchResult(searchResult, growiCommandArgs);
+  }
+
+
+  handler.handleCommand = async function(growiCommand, client, body) {
+    const { responseUrl, growiCommandArgs } = growiCommand;
+
+    const respondBody = await buildRespondBody(growiCommandArgs);
+    await respond(responseUrl, respondBody);
   };
 
   handler.handleInteractions = async function(client, interactionPayload, interactionPayloadAccessor, handlerMethodName) {
@@ -205,131 +275,12 @@ module.exports = (crowi) => {
     }
     const parsedValue = JSON.parse(value);
 
-    const { body, growiCommandArgs, offset: offsetNum } = parsedValue;
+    const { growiCommandArgs, offset: offsetNum } = parsedValue;
     const newOffsetNum = offsetNum + PAGINGLIMIT;
-    let searchResult;
-    try {
-      searchResult = await this.retrieveSearchResults(responseUrl, client, body, growiCommandArgs, newOffsetNum);
-    }
-    catch (err) {
-      logger.error('Failed to get search results.', err);
-      throw new SlackbotError({
-        method: 'postEphemeral',
-        to: 'channel',
-        popupMessage: 'Failed To Search',
-        mainMessage: '*Failed to search.*\n Hint\n `/growi search [keyword]`',
-      });
-    }
 
-    const appUrl = crowi.appService.getSiteUrl();
-    const appTitle = crowi.appService.getAppTitle();
+    const searchResult = await retrieveSearchResults(growiCommandArgs, newOffsetNum);
 
-    const {
-      pages, offset, resultsTotal,
-    } = searchResult;
-
-    const keywords = this.getKeywords(growiCommandArgs);
-
-
-    let searchResultsDesc;
-
-    if (resultsTotal === 0 || resultsTotal == null) {
-      if (keywords === '') return;
-      await respond(responseUrl, {
-        text: 'No page found.',
-        blocks: [
-          markdownSectionBlock('Please try with other keywords.'),
-        ],
-      });
-      return;
-    }
-    switch (resultsTotal) {
-      case 1:
-        searchResultsDesc = `*${resultsTotal}* page is found.`;
-        break;
-      default:
-        searchResultsDesc = `*${resultsTotal}* pages are found.`;
-        break;
-    }
-
-    const contextBlock = {
-      type: 'context',
-      elements: [
-        {
-          type: 'mrkdwn',
-          text: `keyword(s) : *"${keywords}"*  |  Current: ${offset + 1} - ${offset + pages.length}  |  Total ${resultsTotal} pages`,
-        },
-      ],
-    };
-
-    const now = new Date();
-    const blocks = [
-      markdownSectionBlock(`:mag: <${decodeURI(appUrl)}|*${appTitle}*>\n${searchResultsDesc}`),
-      contextBlock,
-      { type: 'divider' },
-      // create an array by map and extract
-      ...pages.map((page) => {
-        const { path, updatedAt, commentCount } = page;
-        // generate URL
-        const url = new URL(path, appUrl);
-        const { href, pathname } = url;
-
-        return {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: `${this.appendSpeechBaloon(`*${this.generatePageLinkMrkdwn(pathname, href)}*`, commentCount)}`
-              + `\n    Last updated: ${this.generateLastUpdateMrkdwn(updatedAt, now)}`,
-          },
-          accessory: {
-            type: 'button',
-            action_id: 'search:shareSinglePageResult',
-            text: {
-              type: 'plain_text',
-              text: 'Share',
-            },
-            value: JSON.stringify({ page, href, pathname }),
-          },
-        };
-      }),
-      { type: 'divider' },
-      contextBlock,
-    ];
-
-    const actionBlocks = {
-      type: 'actions',
-      elements: [
-        {
-          type: 'button',
-          text: {
-            type: 'plain_text',
-            text: 'Dismiss',
-          },
-          style: 'danger',
-          action_id: 'search:dismissSearchResults',
-        },
-      ],
-    };
-    // show "Next" button if next page exists
-    if (resultsTotal > offset + PAGINGLIMIT) {
-      actionBlocks.elements.unshift(
-        {
-          type: 'button',
-          text: {
-            type: 'plain_text',
-            text: 'Next',
-          },
-          action_id: 'search:showNextResults',
-          value: JSON.stringify({ offset, body, growiCommandArgs }),
-        },
-      );
-    }
-    blocks.push(actionBlocks);
-
-    await respond(responseUrl, {
-      text: 'Successed To Search',
-      blocks,
-    });
+    await respond(responseUrl, buildRespondBodyForSearchResult(searchResult, growiCommandArgs));
   };
 
   handler.dismissSearchResults = async function(client, payload) {
@@ -338,86 +289,6 @@ module.exports = (crowi) => {
     return deleteOriginal(responseUrl, {
       delete_original: true,
     });
-  };
-
-  handler.retrieveSearchResults = async function(responseUrl, client, body, growiCommandArgs, offset = 0) {
-    const firstKeyword = growiCommandArgs[0];
-    if (firstKeyword == null) {
-      await respond(responseUrl, {
-        text: 'Input keywords',
-        blocks: [
-          markdownSectionBlock('*Input keywords.*\n Hint\n `/growi search [keyword]`'),
-        ],
-      });
-      return { pages: [] };
-    }
-
-    const keywords = this.getKeywords(growiCommandArgs);
-
-    const { searchService } = crowi;
-    const options = { limit: PAGINGLIMIT, offset };
-    const results = await searchService.searchKeyword(keywords, null, {}, options);
-    const resultsTotal = results.meta.total;
-
-    // no search results
-    if (results.data.length === 0) {
-      logger.info(`No page found with "${keywords}"`);
-      await respond(responseUrl, {
-        text: `No page found with "${keywords}"`,
-        blocks: [
-          markdownSectionBlock(`*No page matches your keyword(s) "${keywords}".*`),
-          markdownSectionBlock(':mag: *Help: Searching*'),
-          divider(),
-          markdownSectionBlock('`word1` `word2` (divide with space) \n Search pages that include both word1, word2 in the title or body'),
-          divider(),
-          markdownSectionBlock('`"This is GROWI"` (surround with double quotes) \n Search pages that include the phrase "This is GROWI"'),
-          divider(),
-          markdownSectionBlock('`-keyword` \n Exclude pages that include keyword in the title or body'),
-          divider(),
-          markdownSectionBlock('`prefix:/user/` \n Search only the pages that the title start with /user/'),
-          divider(),
-          markdownSectionBlock('`-prefix:/user/` \n Exclude the pages that the title start with /user/'),
-          divider(),
-          markdownSectionBlock('`tag:wiki` \n Search for pages with wiki tag'),
-          divider(),
-          markdownSectionBlock('`-tag:wiki` \n Exclude pages with wiki tag'),
-        ],
-      });
-      return { pages: [] };
-    }
-
-    const pages = results.data.map((data) => {
-      const { path, updated_at: updatedAt, comment_count: commentCount } = data._source;
-      return { path, updatedAt, commentCount };
-    });
-
-    return {
-      pages, offset, resultsTotal,
-    };
-  };
-
-  handler.getKeywords = function(growiCommandArgs) {
-    const keywords = growiCommandArgs.join(' ');
-    return keywords;
-  };
-
-  handler.appendSpeechBaloon = function(mrkdwn, commentCount) {
-    return (commentCount != null && commentCount > 0)
-      ? `${mrkdwn}   :speech_balloon: ${commentCount}`
-      : mrkdwn;
-  };
-
-  handler.generatePageLinkMrkdwn = function(pathname, href) {
-    return `<${decodeURI(href)} | ${decodeURI(pathname)}>`;
-  };
-
-  handler.generateLastUpdateMrkdwn = function(updatedAt, baseDate) {
-    if (updatedAt != null) {
-      // cast to date
-      const date = new Date(updatedAt);
-      return formatDistanceStrict(date, baseDate);
-    }
-    return '';
   };
 
   return handler;
