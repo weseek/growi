e328925ff416826e2858b38b906e5890c3ce5ed2
Taichi Masuyama <montanha.masu536@gmail.com>
2021-10-05 15:17:43 +0000

    Modified condition

--- packages/slackbot-proxy/src/services/SystemInformationService.ts
+++ packages/slackbot-proxy/src/services/SystemInformationService.ts
@@ -1,6 +1,5 @@
 import { Inject, Service } from '@tsed/di';
 
-import compareVersions from 'compare-versions';
 import readPkgUp from 'read-pkg-up';
 
 import { SystemInformation } from '~/entities/system-information';
@@ -38,8 +37,10 @@ export class SystemInformationService {
       const systemInfo = await this.repository.findOne();
       if (systemInfo == null) return;
 
-      // make relations expired
-      await this.expireIfVersionLowerThan448(systemInfo.version);
+      // make relations expired if version is updated
+      if (!(systemInfo.version === proxyVersion)) {
+        await this.relationsService.resetAllExpiredAtCommands();
+      }
 
       // update the version
       systemInfo.setVersion(proxyVersion);
@@ -52,18 +53,6 @@ export class SystemInformationService {
       await this.repository.save(newSystemInfo);
 
       // make relations expired
-      await this.expireIfVersionLowerThan448(proxyVersion);
-    }
-  }
-
-  async expireIfVersionLowerThan448(proxyVersion: string): Promise<void> {
-    const versionRegExp = /^\d{1,2}.\d{1,2}.\d{1,2}/g;
-    const version = proxyVersion.match(versionRegExp)?.[0];
-    if (version == null) return logger.error('version is null');
-
-    // make all relations expired if the previous version was <= 4.4.8
-    const shouldExpire = compareVersions(version, '4.4.8') <= 0;
-    if (shouldExpire) {
       await this.relationsService.resetAllExpiredAtCommands();
     }
   }
