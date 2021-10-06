b396c7eab9c5082047cb431823aba3ab7b3968c5
Taichi Masuyama <montanha.masu536@gmail.com>
2021-10-06 03:56:49 +0000

    Fixed

--- packages/slackbot-proxy/src/repositories/system-information.ts
+++ packages/slackbot-proxy/src/repositories/system-information.ts
@@ -8,8 +8,9 @@ import { SystemInformation } from '~/entities/system-information';
 export class SystemInformationRepository extends Repository<SystemInformation> {
 
   async createOrUpdateUniqueRecordWithVersion(systemInfo: SystemInformation | undefined, proxyVersion: string): Promise<void> {
+    const isExist = systemInfo != null;
     // update the version if it exists
-    if (systemInfo !== undefined) {
+    if (isExist) {
       systemInfo.setVersion(proxyVersion);
       await this.save(systemInfo);
       return;
