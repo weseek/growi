041da4b5483e943895b82da194bf8053a55a3a2a
kaori <kaori@weseek.co.jp>
2021-10-07 10:21:45 +0900

    clean code

--- packages/app/src/interfaces/in-app-notification-types.ts
+++ packages/app/src/interfaces/in-app-notification-types.ts
@@ -2,52 +2,9 @@ export interface InAppNotification {
   _id: string
   user: string
   targetModel: 'Page'
-  // target: Page
-  target: any
+  target: any /* Need to set "Page" as a type" */
   action: 'COMMENT' | 'LIKE'
   status: string
-  actionUsers: any[]
-  createdAt: string
-}
-
-export interface Page {
-  _id: string
-  path: string
-  revision: Revision
-  redirectTo: string
-  status: string
-  creator: User
-  lastUpdateUser: User
-  liker: User[]
-  seenUsers: User[]
-  commentCount: number
-  bookmarkCount?: number
-  createdAt: Date
-  updatedAt: Date
-}
-
-export interface Revision {
-  _id: string
-  path: string
-  body: string
-  format: string
-  author: User
-  createdAt: string
-}
-
-export interface User {
-  _id: string
-  image: string
-  name: string
-  username: string
-}
-
-export interface Comment {
-  _id: string
-  page: string
-  creator: string
-  revision: string
-  comment: string
-  commentPosition: number
+  actionUsers: any[] /* Need to set "User[]" as a type" */
   createdAt: string
 }
