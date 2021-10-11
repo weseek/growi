2c8dfdcf257f2d6d6aaaff37d28f7797a0fb8a0a
Steven Fukase <fukasesteven@gmail.com>
2021-10-11 08:40:49 +0000

    removed toggle

--- packages/app/src/components/PageEditor/ConflictDiffModal.tsx
+++ packages/app/src/components/PageEditor/ConflictDiffModal.tsx
@@ -6,8 +6,8 @@ import { UnControlled as ReactCodeMirror } from 'react-codemirror2';
 
 export const ConflictDiffModal: FC = () => {
   return (
-    <Modal isOpen toggle={} className="modal-gfm-cheatsheet">
-      <ModalHeader tag="h4" toggle={} className="bg-primary text-light">
+    <Modal isOpen className="modal-gfm-cheatsheet">
+      <ModalHeader tag="h4" className="bg-primary text-light">
         <i className="icon-fw icon-question" />Markdown help
       </ModalHeader>
       <ModalBody>
