936a756a6f69cffa7697b644cd7f96ead8273648
kaori <kaori@weseek.co.jp>
2021-10-12 18:19:00 +0900

    unintegrated emojione

--- packages/app/src/client/util/markdown-it/emoji.js
+++ packages/app/src/client/util/markdown-it/emoji.js
@@ -5,25 +5,7 @@ export default class EmojiConfigurer {
   }
 
   configure(md) {
-    const emojiStrategy = this.crowi.getEmojiStrategy();
-
-    const emojiShortnameUnicodeMap = {};
-
-    /* eslint-disable guard-for-in, no-restricted-syntax */
-    for (const unicode in emojiStrategy) {
-      const data = emojiStrategy[unicode];
-      const shortname = data.shortname.replace(/:/g, '');
-      emojiShortnameUnicodeMap[shortname] = String.fromCharCode(unicode);
-    }
-    /* eslint-enable guard-for-in, no-restricted-syntax */
-
-    md.use(require('markdown-it-emoji'), { defs: emojiShortnameUnicodeMap });
-
-    // integrate markdown-it-emoji and emojione
-    md.renderer.rules.emoji = (token, idx) => {
-      const shortname = `:${token[idx].markup}:`;
-      return emojione.shortnameToImage(shortname);
-    };
+    md.use(require('markdown-it-emoji'));
   }
 
 }
