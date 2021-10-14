656e0ecdeff9b2f709c00aaf903d6ba15183f688
Taichi Masuyama <montanha.masu536@gmail.com>
2021-10-14 10:47:11 +0000

    Modified

--- packages/slack/src/interfaces/growi-event-processor.ts
+++ packages/slack/src/interfaces/growi-event-processor.ts
@@ -3,5 +3,5 @@ import { WebClient } from '@slack/web-api';
 export interface GrowiEventProcessor {
   shouldHandleEvent(eventType: string): boolean;
 
-  processEvent(client: WebClient, event: any, tokenPtoG?: string): Promise<void>;
+  processEvent(client: WebClient, event: any): Promise<void>;
 }
