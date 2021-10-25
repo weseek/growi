7838f93945232c5939603a9a38fe42a8e6922623
Shun Miyazawa <shun-m@weseek.co.jp>
2021-10-25 19:20:52 +0900

    create PageUpdateNotification file

---
+++ packages/app/src/components/InAppNotification/PageUpdateNotification.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { InAppNotification as IInAppNotification } from '../../interfaces/in-app-notification';
+
+import FormattedDistanceDate from '../FormattedDistanceDate';
+
+interface Props {
+  actionUsers: string
+  notification: IInAppNotification
+  onClick: () => void
+}
+export const PageUpdateNotification = (props: Props) => {
+
+  return (
+    <>
+      <div>
+        <b>{props.actionUsers}</b> page updated on {props.notification.target.path}
+      </div>
+      <i className="fa fa-comment-o mr-2" />
+      <FormattedDistanceDate id={props.notification._id} date={props.notification.createdAt} isShowTooltip={false} />
+    </>
+  );
+
+};
