d17d1c2ea3c06ef0e73a3d79ac8922070781a54f
Taichi Masuyama <montanha.masu536@gmail.com>
2021-10-27 11:32:27 +0000

    Added null check & got rid of proptypes

--- packages/app/src/components/Admin/App/V5PageMigrationModal.tsx
+++ packages/app/src/components/Admin/App/V5PageMigrationModal.tsx
@@ -1,5 +1,4 @@
-import React, { FC, useCallback } from 'react';
-import PropTypes from 'prop-types';
+import React, { FC } from 'react';
 import {
   Modal, ModalHeader, ModalBody, ModalFooter,
 } from 'reactstrap';
@@ -7,15 +6,23 @@ import { useTranslation } from 'react-i18next';
 
 type V5PageMigrationModalProps = {
   isModalOpen: boolean
-  onConfirm: () => Promise<any>;
-  onCancel: () => void;
+  onConfirm?: () => Promise<any>;
+  onCancel?: () => void;
 };
 
-export const V5PageMigrationModal: FC<V5PageMigrationModalProps> = (props) => {
+export const V5PageMigrationModal: FC<V5PageMigrationModalProps> = (props: V5PageMigrationModalProps) => {
   const { t } = useTranslation();
 
   const onCancel = () => {
-    props.onCancel();
+    if (props.onCancel != null) {
+      props.onCancel();
+    }
+  };
+
+  const onConfirm = () => {
+    if (props.onConfirm != null) {
+      props.onConfirm();
+    }
   };
 
   return (
@@ -41,17 +48,11 @@ export const V5PageMigrationModal: FC<V5PageMigrationModalProps> = (props) => {
         <button
           type="button"
           className="btn btn-outline-primary ml-3"
-          onClick={props.onConfirm}
+          onClick={onConfirm}
         >
           Start Upgrading
         </button>
       </ModalFooter>
     </Modal>
   );
-};
-
-V5PageMigrationModal.propTypes = {
-  isModalOpen: PropTypes.bool.isRequired,
-  onConfirm: PropTypes.func.isRequired,
-  onCancel: PropTypes.func.isRequired,
 };
