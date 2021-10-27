bea60a1f1863fd61b9be71c51decb34461338312
Yuki Takei <yuki@weseek.co.jp>
2021-10-27 14:59:52 +0000

    update useSWRxRecentlyUpdated

--- packages/app/src/stores/page.tsx
+++ packages/app/src/stores/page.tsx
@@ -5,7 +5,7 @@ import { apiv3Get } from '~/client/util/apiv3-client';
 import { IPage } from '~/interfaces/page';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const useSWRxRecentlyUpdated = <Data, Error>(): SWRResponse<IPage[], Error> => {
+export const useSWRxRecentlyUpdated = (): SWRResponse<IPage[], Error> => {
   return useSWR(
     '/pages/recent',
     endpoint => apiv3Get<{ pages: IPage[] }>(endpoint).then(response => response.data?.pages),
