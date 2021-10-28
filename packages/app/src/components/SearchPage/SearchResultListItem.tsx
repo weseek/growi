4b3aa64ddb1a80cdeac47b23ea0f15792cffd5ff
yuto-oweseek <yuto-o@weseek.co.jp>
2021-10-28 14:58:36 +0900

    remove usememo

--- packages/app/src/components/SearchPage/SearchResultListItem.tsx
+++ packages/app/src/components/SearchPage/SearchResultListItem.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useMemo } from 'react';
+import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { UserPicture, PageListMeta, PagePathLabel } from '@growi/ui';
 import { DevidedPagePath } from '@growi/core';
