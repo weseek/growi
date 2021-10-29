0d3296a1f9794863a43e8b68514c1a468bc63812
Shun Miyazawa <shun-m@weseek.co.jp>
2021-10-29 20:36:58 +0900

    create update button

--- packages/app/src/components/Me/InAppNotificationSettings.tsx
+++ packages/app/src/components/Me/InAppNotificationSettings.tsx
@@ -11,9 +11,27 @@ type Props = {
 const InAppNotificationSetting: FC<Props> = (props: Props) => {
   const { t } = useTranslation();
 
+  const updateSettingsHandler = () => {
+    // TODO: 80102
+    return;
+  };
+
   return (
     <>
       <h2 className="border-bottom my-4">{t('in_app_notification_settings.in_app_notification_settings')}</h2>
+
+
+      <div className="row my-3">
+        <div className="offset-4 col-5">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={updateSettingsHandler}
+          >
+            {t('Update')}
+          </button>
+        </div>
+      </div>
     </>
   );
 };
