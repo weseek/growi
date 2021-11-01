506f141d6a8e712f8ce2b80df145cf4ec5d56e8b
Shun Miyazawa <shun-m@weseek.co.jp>
2021-11-01 15:28:02 +0900

    create in-app-notification-settngs model

--- packages/app/src/server/models/in-app-notification-settings.tsx
+++ packages/app/src/server/models/in-app-notification-settings.tsx
@@ -1,1 +1,34 @@
 import { Schema, Model, Document } from 'mongoose';
+import { getOrCreateModel } from '../util/mongoose-utils';
+
+export interface ISubscribeRule {
+  name: string,
+  isEnabled: boolean;
+}
+
+export interface ISubscribeSettings {
+  subscribeRules: ISubscribeRule[];
+}
+
+export interface IInAppNotificationSettings {
+  userId: Schema.Types.ObjectId;
+  subscribeSettings: ISubscribeSettings;
+}
+
+export interface InAppNotificationSettingsDocument extends IInAppNotificationSettings, Document {}
+export type InAppNotificationSettingsModel = Model<InAppNotificationSettingsDocument>
+
+const subscribeSettingsSchema = new Schema<ISubscribeSettings>({
+  subscribeRules: {
+    type: [
+      { name: { type: String }, isEnabled: { type: Boolean } },
+    ],
+  },
+});
+
+const inAppNotificationSettingsSchema = new Schema<IInAppNotificationSettings>({
+  userId: { type: String },
+  subscribeSettings: subscribeSettingsSchema,
+});
+
+export default getOrCreateModel<InAppNotificationSettingsDocument, InAppNotificationSettingsModel>('EditorSettings', inAppNotificationSettingsSchema);
