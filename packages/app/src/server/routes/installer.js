5e2f1daf744c562da89fccabae777284729fc76a
Taichi Masuyama <montanha.masu536@gmail.com>
2021-11-01 08:04:59 +0000

    WIP: need to modify insertMany before doing this

--- packages/app/src/server/routes/installer.js
+++ packages/app/src/server/routes/installer.js
@@ -44,7 +44,9 @@ module.exports = function(crowi) {
     promises.push(createPage(path.join(crowi.localeDir, lang, 'sandbox-diagrams.md'), '/Sandbox/Diagrams', owner, lang));
     promises.push(createPage(path.join(crowi.localeDir, lang, 'sandbox-math.md'), '/Sandbox/Math', owner, lang));
 
-    await Page.insertMany();
+    await Page.insertMany(
+      { path: path.join(crowi.localeDir, lang, 'sandbox.md') },
+    );
 
     try {
       await initSearchIndex();
