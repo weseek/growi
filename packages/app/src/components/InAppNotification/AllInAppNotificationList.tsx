f1de9c7b772269796d100f65744d4e4c36246f55
kaori <kaori@weseek.co.jp>
2021-11-01 14:55:08 +0900

    show in app notification list all

---
+++ packages/app/src/components/InAppNotification/AllInAppNotificationList.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+
+type Props = {
+
+};
+
+const AllInAppNotificationList: FC<Props> = (props: Props) => {
+
+  return (
+    <>AllInAppNotificationList</>
+  );
+};
+
+
+export default AllInAppNotificationList;
