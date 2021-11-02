b7395a8c3b8d452d450070ed302a428431dcb8cb
Yuki Takei <yuki@weseek.co.jp>
2021-11-02 14:52:38 +0000

    impl browser-utils to ui package

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,4 +1,8 @@
+export * from './interfaces/breakpoints';
+
 export * from './components/Attachment/Attachment';
 export * from './components/PagePath/PageListMeta';
 export * from './components/PagePath/PagePathLabel';
 export * from './components/User/UserPicture';
+
+export * from './utils/browser-utils';
---
+++ packages/ui/src/utils/browser-utils.ts
@@ -0,0 +1,18 @@
+import { Breakpoint } from '~/interfaces/breakpoints';
+
+
+export const addBreakpointListener = (
+    breakpoint: Breakpoint,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    listener: (this: MediaQueryList, ev: MediaQueryListEvent) => any,
+): MediaQueryList => {
+  // get the value of '--breakpoint-*'
+  const breakpointPixel = parseInt(window.getComputedStyle(document.documentElement).getPropertyValue(`--breakpoint-${breakpoint}`), 10);
+
+  const mediaQueryList = window.matchMedia(`(min-width: ${breakpointPixel}px)`);
+
+  // add event listener
+  mediaQueryList.addEventListener('change', listener);
+
+  return mediaQueryList;
+};
