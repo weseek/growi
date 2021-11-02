f62111b8a44f243e785770f8629ed061866ab7c3
Yuki Takei <yuki@weseek.co.jp>
2021-11-02 22:57:47 +0000

    remove default fetcher

--- packages/app/src/utils/swr-utils.ts
+++ packages/app/src/utils/swr-utils.ts
@@ -1,9 +1,6 @@
 import { SWRConfiguration } from 'swr';
 
-import axios from './axios';
-
 export const swrGlobalConfiguration: SWRConfiguration = {
-  fetcher: url => axios.get(url).then(res => res.data),
   revalidateOnFocus: false,
   errorRetryCount: 1,
 };
