49beba5f8965815a192370a8ebd8a134084eea00
Shun Miyazawa <shun-m@weseek.co.jp>
2021-11-03 21:17:15 +0900

    success put in-app-notification-settings

--- packages/app/src/components/Me/InAppNotificationSettings.tsx
+++ packages/app/src/components/Me/InAppNotificationSettings.tsx
@@ -3,23 +3,38 @@ import { useTranslation } from 'react-i18next';
 
 import AppContainer from '~/client/services/AppContainer';
 import { withUnstatedContainers } from '../UnstatedUtils';
+import { toastSuccess, toastError } from '~/client/util/apiNotification';
 
 type Props = {
   appContainer: AppContainer,
 };
 
 const InAppNotificationSetting: FC<Props> = (props: Props) => {
+  const { appContainer } = props;
   const { t } = useTranslation();
 
-  const updateSettingsHandler = () => {
+  const updateSettingsHandler = async() => {
+
+    const defaultSubscribeRules = [
+      { name: 'PAGE_CREATE', isEnabled: true },
+    ];
+
     // TODO: 80102
+    try {
+      const res = await appContainer.apiv3Put('/personal-setting/in-app-notification-settings', { defaultSubscribeRules });
+      console.log(res);
+      toastSuccess('yoyoyo!');
+    }
+    catch (err) {
+      toastError(err);
+    }
+
     return;
   };
 
   return (
     <>
       <h2 className="border-bottom my-4">{t('in_app_notification_settings.in_app_notification_settings')}</h2>
-
 
       <div className="row my-3">
         <div className="offset-4 col-5">
