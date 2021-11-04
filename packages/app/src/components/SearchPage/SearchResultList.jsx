9e3d6dfc0b2f2cdc3f078e0aba775fb7be8147e6
Yohei-Shiina <yohei0125@weseek.co.jp>
2021-11-04 14:34:11 +0900

    80388 add null check & change code positions

--- packages/app/src/components/SearchPage/SearchResultList.jsx
+++ packages/app/src/components/SearchPage/SearchResultList.jsx
@@ -5,16 +5,17 @@ import SearchResultListItem from './SearchResultListItem';
 class SearchResultList extends React.Component {
 
   render() {
+    const { selectedPage } = this.props;
+    const selectedPageId = selectedPage != null && selectedPage.id != null ? selectedPage.id : '';
     return this.props.pages.map((page) => {
       // TODO : send cetain  length of body (revisionBody) from elastisearch by adding some settings to the query and
       //         when keyword is not in page content, display revisionBody.
       // TASK : https://estoc.weseek.co.jp/redmine/issues/79606
-
-      const { selectedPage } = this.props;
       return (
         <SearchResultListItem
+          key={page._id}
           page={page}
-          selectedPageId={selectedPage.id != null ? selectedPage.id : ''}
+          selectedPageId={selectedPageId}
           onClickInvoked={this.props.onClickInvoked}
           noLink
         />
