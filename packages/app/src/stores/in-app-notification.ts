ceaaa5344d5357ebbf9eaffe36f892487112ad94
kaori <kaori@weseek.co.jp>
2021-11-05 20:16:13 +0900

    improve swr

--- packages/app/src/stores/in-app-notification.ts
+++ packages/app/src/stores/in-app-notification.ts
@@ -1,5 +1,6 @@
 import useSWR, { SWRResponse } from 'swr';
 
+import { PaginateResult } from 'mongoose';
 import { apiv3Get } from '../client/util/apiv3-client';
 import { IInAppNotification } from '../interfaces/in-app-notification';
 
@@ -7,10 +8,9 @@ import { IInAppNotification } from '../interfaces/in-app-notification';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const useSWRxInAppNotifications = <Data, Error>(
   limit: number,
-): SWRResponse<any, Error> => {
+): SWRResponse<PaginateResult<IInAppNotification>, Error> => {
   return useSWR(
     '/in-app-notification/list',
-    // endpoint => apiv3Get<{ notifications: IInAppNotification[], limit: number }>(endpoint).then(response => response.data?.notifications),
     endpoint => apiv3Get(endpoint, { limit }).then(response => response.data),
   );
 };
