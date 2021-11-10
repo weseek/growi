1b19aed88e01e9567201518f7555f3daf918f838
kaori <kaori@weseek.co.jp>
2021-11-10 13:17:00 +0900

    show paginationResults

--- packages/app/src/components/InAppNotification/AllInAppNotifications.tsx
+++ packages/app/src/components/InAppNotification/AllInAppNotifications.tsx
@@ -1,15 +1,42 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 import InAppNotificationList from './InAppNotificationList';
 import { useSWRxInAppNotifications } from '../../stores/in-app-notification';
+import PaginationWrapper from '../PaginationWrapper';
 
 
 const AllInAppNotifications: FC = () => {
   const limit = 6;
+  const [activePage, setActivePage] = useState(1);
   const { data: inAppNotificationData } = useSWRxInAppNotifications(limit);
 
+
+  if (inAppNotificationData == null) {
+    return (
+      <div className="wiki">
+        <div className="text-muted text-center">
+          <i className="fa fa-2x fa-spinner fa-pulse mr-1"></i>
+        </div>
+      </div>
+    );
+  }
+
+  function setPageNumber(selectedPageNumber): void {
+    setActivePage(selectedPageNumber);
+  }
+
   return (
-    <InAppNotificationList inAppNotificationData={inAppNotificationData} />
+    <>
+      <InAppNotificationList inAppNotificationData={inAppNotificationData} />
+      <PaginationWrapper
+        activePage={activePage}
+        changePage={setPageNumber}
+        totalItemsCount={inAppNotificationData.totalDocs}
+        pagingLimit={inAppNotificationData.limit}
+        align="center"
+        size="sm"
+      />
+    </>
   );
 };
 
