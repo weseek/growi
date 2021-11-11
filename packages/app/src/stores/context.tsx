cacfc98dcf36c7023431f18f3d3335b7eb6e57ff
Taichi Masuyama <montanha.masu536@gmail.com>
2021-11-11 08:34:03 +0000

    Fix lint

--- packages/app/src/stores/context.tsx
+++ packages/app/src/stores/context.tsx
@@ -4,5 +4,5 @@ import { useStaticSWR } from './use-static-swr';
 import { TargetAndAncestors } from '../interfaces/page-listing-results';
 
 export const useTargetAndAncestors = (initialData?: TargetAndAncestors): SWRResponse<TargetAndAncestors, Error> => {
-  return useStaticSWR<TargetAndAncestors, Error>('targetAndAncestors', initialData);
+  return useStaticSWR<TargetAndAncestors, Error>('targetAndAncestors', initialData || null);
 };
