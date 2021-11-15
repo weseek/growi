c989fb0338e172a8c9d91814fc8d9b46697439f2
Mao <maow89126@gmail.com>
2021-11-15 14:52:36 +0000

    80481 fix temporary workaround

--- packages/app/src/components/SearchPage/SearchResultContent.tsx
+++ packages/app/src/components/SearchPage/SearchResultContent.tsx
@@ -12,20 +12,24 @@ type Props ={
   searchingKeyword:string,
   focusedPage: null | any,
 }
+
+const SearchResultContentSubNavigationWrapper = (props) => {
+  return <SearchResultContentSubNavigation {...props}></SearchResultContentSubNavigation>;
+};
+
+const RevisionLoaderWrapper = (props) => {
+  return <RevisionLoader {...props}></RevisionLoader>;
+};
 const SearchResultContent: FC<Props> = (props: Props) => {
   const page = props.focusedPage;
   if (page == null) return null;
-  // Temporaly workaround for lint error
-  // later needs to be fixed: RevisoinRender to typescriptcomponet
-  const RevisionLoaderTypeAny: any = RevisionLoader;
-  const SearchResultContentSubNavigationTypeAny: any = SearchResultContentSubNavigation;
   const growiRenderer = props.appContainer.getRenderer('searchresult');
   let showTags = false;
   if (page.tags != null && page.tags.length > 0) { showTags = true }
   return (
     <div key={page._id} className="search-result-page mb-5">
-      <SearchResultContentSubNavigationTypeAny pageId={page._id} path={page.path}></SearchResultContentSubNavigationTypeAny>
-      <RevisionLoaderTypeAny
+      <SearchResultContentSubNavigationWrapper pageId={page._id} path={page.path}></SearchResultContentSubNavigationWrapper>
+      <RevisionLoaderWrapper
         growiRenderer={growiRenderer}
         pageId={page._id}
         pagePath={page.path}
