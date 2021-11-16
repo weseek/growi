a296925adc59b7a253764fe90ffa08469b0799da
kaori <kaori@weseek.co.jp>
2021-11-16 15:06:46 +0900

    set types to IInAppNotification

--- packages/app/src/interfaces/in-app-notification.ts
+++ packages/app/src/interfaces/in-app-notification.ts
@@ -1,11 +1,14 @@
 // refer types https://github.com/crowi/crowi/blob/eecf2bc821098d2516b58104fe88fae81497d3ea/client/types/crowi.d.ts
+import { IUser } from './user';
+import { IPage } from './page';
+
 export interface IInAppNotification {
   _id: string
   user: string
   targetModel: 'Page'
-  target: any /* Need to set "Page" as a type" */
+  target: IPage
   action: 'COMMENT' | 'LIKE'
   status: string
-  actionUsers: any[] /* Need to set "User[]" as a type" */
-  createdAt: string
+  actionUsers: IUser[]
+  createdAt: Date
 }
