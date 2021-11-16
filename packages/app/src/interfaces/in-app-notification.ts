8d0b6934a440fba5f53684ec91e4acb069d55f45
kaori <kaori@weseek.co.jp>
2021-11-16 15:55:46 +0900

    use mongoose type

--- packages/app/src/interfaces/in-app-notification.ts
+++ packages/app/src/interfaces/in-app-notification.ts
@@ -1,13 +1,14 @@
+import { Types } from 'mongoose';
 import { IUser } from './user';
 import { IPage } from './page';
 
 export interface IInAppNotification {
-  _id: string
-  user: string
+  _id: Types.ObjectId
+  user: IUser
   targetModel: 'Page'
   target: IPage
   action: 'COMMENT' | 'LIKE'
   status: string
   actionUsers: IUser[]
-  createdAt: string
+  createdAt: Date
 }
