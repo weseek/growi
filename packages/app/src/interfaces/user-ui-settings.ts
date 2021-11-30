1c6bc48c68b4244fffc0c6a3280e29e59db38525
Taichi Masuyama <montanha.masu536@gmail.com>
2021-11-30 05:55:17 +0000

    Fixed type

--- packages/app/src/interfaces/user-ui-settings.ts
+++ packages/app/src/interfaces/user-ui-settings.ts
@@ -4,7 +4,7 @@ import { SidebarContentsType } from './ui';
 import { Ref } from './common';
 
 export interface IUserUISettings {
-  userId: Ref<IUser> | null;
+  user: Ref<IUser> | null;
   isSidebarCollapsed: boolean,
   currentSidebarContents: SidebarContentsType,
   currentProductNavWidth: number,
