9e23ff2d15671509a9d1048ea903f15b6b67c202
Shun Miyazawa <shun-m@weseek.co.jp>
2021-12-02 16:12:41 +0900

    added the ability to save snapshots of pageId

--- packages/app/src/server/service/in-app-notification.ts
+++ packages/app/src/server/service/in-app-notification.ts
@@ -53,7 +53,7 @@ export default class InAppNotificationService {
   }
 
   upsertByActivity = async function(
-      users: Types.ObjectId[], activity: ActivityDocument, createdAt?: Date | null,
+      users: Types.ObjectId[], activity: ActivityDocument, pageIdSnapshot: string, createdAt?: Date | null,
   ): Promise<void> {
     const {
       _id: activityId, targetModel, target, action,
@@ -62,7 +62,7 @@ export default class InAppNotificationService {
     const lastWeek = subDays(now, 7);
     const operations = users.map((user) => {
       const filter = {
-        user, target, action, createdAt: { $gt: lastWeek },
+        user, target, action, createdAt: { $gt: lastWeek }, pageIdSnapshot,
       };
       const parameters = {
         user,
@@ -71,6 +71,7 @@ export default class InAppNotificationService {
         action,
         status: STATUS_UNREAD,
         createdAt: now,
+        pageIdSnapshot,
         $addToSet: { activities: activityId },
       };
       return {
