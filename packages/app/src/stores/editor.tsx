c9fc618a3812787ee61c6d505eaa11589f0f7848
stevenfukase <fukasesteven@gmail.com>
2021-12-07 09:41:18 +0000

    wip

--- packages/app/src/stores/editor.tsx
+++ packages/app/src/stores/editor.tsx
@@ -7,3 +7,10 @@ export const useIsSlackEnabled = (isEnabled?: boolean): SWRResponse<boolean, Err
     useStaticSWR('isSlackEnabled', isEnabled || null, { fallbackData: initialData })
   );
 };
+
+export const useSlackChannels = (isEnabled?: boolean): SWRResponse<boolean, Error> => {
+  const initialData = false;
+  return (
+    useStaticSWR('isSlackEnabled', isEnabled || null, { fallbackData: initialData })
+  );
+};
