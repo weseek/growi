d45580cfe3788586f60e17f1c3d797d3854b96dc
Shun Miyazawa <shun-m@weseek.co.jp>
2021-12-08 00:00:40 +0900

    fix type

--- packages/app/src/models/serializers/in-app-notification-snapshot/page.ts
+++ packages/app/src/models/serializers/in-app-notification-snapshot/page.ts
@@ -1,11 +1,12 @@
 import { IUser } from '~/interfaces/user';
+import { IPage } from '~/interfaces/page';
 
 export interface IPageSnapshot {
   path: string
   creator: IUser
 }
 
-export const stringifySnapshot = (page: IPageSnapshot): string => {
+export const stringifySnapshot = (page: IPage): string => {
   return JSON.stringify({
     path: page.path,
     creator: page.creator,
