465a49ec9aab23636602c2a7e15cbae33e5f3738
Yuki Takei <yuki@weseek.co.jp>
2021-12-07 14:46:28 +0000

    make user unique

--- packages/app/src/server/models/user-ui-settings.ts
+++ packages/app/src/server/models/user-ui-settings.ts
@@ -12,7 +12,7 @@ export interface UserUISettingsDocument extends IUserUISettings, Document {}
 export type UserUISettingsModel = Model<UserUISettingsDocument>
 
 const schema = new Schema<UserUISettingsDocument, UserUISettingsModel>({
-  user: { type: Schema.Types.ObjectId, ref: 'User', index: true },
+  user: { type: Schema.Types.ObjectId, ref: 'User', unique: true },
   isSidebarCollapsed: { type: Boolean, default: false },
   currentSidebarContents: {
     type: String,
