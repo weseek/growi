8caf55a7447105da0d1861c42ba2d70d89ca12ee
kaori <kaori@weseek.co.jp>
2021-12-10 04:02:33 +0900

    replace icon

---
+++ packages/app/src/components/Icons/TriangleIcon.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TriangleIcon = (): JSX.Element => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="12" height="8" viewBox="0 0 12 8">
+    <path
+      id="多角形_48"
+      data-name="多角形 48"
+      d="M5.2,1.067a1,1,0,0,1,1.6,0l4,5.333A1,1,0,0,1,10,8H2a1,1,0,0,1-.8-1.6Z"
+      transform="translate(12 8) rotate(180)"
+      fill="#bcbcbc"
+    />
+  </svg>
+);
+
+export default TriangleIcon;
