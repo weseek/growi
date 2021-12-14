68dcd570616bdf09dab963767b39bff84a436033
kaori <kaori@weseek.co.jp>
2021-12-15 01:48:25 +0900

    replace css to bootstrap classes

--- packages/app/src/components/Sidebar/PageTree/PrivateLegacyPages.tsx
+++ packages/app/src/components/Sidebar/PageTree/PrivateLegacyPages.tsx
@@ -5,11 +5,9 @@ const PrivateLegacyPages: FC = memo(() => {
   const { t } = useTranslation();
 
   return (
-    <div className="grw-prvt-legacy-pages p-3">
-      <a href="/private-legacy-pages?q=[nq:PrivateLegacyPages]" className="h5">
-        <i className="icon-drawer mr-2"></i> PrivateLegacyPages
-      </a>
-    </div>
+    <a href="/private-legacy-pages?q=[nq:PrivateLegacyPages]" className="h5">
+      <i className="icon-drawer mr-2"></i> Private Legacy Pages
+    </a>
   );
 });
 
