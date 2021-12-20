ff0d6b97e4abf32e1588a276b10a435273d5c799
Shun Miyazawa <shun-m@weseek.co.jp>
2021-12-20 20:26:25 +0900

    restore

--- packages/app/src/server/routes/apiv3/personal-setting.js
+++ packages/app/src/server/routes/apiv3/personal-setting.js
@@ -577,7 +577,7 @@ module.exports = (crowi) => {
   // eslint-disable-next-line max-len
   router.put('/in-app-notification-settings', accessTokenParser, loginRequiredStrictly, csrf, validator.inAppNotificationSettings, apiV3FormValidator, async(req, res) => {
     const query = { userId: req.user.id };
-    const { subscribeRules } = req.body;
+    const subscribeRules = req.body.subscribeRules;
 
     if (subscribeRules == null) {
       return res.apiv3Err('no-rules-found');
