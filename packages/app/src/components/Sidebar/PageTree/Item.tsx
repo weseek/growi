884ec4e5dc8c32ab9adf642461ac6d4439175cb4
Taichi Masuyama <montanha.masu536@gmail.com>
2021-12-22 08:29:01 +0000

    Show dummy page count

--- packages/app/src/components/Sidebar/PageTree/Item.tsx
+++ packages/app/src/components/Sidebar/PageTree/Item.tsx
@@ -86,8 +86,9 @@ const ItemControl: FC<ItemControlProps> = memo((props: ItemControlProps) => {
 const ItemCount: FC = () => {
   return (
     <>
-      <span className="grw-pagetree-count badge badge-pill badge-light">
+      <span className="grw-pagetree-count badge badge-pill badge-light text-muted">
         {/* TODO: consider to show the number of children pages */}
+        00
       </span>
     </>
   );
