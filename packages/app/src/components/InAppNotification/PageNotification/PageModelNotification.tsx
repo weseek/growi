23dfc70f9743f4732e25ab7c329c9621422fda10
Shun Miyazawa <shun-m@weseek.co.jp>
2021-12-24 10:16:40 +0000

    Revert "add warning"
    This reverts commit 112ca3d1f17a932d84fcd3604764654127989861.

--- packages/app/src/components/InAppNotification/PageNotification/PageModelNotification.tsx
+++ packages/app/src/components/InAppNotification/PageNotification/PageModelNotification.tsx
@@ -1,12 +1,10 @@
 import React, { FC, useCallback } from 'react';
 import { PagePathLabel } from '@growi/ui';
-import { useTranslation } from 'react-i18next';
 import { apiv3Post } from '~/client/util/apiv3-client';
 import { parseSnapshot } from '../../../models/serializers/in-app-notification-snapshot/page';
 import { IInAppNotification } from '~/interfaces/in-app-notification';
 import { HasObjectId } from '~/interfaces/has-object-id';
 import FormattedDistanceDate from '../../FormattedDistanceDate';
-import { toastWarning } from '~/client/util/apiNotification';
 
 interface Props {
   notification: IInAppNotification & HasObjectId
@@ -16,8 +14,6 @@ interface Props {
 }
 
 const PageModelNotification: FC<Props> = (props: Props) => {
-  const { t } = useTranslation();
-
   const {
     notification, actionMsg, actionIcon, actionUsers,
   } = props;
@@ -33,9 +29,6 @@ const PageModelNotification: FC<Props> = (props: Props) => {
     const targetPagePath = notification.target?.path;
     if (targetPagePath != null) {
       window.location.href = targetPagePath;
-    }
-    else {
-      toastWarning(t('in_app_notification.page_not_exist'));
     }
   }, []);
 
