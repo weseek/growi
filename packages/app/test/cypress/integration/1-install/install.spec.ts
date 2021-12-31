831d2b69e3f96df33715c12de60e481d1702f0b3
Yuki Takei <yuki@weseek.co.jp>
2021-12-31 22:43:23 +0000

    mv cypress dir to test/cypress

--- packages/app/test/cypress/integration/1-install/install.spec.ts
+++ packages/app/test/cypress/integration/1-install/install.spec.ts
@@ -1,5 +1,6 @@
-describe('The Home Page', () => {
+describe('Installer', () => {
   it('successfully loads', () => {
     cy.visit('http://localhost:3000'); // change URL to match your dev URL
+    cy.screenshot('on-load');
   });
 });
