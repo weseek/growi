40ab28fcab47f56b4cf1006d3a7235df6944ef08
Shun Miyazawa <shun-m@weseek.co.jp>
2022-01-04 18:07:33 +0900

    create useSWRxBookmarksInfo

--- packages/app/src/stores/bookmarks.tsx
+++ packages/app/src/stores/bookmarks.tsx
@@ -1,1 +1,22 @@
 import useSWR, { SWRResponse } from 'swr';
+
+import { Types } from 'mongoose';
+import { apiv3Get } from '~/client/util/apiv3-client';
+
+import { useIsGuestUser } from './context';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const useSWRxBookmarksInfo = <Data, Error>(pageId: Types.ObjectId):SWRResponse<{isBookmarked: boolean | null, sumOfBookmarks: number | null}, Error> => {
+  const { data: isGuestUser } = useIsGuestUser();
+
+  const key = isGuestUser === false ? ['/bookmarks/info', pageId] : null;
+  return useSWR(
+    key,
+    (endpoint, pageId) => apiv3Get(endpoint, { pageId }).then((response) => {
+      return {
+        isBookmarked: response.data.isBookmarked,
+        sumOfBookmarks: response.data.sumOfBookmarks,
+      };
+    }),
+  );
+};
