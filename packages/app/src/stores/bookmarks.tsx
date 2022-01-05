ade9d6632e4e1557b484a12778ee011d5be7530f
Shun Miyazawa <shun-m@weseek.co.jp>
2022-01-05 18:12:10 +0900

    remove useIsGuestUser

--- packages/app/src/stores/bookmarks.tsx
+++ packages/app/src/stores/bookmarks.tsx
@@ -3,15 +3,11 @@ import useSWR, { SWRResponse } from 'swr';
 import { Types } from 'mongoose';
 import { apiv3Get } from '~/client/util/apiv3-client';
 
-import { useIsGuestUser } from './context';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const useSWRxBookmarksInfo = <Data, Error>(pageId: Types.ObjectId):SWRResponse<{isBookmarked: boolean | null, sumOfBookmarks: number | null}, Error> => {
-  const { data: isGuestUser } = useIsGuestUser();
-
-  const key = isGuestUser === false ? ['/bookmarks/info', pageId] : null;
   return useSWR(
-    key,
+    ['/bookmarks/info', pageId],
     (endpoint, pageId) => apiv3Get(endpoint, { pageId }).then((response) => {
       return {
         isBookmarked: response.data.isBookmarked,
