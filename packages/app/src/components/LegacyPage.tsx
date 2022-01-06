29c10c98a6d47db23738381dc1d5fa59717eac0e
Mao <maow89126@gmail.com>
2022-01-06 05:54:16 +0000

    84556 search and legacy page wrapper

---
+++ packages/app/src/components/LegacyPage.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+type Props = {
+
+}
+
+const LegacyPage: FC<Props> = (props: Props) => {
+  return <></>;
+};
+
+
+export default LegacyPage;
---
+++ packages/app/src/components/SearchPage.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+type Props = {
+
+}
+const SearchPage: FC<Props> = (props: Props) => {
+  return <></>;
+};
+
+
+export default SearchPage;
