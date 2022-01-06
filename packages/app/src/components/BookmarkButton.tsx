5e9e6e04a302c6c12fa903b5644620406cb13904
Shun Miyazawa <shun-m@weseek.co.jp>
2022-01-06 14:57:50 +0900

    Copy the look from LikeButtons

--- packages/app/src/components/BookmarkButton.tsx
+++ packages/app/src/components/BookmarkButton.tsx
@@ -1,9 +1,10 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 import { Types } from 'mongoose';
-import { UncontrolledTooltip } from 'reactstrap';
+import { UncontrolledTooltip, Popover, PopoverBody } from 'reactstrap';
 import { useTranslation } from 'react-i18next';
 
+import UserPictureList from './User/UserPictureList';
 import { toastError } from '~/client/util/apiNotification';
 import { useIsGuestUser } from '~/stores/context';
 import { useSWRxBookmarksInfo } from '~/stores/bookmarks';
@@ -17,11 +18,17 @@ const BookmarkButton: FC<Props> = (props: Props) => {
   const { t } = useTranslation();
   const { pageId } = props;
 
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
   const { data: isGuestUser } = useIsGuestUser();
   const { data: bookmarksInfo, mutate } = useSWRxBookmarksInfo(pageId);
 
   const isBookmarked = bookmarksInfo?.isBookmarked != null ? bookmarksInfo.isBookmarked : false;
   const sumOfBookmarks = bookmarksInfo?.sumOfBookmarks != null ? bookmarksInfo.sumOfBookmarks : 0;
+
+  const togglePopover = () => {
+    setIsPopoverOpen(!isPopoverOpen);
+  };
 
   const handleClick = async() => {
     if (isGuestUser) {
@@ -40,18 +47,15 @@ const BookmarkButton: FC<Props> = (props: Props) => {
   };
 
   return (
-    <div>
+    <div className="btn-group" role="group" aria-label="Bookmark buttons">
       <button
         type="button"
         id="bookmark-button"
         onClick={handleClick}
-        className={`btn btn-bookmark border-0 btn-md
-        ${isBookmarked ? 'active' : ''} ${isGuestUser ? 'disabled' : ''}`}
+        className={`btn btn-bookmark border-0
+          ${isBookmarked ? 'active' : ''} ${isGuestUser ? 'disabled' : ''}`}
       >
         <i className="icon-star mr-3"></i>
-        <span className="total-bookmarks">
-          {sumOfBookmarks}
-        </span>
       </button>
 
       {isGuestUser && (
@@ -59,6 +63,17 @@ const BookmarkButton: FC<Props> = (props: Props) => {
           {t('Not available for guest')}
         </UncontrolledTooltip>
       )}
+
+      <button type="button" id="po-total-bookmarks" className={`btn btn-bookmark border-0 total-bookmarks ${isBookmarked ? 'active' : ''}`}>
+        {sumOfBookmarks}
+      </button>
+      <Popover placement="bottom" isOpen={isPopoverOpen} target="po-total-bookmarks" toggle={togglePopover} trigger="legacy">
+        <PopoverBody className="seen-user-popover">
+          <div className="px-2 text-right user-list-content text-truncate text-muted">
+            {[0].length ? <UserPictureList users={[{ _id: '61d4279c0ffbc6a1235c07f3' }]} /> : t('No users have bookmarked this yet.')}
+          </div>
+        </PopoverBody>
+      </Popover>
     </div>
   );
 };
