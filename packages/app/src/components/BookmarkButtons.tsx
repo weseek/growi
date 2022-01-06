1ed8f6aa7b2c67bcfea874aacde94a0c572fb04e
Shun Miyazawa <34241526+miya@users.noreply.github.com>
2022-01-06 22:52:53 +0900

    feat: 84999 85029 Display a list of bookmarked users (#5040)
    * new file
    
    * create useSWRxBookmarksInfo
    
    * useSWR
    
    * fix
    
    * create getBookmarkedUsers method
    
    * fix name
    
    * useSWR
    
    * add TODO
    
    * add null
    
    * add bookmarkedUserIds
    
    * Copy the look from LikeButtons
    
    * fix
    
    * added the ability to retrieve user documents
    
    * fix response name & add if
    
    * cleanup
    
    * fb fix
    
    * ok
    
    * i18n
    
    * fix
    
    * delete todo
    
    * rename file
    
    * rename path

--- packages/app/src/components/BookmarkButtons.tsx
+++ packages/app/src/components/BookmarkButtons.tsx
@@ -1,9 +1,10 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 import { Types } from 'mongoose';
-import { UncontrolledTooltip } from 'reactstrap';
+import { UncontrolledTooltip, Popover, PopoverBody } from 'reactstrap';
 import { useTranslation } from 'react-i18next';
 
+import UserPictureList from './User/UserPictureList';
 import { toastError } from '~/client/util/apiNotification';
 import { useIsGuestUser } from '~/stores/context';
 import { useSWRxBookmarksInfo } from '~/stores/bookmarks';
@@ -17,15 +18,18 @@ const BookmarkButton: FC<Props> = (props: Props) => {
   const { t } = useTranslation();
   const { pageId } = props;
 
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
   const { data: isGuestUser } = useIsGuestUser();
   const { data: bookmarksInfo, mutate } = useSWRxBookmarksInfo(pageId);
 
   const isBookmarked = bookmarksInfo?.isBookmarked != null ? bookmarksInfo.isBookmarked : false;
   const sumOfBookmarks = bookmarksInfo?.sumOfBookmarks != null ? bookmarksInfo.sumOfBookmarks : 0;
-
-  // TODO: To be used in UserPictureList
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const bookmarkedUsers = bookmarksInfo?.bookmarkedUsers != null ? bookmarksInfo.bookmarkedUsers : [];
+
+  const togglePopover = () => {
+    setIsPopoverOpen(!isPopoverOpen);
+  };
 
   const handleClick = async() => {
     if (isGuestUser) {
@@ -44,18 +48,15 @@ const BookmarkButton: FC<Props> = (props: Props) => {
   };
 
   return (
-    <div>
+    <div className="btn-group" role="group" aria-label="Bookmark buttons">
       <button
         type="button"
         id="bookmark-button"
         onClick={handleClick}
-        className={`btn btn-bookmark border-0 btn-md
-        ${isBookmarked ? 'active' : ''} ${isGuestUser ? 'disabled' : ''}`}
+        className={`btn btn-bookmark border-0
+          ${isBookmarked ? 'active' : ''} ${isGuestUser ? 'disabled' : ''}`}
       >
-        <i className="icon-star mr-3"></i>
-        <span className="total-bookmarks">
-          {sumOfBookmarks}
-        </span>
+        <i className="icon-star"></i>
       </button>
 
       {isGuestUser && (
@@ -63,6 +64,17 @@ const BookmarkButton: FC<Props> = (props: Props) => {
           {t('Not available for guest')}
         </UncontrolledTooltip>
       )}
+
+      <button type="button" id="po-total-bookmarks" className={`btn btn-bookmark border-0 total-bookmarks ${isBookmarked ? 'active' : ''}`}>
+        {sumOfBookmarks}
+      </button>
+      <Popover placement="bottom" isOpen={isPopoverOpen} target="po-total-bookmarks" toggle={togglePopover} trigger="legacy">
+        <PopoverBody className="seen-user-popover">
+          <div className="px-2 text-right user-list-content text-truncate text-muted">
+            {bookmarkedUsers.length ? <UserPictureList users={bookmarkedUsers} /> : t('No users have bookmarked yet')}
+          </div>
+        </PopoverBody>
+      </Popover>
     </div>
   );
 };
