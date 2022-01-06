dc5daa7d8504b6858eadcc2cb4fe06f834f07a57
Shun Miyazawa <shun-m@weseek.co.jp>
2022-01-06 13:34:44 +0900

    create a return type for useSWRxBookmarksInfo in interface

--- packages/app/src/stores/bookmarks.tsx
+++ packages/app/src/stores/bookmarks.tsx
@@ -3,9 +3,11 @@ import useSWR, { SWRResponse } from 'swr';
 import { Types } from 'mongoose';
 import { apiv3Get } from '~/client/util/apiv3-client';
 
+import { IBookmarksInfo } from '~/interfaces/bookmarks';
+
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const useSWRxBookmarksInfo = <Data, Error>(pageId: Types.ObjectId):SWRResponse<{isBookmarked: boolean | null, sumOfBookmarks: number | null}, Error> => {
+export const useSWRxBookmarksInfo = <Data, Error>(pageId: Types.ObjectId):SWRResponse<IBookmarksInfo, Error> => {
   return useSWR(
     ['/bookmarks/info', pageId],
     (endpoint, pageId) => apiv3Get(endpoint, { pageId }).then((response) => {
