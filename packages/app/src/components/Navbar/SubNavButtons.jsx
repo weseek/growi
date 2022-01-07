4acd0c477892786c71e986291f7197cf1619a0b2
Shun Miyazawa <shun-m@weseek.co.jp>
2022-01-07 18:25:02 +0900

    apiv3Put

--- packages/app/src/components/Navbar/SubNavButtons.jsx
+++ packages/app/src/components/Navbar/SubNavButtons.jsx
@@ -27,7 +27,7 @@ const SubnavButtons = React.memo((props) => {
         </span>
         {pageContainer.isAbleToShowLikeButtons && (
           <span>
-            <LikeButtons />
+            <LikeButtons pageId={pageContainer.state.pageId} />
           </span>
         )}
         <span>
