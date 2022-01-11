df82594a51837ea37b713eb5f5ccac8846537597
Shun Miyazawa <shun-m@weseek.co.jp>
2022-01-11 21:00:23 +0900

    jsx --> tsx

--- packages/app/src/components/User/SeenUserInfo.tsx
+++ packages/app/src/components/User/SeenUserInfo.tsx
@@ -1,33 +1,28 @@
-// import React from 'react';
-import PropTypes from 'prop-types';
-
-import React, { useState } from 'react';
-import {
-  Button, Popover, PopoverBody,
-} from 'reactstrap';
+import React, { FC, useState } from 'react';
+import { Button, Popover, PopoverBody } from 'reactstrap';
 import UserPictureList from './UserPictureList';
-
-import { withUnstatedContainers } from '../UnstatedUtils';
-
-import PageContainer from '~/client/services/PageContainer';
 
 import FootstampIcon from '../FootstampIcon';
 
 import { usePageId } from '~/stores/context';
 import { useSWRxPageInfo } from '~/stores/page';
 
-/* eslint react/no-multi-comp: 0, react/prop-types: 0 */
+interface Props {
+  disabled: boolean
+}
 
-const SeenUserInfo = (props) => {
-  const [popoverOpen, setPopoverOpen] = useState(false);
-  const toggle = () => setPopoverOpen(!popoverOpen);
+const SeenUserInfo: FC<Props> = (props: Props) => {
   const { disabled } = props;
+
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
 
   const { data: pageId } = usePageId();
   const { data: pageInfo } = useSWRxPageInfo(pageId);
 
   const seenUsers = pageInfo?.seenUsers ? pageInfo.seenUsers : [];
   const sumOfSeenUsers = pageInfo?.seenUsers ? pageInfo.seenUsers.length : 0;
+
+  const togglePopover = () => setIsPopoverOpen(!isPopoverOpen);
 
   return (
     <div className="grw-seen-user-info">
@@ -37,7 +32,7 @@ const SeenUserInfo = (props) => {
         </span>
         <span className="seen-user-count">{sumOfSeenUsers}</span>
       </Button>
-      <Popover placement="bottom" isOpen={popoverOpen} target="po-seen-user" toggle={toggle} trigger="legacy" disabled={disabled}>
+      <Popover placement="bottom" isOpen={isPopoverOpen} target="po-seen-user" toggle={togglePopover} trigger="legacy" disabled={disabled}>
         <PopoverBody className="seen-user-popover">
           <div className="px-2 text-right user-list-content text-truncate text-muted">
             <UserPictureList users={seenUsers} />
@@ -48,14 +43,4 @@ const SeenUserInfo = (props) => {
   );
 };
 
-SeenUserInfo.propTypes = {
-  pageContainer: PropTypes.instanceOf(PageContainer).isRequired,
-  disabled: PropTypes.bool,
-};
-
-/**
- * Wrapper component for using unstated
- */
-const SeenUserInfoWrapper = withUnstatedContainers(SeenUserInfo, [PageContainer]);
-
-export default (SeenUserInfoWrapper);
+export default SeenUserInfo;
