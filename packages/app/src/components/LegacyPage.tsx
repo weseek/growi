bfdec66fac53c9ed06b3e2cbcfa9d7febd6edad3
Mao <maow89126@gmail.com>
2022-01-11 05:31:01 +0000

    84856 added ToDo comment

--- packages/app/src/components/LegacyPage.tsx
+++ packages/app/src/components/LegacyPage.tsx
@@ -7,6 +7,14 @@ import SearchCore from './SearchCore';
 type Props = {
 
 }
+
+// TODO
+// Task : https://redmine.weseek.co.jp/issues/85465
+// 1. set excluded hoge based on props
+// 2. hide sort bar when this component is used in legacyPage
+// 3. disable search form when this component is used in LegacyPage
+// 4. refactor DeleteSelectedPageGroup component in a way that  SearchPage and LegacyPage can get actionToPage through props
+// 5. onAfterSearchInvoked should be refactored in LegacyPage
 const LegacyPage : FC<Props> = (props: Props) => {
 
   const onAfterSearchHandler = (keyword, searchedKeyword) => {
@@ -17,6 +25,10 @@ const LegacyPage : FC<Props> = (props: Props) => {
     if (window.history && window.history.pushState) {
       window.history.pushState('', `Search - ${keyword}`, `/_search?q=${keyword}${hash}`);
     }
+  };
+
+  const actionToPages = () => {
+
   };
 
   return <SearchCore onAfterSearchInvoked={onAfterSearchHandler} />;
--- packages/app/src/components/SearchPage.tsx
+++ packages/app/src/components/SearchPage.tsx
@@ -6,6 +6,14 @@ import SearchCore from './SearchCore';
 type Props = {
 
 }
+
+// TODO
+// Task : https://redmine.weseek.co.jp/issues/85465
+// 1. set excluded hoge based on props
+// 2. hide sort bar when this component is used in legacyPage
+// 3. disable search form when this component is used in LegacyPage
+// 4. refactor DeleteSelectedPageGroup component in a way that  SearchPage and LegacyPage can get actionToPage through props
+// 5. onAfterSearchInvoked should be refactored in LegacyPage
 const SearchPage : FC<Props> = (props: Props) => {
   const onAfterSearchHandler = (keyword, searchedKeyword) => {
     let hash = window.location.hash || '';
@@ -15,6 +23,10 @@ const SearchPage : FC<Props> = (props: Props) => {
     if (window.history && window.history.pushState) {
       window.history.pushState('', `Search - ${keyword}`, `/_search?q=${keyword}${hash}`);
     }
+  };
+
+  const actionToPages = () => {
+
   };
 
 
