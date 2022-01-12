a2be30d3d6aadd2cc7d0d4072cb48955a18373ad
Taichi Masuyama <montanha.masu536@gmail.com>
2022-01-12 12:51:11 +0000

    Improved types

--- packages/app/src/stores/user-group.tsx
+++ packages/app/src/stores/user-group.tsx
@@ -1,4 +1,4 @@
-import useSWR, { SWRResponse } from 'swr';
+import { SWRResponse } from 'swr';
 import useSWRImmutable from 'swr/immutable';
 
 import { apiv3Get } from '~/client/util/apiv3-client';
@@ -6,30 +6,34 @@ import { IUserGroupHasId, IUserGroupRelationHasId } from '~/interfaces/user';
 import { UserGroupListResult, ChildUserGroupListResult, UserGroupRelationListResult } from '~/interfaces/user-group-response';
 
 
-export const useSWRxUserGroupList = (initialData?: IUserGroupHasId[]): SWRResponse<UserGroupListResult, Error> => {
-  return useSWRImmutable(
+export const useSWRxUserGroupList = (initialData?: IUserGroupHasId[]): SWRResponse<IUserGroupHasId[], Error> => {
+  return useSWRImmutable<IUserGroupHasId[], Error>(
     '/user-groups',
-    endpoint => apiv3Get(endpoint, { pagination: false }).then(result => result.data),
+    endpoint => apiv3Get<UserGroupListResult>(endpoint, { pagination: false }).then(result => result.data.userGroups),
     {
       fallbackData: initialData,
     },
   );
 };
 
-export const useSWRxChildUserGroupList = (parentIds?: string[], initialData?: IUserGroupHasId[]): SWRResponse<ChildUserGroupListResult, Error> => {
-  return useSWRImmutable(
+export const useSWRxChildUserGroupList = (
+    parentIds: string[] | undefined, initialData?: IUserGroupHasId[],
+): SWRResponse<IUserGroupHasId[], Error> => {
+  return useSWRImmutable<IUserGroupHasId[], Error>(
     parentIds != null ? ['/user-groups/children', parentIds] : null,
-    (endpoint, parentIds) => apiv3Get(endpoint, { parentIds }).then(result => result.data),
+    (endpoint, parentIds) => apiv3Get<ChildUserGroupListResult>(endpoint, { parentIds }).then(result => result.data.childUserGroups),
     {
       fallbackData: initialData,
     },
   );
 };
 
-export const useSWRxUserGroupRelationList = (groupIds?: string[], initialData?: IUserGroupRelationHasId[]): SWRResponse<UserGroupRelationListResult, Error> => {
-  return useSWRImmutable(
+export const useSWRxUserGroupRelationList = (
+    groupIds: string[] | undefined, initialData?: IUserGroupRelationHasId[],
+): SWRResponse<IUserGroupRelationHasId[], Error> => {
+  return useSWRImmutable<IUserGroupRelationHasId[], Error>(
     groupIds != null ? ['/user-group-relations', groupIds] : null,
-    (endpoint, parentIds) => apiv3Get(endpoint, { parentIds }).then(result => result.data),
+    (endpoint, parentIds) => apiv3Get<UserGroupRelationListResult>(endpoint, { parentIds }).then(result => result.data.userGroupRelations),
     {
       fallbackData: initialData,
     },
