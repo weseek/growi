d30bb87f8959c24205c12dfb81f33863bdb6903d
Shun Miyazawa <shun-m@weseek.co.jp>
2022-01-14 15:51:26 +0900

    small fix

--- packages/app/src/stores/user.tsx
+++ packages/app/src/stores/user.tsx
@@ -4,12 +4,10 @@ import { apiv3Get } from '~/client/util/apiv3-client';
 
 import { IUserHasId } from '~/interfaces/user';
 
-// // eslint-disable-next-line @typescript-eslint/no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const useSWRxUsersList = <Data, Error>(userIds?: string): SWRResponse<IUserHasId[], Error> => {
   return useSWR(
     ['/users/list', userIds],
-    (endpoint, userIds) => apiv3Get(endpoint, { userIds }).then((response) => {
-      return response.data.users;
-    }),
+    (endpoint, userIds) => apiv3Get(endpoint, { userIds }).then(response => response.data.users),
   );
 };
