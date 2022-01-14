bb8c4ad6d23f678f8cb46b38040530d1da390294
Taichi Masuyama <montanha.masu536@gmail.com>
2022-01-14 13:00:10 +0000

    Renamed utility and added jsdoc

--- packages/app/src/server/util/compare-objectId.ts
+++ packages/app/src/server/util/compare-objectId.ts
@@ -14,13 +14,19 @@ export const includesObjectId = (arr: IObjectId[], id: IObjectId): boolean => {
   return _arr.includes(_id);
 };
 
-export const filterArr1ByArr2 = (arr1: IObjectId[], arr2: IObjectId[]): IObjectId[] => {
+/**
+ * Exclude ObjectIds which exist in testIds from targetIds
+ * @param targetIds Array of mongoose.Types.ObjectId
+ * @param testIds Array of mongoose.Types.ObjectId
+ * @returns Array of mongoose.Types.ObjectId
+ */
+export const excludeTestIdsFromTargetIds = (targetIds: IObjectId[], testIds: IObjectId[]): IObjectId[] => {
   // cast to string
-  const arrToBeFiltered = arr1.map(e => e.toString());
-  const arrToFilter = arr2.map(e => e.toString());
+  const arr1 = targetIds.map(e => e.toString());
+  const arr2 = testIds.map(e => e.toString());
 
   // filter
-  const filtered = arrToBeFiltered.filter(e => !arrToFilter.includes(e));
+  const filtered = arr2.filter(e => !arr1.includes(e));
 
   // cast to ObjectId
   return filtered.map(e => new ObjectId(e));
