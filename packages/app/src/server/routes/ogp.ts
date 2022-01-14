0321889f1764970df58ad851cc373d6b3c0175eb
yuto-oweseek <yuto-o@weseek.co.jp>
2022-01-14 11:14:40 +0000

    send title username and buffered image to ogp server but needs refactor

--- packages/app/src/server/routes/ogp.ts
+++ packages/app/src/server/routes/ogp.ts
@@ -1,11 +1,17 @@
 import {
   Request, Response,
 } from 'express';
+import path from 'path';
+import { promises as fsPromise } from 'fs';
 
 import axios from '~/utils/axios';
+import loggerFactory from '~/utils/logger';
+import { projectRoot } from '~/utils/project-dir-utils';
+
+const logger = loggerFactory('growi:routes:ogp');
 
 module.exports = function(crowi) {
-  const { configManager, appService, aclService } = crowi;
+  const { configManager, aclService } = crowi;
 
   const ogpUri = configManager.getConfig('crowi', 'app:ogpUri');
   if (ogpUri == null) {
@@ -19,6 +25,22 @@ module.exports = function(crowi) {
   return {
     async renderOgp(req: Request, res: Response) {
 
+      // this closure will be moved
+      function stream2buffer(stream): Promise<Buffer> {
+
+        return new Promise((resolve, reject) => {
+
+          const _buf: Buffer[] = [];
+
+          stream.on('data', (chunk) => {
+            _buf.push(chunk);
+          });
+          stream.on('end', () => resolve(Buffer.concat(_buf)));
+          stream.on('error', err => reject(err));
+
+        });
+      }
+
       if (!aclService.isGuestAllowedToRead()) {
         return res.status(400).send('This GROWI is not public');
       }
@@ -28,7 +50,11 @@ module.exports = function(crowi) {
         return res.status(400).send('page id is not included in the parameter');
       }
 
-      let pagePath;
+
+      let user;
+      let pageTitle;
+      let bufferedUserImage: Buffer = Buffer.from('');
+
       try {
         const Page = crowi.model('Page');
         const page = await Page.findByIdAndViewer(pageId);
@@ -36,30 +62,61 @@ module.exports = function(crowi) {
         if (page.status !== 'published' || (page.grant !== 1 && page.grant !== 2)) {
           return res.status(400).send('the page does not e xist');
         }
-        pagePath = page.path;
+
+        const User = crowi.model('User');
+        user = await User.findById(page.creator._id.toString());
+
+        if (/^https:\/\/gravatar\.com\/avatar\/.+/.test(user.imageUrlCached)) {
+          // user image is gravatar
+          bufferedUserImage = (await axios.get(
+            user.imageUrlCached, {
+              responseType: 'arraybuffer',
+            },
+          )).data;
+        }
+        else if (/^\/attachment\/.+/.test(user.imageUrlCached)) {
+          const { fileUploadService } = crowi;
+          const Attachment = crowi.model('Attachment');
+          const attachment = await Attachment.findById(user.imageAttachment);
+          const fileStream = await fileUploadService.findDeliveryFile(attachment);
+          bufferedUserImage = await stream2buffer(fileStream);
+        }
+        else if (user.imageUrlCached === '/images/icons/user.svg') {
+          bufferedUserImage = await fsPromise.readFile(
+            path.join(projectRoot, 'public/images/icons/user.svg'),
+          );
+        }
+        else {
+          throw new Error('imageUrlCached is invalid value');
+        }
+
+        pageTitle = (page.path).replace(/^(.)*\//, '');
+
       }
       catch (err) {
-        console.log(err);
+        logger.error(err);
         return res.status(400).send('the page does not exist');
       }
 
-      const appTitle = appService.getAppTitle();
 
       let result;
       try {
-        result = await axios({
-          url: ogpUri,
-          method: 'GET',
-          responseType: 'stream',
-          params: {
-            title: pagePath,
-            brand: appTitle,
+        result = await axios.post(
+          ogpUri, {
+            data: {
+              title: pageTitle,
+              userName: user.username,
+              userImage: bufferedUserImage,
+            },
+          }, {
+            responseType: 'stream',
           },
-        });
+        );
       }
       catch (err) {
-        const { status, statusText } = err.response;
-        console.log(`Error! HTTP Status: ${status} ${statusText}`);
+        // const { status, statusText } = err.response;
+        // console.log(`Error! HTTP Status: ${status} ${statusText}`);
+        logger.error(err);
         return res.status(500).send();
       }
 
