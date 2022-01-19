7603c8d8d2df0db7a2dff438c915dc0cd0482d62
kaori <kaori@weseek.co.jp>
2022-01-19 12:24:24 +0000

    improve apiLimiter

--- packages/app/src/server/routes/apiv3/forgot-password.js
+++ packages/app/src/server/routes/apiv3/forgot-password.js
@@ -35,7 +35,7 @@ module.exports = (crowi) => {
 
   const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 5, // limit each IP to 5 requests per windowMs
+    max: 10, // limit each IP to 10 requests per windowMs
     message:
       'Too many requests were sent from this IP. Please try a password reset request again on the password reset request form',
   });
