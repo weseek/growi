53156fbddd9586d1d51eeaac781f8e89e409f8d3
Shun Miyazawa <shun-m@weseek.co.jp>
2022-01-20 13:28:19 +0900

    If the length of distinctUserIds is 0, set key to null

--- packages/app/src/stores/user.tsx
+++ packages/app/src/stores/user.tsx
@@ -5,10 +5,12 @@ import { apiv3Get } from '~/client/util/apiv3-client';
 import { IUserHasId } from '~/interfaces/user';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const useSWRxUsersList = <Data, Error>(userIds?: string[]): SWRResponse<IUserHasId[], Error> => {
-  const distinctUserIds = userIds != null ? Array.from(new Set(userIds)).sort() : [];
+export const useSWRxUsersList = <Data, Error>(userIds: string[]): SWRResponse<IUserHasId[], Error> => {
+  const distinctUserIds = userIds.length > 0 ? Array.from(new Set(userIds)).sort() : [];
   return useSWR(
-    ['/users/list', distinctUserIds],
-    endpoint => apiv3Get(endpoint, { userIds: distinctUserIds.join(',') }).then(response => response.data.users),
+    distinctUserIds.length > 0 ? ['/users/list', distinctUserIds] : null,
+    (endpoint, userIds) => apiv3Get(endpoint, { userIds: userIds.join(',') }).then((response) => {
+      return response.data.users;
+    }),
   );
 };
