094e273b1a00b2b549e451a8d6f4bd758512fba9
Yuki Takei <yuki@weseek.co.jp>
2022-01-21 16:45:42 +0000

    fix comment

--- packages/app/src/server/routes/index.js
+++ packages/app/src/server/routes/index.js
@@ -13,7 +13,7 @@ const rateLimit = require('express-rate-limit');
 
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10, // limit each IP to 5 requests per windowMs
+  max: 10, // limit each IP to 10 requests per windowMs
   message:
     'Too many requests sent from this IP, please try again after 15 minutes',
 });
