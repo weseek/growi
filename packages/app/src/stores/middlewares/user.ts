7850e62204ebe4bf4987b44e72ecd252c8b390ab
Shun Miyazawa <shun-m@weseek.co.jp>
2022-01-21 18:27:22 +0900

    fb fix

--- packages/app/src/stores/middlewares/user.ts
+++ packages/app/src/stores/middlewares/user.ts
@@ -4,10 +4,7 @@ export const checkAndUpdateImageUrlCached = (useSWRNext) => {
   return (key, fetcher, config) => {
     const swrNext = useSWRNext(key, fetcher, config);
     if (swrNext.data != null) {
-      const likerIds = swrNext.data?.likerIds != null ? swrNext.data.likerIds : [];
-      const seenUserIds = swrNext.data?.seenUserIds != null ? swrNext.data.seenUserIds : [];
-      const distinctUserIds = Array.from(new Set([...likerIds, ...seenUserIds]));
-
+      const distinctUserIds = Array.from(new Set([swrNext.data]));
       if (distinctUserIds.length > 0) {
         apiv3Put('/users/update.imageUrlCache', { userIds: distinctUserIds });
       }
--- packages/app/src/stores/user.tsx
+++ packages/app/src/stores/user.tsx
@@ -4,6 +4,8 @@ import { apiv3Get } from '~/client/util/apiv3-client';
 
 import { IUserHasId } from '~/interfaces/user';
 
+import { checkAndUpdateImageUrlCached } from '~/stores/middlewares/user';
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const useSWRxUsersList = <Data, Error>(userIds: string[]): SWRResponse<IUserHasId[], Error> => {
   const distinctUserIds = userIds.length > 0 ? Array.from(new Set(userIds)).sort() : [];
@@ -12,5 +14,6 @@ export const useSWRxUsersList = <Data, Error>(userIds: string[]): SWRResponse<IU
     (endpoint, userIds) => apiv3Get(endpoint, { userIds: userIds.join(',') }).then((response) => {
       return response.data.users;
     }),
+    { use: [checkAndUpdateImageUrlCached] },
   );
 };
