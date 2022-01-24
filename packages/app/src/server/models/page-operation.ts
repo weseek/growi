7dcc55c4ea4201015f0528868f9d248498fb81a8
kaori <kaori@weseek.co.jp>
2022-01-24 07:10:10 +0000

    create PageOperationModel

---
+++ packages/app/src/server/models/page-operation.ts
@@ -0,0 +1,41 @@
+import {
+  Types, Document, Schema, Model,
+} from 'mongoose';
+
+import { getOrCreateModel } from '@growi/core';
+
+
+export interface PageOperationDocument extends Document {
+  _id: Types.ObjectId
+  pageId: Types.ObjectId
+  createdAt: Date
+  expiredAt: Date
+}
+
+
+export type PageOperationModel = Model<PageOperationDocument>
+
+const pageOperationSchema = new Schema<PageOperationDocument, PageOperationModel>({
+  pageId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Page',
+    index: true,
+    require: true,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+    required: true,
+  },
+  expiredAt: {
+    type: Date,
+    // 1 hour after being created a document
+    default: new Date(Date.now() + 3600000),
+    required: true,
+  },
+});
+
+
+const PageOperation = getOrCreateModel<PageOperationDocument, PageOperationModel>('PageOperation', pageOperationSchema);
+
+export { PageOperation };
