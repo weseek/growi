0fb46604090a05c54da5b904af40ef9d6cbcd3d9
Mao <maow89126@gmail.com>
2022-01-24 06:40:55 +0000

    86485 i18n

--- packages/app/src/components/DuplicatePage.tsx
+++ packages/app/src/components/DuplicatePage.tsx
@@ -17,7 +17,7 @@ const DuplicatePageAlert : FC<Props> = (props: Props) => {
       <h5 className="font-weight-bold mt-1">{t('duplicated_page_alert.same_page_name_exists', { pageName: devidedPath.latter })}</h5>
       <p>
         {t('duplicated_page_alert.same_page_name_exists_at_path',
-          { path: devidedPath.isFormerRoot ? '/' : devidedPath.former, pathName: devidedPath.latter })}<br />
+          { path: devidedPath.isFormerRoot ? '/' : devidedPath.former, pageName: devidedPath.latter })}<br />
         <p dangerouslySetInnerHTML={{ __html: t('See_more_detail_on_new_schema', { url: t('GROWI.4.9_new_schema') }) }} />
       </p>
       <p className="mb-1">{t('duplicated_page_alert.select_page_to_see')}</p>
