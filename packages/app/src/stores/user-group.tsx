3675bf3a71d4bbe8facbc4266cdcbd86367923b4
Shun Miyazawa <shun-m@weseek.co.jp>
2022-01-27 16:35:20 +0900

    add type

--- packages/app/src/stores/user-group.tsx
+++ packages/app/src/stores/user-group.tsx
@@ -34,10 +34,10 @@ export const useSWRxChildUserGroupList = (
   );
 };
 
-export const useSWRxUserGroupRelations = (groupId: string) => {
+export const useSWRxUserGroupRelations = (groupId: string): SWRResponse<IUserGroupRelationHasId[], Error> => {
   return useSWRImmutable(
     groupId != null ? [`user-groups/${groupId}/user-group-relations`] : null,
-    endpoint => apiv3Get(endpoint).then(result => result.data),
+    endpoint => apiv3Get<UserGroupRelationListResult>(endpoint).then(result => result.data.userGroupRelations),
   );
 };
 
