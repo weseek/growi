8a03c43e4522dc3c84e47ce7224e92d7f720e8ab
yuto-oweseek <yuto-o@weseek.co.jp>
2022-01-28 08:08:18 +0000

    add tag interfaces and update Tag.tsx and TagList.tsx

--- packages/app/src/interfaces/tag.ts
+++ packages/app/src/interfaces/tag.ts
@@ -1,4 +1,11 @@
+import { HasObjectId } from './has-object-id';
+
 export type ITag = {
   name: string,
   createdAt: Date;
 }
+export type ITagData = {
+  name: string,
+  count: number,
+}
+export type ITagDataHasId = ITagData & HasObjectId
