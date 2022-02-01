9f3bed165c83406ecf90e56c12f13f02c5dad204
Yuki Takei <yuki@weseek.co.jp>
2022-02-01 14:12:44 +0000

    typescriptize NotFoundPage

--- packages/app/src/components/NotFoundPage.tsx
+++ packages/app/src/components/NotFoundPage.tsx
@@ -1,14 +1,14 @@
 import React, { useMemo } from 'react';
-import PropTypes from 'prop-types';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
+
 import PageListIcon from './Icons/PageListIcon';
 import TimeLineIcon from './Icons/TimeLineIcon';
 import CustomNavAndContents from './CustomNavigation/CustomNavAndContents';
 import PageList from './PageList';
 import PageTimeline from './PageTimeline';
 
-const NotFoundPage = (props) => {
-  const { t } = props;
+const NotFoundPage = (): JSX.Element => {
+  const { t } = useTranslation();
 
   const navTabMapping = useMemo(() => {
     return {
@@ -35,8 +35,4 @@ const NotFoundPage = (props) => {
   );
 };
 
-NotFoundPage.propTypes = {
-  t: PropTypes.func.isRequired, //  i18next
-};
-
-export default withTranslation()(NotFoundPage);
+export default NotFoundPage;
