7e0e65490b3117ade75fac915063d7dbcdb9b0f1
Mao <maow89126@gmail.com>
2022-02-01 06:06:47 +0000

    85465 LegacyControl

---
+++ packages/app/src/components/LegacyPage/LegacyControl.tsx
@@ -0,0 +1,13 @@
+import React, { FC, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+
+type Props = {
+
+}
+
+const LegacyControl: FC <Props> = (props: Props) => {
+  return <></>;
+};
+
+export default LegacyControl;
