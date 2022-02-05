f94032c04243e80a4ea4fa014a7d242b1ddea896
Yuki Takei <yuki@weseek.co.jp>
2022-02-05 17:15:06 +0000

    improve Ref type

--- packages/app/src/interfaces/page.ts
+++ packages/app/src/interfaces/page.ts
@@ -9,7 +9,7 @@ import { SubscriptionStatusType } from './subscription';
 export interface IPage {
   path: string,
   status: string,
-  revision: Ref<IRevision & HasObjectId>,
+  revision: Ref<IRevision>,
   tags: Ref<ITag>[],
   creator: Ref<IUser>,
   createdAt: Date,
