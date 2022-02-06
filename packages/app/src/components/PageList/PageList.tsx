44cb19e00095b38a311d8e1ba7684f4a369b2025
kaori <kaori@weseek.co.jp>
2022-02-06 15:23:24 +0000

    clean code

--- packages/app/src/components/PageList/PageList.tsx
+++ packages/app/src/components/PageList/PageList.tsx
@@ -27,7 +27,7 @@ const PageList = (props: Props): JSX.Element => {
   }
 
   const pageList = pages.items.map(page => (
-    <PageListItemL key={page._id} page={page} isEnableActions={isEnableActions} />
+    <PageListItemL page={page} isEnableActions={isEnableActions} />
   ));
 
   if (pageList.length === 0) {
