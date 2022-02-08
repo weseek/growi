79590dc62d25913d25afa716e5df7ca2d44a2c23
Yuki Takei <yuki@weseek.co.jp>
2022-02-08 08:40:18 +0000

    WIP: re-impl CustomTabContent

---
+++ packages/app/src/components/CustomNavigation/CustomTabContent.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+  TabContent, TabPane,
+} from 'reactstrap';
+
+import { ICustomNavTabMappings } from '~/interfaces/ui';
+
+
+type Props = {
+  activeTab: string,
+  navTabMapping: ICustomNavTabMappings,
+  additionalClassNames?: string[],
+
+}
+
+const CustomTabContent = (props: Props): JSX.Element => {
+
+  const { activeTab, navTabMapping, additionalClassNames } = props;
+
+  const [activatedContent, setActivatedContent] = useState<Set<string>>(new Set<string>());
+
+  // add activated content to Set
+  useEffect(() => {
+    setActivatedContent(activatedContent.add(activeTab));
+  }, [activatedContent, activeTab]);
+
+  return (
+    <TabContent activeTab={activeTab} className={additionalClassNames != null ? additionalClassNames.join(' ') : ''}>
+      {Object.entries(navTabMapping).map(([key, value]) => {
+
+        const shouldRender = key === activeTab || activatedContent.has(key);
+        const { Content } = value;
+
+        return (
+          <TabPane key={key} tabId={key}>
+            { shouldRender && <Content /> }
+          </TabPane>
+        );
+      })}
+    </TabContent>
+  );
+
+};
+
+CustomTabContent.propTypes = {
+  activeTab: PropTypes.string.isRequired,
+  navTabMapping: PropTypes.object.isRequired,
+  additionalClassNames: PropTypes.arrayOf(PropTypes.string),
+};
+CustomTabContent.defaultProps = {
+  additionalClassNames: [],
+};
+
+export default CustomTabContent;
