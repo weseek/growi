271d0b954f2692a85b5808239f08b83c7a685c6a
Shun Miyazawa <m0zurillex@gmail.com>
2022-02-08 08:11:06 +0000

    Revert "Merge pull request #5276 from weseek/revert-5275-feat/87755-using-swr-to-display-duplicate-modal"
    This reverts commit c423926f4bd3acb249531655cc09859fd6d47296, reversing
    changes made to 2183cdedf4027c86f90b0574023fa6766a070a3a.

--- packages/app/src/components/PageList/PageListItemL.tsx
+++ packages/app/src/components/PageList/PageListItemL.tsx
@@ -5,7 +5,7 @@ import { format } from 'date-fns';
 
 import { UserPicture, PageListMeta } from '@growi/ui';
 import { DevidedPagePath } from '@growi/core';
-import { useIsDeviceSmallerThanLg } from '~/stores/ui';
+import { useIsDeviceSmallerThanLg, usePageDuplicateModalStatus } from '~/stores/ui';
 import {
   IPageInfoAll, IPageWithMeta, isIPageInfoForEntity, isIPageInfoForListing,
 } from '~/interfaces/page';
@@ -34,6 +34,7 @@ export const PageListItemL = memo((props: Props): JSX.Element => {
   } = props;
 
   const { data: isDeviceSmallerThanLg } = useIsDeviceSmallerThanLg();
+  const { open: openDuplicateModal } = usePageDuplicateModalStatus();
 
   const elasticSearchResult = isIPageSearchMeta(pageMeta) ? pageMeta.elasticSearchResult : null;
   const revisionShortBody = isIPageInfoForListing(pageMeta) ? pageMeta.revisionShortBody : null;
@@ -55,6 +56,10 @@ export const PageListItemL = memo((props: Props): JSX.Element => {
       onClickItem(pageData._id);
     }
   }, [isDeviceSmallerThanLg, onClickItem, pageData._id]);
+
+  const onClickDuplicateMenuItem = () => {
+    openDuplicateModal(pageData._id, pageData.path);
+  };
 
   const styleListGroupItem = (!isDeviceSmallerThanLg && onClickCheckbox != null) ? 'list-group-item-action' : '';
   // background color of list item changes when class "active" exists under 'list-group-item'
@@ -117,6 +122,7 @@ export const PageListItemL = memo((props: Props): JSX.Element => {
                   pageInfo={pageMeta}
                   onClickDeleteMenuItem={props.onClickDeleteButton}
                   isEnableActions={isEnableActions}
+                  onClickDuplicateMenuItem={onClickDuplicateMenuItem}
                 />
               </div>
             </div>
