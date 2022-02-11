c6f96e393260b58112c15faeeb1f141189b8103f
Shun Miyazawa <m0zurillex@gmail.com>
2022-02-11 16:45:57 +0900

    add docs

--- packages/core/src/utils/path-utils.js
+++ packages/core/src/utils/path-utils.js
@@ -107,6 +107,13 @@ export function normalizePath(path) {
   return `/${match[3]}`;
 }
 
+
+/**
+ *
+ * @param {string} path
+ * @returns {string}
+ * @memberof pathUtils
+ */
 export function attachTitleHeader(path) {
   return `# ${path}`;
 }
