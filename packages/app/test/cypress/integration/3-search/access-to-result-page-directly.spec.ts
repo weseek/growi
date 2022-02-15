5f22ae812304ed32c10d8aab3dca33f71795e6c9
Yuki Takei <yuki@weseek.co.jp>
2022-02-15 20:02:00 +0000

    update spec

--- packages/app/test/cypress/integration/3-search/access-to-result-page-directly.spec.ts
+++ packages/app/test/cypress/integration/3-search/access-to-result-page-directly.spec.ts
@@ -20,14 +20,14 @@ context('Access to search result page directly', () => {
   });
 
   it('/_search with "q" param is successfully loaded', () => {
-    cy.visit('/_search', { qs: { q: 'sandbox headers blockquotes' } });
+    cy.visit('/_search', { qs: { q: 'bootstrap4' } });
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(1000);
     cy.screenshot(`${ssPrefix}-with-q`, { capture: 'viewport' });
   });
 
   it('checkboxes behaviors', () => {
-    cy.visit('/_search', { qs: { q: 'sandbox headers blockquotes' } });
+    cy.visit('/_search', { qs: { q: 'bootstrap4' } });
 
     cy.getByTestid('cb-select').first().click({force: true});
     cy.screenshot(`${ssPrefix}-the-first-checkbox-on`, { capture: 'viewport' });
