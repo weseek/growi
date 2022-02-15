d5eb9bbb1f0ed94e62a4867ab9e692fe82805c0d
Taichi Masuyama <montanha.masu536@gmail.com>
2022-02-15 18:33:21 +0000

    Revert "Research mongo conn error"
    This reverts commit e6770e17163366b29af29ebc2a7c83439a65a9dd.

--- packages/app/test/integration/v5-global-setup.js
+++ packages/app/test/integration/v5-global-setup.js
@@ -11,6 +11,8 @@ import mongoose from 'mongoose';
 
 import { initMongooseGlobalSettings, getMongoUri, mongoOptions } from '@growi/core';
 
+import { getInstance } from './setup-crowi';
+
 // check env
 if (process.env.NODE_ENV !== 'test') {
   throw new Error('\'process.env.NODE_ENV\' must be \'test\'');
