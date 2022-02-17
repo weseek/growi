75d6725f5be6ddc964bddad24a33030229551d14
yohei0125 <yohei0125@weseek.co.jp>
2022-02-17 22:45:03 +0000

    change test match

--- packages/app/jest.config.js
+++ packages/app/jest.config.js
@@ -38,7 +38,7 @@ module.exports = {
       rootDir: '.',
       roots: ['<rootDir>'],
       testMatch: ['<rootDir>/test/integration/**/*.test.ts', '<rootDir>/test/integration/**/*.test.js',
-                  '?!<rootDir>/test/integration/**/v5.*.test.ts', '?!<rootDir>/test/integration/**/v5.*.test.js'],
+                  '!<rootDir>/test/integration/**/v5.*.test.ts', '!<rootDir>/test/integration/**/v5.*.test.js'],
 
       testEnvironment: 'node',
       globalSetup: '<rootDir>/test/integration/global-setup.js',
