7023e33cc5bf4d00734626f3fe5d981a726a069e
yohei0125 <yohei0125@weseek.co.jp>
2022-02-17 23:48:07 +0000

    add modulePathIgnorePatterns with regex

--- packages/app/jest.config.js
+++ packages/app/jest.config.js
@@ -37,9 +37,9 @@ module.exports = {
 
       rootDir: '.',
       roots: ['<rootDir>'],
-      testMatch: ['<rootDir>/test/integration/**/*.test.ts', '<rootDir>/test/integration/**/*.test.js',
-                  '!<rootDir>/test/integration/**/v5.*.test.ts', '!<rootDir>/test/integration/**/v5.*.test.js'],
-
+      testMatch: ['<rootDir>/test/integration/**/*.test.ts', '<rootDir>/test/integration/**/*.test.js'],
+      // https://regex101.com/r/nErofj/1
+      modulePathIgnorePatterns: ['<rootDir>/test/integration//*.*/v5(..*)*.[t|j]s'],
       testEnvironment: 'node',
       globalSetup: '<rootDir>/test/integration/global-setup.js',
       globalTeardown: '<rootDir>/test/integration/global-teardown.js',
