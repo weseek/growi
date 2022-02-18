c8df95225f227c16b71cfd4c8405b3bc1f9c8071
Shun Miyazawa <m0zurillex@gmail.com>
2022-02-18 17:35:22 +0900

    add vrt test

--- packages/app/test/cypress/integration/2-basic-features/access-to-presentation.spec.ts
+++ packages/app/test/cypress/integration/2-basic-features/access-to-presentation.spec.ts
@@ -20,13 +20,30 @@ context('Open presentation modal', () => {
     }
   });
 
-  it('Successfully loaded the presentation modal in /.', () => {
+  it('Successfully loaded the presentation modal in /', () => {
     cy.visit('/');
     cy.getByTestid('sub-nav-buttons-page-item-control').first().click();
     cy.getByTestid('open-presentation-modal-btn').first().click();
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(1500);
-    cy.screenshot(`${ssPrefix}-sandbox-headers`, { capture: 'viewport' });
+    cy.screenshot(`${ssPrefix}-top`, { capture: 'viewport' });
   });
 
+  it('Successfully loaded the presentation modal in "/Sandbox/Bootstrap4"', () => {
+    cy.visit('/Sandbox/Bootstrap4');
+    cy.getByTestid('sub-nav-buttons-page-item-control').first().click();
+    cy.getByTestid('open-presentation-modal-btn').first().click();
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(2500);
+    cy.screenshot(`${ssPrefix}-sandbox`, { capture: 'viewport' });
+  });
+
+  it('Successfully loaded the presentation modal in "/Sandbox/Bootstrap4#Cards"', () => {
+    cy.visit('/Sandbox/Bootstrap4#Cards');
+    cy.getByTestid('sub-nav-buttons-page-item-control').first().click();
+    cy.getByTestid('open-presentation-modal-btn').first().click();
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(2500);
+    cy.screenshot(`${ssPrefix}-Bootstrap4-with-anker-lint`, { capture: 'viewport' });
+  });
 });
