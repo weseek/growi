3fc4979afa603895f78348089ea974c9abc9aac8
Taichi Masuyama <montanha.masu536@gmail.com>
2022-02-22 11:40:17 +0000

    Fixed

--- packages/app/src/components/PageDuplicateModal.jsx
+++ packages/app/src/components/PageDuplicateModal.jsx
@@ -89,8 +89,7 @@ const PageDuplicateModal = (props) => {
   const getSubordinatedList = useCallback(async() => {
     try {
       const res = await appContainer.apiv3Get('/pages/subordinated-list', { path, limit: LIMIT_FOR_LIST });
-      const { subordinatedPaths } = res.data;
-      setSubordinatedPages(subordinatedPaths);
+      setSubordinatedPages(res.data.subordinatedPages);
     }
     catch (err) {
       setErrs(err);
