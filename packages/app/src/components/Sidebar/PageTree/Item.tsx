0e6e5d11f5f9b81b7f86c49cebf6eab447b03244
kaori <kaori@weseek.co.jp>
2022-02-22 07:46:35 +0000

    fix typo

--- packages/app/src/components/Sidebar/PageTree/Item.tsx
+++ packages/app/src/components/Sidebar/PageTree/Item.tsx
@@ -401,7 +401,7 @@ const Item: FC<ItemProps> = (props: ItemProps) => {
             onClickRenameMenuItem={renameMenuItemClickHandler}
             onClickDeleteMenuItem={deleteMenuItemClickHandler}
           >
-            {/* pass the color propaty to reactstrap dropdownToggle props. https://6-4-0--reactstrap.netlify.app/components/dropdowns/  */}
+            {/* pass the color property to reactstrap dropdownToggle props. https://6-4-0--reactstrap.netlify.app/components/dropdowns/  */}
             <DropdownToggle color="transparent" className="border-0 rounded btn-page-item-control p-0 grw-visible-on-hover">
               <i className="icon-options fa fa-rotate-90 text-muted p-1"></i>
             </DropdownToggle>
