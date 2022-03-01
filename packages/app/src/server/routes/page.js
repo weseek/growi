a71c9f2ca5e431454828a4f345f3cf9da984659d
kaori <kaori@weseek.co.jp>
2022-03-01 05:25:50 +0000

    fix validator

--- packages/app/src/server/routes/page.js
+++ packages/app/src/server/routes/page.js
@@ -1229,7 +1229,7 @@ module.exports = function(crowi, app) {
 
   validator.revertRemove = [
     body('recursively')
-      .custom(v => v === 'true' || v === true || null)
+      .custom(v => v === 'true' || v === true || v == null)
       .withMessage('The body property "recursively" must be "true" or true. (Omit param for false)'),
   ];
 
