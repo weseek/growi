2ecd534318019ca24f07c1f221d1cecef8f47a5e
asami-n <asami-n@weseek.co.jp>
2022-03-08 05:02:46 +0000

    imprv/vrt-delete-modal

---
+++ packages/app/test/cypress/integration/2-basic-features/open-page-delete-modal2.spec.ts
@@ -0,0 +1,35 @@
+context('Open PageDeleteModal', () => {
+
+  const ssPrefix = 'open-page-delete-modal';
+
+  let connectSid: string | undefined;
+
+  before(() => {
+    // login
+    cy.fixture("user-admin.json").then(user => {
+      cy.login(user.username, user.password);
+    });
+    cy.getCookie('connect.sid').then(cookie => {
+      connectSid = cookie?.value;
+    });
+    // collapse sidebar
+    cy.collapseSidebar(true);
+  });
+
+  beforeEach(() => {
+    if (connectSid != null) {
+      cy.setCookie('connect.sid', connectSid);
+      cy.visit('/');
+    }
+  });
+
+  it('PageDeleteModal is shown successfully', () => {
+    cy.visit('/_search?q=tag:we');
+    cy.getByTestid('open-page-item-control-btn')[0].click();
+    cy.contains('Delete').click();
+    cy.screenshot(`${ssPrefix}-open`,{ capture: 'viewport' });
+
+
+  });
+
+});
