387b65b806ecf52a0941792c51b161ecefd51e3b
Tatsuya Ise <tatsuya@weseek.co.jp>
2022-03-09 19:03:38 +0000

    refs #90324: テスト用のユーザーグループを用意

--- packages/app/test/integration/global-setup.js
+++ packages/app/test/integration/global-setup.js
@@ -27,6 +27,8 @@ module.exports = async() => {
   // init DB
   const pageCollection = mongoose.connection.collection('pages');
   const userCollection = mongoose.connection.collection('users');
+  const userGroupCollection = mongoose.connection.collection('userGroups');
+  const userGroupRelationCollection = mongoose.connection.collection('userGroupRelations');
 
   // create global user & rootPage
   const globalUser = (await userCollection.insertMany([{ name: 'globalUser', username: 'globalUser', email: 'globalUser@example.com' }]))[0];
@@ -34,6 +36,30 @@ module.exports = async() => {
     { name: 'v5DummyUser1', username: 'v5DummyUser1', email: 'v5DummyUser1@example.com' },
     { name: 'v5DummyUser2', username: 'v5DummyUser2', email: 'v5DummyUser2@example.com' },
   ]);
+
+  await userGroupCollection.insertMany([{ name: 'DummyGroup1' }]);
+  const dummyUserGroup1 = await userCollection.findOne({ username: 'v5DummyUser1' });
+  await userGroupCollection.insertMany([{ name: 'DummyChildGroup2', parent: dummyUserGroup1 }]);
+  const dummyChildUserGroup2 = await userGroupCollection.findOne({ name: 'DummyChildGroup2' });
+
+  const dummyUser1 = await userCollection.findOne({ username: 'v5DummyUser1' });
+  const dummyUser2 = await userCollection.findOne({ username: 'v5DummyUser2' });
+
+  await userGroupRelationCollection.insertMany([
+    {
+      relatedGroup: dummyUserGroup1,
+      relatedUser: dummyUser1,
+    },
+    {
+      relatedGroup: dummyUserGroup1,
+      relatedUser: dummyUser2,
+    },
+    {
+      relatedGroup: dummyChildUserGroup2,
+      relatedUser: dummyUser1,
+    },
+  ]);
+
   await pageCollection.insertMany([{
     path: '/',
     grant: 1,
