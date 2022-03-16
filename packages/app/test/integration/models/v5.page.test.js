97799a02698dd304cfbd0769ecd4b2845bf421e8
yohei0125 <yohei0125@weseek.co.jp>
2022-03-16 09:33:18 +0000

    remove method

--- packages/app/test/integration/models/v5.page.test.js
+++ packages/app/test/integration/models/v5.page.test.js
@@ -18,14 +18,6 @@ describe('Page', () => {
   let rootPage;
   let dummyUser1;
 
-  // pass unless the data is one of [false, 0, '', null, undefined, NaN]
-  const expectAllToBeTruthy = (dataList) => {
-    dataList.forEach((data, i) => {
-      if (data == null) { console.log(`index: ${i}`) }
-      expect(data).toBeTruthy();
-    });
-  };
-
   beforeAll(async() => {
     crowi = await getInstance();
     await crowi.configManager.updateConfigsInTheSameNamespace('crowi', { 'app:isV5Compatible': true });
