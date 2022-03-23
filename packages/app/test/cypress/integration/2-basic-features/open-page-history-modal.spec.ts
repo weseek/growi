89435ba7330e44570d3456ea741d0aaba41ee5f3
asami-n <asami-n@weseek.co.jp>
2022-03-23 04:59:35 +0000

    Firstcommit

---
+++ packages/app/test/cypress/integration/2-basic-features/open-page-history-modal.spec.ts
@@ -0,0 +1,36 @@
+context('Open Page History Modal', () => {
+
+  const ssPrefix = 'access-to-page-history-modal';
+
+  let connectSid: string | undefined;
+
+  before(() => {
+    // login
+    cy.fixture("user-admin.json").then(user => {
+      cy.login(user.username, user.password);
+    });
+    cy.getCookie('connect.sid').then(cookie => {
+      connectSid = cookie?.value;
+    });
+    // collapse sidebar
+    cy.collapseSidebar(true);
+  });
+
+  beforeEach(() => {
+    if (connectSid != null) {
+      cy.setCookie('connect.sid', connectSid);
+      cy.visit('/');
+    }
+  });
+
+  it('PageHistoryModal is shown successfully', () => {
+     cy.visit('/Sandbox/Bootstrap4', {  });
+     cy.get('#grw-subnav-container').within(() => {
+      //  cy.getByTestid('open-page-item-control-btn').click();
+      //  cy.getByTestid('open-page-move-rename-modal-btn').click();
+    });
+
+    //  cy.getByTestid('page-rename-modal').should('be.visible').screenshot(`${ssPrefix}-open-bootstrap4`);
+  });
+
+});
