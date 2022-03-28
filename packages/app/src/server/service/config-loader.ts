ad3a31178144688d3cf6a212b39195ae0c6499b3
yohei0125 <yohei0125@weseek.co.jp>
2022-03-28 07:25:00 +0000

    change default  of ELASTICSEARCH_VERSION

--- packages/app/src/server/service/config-loader.ts
+++ packages/app/src/server/service/config-loader.ts
@@ -278,7 +278,7 @@ const ENV_VAR_NAME_TO_CONFIG_INFO = {
     ns:      'crowi',
     key:     'app:elasticsearchVersion',
     type:    ValueType.NUMBER,
-    default: 7,
+    default: 6,
   },
   ELASTICSEARCH_URI: {
     ns:      'crowi',
