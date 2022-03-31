4f07812305c000b4dc4a1722b957aeb17c28cb43
Yuki Takei <yuki@weseek.co.jp>
2022-03-31 19:56:54 +0000

    remove test case for presentation modal

--- packages/app/test/cypress/integration/2-basic-features/use-tools.spec.ts
+++ packages/app/test/cypress/integration/2-basic-features/use-tools.spec.ts
@@ -118,16 +118,4 @@ context('Open presentation modal', () => {
     cy.screenshot(`${ssPrefix}-open-bootstrap4`);
   });
 
-  it('PresentationModal for /Sandbox/Bootstrap4#Cards" is shown successfully', () => {
-    cy.visit('/Sandbox/Bootstrap4#Cards');
-
-    cy.get('#grw-subnav-container').within(() => {
-      cy.getByTestid('open-page-item-control-btn').click({force: true});
-      cy.getByTestid('open-presentation-modal-btn').click({force: true});
-    });
-
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(1500);
-    cy.screenshot(`${ssPrefix}-open-bootstrap4-with-ancker-link`);
-  });
 });
