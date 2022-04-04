9163a8cb01b0b6faf44a454b467bc8ddadc6228c
yohei0125 <yohei0125@weseek.co.jp>
2022-04-04 05:22:20 +0000

    add comment

--- packages/app/src/server/service/page.ts
+++ packages/app/src/server/service/page.ts
@@ -2100,8 +2100,8 @@ class PageService {
         isV5Compatible: true,
         isEmpty: true,
         isMovable,
-        isDeletable: true,
-        isAbleToDeleteCompletely: true,
+        isDeletable: true, // Todo: Restrict who can delete a page
+        isAbleToDeleteCompletely: true, // Todo: Restrict who can delete a page
         isRevertible: false,
       };
     }
