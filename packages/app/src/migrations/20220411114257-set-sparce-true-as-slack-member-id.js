47dca6bd7ff3bf0a77849db0e7c6f5f794a89a65
kaori <kaori@weseek.co.jp>
2022-04-11 11:54:20 +0000

    using syncIndexes

--- packages/app/src/migrations/20220411114257-set-sparce-true-as-slack-member-id.js
+++ packages/app/src/migrations/20220411114257-set-sparce-true-as-slack-member-id.js
@@ -6,19 +6,15 @@ import loggerFactory from '~/utils/logger';
 const logger = loggerFactory('growi:migrate:drop-pages-indices');
 
 /**
- * set sparce true to slackMemberId
+ * set sparce true as slackMemberId
  */
 module.exports = {
   async up(db) {
     logger.info('Apply migration');
     mongoose.connect(getMongoUri(), mongoOptions);
-    const User = getModelSafely('User') || require('~/server/models/user')();
 
-    await User.updateMany(
-      {},
-      { $set: { slackMemberId: { sparse: true } } },
-      // { upsert: true, new: true },
-    );
+    const User = getModelSafely('User') || require('~/server/models/user')();
+    await User.syncIndexes();
 
     logger.info('Migration has successfully applied');
   },
