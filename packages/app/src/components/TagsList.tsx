e80bfe14dca214eb159397b7efcb34e032192936
Shun Miyazawa <m0zurillex@gmail.com>
2022-04-12 16:04:32 +0000

    rename

--- packages/app/src/components/TagsList.tsx
+++ packages/app/src/components/TagsList.tsx
@@ -20,7 +20,7 @@ const TagsList: FC<Props> = (props: Props) => {
   const [activePage, setActivePage] = useState(1);
   const [pagingOffset, setPagingOffset] = useState(0);
 
-  const { data: tagsListResult, mutate } = useSWRxTagsList(PAGING_LIMIT, pagingOffset);
+  const { data: tagsList, mutate } = useSWRxTagsList(PAGING_LIMIT, pagingOffset);
 
   const handlePage = (selectedPageNumber: number) => {
     setActivePage(selectedPageNumber);
@@ -34,17 +34,17 @@ const TagsList: FC<Props> = (props: Props) => {
   return (
     <>
       <header className="py-0">
-        <h1 className="title text-center mt-5 mb-3 font-weight-bold">{`${t('Tags')}(${tagsListResult?.totalCount || 0})`}</h1>
+        <h1 className="title text-center mt-5 mb-3 font-weight-bold">{`${t('Tags')}(${tagsList?.totalCount || 0})`}</h1>
       </header>
       <div className="row text-center">
         <div className="col-12 mb-5 px-5">
-          <TagCloudBox tags={tagsListResult?.data || []} minSize={20} />
+          <TagCloudBox tags={tagsList?.data || []} minSize={20} />
         </div>
         <div className="col-12 tag-list mb-4">
           <ul className="list-group text-left">
             {
-              tagsListResult?.data != null && tagsListResult.data.length > 0
-                ? tagsListResult.data.map((tag) => {
+              tagsList?.data != null && tagsList.data.length > 0
+                ? tagsList.data.map((tag) => {
                   return (
                     <a key={tag.name} href={`/_search?q=tag:${tag.name}`} className="list-group-item">
                       <i className="icon-tag mr-2"></i>{tag.name}
@@ -60,7 +60,7 @@ const TagsList: FC<Props> = (props: Props) => {
           <PaginationWrapper
             activePage={activePage}
             changePage={handlePage}
-            totalItemsCount={tagsListResult?.totalCount || 0}
+            totalItemsCount={tagsList?.totalCount || 0}
             pagingLimit={PAGING_LIMIT}
             align="center"
             size="md"
