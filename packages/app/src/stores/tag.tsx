f1cf908f09e1c35adb33b6f4f30c661e8a5e3a0a
Shun Miyazawa <m0zurillex@gmail.com>
2022-04-24 16:24:54 +0000

    fix lint

--- packages/app/src/stores/tag.tsx
+++ packages/app/src/stores/tag.tsx
@@ -1,8 +1,8 @@
-import useSWR, { SWRResponse } from 'swr';
+import { SWRResponse } from 'swr';
 import useSWRImmutable from 'swr/immutable';
 
-import { ITagsListApiv1Result } from '~/interfaces/tag';
 import { apiGet } from '~/client/util/apiv1-client';
+import { ITagsListApiv1Result } from '~/interfaces/tag';
 
 export const useSWRxTagsList = (limit?: number, offset?: number): SWRResponse<ITagsListApiv1Result, Error> => {
   return useSWRImmutable(
