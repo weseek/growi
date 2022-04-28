ea15fe09df827deeebd385d6383e8841a165a7c9
Taichi Masuyama <montanha.masu536@gmail.com>
2022-04-28 00:46:16 +0000

    Fixed linting

--- packages/app/test/integration/models/v5.page.test.js
+++ packages/app/test/integration/models/v5.page.test.js
@@ -884,10 +884,28 @@ describe('Page', () => {
     });
     test('should find parent while NOT creating unnecessary empty pages with all v4 public pages', async() => {
       // All pages does not have parent (v4 schema)
-      const _pageA = await Page.findOne({ path: '/get_parent_A', grant: Page.GRANT_PUBLIC, isEmpty: false, parent: null });
-      const _pageAB = await Page.findOne({ path: '/get_parent_A/get_parent_B', grant: Page.GRANT_PUBLIC, isEmpty: false, parent: null });
-      const _emptyA = await Page.findOne({ path: '/get_parent_A', grant: Page.GRANT_PUBLIC, isEmpty: true });
-      const _emptyAB = await Page.findOne({ path: '/get_parent_A/get_parent_B', grant: Page.GRANT_PUBLIC, isEmpty: true });
+      const _pageA = await Page.findOne({
+        path: '/get_parent_A',
+        grant: Page.GRANT_PUBLIC,
+        isEmpty: false,
+        parent: null,
+      });
+      const _pageAB = await Page.findOne({
+        path: '/get_parent_A/get_parent_B',
+        grant: Page.GRANT_PUBLIC,
+        isEmpty: false,
+        parent: null,
+      });
+      const _emptyA = await Page.findOne({
+        path: '/get_parent_A',
+        grant: Page.GRANT_PUBLIC,
+        isEmpty: true,
+      });
+      const _emptyAB = await Page.findOne({
+        path: '/get_parent_A/get_parent_B',
+        grant: Page.GRANT_PUBLIC,
+        isEmpty: true,
+      });
 
       expect(_pageA).not.toBeNull();
       expect(_pageAB).not.toBeNull();
