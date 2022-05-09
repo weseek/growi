a7c51ead613745ce546c35d693ea56228abd1388
keigo-h <keigo-h@weseek.co.jp>
2022-05-09 06:28:41 +0000

    status

--- packages/app/src/components/DescendantsPageList.tsx
+++ packages/app/src/components/DescendantsPageList.tsx
@@ -63,11 +63,11 @@ export const DescendantsPageListSubstance = (props: SubstanceProps): JSX.Element
 
   const pageDeletedHandler: OnDeletedFunction = useCallback((...args) => {
     if (args[0] == null || args[2] == null) {
+      toastSuccess('ページを削除しました');
       return;
     }
-    if (args[0] != null && args[2] != null) {
-      toastSuccess(args[2] ? t('deleted_pages_completely', { path: args[0] }) : t('deleted_pages', { path: args[0] }));
-    }
+
+    toastSuccess(args[2] ? t('deleted_pages_completely', { path: args[0] }) : t('deleted_pages', { path: args[0] }));
 
     advancePt();
 
