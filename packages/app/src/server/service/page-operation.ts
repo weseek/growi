bd45f32c0367b3d7fbaa5aec731bb365d0e3b282
yohei0125 <yohei0125@weseek.co.jp>
2022-05-11 14:05:27 +0000

    add methods to check if pageOp is processing and pageOp info to page documents

--- packages/app/src/server/service/page-operation.ts
+++ packages/app/src/server/service/page-operation.ts
@@ -1,6 +1,7 @@
 import { pagePathUtils } from '@growi/core';
+import { FilterQuery, QueryOptions } from 'mongoose';
 
-import PageOperation, { PageActionType } from '~/server/models/page-operation';
+import PageOperation, { PageActionType, PageOperationDocument } from '~/server/models/page-operation';
 
 const { isEitherOfPathAreaOverlap, isPathAreaOverlap, isTrashPage } = pagePathUtils;
 
@@ -56,6 +57,95 @@ class PageOperationService {
     }
 
     return true;
+  }
+
+  /**
+   * check if the page operation is currently being processed
+   */
+  isProcessingPageOperation(pageOperation: PageOperationDocument): boolean {
+    const expiryDate = pageOperation.unprocessableExpiryDate;
+    return expiryDate != null && new Date() < expiryDate;
+  }
+
+  /**
+   * return Map data that are something like this
+   * {
+   *    'parentPageIdA': [
+   *       {pageOpId: '001', targtPageId: 'xxx', isProcessing: true},
+   *       {pageOpId: '002', targtPageId: 'xxx', isProcessing: true}
+   *    ],
+   *    'parentPageIdB': [
+   *       {pageOpId: '003', targtPageId: 'yyy', isProcessing: false}
+   *    ]
+   * }
+   */
+  async getRenameSubOpsInfoMap(
+      filter?: FilterQuery<PageOperationDocument>, projection?: any, options?: QueryOptions,
+  ): Promise<any> {
+
+    const renamePageSubOps = await PageOperation.findSubOps(filter, projection, options); // asc
+
+    const parentIdToPageOpsInfo = new Map();
+
+    for (const op of renamePageSubOps) {
+      const parentId = op.page.parent?.toString(); // key for the map
+      const pageOpId = op._id;
+      const targetPageId = op.page._id;
+      const path = op.page.path;
+      const isProcessing = this.isProcessingPageOperation(op);
+
+      const pageOpInfo = {
+        pageOpId, targetPageId, isProcessing, path, parentId,
+      };
+
+      // replace existing data with new data including new info if the key already exists
+      if (parentIdToPageOpsInfo.has(parentId)) {
+        const oldValue = parentIdToPageOpsInfo.get(parentId);
+        const newValue = [...oldValue, pageOpInfo];
+        // replace
+        parentIdToPageOpsInfo.set(parentId, newValue);
+        continue;
+      }
+      // set new data if the key does not exist
+      parentIdToPageOpsInfo.set(parentId, [pageOpInfo]); // value should be array because it's possible that multiple data with same key exist
+    }
+    return parentIdToPageOpsInfo;
+  }
+
+  /**
+   * add RenameOperationInfo to page documents as a new property
+   */
+  addShouldResumeRenameOpInfoToPages(pages, renameSubOpsInfoMap): any {
+    const newPages = [...pages]; // copy
+
+    renameSubOpsInfoMap.forEach((value, parentId) => {
+      for (const page of newPages) {
+        const pageId = page._id.toString();
+
+        if (pageId === parentId) {
+          if (page._doc != null) {
+            page._doc.shouldResumeOpInfo = value;
+          }
+          else {
+            page.shouldResumeOpInfo = value;
+          }
+          continue;
+        }
+      }
+    });
+    return newPages;
+  }
+
+  /**
+   * add RenameOperationInfo to root page document as a new property
+   */
+  async addShouldResumeRenameOpInfoToRootPage(rootPage) {
+    const filter = { actionType: PageActionType.Rename, path: '/' }; // exclude root page
+    const renameSubOpsInfoMap = await this.getRenameSubOpsInfoMap(filter);
+    const pages = await this.addShouldResumeRenameOpInfoToPages([rootPage], renameSubOpsInfoMap);
+
+    const modifiedRootPage = pages[0];
+    return modifiedRootPage;
   }
 
 }
