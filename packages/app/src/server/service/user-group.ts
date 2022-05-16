09c4214e4ea0dca6197174ce8d74015fa659efbb
kaori <kaori@weseek.co.jp>
2022-05-16 12:12:59 +0000

    clean code

--- packages/app/src/server/service/user-group.ts
+++ packages/app/src/server/service/user-group.ts
@@ -146,7 +146,6 @@ class UserGroupService {
     const serializedUser = serializeUserSecurely(user);
 
     return { user: serializedUser, deletedGroupsCount: deleteManyRes.deletedCount };
-
   }
 
 }
