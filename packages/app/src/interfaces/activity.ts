94292b113ca445e9468373e502dc11e997925e9f
Shun Miyazawa <m0zurillex@gmail.com>
2022-05-18 06:26:22 +0000

    fix lint

--- packages/app/src/interfaces/activity.ts
+++ packages/app/src/interfaces/activity.ts
@@ -66,7 +66,7 @@ export type SupportedTargetModelType = typeof SUPPORTED_TARGET_MODEL_TYPE[keyof 
 export type SupportedActionType = typeof SUPPORTED_ACTION_TYPE[keyof typeof SUPPORTED_ACTION_TYPE];
 
 
-export type ISnapshot =  Partial<Pick<IUser, 'username'>>
+export type ISnapshot = Partial<Pick<IUser, 'username'>>
 
 export type IActivity = {
   user?: IUser
