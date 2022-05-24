37545207fefe16c81a3ec057e555c68216c4628a
yohei0125 <yohei0125@weseek.co.jp>
2022-05-24 10:40:04 +0000

    add IPageActionType to page-operation interface

---
+++ packages/app/src/interfaces/page-operation.ts
@@ -0,0 +1,9 @@
+export const IPageActionType = {
+  Rename: 'Rename',
+  Duplicate: 'Duplicate',
+  Delete: 'Delete',
+  DeleteCompletely: 'DeleteCompletely',
+  Revert: 'Revert',
+  NormalizeParent: 'NormalizeParent',
+} as const;
+export type IPageActionType = typeof IPageActionType[keyof typeof IPageActionType]
