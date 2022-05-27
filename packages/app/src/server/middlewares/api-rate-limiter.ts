bbb547a4589c3c9bb37dc440e7b12ffb29d5bea9
yuken <yuken-t@weseek.co.jp>
2022-05-27 09:51:07 +0000

    delete e

--- packages/app/src/server/middlewares/api-rate-limiter.ts
+++ packages/app/src/server/middlewares/api-rate-limiter.ts
@@ -1,4 +1,4 @@
-import e, { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { RateLimiterMemory } from 'rate-limiter-flexible';
 
 import loggerFactory from '~/utils/logger';
