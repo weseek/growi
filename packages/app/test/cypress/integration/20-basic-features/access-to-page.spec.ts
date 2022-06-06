cbbd94e39611b028be58c50edd2f9ef0f08d3034
mudana <mudana@gruneasia.info>
2022-06-06 21:53:22 +0800

    Move confirm notification
    https://youtrack.weseek.co.jp/issue/GW-7728
    - Move confirm notification to access-to-page.spec.ts
    - Delete confirm-notification.spec.ts

--- packages/app/test/cypress/integration/20-basic-features/access-to-page.spec.ts
+++ packages/app/test/cypress/integration/20-basic-features/access-to-page.spec.ts
@@ -146,3 +146,31 @@ context('Access to Template Editing Mode', () => {
 
 });
 
+context('Confirm notification', () => {
+  const ssPrefix = 'confirm-notification-';
+
+  beforeEach(() => {
+    // login
+    cy.fixture("user-admin.json").then(user => {
+      cy.login(user.username, user.password);
+    });
+    // collapse sidebar
+    cy.collapseSidebar(true);
+  });
+
+  it('Notification list successfully loaded', () => {
+    cy.visit('/');
+    cy.get('.notification-wrapper > a').click();
+    cy.get('.notification-wrapper > .dropdown-menu > a').click();
+
+    cy.get('#all-in-app-notifications').should('be.visible');
+
+    cy.screenshot(`${ssPrefix}-see-all`, { capture: 'viewport' });
+
+    cy.get('.grw-custom-nav-tab > div > ul > li:nth-child(2) > a').click();
+
+    cy.screenshot(`${ssPrefix}-see-unread`, { capture: 'viewport' });
+   });
+
+})
+
