ddd81b13568fa54fd3443b0cbb5453f72d6d37cf
kaori <kaori@weseek.co.jp>
2022-06-08 12:46:16 +0000

    move code

--- packages/app/src/server/service/page-grant.ts
+++ packages/app/src/server/service/page-grant.ts
@@ -408,12 +408,6 @@ class PageGrantService {
     const Page = mongoose.model('Page') as unknown as PageModel;
     const UserGroupRelation = mongoose.model('UserGroupRelation') as any; // TODO: Typescriptize model
 
-    // Increment an object (type IRecordApplicableGrant)
-    // grant is never public, anyone with the link, nor specified
-    const data: IRecordApplicableGrant = {
-      [Page.GRANT_RESTRICTED]: null, // any page can be restricted
-    };
-
     // -- Public only if top page
     const isOnlyPublicApplicable = isTopPage(page.path);
     if (isOnlyPublicApplicable) {
@@ -421,6 +415,12 @@ class PageGrantService {
         [Page.GRANT_PUBLIC]: null,
       };
     }
+
+    // Increment an object (type IRecordApplicableGrant)
+    // grant is never public, anyone with the link, nor specified
+    const data: IRecordApplicableGrant = {
+      [Page.GRANT_RESTRICTED]: null, // any page can be restricted
+    };
 
     // -- Any grant is allowed if parent is null
     const isAnyGrantApplicable = page.parent == null;
