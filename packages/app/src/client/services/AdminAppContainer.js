8b0196ce32bb512cd490f4c00015d5f78116cf96
kaori <kaori@weseek.co.jp>
2022-06-09 12:42:26 +0000

    typescriptize AppSetting and fix fileUpload default value

--- packages/app/src/client/services/AdminAppContainer.js
+++ packages/app/src/client/services/AdminAppContainer.js
@@ -21,7 +21,7 @@ export default class AdminAppContainer extends Container {
       confidential: '',
       globalLang: '',
       isEmailPublishedForNewUser: true,
-      fileUpload: '',
+      fileUpload: false,
 
       isV5Compatible: null,
       siteUrl: '',
