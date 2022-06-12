3bd2722a0a51a5fb5ee30a6bba888d8deca85c6b
Taichi Masuyama <montanha.masu536@gmail.com>
2022-06-12 08:36:04 +0000

    Typescriptized & use es6 class for page event class

---
+++ packages/app/src/server/events/page.ts
@@ -0,0 +1,35 @@
+import events from 'events';
+
+import { PageDocument } from '../models/page';
+
+
+const debug = require('debug')('growi:events:page');
+
+class PageEvent extends events.EventEmitter {
+
+  crowi!: any
+
+  constructor(crowi: any) {
+    super();
+    this.crowi = crowi;
+  }
+
+  onCreate(page: PageDocument, user: any): void {
+    debug('onCreate event fired');
+  }
+
+  onUpdate(page: PageDocument, user: any): void {
+    debug('onUpdate event fired');
+  }
+
+  onCreateMany(pages: PageDocument[], user: any): void {
+    debug('onCreateMany event fired');
+  }
+
+  onAddSeenUsers(pages: PageDocument[], user: any): void {
+    debug('onAddSeenUsers event fired');
+  }
+
+}
+
+export default PageEvent;
