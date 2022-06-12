98ee7674318cbc3f63d3fc6cbe1f8c0347a2af1d
Shun Miyazawa <m0zurillex@gmail.com>
2022-06-12 08:31:34 +0000

    create link back to AuditLog Management

--- packages/app/src/components/Admin/AuditLogSettings.tsx
+++ packages/app/src/components/Admin/AuditLogSettings.tsx
@@ -1,11 +1,24 @@
 import React, { FC } from 'react';
 
+import { useTranslation } from 'react-i18next';
+
 export const AuditLogSettings: FC = () => {
+  const { t } = useTranslation();
+
   const adminAuditLogSettingsElm = document.getElementById('admin-audit-log-settings');
   const activityExpirationSeconds = adminAuditLogSettingsElm?.getAttribute('activity-expiration-seconds') || 2592000;
 
-
   return (
-    <>AuditLog Settings</>
+    <div data-testid="admin-auditlog-settings">
+      <h3 className="mb-4">
+        <a href="/admin/audit-log">
+          <i className="icon-arrow-left" />
+          {t('AuditLog')}
+        </a>
+      </h3>
+      <h2 className="admin-setting-header mb-3">
+        {t('AuditLog Settings')}
+      </h2>
+    </div>
   );
 };
