2bba469fe742477ac6051ffa3061d9ec2bdd36d9
Shun Miyazawa <m0zurillex@gmail.com>
2022-06-12 08:06:54 +0000

    get "activityExpirationSeconds" frontend

--- packages/app/src/components/Admin/AuditLogSettings.tsx
+++ packages/app/src/components/Admin/AuditLogSettings.tsx
@@ -1,6 +1,10 @@
 import React, { FC } from 'react';
 
 export const AuditLogSettings: FC = () => {
+  const adminAuditLogSettingsElm = document.getElementById('admin-audit-log-settings');
+  const activityExpirationSeconds = adminAuditLogSettingsElm?.getAttribute('activity-expiration-seconds') || 2592000;
+
+
   return (
     <>AuditLog Settings</>
   );
