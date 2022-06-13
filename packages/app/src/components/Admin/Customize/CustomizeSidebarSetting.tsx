8f2b947c1484b931bae853b3b26c36885fd76c1a
yuken <yuken-t@weseek.co.jp>
2022-06-13 04:36:47 +0000

    add default at dock mode

--- packages/app/src/components/Admin/Customize/CustomizeSidebarSetting.tsx
+++ packages/app/src/components/Admin/Customize/CustomizeSidebarSetting.tsx
@@ -8,6 +8,7 @@ import { isDarkMode as isDarkModeByUtil } from '~/client/util/color-scheme';
 const CustomizeSidebarsetting = (): JSX.Element => {
   const { t } = useTranslation();
   const [isDrawerMode, setIsDrawerMode] = useState(false);
+  const [isDefaultOpenAtDockMode, setIsDefaultOpenAtDockMode] = useState(false);
 
   const isDarkMode = isDarkModeByUtil();
   const colorText = isDarkMode ? 'dark' : 'light';
@@ -21,11 +22,11 @@ const CustomizeSidebarsetting = (): JSX.Element => {
       <div className="row">
         <div className="col-12">
 
-          <h2 className="admin-setting-header">{t('admin:customize_setting.default_sidebar_mode')}</h2>
+          <h2 className="admin-setting-header">{t('admin:customize_setting.default_sidebar_mode.title')}</h2>
 
           <Card className="card well my-3">
             <CardBody className="px-0 py-2">
-              {t('admin:customize_setting.default_sidebar_mode_desc')}
+              {t('admin:customize_setting.default_sidebar_mode.desc')}
             </CardBody>
           </Card>
 
@@ -53,6 +54,43 @@ const CustomizeSidebarsetting = (): JSX.Element => {
                   Dock Mode
                 </div>
               </div>
+            </div>
+          </div>
+
+          <Card className="card well my-3">
+            <CardBody className="px-0 py-2">
+              {t('admin:customize_setting.default_sidebar_mode.dock_mode_default_desc')}
+            </CardBody>
+          </Card>
+
+          <div className="d-flex flex-column align-items-center">
+            <div className="custom-control custom-radio my-3">
+              <input
+                type="radio"
+                id="radio-email-show"
+                className="custom-control-input"
+                name="mailVisibility"
+                checked={!isDrawerMode && isDefaultOpenAtDockMode}
+                disabled={isDrawerMode}
+                onChange={() => { setIsDefaultOpenAtDockMode(true) }}
+              />
+              <label className="custom-control-label" htmlFor="radio-email-show">
+                {t('admin:customize_setting.default_sidebar_mode.dock_mode_default_open')}
+              </label>
+            </div>
+            <div className="custom-control custom-radio my-3">
+              <input
+                type="radio"
+                id="radio-email-show"
+                className="custom-control-input"
+                name="mailVisibility"
+                checked={!isDrawerMode && !isDefaultOpenAtDockMode}
+                disabled={isDrawerMode}
+                onChange={() => { setIsDefaultOpenAtDockMode(false) }}
+              />
+              <label className="custom-control-label" htmlFor="radio-email-show">
+                {t('admin:customize_setting.default_sidebar_mode.dock_mode_default_close')}
+              </label>
             </div>
           </div>
 
