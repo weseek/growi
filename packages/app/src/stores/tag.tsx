63c1d693ba5695e237a311513ebbeaa1e65d145d
kaori <kaori@weseek.co.jp>
2022-06-14 11:31:38 +0000

    clean code

--- packages/app/src/stores/tag.tsx
+++ packages/app/src/stores/tag.tsx
@@ -1,10 +1,8 @@
 import { SWRResponse } from 'swr';
 import useSWRImmutable from 'swr/immutable';
 
-
 import { apiGet } from '~/client/util/apiv1-client';
 import { IResTagsListApiv1 } from '~/interfaces/tag';
-
 
 export const useSWRxTagsList = (limit?: number, offset?: number): SWRResponse<IResTagsListApiv1, Error> => {
   return useSWRImmutable(
