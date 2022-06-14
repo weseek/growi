b7a9b93dfe9595d6720986bf306126469a223db5
mudana <mudana@gruneasia.info>
2022-06-14 12:43:02 +0800

    fix isDefaultLogo load & store
    https://youtrack.weseek.co.jp/issue/GW-7759
    - Set isDefaultLogo true if not exists in customizeParams

--- packages/app/src/client/services/AdminCustomizeContainer.js
+++ packages/app/src/client/services/AdminCustomizeContainer.js
@@ -108,12 +108,17 @@ export default class AdminCustomizeContainer extends Container {
         currentCustomizeCss: customizeParams.customizeCss,
         currentCustomizeScript: customizeParams.customizeScript,
         brandLogoAttachmentId: customizeParams.brandLogoAttachmentId,
-        isDefaultLogo: customizeParams.uploadedLogoSrc == null ? true : customizeParams.isDefaultLogo,
         uploadedLogoSrc: customizeParams.uploadedLogoSrc,
       });
       // search style name from object for display
       this.setState({ currentHighlightJsStyleName: this.state.highlightJsCssSelectorOptions[customizeParams.styleName].name });
 
+      if (!('isDefaultLogo' in customizeParams)) {
+        this.setState({ isDefaultLogo: true });
+      }
+      else {
+        this.setState({ isDefaultLogo: customizeParams.isDefaultLogo });
+      }
     }
     catch (err) {
       this.setState({ retrieveError: err });
