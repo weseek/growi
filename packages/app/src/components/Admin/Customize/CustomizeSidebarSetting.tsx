f1e4ef6f6c5743cb66215690c687df554d9bb192
yuken <yuken-t@weseek.co.jp>
2022-06-16 02:38:07 +0000

    remove import

--- packages/app/src/components/Admin/Customize/CustomizeSidebarSetting.tsx
+++ packages/app/src/components/Admin/Customize/CustomizeSidebarSetting.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useCallback } from 'react';
 
 import { useTranslation } from 'react-i18next';
 import { Card, CardBody } from 'reactstrap';
