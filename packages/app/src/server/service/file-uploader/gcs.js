59ed60752bccb37e93589cbc5240b5d18b6f1deb
yohei0125 <yohei0125@weseek.co.jp>
2022-06-16 05:55:47 +0000

    refactor getGcsInstance

--- packages/app/src/server/service/file-uploader/gcs.js
+++ packages/app/src/server/service/file-uploader/gcs.js
@@ -2,8 +2,8 @@ import loggerFactory from '~/utils/logger';
 
 const logger = loggerFactory('growi:service:fileUploaderAws');
 
-const urljoin = require('url-join');
 const { Storage } = require('@google-cloud/storage');
+const urljoin = require('url-join');
 
 let _instance;
 
@@ -17,20 +17,13 @@ module.exports = function(crowi) {
     return configManager.getConfig('crowi', 'gcs:bucket');
   }
 
-  /**
-   * see https://googleapis.dev/nodejs/storage/latest/Storage.html
-   */
   function getGcsInstance() {
     if (_instance == null) {
+      // see https://googleapis.dev/nodejs/storage/latest/Storage.html
       const keyFilename = configManager.getConfig('crowi', 'gcs:apiKeyJsonPath');
-      if (keyFilename == null) {
-        // Create a client that uses Application Default Credentials
-        _instance = new Storage();
-      }
-      else {
-        // Create a client with explicit credentials
-        _instance = new Storage({ keyFilename });
-      }
+      _instance = keyFilename != null
+        ? new Storage({ keyFilename }) // Create a client with explicit credentials
+        : new Storage(); // Create a client that uses Application Default Credentials
     }
     return _instance;
   }
