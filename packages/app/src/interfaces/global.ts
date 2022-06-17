4917283f580bd5af7712a584d600dae334c77c1e
yuken <yuken-t@weseek.co.jp>
2022-06-17 05:16:57 +0000

    add InterceptorManager to CustomWindow

--- packages/app/src/interfaces/global.ts
+++ packages/app/src/interfaces/global.ts
@@ -1,3 +1,4 @@
+import InterceptorManager from '~/services/interceptor-manager';
 import Xss from '~/services/xss';
 
-export type CustomWindow = Window & typeof globalThis & { xss: Xss };
+export type CustomWindow = Window & typeof globalThis & { xss: Xss } & { interceptorManager: InterceptorManager };
