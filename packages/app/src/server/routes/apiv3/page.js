ca462e133d0e5b883410b6218785832be633dcf6
yohei0125 <yohei0125@weseek.co.jp>
2022-06-20 03:34:37 +0000

    include empty page

--- packages/app/src/server/routes/apiv3/page.js
+++ packages/app/src/server/routes/apiv3/page.js
@@ -256,7 +256,7 @@ module.exports = (crowi) => {
     let page;
     try {
       if (pageId != null) { // prioritized
-        page = await Page.findByIdAndViewer(pageId, user);
+        page = await Page.findByIdAndViewer(pageId, user, {}, true);
       }
       else {
         page = await Page.findByPathAndViewer(path, user);
