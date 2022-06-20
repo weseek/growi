2cc8b81e43e8705f1b2d7f805b44bc23b31f0f76
yuken <yuken-t@weseek.co.jp>
2022-06-20 11:25:39 +0000

    refactor CustomWindow

--- packages/app/src/interfaces/global.ts
+++ packages/app/src/interfaces/global.ts
@@ -1,5 +1,13 @@
+import EventEmitter from 'events';
+
 import Xss from '~/services/xss';
 
+import { IGraphViewer } from './graph-viewer';
 import { IInterceptorManager } from './interceptor-manager';
 
-export type CustomWindow = Window & typeof globalThis & { xss: Xss } & { interceptorManager: IInterceptorManager };
+export type CustomWindow = Window
+                         & typeof globalThis
+                         & { xss: Xss }
+                         & { interceptorManager: IInterceptorManager }
+                         & { globalEmitter: EventEmitter }
+                         & { GraphViewer: IGraphViewer };
