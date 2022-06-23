418e663c74d817083e47c099bcc72d96ae561f2c
Shunm634-source <anakin.yadon@gmail.com>
2022-06-23 04:56:38 +0000

    Initial pushing - Converted CustomScriptEditor file into tsx.

---
+++ packages/app/src/components/Admin/CustomScriptEditor.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import { UnControlled as CodeMirror } from 'react-codemirror2';
+
+require('codemirror/addon/lint/javascript-lint');
+require('codemirror/addon/hint/javascript-hint');
+require('codemirror/addon/hint/show-hint');
+require('codemirror/addon/edit/matchbrackets');
+require('codemirror/addon/edit/closebrackets');
+require('codemirror/mode/javascript/javascript');
+require('~/client/util/codemirror/autorefresh.ext');
+
+require('jquery-ui/ui/widgets/resizable');
+
+type Props = {
+  value: string,
+  onChange: (value: string) => void,
+};
+
+const CustomScriptEditor = (props: Props) => {
+
+  const { value, onChange } = props;
+
+  return (
+    <CodeMirror
+      value={value}
+      autoFocus
+      detach
+      options={{
+        mode: 'javascript',
+        lineNumbers: true,
+        tabSize: 2,
+        indentUnit: 2,
+        theme: 'eclipse',
+        autoRefresh: { force: true }, // force option is enabled by autorefresh.ext.js -- Yuki Takei
+        matchBrackets: true,
+        autoCloseBrackets: true,
+        extraKeys: { 'Ctrl-Space': 'autocomplete' },
+      }}
+      editorDidMount={(editor, next) => {
+        // resizable with jquery.ui
+        $(editor.getWrapperElement()).resizable({
+          resize() {
+            editor.setSize($(this).width(), $(this).height());
+          },
+        });
+      }}
+      onChange={(editor, data, value) => {
+        onChange(value);
+      }}
+    />
+  );
+};
