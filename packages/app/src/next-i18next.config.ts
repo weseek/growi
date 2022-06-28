65d5f78b9b86a1f8e987da32031f3d2f953e1187
Yuki Takei <yuki@weseek.co.jp>
2022-06-28 05:11:20 +0000

    clean code

--- packages/app/src/next-i18next.config.ts
+++ packages/app/src/next-i18next.config.ts
@@ -2,7 +2,6 @@ import I18nextChainedBackend from 'i18next-chained-backend';
 import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector';
 import I18NextHttpBackend from 'i18next-http-backend';
 import I18NextLocalStorageBackend from 'i18next-localstorage-backend';
-import path from 'path';
 
 const isServer = typeof window === 'undefined';
 
@@ -12,7 +11,6 @@ export const
     locales: ['en_US', 'ja_JP', 'zh_CN'],
   };
 export const defaultNS = 'translation';
-export const localePath = path.resolve('./public/static/locales');
 
 export const serializeConfig = false;
 export const use = isServer ? [] : [I18nextChainedBackend];
@@ -20,7 +18,9 @@ export const
   backend = {
     backends: isServer ? [I18nextBrowserLanguageDetector] : [I18NextLocalStorageBackend, I18NextHttpBackend],
     backendOptions: [
+      // options for i18next-localstorage-backend
       { expirationTime: 60 * 60 * 1000 }, // 1 hour
-      { loadPath: '/static/locales/{{lng}}/{{ns}}.json', } // for i18next-http-backend
+      // options for i18next-http-backend
+      { loadPath: '/static/locales/{{lng}}/{{ns}}.json', },
     ],
   };
