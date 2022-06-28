3232eb987bd700274925656fe427ac63e02ffa7b
yuken <yuken-t@weseek.co.jp>
2022-06-28 02:56:01 +0000

    create PageAlerts

---
+++ packages/app/src/components/PageAlert/PageAlerts.tsx
@@ -0,0 +1,13 @@
+
+
+export const PageAlerts = (): JSX.Element => {
+
+
+  return (
+    <div className="row d-edit-none">
+      <div className="col-sm-12">
+        {/* alerts */}
+      </div>
+    </div>
+  );
+}
