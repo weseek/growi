89f36d31a882bdb3aa89a2f005ff53a9301a05eb
yohei0125 <yohei0125@weseek.co.jp>
2022-06-28 08:28:25 +0000

    add UnlinkAlert

--- packages/app/src/components/PageAlert/UnlinkAlert.tsx
+++ packages/app/src/components/PageAlert/UnlinkAlert.tsx
@@ -1,4 +1,10 @@
+import { useTranslation } from 'react-i18next';
 
-export const UnlinkAlert = (): JSX.Element => {
-  return <>UnlinkAlert</>
+export const UnlinkAlert = (props): JSX.Element => {
+  const { t } = useTranslation();
+  return (
+    <div className="alert alert-info d-edit-none py-3 px-4">
+    <strong>{ t('Unlinked') }: </strong> { t('page_page.notice.unlinked') }
+  </div>
+  )
 }
