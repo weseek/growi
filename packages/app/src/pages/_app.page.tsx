585d2aa8d253bf0047a50921f198df6a6f21ea0a
Yuki Takei <yuki@weseek.co.jp>
2022-06-29 20:32:22 +0000

    reorganize scss

--- packages/app/src/pages/_app.page.tsx
+++ packages/app/src/pages/_app.page.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 
-import { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
-
-import * as nextI18nConfig from '../next-i18next.config';
-
-// import { appWithTranslation } from '~/i18n';
+import { AppProps } from 'next/app';
 
 import '~/styles/style-next.scss';
 import '~/styles/theme/default.scss';
 // import InterceptorManager from '~/service/interceptor-manager';
 
-import { useGrowiVersion } from '../stores/context';
+import * as nextI18nConfig from '../next-i18next.config';
 import { useI18nextHMR } from '../services/i18next-hmr';
+import { useGrowiVersion } from '../stores/context';
 
 import { CommonProps } from './commons';
 // import { useInterceptorManager } from '~/stores/interceptor';
