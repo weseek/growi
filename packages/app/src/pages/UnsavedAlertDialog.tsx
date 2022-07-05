9eb970965c850fa2cd2cc21c670b54a3b0fe962d
kaori <kaori@weseek.co.jp>
2022-07-05 11:46:13 +0000

    import useTranslation from next-i18next

--- packages/app/src/pages/UnsavedAlertDialog.tsx
+++ packages/app/src/pages/UnsavedAlertDialog.tsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect } from 'react';
 
 import { useRouter } from 'next/router';
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'next-i18next';
 
 import { useIsEnabledUnsavedWarning } from '~/stores/editor';
 
