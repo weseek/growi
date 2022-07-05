5baf3da370c23e8fe6dcaa833aada75faa5bdac1
mudana <mudana@gruneasia.info>
2022-07-05 18:42:47 +0800

    switch sidebar size
    https://youtrack.weseek.co.jp/issue/GW-7735
    - Add switch page sidebar size
    - Add open first page from recent changes page list

--- packages/app/test/cypress/integration/20-basic-features/access-to-side-bar.spec.ts
+++ packages/app/test/cypress/integration/20-basic-features/access-to-side-bar.spec.ts
@@ -20,7 +20,24 @@ context('Access to sidebar', () => {
 
   it('Successully change side bar size of latest changes', () => {
     cy.visit('/');
-    cy.getByTestid('grw-navigation-resize-button').click({force: true});
     cy.getByTestid('grw-sidebar-nav-primary-recent-changes').click();
+    cy.getByTestid('grw-navigation-resize-button').click({force: true});
+    cy.get('#grw-sidebar-contents-wrapper').within(() => {
+      cy.get('#recentChangesResize').click({force: true});
+      cy.screenshot(`${ssPrefix}1-current-sidebar-size`);
+      cy.get('#recentChangesResize').click({force: true});
+      cy.screenshot(`${ssPrefix}2-switch-sidebar-size`);
+    });
   });
+
+  it('Successfully access page from sidebar ', () => {
+    cy.visit('/');
+    cy.getByTestid('grw-sidebar-nav-primary-recent-changes').click();
+    cy.getByTestid('grw-navigation-resize-button').click({force: true});
+    cy.screenshot(`${ssPrefix}1-recent-changes-page-list`);
+    cy.get('.list-group-item').eq(0).within(() => {
+      cy.get('span.grw-page-path-hierarchical-link').find('a').click();
+    })
+    cy.screenshot(`${ssPrefix}2-open-first-recent-changes-page`);
+  })
 });
