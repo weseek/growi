4872d3d5de20598beecbae44884152da5cc87cfb
Taichi Masuyama <montanha.masu536@gmail.com>
2022-07-08 06:13:52 +0000

    Omit appContainer arg from PukiwikiLikeLinker

--- packages/plugin-pukiwiki-like-linker/src/client-entry.js
+++ packages/plugin-pukiwiki-like-linker/src/client-entry.js
@@ -1,6 +1,6 @@
 import PukiwikiLikeLinker from './resource/js/util/PreProcessor/PukiwikiLikeLinker';
 
-export default (appContainer) => {
+export default () => {
   // add preprocessor to head of array
   window.growiRenderer.preProcessors.unshift(new PukiwikiLikeLinker());
 };
