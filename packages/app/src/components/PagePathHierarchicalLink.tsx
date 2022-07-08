8a2afdfce4f3ff1f6db70bb595a3f52826796267
Yuki Takei <yuki@weseek.co.jp>
2022-07-08 17:47:51 +0000

    remove unnecessary line

--- packages/app/src/components/PagePathHierarchicalLink.tsx
+++ packages/app/src/components/PagePathHierarchicalLink.tsx
@@ -1,7 +1,6 @@
 import React, { memo } from 'react';
 
 import Link from 'next/link';
-import PropTypes from 'prop-types';
 import urljoin from 'url-join';
 
 import LinkedPagePath from '../models/linked-page-path';
