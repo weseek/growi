acdd71aad6018ec9b8d22e2228a512e4c4e54a4f
yohei0125 <yohei0125@weseek.co.jp>
2022-07-11 03:56:32 +0000

    rm useEffect to enable transition

--- packages/app/src/components/Sidebar.tsx
+++ packages/app/src/components/Sidebar.tsx
@@ -96,8 +96,6 @@ const Sidebar = (): JSX.Element => {
 
   const { scheduleToPut } = useUserUISettings();
 
-  const [isTransitionEnabled, setTransitionEnabled] = useState(false);
-
   const [isHover, setHover] = useState(false);
   const [isHoverOnResizableContainer, setHoverOnResizableContainer] = useState(false);
   const [isDragging, setDrag] = useState(false);
@@ -242,12 +240,6 @@ const Sidebar = (): JSX.Element => {
   }, [dragableAreaMouseUpHandler, draggableAreaMoveHandler, isResizableByDrag]);
 
   useEffect(() => {
-    setTimeout(() => {
-      setTransitionEnabled(true);
-    }, 1000);
-  }, []);
-
-  useEffect(() => {
     toggleDrawerMode(isDrawerMode);
   }, [isDrawerMode, toggleDrawerMode]);
 
@@ -300,7 +292,7 @@ const Sidebar = (): JSX.Element => {
         <div className={`d-print-none ${isDrawerMode ? 'grw-sidebar-drawer' : 'grw-sidebar-dock'} ${isDrawerOpened ? 'open' : ''}`}>
           <div className="data-layout-container">
             <div
-              className={`navigation ${isTransitionEnabled ? 'transition-enabled' : ''}`}
+              className='navigation transition-enabled'
               onMouseEnter={hoverOnHandler}
               onMouseLeave={hoverOutHandler}
             >
