a49f4d02aab2a362c5f55aa63bb1a3c2101662b7
mudana <mudana@gruneasia.info>
2022-07-15 18:13:20 +0800

    Show bookmarks list on the sidebar
    https://youtrack.weseek.co.jp/issue/GW-7832
    - Add offset option to user's bookmarks route
    - Implement SWRInfinite to fetch user's bookmarks list
    - Modify useCurrentUser return type to IUserHasId
    - Implement Infinite scroll to bookmarks list on side bar
    - Add styling for bookmark list

--- packages/app/src/components/Sidebar/Bookmarks.tsx
+++ packages/app/src/components/Sidebar/Bookmarks.tsx
@@ -1,16 +1,68 @@
-import React, { FC } from 'react';
 
+import React from 'react';
+
+import { DevidedPagePath } from '@growi/core';
+import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 
-const Bookmarks: FC = () => {
-  const { t } = useTranslation('');
+import LinkedPagePath from '~/models/linked-page-path';
+import { useSWRInifiniteBookmarkedPage } from '~/stores/bookmark';
+import { useCurrentUser } from '~/stores/context';
 
-  // TODO Add bookmarks item
+import PagePathHierarchicalLink from '../PagePathHierarchicalLink';
+
+import InfiniteScroll from './InfiniteScroll';
+
+
+const BookmarksItem = ({ data }) => {
+  const { page } = data;
+  const dPagePath = new DevidedPagePath(page.path, false, true);
+  const linkedPagePathLatter = new LinkedPagePath(dPagePath.latter);
 
   return (
-    <div className="grw-sidebar-content-header p-3">
-      <h3 className="mb-0">{t('Bookmarks')}</h3>
-    </div>
+    <li className="list-group-item py-3 px-0">
+      <div className="d-flex w-100">
+        <h5 className="my-0">
+          <PagePathHierarchicalLink linkedPagePath={linkedPagePathLatter} basePath={dPagePath.isRoot ? undefined : dPagePath.former} />
+        </h5>
+      </div>
+    </li>
+  );
+
+};
+
+BookmarksItem.propTypes = {
+  data: PropTypes.any,
+};
+
+const Bookmarks = () : JSX.Element => {
+  const { t } = useTranslation('');
+  const { data: currentUser } = useCurrentUser();
+  const swr = useSWRInifiniteBookmarkedPage(currentUser?._id);
+  const isEmpty = swr.data?.[0].docs.length === 0;
+  const isReachingEnd = isEmpty || (swr.data && swr.data[0].nextPage == null);
+
+  return (
+    <>
+      <div className="grw-sidebar-content-header p-3">
+        <h3 className="mb-0">{t('Bookmarks')}</h3>
+      </div>
+      <div className="grw-bookmarks-list p-3">
+        {isEmpty ? t('No bookmarks yet') : (
+          <ul className="list-group list-group-flush">
+            <InfiniteScroll
+              swrInifiniteResponse={swr}
+              isReachingEnd={isReachingEnd}
+            >
+              {paginationResult => paginationResult?.docs.map(data => (
+                <BookmarksItem key={data._id} data={data} />
+              ))
+              }
+            </InfiniteScroll>
+          </ul>
+        )}
+      </div>
+    </>
   );
 
 };
--- packages/app/src/stores/bookmark.ts
+++ packages/app/src/stores/bookmark.ts
@@ -1,5 +1,6 @@
 import { SWRResponse } from 'swr';
 import useSWRImmutable from 'swr/immutable';
+import useSWRInfinite, { SWRInfiniteResponse } from 'swr/infinite';
 
 import { apiv3Get } from '../client/util/apiv3-client';
 import { IBookmarkInfo } from '../interfaces/bookmark-info';
@@ -15,5 +16,19 @@ export const useSWRBookmarkInfo = (pageId: string | null | undefined): SWRRespon
         bookmarkedUsers: response.data.bookmarkedUsers,
       };
     }),
+  );
+};
+
+export const useSWRInifiniteBookmarkedPage = (userId: string | null | undefined) : SWRInfiniteResponse => {
+  const getKey = (page: number) => {
+    return userId != null ? `/bookmarks/${userId}/?page=${page + 1}` : null;
+  };
+  return useSWRInfinite(
+    getKey,
+    (endpoint: string) => apiv3Get<{ paginationResult }>(endpoint).then(response => response.data?.paginationResult),
+    {
+      revalidateFirstPage: false,
+      revalidateAll: false,
+    },
   );
 };
