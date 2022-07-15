e84dc97e0d3a5d86b706d27d94e31e9816bb65ee
yohei0125 <yohei0125@weseek.co.jp>
2022-07-15 11:10:39 +0000

    fix SWRResponseWithUtils

--- packages/core/src/utils/with-utils.ts
+++ packages/core/src/utils/with-utils.ts
@@ -1,6 +1,6 @@
 import { SWRResponse } from 'swr';
 
-export type SWRResponseWithUtils<R extends SWRResponse, Utils> = R & Utils;
+export type SWRResponseWithUtils<R extends SWRResponse, U> = R & U;
 
 export const withUtils = <R extends SWRResponse, U>(response: R, utils: U): SWRResponseWithUtils<R, U> => {
   return Object.assign(response, utils);
