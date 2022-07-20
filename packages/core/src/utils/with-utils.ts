d0d03c0a5bcebe750c7fbe585be6ab81e1a9767e
yohei0125 <yohei0125@weseek.co.jp>
2022-07-20 05:03:51 +0000

    export type SWRResponseWithUtils

--- packages/core/src/utils/with-utils.ts
+++ packages/core/src/utils/with-utils.ts
@@ -1,6 +1,6 @@
 import { SWRResponse } from 'swr';
 
-type SWRResponseWithUtils<U, D = any, E = any> = SWRResponse<D, E> & U;
+export type SWRResponseWithUtils<U, D = any, E = any> = SWRResponse<D, E> & U;
 
 export const withUtils = <U, D = any, E = any>(response: SWRResponse<D, E>, utils: U): SWRResponseWithUtils<U, D, E> => {
   return Object.assign(response, utils);
