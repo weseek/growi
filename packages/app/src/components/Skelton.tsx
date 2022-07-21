8b24ab5382ca198642763358646c0c65d921abfd
yuken <yuken-t@weseek.co.jp>
2022-07-21 03:05:16 +0000

    add rounded-pill props to Skelton

--- packages/app/src/components/Skelton.tsx
+++ packages/app/src/components/Skelton.tsx
@@ -5,16 +5,19 @@ import styles from './Skelton.module.scss';
 type SkeltonProps = {
   width?: number,
   height?: number,
-  additionalClass?: string
+  additionalClass?: string,
+  roundedPill?: boolean,
 }
 
 export const Skelton = (props: SkeltonProps): JSX.Element => {
-  const { width, height, additionalClass } = props;
+  const {
+    width, height, additionalClass, roundedPill,
+  } = props;
 
   const style = {
     width,
     height,
   };
 
-  return <div style={style} className={`${styles['grw-skelton']} ${additionalClass}`}></div>;
+  return <div style={style} className={`${styles['grw-skelton']} ${additionalClass} ${roundedPill ? 'rounded-pill' : ''}`}></div>;
 };
