7ac314db4dbc182618b929022942cef48b206e42
Shun Miyazawa <m0zurillex@gmail.com>
2022-07-25 04:31:40 +0000

    Show icon

--- packages/app/src/components/Admin/AuditLog/ActivityTable.tsx
+++ packages/app/src/components/Admin/AuditLog/ActivityTable.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 
+import { UserPicture } from '@growi/ui';
 import { format } from 'date-fns';
 import { useTranslation } from 'react-i18next';
 
@@ -21,6 +22,7 @@ export const ActivityTable : FC<Props> = (props: Props) => {
       <table className="table table-default table-bordered table-user-list">
         <thead>
           <tr>
+            <th scope="col"></th>
             <th scope="col">{t('admin:audit_log_management.username')}</th>
             <th scope="col">{t('admin:audit_log_management.date')}</th>
             <th scope="col">{t('admin:audit_log_management.action')}</th>
@@ -32,6 +34,9 @@ export const ActivityTable : FC<Props> = (props: Props) => {
           {props.activityList.map((activity) => {
             return (
               <tr data-testid="activity-table" key={activity._id}>
+                <td>
+                  <UserPicture user={activity.user} className="picture rounded-circle" />
+                </td>
                 <td>{activity.snapshot?.username}</td>
                 <td>{formatDate(activity.createdAt)}</td>
                 <td>{t(`admin:audit_log_action.${activity.action}`)}</td>
