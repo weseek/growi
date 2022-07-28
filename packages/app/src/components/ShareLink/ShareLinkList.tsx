086a7bfda24b265be7e26d580720aa4d1214a160
keigo-h <keigo-h@weseek.co.jp>
2022-07-28 09:36:48 +0000

    fix key error

--- packages/app/src/components/ShareLink/ShareLinkList.tsx
+++ packages/app/src/components/ShareLink/ShareLinkList.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import dateFnsFormat from 'date-fns/format';
 import { useTranslation } from 'react-i18next';
 
+import ShareLink from '../Icons/ShareLinkIcon';
 import CopyDropdown from '../Page/CopyDropdown';
 
 
@@ -76,6 +77,7 @@ const ShareLinkList = (props: Props): JSX.Element => {
       <>
         {props.shareLinks.map(shareLink => (
           <ShareLinkTr
+            key={shareLink}
             isAdmin={props.isAdmin}
             shareLink={shareLink}
             onDelete={() => {
