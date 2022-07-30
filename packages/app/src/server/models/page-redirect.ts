f586e068f89d5a0723eb1d30033b8fe14a82593f
Yuki Takei <yuki@weseek.co.jp>
2022-07-30 20:07:11 +0000

    re-impl retrievePageRedirectEndpoints with $graphLookup

--- packages/app/src/server/models/page-redirect.ts
+++ packages/app/src/server/models/page-redirect.ts
@@ -4,22 +4,32 @@ import {
   Schema, Model, Document,
 } from 'mongoose';
 
+import loggerFactory from '~/utils/logger';
+
 import { getOrCreateModel } from '../util/mongoose-utils';
 
-export interface IPageRedirectChains {
-  start: IPageRedirect,
-  end: IPageRedirect,
-}
 
-export interface IPageRedirect {
+const logger = loggerFactory('growi:models:page-redirects');
+
+
+export type IPageRedirect = {
   fromPath: string,
   toPath: string,
 }
 
+const CHAINS_ATTRIBUTE_NAME = 'chains';
+type IPageRedirectChains = IPageRedirect & { [CHAINS_ATTRIBUTE_NAME]: IPageRedirect[] };
+
+export type IPageRedirectEndpoints = {
+  start: IPageRedirect,
+  end: IPageRedirect,
+}
+
+
 export interface PageRedirectDocument extends IPageRedirect, Document {}
 
 export interface PageRedirectModel extends Model<PageRedirectDocument> {
-  retrievePageRedirectChains(fromPath: string, storedChains?: IPageRedirectChains): Promise<IPageRedirectChains>
+  retrievePageRedirectEndpoints(fromPath: string): Promise<IPageRedirectEndpoints>
   removePageRedirectByToPath(toPath: string): Promise<void>
 }
 
@@ -30,18 +40,36 @@ const schema = new Schema<PageRedirectDocument, PageRedirectModel>({
   toPath: { type: String, required: true },
 });
 
-schema.statics.retrievePageRedirectChains = async function(fromPath: string, storedChains?: IPageRedirectChains): Promise<IPageRedirectChains|null> {
-  const chainedRedirect = await this.findOne({ fromPath });
+schema.statics.retrievePageRedirectEndpoints = async function(fromPath: string): Promise<IPageRedirectEndpoints|null> {
+  const chainedRedirect: IPageRedirectChains[] = await this.aggregate([
+    { $match: { fromPath } },
+    {
+      $graphLookup: {
+        from: 'pageredirects',
+        startWith: '$toPath',
+        connectFromField: 'toPath',
+        connectToField: 'fromPath',
+        as: CHAINS_ATTRIBUTE_NAME,
+      },
+    },
+  ]);
 
-  if (chainedRedirect == null) {
-    return storedChains ?? null;
+  if (chainedRedirect.length === 0) {
+    return null;
   }
 
-  const chains = storedChains ?? { start: chainedRedirect, end: chainedRedirect };
-  chains.end = chainedRedirect;
+  if (chainedRedirect.length > 1) {
+    logger.warn(`Although two or more PageRedirect documents starts from '${fromPath}' exists, The first one is used.`);
+  }
 
-  // find the end recursively
-  return this.retrievePageRedirectChains(chainedRedirect.toPath, chains);
+  const chains = chainedRedirect[0];
+  const start = { fromPath: chains.fromPath, toPath: chains.toPath };
+  const chainsNum = chains[CHAINS_ATTRIBUTE_NAME].length;
+  const end = chainsNum === 0
+    ? start
+    : chains[CHAINS_ATTRIBUTE_NAME][chainsNum - 1];
+
+  return { start, end };
 };
 
 schema.statics.removePageRedirectByToPath = async function(toPath: string): Promise<void> {
