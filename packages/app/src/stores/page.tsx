a4aaefccab44dbc01886ecf918cc504407717677
yuken <yuken-t@weseek.co.jp>
2022-08-02 08:03:03 +0000

    add status to error and handling in useSWRxPage

--- packages/app/src/stores/page.tsx
+++ packages/app/src/stores/page.tsx
@@ -14,10 +14,16 @@ import { IPageTagsInfo } from '../interfaces/tag';
 
 import { useCurrentPageId } from './context';
 
-export const useSWRxPage = (pageId?: string|null, shareLinkId?: string): SWRResponse<IPagePopulatedToShowRevision, Error> => {
-  return useSWR<IPagePopulatedToShowRevision, Error>(
+export const useSWRxPage = (pageId?: string|null, shareLinkId?: string): SWRResponse<IPagePopulatedToShowRevision, any> => {
+  return useSWR<IPagePopulatedToShowRevision, any>(
     pageId != null ? ['/page', pageId, shareLinkId] : null,
     (endpoint, pageId, shareLinkId) => apiv3Get<{ page: IPagePopulatedToShowRevision }>(endpoint, { pageId, shareLinkId }).then(result => result.data.page),
+    {
+      onErrorRetry: (error) => {
+        // for empty page
+        if (error[0].status === 404) return;
+      },
+    },
   );
 };
 
