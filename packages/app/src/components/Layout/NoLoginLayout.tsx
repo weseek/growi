a8e72c23232535661d9125df9127d6350dbd7a2c
Shunm634-source <anakin.yadon@gmail.com>
2022-08-04 06:37:38 +0000

    Deleted the redundant code

--- packages/app/src/components/Layout/NoLoginLayout.tsx
+++ packages/app/src/components/Layout/NoLoginLayout.tsx
@@ -5,6 +5,8 @@ import Head from 'next/head';
 import { useGrowiTheme } from '~/stores/context';
 import { useNextThemes } from '~/stores/use-next-themes';
 
+import GrowiLogo from '../Icons/GrowiLogo';
+
 import { RawLayout } from './RawLayout';
 
 import styles from './Installer.module.scss';
@@ -33,7 +35,7 @@ export const NoLoginLayout = ({
 
                 <div className="col-md-12">
                   <div className="login-header mx-auto">
-                    <div className="logo"></div>
+                    <GrowiLogo />
                     <h1 className="my-3">GROWI</h1>
                     <div className="login-form-errors px-3"></div>
                   </div>
