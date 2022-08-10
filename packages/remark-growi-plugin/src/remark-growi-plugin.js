4e58b2967c472266eabdb3e0d3363451fc88d8de
Yuki Takei <yuki@weseek.co.jp>
2022-08-10 13:34:11 +0000

    format test file

--- packages/remark-growi-plugin/src/remark-growi-plugin.js
+++ packages/remark-growi-plugin/src/remark-growi-plugin.js
@@ -1,24 +1,35 @@
-import { Plugin } from 'unified';
+/**
+ * @typedef {import('mdast').Root} Root
+ *
+ * @typedef {import('mdast-util-directive')} DoNotTouchAsThisImportIncludesDirectivesInTree
+ */
 
 import { directiveFromMarkdown, directiveToMarkdown } from './mdast-util-growi-plugin/index.js';
 import { directive } from './micromark-extension-growi-plugin/dev/index.js';
 
-export const remarkGrowiPlugin: Plugin = function() {
+/**
+    * Plugin to support GROWI plugin (`$lsx(/path, depth=2)`).
+    *
+    * @type {import('unified').Plugin<void[], Root>}
+    */
+export function remarkGrowiPlugin() {
   const data = this.data();
-
-  function add(field: string, value) {
-    if (data[field] != null) {
-      const array = data[field];
-      if (Array.isArray(array)) {
-        array.push(value);
-      }
-    }
-    else {
-      data[field] = [value];
-    }
-  }
 
   add('micromarkExtensions', directive());
   add('fromMarkdownExtensions', directiveFromMarkdown);
   add('toMarkdownExtensions', directiveToMarkdown);
-};
+
+  /**
+      * @param {string} field
+      * @param {unknown} value
+      */
+  function add(field, value) {
+    const list = /** @type {unknown[]} */ (
+      // Other extensions
+      /* c8 ignore next 2 */
+      data[field] ? data[field] : (data[field] = [])
+    );
+
+    list.push(value);
+  }
+}
