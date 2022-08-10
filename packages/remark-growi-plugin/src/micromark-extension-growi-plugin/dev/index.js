54bf99418d33c9577a263ee689eb7e9c599f177f
Yuki Takei <yuki@weseek.co.jp>
2022-08-10 12:15:02 +0000

    remove unnecessary files

--- packages/remark-growi-plugin/src/micromark-extension-growi-plugin/dev/index.js
+++ packages/remark-growi-plugin/src/micromark-extension-growi-plugin/dev/index.js
@@ -1,7 +1,1 @@
-/**
- * @typedef {import('./lib/html.js').Handle} Handle
- * @typedef {import('./lib/html.js').HtmlOptions} HtmlOptions
- */
-
-export {directive} from './lib/syntax.js'
-export {directiveHtml} from './lib/html.js'
+export { directive } from './lib/syntax.js';
