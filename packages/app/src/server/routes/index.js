40c4f2270a150fb74e9a42319e7a40d29869e1d9
yuken <yuken-t@weseek.co.jp>
2022-08-19 03:01:52 +0000

    refactor express routing

--- packages/app/src/server/routes/index.js
+++ packages/app/src/server/routes/index.js
@@ -239,8 +239,7 @@ module.exports = function(crowi, app) {
     .get('/:token', injectResetOrderByTokenMiddleware, forgotPassword.resetPassword)
     .use(forgotPassword.handleErrosMiddleware));
 
-  app.use('/_private-legacy-pages', express.Router()
-    .get('/', injectUserUISettings, next.delegateToNext));
+  app.get('/_private-legacy-pages', next.delegateToNext);
   app.use('/user-activation', express.Router()
     .get('/:token', applicationInstalled, injectUserRegistrationOrderByTokenMiddleware, userActivation.form)
     .use(userActivation.tokenErrorHandlerMiddeware));
