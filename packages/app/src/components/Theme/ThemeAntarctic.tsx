7d5a77a4c82fb1db7953a9a8a6b33e9a83547baa
yohei0125 <yohei0125@weseek.co.jp>
2022-08-25 04:52:20 +0000

    refactor themeAntarctic

--- packages/app/src/components/Theme/ThemeAntarctic.tsx
+++ packages/app/src/components/Theme/ThemeAntarctic.tsx
@@ -22,13 +22,14 @@ const ThemeAntarctic = ({ children, colorScheme }: Props): JSX.Element => {
   const newChildren = (
     <>
       {colorScheme != null && (
-        <div className={'grw-bg-image-wrapper'}>
-          <Image className='grw-bg-image' alt='background image' src={getBackgroundImageSrc(colorScheme)} layout='fill' quality="100" />
+        <div className='grw-bg-image-wrapper'>
+          <Image className='grw-bg-image' alt='background-image' src={getBackgroundImageSrc(colorScheme)} layout='fill' quality="100" />
         </div>
       )}
       {children}
     </>
   );
-  return <ThemeInjector className={`${styles.theme}`}>{newChildren}</ThemeInjector>;
+  return <ThemeInjector className={styles.theme}>{newChildren}</ThemeInjector>;
+
 };
 export default ThemeAntarctic;
