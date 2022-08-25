6ae4e6d45fb488c7060d27f31ae9964d64905c0f
yohei0125 <yohei0125@weseek.co.jp>
2022-08-25 04:39:29 +0000

    refactor themeInjector

--- packages/app/src/components/Theme/utils/ThemeInjector.tsx
+++ packages/app/src/components/Theme/utils/ThemeInjector.tsx
@@ -8,6 +8,5 @@ type Props = {
 
 export const ThemeInjector = ({ children, className: themeClassName }: Props): JSX.Element => {
   const className = `${children.props.className ?? ''} ${themeClassName}`;
-  const newChildren = React.cloneElement(children, { className });
-  return <div className={`${newChildren.props.className}`}>{newChildren}</div>;
+  return React.cloneElement(<div>{children}</div>, { className });
 };
