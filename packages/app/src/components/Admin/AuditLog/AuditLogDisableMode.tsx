a00cf0d6d4dd87a98d753342f5ee1039e7f80bdb
kaori <kaori@weseek.co.jp>
2022-08-29 07:57:55 +0000

    fiix auditlog i18n

--- packages/app/src/components/Admin/AuditLog/AuditLogDisableMode.tsx
+++ packages/app/src/components/Admin/AuditLog/AuditLogDisableMode.tsx
@@ -3,7 +3,7 @@ import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
 export const AuditLogDisableMode: FC = () => {
-  const { t } = useTranslation();
+  const { t } = useTranslation('admin');
 
   return (
     <div id="content-main" className="content-main container-lg">
@@ -15,7 +15,7 @@ export const AuditLogDisableMode: FC = () => {
               <h1 className="text-center">{t('audit_log_management.audit_log')}</h1>
               <h3
                 // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{ __html: t('admin:audit_log_management.disable_mode_explain') }}
+                dangerouslySetInnerHTML={{ __html: t('audit_log_management.disable_mode_explain') }}
               />
             </div>
           </div>
