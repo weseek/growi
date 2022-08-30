2f81eb5837dc4dc81b58e2e0ce81faaf57ce1ec5
jam411 <ryoji-s@weseek.co.jp>
2022-08-30 08:03:11 +0000

    update BookmarkListPage

--- packages/app/src/interfaces/bookmark-info.ts
+++ packages/app/src/interfaces/bookmark-info.ts
@@ -1,5 +1,8 @@
-import { IPageHasId } from '~/interfaces/page';
+import { Ref } from '@growi/core';
+
 import { IUser } from '~/interfaces/user';
+
+import { IPage } from './page';
 
 export type IBookmarkInfo = {
   sumOfBookmarks: number;
@@ -9,7 +12,7 @@ export type IBookmarkInfo = {
 
 export type BookmarkListPage = {
   _id: string,
-  page: IPageHasId,
-  user: string,
+  page: Populated<IPage>,
+  user: Ref<IUser>,
   createdAt: Date,
 }
