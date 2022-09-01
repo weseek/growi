8def9cb7b1c59ac9dfe69a144a3e8f2ff61f6c24
Shunm634-source <anakin.yadon@gmail.com>
2022-09-01 09:15:17 +0000

    modified

--- packages/app/src/pages/admin/[[...path]].page.tsx
+++ packages/app/src/pages/admin/[[...path]].page.tsx
@@ -143,7 +143,7 @@ const AdminMarkdownSettingsPage: NextPage<Props> = (props: Props) => {
     'global-notification': {
       new: {
         title: t('external_notification.external_notification'),
-        component: <ManageGlobalNotification isMailerSetup={props.isMailerSetup} />,
+        component: <ManageGlobalNotification />,
       },
     },
     'slack-integration': {
