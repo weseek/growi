93f77f0dd5bf7691933442721082bb1d341fa606
Taichi Masuyama <montanha.masu536@gmail.com>
2022-09-04 10:49:31 +0000

    Fixed style

--- packages/app/src/components/Common/ClosableTextInput.tsx
+++ packages/app/src/components/Common/ClosableTextInput.tsx
@@ -106,7 +106,7 @@ const ClosableTextInput: FC<ClosableTextInputProps> = memo((props: ClosableTextI
 
 
   return (
-    <div className="d-block flex-fill">
+    <div>
       <input
         value={inputText || ''}
         ref={inputRef}
