7456e642d1a873e97bc0aba6bcf0d788b20c1f50
Shun Miyazawa <m0zurillex@gmail.com>
2022-09-09 03:39:56 +0000

    cleanup

--- packages/app/src/stores/context.tsx
+++ packages/app/src/stores/context.tsx
@@ -85,10 +85,6 @@ export const useIsUserPage = (initialData?: boolean): SWRResponse<boolean, Error
   return useStaticSWR<boolean, Error>('isUserPage', initialData, { fallbackData: false });
 };
 
-// export const useIsTrashPage = (initialData?: boolean): SWRResponse<boolean, Error> => {
-//   return useStaticSWR<boolean, Error>('isTrashPage', initialData, { fallbackData: false });
-// };
-
 export const useIsNotCreatable = (initialData?: boolean): SWRResponse<boolean, Error> => {
   return useStaticSWR<boolean, Error>('isNotCreatable', initialData, { fallbackData: false });
 };
