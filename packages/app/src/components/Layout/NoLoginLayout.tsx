f855b69a7a7c33e136873b5d4715485c6f27f977
yohei0125 <yohei0125@weseek.co.jp>
2022-09-12 04:56:26 +0000

    render register fail alert

--- packages/app/src/components/Layout/NoLoginLayout.tsx
+++ packages/app/src/components/Layout/NoLoginLayout.tsx
@@ -1,5 +1,8 @@
 import React, { ReactNode } from 'react';
 
+import { isServer } from '@growi/core';
+import { useTranslation } from 'next-i18next';
+
 import GrowiLogo from '../Icons/GrowiLogo';
 
 import { RawLayout } from './RawLayout';
@@ -12,13 +15,24 @@ type Props = {
   children?: ReactNode,
 }
 
-export const NoLoginLayout = ({
-  children, title, className,
-}: Props): JSX.Element => {
+export const NoLoginLayout = ({ children, title, className }: Props): JSX.Element => {
+
+  const { t } = useTranslation();
+
+  if (isServer()) {
+    return <></>;
+  }
   const classNames: string[] = ['wrapper'];
   if (className != null) {
     classNames.push(className);
   }
+
+  // errors?='aaa,bbb' => ['aaa', 'bbb']
+  const { search } = window.location;
+  const params = new URLSearchParams(search);
+  const errorString = params.get('errors');
+  const errors = errorString?.split(',');
+
   return (
     <RawLayout title={title} className={`${commonStyles.nologin}`}>
       <div className="nologin">
@@ -33,6 +47,16 @@ export const NoLoginLayout = ({
                     <GrowiLogo />
                     <h1 className="my-3">GROWI</h1>
                     <div className="noLogin-form-errors px-3"></div>
+                    { errors != null && errors.length > 0 && (
+                      <div className="register-form-errors px-3">
+                        <div className="alert alert-danger">
+                          {errors.map((err) => {
+                            const i18nKey = `message.${err}`;
+                            return <>{t(i18nKey)}<br/></>;
+                          })}
+                        </div>
+                      </div>
+                    )}
                   </div>
                 </div>
 
