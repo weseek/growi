66cc001ebf412c45a119796513143d6363dceff1
yohei0125 <yohei0125@weseek.co.jp>
2022-09-12 02:56:40 +0000

    use delegateToNext for GET /register

--- packages/app/src/server/routes/index.js
+++ packages/app/src/server/routes/index.js
@@ -85,7 +85,7 @@ module.exports = function(crowi, app) {
   app.post('/login'                   , applicationInstalled, loginFormValidator.loginRules(), loginFormValidator.loginValidation, csrfProtection,  addActivity, loginPassport.loginWithLocal, loginPassport.loginWithLdap, loginPassport.loginFailure);
 
   app.post('/register'                , applicationInstalled, registerFormValidator.registerRules(), registerFormValidator.registerValidation, csrfProtection, addActivity, login.register);
-  app.get('/register'                 , applicationInstalled, login.preLogin, login.register);
+  app.get('/register'                 , applicationInstalled, login.preLogin, next.delegateToNext);
 
   app.get('/admin/*'                    , applicationInstalled, loginRequiredStrictly , adminRequired , next.delegateToNext);
   // app.get('/admin'                    , applicationInstalled, loginRequiredStrictly , adminRequired , admin.index);
