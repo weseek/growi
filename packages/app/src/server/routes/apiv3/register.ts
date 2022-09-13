3271a80734102bd4451ae8058204e723b06a2b23
yohei0125 <yohei0125@weseek.co.jp>
2022-09-13 10:48:28 +0000

    use apiv3 for success response and apiv3Err for error response

--- packages/app/src/server/routes/apiv3/register.ts
+++ packages/app/src/server/routes/apiv3/register.ts
@@ -35,7 +35,7 @@ module.exports = (crowi) => {
       const parameters = { action: SupportedAction.ACTION_USER_REGISTRATION_SUCCESS };
       activityEvent.emit('update', res.locals.activity._id, parameters);
 
-      return res.safeRedirect(redirectTo);
+      return res.apiv3({ redirectTo });
     });
   };
 
@@ -65,64 +65,66 @@ module.exports = (crowi) => {
       .forEach(result => logger.error(result.reason));
   }
   // eslint-disable-next-line max-len
-  router.post('/', async(req: any, res) => {
+  router.post('/', async(req: any, res: any) => {
     if (req.user != null) {
-      return res.redirect('/');
+      return res.apiv3Err('user_already_logged_in', 403);
     }
 
     // config で closed ならさよなら
     if (configManager.getConfig('crowi', 'security:registrationMode') === aclService.labels.SECURITY_REGISTRATION_MODE_CLOSED) {
-      return res.redirect('/');
+      return res.apiv3Err('registration_closed', 403);
     }
 
-    if (req.form.isValid) {
-      const registerForm = req.form.registerForm || {};
+    if (!req.form.isValid) {
+      const errors = req.form.errors;
+      return res.apiv3Err(errors, 401);
+    }
+    const registerForm = req.form.registerForm || {};
 
-      const name = registerForm.name;
-      const username = registerForm.username;
-      const email = registerForm.email;
-      const password = registerForm.password;
+    const name = registerForm.name;
+    const username = registerForm.username;
+    const email = registerForm.email;
+    const password = registerForm.password;
 
-      // email と username の unique チェックする
-      User.isRegisterable(email, username, (isRegisterable, errOn) => {
-        const errors: string[] = [];
-        if (!User.isEmailValid(email)) {
-          errors.push('email_address_could_not_be_used');
+    // email と username の unique チェックする
+    User.isRegisterable(email, username, (isRegisterable, errOn) => {
+      const errors: string[] = [];
+      if (!User.isEmailValid(email)) {
+        errors.push('email_address_could_not_be_used');
+      }
+      if (!isRegisterable) {
+        if (!errOn.username) {
+          errors.push('user_id_is_not_available');
         }
-        if (!isRegisterable) {
-          if (!errOn.username) {
-            errors.push('user_id_is_not_available');
-          }
-          if (!errOn.email) {
-            errors.push('email_address_is_already_registered');
-          }
+        if (!errOn.email) {
+          errors.push('email_address_is_already_registered');
         }
-        if (errors.length > 0) {
-          debug('isError user register error', errOn);
-          return res.send({ errors: ['email_address_could_not_be_used', 'user_id_is_not_available', 'email_address_is_already_registered'] });
+      }
+      if (errors.length > 0) {
+        debug('isError user register error', errOn);
+        return res.apiv3Err(errors, 400);
+      }
+
+      User.createUserByEmailAndPassword(name, username, email, password, undefined, async(err, userData) => {
+        if (err) {
+          const errors: string[] = [];
+          if (err.name === 'UserUpperLimitException') {
+            errors.push('can_not_register_maximum_number_of_users');
+          }
+          else {
+            errors.push('failed_to_register');
+          }
+          return res.redirect(`/register?errors=${errors}`);
         }
 
-        User.createUserByEmailAndPassword(name, username, email, password, undefined, async(err, userData) => {
-          if (err) {
-            const errors: string[] = [];
-            if (err.name === 'UserUpperLimitException') {
-              errors.push('can_not_register_maximum_number_of_users');
-            }
-            else {
-              errors.push('failed_to_register');
-            }
-            return res.redirect(`/register?errors=${errors}`);
-          }
+        if (configManager.getConfig('crowi', 'security:registrationMode') !== aclService.labels.SECURITY_REGISTRATION_MODE_RESTRICTED) {
+          // send mail asynchronous
+          sendEmailToAllAdmins(userData);
+        }
 
-          if (configManager.getConfig('crowi', 'security:registrationMode') !== aclService.labels.SECURITY_REGISTRATION_MODE_RESTRICTED) {
-            // send mail asynchronous
-            sendEmailToAllAdmins(userData);
-          }
-
-          return registerSuccessHandler(req, res, userData);
-        });
+        return registerSuccessHandler(req, res, userData);
       });
-    }
+    });
   });
 
   return router;
