71052e1cffffb3c4330d62b0dcf7b85df24c49cc
Yuken Tezuka <yuken-t@weseek.co.jp>
2022-09-16 07:22:16 +0000

    add data-testid to revision renderer

--- packages/app/src/components/Page/RevisionBody.jsx
+++ packages/app/src/components/Page/RevisionBody.jsx
@@ -62,6 +62,7 @@ export default class RevisionBody extends React.PureComponent {
           this.element = elem;
         }}
         id="wiki"
+        data-testid="wiki"
         className={`wiki ${additionalClassName}`}
         // eslint-disable-next-line react/no-danger
         dangerouslySetInnerHTML={this.generateInnerHtml(this.props.html)}
