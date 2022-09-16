49f8d83be0344e8bb9f7df7dc62a74f98a017713
jam411 <ryoji-s@weseek.co.jp>
2022-09-16 10:25:40 +0000

    update access-to-page.spec.ts

--- packages/app/test/cypress/integration/21-basic-features-for-guest/access-to-page.spec.ts
+++ packages/app/test/cypress/integration/21-basic-features-for-guest/access-to-page.spec.ts
@@ -24,6 +24,8 @@ context('Access to page by guest', () => {
     cy.visit('/Sandbox/Math');
 
     cy.get('mjx-container').should('be.visible');
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(2000); // wait for 2 seconds for MathJax.typesetPromise();
 
     cy.screenshot(`${ssPrefix}-sandbox-math`);
   });
