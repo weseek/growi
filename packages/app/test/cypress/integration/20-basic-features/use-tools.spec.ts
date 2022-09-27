58ad9cc89a9b3aa500cb22626cee978d0119548f
kaori <kaori@weseek.co.jp>
2022-09-27 09:31:07 +0000

    capture page-rename-modal elm

--- packages/app/test/cypress/integration/20-basic-features/use-tools.spec.ts
+++ packages/app/test/cypress/integration/20-basic-features/use-tools.spec.ts
@@ -306,12 +306,23 @@ context('Tag Oprations', () =>{
       cy.get('.list-group-item').each(($row) => {
         if($row.find('a').text() === oldPageName){
           cy.wrap($row).within(() => {
+            cy.getByTestid('open-page-item-control-btn').first().click();
             cy.getByTestid('page-item-control-menu').should('have.class', 'show').first().within(() => {
             // eslint-disable-next-line cypress/no-unnecessary-waiting
-            cy.wait(300);
+            cy.wait(1000);
             cy.screenshot(`${ssPrefix}2-open-page-item-control-menu`);
             cy.getByTestid('open-page-move-rename-modal-btn').click({force: true});
             })
+          });
+        }
+      });
+    });
+
+    cy.getByTestid('search-result-list').within(() => {
+      cy.get('.list-group-item').each(($row) => {
+        if($row.find('a').text() === oldPageName){
+          cy.wrap($row).within(() => {
+            cy.getByTestid('open-page-move-rename-modal-btn').click({force: true});
           });
         }
       });
