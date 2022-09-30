e3a5723342e60462de226f8e131a3ef0cd591d42
Yuken Tezuka <yuken-t@weseek.co.jp>
2022-09-30 12:06:28 +0000

    wait for loading

--- packages/app/test/cypress/integration/20-basic-features/access-to-page.spec.ts
+++ packages/app/test/cypress/integration/20-basic-features/access-to-page.spec.ts
@@ -46,6 +46,8 @@ context('Access to page', () => {
 
   it('/user/admin is successfully loaded', () => {
     cy.visit('/user/admin', {  });
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(500); // wait for loading
     cy.screenshot(`${ssPrefix}-user-admin`);
   });
 
