c9f3403b6b5e5e90cf775d209a415540f8f562a7
Taichi Masuyama <montanha.masu536@gmail.com>
2022-10-02 09:00:17 +0000

    Prepare axios client for x-tar

--- packages/app/src/utils/axios.ts
+++ packages/app/src/utils/axios.ts
@@ -32,4 +32,14 @@ customAxios.interceptors.response.use((response) => {
   return response;
 });
 
+export const customAxiosXTar = axios.create({
+  headers: {
+    'Content-Type': 'application/x-tar',
+  },
+});
+customAxiosXTar.interceptors.response.use((response) => {
+  convertDates(response.data);
+  return response;
+});
+
 export default customAxios;
