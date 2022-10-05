22b1cb05738bfe038931050c6d43381f90f9f985
atsuki-t <atsuki-t@weseek.co.jp>
2022-10-05 05:22:18 +0000

    improve g2g data transfer i18n

--- packages/app/src/components/Admin/G2GDataTransfer.tsx
+++ packages/app/src/components/Admin/G2GDataTransfer.tsx
@@ -87,29 +87,29 @@ const G2GDataTransfer = (): JSX.Element => {
 
   return (
     <div data-testid="admin-export-archive-data">
-      <h2 className="border-bottom">{t('g2g_data_transfer.transfer_data_to_another_growi')}</h2>
+      <h2 className="border-bottom">{t('admin:g2g_data_transfer.transfer_data_to_another_growi')}</h2>
 
       <button type="button" className="btn btn-outline-secondary mt-4" disabled={isExporting} onClick={() => setExportModalOpen(true)}>
-        {t('g2g_data_transfer.advanced_options')}
+        {t('admin:g2g_data_transfer.advanced_options')}
       </button>
 
       <form onSubmit={transferData}>
         <div className="form-group row mt-3">
           <div className="col-9">
-            <input className="form-control" type="text" placeholder={t('g2g_data_transfer.paste_transfer_key')} />
+            <input className="form-control" type="text" placeholder={t('admin:g2g_data_transfer.paste_transfer_key')} />
           </div>
           <div className="col-3">
-            <button type="submit" className="btn btn-primary w-100">{t('g2g_data_transfer.start_transfer')}</button>
+            <button type="submit" className="btn btn-primary w-100">{t('admin:g2g_data_transfer.start_transfer')}</button>
           </div>
         </div>
       </form>
 
-      <h2 className="border-bottom mt-5">{t('g2g_data_transfer.transfer_data_to_this_growi')}</h2>
+      <h2 className="border-bottom mt-5">{t('admin:g2g_data_transfer.transfer_data_to_this_growi')}</h2>
 
       <div className="form-group row mt-4">
         <div className="col-md-3">
           <button type="button" className="btn btn-primary w-100" onClick={onClickHandler}>
-            {t('g2g_data_transfer.publish_transfer_key')}
+            {t('admin:g2g_data_transfer.publish_transfer_key')}
           </button>
         </div>
         <div className="col-md-9">
@@ -121,9 +121,9 @@ const G2GDataTransfer = (): JSX.Element => {
       </div>
 
       <div className="alert alert-warning mt-4">
-        <p className="mb-1">{t('g2g_data_transfer.transfer_key_limit')}</p>
-        <p className="mb-1">{t('g2g_data_transfer.once_transfer_key_used')}</p>
-        <p className="mb-0">{t('g2g_data_transfer.transfer_to_growi_cloud')}</p>
+        <p className="mb-1">{t('admin:g2g_data_transfer.transfer_key_limit')}</p>
+        <p className="mb-1">{t('admin:g2g_data_transfer.once_transfer_key_used')}</p>
+        <p className="mb-0">{t('admin:g2g_data_transfer.transfer_to_growi_cloud')}</p>
       </div>
 
       <SelectCollectionsModal
@@ -131,7 +131,6 @@ const G2GDataTransfer = (): JSX.Element => {
         onExportingRequested={exportingRequestedHandler}
         onClose={() => setExportModalOpen(false)}
         collections={collections}
-        allChecked={true}
       />
     </div>
   );
