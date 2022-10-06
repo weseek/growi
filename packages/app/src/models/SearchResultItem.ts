88c4449b78b4d39600505578aebb03febb629795
jam411 <ryoji-s@weseek.co.jp>
2022-10-06 16:27:38 +0000

    update

--- packages/app/src/models/SearchResultItem.ts
+++ packages/app/src/models/SearchResultItem.ts
@@ -1,6 +1,11 @@
 export type SearchResultItem = {
   id: number,
   name: string,
+  owner: {
+    login: string,
+    html_url: string,
+    avatar_url: string,
+  },
   full_name: string,
   html_url: string,
   description: string,
