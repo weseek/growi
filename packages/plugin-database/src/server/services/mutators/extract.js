1a8783bd676f952669feccc77f9d9ebea911c4fa
Ryu Sato <ryu@weseek.co.jp>
2022-10-06 09:07:02 +0000

    implement "extract" mutator

--- packages/plugin-database/src/server/services/mutators/extract.js
+++ packages/plugin-database/src/server/services/mutators/extract.js
@@ -1,6 +1,64 @@
 import MarkdownTable from '../../../../../app/src/client/models/MarkdownTable';
 
-export const extract = (body, direction, index, operation, keyword) => {
-  const hoge = MarkdownTable.fromMarkdownString(body);
-  return hoge;
+/*
+  direction:string 'col' or 'row'
+  index:integer
+  operation:string ex. '=='
+  keyword:string
+*/
+export const extract = (mdTableText, direction, index, operation, keyword) => {
+  const mdTable = MarkdownTable.fromMarkdownString(mdTableText);
+
+  // Adjust index from plugin's to array's and validate value
+  const arrIndex = index - 1;
+  if ((direction === 'col' && arrIndex < 0)
+      || (direction === 'row' && arrIndex < 1)) {
+    throw Error(`Invalid index: ${index}`);
+  }
+
+  let colFilter;
+  const colFilterWrapper = (row, compare) => row.some(col => col.includes('$$calc')) || compare;
+  const colEqualFilter = row => colFilterWrapper(row, row[arrIndex] === keyword);
+  const colLtFilter = row => colFilterWrapper(row, row[arrIndex] < keyword);
+  const colLeFilter = row => colFilterWrapper(row, row[arrIndex] <= keyword);
+  const colGtFilter = row => colFilterWrapper(row, row[arrIndex] > keyword);
+  const colGeFilter = row => colFilterWrapper(row, row[arrIndex] >= keyword);
+  switch (operation) {
+    case '==': colFilter = colEqualFilter; break;
+    case '<': colFilter = colLtFilter; break;
+    case '<=': colFilter = colLeFilter; break;
+    case '>': colFilter = colGtFilter; break;
+    case '>=': colFilter = colGeFilter; break;
+    default:
+      throw Error('Unknown operation');
+  }
+
+  let rowFilter;
+  const rowFilterWrapper = (item, compare) => item.includes('$$calc') || compare;
+  const rowEqualFilter = item => rowFilterWrapper(item, item === keyword);
+  const rowLtFilter = item => rowFilterWrapper(item, item < keyword);
+  const rowLeFilter = item => rowFilterWrapper(item, item <= keyword);
+  const rowGtFilter = item => rowFilterWrapper(item, item > keyword);
+  const rowGeFilter = item => rowFilterWrapper(item, item >= keyword);
+  switch (operation) {
+    case '==': rowFilter = rowEqualFilter; break;
+    case '<': rowFilter = rowLtFilter; break;
+    case '<=': rowFilter = rowLeFilter; break;
+    case '>': rowFilter = rowGtFilter; break;
+    case '>=': rowFilter = rowGeFilter; break;
+    default:
+      throw Error('Unknown operation');
+  }
+
+  const header = mdTable.table[0]; // always leave header
+  let extractedTable;
+  if (direction === 'col') {
+    extractedTable = [header].concat(mdTable.table.filter(colFilter));
+  }
+  if (direction === 'row') {
+    extractedTable = [header].concat(mdTable.table[arrIndex].filter(rowFilter));
+  }
+
+  mdTable.table = extractedTable;
+  return mdTable.toString();
 };
