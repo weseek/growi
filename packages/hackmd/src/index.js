3035d535fd472d8f8a74d779f3e62c37c9be7a0f
Yuken Tezuka <yuken-t@weseek.co.jp>
2022-10-12 09:26:29 +0000

    refactor vite config file

---
+++ packages/hackmd/src/index.js
@@ -0,0 +1,5 @@
+
+
+export * as styles from '../dist/assets/styles_bundle';
+
+export * as agent from '../dist/assets/agent_bundle';
--- packages/hackmd/vite.config.js
+++ packages/hackmd/vite.config.js
@@ -1,8 +1,19 @@
+import { resolve } from 'path';
+
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   build: {
     manifest: true,
+    rollupOptions: {
+      input: {
+        styles: resolve(__dirname, 'src/hackmd-styles.js'),
+        agent: resolve(__dirname, 'src/hackmd-agent.js'),
+      },
+      output: {
+        entryFileNames: 'assets/[name]_bundle.js',
+      },
+    },
   },
 });
