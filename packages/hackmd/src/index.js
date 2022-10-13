0d05cbcec8101a745292037d57076f5fd678409f
Yuken Tezuka <yuken-t@weseek.co.jp>
2022-10-13 07:16:53 +0000

    export file as string

--- packages/hackmd/src/index.js
+++ packages/hackmd/src/index.js
@@ -1,7 +1,7 @@
+const fs = require('fs');
+const path = require('path');
 
-
-export * as styles from '../dist/assets/styles_bundle';
-
-export * as agent from '../dist/assets/agent_bundle';
-
-export * as stylesCSS from '../dist/assets/styles_bundle';
+// export to app as string
+export const styles = fs.readFileSync(path.resolve(__dirname, '../dist/assets/styles_bundle.js'));
+export const agent = fs.readFileSync(path.resolve(__dirname, '../dist/assets/agent_bundle.js'));
+export const stylesCSS = fs.readFileSync(path.resolve(__dirname, '../dist/assets/styles_bundle.css'));
