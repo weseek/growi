58b84aac69a93f4ae565c7ba923294f62ada5b51
Yuken Tezuka <yuken-t@weseek.co.jp>
2022-10-13 07:33:49 +0000

    add toString method

--- packages/hackmd/src/index.js
+++ packages/hackmd/src/index.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 const path = require('path');
 
 // export to app as string
-export const styles = fs.readFileSync(path.resolve(__dirname, '../dist/assets/styles_bundle.js'));
-export const agent = fs.readFileSync(path.resolve(__dirname, '../dist/assets/agent_bundle.js'));
-export const stylesCSS = fs.readFileSync(path.resolve(__dirname, '../dist/assets/styles_bundle.css'));
+export const styles = fs.readFileSync(path.resolve(__dirname, '../dist/assets/styles_bundle.js')).toString();
+export const agent = fs.readFileSync(path.resolve(__dirname, '../dist/assets/agent_bundle.js')).toString();
+export const stylesCSS = fs.readFileSync(path.resolve(__dirname, '../dist/assets/styles_bundle.css')).toString();
