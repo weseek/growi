ded3dca5b919695f7c46cc394ecd83f8697d1875
Yuken Tezuka <yuken-t@weseek.co.jp>
2022-10-17 05:22:48 +0000

    reload page tree after convrtting pages by path

--- packages/app/src/components/PrivateLegacyPages.tsx
+++ packages/app/src/components/PrivateLegacyPages.tsx
@@ -455,6 +455,7 @@ const PrivateLegacyPages = (): JSX.Element => {
             toastSuccess(t('private_legacy_pages.by_path_modal.success'));
             setOpenConvertModal(false);
             mutate();
+            advancePt();
           }
           catch (errs) {
             if (errs.length === 1) {
