f0b8589d9e4fe652fd7d418d2fd105d44426cd1d
Shun Miyazawa <m0zurillex@gmail.com>
2022-10-18 08:49:49 +0000

    Element Existence Check

--- packages/app/test/cypress/integration/20-basic-features/use-tools.spec.ts
+++ packages/app/test/cypress/integration/20-basic-features/use-tools.spec.ts
@@ -104,8 +104,8 @@ context('Modal for page operation', () => {
     cy.visit('/Sandbox/Bootstrap4');
 
     cy.getByTestid('grw-contextual-sub-nav').within(() => {
-       cy.getByTestid('open-page-item-control-btn').click();
-       cy.getByTestid('open-page-delete-modal-btn').click();
+       cy.getByTestid('open-page-item-control-btn').should('be.visible').click();
+       cy.getByTestid('open-page-delete-modal-btn').should('be.visible').click();
     });
 
      cy.getByTestid('page-delete-modal').should('be.visible').screenshot(`${ssPrefix}-delete-bootstrap4`);
