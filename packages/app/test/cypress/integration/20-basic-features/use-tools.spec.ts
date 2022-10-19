f7b29e51466957b27f8092f2a89ece362c2d46c6
Shun Miyazawa <m0zurillex@gmail.com>
2022-10-19 03:38:39 +0000

    added timeout

--- packages/app/test/cypress/integration/20-basic-features/use-tools.spec.ts
+++ packages/app/test/cypress/integration/20-basic-features/use-tools.spec.ts
@@ -104,8 +104,8 @@ context('Modal for page operation', () => {
     cy.visit('/Sandbox/Bootstrap4');
 
      cy.get('#grw-subnav-container').within(() => {
-       cy.getByTestid('open-page-item-control-btn').click();
-       cy.getByTestid('open-page-delete-modal-btn').click();
+       cy.getByTestid('open-page-item-control-btn', { timeout: 10000 }).should('be.visible').click();
+       cy.getByTestid('open-page-delete-modal-btn', { timeout: 10000 }).should('be.visible').click();
     });
 
      cy.getByTestid('page-delete-modal').should('be.visible').screenshot(`${ssPrefix}-delete-bootstrap4`);
