e5565072c415255f99a417c1c8b4b504c0ac71b3
kaori <kaori@weseek.co.jp>
2022-10-21 08:22:02 +0000

    force true

--- packages/app/test/cypress/integration/20-basic-features/use-tools.spec.ts
+++ packages/app/test/cypress/integration/20-basic-features/use-tools.spec.ts
@@ -104,10 +104,8 @@ context('Modal for page operation', () => {
     cy.visit('/Sandbox/Bootstrap4');
 
      cy.get('#grw-subnav-container').within(() => {
-      // eslint-disable-next-line cypress/no-unnecessary-waiting
-      cy.wait(1000)
        cy.getByTestid('open-page-item-control-btn').click();
-       cy.getByTestid('open-page-delete-modal-btn').click();
+       cy.getByTestid('open-page-delete-modal-btn').click({force: true});
     });
 
      cy.getByTestid('page-delete-modal').should('be.visible').screenshot(`${ssPrefix}-delete-bootstrap4`);
