9f6010ad3a2f05665de90017d74ce5d5b3fea09a
Yohei-Shiina <yohei0125@weseek.co.jp>
2022-10-24 05:19:29 +0000

    change to layout fill

--- packages/app/src/components/Navbar/GrowiNavbar.tsx
+++ packages/app/src/components/Navbar/GrowiNavbar.tsx
@@ -132,11 +132,10 @@ const GrowiNavbarLogo: FC<NavbarLogoProps> = memo((props: NavbarLogoProps) => {
     ? (
       // The nature of next/image, which wraps the <img> with other tags such as <div>, makes it difficult to adjust the style of the image.
       // Instead of adding css selectors to the <Image />, wrap the <Image /> in a <div> and add selectors there to control the styles.
-      <div id="settingBrandLogo" className='position-relative mx-2 d-flex'>
+      <div id="settingBrandLogo" className='position-relative picture picture-lg d-flex mx-2'>
         <Image loader={nextImageLoader} alt='logo'
           src={logoSrc}
-          height='48'
-          width='48'
+          layout='fill'
           quality='100'
           className='p-2'
         />
