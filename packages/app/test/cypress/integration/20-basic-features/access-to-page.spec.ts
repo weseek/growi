11c298656f8e94de094fcfd367fb2c3bb7d2d595
jam411 <ryoji-s@weseek.co.jp>
2022-10-25 03:42:32 +0000

    fix 20

--- packages/app/test/cypress/integration/20-basic-features/access-to-page.spec.ts
+++ packages/app/test/cypress/integration/20-basic-features/access-to-page.spec.ts
@@ -99,13 +99,13 @@ context('Access to special pages', () => {
 
   it('/tags is successfully loaded', () => {
     cy.visit('/tags');
+    cy.getByTestid('tags-page').should('be.visible');
 
     // open sidebar
     cy.collapseSidebar(false);
     // select tags
     cy.getByTestid('grw-sidebar-nav-primary-tags').click();
     cy.getByTestid('grw-sidebar-content-tags').should('be.visible');
-    cy.getByTestid('tags-page').should('be.visible');
     cy.getByTestid('grw-tags-list').should('be.visible');
     cy.getByTestid('grw-tags-list').contains('You have no tag, You can set tags on pages');
 
--- packages/app/test/cypress/integration/21-basic-features-for-guest/access-to-page.spec.ts
+++ packages/app/test/cypress/integration/21-basic-features-for-guest/access-to-page.spec.ts
@@ -74,13 +74,13 @@ context('Access to special pages by guest', () => {
 
   it('/tags is successfully loaded', () => {
     cy.visit('/tags');
+    cy.getByTestid('tags-page').should('be.visible');
 
     // open sidebar
     cy.collapseSidebar(false);
     // select tags
     cy.getByTestid('grw-sidebar-nav-primary-tags').click();
     cy.getByTestid('grw-sidebar-content-tags').should('be.visible');
-    // cy.getByTestid('tags-page').should('be.visible');
     cy.getByTestid('grw-tags-list').should('be.visible');
     cy.getByTestid('grw-tags-list').contains('You have no tag, You can set tags on pages');
 
