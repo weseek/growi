0adc1aad8f8166d3713b874d8d71b8bed2cd21f8
jam411 <ryoji-s@weseek.co.jp>
2022-11-01 02:55:17 +0000

    AppSetting.jsx

--- packages/app/src/components/Admin/App/AppSetting.jsx
+++ packages/app/src/components/Admin/App/AppSetting.jsx
@@ -23,7 +23,7 @@ const AppSetting = (props) => {
   const submitHandler = useCallback(async() => {
     try {
       await adminAppContainer.updateAppSettingHandler();
-      toastSuccess(t('toaster.update_successed', { target: t('commons:headers.app_settings') }));
+      toastSuccess(t('commons:toaster.update_successed', { target: t('commons:headers.app_settings') }));
     }
     catch (err) {
       toastError(err);
