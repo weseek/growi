0bd1198bd2180cac1cc2ba2b921e8dfe557dec9c
Shun Miyazawa <m0zurillex@gmail.com>
2022-11-07 07:03:06 +0000

    Fixed

--- packages/app/test/cypress/integration/50-sidebar/access-to-side-bar.spec.ts
+++ packages/app/test/cypress/integration/50-sidebar/access-to-side-bar.spec.ts
@@ -79,7 +79,7 @@ context('Access to sidebar', () => {
         cy.getByTestid('grw-navigation-resize-button').click({force: true});
       }
     });
-    cy.getByTestid('grw-contextual-navigation-sub').screenshot(`${ssPrefix}page-tree-1-access-to-page-tree`);
+    cy.getByTestid('grw-contextual-navigation-sub').should('be.visible').screenshot(`${ssPrefix}page-tree-1-access-to-page-tree`);
     cy.get('.grw-pagetree-triangle-btn').eq(0).click();
     cy.getByTestid('grw-contextual-navigation-sub').screenshot(`${ssPrefix}page-tree-2-hide-page-tree-item`);
     cy.get('.grw-pagetree-triangle-btn').eq(0).click();
