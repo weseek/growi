35b8a1d988165497de59a9f43e5333fbdd8abcf9
yuya-o <yuya-o@weseek.co.jp>
2022-11-11 04:46:50 +0000

    separated SidebarHeaderSkelton component

--- packages/app/src/components/Sidebar/PageTreeSkelton.tsx
+++ packages/app/src/components/Sidebar/PageTreeSkelton.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 
-import { Skelton } from '~/components/Skelton';
-
 import ItemsTreeSkelton from './PageTree/ItemsTreeSkelton';
-
-import styles from './SidebarSkelton.module.scss';
+import SidebarHeaderSkelton from './Skelton/SidebarHeaderSkelton';
 
 const PageTreeSkelton = (): JSX.Element => {
 
   return (
     <>
-      <div className="grw-sidebar-content-header p-3">
-        <Skelton additionalClass={styles['grw-sidebar-content-header-skelton']} />
-      </div>
+      <SidebarHeaderSkelton />
       <ItemsTreeSkelton />
     </>
   );
