77e98c68f84a2784156f7a6b85c9be2894123908
Yuki Takei <yuki@weseek.co.jp>
2022-11-22 13:07:51 +0000

    fix import line

--- packages/remark-drawio-plugin/src/utils/global.ts
+++ packages/remark-drawio-plugin/src/utils/global.ts
@@ -1,4 +1,4 @@
-import { IGraphViewer } from '~/interfaces/graph-viewer';
+import { IGraphViewer } from '../interfaces/graph-viewer';
 
 export const isGraphViewer = (val: unknown): val is IGraphViewer => {
   return (typeof val === 'function' && 'createViewerForElement' in val && 'processElements' in val);
