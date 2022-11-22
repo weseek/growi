45e768043d56ff270975b1588bb55251774dc77e
Shun Miyazawa <m0zurillex@gmail.com>
2022-11-22 09:34:07 +0000

    fix

--- packages/app/test/cypress/integration/20-basic-features/access-to-page.spec.ts
+++ packages/app/test/cypress/integration/20-basic-features/access-to-page.spec.ts
@@ -19,7 +19,8 @@ context('Access to page', () => {
     cy.visit('/Sandbox#Headers');
     cy.waitUntilSkeletonDisappear();
 
-    cy.get('#revision-toc').should('be.visible');
+    // for check download toc data
+    cy.get('.toc-link').should('be.visible');
 
     // hide fab // disable fab for sticky-events warning
     // cy.getByTestid('grw-fab-container').invoke('attr', 'style', 'display: none');
