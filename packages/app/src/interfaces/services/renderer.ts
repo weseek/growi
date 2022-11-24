56b713886c2457396e125ad6661019bf80a8b692
Yohei-Shiina <yohei0125@weseek.co.jp>
2022-11-24 13:30:58 +0000

    replace with RehypeSanitizeOptionConfig

--- packages/app/src/interfaces/services/renderer.ts
+++ packages/app/src/interfaces/services/renderer.ts
@@ -1,5 +1,4 @@
-import { XssOptionConfig } from '~/services/xss/xssOption';
-
+import { RehypeSanitizeOptionConfig } from '../rehype';
 
 export type RendererConfig = {
   isEnabledLinebreaks: boolean,
@@ -10,4 +9,4 @@ export type RendererConfig = {
 
   plantumlUri: string | null,
   blockdiagUri: string | null,
-} & XssOptionConfig;
+} & RehypeSanitizeOptionConfig;
