4fd566f0f1829cd2e8727047df39d251934c70c3
mizozobu <hamc.m.so.j12@gmail.com>
2022-11-30 22:31:17 +0900

    fix: add mr to data transfer link on admin nav

--- packages/app/src/components/Admin/Common/AdminNavigation.jsx
+++ packages/app/src/components/Admin/Common/AdminNavigation.jsx
@@ -36,7 +36,7 @@ const AdminNavigation = (props) => {
       case 'user-groups':              return <><i className="mr-1 icon-fw icon-people"></i>{          t('user_group_management.user_group_management') }</>;
       case 'search':                   return <><i className="mr-1 icon-fw icon-magnifier"></i>{       t('full_text_search_management.full_text_search_management') }</>;
       case 'audit-log':                return <><i className="mr-1 icon-fw icon-feed"></i>{            t('audit_log_management.audit_log') }</>;
-      case 'data-transfer':            return <><i className="icon-fw icon-arrow-right"></i>{          t('g2g_data_transfer.data_transfer') }</>;
+      case 'data-transfer':            return <><i className="mr-1 icon-fw icon-arrow-right"></i>{          t('g2g_data_transfer.data_transfer') }</>;
       case 'cloud':                    return <><i className="mr-1 icon-fw icon-share-alt"></i>{       t('cloud_setting_management.to_cloud_settings') } </>;
       default:                         return <><i className="mr-1 icon-fw icon-home"></i>{            t('wiki_management_home_page') }</>;
       /* eslint-enable no-multi-spaces, max-len */
