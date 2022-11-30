b1f76d8c3f227216f3139405ce0f224aa3735db0
jam411 <ryoji-s@weseek.co.jp>
2022-11-30 10:10:54 +0000

    update

--- packages/app/test/cypress/integration/10-install/install.spec.ts
+++ packages/app/test/cypress/integration/10-install/install.spec.ts
@@ -7,7 +7,7 @@ describe('Install', () => {
 
   it('Successfully show installer', () => {
     cy.getByTestid('installerForm').should('be.visible');
-    cy.screenshot(`${ssPrefix}redirect-to-installer`);
+    cy.screenshot(`${ssPrefix}-redirect-to-installer-page`);
   });
 
   it('Sccessfully choose languages', () => {
@@ -15,31 +15,28 @@ describe('Install', () => {
     cy.getByTestid('dropdownLanguage').should('be.visible');
     // TODO: should not use wait.
     // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(1000); // wait for load
+    cy.wait(1000); // waiting for load
 
     cy.getByTestid('dropdownLanguage').click();
     cy.get('.dropdown-menu').should('be.visible');
-
     cy.getByTestid('dropdownLanguageMenu-en_US').click();
     cy.get('.alert-success').should('be.visible');
     cy.screenshot(`${ssPrefix}-select-en_US`);
 
     cy.getByTestid('dropdownLanguage').click();
     cy.get('.dropdown-menu').should('be.visible');
-
     cy.getByTestid('dropdownLanguageMenu-ja_JP').click();
     cy.get('.alert-success').should('be.visible');
     cy.screenshot(`${ssPrefix}-select-ja_JP`);
 
     cy.getByTestid('dropdownLanguage').click();
     cy.get('.dropdown-menu').should('be.visible');
-
     cy.getByTestid('dropdownLanguageMenu-zh_CN').click();
     cy.get('.alert-success').should('be.visible');
     cy.screenshot(`${ssPrefix}-select-zh_CN`);
   });
 
-  it('Successfully installing and redirect root page', () => {
+  it('Successfully installing and redirect to root page', () => {
     cy.fixture("user-admin.json").then(user => {
       cy.getByTestid('tiUsername').type(user.username);
       cy.getByTestid('tiName').type(user.name);
@@ -50,10 +47,10 @@ describe('Install', () => {
 
     cy.getByTestid('btnSubmit').click();
 
-    // TODO: installer do not redirect to top page.
+    // Requires a lot of time (more than 10000ms) for redirect to root page
     cy.waitUntilSkeletonDisappear();
-    cy.screenshot(`${ssPrefix}-installed`, {
-      blackout: ['#grw-sidebar-contents-wrapper','[data-line="2"]:eq(0) > a > img', '[data-hide-in-vrt=true]'],
+    cy.screenshot(`${ssPrefix}-installed-redirect-to-root-page`, {
+      blackout: ['[data-hide-in-vrt=true]']
     });
   });
 });
