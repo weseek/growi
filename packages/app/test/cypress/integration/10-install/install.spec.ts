4896a37ef1d6c17597efbe775d5b06c55e002389
jam411 <ryoji-s@weseek.co.jp>
2022-11-30 10:00:18 +0000

    update

--- packages/app/test/cypress/integration/10-install/install.spec.ts
+++ packages/app/test/cypress/integration/10-install/install.spec.ts
@@ -1,47 +1,45 @@
-context('Installer', () => {
-
+describe('Install', () => {
   const ssPrefix = 'installer-';
 
   beforeEach(() => {
     cy.visit('/');
-  })
-
-  it('successfully loads', () => {
-    cy.screenshot(`${ssPrefix}-on-load`);
-    cy.getByTestid('installerForm').should('be.visible');
   });
 
-  it('the dropdown for language works', () => {
+  it('Successfully show installer', () => {
+    cy.getByTestid('installerForm').should('be.visible');
+    cy.screenshot(`${ssPrefix}redirect-to-installer`);
+  });
+
+  it('Sccessfully choose languages', () => {
+    cy.getByTestid('installerForm').should('be.visible');
     cy.getByTestid('dropdownLanguage').should('be.visible');
+    // TODO: should not use wait.
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1000); // wait for load
 
     cy.getByTestid('dropdownLanguage').click();
-    cy.screenshot(`${ssPrefix}-open-dropdownLanguage`);
-    cy.getByTestid('dropdownLanguage').click(); // close
+    cy.get('.dropdown-menu').should('be.visible');
 
-    cy.getByTestid('dropdownLanguage').click();
     cy.getByTestid('dropdownLanguageMenu-en_US').click();
+    cy.get('.alert-success').should('be.visible');
     cy.screenshot(`${ssPrefix}-select-en_US`);
 
     cy.getByTestid('dropdownLanguage').click();
+    cy.get('.dropdown-menu').should('be.visible');
+
     cy.getByTestid('dropdownLanguageMenu-ja_JP').click();
+    cy.get('.alert-success').should('be.visible');
     cy.screenshot(`${ssPrefix}-select-ja_JP`);
 
     cy.getByTestid('dropdownLanguage').click();
+    cy.get('.dropdown-menu').should('be.visible');
+
     cy.getByTestid('dropdownLanguageMenu-zh_CN').click();
+    cy.get('.alert-success').should('be.visible');
     cy.screenshot(`${ssPrefix}-select-zh_CN`);
   });
 
-});
-
-context('Installing', () => {
-
-  const ssPrefix = 'installing-';
-
-  beforeEach(() => {
-    cy.visit('/');
-  })
-
-  it('has succeeded', () => {
+  it('Successfully installing and redirect root page', () => {
     cy.fixture("user-admin.json").then(user => {
       cy.getByTestid('tiUsername').type(user.username);
       cy.getByTestid('tiName').type(user.name);
@@ -52,9 +50,10 @@ context('Installing', () => {
 
     cy.getByTestid('btnSubmit').click();
 
+    // TODO: installer do not redirect to top page.
+    cy.waitUntilSkeletonDisappear();
     cy.screenshot(`${ssPrefix}-installed`, {
       blackout: ['#grw-sidebar-contents-wrapper','[data-line="2"]:eq(0) > a > img', '[data-hide-in-vrt=true]'],
     });
   });
-
 });
