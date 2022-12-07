718341c08272a865303107997b996f8fa5f51303
Shun Miyazawa <34241526+miya@users.noreply.github.com>
2022-12-07 22:36:11 +0900

    fix specPattern path

--- packages/app/cypress.config.ts
+++ packages/app/cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    specPattern: '**/*.spec.ts',
+    specPattern: 'test/cypress/integration/**/*',
     supportFile: 'test/cypress/support/index.ts',
     setupNodeEvents: (on, config) => {
       // change screen size
