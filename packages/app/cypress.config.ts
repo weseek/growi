eb85cde6c1785b75c367de6f71031ba915019dff
Shun Miyazawa <m0zurillex@gmail.com>
2022-12-07 06:37:59 +0000

    fix  directory path

--- packages/app/cypress.config.ts
+++ packages/app/cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    specPattern: 'test/cypress/integration/**/*.spec.ts',
+    specPattern: 'test/cypress/integration',
     supportFile: 'test/cypress/support/index.ts',
   },
   fileServerFolder: 'test/cypress',
