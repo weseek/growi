5dde0c74215c3088178d00863c39994ef0a772e9
yuya-o <yuya-o@weseek.co.jp>
2022-12-08 10:08:05 +0000

    set CUSTOM before getting currentSidebarContents

--- packages/app/src/stores/ui.tsx
+++ packages/app/src/stores/ui.tsx
@@ -267,7 +267,7 @@ export const useSidebarCollapsed = (initialData?: boolean): SWRResponse<boolean,
 };
 
 export const useCurrentSidebarContents = (initialData?: SidebarContentsType): SWRResponse<SidebarContentsType, Error> => {
-  return useStaticSWR('sidebarContents', initialData, { fallbackData: SidebarContentsType.TREE });
+  return useStaticSWR('sidebarContents', initialData, { fallbackData: SidebarContentsType.CUSTOM });
 };
 
 export const useCurrentProductNavWidth = (initialData?: number): SWRResponse<number, Error> => {
