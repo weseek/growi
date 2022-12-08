0779870c507a3b21293cd941b49d8d0d2384a304
jam411 <ryoji-s@weseek.co.jp>
2022-12-08 06:22:57 +0000

    update

--- packages/app/src/components/Page/RenderTagLabels.tsx
+++ packages/app/src/components/Page/RenderTagLabels.tsx
@@ -26,18 +26,18 @@ const RenderTagLabels = React.memo((props: RenderTagLabelsProps) => {
     <>
       {tags.map((tag) => {
         return (
-          <a key={tag} href={`/_search?q=tag:${tag}`} className="ml-2 grw-tag-label badge badge-secondary">
+          <a key={tag} href={`/_search?q=tag:${tag}`} className="grw-tag-label badge badge-secondary mr-2">
             {tag}
           </a>
         );
       })}
       <div id="edit-tags-btn-wrapper-for-tooltip">
         <a
-          className={`btn btn-link btn-edit-tags text-muted ${isTagsEmpty ? 'px-2 py-0 no-tags' : 'p-0'} ${isGuestUser && 'disabled'}`}
+          className={`btn btn-link btn-edit-tags text-muted p-0 ${isTagsEmpty && 'no-tags'} ${isGuestUser && 'disabled'}`}
           onClick={openEditorHandler}
         >
           { isTagsEmpty && <>{ t('Add tags for this page') }</>}
-          <i className={'ml-1 icon-plus'}/>
+          <i className={`icon-plus ${isTagsEmpty && 'ml-1'}`}/>
         </a>
       </div>
       {isGuestUser && (
