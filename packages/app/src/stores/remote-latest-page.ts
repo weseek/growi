92e86622efda4b4e9352a436b30ce9dfc882a8dd
Yuken Tezuka <yuken-t@weseek.co.jp>
2022-12-12 10:10:43 +0000

    create useSetRemoteLatestPageData

--- packages/app/src/stores/remote-latest-page.ts
+++ packages/app/src/stores/remote-latest-page.ts
@@ -20,3 +20,34 @@ export const useRemoteRevisionLastUpdatUser = (initialData?: IUser): SWRResponse
 export const useRemoteRevisionLastUpdatedAt = (initialData?: Date): SWRResponse<Date, Error> => {
   return useStaticSWR<Date, Error>('remoteRevisionLastUpdatedAt', initialData);
 };
+
+
+// set remote data all at once
+export const useSetRemoteLatestPageData = (): { setRemoteLatestPageData: (pageData: any) => void } => {
+  const { mutate: mutateRemoteRevisionId } = useRemoteRevisionId();
+  const { mutate: mutateRemoteRevisionBody } = useRemoteRevisionBody();
+  const { mutate: mutateRemoteRevisionLastUpdatUser } = useRemoteRevisionLastUpdatUser();
+  const { mutate: mutateRemoteRevisionLastUpdatedAt } = useRemoteRevisionLastUpdatedAt();
+
+  type RemoteRevisionData = {
+    remoteRevisionId: string,
+    remoteRevisionBody: string,
+    remoteRevisionLastUpdatUser: IUser,
+    remoteRevisionLastUpdatedAt: Date
+  }
+
+  const setRemoteLatestPageData = (remoteRevisionData: RemoteRevisionData) => {
+    const {
+      remoteRevisionId, remoteRevisionBody, remoteRevisionLastUpdatUser, remoteRevisionLastUpdatedAt,
+    } = remoteRevisionData;
+    mutateRemoteRevisionId(remoteRevisionId);
+    mutateRemoteRevisionBody(remoteRevisionBody);
+    mutateRemoteRevisionLastUpdatUser(remoteRevisionLastUpdatUser);
+    mutateRemoteRevisionLastUpdatedAt(remoteRevisionLastUpdatedAt);
+  };
+
+  return {
+    setRemoteLatestPageData,
+  };
+
+};
