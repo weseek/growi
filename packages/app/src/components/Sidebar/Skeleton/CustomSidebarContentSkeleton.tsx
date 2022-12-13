56c81d4eccea2d41b19ef3ec582fb9a1959c27f9
yuya-o <yuya-o@weseek.co.jp>
2022-12-13 13:47:35 +0000

    CSS arrangement

--- packages/app/src/components/Sidebar/Skeleton/CustomSidebarContentSkeleton.tsx
+++ packages/app/src/components/Sidebar/Skeleton/CustomSidebarContentSkeleton.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 
 import { Skeleton } from '~/components/Skeleton';
 
-import customSidebarStyles from '../CustomSidebar.module.scss';
-import styles from './SidebarSkeleton.module.scss';
+import styles from '../CustomSidebar.module.scss';
 
 const CustomSidebarContentSkeleton = (): JSX.Element => {
 
   return (
-    <div className={`p-3 grw-custom-sidebar-content ${customSidebarStyles['grw-custom-sidebar-content']}`}>
-      <Skeleton additionalClass={`${styles['grw-sidebar-skeleton-text-full']} mt-3`} />
-      <Skeleton additionalClass={`${styles['grw-sidebar-skeleton-text-full']} my-4`} />
+    <div className={`p-3 grw-custom-sidebar-content ${styles['grw-custom-sidebar-content']}`}>
+      <Skeleton additionalClass={`grw-custom-sidebar-skeleton-text ${styles['grw-custom-sidebar-skeleton-text']} mt-3`} />
+      <Skeleton additionalClass={`grw-custom-sidebar-skeleton-text ${styles['grw-custom-sidebar-skeleton-text']} my-4`} />
     </div>
   );
 };
