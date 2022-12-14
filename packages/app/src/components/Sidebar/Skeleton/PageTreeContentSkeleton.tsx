2abeb796da20b69ce4a5b3d770dfeade0df8a215
yuya-o <yuya-o@weseek.co.jp>
2022-12-14 03:51:56 +0000

    small adjustments on PageTree, Tag

--- packages/app/src/components/Sidebar/Skeleton/PageTreeContentSkeleton.tsx
+++ packages/app/src/components/Sidebar/Skeleton/PageTreeContentSkeleton.tsx
@@ -8,9 +8,9 @@ const PageTreeContentSkeleton = (): JSX.Element => {
 
   return (
     <ul className={`grw-pagetree ${styles['grw-pagetree']} list-group py-3`} >
-      <Skeleton additionalClass={styles['grw-pagetree-item-skeleton']} />
-      <Skeleton additionalClass={`${styles['grw-pagetree-item-skeleton']} ${styles['grw-pagetree-item-skeleton-children']}`} />
-      <Skeleton additionalClass={`${styles['grw-pagetree-item-skeleton']} ${styles['grw-pagetree-item-skeleton-children']}`} />
+      <Skeleton additionalClass={`${styles['grw-pagetree-item-skeleton-text']} pr-3`} />
+      <Skeleton additionalClass={`${styles['grw-pagetree-item-skeleton-text-child']} pr-3`} />
+      <Skeleton additionalClass={`${styles['grw-pagetree-item-skeleton-text-child']} pr-3`} />
     </ul>
   );
 };
