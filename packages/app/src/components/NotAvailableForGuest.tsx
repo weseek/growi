3dbb41d3e5b0579e3d82bbd4b481fd9a12d1d567
Yuken Tezuka <yuken-t@weseek.co.jp>
2022-12-14 07:28:44 +0000

    remove unused code

--- packages/app/src/components/NotAvailableForGuest.tsx
+++ packages/app/src/components/NotAvailableForGuest.tsx
@@ -16,20 +16,13 @@ export const NotAvailableForGuest = ({ children }: NotAvailableForGuestProps): J
   const { data: isGuestUser } = useIsGuestUser();
   const isDisabled = !!isGuestUser;
 
-  const id = children.props.id || `grw-not-available-for-guest-${Math.random().toString(32).substring(2)}`;
-
-  // clone and add className
-  const clonedChild = React.cloneElement(children, {
-    id,
-    className: `${children.props.className} grw-not-available-for-guest`,
-  });
-
+  const id = `grw-not-available-for-guest-${Math.random().toString(32).substring(2)}`;
 
   return (
     <>
       <div id={id}>
         <Disable disabled={isDisabled}>
-          { clonedChild }
+          { children }
         </Disable>
       </div>
       <UncontrolledTooltip placement="top" target={id}>{t('Not available for guest')}</UncontrolledTooltip>
