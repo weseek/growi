7b2feb8ef56bc414309e13284ef29765b862836f
Yuki Takei <yuki@weseek.co.jp>
2022-12-26 12:34:32 +0000

    remove unnecessary code

--- packages/app/src/stores/renderer.tsx
+++ packages/app/src/stores/renderer.tsx
@@ -2,7 +2,6 @@ import { HtmlElementNode } from 'rehype-toc';
 import useSWR, { SWRResponse } from 'swr';
 import useSWRImmutable from 'swr/immutable';
 
-import { RendererConfig } from '~/interfaces/services/renderer';
 import {
   RendererOptions,
   generateSimpleViewOptions, generatePreviewOptions,
@@ -17,31 +16,6 @@ import {
 import { useCurrentPagePath } from './page';
 import { useCurrentPageTocNode } from './ui';
 
-interface ReactMarkdownOptionsGenerator {
-  (config: RendererConfig): RendererOptions
-}
-
-// The base hook with common processes
-const _useOptionsBase = (
-    rendererId: string, generator: ReactMarkdownOptionsGenerator,
-): SWRResponse<RendererOptions, Error> => {
-  const { data: rendererConfig } = useRendererConfig();
-
-  const isAllDataValid = rendererConfig != null;
-
-  const key = isAllDataValid
-    ? [rendererId, rendererConfig]
-    : null;
-
-  const swrResult = useSWRImmutable<RendererOptions, Error>(key);
-
-  if (isAllDataValid && swrResult.data == null) {
-    swrResult.mutate(generator(rendererConfig));
-  }
-
-  // call useSWRImmutable again to foce to update cache
-  return useSWRImmutable<RendererOptions, Error>(key);
-};
 
 export const useViewOptions = (storeTocNodeHandler: (toc: HtmlElementNode) => void): SWRResponse<RendererOptions, Error> => {
   const { data: currentPagePath } = useCurrentPagePath();
