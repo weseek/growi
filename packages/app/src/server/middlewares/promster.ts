dbe3648bb2dd702a1d8feb71d60d9c2442a6297b
Yuki Takei <yuki@weseek.co.jp>
2023-01-04 14:11:31 +0000

    typescriptize

--- packages/app/src/server/middlewares/promster.ts
+++ packages/app/src/server/middlewares/promster.ts
@@ -1,4 +1,15 @@
-module.exports = (crowi, app) => {
+import {
+  Request, Response, NextFunction,
+} from 'express';
+
+import loggerFactory from '~/utils/logger';
+
+const logger = loggerFactory('growi:middlewares:promster');
+
+type Middleware = (req: Request, res: Response, next: NextFunction) => void;
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+function middleware(crowi: any, app): Middleware {
   const { configManager } = crowi;
 
   // when disabled
@@ -6,6 +17,10 @@ module.exports = (crowi, app) => {
     return (req, res, next) => next();
   }
 
+  logger.info('Promster is enabled');
+
   const { createMiddleware } = require('@promster/express');
   return createMiddleware({ app });
-};
+}
+
+module.exports = middleware;
