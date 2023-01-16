42cad6b58352911511282a9e4e547083754c8cfe
Yuki Takei <yuki@weseek.co.jp>
2023-01-16 21:18:01 +0900

    fis repos

--- packages/app/docker/codebuild/oidc.tf
+++ packages/app/docker/codebuild/oidc.tf
@@ -1,9 +1,9 @@
 module "oidc_github" {
   source  = "unfunco/oidc-github/aws"
 
-#   iam_role_name = "github-oidc-"
+  iam_role_name = "GitHubOIDC-for-growi"
 
   github_repositories = [
-    "weseek/github",
+    "weseek/growi",
   ]
 }
