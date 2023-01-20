d94bcab7f4fce7d45e3e6ea9bd1929261484afc8
Yuken Tezuka <yuken-t@weseek.co.jp>
2023-01-20 04:12:46 +0000

    refactor regexp

--- packages/app/migration.js
+++ packages/app/migration.js
@@ -4,9 +4,9 @@ const { MongoClient } = require('mongodb');
 const uri = 'mongodb://mongo:27017/growi';
 
 const oldDrawioRegExp = /::: drawio\n(.+)\n:::/g; // drawioの旧記法
-const oldPlantUmlRegExp = /@startuml\n(.+)\n@enduml/g; // plantUMLの旧記法
-const oldTsvTableRegExp = /::: tsv(-h)?\n(.+)\n:::/g; // TSVによるテーブル描画の旧記法
-const oldCsvTableRegExp = /::: csv(-h)?\n(.+)\n:::/g; // CSVによるテーブル描画の旧記法
+const oldPlantUmlRegExp = /@startuml\n([\s\S]*?)\n@enduml/g; // plantUMLの旧記法
+const oldTsvTableRegExp = /::: tsv(-h)?\n([\s\S]*?)\n:::/g; // TSVによるテーブル描画の旧記法
+const oldCsvTableRegExp = /::: csv(-h)?\n([\s\S]*?)\n:::/g; // CSVによるテーブル描画の旧記法
 
 async function main() {
 
@@ -29,16 +29,17 @@ async function main() {
       ],
     });
 
-    // cursorをイテレートして新記法に置き換える
-    await aggrCursor.forEach((doc) => {
+    let doc;
+    // eslint-disable-next-line no-cond-assign, no-await-in-loop
+    while ((doc = await aggrCursor.next())) {
       const bodyDrawio = doc.body.replaceAll(oldDrawioRegExp, '``` drawio\n$1\n```'); // drawioを新記法に置き換え
       const bodyPlantUml = bodyDrawio.replaceAll(oldPlantUmlRegExp, '``` plantuml\n$1\n```'); // PlantUMLを新記法に置き換え
       const bodyTsvTable = bodyPlantUml.replaceAll(oldTsvTableRegExp, '``` tsv$1\n$2\n```'); // TSVによるテーブルを新記法に置き換え
-      const bodyCsvTable = bodyTsvTable.replaceAll(oldTsvTableRegExp, '``` csv$1\n$2\n```'); // CSVによるテーブルを新記法に置き換え
+      const bodyCsvTable = bodyTsvTable.replaceAll(oldCsvTableRegExp, '``` csv$1\n$2\n```'); // CSVによるテーブルを新記法に置き換え
 
-      // 保存処理
-      revision.updateOne({ _id: doc._id }, [{ $set: { body: bodyCsvTable } }]);
-    });
+      // eslint-disable-next-line no-await-in-loop
+      await revision.updateOne({ _id: doc._id }, [{ $set: { body: bodyCsvTable } }]);
+    }
   }
   catch (e) {
     console.error(e);
