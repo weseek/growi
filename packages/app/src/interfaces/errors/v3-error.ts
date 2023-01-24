bf7fc9920fdf73368777ec2e7d41761bfd6c44f6
Shun Miyazawa <shun-m@weseek.co.jp>
2023-01-24 10:03:25 +0000

    create type guard

--- packages/app/src/interfaces/errors/v3-error.ts
+++ packages/app/src/interfaces/errors/v3-error.ts
@@ -1,3 +1,8 @@
 import { ErrorV3 } from '@growi/core';
 
 export type IErrorV3 = ErrorV3
+
+// type guard
+export const isErrorV3 = (args: any): args is IErrorV3 => {
+  return args.message != null && args.code != null;
+};
