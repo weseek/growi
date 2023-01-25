f5b24ac693969fb57331c293214d2b7fa3630173
Yuken Tezuka <yuken-t@weseek.co.jp>
2023-01-25 07:58:29 +0000

    refactor replace method

--- packages/app/migration.sh
+++ packages/app/migration.sh
@@ -1,14 +1,33 @@
-var oldDrawioRegExp = /:::\s?drawio\n(.+)\n:::/g; // drawio old format
-var oldPlantUmlRegExp = /@startuml\n([\s\S]*?)\n@enduml/g; // plantUML old format
-var oldTsvTableRegExp = /::: tsv(-h)?\n([\s\S]*?)\n:::/g; // TSV old format
-var oldCsvTableRegExp = /::: csv(-h)?\n([\s\S]*?)\n:::/g; // CSV old format
 
-function replaceBody(body) {
-  const drawioReplaced = body.replace(oldDrawioRegExp, '``` drawio\n$1\n```');
-  const plantUmlReplaced = drawioReplaced.replace(oldPlantUmlRegExp, '``` plantuml\n$1\n```');
-  const tsvReplaced = plantUmlReplaced.replace(oldTsvTableRegExp, '``` tsv$1\n$2\n```');
-  const csvReplaced = tsvReplaced.replace(oldCsvTableRegExp, '``` csv$1\n$2\n```');
-  return csvReplaced;
+
+
+
+function drawioProcessor(body) {
+  var oldDrawioRegExp = /:::\s?drawio\n(.+)\n:::/g; // drawio old format
+  return body.replace(oldDrawioRegExp, '``` drawio\n$1\n```');
+}
+
+function plantumlProcessor(body) {
+  var oldPlantUmlRegExp = /@startuml\n([\s\S]*?)\n@enduml/g; // plantUML old format
+  return body.replace(oldPlantUmlRegExp, '``` plantuml\n$1\n```');
+}
+
+function tsvProcessor(body) {
+  var oldTsvTableRegExp = /::: tsv(-h)?\n([\s\S]*?)\n:::/g; // TSV old format
+  return body.replace(oldTsvTableRegExp, '``` tsv$1\n$2\n```');
+}
+
+function csvProcessor(body) {
+  var oldCsvTableRegExp = /::: csv(-h)?\n([\s\S]*?)\n:::/g; // CSV old format
+  return body.replace(oldCsvTableRegExp, '``` csv$1\n$2\n```');
+}
+
+function replaceLatestRevisions(body, processors) {
+  var replacedBody = body;
+  processors.forEach((processor) => {
+    replacedBody = processor(body)
+  })
+  return replacedBody;
 }
 
 var pagesCollection = db.getCollection("pages");
@@ -22,9 +41,10 @@ pagesCollection.find({}).forEach((doc) => {
 
 var revisionsCollection = db.getCollection("revisions");
 var operations = [];
+var oldFormatProcessors = [drawioProcessor, plantumlProcessor, tsvProcessor, csvProcessor];
 
 revisionsCollection.find({ _id: { $in: revisionIds } }).forEach((doc) => {
-    var replacedBody = replaceBody(doc.body);
+    var replacedBody = replaceLatestRevisions(doc.body, oldFormatProcessors);
     var operation = {
         updateOne: {
             filter: {_id: doc._id},
