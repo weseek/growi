2ebc54715bc96b2e4b473369fc52aa0ee5b5a842
Taichi Masuyama <montanha.masu536@gmail.com>
2023-01-27 08:23:41 +0000

    add comment

--- packages/app/src/interfaces/questionnaire/questionnaire-answer-status.ts
+++ packages/app/src/interfaces/questionnaire/questionnaire-answer-status.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line max-len
+// see: https://dev.growi.org/6385911e1632aa30f4dae6a4#mdcont-%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC%E3%81%94%E3%81%A8%E3%81%AB%E5%9B%9E%E7%AD%94%E6%B8%88%E3%81%BF%E3%81%8B%E3%81%A9%E3%81%86%E3%81%8B%E3%82%92%E4%BF%9D%E5%AD%98%E3%81%99%E3%82%8B
+
 import { Types } from 'mongoose';
 
 export const StatusType = {
