7f0852b1978fdd8471f94d77d128d2dcdeb54d3c
Yuki Takei <yuki@weseek.co.jp>
2023-02-05 16:19:47 +0000

    add .preview-sidebar class

--- packages/app/src/client/services/layout.ts
+++ packages/app/src/client/services/layout.ts
@@ -1,17 +1,10 @@
+import type { IPage } from '~/interfaces/page';
 import { useIsContainerFluid } from '~/stores/context';
 import { useSWRxCurrentPage } from '~/stores/page';
 import { useEditorMode } from '~/stores/ui';
 
 export const useEditorModeClassName = (): string => {
   const { getClassNamesByEditorMode } = useEditorMode();
-
-  // TODO: Enable `editing-sidebar` class somehow
-  // https://redmine.weseek.co.jp/issues/111527
-  // const classNames: string[] = [];
-  // if (currentPage != null) {
-  //   const isSidebar = currentPage.path === '/Sidebar';
-  //   classNames.push(...getClassNamesByEditorMode(/* isSidebar */));
-  // }
 
   return `${getClassNamesByEditorMode().join(' ') ?? ''}`;
 };
