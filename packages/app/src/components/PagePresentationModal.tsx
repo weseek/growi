5ff1153d985bf8796cb07d2d0d7d6999a4a82c26
Yuki Takei <yuki@weseek.co.jp>
2023-02-07 06:33:53 +0000

    WIP: render with ReactMarkdown

--- packages/app/src/components/PagePresentationModal.tsx
+++ packages/app/src/components/PagePresentationModal.tsx
@@ -1,27 +1,45 @@
 import React from 'react';
 
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 import {
   Modal, ModalBody,
 } from 'reactstrap';
 
 import { usePagePresentationModal } from '~/stores/modal';
+import { useSWRxCurrentPage } from '~/stores/page';
+import { usePresentationViewOptions } from '~/stores/renderer';
 
 import styles from './PagePresentationModal.module.scss';
 
 const PagePresentationModal = (): JSX.Element => {
 
-  const { data, close: closePresentationModal } = usePagePresentationModal();
+  const { data: presentationModalData, close: closePresentationModal } = usePagePresentationModal();
+
+  const { data: currentPage } = useSWRxCurrentPage();
+  const { data: rendererOptions } = usePresentationViewOptions();
+
+  const isOpen = presentationModalData?.isOpened ?? false;
+
+  if (!isOpen) {
+    return <></>;
+  }
+
+  const markdown = currentPage?.revision.body;
 
   return (
     <Modal
-      isOpen={data?.isOpened}
+      isOpen={isOpen}
       toggle={closePresentationModal}
       data-testid="page-presentation-modal"
       className={`grw-presentation-modal ${styles['grw-presentation-modal']} grw-body-only-modal-expanded`}
       unmountOnClose={false}
     >
       <ModalBody className="modal-body">
-        (TBD)
+        { markdown != null && (
+          <ReactMarkdown {...rendererOptions}>
+            {markdown}
+          </ReactMarkdown>
+        )}
       </ModalBody>
     </Modal>
   );
