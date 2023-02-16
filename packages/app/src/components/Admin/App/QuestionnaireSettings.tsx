d52b937cfe12848a1977cec1aa06d9859ed1d286
Futa Arai <arafubeatbox@gmail.com>
2023-02-16 00:15:15 +0000

    refs 114888: refactor

--- packages/app/src/components/Admin/App/QuestionnaireSettings.tsx
+++ packages/app/src/components/Admin/App/QuestionnaireSettings.tsx
@@ -31,7 +31,7 @@ const QuestionnaireSettings = (): JSX.Element => {
         isQuestionnaireEnabled,
         isAppSiteUrlHashed,
       });
-      toastSuccess(t('commons:toaster.update_successed', { target: t('admin:app_setting.questionnaire_settings') }));
+      toastSuccess(t('commons:toaster.update_successed', { target: t('app_setting.questionnaire_settings') }));
     }
     catch (err) {
       toastError(err);
@@ -50,16 +50,16 @@ const QuestionnaireSettings = (): JSX.Element => {
   return (
     <div id="questionnaire-settings" className="mb-5">
       <p className="card well">
-        <div className="mb-4">{t('admin:app_setting.questionnaire_settings_explanation')}</div>
+        <div className="mb-4">{t('app_setting.questionnaire_settings_explanation')}</div>
         <span>
           <div className="mb-2">
-            <span className="text-info mr-2"><i className="icon-info icon-fw"></i>{t('admin:app_setting.about_data_sent')}</span>
-            <a href={t('admin:app_setting.docs_link')} rel="noreferrer" target="_blank" className="d-inline">
-              {t('admin:app_setting.learn_more')} <i className="icon-share-alt"></i>
+            <span className="text-info mr-2"><i className="icon-info icon-fw"></i>{t('app_setting.about_data_sent')}</span>
+            <a href={t('app_setting.docs_link')} rel="noreferrer" target="_blank" className="d-inline">
+              {t('app_setting.learn_more')} <i className="icon-share-alt"></i>
             </a>
           </div>
-          {t('admin:app_setting.other_info_will_be_sent')}<br />
-          {t('admin:app_setting.we_will_use_the_data_to_improve_growi')}
+          {t('app_setting.other_info_will_be_sent')}<br />
+          {t('app_setting.we_will_use_the_data_to_improve_growi')}
         </span>
       </p>
 
@@ -79,7 +79,7 @@ const QuestionnaireSettings = (): JSX.Element => {
                 onChange={onChangeIsQuestionnaireEnabledHandler}
               />
               <label className="custom-control-label" htmlFor="isQuestionnaireEnabled">
-                {t('admin:app_setting.enable_questionnaire')}
+                {t('app_setting.enable_questionnaire')}
               </label>
             </div>
           </div>
@@ -95,10 +95,10 @@ const QuestionnaireSettings = (): JSX.Element => {
                 disabled={!isQuestionnaireEnabled}
               />
               <label className="custom-control-label" htmlFor="isAppSiteUrlHashed">
-                {t('admin:app_setting.anonymize_app_site_url')}
+                {t('app_setting.anonymize_app_site_url')}
               </label>
               <p className="form-text text-muted small">
-                {t('admin:app_setting.url_anonymization_explanation')}
+                {t('app_setting.url_anonymization_explanation')}
               </p>
             </div>
           </div>
