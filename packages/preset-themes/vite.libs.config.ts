44e692132164a8832f21e97a3619f144229518c6
Yuki Takei <yuki@weseek.co.jp>
2023-02-16 21:45:01 +0000

    clean preset-themes

--- packages/preset-themes/vite.libs.config.ts
+++ packages/preset-themes/vite.libs.config.ts
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    dts({ outputDir: 'types' }),
+  ],
   build: {
     outDir: 'dist/libs',
     copyPublicDir: false,
     lib: {
       entry: 'src/index.ts',
       name: 'preset-themes-libs',
-      formats: ['umd'],
+      formats: ['es', 'umd'],
+    },
+    rollupOptions: {
+      external: [
+      ],
     },
   },
 });
