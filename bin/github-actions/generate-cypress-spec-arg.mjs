577f7d4f8f320bf45155a61e1e7fa9d79ab814fd
Yuki Takei <yuki@weseek.co.jp>
2023-03-05 15:27:55 +0000

    add yargs as procudtion dependencies

--- bin/github-actions/generate-cypress-spec-arg.mjs
+++ bin/github-actions/generate-cypress-spec-arg.mjs
@@ -13,8 +13,8 @@
  *  => A1Z,A3Z,A5Z
  */
 
-const yargs = require('yargs/yargs');
-const { hideBin } = require('yargs/helpers');
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
 const argv = yargs(hideBin(process.argv)).argv;
 
