417450c39e1dd9f73f18c9fef2142d7d82a1c892
Yuki Takei <yuki@weseek.co.jp>
2023-03-05 12:13:48 +0000

    fix import errors

--- apps/app/src/components/PageComment/Comment.tsx
+++ apps/app/src/components/PageComment/Comment.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useState } from 'react';
 
 import type { IUser } from '@growi/core';
-import { pathUtils } from '@growi/core/dist/utils/page-utils';
+import * as pathUtils from '@growi/core/dist/utils/path-utils';
 import { UserPicture } from '@growi/ui/dist/components/User/UserPicture';
 import { format, parseISO } from 'date-fns';
 import { useTranslation } from 'next-i18next';
