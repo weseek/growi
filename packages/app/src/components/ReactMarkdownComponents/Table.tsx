a740883a84d6980bab2373c3a11229e8c3fb4a57
Shun Miyazawa <shun-m@weseek.co.jp>
2023-03-13 12:09:31 +0000

    Add data-line

--- packages/app/src/components/ReactMarkdownComponents/Table.tsx
+++ packages/app/src/components/ReactMarkdownComponents/Table.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 type TableProps = {
   children: React.ReactNode,
   className?: string
+  'data-line': number
 }
 
 export const Table = React.memo((props: TableProps): JSX.Element => {
@@ -10,7 +11,7 @@ export const Table = React.memo((props: TableProps): JSX.Element => {
   const { children, className } = props;
 
   return (
-    <table className={`${className}`}>
+    <table className={`${className}`} data-line={props['data-line']}>
       {children}
     </table>
   );
