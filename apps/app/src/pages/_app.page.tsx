68e9e250fc5c303f8016caf5ba3e54cdf8b5de0e
Yuki Takei <yuki@weseek.co.jp>
2023-03-16 12:32:22 +0000

    build styles-prebuilt with turbo

--- apps/app/src/pages/_app.page.tsx
+++ apps/app/src/pages/_app.page.tsx
@@ -17,10 +17,10 @@ import { swrGlobalConfiguration } from '~/utils/swr-utils';
 import { CommonProps } from './utils/commons';
 import { registerTransformerForObjectId } from './utils/objectid-transformer';
 
-import '^/dist/assets/vendor.css';
+import '~/styles/prebuilt/vendor.css';
 import '~/styles/font-icons.scss';
 import '~/styles/style-app.scss';
-import '^/dist/assets/apply-colors.css';
+import '~/styles/prebuilt/apply-colors.css';
 
 const isDev = process.env.NODE_ENV === 'development';
 
