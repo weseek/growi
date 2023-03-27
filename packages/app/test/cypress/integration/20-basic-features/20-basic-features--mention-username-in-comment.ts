d4e9014ed7b7f97c2535ca5c7251943aad6114f0
Mudana-Grune <mudana@gruneasia.info>
2023-03-27 18:38:24 +0800

    Create cypress test for mention username in comment
    https://youtrack.weseek.co.jp/issue/GW-7939
    - Add data-testid for page comment editor button and add comment button
    - Creat cypress spec file for mention username

---
+++ packages/app/test/cypress/integration/20-basic-features/20-basic-features--mention-username-in-comment.ts
@@ -0,0 +1,23 @@
+context('Mention username in comment', () => {
+  const ssPrefix = 'mention-username-';
+
+  beforeEach(() => {
+    // login
+    cy.fixture("user-admin.json").then(user => {
+      cy.login(user.username, user.password);
+    });
+  });
+
+  it('/Successfully open comment editor', () => {
+
+    cy.visit('/Sandbox');
+    cy.waitUntilSkeletonDisappear();
+
+    cy.collapseSidebar(true, true);
+    cy.getByTestid('pageCommentButton').click();
+    cy.getByTestid('openCommentEditorButton').click();
+
+    cy.screenshot(`${ssPrefix}-`);
+  });
+});
+
