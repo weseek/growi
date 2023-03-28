f54d88a778669aede95e5fae11c88a3a0914f322
Mudana-Grune <mudana@gruneasia.info>
2023-03-28 16:42:16 +0800

    Implement waitUntil on open comment editor and mention

--- packages/app/test/cypress/integration/20-basic-features/20-basic-features--mention-username-in-comment.ts
+++ packages/app/test/cypress/integration/20-basic-features/20-basic-features--mention-username-in-comment.ts
@@ -6,20 +6,33 @@ context('Mention username in comment', () => {
     cy.fixture("user-admin.json").then(user => {
       cy.login(user.username, user.password);
     });
-  });
 
-  it('Successfully mention username in comment', () => {
-    const username = '@adm';
+    // Visit /Sandbox
     cy.visit('/Sandbox');
     cy.waitUntilSkeletonDisappear();
 
     cy.collapseSidebar(true, true);
-    cy.getByTestid('pageCommentButton').click();
-    cy.getByTestid('openCommentEditorButton').click();
 
-    cy.get('.Codemirror').type(username);
+    // Go to comment page
+    cy.getByTestid('pageCommentButton').click();
+
+    // Open comment editor
+    cy.waitUntil(() => {
+      // do
+      cy.getByTestid('openCommentEditorButton').click();
+      // wait until
+      return cy.get('.comment-write').then($elem => $elem.is(':visible'))
+    })
+
+  });
+
+  it('Successfully mention username in comment', () => {
+    const username = '@adm';
 
     cy.waitUntil(() => {
+      // do
+      cy.get('.Codemirror').type(username);
+      // wait until
       return cy.get('.Codemirror-hints').then($elem => $elem.is(':visible'));
     });
     cy.get('#comments-container').within(() => { cy.screenshot(`${ssPrefix}1-username-found`) });
@@ -27,16 +40,11 @@ context('Mention username in comment', () => {
 
   it('Username not found when mention username in comment', () => {
     const username = '@user';
-    cy.visit('/Sandbox');
-    cy.waitUntilSkeletonDisappear();
-
-    cy.collapseSidebar(true, true);
-    cy.getByTestid('pageCommentButton').click();
-    cy.getByTestid('openCommentEditorButton').click();
-
-    cy.get('.Codemirror').type(username);
 
     cy.waitUntil(() => {
+      // do
+      cy.get('.Codemirror').type(username);
+      // wait until
       return cy.get('.Codemirror-hints').then($elem => $elem.is(':visible'));
     });
     cy.get('#comments-container').within(() => { cy.screenshot(`${ssPrefix}2-username-not-found`) });
