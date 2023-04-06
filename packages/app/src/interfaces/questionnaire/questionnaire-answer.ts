335651ee20d30be0ae32a8eab5ca8af555679d5c
Futa Arai <arafubeatbox@gmail.com>
2023-04-06 11:49:11 +0000

    refs 119367: add questionnaireOrder to questionnareAnswer

--- packages/app/src/interfaces/questionnaire/questionnaire-answer.ts
+++ packages/app/src/interfaces/questionnaire/questionnaire-answer.ts
@@ -2,9 +2,10 @@ import { IAnswer } from './answer';
 import { IGrowiInfo } from './growi-info';
 import { IUserInfo } from './user-info';
 
-export interface IQuestionnaireAnswer {
+export interface IQuestionnaireAnswer<ID = string> {
   answers: IAnswer[]
   answeredAt: Date
   growiInfo: IGrowiInfo
   userInfo: IUserInfo
+  questionnaireOrder: ID
 }
