9e87bdddacd1e018c3ef13bd4b5b8148faa529f7
ryoji-s <ryoji-s@weseek.co.jp>
2023-04-13 00:59:11 +0000

    add sanitize for registration white list

--- packages/app/src/client/services/AdminLocalSecurityContainer.js
+++ packages/app/src/client/services/AdminLocalSecurityContainer.js
@@ -1,6 +1,7 @@
 import { isServer } from '@growi/core';
 import { Container } from 'unstated';
 
+import Xss from '~/services/xss';
 import loggerFactory from '~/utils/logger';
 
 import { apiv3Get, apiv3Put } from '../util/apiv3-client';
@@ -23,6 +24,7 @@ export default class AdminLocalSecurityContainer extends Container {
     this.appContainer = appContainer;
     this.dummyRegistrationMode = 0;
     this.dummyRegistrationModeForError = 1;
+    this.xss = new Xss();
 
     this.state = {
       retrieveError: null,
@@ -56,13 +58,40 @@ export default class AdminLocalSecurityContainer extends Container {
 
   }
 
+  isValidEmailDomain(emailDomain) {
+    /**
+     * 1. Must start with an "@"
+     * 2. Domain name must be a-z | A-Z | 0-9 and hyphen (-)
+     * 3. Domain name length must be 1-63 characters
+     * 4. Last Tld must be at least 2 and no more than 6 characters and no hyphen (-)
+     * 5. Do not use hyphens (-) at the beginning or end of the domain name (e.g. -google.com or google-.com)
+     * 6. Domain name can be a subdomain
+     * see: https://regex101.com/r/4xc7lg/1
+     */
+    // eslint-disable-next-line regex/invalid
+    const pattern = /^@((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z0-9]{2,6}$/;
+    return emailDomain.match(pattern);
+  }
+
+  sanitizeRegistrationWhiteList(lines) {
+    const sanitizedLines = lines
+      .filter(line => line !== '')
+      .map((line) => {
+        const sanitizedLine = this.xss.process(line);
+        if (sanitizedLine !== line || !this.isValidEmailDomain(line)) {
+          throw new Error('The input to the white list contains an invalid character. Please enter it in a format such as @growi.org.');
+        }
+        return sanitizedLine;
+      });
+    return sanitizedLines;
+  }
+
   /**
    * Workaround for the mangling in production build to break constructor.name
    */
   static getClassName() {
     return 'AdminLocalSecurityContainer';
   }
-
 
   /**
    * Change registration mode
@@ -97,9 +126,12 @@ export default class AdminLocalSecurityContainer extends Container {
    */
   async updateLocalSecuritySetting() {
     const { registrationWhiteList, isPasswordResetEnabled, isEmailAuthenticationEnabled } = this.state;
+
+    const sanitizedRegistrationWhiteList = this.sanitizeRegistrationWhiteList(registrationWhiteList);
+
     const response = await apiv3Put('/security-setting/local-setting', {
       registrationMode: this.state.registrationMode,
-      registrationWhiteList,
+      registrationWhiteList: sanitizedRegistrationWhiteList,
       isPasswordResetEnabled,
       isEmailAuthenticationEnabled,
     });
@@ -115,6 +147,5 @@ export default class AdminLocalSecurityContainer extends Container {
 
     return localSettingParams;
   }
-
 
 }
