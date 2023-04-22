ebdeaf0c41a3a9a36d672742c2e5635a2b95183d
Futa Arai <arafubeatbox@gmail.com>
2023-04-22 03:17:50 +0000

    refs 120355: remove unnecessary files

--- packages/core/src/plugin/index.ts
+++ packages/core/src/plugin/index.ts
@@ -1,6 +1,2 @@
 export * from './interfaces/option-parser';
 export * from './util/option-parser';
-export * from './service/tag-cache-manager';
-export * from './util/custom-tag-utils';
-export * from './util/args-parser';
-export * from './model/tag-context';
