d219ae3a8f37e73765df9d45ceb93ef3d3f65d47
reiji-h <reiji-h@weseek.co.jp>
2023-05-10 04:17:16 +0000

    change from EnvVarsTable.jsx to EnvVarsTable.tsx

--- apps/app/src/components/Admin/AdminHome/EnvVarsTable.tsx
+++ apps/app/src/components/Admin/AdminHome/EnvVarsTable.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+
 import PropTypes from 'prop-types';
 
-const EnvVarsTable = (props) => {
-  const envVarRows = [];
+type EnvVarsTableProps = {
+  envVars: object,
+}
+
+const EnvVarsTable: React.FC<EnvVarsTableProps> = (props: EnvVarsTableProps) => {
+  const envVarRows: JSX.Element[] = [];
 
   for (const [key, value] of Object.entries(props.envVars)) {
     if (value != null) {
@@ -22,7 +27,6 @@ const EnvVarsTable = (props) => {
       </tbody>
     </table>
   );
-
 };
 
 EnvVarsTable.propTypes = {
