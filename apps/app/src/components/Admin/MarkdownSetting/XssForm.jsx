a624b937af6d6e507cee4d6fef3b23195fdd601c
soumaeda <souma-e@weseek.co.jp>
2023-05-11 07:07:51 +0000

    121773 fix Whitelist.jsx

--- apps/app/src/components/Admin/MarkdownSetting/XssForm.jsx
+++ apps/app/src/components/Admin/MarkdownSetting/XssForm.jsx
@@ -12,7 +12,7 @@ import loggerFactory from '~/utils/logger';
 import { withUnstatedContainers } from '../../UnstatedUtils';
 import AdminUpdateButtonRow from '../Common/AdminUpdateButtonRow';
 
-import WhitelistInput from './WhitelistInput';
+import WhitelistInput from './WhiteIistInput';
 
 const logger = loggerFactory('growi:importer');
 
