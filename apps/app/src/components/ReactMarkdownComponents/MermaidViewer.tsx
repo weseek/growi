b6eee8d62ed3c00b65a0ac56382d89426ad4ef21
Shun Miyazawa <shun-m@weseek.co.jp>
2023-05-12 09:40:39 +0000

    Create MermaidViewer

---
+++ apps/app/src/components/ReactMarkdownComponents/MermaidViewer.tsx
@@ -0,0 +1,27 @@
+import { useRef, useEffect, ReactNode } from 'react';
+
+import mermaid from 'mermaid';
+
+type MermaidViewerProps = {
+  children: ReactNode,
+}
+
+export const MermaidViewer = (props: MermaidViewerProps): JSX.Element => {
+  const { children } = props;
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (ref.current != null && children != null) {
+      mermaid.init({}, ref.current);
+    }
+  }, [children]);
+
+  return (
+    children
+      ? <div ref={ref} key={children as string}>
+        {children}
+      </div>
+      : <div key={children as string} />
+  );
+};
