11237c2ee6ab65bc9759f174ea364e1a8899b5f0
soumaeda <souma-e@weseek.co.jp>
2023-05-16 04:01:36 +0000

    121773 fix brank

--- apps/app/src/components/Admin/MarkdownSetting/XssForm.jsx
+++ apps/app/src/components/Admin/MarkdownSetting/XssForm.jsx
@@ -12,7 +12,6 @@ import loggerFactory from '~/utils/logger';
 import { withUnstatedContainers } from '../../UnstatedUtils';
 import AdminUpdateButtonRow from '../Common/AdminUpdateButtonRow';
 
-
 import WhitelistInput from './WhitelistInput';
 
 const logger = loggerFactory('growi:importer');
