4a88694ef82ea663baa101b3c24755388abddd5c
Yuki Takei <yuki@weseek.co.jp>
2023-05-23 11:44:33 +0000

    clean .eslintrc.js for models

--- apps/app/src/server/models/.eslintrc.js
+++ apps/app/src/server/models/.eslintrc.js
@@ -3,17 +3,7 @@ const rulesDirPlugin = require('eslint-plugin-rulesdir');
 rulesDirPlugin.RULES_DIR = 'src/server/models/eslint-rules-dir';
 
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 13,
-    sourceType: 'module',
-  },
   plugins: [
-    '@typescript-eslint',
     'rulesdir',
   ],
   rules: {
