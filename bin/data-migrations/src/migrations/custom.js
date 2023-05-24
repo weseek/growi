62705c6ca12f26c284970a387b83702b98eb3a53
Shun Miyazawa <m0zurillex@gmail.com>
2023-05-24 14:20:49 +0000

    remove unnecessary comment

--- bin/data-migrations/src/migrations/custom.js
+++ bin/data-migrations/src/migrations/custom.js
@@ -1,15 +1,6 @@
-
-/* eslint-disable no-undef, no-var, vars-on-top, no-restricted-globals, regex/invalid */
-// ignore lint error because this file is js as mongoshell
-
-// ===========================================
-// processors for old format
-// ===========================================
-
-// processor for MIGRATION_MODULE=custom
-
 module.exports = [
   (body) => {
+    // processor for MIGRATION_MODULE=custom
     // ADD YOUR PROCESS HERE!
     // https://github.com/weseek/growi/discussions/7180
     return body;
