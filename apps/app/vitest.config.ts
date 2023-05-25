9820d4b4f10f29c5405a508bc2cf2db9dc9aad71
Yuki Takei <yuki@weseek.co.jp>
2023-05-25 20:30:12 +0000

    add vite-tsconfig-paths

--- apps/app/vitest.config.ts
+++ apps/app/vitest.config.ts
@@ -1,6 +1,10 @@
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineProject } from 'vitest/config';
 
 export default defineProject({
+  plugins: [
+    tsconfigPaths(),
+  ],
   test: {
     environment: 'node',
     exclude: ['**/test/**'],
