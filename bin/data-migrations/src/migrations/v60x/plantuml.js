8458d8936bc16c98aa396c8cb12e0e21773c0058
Shun Miyazawa <m0zurillex@gmail.com>
2023-05-25 11:31:35 +0000

    apply jsdoc

--- bin/data-migrations/src/migrations/v60x/plantuml.js
+++ bin/data-migrations/src/migrations/v60x/plantuml.js
@@ -1,4 +1,11 @@
+/**
+ * @typedef {import('../../types').MigrationModule} MigrationModule
+ */
+
 module.exports = [
+  /**
+   * @type {MigrationModule}
+   */
   (body) => {
     const oldDrawioRegExp = /:::\s?drawio\n(.+?)\n:::/g; // drawio old format
     return body.replace(oldDrawioRegExp, '``` drawio\n$1\n```');
