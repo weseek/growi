cdc52dc13c183f18638e2746bbb6909452bc6bcd
Yuki Takei <yuki@weseek.co.jp>
2023-05-28 18:29:22 +0000

    specify mongodb version with env var

--- apps/app/test-with-vite/setup/mongoms.ts
+++ apps/app/test-with-vite/setup/mongoms.ts
@@ -5,8 +5,18 @@ import { mongoOptions } from '../../src/server/util/mongoose-utils';
 
 
 beforeAll(async() => {
+  // set debug flag
+  process.env.MONGOMS_DEBUG = process.env.VITE_MONGOMS_DEBUG;
+
+  // set version
   const mongoServer = await MongoMemoryServer.create({
-    instance: { dbName: 'growi_test' },
+    instance: {
+      dbName: 'growi_test',
+    },
+    binary: {
+      version: process.env.VITE_MONGOMS_VERSION,
+      downloadDir: 'node_modules/.cache/mongodb-binaries',
+    },
   });
   await mongoose.connect(mongoServer.getUri(), mongoOptions);
 });
