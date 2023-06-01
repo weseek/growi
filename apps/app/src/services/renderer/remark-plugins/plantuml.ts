9e751fd39ba41621b9bd10d69af8343a82b301e5
Mudana-Grune <mudana@gruneasia.info>
2023-06-01 21:22:22 +0800

    Modify plantuml plugin
    https://youtrack.weseek.co.jp/issue/GW-7959
    - Wrap plantUml image inside p tag
    - TO DO: add data-line attribute to paragraph tag

--- apps/app/src/services/renderer/remark-plugins/plantuml.ts
+++ apps/app/src/services/renderer/remark-plugins/plantuml.ts
@@ -1,15 +1,34 @@
 import plantuml from '@akebifiky/remark-simple-plantuml';
 import { Plugin } from 'unified';
+import { Node } from 'unist';
 import urljoin from 'url-join';
 
 type PlantUMLPluginParams = {
-  plantumlUri: string,
-}
+  plantumlUri: string;
+};
 
 export const remarkPlugin: Plugin<[PlantUMLPluginParams]> = (options) => {
   const plantumlUri = options.plantumlUri;
-
   const baseUrl = urljoin(plantumlUri, '/svg');
 
-  return plantuml.bind(this)({ baseUrl });
+  return (tree: Node) => {
+    const result = plantuml({ baseUrl })(tree);
+
+    const children = result.children.map((child: Node) => {
+      const modifiedTree = {
+        type: 'paragraph',
+        children: [child],
+        properties: {
+          'data-line': child.position?.start.line,
+        },
+      };
+      return modifiedTree;
+    });
+    const wrappedTree = {
+      type: 'root',
+      children
+    };
+
+    return wrappedTree;
+  };
 };
