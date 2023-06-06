3b3d35b66249a095683c6729c9479bfb685f107c
soumaeda <souma-e@weseek.co.jp>
2023-06-06 13:01:09 +0000

    123684 remove comment

--- apps/app/src/components/Admin/Users/PasswordResetModal.jsx
+++ apps/app/src/components/Admin/Users/PasswordResetModal.jsx
@@ -24,7 +24,6 @@ class PasswordResetModal extends React.Component {
     this.resetPassword = this.resetPassword.bind(this);
   }
 
-  // Reset Password
   async resetPassword() {
     const { t, userForPasswordResetModal } = this.props;
     try {
