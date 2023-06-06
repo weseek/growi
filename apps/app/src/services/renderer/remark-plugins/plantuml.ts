cde84f560d5557d3453f565b4477aa393fdb3595
Mudana-Grune <mudana@gruneasia.info>
2023-06-06 18:19:00 +0800

    Add data-line property to plantUML image wrapper
    https://youtrack.weseek.co.jp/issue/GW-7959
    - Change tree.children loop with map
    - Add condition to check lang and value of child
    - Add data-line attribute with hProperties
    - Add condition to apply hProperties value if line not null

--- apps/app/src/services/renderer/remark-plugins/plantuml.ts
+++ apps/app/src/services/renderer/remark-plugins/plantuml.ts
@@ -12,26 +12,31 @@ export const remarkPlugin: Plugin<[PlantUMLPluginParams]> = (options) => {
   const baseUri = urljoin(plantumlUri);
 
   return (tree: Parent) => {
-    const modifiedChildren: Node[] = [];
-
-    tree.children.forEach((child: Node) => {
-      console.log(child)
-      if (child.lang === 'plantuml') {
+    const modifiedChildren: Node[] = tree.children.map((child: Node) => {
+      const { lang, value, position } = child;
+      // Check if the node is a 'plantuml' code block and has a non-empty value
+      if (lang === 'plantuml' && value !== '') {
+        const line = position?.start?.line;
         const modifiedChild: Parent = {
           type: 'paragraph',
           children: [child],
+          data: {
+            // Add 'data-line' attribute to the paragraph's hProperties
+            hProperties: line ? { 'data-line': line } : {},
+          },
         };
-        modifiedChildren.push(modifiedChild);
-      } else {
-        modifiedChildren.push(child);
+        return modifiedChild; // Return the modified child node
       }
+      return child; // Return the original child node
     });
 
+    // Create the wrapped tree with modified children
     const wrappedTree: Parent = {
       type: 'root',
       children: modifiedChildren,
     };
 
+    // Apply the plantuml plugin to the wrapped tree
     return plantuml({ baseUri })(wrappedTree);
   };
 };
