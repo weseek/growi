da943486b40ddf425af361d058206dbbe5baba39
soumaeda <souma-e@weseek.co.jp>
2023-06-06 11:12:44 +0000

    123684

---
+++ packages/micromark-extension-gfm-table/dev/lib/html.d.ts
@@ -0,0 +1,8 @@
+/**
+ * HTML extension for micromark (passed in `htmlExtensions`).
+ *
+ * @type {HtmlExtension}
+ */
+export const gfmTableHtml: HtmlExtension;
+export type HtmlExtension = import('micromark-util-types').HtmlExtension;
+export type Align = import('./syntax.js').Align;
---
+++ packages/micromark-extension-gfm-table/dev/lib/syntax.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Syntax extension for micromark (passed in `extensions`).
+ *
+ * @type {Extension}
+ */
+export const gfmTable: Extension;
+export type Extension = import('micromark-util-types').Extension;
+export type Resolver = import('micromark-util-types').Resolver;
+export type Tokenizer = import('micromark-util-types').Tokenizer;
+export type State = import('micromark-util-types').State;
+export type Token = import('micromark-util-types').Token;
+export type Align = 'left' | 'center' | 'right' | 'none';
