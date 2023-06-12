691b66f6c9b621e4a28722d14e15bba5269684df
Yuki Takei <yuki@weseek.co.jp>
2023-06-12 13:36:22 +0000

    Revert "Merge pull request #7796 from weseek/imprv/suppress-unnecessary-bookmark-api-requests"
    This reverts commit a9f8ddc67034b671acca00726f972ab0f2014e26, reversing
    changes made to 4e3a2dea3797a716340376c013da9863d1a4d078.

--- apps/app/src/stores/bookmark.ts
+++ apps/app/src/stores/bookmark.ts
@@ -17,8 +17,6 @@ export const useSWRBookmarkInfo = (pageId: string | null | undefined): SWRRespon
         pageId: response.data.pageId,
       };
     }),
-    // supress unnecessary API requests when using for mutation purposes
-    { revalidateOnMount: false },
   );
 };
 
