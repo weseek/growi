de6e8e2124b66e70b41793a37f5c2c717b529a65
Futa Arai <arafubeatbox@gmail.com>
2023-06-18 23:06:22 +0000

    refs 124384: add comments

--- apps/app/src/server/service/external-group/external-user-group-sync-service.ts
+++ apps/app/src/server/service/external-group/external-user-group-sync-service.ts
@@ -57,7 +57,7 @@ abstract class ExternalUserGroupSyncService {
 
   /** External user group node sync method
    * 1. Create/Update ExternalUserGroup from using information of ExternalUserGroupTreeNode
-   * 2. For every element in node.users, call getMemberUser and create an ExternalUserGroupRelation with ExternalUserGroup if it does not have one
+   * 2. For every element in node.userInfos, call getMemberUser and create an ExternalUserGroupRelation with ExternalUserGroup if it does not have one
    * 3. Retrun ExternalUserGroup
    * @param {string} node Node of external group tree
    * @param {string} parentId Parent group id (id in GROWI) of the group we wan't to create/update
@@ -89,9 +89,8 @@ abstract class ExternalUserGroupSyncService {
   }
 
   /** Method to get group member GROWI user
-   * 1. If externalUserInfo is an id, execute search on external app/server for user info. If it is full user info, use it as it is in 2.
-   * 2. Search for GROWI user based on user info of 1, and return user
-   *   - if autoGenerateUserOnHogeGroupSync is true and GROWI user is not found, create new GROWI user
+   * 1. Search for GROWI user based on user info of 1, and return user
+   * 2. If autoGenerateUserOnHogeGroupSync is true and GROWI user is not found, create new GROWI user
    * @param {ExternalUserInfo} externalUserInfo Search external app/server using this identifier
    * @returns {Promise<IUserHasId | null>} User when found or created, null when neither
    */
@@ -117,7 +116,6 @@ abstract class ExternalUserGroupSyncService {
   /** Method to generate external group tree structure
    * 1. Fetch user group info from external app/server
    * 2. Convert each group tree structure to ExternalUserGroupTreeNode
-   *   - Store the full user info in externalUserInfos if possible. Else just store the id and leave the user info fetching to getMemberUser.
    * 3. Return the root node of each tree
   */
   abstract generateExternalUserGroupTrees(): Promise<ExternalUserGroupTreeNode[]>
