91a4b0680bac8ef2246d3518e7ddba1126471a9c
Yuki Takei <yuki@weseek.co.jp>
2023-06-22 17:35:08 +0000

    enable tsconfig paths

--- packages/pluginkit/vite.config.ts
+++ packages/pluginkit/vite.config.ts
@@ -1,13 +1,16 @@
 import path from 'path';
 
+
 import glob from 'glob';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     dts(),
+    tsconfigPaths(),
   ],
   build: {
     outDir: 'dist',
