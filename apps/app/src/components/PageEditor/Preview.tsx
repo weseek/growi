8056228609eb9e60d1f54614a192fb36656a7fdb
reiji-h <reiji-h@weseek.co.jp>
2023-06-28 01:55:37 +0000

    enable isSlidesOverviewEnabled

--- apps/app/src/components/PageEditor/Preview.tsx
+++ apps/app/src/components/PageEditor/Preview.tsx
@@ -32,7 +32,7 @@ const Preview = React.forwardRef((props: Props, ref: RefObject<HTMLDivElement>):
       }}
     >
       { markdown != null && (
-        <RevisionRenderer rendererOptions={rendererOptions} markdown={markdown}></RevisionRenderer>
+        <RevisionRenderer rendererOptions={rendererOptions} markdown={markdown} isSlidesOverviewEnabled></RevisionRenderer>
       ) }
     </div>
   );
