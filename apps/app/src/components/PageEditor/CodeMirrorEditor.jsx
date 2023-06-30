76b2a7d87998e55ecd5e8225b1111270fa3ff1f0
Mudana-Grune <mudana@gruneasia.info>
2023-06-30 17:18:43 +0800

    Revert "Fold plantUml section"
    This reverts commit c2f8ec06f64ce48a1c229851aa7a5c63a13ff1e1.

--- apps/app/src/components/PageEditor/CodeMirrorEditor.jsx
+++ apps/app/src/components/PageEditor/CodeMirrorEditor.jsx
@@ -32,7 +32,6 @@ import mtu from './MarkdownTableUtil';
 import pasteHelper from './PasteHelper';
 import PreventMarkdownListInterceptor from './PreventMarkdownListInterceptor';
 import SimpleCheatsheet from './SimpleCheatsheet';
-import markdownPlantUmlUtils from './MarkdownPlantUMLUtil';
 
 import styles from './CodeMirrorEditor.module.scss';
 
@@ -153,7 +152,6 @@ class CodeMirrorEditor extends AbstractEditor {
     // this.showGridEditorHandler = this.showGridEditorHandler.bind(this);
 
     this.foldDrawioSection = this.foldDrawioSection.bind(this);
-    this.foldPlantUMLSection = this.foldPlantUMLSection.bind(this);
     this.clickDrawioIconHandler = this.clickDrawioIconHandler.bind(this);
     this.clickTableIconHandler = this.clickTableIconHandler.bind(this);
 
@@ -185,7 +183,6 @@ class CodeMirrorEditor extends AbstractEditor {
 
     // fold drawio section
     this.foldDrawioSection();
-    this.foldPlantUMLSection();
 
     // initialize commentMentionHelper if comment editor is opened
     if (this.props.isComment) {
@@ -207,7 +204,6 @@ class CodeMirrorEditor extends AbstractEditor {
 
     // fold drawio section
     this.foldDrawioSection();
-    this.foldPlantUMLSection();
   }
 
   initializeEditorSettings(editorSettings) {
@@ -870,14 +866,6 @@ class CodeMirrorEditor extends AbstractEditor {
   foldDrawioSection() {
     const editor = this.getCodeMirror();
     const lineNumbers = mdu.findAllDrawioSection(editor);
-    lineNumbers.forEach((lineNumber) => {
-      editor.foldCode({ line: lineNumber, ch: 0 }, { scanUp: false }, 'fold');
-    });
-  }
-
-  foldPlantUMLSection() {
-    const editor = this.getCodeMirror();
-    const lineNumbers = markdownPlantUmlUtils.findAllPlantUMLSections(editor);
     lineNumbers.forEach((lineNumber) => {
       editor.foldCode({ line: lineNumber, ch: 0 }, { scanUp: false }, 'fold');
     });
--- apps/app/src/services/renderer/remark-plugins/plantuml.ts
+++ apps/app/src/services/renderer/remark-plugins/plantuml.ts
@@ -1,5 +1,6 @@
 import plantuml from '@akebifiky/remark-simple-plantuml';
 import { Plugin } from 'unified';
+import { Parent, Node } from 'unist';
 import urljoin from 'url-join';
 
 type PlantUMLPluginParams = {
@@ -8,8 +9,36 @@ type PlantUMLPluginParams = {
 
 export const remarkPlugin: Plugin<[PlantUMLPluginParams]> = (options) => {
   const plantumlUri = options.plantumlUri;
-
   const baseUrl = urljoin(plantumlUri, '/svg');
 
-  return plantuml.bind(this)({ baseUrl });
+  return (tree: Parent) => {
+    const modifiedChildren: Node[] = tree.children.map((child: Node) => {
+      const { lang, value, position } = child;
+      // Check if the node is a 'plantuml' code block and has a non-empty value
+      if (!lang || lang !== 'plantuml' || !value) {
+        return child; // Return early if the conditions are not met
+      }
+
+      const line = position?.start?.line;
+      const modifiedChild: Parent = {
+        type: 'paragraph',
+        children: [child],
+        data: {
+          // Add 'data-line' attribute to the paragraph's hProperties
+          hProperties: line ? { 'data-line': line } : {},
+        },
+      };
+
+      return modifiedChild; // Return the modified child node
+    });
+
+    // Create the wrapped tree with modified children
+    const wrappedTree: Parent = {
+      type: 'root',
+      children: modifiedChildren,
+    };
+
+    // Apply the plantuml plugin to the wrapped tree
+    return plantuml({ baseUrl })(wrappedTree);
+  };
 };
