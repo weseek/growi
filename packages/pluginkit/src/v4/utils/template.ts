eeec6d67239ce79e4192eec7b46d6252125656bc
ryoji-s <ryoji-s@weseek.co.jp>
2023-07-06 02:12:36 +0000

    update utils template

--- packages/pluginkit/src/v4/utils/template.ts
+++ packages/pluginkit/src/v4/utils/template.ts
@@ -1,11 +1,23 @@
+import { Lang } from '@growi/core';
+
 import type { TemplateSummary, TemplateStatus } from '../interfaces';
 
-export const getLocalizedTemplate = (templateSummary: TemplateSummary | undefined, locale?: string): TemplateStatus | undefined => {
+export const getLocalizedTemplate = (templateSummary: TemplateSummary | undefined, locale?: string, usersDefaultLang?: Lang): TemplateStatus | undefined => {
   if (templateSummary == null) {
     return undefined;
   }
 
-  return locale != null
-    ? templateSummary[locale]
+  const selectedLocale = usersDefaultLang ?? locale;
+
+  return selectedLocale != null && selectedLocale in templateSummary
+    ? templateSummary[selectedLocale]
     : templateSummary.default;
+};
+
+export const getTemplateLocales = (templateSummary: TemplateSummary | undefined): Set<string> | undefined => {
+  if (templateSummary == null) {
+    return undefined;
+  }
+
+  return new Set(Object.values(templateSummary).map(s => s.locale));
 };
