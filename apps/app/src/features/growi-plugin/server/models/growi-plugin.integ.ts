8d8b53d4dc43e12ccfcca49ff669afe4d790a2e8
Yuki Takei <yuki@weseek.co.jp>
2023-07-08 19:37:00 +0000

    add tests for GrowiPlugin

--- apps/app/src/features/growi-plugin/server/models/growi-plugin.integ.ts
+++ apps/app/src/features/growi-plugin/server/models/growi-plugin.integ.ts
@@ -57,7 +57,11 @@ describe('GrowiPlugin find methods', () => {
     ]);
   });
 
-  describe('.findEnabledPlugins', () => {
+  afterAll(async() => {
+    await GrowiPlugin.deleteMany({});
+  });
+
+  describe.concurrent('.findEnabledPlugins', () => {
     it('shoud returns documents which isEnabled is true', async() => {
       // when
       const results = await GrowiPlugin.findEnabledPlugins();
@@ -71,7 +75,7 @@ describe('GrowiPlugin find methods', () => {
     });
   });
 
-  describe('.findEnabledPluginsByType', () => {
+  describe.concurrent('.findEnabledPluginsByType', () => {
     it("shoud returns documents which type is 'template'", async() => {
       // when
       const results = await GrowiPlugin.findEnabledPluginsByType(GrowiPluginType.Template);
@@ -81,6 +85,72 @@ describe('GrowiPlugin find methods', () => {
       // then
       expect(results.length === 1).toBeTruthy();
       expect(pluginNames.includes('@growi/growi-plugin-example2')).toBeTruthy();
+    });
+  });
+
+});
+
+
+describe('GrowiPlugin activate/deactivate', () => {
+
+  beforeAll(async() => {
+    await GrowiPlugin.insertMany([
+      {
+        isEnabled: false,
+        installedPath: 'weseek/growi-plugin-example1',
+        organizationName: 'weseek',
+        origin: {
+          url: 'https://github.com/weseek/growi-plugin-example1',
+        },
+        meta: {
+          name: '@growi/growi-plugin-example1',
+          types: [GrowiPluginType.Script],
+        },
+      },
+    ]);
+  });
+
+  afterAll(async() => {
+    await GrowiPlugin.deleteMany({});
+  });
+
+  describe('.activatePlugin', () => {
+    it('shoud update the property "isEnabled" to true', async() => {
+      // setup
+      const plugin = await GrowiPlugin.findOne({});
+      assert(plugin != null);
+
+      expect(plugin.isEnabled).toBeFalsy(); // isEnabled: false
+
+      // when
+      const result = await GrowiPlugin.activatePlugin(plugin._id);
+      const pluginAfterActivated = await GrowiPlugin.findOne({ _id: plugin._id });
+
+      // then
+      expect(result).toEqual('@growi/growi-plugin-example1'); // equals to meta.name
+      expect(pluginAfterActivated).not.toBeNull();
+      assert(pluginAfterActivated != null);
+      expect(pluginAfterActivated.isEnabled).toBeTruthy(); // isEnabled: true
+    });
+  });
+
+  describe('.deactivatePlugin', () => {
+    it('shoud update the property "isEnabled" to true', async() => {
+      // setup
+      const plugin = await GrowiPlugin.findOne({});
+      assert(plugin != null);
+
+      expect(plugin.isEnabled).toBeTruthy(); // isEnabled: true
+
+      // when
+      const result = await GrowiPlugin.deactivatePlugin(plugin._id);
+      const pluginAfterActivated = await GrowiPlugin.findOne({ _id: plugin._id });
+
+      // then
+      expect(result).toEqual('@growi/growi-plugin-example1'); // equals to meta.name
+      expect(pluginAfterActivated).not.toBeNull();
+      assert(pluginAfterActivated != null);
+      expect(pluginAfterActivated.isEnabled).toBeFalsy(); // isEnabled: false
     });
   });
 
