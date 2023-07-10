a2a9be2bc6b308e275ebf81cdd7d5e865e9c6df9
Futa Arai <arafubeatbox@gmail.com>
2023-07-10 11:22:03 +0900

    refs 126149: removeAllInvalidRelations test

--- apps/app/src/features/external-user-group/server/models/external-user-group-relation.integ.ts
+++ apps/app/src/features/external-user-group/server/models/external-user-group-relation.integ.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 
+import ExternalUserGroup from './external-user-group';
 import ExternalUserGroupRelation from './external-user-group-relation';
 
 // TODO: use actual user model after ~/server/models/user.js becomes importable in vitest
@@ -14,17 +15,33 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 describe('ExternalUserGroupRelation model', () => {
+  let user1;
   const userId1 = new mongoose.Types.ObjectId();
-  const groupId1 = new mongoose.Types.ObjectId();
-  const groupId2 = new mongoose.Types.ObjectId();
+
+  beforeAll(async() => {
+    user1 = await User.create({
+      _id: userId1, name: 'user1', username: 'user1', email: 'user1@example.com',
+    });
+  });
+
+  afterEach(async() => {
+    await ExternalUserGroup.deleteMany();
+    await ExternalUserGroupRelation.deleteMany();
+  });
 
   describe('createRelations', () => {
-    let user1;
+    const groupId1 = new mongoose.Types.ObjectId();
+    const groupId2 = new mongoose.Types.ObjectId();
 
     beforeAll(async() => {
-      user1 = await User.create({
-        _id: userId1, name: 'user1', username: 'user1', email: 'user1@example.com',
-      });
+      await ExternalUserGroup.insertMany([
+        {
+          _id: groupId1, name: 'test group 1', externalId: 'testExternalId', provider: 'testProvider',
+        },
+        {
+          _id: groupId2, name: 'test group 2', externalId: 'testExternalId', provider: 'testProvider',
+        },
+      ]);
     });
 
     it('creates relation for user', async() => {
@@ -34,6 +51,25 @@ describe('ExternalUserGroupRelation model', () => {
         return [relation.relatedGroup, relation.relatedUser];
       });
       expect(idCombinations).toStrictEqual([[groupId1, userId1], [groupId2, userId1]]);
+    });
+  });
+
+  describe('removeAllInvalidRelations', () => {
+    const groupId1 = new mongoose.Types.ObjectId();
+    const groupId2 = new mongoose.Types.ObjectId();
+
+    beforeAll(async() => {
+      await ExternalUserGroupRelation.createRelations([groupId1, groupId2], user1);
+    });
+
+    it('removes invalid relations', async() => {
+      const relationsBeforeRemoval = await ExternalUserGroupRelation.find();
+      expect(relationsBeforeRemoval.length).not.toBe(0);
+
+      await ExternalUserGroupRelation.removeAllInvalidRelations();
+
+      const relationsAfterRemoval = await ExternalUserGroupRelation.find();
+      expect(relationsAfterRemoval.length).toBe(0);
     });
   });
 });
