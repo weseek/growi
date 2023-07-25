0b3aa439459fc396a1a36bc2af64d17a2968e63b
Yuki Takei <yuki@weseek.co.jp>
2023-07-25 22:16:38 +0000

    set height and scrollPastEnd

--- packages/editor/src/components/CodemirrorEditor.tsx
+++ packages/editor/src/components/CodemirrorEditor.tsx
@@ -1,12 +1,19 @@
 import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
 import { languages } from '@codemirror/language-data';
+import { scrollPastEnd } from '@codemirror/view';
 import CodeMirror from '@uiw/react-codemirror';
+
+
+import style from './CodemirrorEditor.module.scss';
+
 
 export const CodemirrorEditor = (): JSX.Element => {
   return (
     <CodeMirror
+      className={`${style['codemirror-editor']}`}
       extensions={[
         markdown({ base: markdownLanguage, codeLanguages: languages }),
+        scrollPastEnd(),
       ]}
     />
   );
