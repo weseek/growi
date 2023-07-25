734b8d5427bc9be1b025034b43ba20a4cbaa2275
Yuki Takei <yuki@weseek.co.jp>
2023-07-25 19:39:30 +0000

    remove unnecessary files

---
+++ packages/editor/src/components/CodemirrorEditor.tsx
@@ -0,0 +1,7 @@
+export const CodemirrorEditor = (): JSX.Element => {
+  return (
+    <>
+      CodemirrorEditor
+    </>
+  );
+};
--- packages/editor/src/main.tsx
+++ packages/editor/src/main.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
-import './index.css';
+import { CodemirrorEditor } from './components/CodemirrorEditor';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <CodemirrorEditor />
   </React.StrictMode>,
 );
