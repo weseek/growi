f61c4b1e07833cdf39d6fad9d52ae1a8cc1143a9
Yuki Takei <yuki@weseek.co.jp>
2023-07-25 19:39:30 +0000

    clean files

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts(),
+  ],
 })
