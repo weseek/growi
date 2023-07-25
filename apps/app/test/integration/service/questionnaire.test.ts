6346465e02e1979b953fdf0001661ad99c0451db
Tatsuya Ise <tatsuya@weseek.co.jp>
2023-07-25 03:27:19 +0000

    refs #124935: Fix quistionnaire answer - Fix and add tests for questionnaire.

--- apps/app/test/integration/service/questionnaire.test.ts
+++ apps/app/test/integration/service/questionnaire.test.ts
@@ -30,6 +30,11 @@ describe('QuestionnaireService', () => {
     });
   });
 
+  afterAll(async() => {
+    delete process.env.FORCE_WIKI_MODE;
+    await crowi.setupConfigManager();
+  });
+
   describe('getGrowiInfo', () => {
     test('Should get correct GROWI info', async() => {
       const growiInfo = await crowi.questionnaireService.getGrowiInfo();
@@ -53,7 +58,7 @@ describe('QuestionnaireService', () => {
         deploymentType: 'growi-docker-compose',
         type: 'on-premise',
         version: crowi.version,
-        wikiType: 'open',
+        wikiType: 'closed',
       });
     });
 
@@ -70,6 +75,23 @@ describe('QuestionnaireService', () => {
         expect(growiInfo.appSiteUrlHashed).toBeTruthy();
       });
     });
+
+    test('WikiType is "open" when env value FORCE_WIKI_MODE is "public"', async() => {
+      process.env.FORCE_WIKI_MODE = 'public';
+      await crowi.setupConfigManager();
+
+      const growiInfo = await crowi.questionnaireService.getGrowiInfo();
+      expect(growiInfo.wikiType).toEqual('open');
+    });
+
+    test('WikiType is "close" when env value FORCE_WIKI_MODE is "private"', async() => {
+      process.env.FORCE_WIKI_MODE = 'private';
+      await crowi.setupConfigManager();
+
+      const growiInfo = await crowi.questionnaireService.getGrowiInfo();
+      expect(growiInfo.wikiType).toEqual('closed');
+    });
+
   });
 
   describe('getUserInfo', () => {
