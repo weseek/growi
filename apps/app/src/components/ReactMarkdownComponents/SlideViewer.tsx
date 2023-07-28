fc3986b6694b2c2a8dfc3fc147a678bd8c37e7ba
reiji-h <reiji-h@weseek.co.jp>
2023-07-28 02:10:22 +0000

    change marp container name

--- apps/app/src/components/ReactMarkdownComponents/SlideViewer.tsx
+++ apps/app/src/components/ReactMarkdownComponents/SlideViewer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { MARP_CONTAINER_CLASS_NAME } from '@growi/presentation';
 import dynamic from 'next/dynamic';
 import { ReactMarkdownOptions } from 'react-markdown/lib/react-markdown';
 
@@ -24,12 +25,16 @@ export const SlideViewer: React.FC<SlideViewerProps> = React.memo((props: SlideV
   const { data: rendererOptions } = usePresentationViewOptions();
 
   return (
-    <Slides
-      hasMarpFlag={marp === 'marp'}
-      options={{ rendererOptions: rendererOptions as ReactMarkdownOptions }}
-    >
-      {children}
-    </Slides>
+    <div className={`${MARP_CONTAINER_CLASS_NAME}`}>
+      <div className="slides">
+        <Slides
+          hasMarpFlag={marp === 'marp'}
+          options={{ rendererOptions: rendererOptions as ReactMarkdownOptions }}
+        >
+          {children}
+        </Slides>
+      </div>
+    </div>
   );
 });
 
