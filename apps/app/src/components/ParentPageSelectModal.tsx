45e4fa27d2c05503e1ee1ad171dd9fc8d8fe5407
WNomunomu <kohsei.nmr@gmail.com>
2023-08-03 09:14:10 +0000

    merge from master

--- apps/app/src/components/ParentPageSelectModal.tsx
+++ apps/app/src/components/ParentPageSelectModal.tsx
@@ -8,12 +8,12 @@ import { useParentPageSelectModal } from '~/stores/modal';
 
 export const ParentPageSelectModal = () => {
   const {
-    data, error, open, close,
+    data: parentPageSelectModalData,
+    open: openModal,
+    close: closeModal,
+    error,
   } = useParentPageSelectModal();
 
-  const {
-    data: parentPageSelectModalData, open: openModal, close: closeModal,
-  } = useParentPageSelectModal();
   const { isOpened } = parentPageSelectModalData;
 
   if (error) {
@@ -22,8 +22,32 @@ export const ParentPageSelectModal = () => {
 
   if (!isOpened) {
     // modalが開いていないときの処理
+    return <></>;
   }
-  else {
-    // modalが開いている時の処理
-  }
+
+  // modalが開いている時の処理
+  return (
+    <>
+      <Modal isOpen={isOpened} toggle={() => closeModal()}>
+        <ModalHeader>Modal title</ModalHeader>
+        <ModalBody>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui officia deserunt mollit anim id est laborum.
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary">
+          Do Something
+          </Button>{' '}
+          <Button color="secondary">
+          Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </>
+  );
 };
