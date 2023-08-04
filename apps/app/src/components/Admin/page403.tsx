14698bf9324ebc7e0da8afdb69dee55ae8c535ef
Shun Miyazawa <shun-m@weseek.co.jp>
2023-08-04 09:46:49 +0000

    add contents

---
+++ apps/app/src/components/Admin/page403.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import DefaultErrorPage from 'next/error';
+import { useTranslation } from 'react-i18next';
+
+
+export const Page403 = (): JSX.Element => {
+  const { t } = useTranslation('commons');
+
+  const errorMessage = t('forbidden_page.do_not_have_admin_permission', { ns: 'commons' });
+
+  return (
+    <>
+      <DefaultErrorPage statusCode={403} title={errorMessage} />
+    </>
+  );
+};
