a296f93187db863628c014acfb33702cc6cd7826
WNomunomu <kohsei.nmr@gmail.com>
2023-08-08 08:57:46 +0000

    remove unnecessary

--- apps/app/src/components/ParentPageSelectModal.tsx
+++ apps/app/src/components/ParentPageSelectModal.tsx
@@ -4,27 +4,15 @@ import {
   Modal, ModalHeader, ModalBody, ModalFooter, Button,
 } from 'reactstrap';
 
-import { useTargetAndAncestors, useIsGuestUser, useIsReadOnlyUser } from '~/stores/context';
 import { useParentPageSelectModal } from '~/stores/modal';
-import { useCurrentPagePath, useCurrentPageId } from '~/stores/page';
-
-import ItemsTree from './Sidebar/PageTree/ItemsTree';
 
 export const ParentPageSelectModal = (): JSX.Element => {
   const {
     data: parentPageSelectModalData,
     close: closeModal,
   } = useParentPageSelectModal();
-  const { data: isGuestUser } = useIsGuestUser();
-  const { data: isReadOnlyUser } = useIsReadOnlyUser();
-  const { data: currentPath } = useCurrentPagePath();
-  const { data: targetId } = useCurrentPageId();
-  const { data: targetAndAncestorsData } = useTargetAndAncestors();
 
   const isOpened = parentPageSelectModalData?.isOpened ?? false;
-
-  const targetPathOrId = targetId || currentPath;
-  const path = currentPath || '/';
 
   return (
     <Modal
