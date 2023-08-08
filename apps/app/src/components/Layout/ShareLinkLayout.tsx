0f43741dbafa66b84a654c5f833c1e7e51986a6c
Yuki Takei <yuki@weseek.co.jp>
2023-08-08 12:30:48 +0000

    omit GrowiNavbar

--- apps/app/src/components/Layout/ShareLinkLayout.tsx
+++ apps/app/src/components/Layout/ShareLinkLayout.tsx
@@ -3,7 +3,6 @@ import React, { ReactNode } from 'react';
 import dynamic from 'next/dynamic';
 
 import { useEditorModeClassName } from '../../client/services/layout';
-import { GrowiNavbar } from '../Navbar/GrowiNavbar';
 
 import { RawLayout } from './RawLayout';
 
@@ -22,7 +21,6 @@ export const ShareLinkLayout = ({ children }: Props): JSX.Element => {
 
   return (
     <RawLayout className={className}>
-      <GrowiNavbar isGlobalSearchHidden={true} />
 
       <div className="page-wrapper d-flex d-print-block">
         <div className="flex-fill mw-0">
