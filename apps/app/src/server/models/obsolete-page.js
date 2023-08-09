ea0548c3328363ccb1ad801f3ca63c171d1617c6
Futa Arai <arafubeatbox@gmail.com>
2023-08-09 15:47:11 +0900

    remove unnecessary validateCrowi

--- apps/app/src/server/models/obsolete-page.js
+++ apps/app/src/server/models/obsolete-page.js
@@ -331,7 +331,6 @@ export const getPageSchema = (crowi) => {
 
     let userGroups = [];
     if (user != null) {
-      validateCrowi();
       userGroups = await UserGroupRelation.findAllUserGroupIdsRelatedToUser(user);
     }
 
