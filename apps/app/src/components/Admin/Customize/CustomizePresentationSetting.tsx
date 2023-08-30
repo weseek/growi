242d20cc73d2ef66a498ff3af4bcb95f54b85cb6
Meiri Kikuta <meiri-k@weseek.co.jp>
2023-08-30 06:20:55 +0000

    imprv/115673-128224-create-presentationfile10.3

--- apps/app/src/components/Admin/Customize/CustomizePresentationSetting.tsx
+++ apps/app/src/components/Admin/Customize/CustomizePresentationSetting.tsx
@@ -37,24 +37,24 @@ const CustomizeFunctionSettingPresentation = (props: Props): JSX.Element => {
       <div className="form-group row">
         <div className="offset-md-3 col-md-6 text-left">
             optionId="isEnabledMarp"
-            label={t('admin:customize_settings.presentation_options.enable_marp')}
+            label={t('admin:customize_settings.function_options.enable_marp')}
             isChecked={adminCustomizeContainer.state.isEnabledMarp || false}
             onChecked={() => { adminCustomizeContainer.switchIsEnabledMarp() }}
           <p className="form-text text-muted">
-            {t('admin:customize_settings.presentation_options.enable_marp_desc')}
+            {t('admin:customize_settings.function_options.enable_marp_desc')}
             <br></br>
             <a
-              href={`${t('admin:customize_settings.presentation_options.marp_official_site_link')}`}
+              href={`${t('admin:customize_settings.function_options.marp_official_site_link')}`}
               target="_blank"
               rel="noopener noreferrer"
-            >{`${t('admin:customize_settings.presentation_options.marp_official_site')}`}
+            >{`${t('admin:customize_settings.function_options.marp_official_site')}`}
             </a>
             <br></br>
             <a
-              href={`${t('admin:customize_settings.presentation_options.marp_in_gorwi_link')}`}
+              href={`${t('admin:customize_settings.function_options.marp_in_gorwi_link')}`}
               target="_blank"
               rel="noopener noreferrer"
-            >{`${t('admin:customize_settings.presentation_options.marp_in_growi')}`}
+            >{`${t('admin:customize_settings.function_options.marp_in_growi')}`}
             </a>
           </p>
 
