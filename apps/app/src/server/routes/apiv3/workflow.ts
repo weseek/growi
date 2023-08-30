aa0473a9122db584db49581cebc3a5401f60b8e1
Shun Miyazawa <shun-m@weseek.co.jp>
2023-08-30 06:33:50 +0000

    update

--- apps/app/src/server/routes/apiv3/workflow.ts
+++ apps/app/src/server/routes/apiv3/workflow.ts
@@ -15,15 +15,24 @@ module.exports = (crowi: Crowi): Router => {
   const loginRequired = require('../../middlewares/login-required')(crowi, true);
 
 
-  // description: パスパラメーターで pageId を受け取り、対象ページに付随する workflow データを返却する
+  // description: workflowId を受け取り、対象の workflow document を返却する
+  router.get('/:workflowId', accessTokenParser, loginRequired, async(req: RequestWithUser, res: ApiV3Response) => {
+    const { workflowId } = req.params;
+
+    return res.apiv3();
+  });
+
+
+  // description: pageId を受け取り、対象ページに付随する workflow document を配列で返却する
   router.get('/list/:pageId', accessTokenParser, loginRequired, async(req: RequestWithUser, res: ApiV3Response) => {
     const { pageId } = req.params;
 
     return res.apiv3();
   });
 
+
   // description: Workflow.status が "inprogress" な Workflow document を作成, 作成された Workflow document を返却する
-  router.post('/', accessTokenParser, loginRequired, async(req: RequestWithUser, res: ApiV3Response) => {
+  router.post('/create', accessTokenParser, loginRequired, async(req: RequestWithUser, res: ApiV3Response) => {
     const {
       pageId,
       workflowName,
@@ -35,13 +44,20 @@ module.exports = (crowi: Crowi): Router => {
     return res.apiv3();
   });
 
-  // wokflow の更新
+
+  // description: workflowId と workflowTasks を受け取り、対象 workflow document の workflow.tasks の更新を行う
   router.post('/update', accessTokenParser, loginRequired, async(req: RequestWithUser, res: ApiV3Response) => {
+    const { workflowId, workflowTasks } = req.body;
+
     return res.apiv3();
   });
 
-  // workflow の削除
-  router.delete('/', accessTokenParser, loginRequired, async(req: RequestWithUser, res: ApiV3Response) => {
+
+  // description: approver のアクション ("approve", "remand" など) を行うためのエンドポイント。workflowId を受け取り、対象の workflow 内の自身の approver.status を更新する
+  router.post('/action', accessTokenParser, loginRequired, async(req: RequestWithUser, res: ApiV3Response) => {
+    const { workflowId } = req.body;
+    const { user } = req;
+
     return res.apiv3();
   });
 
