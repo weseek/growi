00ff174ee99b179db9dddab77fcc9490885db90d
Shun Miyazawa <shun-m@weseek.co.jp>
2023-08-30 04:34:21 +0000

    update

--- apps/app/src/server/routes/apiv3/workflow.ts
+++ apps/app/src/server/routes/apiv3/workflow.ts
@@ -1,66 +1,47 @@
-import { ErrorV3 } from '@growi/core/dist/models';
+import type { IUserHasId } from '@growi/core';
 import express, { Request, Router } from 'express';
-
-import loggerFactory from '~/utils/logger';
 
 import Crowi from '../../crowi';
 
 import { ApiV3Response } from './interfaces/apiv3-response';
 
-const logger = loggerFactory('growi:routes:apiv3:workflow');
 const router = express.Router();
 
-type FormRequest = Request & { form: any, logIn: any };
+type RequestWithUser = Request & { user?: IUserHasId }
 
 module.exports = (crowi: Crowi): Router => {
 
   const accessTokenParser = require('../../middlewares/access-token-parser')(crowi);
-  const loginRequiredStrictly = require('../../middlewares/login-required')(crowi);
+  const loginRequired = require('../../middlewares/login-required')(crowi, true);
 
-  /**
-   * @swagger
-   *
-   *  paths:
-   *    /workflow/list/{pageId}:
-   *      get:
-   *        tags: [Workflows]
-   *        operationId: getPageWorkflowsList
-   *        description: Get Workflows Present Within the Page
-   *        parameters:
-   *          - name: pageId
-   *            in: path
-   *            required: true
-   *            description: pageId to get the workflow
-   *            schema:
-   *              type: string
-   *        responses:
-   *          200:
-   *            description: Revoke user read only access success
-   *            content:
-   *              application/json:
-   *                schema:
-   *                  properties:
-   *                    workflowData:
-   *                      type: object
-   */
-  router.get('/list/:pageId', async(req: FormRequest, res: ApiV3Response) => {
-    // パスパラメーターで pageId を受け取り、対象ページに付随する workflow データを返却する
+
+  // description: パスパラメーターで pageId を受け取り、対象ページに付随する workflow データを返却する
+  router.get('/list/:pageId', accessTokenParser, loginRequired, async(req: RequestWithUser, res: ApiV3Response) => {
     const { pageId } = req.params;
 
     return res.apiv3();
   });
 
-  router.post('/', async(req: FormRequest, res: ApiV3Response) => {
+  // description: Workflow.status が "inprogress" な Workflow document を作成, 作成された Workflow document を返却する
+  router.post('/', accessTokenParser, loginRequired, async(req: RequestWithUser, res: ApiV3Response) => {
+    const {
+      pageId,
+      workflowName,
+      workflowComment,
+      workflowTask,
+    } = req.body;
+    const { user } = req;
+
     return res.apiv3();
   });
 
   // wokflow の更新
-  router.post('/update', async(req: FormRequest, res: ApiV3Response) => {
+  router.post('/update', accessTokenParser, loginRequired, async(req: RequestWithUser, res: ApiV3Response) => {
     return res.apiv3();
   });
 
   // workflow の削除
-  router.delete('/', async(req: FormRequest, res: ApiV3Response) => {
+  router.delete('/', accessTokenParser, loginRequired, async(req: RequestWithUser, res: ApiV3Response) => {
     return res.apiv3();
   });
 
