6e7d702427f048a0cda0993dfd52bd43fd5876d6
Shun Miyazawa <shun-m@weseek.co.jp>
2023-08-30 15:55:45 +0000

    fix type

--- apps/app/src/interfaces/workflow.ts
+++ apps/app/src/interfaces/workflow.ts
@@ -1,24 +1,29 @@
 import type { IUserHasId, HasObjectId } from '@growi/core';
 
 
-export type IWorkflowApproverStatus = {
-  None: 'NONE',
-  Apporve: 'APPROVE',
-  Remand: 'REMAND',
-  Delegate: 'DELEGATE',
-}
+const WORKFLOW_APPROVER_STATUS = {
+  NONE: 'NONE',
+  APPROVE: 'APPROVE',
+  REMAND: 'REMAND',
+  DELEGATE: 'DELEGATE',
+} as const;
+type IWorkflowApproverStatus = typeof WORKFLOW_APPROVER_STATUS[keyof typeof WORKFLOW_APPROVER_STATUS];
 
-export type IWorkflowStatus = {
-  Inprogress: 'INPROGRESS',
-  Approve: 'APPROVE',
-  Reject: 'REJECT',
-  Cancel: 'CANCEL'
-}
 
-export type IWorkflowTaskApprovalType = {
-  And: 'AND',
-  Or: 'OR',
-}
+const WORKFLOW_STATUS = {
+  INPROGRESS: 'INPROGRESS',
+  APPROVE: 'APPROVE',
+  REJECT: 'REJECT',
+  CANCEL: 'CANCEL',
+} as const;
+type IWorkflowStatus = typeof WORKFLOW_STATUS[keyof typeof WORKFLOW_STATUS];
+
+
+const WORKFLOW_APPROVAL_TYPE = {
+  AND: 'AND',
+  OR: 'OR',
+} as const;
+export type IWorkflowApprovalType = typeof WORKFLOW_APPROVAL_TYPE[keyof typeof WORKFLOW_APPROVAL_TYPE];
 
 
 export type IWorkflowApprover = {
@@ -26,18 +31,16 @@ export type IWorkflowApprover = {
   status: IWorkflowApproverStatus,
 }
 
-export type IWorkflowTask = {
-  approver: IWorkflowApprover[],
-  approvalType: IWorkflowTaskApprovalType,
-  isApproved?: boolean,
-}
+export type IWorkflowApproverGroup = {
+  approvalType: IWorkflowApprovalType
+  approveres: (IWorkflowApprover | IWorkflowApproverGroup)[],
+};
 
 export type IWorkflow = {
   name: string,
   comment: string,
   pageId: string,
   status: IWorkflowStatus,
-  tasks: IWorkflowTask[],
+  approverGroup: IWorkflowApproverGroup[]
 }
-
 export type IWorkflowHasId = IWorkflow & HasObjectId
