d96b30b21b4fc781a984dff8402a660e3ac8e7a0
WNomunomu <kohsei.nmr@gmail.com>
2023-09-07 08:35:23 +0000

    introduce emoji-mart

--- packages/editor/src/components/CodeMirrorEditor/Toolbar/EmojiButton.tsx
+++ packages/editor/src/components/CodeMirrorEditor/Toolbar/EmojiButton.tsx
@@ -1,7 +1,22 @@
+import React, { useState } from 'react';
+
+import { Picker } from 'emoji-mart';
+
 export const EmojiButton = (): JSX.Element => {
+  const [emojiState, setEmojiState] = useState(false);
+
+  const onClickEmojiButtonHandler = () => {
+    setEmojiState(!emojiState);
+  };
+
   return (
-    <button type="button" className="btn btn-toolbar-button">
-      <span className="material-icons-outlined fs-6">emoji_emotions</span>
-    </button>
+    <>
+      <button type="button" className="btn btn-toolbar-button" onClick={onClickEmojiButtonHandler}>
+        <span className="material-icons-outlined fs-6">emoji_emotions</span>
+      </button>
+      {emojiState && (
+        <Picker />
+      )}
+    </>
   );
 };
