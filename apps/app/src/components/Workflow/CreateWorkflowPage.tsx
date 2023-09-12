e251fd86046b5e1bb413f9cff2acb2f9c895b6e6
Shun Miyazawa <shun-m@weseek.co.jp>
2023-09-12 00:02:59 +0000

    create form

--- apps/app/src/components/Workflow/CreateWorkflowPage.tsx
+++ apps/app/src/components/Workflow/CreateWorkflowPage.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import { useTranslation } from 'next-i18next';
 import { ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
@@ -13,6 +13,17 @@ export const CreateWorkflowPage = (props: Props): JSX.Element => {
   const { t } = useTranslation();
 
   const { onClickWorkflowListPageBackButton } = props;
+
+  const [workflowName, setWorkflowName] = useState<string>('');
+  const [workflowDescription, setWorkflowDescription] = useState<string>('');
+
+  const workflowNameChangeHandler = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setWorkflowName(event.target.value);
+  }, []);
+
+  const workflowDescriptionChangeHandler = useCallback((event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setWorkflowDescription(event.target.value);
+  }, []);
 
   const workflowListPageBackButtonClickHandler = useCallback(() => {
     if (onClickWorkflowListPageBackButton == null) {
@@ -29,7 +40,45 @@ export const CreateWorkflowPage = (props: Props): JSX.Element => {
       </ModalHeader>
 
       <ModalBody>
-        Body
+
+        <div className="row align-items-center">
+          <label htmlFor="name" className="col-md-4 col-form-label">
+            {t('approval_workflow.name')}
+          </label>
+          <div className="col-md-8 mb-3">
+            <div className="row">
+              <div className="col">
+                <input
+                  className="form-control"
+                  type="text"
+                  name="name"
+                  value={workflowName}
+                  onChange={workflowNameChangeHandler}
+                  required
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <label htmlFor="description" className="col-md-4 col-form-label">
+            {t('approval_workflow.description')}
+          </label>
+          <div className="col-md-8">
+            <div className="row">
+              <div className="col">
+                <textarea
+                  className="form-control"
+                  name="description"
+                  value={workflowDescription}
+                  onChange={workflowDescriptionChangeHandler}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
       </ModalBody>
 
       <ModalFooter>
