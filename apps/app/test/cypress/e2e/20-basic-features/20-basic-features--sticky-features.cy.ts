867704f70bea8ba3bb2ff94a3ce31e896f2170de
WNomunomu <kohsei.nmr@gmail.com>
2023-09-12 07:46:33 +0000

    remove unnecessary

--- apps/app/test/cypress/e2e/20-basic-features/20-basic-features--sticky-features.cy.ts
+++ apps/app/test/cypress/e2e/20-basic-features/20-basic-features--sticky-features.cy.ts
@@ -111,7 +111,6 @@ context('Access to any page', () => {
     });
     cy.waitUntil(() => {
       cy.getByTestid('grw-subnav-switcher').within(() => {
-        // cy.getByTestid('editor-button').should('be.visible').click();
         cy.getByTestid('editor-button').as('editorButton').should('be.visible');
         cy.get('@editorButton').click();
       });
