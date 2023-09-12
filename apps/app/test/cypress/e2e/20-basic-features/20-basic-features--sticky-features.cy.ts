c435176fab8454b609548cc95bb579e9a7ca474c
WNomunomu <kohsei.nmr@gmail.com>
2023-09-12 07:13:04 +0000

    test cypress

--- apps/app/test/cypress/e2e/20-basic-features/20-basic-features--sticky-features.cy.ts
+++ apps/app/test/cypress/e2e/20-basic-features/20-basic-features--sticky-features.cy.ts
@@ -112,8 +112,8 @@ context('Access to any page', () => {
     cy.waitUntil(() => {
       cy.getByTestid('grw-subnav-switcher').within(() => {
         // cy.getByTestid('editor-button').should('be.visible').click();
-        cy.getByTestid('editor-button').as('editorButton').click()
-        cy.get('@editorButton').should('be.visible')
+        cy.getByTestid('editor-button').as('editorButton').should('be.visible');
+        cy.get('@editorButton').click();
       });
       return cy.get('.layout-root').then($elem => $elem.hasClass('editing'));
     });
