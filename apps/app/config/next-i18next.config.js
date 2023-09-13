7cc8e18b7bb89e8f5ba51f2c0b0328b4d7987f96
reiji-h <reiji-h@weseek.co.jp>
2023-09-13 06:39:30 +0000

    add locize package

--- apps/app/config/next-i18next.config.js
+++ apps/app/config/next-i18next.config.js
@@ -5,6 +5,7 @@ const { isServer } = require('@growi/core/dist/utils');
 const I18nextChainedBackend = require('i18next-chained-backend').default;
 const I18NextHttpBackend = require('i18next-http-backend');
 const I18NextLocalStorageBackend = require('i18next-localstorage-backend').default;
+const i18NextLocizeBackend = require('i18next-locize-backend');
 
 const isDev = process.env.NODE_ENV === 'development';
 
@@ -19,12 +20,17 @@ module.exports = {
   serializeConfig: false,
   use: isServer() ? [] : [I18nextChainedBackend],
   backend: {
-    backends: isServer() ? [] : [I18NextLocalStorageBackend, I18NextHttpBackend],
+    backends: isServer() ? [] : [I18NextLocalStorageBackend, I18NextHttpBackend, i18NextLocizeBackend],
     backendOptions: [
       // options for i18next-localstorage-backend
       { expirationTime: isDev ? 0 : 24 * 60 * 60 * 1000 }, // 1 day in production
       // options for i18next-http-backend
       { loadPath: '/static/locales/{{lng}}/{{ns}}.json' },
+      // options for i18next-locize-backend
+      {
+        projectId: '89632551-6541-4261-a4f5-66ee56bec3df',
+        api: '6fc5f3ee-7a02-4449-bf91-c9759283a8f5',
+      },
     ],
   },
 };
