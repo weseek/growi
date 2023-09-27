2cfdcc49cc4740367d6971724dd6214f42d1f664
reiji-h <reiji-h@weseek.co.jp>
2023-09-27 06:37:39 +0000

    remove state update

--- packages/editor/src/services/codemirror-editor/use-codemirror-editor/utils/insert-text.ts
+++ packages/editor/src/services/codemirror-editor/use-codemirror-editor/utils/insert-text.ts
@@ -8,9 +8,7 @@ export const useInsertText = (view?: EditorView): InsertText => {
 
   return useCallback((text) => {
     view?.dispatch(
-      view?.state.update(
-        view?.state.replaceSelection(text),
-      ),
+      view?.state.replaceSelection(text),
     );
   }, [view]);
 
