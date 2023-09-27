8ae52dbf94e135723ea1d565536a01da011fe135
Tatsuya Ise <tatsuya@weseek.co.jp>
2023-09-27 05:16:07 +0000

    refs #131350: Add selected paragraph to comment. - Testing body selection   - Add test code.

--- apps/app/src/components/Page/RevisionRenderer.tsx
+++ apps/app/src/components/Page/RevisionRenderer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
 import ReactMarkdown from 'react-markdown';
@@ -33,6 +33,23 @@ const RevisionRenderer = React.memo((props: Props): JSX.Element => {
     rendererOptions, markdown, additionalClassName,
   } = props;
 
+  // TODO: Delete these experimental codes when the test is finished.
+  rendererOptions.sourcePos = true;
+  const [selected, setSelected] = useState('');
+  useEffect(() => {
+    document.addEventListener('mouseup', () => {
+      const sel = window.getSelection();
+      console.log(sel);
+      if (!sel?.rangeCount) return; // 範囲選択されている箇所がない場合は何もせず終了
+      const range = sel.getRangeAt(0);
+      const newNode = document.createElement('span');
+      newNode.setAttribute('style', 'background-color: blue;'); // 範囲選択箇所の背景を青にする
+      newNode.innerHTML = sel.toString();
+      // range.deleteContents(); // 範囲選択箇所を一旦削除
+      // range.insertNode(newNode); // 範囲選択箇所の先頭から、修飾したspanを挿入
+    });
+  }, []);
+
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
       <ReactMarkdown
@@ -41,6 +58,8 @@ const RevisionRenderer = React.memo((props: Props): JSX.Element => {
       >
         {markdown}
       </ReactMarkdown>
+      {/* TODO: Remove this line. */}
+      <div>{selected}</div>
     </ErrorBoundary>
   );
 
