5e200a2c9a2089b7c98f224298048be37d145716
Shun Miyazawa <shun-m@weseek.co.jp>
2023-09-28 02:56:28 +0000

    fix fb

--- apps/app/src/features/approval-workflow/server/models/serializers/workflow-seroalizer.spec.ts
+++ apps/app/src/features/approval-workflow/server/models/serializers/workflow-seroalizer.spec.ts
@@ -4,41 +4,31 @@ import {
   IWorkflowHasId, WorkflowApprovalType, WorkflowStatus, WorkflowApproverStatus,
 } from '../../../interfaces/workflow';
 
+
 import { serializeWorkflowSecurely } from './workflow-seroalizer';
 
 
 describe('workflow-seroalizer', () => {
 
   describe('.serializeWorkflowSecurely', async() => {
-    class DummyUser {
 
-      username: string;
-
-      password: string;
-
-      apiToken: string;
-
-      constructor(username: string, password: string, apiToken: string) {
-        this.username = username;
-        this.password = password;
-        this.apiToken = apiToken;
-      }
-
-    }
-
-    // Within the test, since the User Model doesn't exist, it's being replaced with a dummy User class
-    // eslint-disable-next-line max-len
-    // see: https://github.com/weseek/growi/blob/feat/131160-unit-test-implementation-of-serialize-workflow-securely/apps/app/src/server/models/serializers/user-serializer.js#L21
-    vi.spyOn(mongoose, 'model').mockReturnValue(DummyUser);
-
-    let workflow;
     const password = 'password';
     const apiToken = 'bX8XYssHo2L2v2SdWFvTb2zCTMpwGHnhZRf+fMKrfec=';
 
-    beforeEach(() => {
-      const creator = new DummyUser('workflow-creator', password, apiToken);
-      const approver = new DummyUser('workflow-approver', password, apiToken);
+    let workflow;
 
+    beforeEach(() => {
+      const creator = {
+        username: 'workflow-creator',
+        password,
+        apiToken,
+      };
+
+      const approver = {
+        name: 'Workflow Approver',
+        password,
+        apiToken,
+      };
       workflow = {
         _id: new mongoose.Types.ObjectId().toString(),
         creator,
@@ -61,10 +51,34 @@ describe('workflow-seroalizer', () => {
       } as IWorkflowHasId;
     });
 
+    const mocks = vi.hoisted(() => {
+      return {
+        serializeUserSecurelyMock: vi.fn(() => { return {} }),
+      };
+    });
+
+    vi.mock('~/server/models/serializers/user-serializer', () => {
+      return { serializeUserSecurely: mocks.serializeUserSecurelyMock };
+    });
+
     it('Should serialize creator and approver', () => {
       // setup
+      mocks.serializeUserSecurelyMock.mockImplementationOnce(() => {
+        delete workflow.creator.password;
+        delete workflow.creator.apiToken;
+
+        return workflow;
+      });
+
+      mocks.serializeUserSecurelyMock.mockImplementationOnce(() => {
+        delete workflow.approverGroups[0].approvers[0].user.password;
+        delete workflow.approverGroups[0].approvers[0].user.apiToken;
+
+        return workflow;
+      });
+
       expect(workflow.creator.password).toEqual(password);
-      expect(workflow.creator.apiToken).toEqual(apiToken);
+      expect(workflow.creator.apiToken).toEqual(password);
       expect(workflow.approverGroups[0].approvers[0].user.password).toEqual(password);
       expect(workflow.approverGroups[0].approvers[0].user.apiToken).toEqual(apiToken);
 
@@ -80,10 +94,18 @@ describe('workflow-seroalizer', () => {
 
     it('Should serialize only the creator', () => {
       // setup
+      mocks.serializeUserSecurelyMock.mockImplementationOnce(() => {
+        delete workflow.creator.password;
+        delete workflow.creator.apiToken;
+
+        return workflow;
+      });
+
       expect(workflow.creator.password).toEqual(password);
-      expect(workflow.creator.apiToken).toEqual(apiToken);
+      expect(workflow.creator.apiToken).toEqual(password);
       expect(workflow.approverGroups[0].approvers[0].user.password).toEqual(password);
       expect(workflow.approverGroups[0].approvers[0].user.apiToken).toEqual(apiToken);
+
 
       // when
       const serializedWorkflow = serializeWorkflowSecurely(workflow, true);
