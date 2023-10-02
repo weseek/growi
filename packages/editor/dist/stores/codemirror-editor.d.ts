d5dbb4a5cfe811f97422801cb89206d8c1d1e6c4
WNomunomu <kohsei.nmr@gmail.com>
2023-10-02 05:10:13 +0000

    remove unnecessary

--- packages/editor/dist/stores/codemirror-editor.d.ts
+++
@@ -1,4 +0,0 @@
-import type { ReactCodeMirrorProps } from '@uiw/react-codemirror';
-import type { SWRResponse } from 'swr';
-import type { UseCodeMirrorEditor } from '../services';
-export declare const useCodeMirrorEditorIsolated: (key: string | null, container?: HTMLDivElement | null, props?: ReactCodeMirrorProps) => SWRResponse<UseCodeMirrorEditor>;
--- packages/editor/dist/stores/index.js
+++
@@ -1,5 +0,0 @@
-import { useCodeMirrorEditorIsolated as e } from "./codemirror-editor.js";
-export {
-  e as useCodeMirrorEditorIsolated
-};
-//# sourceMappingURL=index.js.map
