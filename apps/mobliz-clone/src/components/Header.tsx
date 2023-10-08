80e468f05d1da09e0d4145bc1684eeddd734311e
atsuki-t <atsuki-t@weseek.co.jp>
2023-10-08 06:02:04 +0000

    setting mobliz-clone page

---
+++ apps/mobliz-clone/src/components/Header.tsx
@@ -0,0 +1,5 @@
+const Header: React.FC = () => {
+  return <header />;
+};
+
+export default Header;
---
+++ apps/mobliz-clone/src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import '~/styles/globals.scss';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import Layout from '~/components/Layout';
+
+const App = ({ Component, pageProps }: AppProps): JSX.Element => (
+  <>
+    <Head>
+      <title />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </>
+);
+
+export default App;
