1b14d15119a416edef5e4bd85789a02c0c367bc7
Futa Arai <arafubeatbox@gmail.com>
2023-10-09 16:22:00 +0900

    add throwIfRejected parameter

--- apps/app/src/utils/promise.ts
+++ apps/app/src/utils/promise.ts
@@ -4,21 +4,31 @@
  * @param items array to process
  * @param limit batch size
  * @param fn function to apply on each item
+ * @param throwIfRejected whether or not to throw Error when there is a rejected Promise
  * @returns result of fn applied to each item
  */
 export const batchProcessPromiseAll = async<I, O>(
   items: Array<I>,
   limit: number,
   fn: (item: I, index?: number, array?: Array<I>) => Promise<O>,
+  throwIfRejected = true,
 ): Promise<O[]> => {
-  let results: O[] = [];
+  const results: O[] = [];
+
   for (let start = 0; start < items.length; start += limit) {
     const end = Math.min(start + limit, items.length);
 
     // eslint-disable-next-line no-await-in-loop
-    const slicedResults = await Promise.all(items.slice(start, end).map(fn));
+    const slicedResults = await Promise.allSettled(items.slice(start, end).map(fn));
 
-    results = results.concat(slicedResults);
+    slicedResults.forEach((result) => {
+      if (result.status === 'fulfilled') {
+        results.push(result.value);
+      }
+      else if (throwIfRejected && result.reason instanceof Error) {
+        throw result.reason;
+      }
+    });
   }
 
   return results;
