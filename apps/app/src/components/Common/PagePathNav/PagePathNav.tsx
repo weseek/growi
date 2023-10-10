fea4905f4a8998f38532f8e28182ea75383b247a
Yuki Takei <yuki@weseek.co.jp>
2023-10-10 10:22:45 +0000

    add CollapsedParentsDropdown

--- apps/app/src/components/Common/PagePathNav/PagePathNav.tsx
+++ apps/app/src/components/Common/PagePathNav/PagePathNav.tsx
@@ -9,7 +9,7 @@ import { useIsNotFound } from '~/stores/page';
 
 import LinkedPagePath from '../../../models/linked-page-path';
 import { PagePathHierarchicalLink } from '../PagePathHierarchicalLink';
-
+import { CollapsedParentsDropdown } from '../PagePathHierarchicalLink/CollapsedParentsDropdown';
 
 import styles from './PagePathNav.module.scss';
 
@@ -36,12 +36,20 @@ export const PagePathNav: FC<Props> = (props: Props) => {
   const isInTrash = isTrashPage(pagePath);
 
   let formerLink;
+  let collapsedParent;
   let latterLink;
 
   // one line
-  if (dPagePath.isRoot || dPagePath.isFormerRoot || isSingleLineMode) {
+  if (dPagePath.isRoot || dPagePath.isFormerRoot || (!isCollapseParents && isSingleLineMode)) {
     const linkedPagePath = new LinkedPagePath(pagePath);
     latterLink = <PagePathHierarchicalLink linkedPagePath={linkedPagePath} isInTrash={isInTrash} />;
+  }
+  // collapse parents
+  else if (isCollapseParents) {
+    const linkedPagePathFormer = new LinkedPagePath(dPagePath.former);
+    const linkedPagePathLatter = new LinkedPagePath(dPagePath.latter);
+    collapsedParent = <CollapsedParentsDropdown linkedPagePath={linkedPagePathFormer} />;
+    latterLink = <PagePathHierarchicalLink linkedPagePath={linkedPagePathLatter} basePath={dPagePath.former} isInTrash={isInTrash} />;
   }
   // two line
   else {
@@ -58,7 +66,11 @@ export const PagePathNav: FC<Props> = (props: Props) => {
     <div className="grw-page-path-nav">
       {formerLink}
       <div className="d-flex align-items-center">
-        <h1 className="m-0 text-truncate">{latterLink}</h1>
+        <h1 className="m-0 text-truncate">
+          {collapsedParent}
+          <span className={styles['grw-mr-02em']}>/</span>
+          {latterLink}
+        </h1>
         { pageId != null && !isNotFound && (
           <div className="mx-2">
             <CopyDropdown pageId={pageId} pagePath={pagePath} dropdownToggleId={copyDropdownId} dropdownToggleClassName={copyDropdownToggleClassName}>
@@ -76,7 +88,7 @@ type PagePathNavStickyProps = Omit<Props, 'isCollapseParents'>;
 
 export const PagePathNavSticky = (props: PagePathNavStickyProps): JSX.Element => {
   return (
-    <Sticky className={styles['grw-page-path-nav-sticky']}>
+    <Sticky className={`${styles['grw-page-path-nav-sticky']} mb-4`}>
       {({ status }: { status: boolean }) => {
         const isCollapseParents = status === Sticky.STATUS_FIXED;
         return <PagePathNav {...props} isCollapseParents={isCollapseParents} />;
