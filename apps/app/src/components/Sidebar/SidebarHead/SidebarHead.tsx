560f6de55ed2aee8fd05a2dce3d1d33c86040429
Yuki Takei <yuki@weseek.co.jp>
2023-10-19 14:44:19 +0000

    WIP: add collapse button

--- apps/app/src/components/Sidebar/SidebarHead/SidebarHead.tsx
+++ apps/app/src/components/Sidebar/SidebarHead/SidebarHead.tsx
@@ -17,11 +17,14 @@ export const SidebarHead: FC = memo(() => {
   const { data: isDefaultLogo } = useIsDefaultLogo();
 
   return (
-    <div className={styles['grw-sidebar-head']}>
+    <div className={`${styles['grw-sidebar-head']} d-flex justify-content-between`}>
       {/* Brand Logo  */}
       <Link href="/" className="grw-logo d-block">
         <SidebarBrandLogo isDefaultLogo={isDefaultLogo} />
       </Link>
+      <button type="button" className="btn btn-secondary btn-lg">
+        <span className="material-icons">first_page</span>
+      </button>
     </div>
   );
 
