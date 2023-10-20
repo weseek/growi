bd9fc5246162a74b745749372e24492d0b6812aa
Yuki Takei <yuki@weseek.co.jp>
2023-10-20 13:56:30 +0000

    add logo and truncatable app title

--- apps/app/src/components/Sidebar/SidebarHead/SidebarHead.tsx
+++ apps/app/src/components/Sidebar/SidebarHead/SidebarHead.tsx
@@ -17,14 +17,19 @@ export const SidebarHead: FC = memo(() => {
   const { data: isDefaultLogo } = useIsDefaultLogo();
 
   return (
-    <div className={`${styles['grw-sidebar-head']} d-flex justify-content-between`}>
+    <div className={`${styles['grw-sidebar-head']} d-flex w-100`}>
       {/* Brand Logo  */}
       <Link href="/" className="grw-logo d-block">
         <SidebarBrandLogo isDefaultLogo={isDefaultLogo} />
       </Link>
-      <button type="button" className="btn btn-secondary btn-lg">
-        <span className="material-icons">first_page</span>
-      </button>
+      <div className="flex-grow-1 d-flex align-items-center justify-content-between gap-3 overflow-hidden">
+        <div className="grw-app-title text-truncate">
+          <span className="fs-4">GROWI</span>
+        </div>
+        <button type="button" className="btn btn-secondary btn-lg">
+          <span className="material-icons">first_page</span>
+        </button>
+      </div>
     </div>
   );
 
