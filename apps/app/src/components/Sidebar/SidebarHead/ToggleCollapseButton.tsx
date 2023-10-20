420674fc63c7a78d9c7e0156223c707c7c2f2bb1
Yuki Takei <yuki@weseek.co.jp>
2023-10-20 16:12:50 +0000

    rotate icon

--- apps/app/src/components/Sidebar/SidebarHead/ToggleCollapseButton.tsx
+++ apps/app/src/components/Sidebar/SidebarHead/ToggleCollapseButton.tsx
@@ -16,9 +16,11 @@ export const ToggleCollapseButton = memo((): JSX.Element => {
     mutateCollapsedContentsOpened(false);
   }, [isCollapsedMode, mutateCollapsedContentsOpened, mutateCollapsedMode]);
 
+  const rotationClass = isCollapsedMode ? 'rotate180' : '';
+
   return (
     <button type="button" className={`btn btn-primary ${styles['btn-toggle-collapse']} p-2`} onClick={toggle}>
-      <span className="material-icons fs-2">first_page</span>
+      <span className={`material-icons fs-2 ${rotationClass}`}>first_page</span>
     </button>
   );
 });
