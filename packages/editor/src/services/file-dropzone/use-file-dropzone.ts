c93052d1e605495534241ca803171b700fc242b1
reiji-h <reiji-h@weseek.co.jp>
2023-10-20 07:24:55 +0000

    add isUploading val

--- packages/editor/src/services/file-dropzone/use-file-dropzone.ts
+++ packages/editor/src/services/file-dropzone/use-file-dropzone.ts
@@ -1,27 +1,41 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 import { useDropzone } from 'react-dropzone';
 import type { DropzoneState } from 'react-dropzone';
+
+type FileDropzoneState = DropzoneState & {
+  isUploading: boolean,
+}
 
 type DropzoneEditor = {
   onUpload?: (files: File[]) => void,
 }
 
-export const useFileDropzone = (props: DropzoneEditor): DropzoneState => {
+export const useFileDropzone = (props: DropzoneEditor): FileDropzoneState => {
 
   const { onUpload } = props;
+
+  const [isUploading, setIsUploading] = useState(false);
 
   const dropHandler = useCallback((acceptedFiles: File[]) => {
     if (onUpload == null) {
       return;
     }
-    onUpload(acceptedFiles);
-  }, [onUpload]);
 
-  return useDropzone({
+    setIsUploading(true);
+    onUpload(acceptedFiles);
+    setIsUploading(false);
+
+  }, [onUpload, setIsUploading]);
+
+  const dropzoneState = useDropzone({
     noKeyboard: true,
     noClick: true,
     onDrop: dropHandler,
   });
 
+  return {
+    ...dropzoneState,
+    isUploading,
+  };
 };
