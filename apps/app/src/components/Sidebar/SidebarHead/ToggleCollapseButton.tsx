72fe9135d259768e3658cddd2f9c86b2fcd84ddc
Yuki Takei <yuki@weseek.co.jp>
2023-10-20 16:16:59 +0000

    update icon and color

--- apps/app/src/components/Sidebar/SidebarHead/ToggleCollapseButton.tsx
+++ apps/app/src/components/Sidebar/SidebarHead/ToggleCollapseButton.tsx
@@ -17,10 +17,11 @@ export const ToggleCollapseButton = memo((): JSX.Element => {
   }, [isCollapsedMode, mutateCollapsedContentsOpened, mutateCollapsedMode]);
 
   const rotationClass = isCollapsedMode ? 'rotate180' : '';
+  const icon = isCollapsedMode ? 'keyboard_double_arrow_left' : 'first_page';
 
   return (
     <button type="button" className={`btn btn-primary ${styles['btn-toggle-collapse']} p-2`} onClick={toggle}>
-      <span className={`material-icons fs-2 ${rotationClass}`}>first_page</span>
+      <span className={`material-icons fs-2 ${rotationClass}`}>{icon}</span>
     </button>
   );
 });
