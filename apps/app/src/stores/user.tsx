805e792043237fb3d90c8889e64852539d28233b
Shun Miyazawa <m0zurillex@gmail.com>
2023-10-28 10:20:16 +0000

    create useSWRMUTxSearchUser

--- apps/app/src/stores/user.tsx
+++ apps/app/src/stores/user.tsx
@@ -1,8 +1,10 @@
 import type { IUserHasId } from '@growi/core';
 import useSWR, { SWRResponse } from 'swr';
 import useSWRImmutable from 'swr/immutable';
+import useSWRMutation, { type SWRMutationResponse } from 'swr/mutation';
 
 import { apiv3Get } from '~/client/util/apiv3-client';
+import type { PaginateResult } from '~/interfaces/mongoose-utils';
 import { checkAndUpdateImageUrlCached } from '~/stores/middlewares/user';
 
 export const useSWRxUsersList = (userIds: string[]): SWRResponse<IUserHasId[], Error> => {
@@ -49,16 +51,21 @@ export const useSWRxUsernames = (q: string, offset?: number, limit?: number, opt
   );
 };
 
-type UserSearchResult = {
-  users: IUserHasId[],
-  totalCount: number
-}
 
-export const useSWRxSearchUsers = (q: string, offset?: number, limit?: number, excludedUserIds?: string): SWRResponse<UserSearchResult, Error> => {
-  return useSWRImmutable(
-    (q != null && q.trim() !== '') ? ['/users/users', q, offset, limit, excludedUserIds] : null,
-    ([endpoint, q, offset, limit, excludedUserIds]) => apiv3Get(endpoint, {
-      q, offset, limit, excludedUserIds,
-    }).then(result => result.data),
+export const useSWRMUTxSearchUser = (
+    sortOrder: string,
+    sort: string,
+    page: number,
+    searchText?: string,
+    excludedUserIds?: string[],
+    forceIncludeAttributes?: string[],
+    selectedStatusList?: string[],
+): SWRMutationResponse<PaginateResult<IUserHasId>, Error> => {
+
+  return useSWRMutation(
+    ['/users', sortOrder, sort, page, searchText, excludedUserIds, forceIncludeAttributes, selectedStatusList],
+    ([endpoint, sortOrder, sort, page, searchText, excludedUserIds, forceIncludeAttributes, selectedStatusList]) => apiv3Get(endpoint, {
+      searchText, sortOrder, sort, page, excludedUserIds, forceIncludeAttributes, selectedStatusList,
+    }).then(result => result.data.paginateResult),
   );
 };
