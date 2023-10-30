8467323ea6cd2dd3d5dcb79b4fb1cbd460623a2f
WNomunomu <kohsei.nmr@gmail.com>
2023-10-30 13:24:09 +0000

    add types to IInAppNotification

--- apps/app/src/interfaces/in-app-notification.ts
+++ apps/app/src/interfaces/in-app-notification.ts
@@ -1,4 +1,4 @@
-import type { IUser } from '@growi/core';
+import type { IPageHasId, IUser, IUserHasId } from '@growi/core';
 
 import { SupportedTargetModelType, SupportedActionType } from './activity';
 
@@ -13,7 +13,7 @@ export enum InAppNotificationStatuses {
 export interface IInAppNotification {
   user: IUser
   targetModel: SupportedTargetModelType
-  target: any
+  target: IUserHasId | IPageHasId
   action: SupportedActionType
   status: InAppNotificationStatuses
   actionUsers: IUser[]
