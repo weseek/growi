f4193788262483d55ff1b28d30e522abcc8452f5
soumaeda <soumaeda1126@icloud.com>
2023-10-31 05:42:45 +0000

    133069 change handler name

--- packages/editor/src/components/CodeMirrorEditor/Toolbar/TextFormatTools.tsx
+++ packages/editor/src/components/CodeMirrorEditor/Toolbar/TextFormatTools.tsx
@@ -39,7 +39,7 @@ export const TextFormatTools = (props: TextFormatToolsType): JSX.Element => {
     setOpen(bool => !bool);
   }, []);
 
-  const insertMarkdownText = (prefix: string, suffix: string) => codeMirrorEditor?.insertMarkdownText(prefix, suffix);
+  const onClickInsertMarkdownText = (prefix: string, suffix: string) => codeMirrorEditor?.insertMarkdownText(prefix, suffix);
 
   return (
     <div className="d-flex">
@@ -47,19 +47,19 @@ export const TextFormatTools = (props: TextFormatToolsType): JSX.Element => {
 
       <Collapse isOpen={isOpen} horizontal>
         <div className="d-flex px-1 gap-1" style={{ width: '220px' }}>
-          <button type="button" className="btn btn-toolbar-button" onClick={() => insertMarkdownText('**', '**')}>
+          <button type="button" className="btn btn-toolbar-button" onClick={() => onClickInsertMarkdownText('**', '**')}>
             <span className="material-symbols-outlined fs-5">format_bold</span>
           </button>
           <button type="button" className="btn btn-toolbar-button">
-            <span className="material-symbols-outlined fs-5" onClick={() => insertMarkdownText('*', '*')}>format_italic</span>
+            <span className="material-symbols-outlined fs-5" onClick={() => onClickInsertMarkdownText('*', '*')}>format_italic</span>
           </button>
-          <button type="button" className="btn btn-toolbar-button" onClick={() => insertMarkdownText('~', '~')}>
+          <button type="button" className="btn btn-toolbar-button" onClick={() => onClickInsertMarkdownText('~', '~')}>
             <span className="material-symbols-outlined fs-5">format_strikethrough</span>
           </button>
           <button type="button" className="btn btn-toolbar-button">
             <span className="material-symbols-outlined fs-5">block</span>
           </button>
-          <button type="button" className="btn btn-toolbar-button" onClick={() => insertMarkdownText('`', '`')}>
+          <button type="button" className="btn btn-toolbar-button" onClick={() => onClickInsertMarkdownText('`', '`')}>
             <span className="material-symbols-outlined fs-5">code</span>
           </button>
           <button type="button" className="btn btn-toolbar-button">
