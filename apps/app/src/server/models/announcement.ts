151679d7a6bfc3cf364c873b047cd9dc0a20cc4f
WNomunomu <kohsei.nmr@gmail.com>
2023-11-01 07:13:18 +0000

    create AnnouncementSchema

--- apps/app/src/server/models/announcement.ts
+++ apps/app/src/server/models/announcement.ts
@@ -1,5 +1,8 @@
-import type { IUserHasId } from '@growi/core';
-import { Document } from 'mongoose';
+import {
+  Types, Document, Schema, Model,
+} from 'mongoose';
+
+import { getOrCreateModel } from '../util/mongoose-utils';
 
 enum AnnouncementStatuses {
   STATUS_UNREAD = 'UNREAD',
@@ -8,17 +11,72 @@ enum AnnouncementStatuses {
 }
 
 export interface AnnouncementDocument extends Document {
-  sender: IUserHasId,
-  name: string,
-  comment: string,
-  emoji: string,
-  isReadReceiptTrackingEnabled: boolean,
-  pageId,
+  _id: Types.ObjectId
+  sender: Types.ObjectId
+  name: string
+  comment: string
+  emoji: string
+  isReadReceiptTrackingEnabled: boolean
+  pageId: Types.ObjectId
   receivers: [
     {
-      receiver: IUserHasId,
+      receiver: Types.ObjectId,
       updatedAt: Date,
       readStatus: AnnouncementStatuses,
     },
-  ],
+  ]
 }
+
+export interface AnnouncementModel extends Model<AnnouncementDocument> {
+  getReadRate(receivers: [Types.ObjectId]): Promise<number | undefined>
+}
+
+const AnnouncementSchema = new Schema<AnnouncementDocument>({
+  sender: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  comment: {
+    type: String,
+  },
+  emoji: {
+    type: String,
+    required: true,
+    // enumにgetEmojiDataArray関数で取得したfixedEmojiDataArrayを入れる。
+  },
+  isReadReceiptTrackingEnabled: {
+    type: Boolean,
+    required: true,
+  },
+  pageId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  receivers: [
+    {
+      receiver: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+      },
+      updatedAt: {
+        type: Date,
+      },
+      readStatus: {
+        type: String,
+        enum: Object.values(AnnouncementStatuses),
+        default: AnnouncementStatuses.STATUS_UNREAD,
+      },
+    },
+  ],
+}, {});
+
+const Announcement = getOrCreateModel<AnnouncementDocument, AnnouncementModel>('Announcement', AnnouncementSchema);
+
+export { Announcement };
