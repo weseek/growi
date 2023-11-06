d1e76600491ba8fc712dc7f86bb1c763a99d8c26
Shun Miyazawa <m0zurillex@gmail.com>
2023-11-06 02:06:12 +0000

    Create updated data

--- apps/app/src/features/approval-workflow/client/components/ModalComponents/WorkflowEditModalContent.tsx
+++ apps/app/src/features/approval-workflow/client/components/ModalComponents/WorkflowEditModalContent.tsx
@@ -3,7 +3,13 @@ import React, { useState, useCallback } from 'react';
 import { useTranslation } from 'next-i18next';
 import { ModalBody, ModalFooter } from 'reactstrap';
 
-import { IWorkflowHasId } from '~/features/approval-workflow/interfaces/workflow';
+import type {
+  IWorkflowHasId,
+  IWorkflowApproverGroupForRenderList,
+  WorkflowApprovalType,
+  CreateApproverGroupData,
+  UpdateApproverGroupData,
+} from '~/features/approval-workflow/interfaces/workflow';
 
 import { getLatestApprovedApproverGroupIndex } from '../../../utils/workflow';
 import { useEditingApproverGroups } from '../../services/workflow';
@@ -11,6 +17,12 @@ import { useSWRxWorkflow } from '../../stores/workflow';
 
 import { EditableApproverGroupCards } from './EditableApproverGroupCards';
 import { WorkflowModalHeader } from './WorkflowModalHeader';
+
+const findArrayDiff = (userIds1: string[], userIds2: string[]): { userIdToAdd?: string, userIdToRemove?: string } => {
+  const userIdToAdd = userIds2.find(item => !userIds1.includes(item));
+  const userIdToRemove = userIds1.find(item => !userIds2.includes(item));
+  return { userIdToAdd, userIdToRemove };
+};
 
 type Props = {
   workflow: IWorkflowHasId
@@ -30,10 +42,62 @@ export const WorkflowEditModalContent = (props: Props): JSX.Element => {
   const [editingWorkflowName, setEditingWorkflowName] = useState<string | undefined>(workflow.name);
   const [editingWorkflowDescription, setEditingWorkflowDescription] = useState<string | undefined>(workflow.comment);
 
+  const [createApproverGroupData, setCreateApproverGroupData] = useState<Array<CreateApproverGroupData & { uuidForRender: string }>>([]);
+  const [updateApproverGroupData, setUpdateApproverGroupData] = useState<Array<UpdateApproverGroupData & { uuidForRender: string }>>([]);
+
+  const updateGroup = useCallback((
+      groupId: string,
+      uuidForRender: string,
+      approvalType: WorkflowApprovalType,
+      userIdToAdd?: string,
+      userIdToRemove?: string,
+  ) => {
+    const clonedData = [...updateApproverGroupData];
+    const targetData = clonedData.find(v => uuidForRender === v.uuidForRender);
+
+    if (targetData != null) {
+      targetData.approvalType = approvalType;
+
+      if (userIdToAdd != null && !targetData.userIdsToAdd?.includes(userIdToAdd)) {
+        targetData.userIdsToAdd?.push(userIdToAdd);
+
+        const removeIndex = targetData.userIdsToRemove?.indexOf(userIdToAdd);
+        if (removeIndex != null && removeIndex >= 0) targetData.userIdsToRemove?.splice(removeIndex, 1);
+      }
+
+      if (userIdToRemove != null && !targetData.userIdsToRemove?.includes(userIdToRemove)) {
+        targetData.userIdsToRemove?.push(userIdToRemove);
+
+        const removeIndex = targetData.userIdsToAdd?.indexOf(userIdToRemove);
+        if (removeIndex != null && removeIndex >= 0) targetData.userIdsToAdd?.splice(removeIndex, 1);
+      }
+      setUpdateApproverGroupData(clonedData);
+    }
+    else {
+      const newData = {
+        groupId,
+        uuidForRender,
+        approvalType,
+        userIdsToAdd: userIdToAdd != null ? [userIdToAdd] : [],
+        userIdsToRemove: userIdToRemove != null ? [userIdToRemove] : [],
+      };
+      setUpdateApproverGroupData([...updateApproverGroupData, newData]);
+    }
+  }, [updateApproverGroupData]);
+
   const { update: updateWorkflow } = useSWRxWorkflow(workflow?._id);
 
   const latestApprovedApproverGroupIndex = getLatestApprovedApproverGroupIndex(workflow);
   const excludedSearchUserIds = [workflow.creator._id, ...allEditingApproverIds];
+
+  const onUpdateApproverGroupsHandler = useCallback((groupIndex: number, approverGroup: IWorkflowApproverGroupForRenderList) => {
+    const oldUserIds = editingApproverGroups[groupIndex].approvers.map(v => (typeof v.user === 'string' ? v.user : v.user._id));
+    const newUserIds = approverGroup.approvers.map(v => (typeof v.user === 'string' ? v.user : v.user._id));
+    const result = findArrayDiff(oldUserIds, newUserIds);
+
+    updateGroup(approverGroup._id, approverGroup.uuidForRenderList, approverGroup.approvalType, result.userIdToAdd, result.userIdToRemove);
+    updateApproverGroupHandler(groupIndex, approverGroup);
+  }, [editingApproverGroups, updateApproverGroupHandler, updateGroup]);
 
   const clickSaveWorkflowButtonClickHandler = useCallback(async() => {
     try {
@@ -41,7 +105,7 @@ export const WorkflowEditModalContent = (props: Props): JSX.Element => {
         name: editingWorkflowName,
         comment: editingWorkflowDescription,
         createApproverGroupData: undefined,
-        updateApproverGroupData: undefined,
+        updateApproverGroupData,
       };
       await updateWorkflow(updateData);
 
@@ -52,7 +116,7 @@ export const WorkflowEditModalContent = (props: Props): JSX.Element => {
     catch (err) {
       // TODO: Consider how to display errors
     }
-  }, [editingWorkflowDescription, editingWorkflowName, onUpdated, updateWorkflow]);
+  }, [editingWorkflowDescription, editingWorkflowName, onUpdated, updateApproverGroupData, updateWorkflow]);
 
 
   return (
@@ -67,7 +131,7 @@ export const WorkflowEditModalContent = (props: Props): JSX.Element => {
           editingApproverGroups={editingApproverGroups}
           excludedSearchUserIds={excludedSearchUserIds}
           latestApprovedApproverGroupIndex={latestApprovedApproverGroupIndex ?? undefined}
-          onUpdateApproverGroups={updateApproverGroupHandler}
+          onUpdateApproverGroups={onUpdateApproverGroupsHandler}
           onClickAddApproverGroupCard={addApproverGroupHandler}
           onClickRemoveApproverGroupCard={removeApproverGroupHandler}
         />
