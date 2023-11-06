9e2c6e722bf384831d72afcc56a021cefbba9d66
Shun Miyazawa <m0zurillex@gmail.com>
2023-11-06 08:21:14 +0000

    lax validation

--- apps/app/src/features/approval-workflow/server/services/workflow.ts
+++ apps/app/src/features/approval-workflow/server/services/workflow.ts
@@ -4,11 +4,12 @@ import { ObjectIdLike } from '~/server/interfaces/mongoose-utils';
 import loggerFactory from '~/utils/logger';
 
 import {
-  IWorkflowHasId,
-  IWorkflowReq,
+  type IWorkflowHasId,
+  type IWorkflowReq,
+  type IWorkflowApproverGroupReq,
   WorkflowStatus,
-  CreateApproverGroupData,
-  UpdateApproverGroupData,
+  type CreateApproverGroupData,
+  type UpdateApproverGroupData,
 } from '../../interfaces/workflow';
 import Workflow from '../models/workflow';
 
@@ -84,6 +85,12 @@ class WorkflowServiceImpl implements WorkflowService {
     if (updateApproverGroupData != null && updateApproverGroupData.length > 0) {
       WorkflowApproverGroupService.updateApproverGroup(targetWorkflow, updateApproverGroupData);
     }
+
+    WorkflowApproverGroupService.validateApproverGroups(
+      false,
+      targetWorkflow.creator._id,
+      targetWorkflow.approverGroups as unknown as IWorkflowApproverGroupReq[],
+    );
 
     targetWorkflow.name = name;
     targetWorkflow.comment = comment;
