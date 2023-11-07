bb7cba4641da16ea1b0b32fdcd28ea62280061a2
soumaeda <soumaeda1126@icloud.com>
2023-11-07 05:23:36 +0000

    133085 fix line

--- packages/editor/src/services/codemirror-editor/use-codemirror-editor/utils/insert-header.ts
+++ packages/editor/src/services/codemirror-editor/use-codemirror-editor/utils/insert-header.ts
@@ -7,12 +7,11 @@ export type InsertHeader = (prefix: string) => void;
 export const useInsertHeader = (view?: EditorView): InsertHeader => {
 
   return useCallback((prefix) => {
-
     if (view == null) {
       return;
     }
 
-    const selection = view?.state.sliceDoc(
+    const selection = view.state.sliceDoc(
       view.state.selection.main.from,
       view.state.selection.main.to,
     );
@@ -20,8 +19,8 @@ export const useInsertHeader = (view?: EditorView): InsertHeader => {
     const cursorPos = view.state.selection.main.head;
     const line = view.state.doc.lineAt(cursorPos);
     const insertPos = line.text.startsWith(prefix) ? cursorPos - 1 : cursorPos;
-    let insertText = prefix;
 
+    let insertText = prefix;
     if (!line.text.startsWith(prefix)) {
       insertText += ' ';
     }
