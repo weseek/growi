625497ee42ecc68854d20e40f4546899169413a9
reiji-h <reiji-h@weseek.co.jp>
2023-11-08 05:55:49 +0000

    add project versions

--- apps/app/config/next-i18next.config.js
+++ apps/app/config/next-i18next.config.js
@@ -3,9 +3,8 @@ const path = require('path');
 const { AllLang, Lang } = require('@growi/core');
 const { isServer } = require('@growi/core/dist/utils');
 const I18nextChainedBackend = require('i18next-chained-backend').default;
-const I18NextHttpBackend = require('i18next-http-backend');
 const I18NextLocalStorageBackend = require('i18next-localstorage-backend').default;
-const i18NextLocizeBackend = require('i18next-locize-backend');
+const i18NextLocizeBackend = require('i18next-locize-backend/cjs');
 
 const isDev = process.env.NODE_ENV === 'development';
 
@@ -20,16 +19,18 @@ module.exports = {
   serializeConfig: false,
   use: isServer() ? [] : [I18nextChainedBackend],
   backend: {
-    backends: isServer() ? [] : [I18NextLocalStorageBackend, I18NextHttpBackend, i18NextLocizeBackend],
+    backends: isServer() ? [] : [I18NextLocalStorageBackend, i18NextLocizeBackend],
     backendOptions: [
       // options for i18next-localstorage-backend
       { expirationTime: isDev ? 0 : 24 * 60 * 60 * 1000 }, // 1 day in production
-      // options for i18next-http-backend
-      { loadPath: '/static/locales/{{lng}}/{{ns}}.json' },
       // options for i18next-locize-backend
       {
-        projectId: '89632551-6541-4261-a4f5-66ee56bec3df',
-        api: '6fc5f3ee-7a02-4449-bf91-c9759283a8f5',
+        loadPath: '/static/locales/{{lng}}/{{ns}}.json',
+        addPath: 'static/locales/add/{{lng}}/{{ns}}',
+        projectId: '2ce91e4f-98e9-43a0-8da7-ae5ebd4d2e49',
+        apiKey: '53989b1a-db2d-4084-a765-d33ac45b356e',
+        referenceLng: 'ja_JP',
+        version: 'latest'
       },
     ],
   },
