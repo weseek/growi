1442f32520cb0c42e418b53a4ce4e2f4140dc449
reiji-h <reiji-h@weseek.co.jp>
2023-11-10 06:42:24 +0000

    fix line error

--- packages/editor/src/services/file-dropzone/index.ts
+++ packages/editor/src/services/file-dropzone/index.ts
@@ -1,2 +1,2 @@
-export * from './use-file-dropzone/use-file-dropzone.ts';
-export * from './use-file-dropzone/FileDropzoneOverlay.tsx'
+export * from './use-file-dropzone/use-file-dropzone';
+export * from './use-file-dropzone/FileDropzoneOverlay';
