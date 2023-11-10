48c2ec422bfdc3f07654cd8612e97126df2e4446
WNomunomu <kohsei.nmr@gmail.com>
2023-11-10 05:03:43 +0000

    fix type

--- apps/app/src/components/InAppNotification/PageNotification/ModelNotification.tsx
+++ apps/app/src/components/InAppNotification/PageNotification/ModelNotification.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useImperativeHandle } from 'react';
 
-import type { HasObjectId } from '@growi/core';
+import type { IUser, IPage, HasObjectId } from '@growi/core';
 import { PagePathLabel } from '@growi/ui/dist/components';
 
 import type { IInAppNotificationOpenable } from '~/client/interfaces/in-app-notification-openable';
@@ -9,7 +9,7 @@ import type { IInAppNotification } from '~/interfaces/in-app-notification';
 import FormattedDistanceDate from '../../FormattedDistanceDate';
 
 type Props = {
-  notification: IInAppNotification & HasObjectId
+  notification: IInAppNotification<IUser | IPage> & HasObjectId
   actionMsg: string
   actionIcon: string
   actionUsers: string
@@ -31,7 +31,9 @@ export const ModelNotification: FC<Props> = (props) => {
   return (
     <div className="p-2 overflow-hidden">
       <div className="text-truncate">
-        <b>{actionUsers}</b> {actionMsg} <PagePathLabel path={notification.parsedSnapshot?.path ?? ''} />
+        <b>{actionUsers}</b>
+        {actionMsg}
+        <PagePathLabel path={notification.parsedSnapshot?.path ?? ''} />
       </div>
       <i className={`${actionIcon} me-2`} />
       <FormattedDistanceDate
--- apps/app/src/server/routes/apiv3/in-app-notification.ts
+++ apps/app/src/server/routes/apiv3/in-app-notification.ts
@@ -1,5 +1,8 @@
+import type { IUser, IPage } from '@growi/core';
+
 import { SupportedAction } from '~/interfaces/activity';
 import { generateAddActivityMiddleware } from '~/server/middlewares/add-activity';
+
 
 import { IInAppNotification } from '../../../interfaces/in-app-notification';
 
@@ -48,12 +51,12 @@ module.exports = (crowi) => {
       return activities.map(({ user }) => user).filter((user, i, self) => self.indexOf(user) === i);
     };
 
-    const serializedDocs: Array<IInAppNotification> = paginationResult.docs.map((doc) => {
+    const serializedDocs: Array<IInAppNotification<IUser | IPage>> = paginationResult.docs.map((doc) => {
       if (doc.user != null && doc.user instanceof User) {
         doc.user = serializeUserSecurely(doc.user);
       }
       // To add a new property into mongoose doc, need to change the format of doc to an object
-      const docObj: IInAppNotification = doc.toObject();
+      const docObj: IInAppNotification<IUser | IPage> = doc.toObject();
       const actionUsersNew = getActionUsersFromActivities(doc.activities);
 
       const serializedActionUsers = actionUsersNew.map((actionUser) => {
