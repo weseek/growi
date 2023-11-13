d00f44cf719171f8c7d1c88981804148d03b4fca
ryoji-s <ryoji-s@weseek.co.jp>
2023-11-13 11:46:25 +0000

    imporv on activated

--- apps/app/src/server/events/user.ts
+++ apps/app/src/server/events/user.ts
@@ -2,8 +2,12 @@ import EventEmitter from 'events';
 
 import type { IUserHasId } from '@growi/core';
 import { pagePathUtils } from '@growi/core/dist/utils';
+import mongoose from 'mongoose';
 
+import type { PageModel } from '~/server/models/page';
 import loggerFactory from '~/utils/logger';
+
+import PageService from '../service/page';
 
 const logger = loggerFactory('growi:events:user');
 
@@ -11,31 +15,37 @@ class UserEvent extends EventEmitter {
 
   crowi: any;
 
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  pageService: PageService | null;
+
   constructor(crowi: any) {
     super();
     this.crowi = crowi;
+    this.pageService = this.crowi.pageService;
   }
 
   async onActivated(user: IUserHasId): Promise<void> {
     try {
-      if (this.crowi.pageService === null) {
+      if (this.pageService === null) {
         throw new Error('crowi pageService is null');
       }
 
-      const Page = this.crowi.model('Page');
+      const Page = mongoose.model('Page') as unknown as PageModel;
       const userHomepagePath = pagePathUtils.userHomepagePath(user);
 
       let page = await Page.findByPath(userHomepagePath, true);
 
+      // TODO: Make it more type safe
+      // Since the type of page.creator is 'any', we resort to the following comparison,
+      // checking if page.creator.toString() is not equal to user._id.toString().
+      // Our code covers null, string, or object types.
       if (page != null && page.creator != null && page.creator.toString() !== user._id.toString()) {
-        await this.crowi.pageService.deleteCompletelyUserHomeBySystem(userHomepagePath);
+        await this.pageService.deleteCompletelyUserHomeBySystem(userHomepagePath);
         page = null;
       }
 
       if (page == null) {
         const body = `# ${user.username}\nThis is ${user.username}'s page`;
-        await this.crowi.pageService.create(userHomepagePath, body, user, {});
+        await this.pageService.create(userHomepagePath, body, user, {});
         logger.debug('User page created', page);
       }
     }
