de2480cd480ea93c09ccbe96db203f2d52dcc927
Shun Miyazawa <m0zurillex@gmail.com>
2023-11-13 02:09:43 +0000

    add search button

---
+++ apps/app/src/components/PageControls/SearchButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchButton = (): JSX.Element => {
+  return (
+    <button type="button" className="btn">
+      <span className="material-symbols-outlined me-1">search</span>
+    </button>
+  );
+};
+
+export default SearchButton;
