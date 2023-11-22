c917d4618373237b040ff48467ca265651849b93
soumaeda <soumaeda1126@icloud.com>
2023-11-22 06:36:03 +0000

    134432 remove wrap with handler

--- packages/editor/src/components/CodeMirrorEditor/Toolbar/TableButton.tsx
+++ packages/editor/src/components/CodeMirrorEditor/Toolbar/TableButton.tsx
@@ -4,11 +4,8 @@ type TableButtonProps = {
 
 export const TableButton = (props: TableButtonProps): JSX.Element => {
   const { onClickTableBtn } = props;
-  const openTableModalHandler = () => {
-    onClickTableBtn();
-  };
   return (
-    <button type="button" className="btn btn-toolbar-button" onClick={openTableModalHandler}>
+    <button type="button" className="btn btn-toolbar-button" onClick={onClickTableBtn}>
       <span className="material-symbols-outlined fs-5">table_chart</span>
     </button>
   );
