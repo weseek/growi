367e536ec617bcb9d0b21cee7124132d2cfb5915
Shun Miyazawa <m0zurillex@gmail.com>
2023-11-27 08:12:23 +0000

    Apply styles

--- apps/app/src/components/PageControls/SearchButton.tsx
+++ apps/app/src/components/PageControls/SearchButton.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import styles from './SearchButton.module.scss';
+
 const SearchButton = (): JSX.Element => {
   return (
-    <button type="button" className="btn border-0 d-flex align-items-center">
-      <span className="material-symbols-outlined">search</span>
+    <button type="button" className={`me-3 btn border-0 d-flex align-items-center ${styles['btn-search']}`}>
+      <span className="material-symbols-outlined fill">search</span>
     </button>
   );
 };
