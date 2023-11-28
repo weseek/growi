f225150dc1fa3dabbf56a8032297853a0d88aab8
yukendev <yuken0906dev@gmail.com>
2023-11-28 03:48:25 +0000

    Revert "feat: i18n to UISettings component"
    This reverts commit 0ced3b22629d1b3e5ba7cbb3eec052aa12e3defc.

--- apps/app/src/components/Me/UISettings.tsx
+++ apps/app/src/components/Me/UISettings.tsx
@@ -5,16 +5,16 @@ export const UISettings = (): JSX.Element => {
   const { t } = useTranslation();
   return (
     <>
-      <h2 className="border-bottom mb-4">{t('ui_settings.ui_settings')}</h2>
+      <h2 className="border-bottom mb-4">UI設定</h2>
 
       <div className="row justify-content-center">
         <div className="col-md-6">
           <div className="form-check form-switch form-check-primary">
             <input type="checkbox" className="form-check-input" id="isQuestionnaireEnabled" onChange={() => {}} />
             <label className="form-label form-check-label" htmlFor="isQuestionnaireEnabled">
-              {t('ui_settings.wide_view.enable_wide_view')}
+              ワイドビューを有効にする
             </label>
-            <p className="form-text text-muted small">{t('ui_settings.wide_view.description')}</p>
+            <p className="form-text text-muted small">ワイドビューを有効にすると、ページ内コンテンツがページの横幅いっぱいに広がります</p>
           </div>
         </div>
       </div>
