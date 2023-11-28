212eb42877b8ae7112173f11dbeb09d863c12fe9
Shun Miyazawa <m0zurillex@gmail.com>
2023-11-28 05:18:08 +0000

    repalce to material-icon

--- apps/app/src/features/approval-workflow/client/components/ModalComponents/WorkflowModalHeader.tsx
+++ apps/app/src/features/approval-workflow/client/components/ModalComponents/WorkflowModalHeader.tsx
@@ -19,8 +19,8 @@ export const WorkflowModalHeader = (props: Props): JSX.Element => {
     <ModalHeader toggle={close}>
       <div className="d-flex align-items-center">
         { onClickPageBackButton != null && (
-          <button type="button" className="btn" onClick={onClickPageBackButton}>
-            <i className="fa fa-fw fa-chevron-left" aria-hidden="true" />
+          <button type="button" className="btn d-flex justify-content-center p-0 me-3" onClick={onClickPageBackButton}>
+            <span className="fs-1 material-symbols-outlined" aria-hidden="true">chevron_left</span>
           </button>
         ) }
 
