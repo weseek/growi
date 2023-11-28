f17c20ff290adb83ed11889016b30ba9b414459a
Yuki Takei <yuki@weseek.co.jp>
2023-11-28 14:38:42 +0000

    refactor

--- apps/app/src/components/ShareLinkPageView.tsx
+++ apps/app/src/components/ShareLinkPageView.tsx
@@ -3,7 +3,7 @@ import React, { useMemo } from 'react';
 import type { IPagePopulatedToShowRevision } from '@growi/core';
 import dynamic from 'next/dynamic';
 
-import { useLayoutFluid } from '~/client/services/layout';
+import { useShouldExpandContent } from '~/client/services/layout';
 import type { RendererConfig } from '~/interfaces/services/renderer';
 import type { IShareLinkHasId } from '~/interfaces/share-link';
 import { generateSSRViewOptions } from '~/services/renderer/renderer';
@@ -45,7 +45,7 @@ export const ShareLinkPageView = (props: Props): JSX.Element => {
 
   const { data: viewOptions } = useViewOptions();
 
-  const isLayoutFluid = useLayoutFluid(page);
+  const shouldExpandContent = useShouldExpandContent(page);
 
   const isNotFound = isNotFoundMeta || page == null || shareLink == null;
 
@@ -96,7 +96,7 @@ export const ShareLinkPageView = (props: Props): JSX.Element => {
     <PageViewLayout
       headerContents={headerContents}
       sideContents={sideContents}
-      isLayoutFluid={isLayoutFluid}
+      expandContentWidth={shouldExpandContent}
     >
       { specialContents }
       { specialContents == null && (
