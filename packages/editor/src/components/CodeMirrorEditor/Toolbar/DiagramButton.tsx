2f792a617bdd1759d630cf57600b9d3c875f3da5
soumaeda <soumaeda1126@icloud.com>
2023-11-29 05:42:56 +0000

    135366 add dependency

--- packages/editor/src/components/CodeMirrorEditor/Toolbar/DiagramButton.tsx
+++ packages/editor/src/components/CodeMirrorEditor/Toolbar/DiagramButton.tsx
@@ -6,7 +6,7 @@ export const DiagramButton = (): JSX.Element => {
   const { open: openDrawioModal } = useDrawioModalForEditor();
   const openDrawioModalHandler = useCallback(() => {
     openDrawioModal();
-  }, []);
+  }, [openDrawioModal]);
   return (
     <button type="button" className="btn btn-toolbar-button" onClick={openDrawioModalHandler}>
       <span className="material-symbols-outlined fs-5">lan</span>
