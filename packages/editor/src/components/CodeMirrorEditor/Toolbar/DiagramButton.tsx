47b77ff5bf69385cda292a6a991a6c66b770efc4
soumaeda <soumaeda1126@icloud.com>
2023-12-04 04:53:21 +0000

    change handler name

--- packages/editor/src/components/CodeMirrorEditor/Toolbar/DiagramButton.tsx
+++ packages/editor/src/components/CodeMirrorEditor/Toolbar/DiagramButton.tsx
@@ -4,11 +4,11 @@ import { useDrawioModalForEditor } from '../../../stores/use-drawio';
 
 export const DiagramButton = (): JSX.Element => {
   const { open: openDrawioModal } = useDrawioModalForEditor();
-  const openDrawioModalHandler = useCallback(() => {
+  const onClickOpenDrawioModal = useCallback(() => {
     openDrawioModal();
   }, [openDrawioModal]);
   return (
-    <button type="button" className="btn btn-toolbar-button" onClick={openDrawioModalHandler}>
+    <button type="button" className="btn btn-toolbar-button" onClick={onClickOpenDrawioModal}>
       <span className="material-symbols-outlined fs-5">lan</span>
     </button>
   );
