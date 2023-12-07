6f93a698cf2e8c576f9915661971285f8a1a671b
soumaeda <soumaeda1126@icloud.com>
2023-12-07 07:28:36 +0000

    135426 use firstLineNum and lastLineNum as const

--- apps/app/src/components/PageEditor/MarkdownDrawioUtil.js
+++ apps/app/src/components/PageEditor/MarkdownDrawioUtil.js
@@ -69,7 +69,8 @@ class MarkdownDrawioUtil {
       return null;
     }
 
-    const botLine = Math.max(this.firstLineNum(), line);
+    const firstLineNum = this.firstLineNum();
+    const botLine = Math.max(firstLineNum, line);
     return this.getLine(editor, botLine).from;
   }
 
@@ -102,7 +103,8 @@ class MarkdownDrawioUtil {
       return null;
     }
 
-    const eodLine = Math.min(line, this.lastLineNum(editor));
+    const lastLineNum = this.lastLineNum(editor);
+    const eodLine = Math.min(line, lastLineNum);
     return this.getLine(editor, eodLine).to;
   }
 
@@ -195,7 +197,7 @@ class MarkdownDrawioUtil {
     const lineNumbers = [];
     // refs: https://github.com/codemirror/CodeMirror/blob/5.64.0/addon/fold/foldcode.js#L106-L111
     for (let i = this.firstLineNum(), e = this.lastLineNum(editor); i <= e; i++) {
-      const lineText = this.getLine(editor, i + 1).text;
+      const lineText = this.getLine(editor, i).text;
       const match = this.lineBeginPartOfDrawioRE.exec(lineText);
       if (match) {
         lineNumbers.push(i);
