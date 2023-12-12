a3bf373a9521119463fabaf587325e1ab414cb2a
soumaeda <soumaeda1126@icloud.com>
2023-12-12 03:21:03 +0000

    use edlitorKey as not optional

--- packages/editor/src/stores/use-drawio.ts
+++ packages/editor/src/stores/use-drawio.ts
@@ -7,13 +7,13 @@ type DrawioModalSaveHandler = () => void;
 
 type DrawioModalStatus = {
   isOpened: boolean,
-  editorKey?: string,
+  editorKey: string,
   onSave?: DrawioModalSaveHandler,
 }
 
 type DrawioModalStatusUtils = {
   open(
-    editorKey?: string,
+    editorKey: string,
     onSave?: DrawioModalSaveHandler,
   ): void,
   close(): void,
@@ -22,17 +22,18 @@ type DrawioModalStatusUtils = {
 export const useDrawioModalForEditor = (status?: DrawioModalStatus): SWRResponse<DrawioModalStatus, Error> & DrawioModalStatusUtils => {
   const initialData: DrawioModalStatus = {
     isOpened: false,
+    editorKey: '',
   };
   const swrResponse = useSWRStatic<DrawioModalStatus, Error>('drawioModalStatus', status, { fallbackData: initialData });
 
   const { mutate } = swrResponse;
 
-  const open = useCallback((editorKey?: string, onSave?: DrawioModalSaveHandler): void => {
+  const open = useCallback((editorKey: string, onSave?: DrawioModalSaveHandler): void => {
     mutate({ isOpened: true, editorKey, onSave });
   }, [mutate]);
 
   const close = useCallback((): void => {
-    mutate({ isOpened: false, editorKey: undefined, onSave: undefined });
+    mutate({ isOpened: false, editorKey: '', onSave: undefined });
   }, [mutate]);
 
   return {
