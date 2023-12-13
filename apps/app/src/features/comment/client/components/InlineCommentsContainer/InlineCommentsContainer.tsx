d77df865bf892f36c5fe8c3daf41cd5b6a2bbd39
Yuki Takei <yuki@weseek.co.jp>
2023-12-13 11:23:57 +0000

    impl findWikiElement

--- apps/app/src/features/comment/client/components/InlineCommentsContainer/InlineCommentsContainer.tsx
+++ apps/app/src/features/comment/client/components/InlineCommentsContainer/InlineCommentsContainer.tsx
@@ -1,12 +1,32 @@
 import {
-  type ReactNode, useRef, useMemo,
+  type ReactNode, useRef, useMemo, RefObject,
 } from 'react';
 
 import { useRenderedObserver } from '@growi/ui/dist/utils';
 
+import loggerFactory from '~/utils/logger';
+
 import { useSWRxInlineComment } from '../../stores';
 
 import { InlineComment } from './InlineComment';
+
+
+const logger = loggerFactory('growi:components:InlineCommentsContainer');
+
+
+const findWikiElement = (containerRef: RefObject<Element>): Element | undefined => {
+  const wikiElements = containerRef.current?.getElementsByClassName('wiki');
+
+  if (wikiElements == null || wikiElements.length === 0) {
+    return;
+  }
+  if (wikiElements.length !== 1) {
+    logger.error("Could not identify target wiki element. Multiple element that has '.wiki' class has found.");
+    return;
+  }
+
+  return wikiElements[0];
+};
 
 export const InlineCommentsContainer = ({ children }: { children?: ReactNode }): JSX.Element => {
 
@@ -14,10 +34,18 @@ export const InlineCommentsContainer = ({ children }: { children?: ReactNode }):
 
   const containerRef = useRef<HTMLDivElement>(null);
 
+  // create InlineComment components after the container element has rendered
   const { isRendering } = useRenderedObserver(containerRef);
-
   const inlineCommentComponents = useMemo(() => {
     if (isRendering !== false) return <></>;
+
+    const wikiElement = findWikiElement(containerRef);
+
+    if (wikiElement == null) {
+      return <></>;
+    }
+
+    console.log({ wikiElement });
 
     return (inlineComments ?? []).map((inlineComment) => {
       return <InlineComment key={inlineComment._id} inlineComment={inlineComment} />;
