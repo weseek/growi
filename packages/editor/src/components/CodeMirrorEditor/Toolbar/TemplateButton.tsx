2a08c1581d4ef012f62f056ffee0e53b248f727e
soumaeda <soumaeda1126@icloud.com>
2023-12-14 04:09:10 +0000

    136480 enable templete modal in editor

--- packages/editor/src/components/CodeMirrorEditor/Toolbar/TemplateButton.tsx
+++ packages/editor/src/components/CodeMirrorEditor/Toolbar/TemplateButton.tsx
@@ -1,6 +1,27 @@
-export const TemplateButton = (): JSX.Element => {
+import { useCallback } from 'react';
+
+
+import { useCodeMirrorEditorIsolated } from '../../../stores';
+import { useTemplateModalForEditor } from '../../../stores/use-templete';
+
+type Props = {
+  editorKey: string,
+}
+
+export const TemplateButton = (props: Props): JSX.Element => {
+  const { editorKey } = props;
+  const { data: codeMirrorEditor } = useCodeMirrorEditorIsolated(editorKey);
+  const { open: openTempleteModal } = useTemplateModalForEditor();
+
+  const onClickTempleteButton = useCallback(() => {
+    const editor = codeMirrorEditor?.view;
+    const insertText = (text: string) => editor?.dispatch(editor.state.replaceSelection(text));
+    const onSubmit = (templateText: string) => insertText(templateText);
+    openTempleteModal({ onSubmit });
+  }, [codeMirrorEditor?.view, openTempleteModal]);
+
   return (
-    <button type="button" className="btn btn-toolbar-button">
+    <button type="button" className="btn btn-toolbar-button" onClick={onClickTempleteButton}>
       <span className="material-symbols-outlined fs-5">file_copy</span>
     </button>
   );
