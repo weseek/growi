b6b5aee9826f25433ab01adf1dfad9c5417cc2d4
WNomunomu <kohsei.nmr@gmail.com>
2023-12-25 07:15:04 +0000

    create Successfully create untitled page test

--- apps/app/test/cypress/e2e/50-sidebar/50-sidebar--access-to-side-bar.cy.ts
+++ apps/app/test/cypress/e2e/50-sidebar/50-sidebar--access-to-side-bar.cy.ts
@@ -313,6 +313,28 @@ describe('Access to sidebar', () => {
           cy.screenshot(`${ssPrefix}access-to-trash-page`, { blackout: blackoutOverride });
         });
       });
+
+      describe('Test PageCreateButton', () => {
+        beforeEach(() => {
+          cy.getByTestid('grw-sidebar-nav-page-create-button').should('be.visible')
+            .then($elem => {
+              if (!$elem.hasClass('active')) {
+                cy.getByTestid('grw-sidebar-nav-page-create-button').click();
+              }
+            })
+        });
+
+        it('Successfully create untitled page', () => {
+          cy.visit('/');
+
+          cy.getByTestid('grw-sidebar-nav-page-create-button').click();
+
+          cy.getByTestid('page-editor').should('be.visible');
+          cy.getByTestid('save-page-btn').as('save-page-btn').should('be.visible');
+
+          cy.screenshot(`${ssPrefix}create-untitled-page`);
+        });
+      });
     });
   });
 });
