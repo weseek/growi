490017626762bed4f56d7ccf4b32742e5e1a28ac
soumaeda <soumaeda1126@icloud.com>
2023-12-26 07:17:33 +0000

    able to insert list number as one

--- packages/editor/src/components/CodeMirrorEditor/CodeMirrorEditor.tsx
+++ packages/editor/src/components/CodeMirrorEditor/CodeMirrorEditor.tsx
@@ -46,6 +46,66 @@ export const CodeMirrorEditor = (props: Props): JSX.Element => {
   }, [onChange]);
   const { data: codeMirrorEditor } = useCodeMirrorEditorIsolated(editorKey, containerRef.current, cmProps);
 
+  /**
+   * return the postion of the BOL(beginning of line)
+   */
+  const getBol = (editor: EditorView) => {
+    const curPos = editor.state.selection.main.head;
+    return editor.state.doc.lineAt(curPos).from;
+  };
+
+  const getStrFromBol = (editor: EditorView) => {
+    const curPos = editor.state.selection.main.head;
+    return editor.state.sliceDoc(getBol(editor), curPos);
+  };
+
+  const selectLowerPos = (editor: EditorView, pos1: number, pos2: number) => {
+    // if both is in same line
+    if (editor.state.doc.lineAt(pos1).number === editor.state.doc.lineAt(pos2).number) {
+      return (editor.state.doc.lineAt(pos1).from < editor.state.doc.lineAt(pos1).to) ? pos2 : pos1;
+    }
+    return (editor.state.doc.lineAt(pos1) < editor.state.doc.lineAt(pos2)) ? pos2 : pos1;
+  };
+
+  const replaceBolToCurrentPos = (editor: EditorView, text: string) => {
+    const curPos = editor.state.selection.main.head;
+    const pos = selectLowerPos(editor, editor.state.doc.lineAt(curPos).from, editor.state.doc.lineAt(curPos).to);
+    editor.dispatch({
+      changes: {
+        from: getBol(editor),
+        to: pos,
+        insert: text,
+      },
+    });
+  };
+
+  const indentAndMarkOnlyRE = /^(\d+)[.)](\s*)$/;
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const newlineAndIndentContinueMarkdownList = (editor: EditorView) => {
+    const strFromBol = getStrFromBol(editor);
+
+    if (indentAndMarkOnlyRE.test(strFromBol)) {
+      replaceBolToCurrentPos(editor, '1. ');
+    }
+  };
+
+  useEffect(() => {
+    const handleEnterKey = (event: KeyboardEvent) => {
+      if (event.key === 'Enter') {
+        event.preventDefault();
+        const editor = codeMirrorEditor?.view;
+        newlineAndIndentContinueMarkdownList(editor);
+      }
+    };
+
+    codeMirrorEditor?.view?.dom.addEventListener('keydown', handleEnterKey);
+
+    return () => {
+      codeMirrorEditor?.view?.dom.removeEventListener('keydown', handleEnterKey);
+    };
+  }, [codeMirrorEditor, newlineAndIndentContinueMarkdownList]);
+
   useEffect(() => {
     if (indentSize == null) {
       return;
