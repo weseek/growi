0f347ed3039aed76475d13b422566de3cda114e9
Ryu Sato <ryu@weseek.co.jp>
2023-12-29 04:45:49 +0000

    use unzip-stream instead of unzipper to extract plugin

--- apps/app/src/features/growi-plugin/server/services/growi-plugin/growi-plugin.ts
+++ apps/app/src/features/growi-plugin/server/services/growi-plugin/growi-plugin.ts
@@ -10,7 +10,7 @@ import axios from 'axios';
 import mongoose from 'mongoose';
 import sanitize from 'sanitize-filename';
 import streamToPromise from 'stream-to-promise';
-import unzipper from 'unzipper';
+import unzip from 'unzip-stream';
 
 import loggerFactory from '~/utils/logger';
 
@@ -205,7 +205,7 @@ export class GrowiPluginService implements IGrowiPluginService {
   private async unzip(zipFilePath: fs.PathLike, destPath: fs.PathLike): Promise<void> {
     try {
       const stream = fs.createReadStream(zipFilePath);
-      const unzipStream = stream.pipe(unzipper.Extract({ path: destPath }));
+      const unzipStream = stream.pipe(unzip.Extract({ path: destPath }));
 
       await streamToPromise(unzipStream);
     }
