f5c3714e44fa1c4fc9bd8390c134b74e3fb3a66e
Shun Miyazawa <m0zurillex@gmail.com>
2024-01-10 08:55:32 +0000

    Add sidebar primary icon

--- apps/app/src/interfaces/ui.ts
+++ apps/app/src/interfaces/ui.ts
@@ -14,6 +14,7 @@ export const SidebarContentsType = {
   TREE: 'tree',
   TAG: 'tag',
   BOOKMARKS: 'bookmarks',
+  NOTIFICATION: 'notification',
 } as const;
 export const AllSidebarContentsType = Object.values(SidebarContentsType);
 export type SidebarContentsType = typeof SidebarContentsType[keyof typeof SidebarContentsType];
