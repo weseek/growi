f0d87cbde6980a8d9c610e074124486d8ef5351b
Shun Miyazawa <m0zurillex@gmail.com>
2024-01-11 07:40:12 +0000

    add TODO

--- apps/app/src/components/Sidebar/InAppNotification/InAppNotificationSubstance.tsx
+++ apps/app/src/components/Sidebar/InAppNotification/InAppNotificationSubstance.tsx
@@ -5,6 +5,7 @@ import { useSWRxInAppNotifications } from '~/stores/in-app-notification';
 
 export const InAppNotificationSubstance = (): JSX.Element => {
 
+  // TODO: Infinite scroll implemented (https://redmine.weseek.co.jp/issues/138057)
   const { data: inAppNotificationData } = useSWRxInAppNotifications(6, undefined, undefined, { revalidateOnFocus: true });
 
   return (
