bee0c100d20c89016cce3b88b683c317e38b72d9
Shun Miyazawa <m0zurillex@gmail.com>
2024-01-11 02:26:45 +0000

    Show notification in sidebar

--- apps/app/src/components/Sidebar/InAppNotification/InAppNotificationSubstance.tsx
+++ apps/app/src/components/Sidebar/InAppNotification/InAppNotificationSubstance.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import InAppNotificationList from '~/components/InAppNotification/InAppNotificationList';
+import { useSWRxInAppNotifications } from '~/stores/in-app-notification';
+
 export const InAppNotificationSubstance = (): JSX.Element => {
 
+  const { data: inAppNotificationData } = useSWRxInAppNotifications(6, undefined, undefined, { revalidateOnFocus: true });
+
   return (
-    <>InAppNotificationSubstance</>
+    <>
+      <InAppNotificationList type="dropdown-item" inAppNotificationData={inAppNotificationData} />
+    </>
   );
 };
