a95c36f924c82e447da92d9dcfab4489375b9ef8
Shun Miyazawa <m0zurillex@gmail.com>
2024-01-20 09:44:53 +0000

    Add component test for PageItemControl.tsx

---
+++ apps/app/src/components/Common/Dropdown/PageItemControl.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { render, screen, fireEvent } from '@testing-library/react';
+
+import { PageItemControl } from './PageItemControl';
+
+
+describe('PageItemControl.tsx', () => {
+
+  it('Should fire onClickRenameMenuItem() when clicking the rename button, with pageInfo.isDeletable being "false"', async() => {
+    // setup
+    const onClickRenameMenuItemMock = vi.fn(() => {});
+
+    const pageInfo = {
+      isV5Compatible: true,
+      isEmpty: false,
+      isDeletable: false,
+      isAbleToDeleteCompletely: true,
+      isRevertible: true,
+    };
+
+    const props = {
+      pageId: 'dummy-page-id',
+      isEnableActions: true,
+      pageInfo,
+      onClickRenameMenuItem: onClickRenameMenuItemMock,
+    };
+
+    render(<PageItemControl {...props} />);
+
+    // when
+    const renameButton = screen.getByText('Move/Rename');
+    fireEvent.click(renameButton);
+
+    // then
+    expect(onClickRenameMenuItemMock).toHaveBeenCalled();
+  });
+});
