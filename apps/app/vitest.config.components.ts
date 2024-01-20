4a1f46f32b7d0d02a36b1ba61bb0687e5a6133ad
Shun Miyazawa <m0zurillex@gmail.com>
2024-01-20 08:35:00 +0000

    Add config file & npm-script for conponent test

---
+++ apps/app/vitest.config.components.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    include: [
+      '**/*.test.tsx', '**/*.test.jsx',
+    ],
+  },
+});
