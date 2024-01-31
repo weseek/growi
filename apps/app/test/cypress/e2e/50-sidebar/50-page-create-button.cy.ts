40abb2c6d46ac457eb3830c3a6d7ced626248a1a
kosei-n <kosei-n@weseek.co.jp>
2024-01-31 08:18:07 +0000

    implement test

--- apps/app/test/cypress/e2e/50-sidebar/50-page-create-button.cy.ts
+++ apps/app/test/cypress/e2e/50-sidebar/50-page-create-button.cy.ts
@@ -19,7 +19,7 @@ describe('Access to PageCreateButton', () => {
 
       //   it('Successfully create untitled page', () => {
       //     cy.waitUntil(() => cy.getByTestid('grw-sidebar-nav-page-create-button').should('be.visible'));
-      //     cy.getByTestid('grw-sidebar-nav-page-create-button').click({force: true});
+      //     cy.getByTestid('grw-sidebar-nav-page-create-button').click({ force: true });
 
       //     cy.waitUntil(() => cy.getByTestid('page-editor').should('be.visible'));
       //     cy.getByTestid('save-page-btn').as('save-page-btn').should('be.visible');
@@ -31,20 +31,33 @@ describe('Access to PageCreateButton', () => {
 
       describe('Test PageCreateButton dropdown', () => {
 
-        it('Successfully create untitled page', () => {
+        beforeEach(() => {
           cy.waitUntil(() => cy.getByTestid('grw-sidebar-nav-page-create-button').should('be.visible'));
-          cy.getByTestid('grw-sidebar-nav-page-create-button').trigger('mouseover', {force: true});
-          cy.getByTestid('grw-sidebar-nav-dropend-toggle').click({force: true});
-          cy.getByTestid('grw-sidebar-nav-page-create-dropdown-item').click({force: true});
-
-          cy.waitUntil(() => cy.getByTestid('page-editor').should('be.visible'));
-          cy.getByTestid('save-page-btn').as('save-page-btn').should('be.visible');
-
-          cy.screenshot(`${ssPrefix}create-untitled-page`, { capture: 'fullPage' });
+          cy.getByTestid('grw-sidebar-nav-page-create-button').trigger('mouseover', { force: true });
+          cy.getByTestid('grw-sidebar-nav-dropend-toggle').click({ force: true });
         });
-        // it("Successfully create today's memo", () => {});
-        // it('Successfully create children template', () => {});
-        // it('Successfully create descendants template', () => {});
+
+        // it('Successfully create untitled page', () => {
+        //   cy.getByTestid('grw-sidebar-nav-page-create-dropdown-item').click({ force: true });
+
+        //   cy.waitUntil(() => cy.getByTestid('page-editor').should('be.visible'));
+        //   cy.getByTestid('save-page-btn').as('save-page-btn').should('be.visible');
+
+        //   cy.screenshot(`${ssPrefix}create-untitled-page`, { capture: 'fullPage' });
+        // });
+
+        // it("Successfully create today's memo", () => {
+        //   cy.getByTestid("grw-sidebar-nav-today's-memo-crate-dropdown-item").click({ force:true });
+        //   // 確実にtoday's memoが作成された保証を得て、スクショ撮影
+        // });
+
+        it('Successfully create children template', () => {
+          cy.getByTestid("grw-sidebar-nav-children-template-create-dropdown-item").click({ force:true });
+        });
+
+        // it('Successfully create descendants template', () => {
+        //   cy.getByTestid("grw-sidebar-nav-descendants-template-create-dropdown-item").click({ force:true });
+        // });
 
       });
     });
