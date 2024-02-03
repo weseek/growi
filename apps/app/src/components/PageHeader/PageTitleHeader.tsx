6a6dec5d00feee0536c71a29dea19b6f4f392ffe
kosei-n <kosei-n@weseek.co.jp>
2024-02-03 06:53:44 +0000

    implement an operation when escape key is pressed

--- apps/app/src/components/PageHeader/PageTitleHeader.tsx
+++ apps/app/src/components/PageHeader/PageTitleHeader.tsx
@@ -1,5 +1,7 @@
 import type { FC } from 'react';
-import { useState, useMemo, useEffect } from 'react';
+import {
+  useState, useMemo, useEffect, useRef,
+} from 'react';
 
 import nodePath from 'path';
 
@@ -12,12 +14,14 @@ import { usePagePathRenameHandler } from './page-header-utils';
 
 
 export const PageTitleHeader: FC<Props> = (props) => {
-  const { currentPagePath, currentPage } = props;
+  const { currentPagePath, currentPage, editingPagePathHandler } = props;
   const pageTitle = nodePath.basename(currentPagePath ?? '') || '/';
 
   const [isRenameInputShown, setRenameInputShown] = useState(false);
 
-  const { editingPagePath, setEditingPagePath } = props.editingPagePathHandler;
+  const ref = useRef<HTMLDivElement>(null);
+
+  const { editingPagePath, setEditingPagePath } = editingPagePathHandler;
 
   const editingPageTitle = nodePath.basename(editingPagePath);
 
@@ -52,15 +56,55 @@ export const PageTitleHeader: FC<Props> = (props) => {
     pagePathRenameHandler(editingPagePath);
   };
 
+  // useEffect(() => {
+  //   const element = ref.current;
+
+  //   if (element == null) {
+  //     return;
+  //   }
+
+  //   const handleClickOutside = (e) => {
+  //     if (!element.contains(e.target)) {
+  //       pagePathRenameHandler(editingPagePath);
+  //     }
+  //   };
+
+  //   document.addEventListener('click', handleClickOutside);
+
+  //   return () => {
+  //     document.removeEventListener('click', handleClickOutside);
+  //   };
+
+  // }, []);
+
+  // const clickOutSideHandler = (e) => {
+  //   const container = document.getElementById('page-title-header');
+
+  //   if (container && !container.contains(e.target)) {
+  //     pagePathRenameHandler(editingPagePath);
+  //     console.log('click outside');
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   document.addEventListener('click', clickOutSideHandler);
+
+  //   return () => {
+  //     document.removeEventListener('click', clickOutSideHandler);
+  //   };
+  // }, []);
+
   return (
     <div
       className="row"
-      onBlur={onBlurHandler}
+      // onBlur={onBlurHandler}
+      ref={ref}
     >
       <div className="col-4">
         <TextInputForPageTitleAndPath
           currentPage={currentPage}
           stateHandler={stateHandler}
+          editingPagePathHandler={editingPagePathHandler}
           inputValue={editingPageTitle}
           CustomComponent={PageTitle}
           handleInputChange={handleInputChange}
