c5ed3fb9b2d8d86e5a32ecdfe82c9a474bf74021
Shun Miyazawa <m0zurillex@gmail.com>
2024-02-05 06:02:42 +0000

    impl

--- apps/app/src/components/Common/PagePathHierarchicalLink/CollapsedParentsDropdown.tsx
+++ apps/app/src/components/Common/PagePathHierarchicalLink/CollapsedParentsDropdown.tsx
@@ -1,12 +1,26 @@
+import { useMemo } from 'react';
+
+import Link from 'next/link';
 import {
   DropdownItem, DropdownMenu, DropdownToggle, UncontrolledDropdown,
 } from 'reactstrap';
 
-import LinkedPagePath from '~/models/linked-page-path';
-
+import type LinkedPagePath from '~/models/linked-page-path';
 
 import styles from './CollapsedParentsDropdown.module.scss';
 
+const getAncestorPathAndPathName = (linkedPagePath: LinkedPagePath) => {
+  const data: Array<{ path: string, pathName: string }> = [];
+  let linkedPagePathTmp = linkedPagePath;
+
+  while (linkedPagePathTmp.parent != null) {
+    data.push({ path: linkedPagePathTmp.path, pathName: linkedPagePathTmp.pathName });
+    linkedPagePathTmp = linkedPagePathTmp.parent;
+  }
+
+  const reversedData = data.reverse();
+  return reversedData;
+};
 
 type Props = {
   linkedPagePath: LinkedPagePath,
@@ -15,20 +29,19 @@ type Props = {
 export const CollapsedParentsDropdown = (props: Props): JSX.Element => {
   const { linkedPagePath } = props;
 
+  const ancestorPathAndPathName = useMemo(() => getAncestorPathAndPathName(linkedPagePath), [linkedPagePath]);
+
   return (
     <UncontrolledDropdown className="d-inline-block">
       <DropdownToggle color="transparent">...</DropdownToggle>
       <DropdownMenu className={`dropdown-menu ${styles['collapsed-parents-dropdown-menu']}`} container="body">
-        {/* TODO: generate DropdownItems */}
-        <DropdownItem>
-          <a role="menuitem">foo</a>
-        </DropdownItem>
-        <DropdownItem>
-          <a role="menuitem">bar</a>
-        </DropdownItem>
-        <DropdownItem>
-          <a role="menuitem">baz</a>
-        </DropdownItem>
+        {ancestorPathAndPathName.map(data => (
+          <DropdownItem key={data.path}>
+            <Link href={data.path} legacyBehavior>
+              <a role="menuitem">{data.pathName}</a>
+            </Link>
+          </DropdownItem>
+        ))}
       </DropdownMenu>
     </UncontrolledDropdown>
   );
