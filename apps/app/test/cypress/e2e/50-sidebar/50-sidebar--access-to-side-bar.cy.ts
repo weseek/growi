cf7d8b9ffe4557045c0649501aa8b228a34c9c41
kosei-n <kosei-n@weseek.co.jp>
2024-02-08 05:04:45 +0000

    replace test of PageCreateButton

--- apps/app/test/cypress/e2e/50-sidebar/50-sidebar--access-to-side-bar.cy.ts
+++ apps/app/test/cypress/e2e/50-sidebar/50-sidebar--access-to-side-bar.cy.ts
@@ -313,6 +313,73 @@ describe('Access to sidebar', () => {
         });
       });
 
+      describe('Test PageCreateButton', () => {
+
+        it('Successfully create untitled page', () => {
+          cy.waitUntil(() => cy.getByTestid('grw-sidebar-nav-page-create-button').should('be.visible'));
+          cy.getByTestid('grw-sidebar-nav-page-create-button').click({ force: true });
+
+          cy.waitUntil(() => cy.getByTestid('page-editor').should('be.visible'));
+          cy.getByTestid('save-page-btn').as('save-page-btn').should('be.visible');
+
+          cy.screenshot(`${ssPrefix}create-untitled-page`, { capture: 'fullPage' });
+        });
+
+      });
+
+      describe('Test PageCreateButton dropdown', () => {
+        beforeEach(() => {
+          cy.waitUntil(() => cy.getByTestid('grw-sidebar-nav-page-create-button').should('be.visible'));
+          cy.getByTestid('grw-sidebar-nav-page-create-button').trigger('mouseover', { force: true });
+          cy.getByTestid('grw-sidebar-nav-dropend-toggle').click({ force: true });
+        });
+
+        it ('PageCreateButton dropdown is shown and closed successfully', () => {
+          cy.waitUntil(() => cy.getByTestid('grw-sidebar-nav-page-create-dropdown-item').should('be.visible'));
+          cy.screenshot(`${ssPrefix}PageCreateButton-dropdown-opened`);
+          cy.getByTestid('grw-sidebar-nav-dropend-toggle').click({ force: true });
+
+          cy.screenshot(`${ssPrefix}PageCreateButton-dropdown-closed`);
+        });
+
+        it('Successfully create untitled page', () => {
+          cy.getByTestid('grw-sidebar-nav-page-create-dropdown-item').click({ force: true });
+
+          cy.waitUntil(() => cy.getByTestid('page-editor').should('be.visible'));
+          cy.getByTestid('save-page-btn').as('save-page-btn').should('be.visible');
+
+          cy.screenshot(`${ssPrefix}create-untitled-page`, { capture: 'fullPage' });
+        });
+
+        it("Successfully create today page", () => {
+          cy.getByTestid("grw-sidebar-nav-today-page-create-dropdown-item").click({ force:true });
+
+          cy.waitUntil(() => cy.getByTestid('page-editor').should('be.visible'));
+          cy.getByTestid('save-page-btn').as('save-page-btn').should('be.visible');
+
+          cy.screenshot(`${ssPrefix}create-today-page`, { capture: 'fullPage' });
+        });
+
+        it('Successfully create children template', () => {
+          cy.getByTestid("grw-sidebar-nav-children-template-create-dropdown-item").click({ force:true });
+
+          cy.waitUntil(() => cy.getByTestid('page-editor').should('be.visible'));
+          cy.getByTestid('save-page-btn').as('save-page-btn').should('be.visible');
+
+          cy.screenshot(`${ssPrefix}create-children-template`, { capture: 'fullPage' });
+        });
+
+        it('Successfully create descendants template', () => {
+          cy.getByTestid("grw-sidebar-nav-descendants-template-create-dropdown-item").click({ force:true });
+
+          cy.waitUntil(() => cy.getByTestid('page-editor').should('be.visible'));
+          cy.getByTestid('save-page-btn').as('save-page-btn').should('be.visible');
+
+          cy.screenshot(`${ssPrefix}create-descendants-template`, { capture: 'fullPage' });
+        });
+
+      });
+
     });
   });
 });
