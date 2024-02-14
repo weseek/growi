7bdd75af6e6119ab261f1e8d7d6aef7ca6c9f42b
Shun Miyazawa <m0zurillex@gmail.com>
2024-02-14 10:58:02 +0000

    Optional

--- packages/core/src/utils/should-create-wip-page.ts
+++ packages/core/src/utils/should-create-wip-page.ts
@@ -2,7 +2,7 @@ import { checkTemplatePath } from './template-checker';
 
 /**
  * Returns Whether to create pages with the wip flag
- * @param {string} path
+ * @param {string|undefined} path
  * @returns {boolean}
  */
 export const shouldCreateWipPage = (path?: string): boolean => {
