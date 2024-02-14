2520b8e8ed4437796322a4483202dde7d9bafc63
kosei-n <kosei-n@weseek.co.jp>
2024-02-14 08:03:54 +0000

    create some callback functions

--- apps/app/src/components/PageSelectModal/PageSelectModal.tsx
+++ apps/app/src/components/PageSelectModal/PageSelectModal.tsx
@@ -1,16 +1,20 @@
 import type { FC } from 'react';
-import React from 'react';
+import { useState } from 'react';
+
+import nodePath from 'path';
 
 import { useTranslation } from 'next-i18next';
 import {
   Modal, ModalHeader, ModalBody, ModalFooter, Button,
 } from 'reactstrap';
 
+import type { IPageForItem } from '~/interfaces/page';
 import { useTargetAndAncestors, useIsGuestUser, useIsReadOnlyUser } from '~/stores/context';
 import { usePageSelectModal } from '~/stores/modal';
-import { useCurrentPagePath, useCurrentPageId } from '~/stores/page';
+import { useCurrentPagePath, useCurrentPageId, useSWRxCurrentPage } from '~/stores/page';
 
 import { ItemsTree } from '../ItemsTree';
+import { usePagePathRenameHandler } from '../PageEditor/page-path-rename-utils';
 
 import { TreeItemForModal } from './TreeItemForModal';
 
@@ -23,6 +27,8 @@ export const PageSelectModal: FC = () => {
 
   const isOpened = PageSelectModalData?.isOpened ?? false;
 
+  const [clickedParentPagePath, setClickedParentPagePath] = useState('');
+
   const { t } = useTranslation();
 
   const { data: isGuestUser } = useIsGuestUser();
@@ -30,6 +36,9 @@ export const PageSelectModal: FC = () => {
   const { data: currentPath } = useCurrentPagePath();
   const { data: targetId } = useCurrentPageId();
   const { data: targetAndAncestorsData } = useTargetAndAncestors();
+  const { data: currentPage } = useSWRxCurrentPage();
+
+  const pagePathRenameHandler = usePagePathRenameHandler(currentPage);
 
   const targetPathOrId = targetId || currentPath;
 
@@ -38,6 +47,30 @@ export const PageSelectModal: FC = () => {
   }
 
   const path = currentPath || '/';
+
+  const onClickTreeItem = (page: IPageForItem) => {
+    const parentPagePath = page.path;
+
+    if (parentPagePath == null) {
+      return;
+    }
+
+    setClickedParentPagePath(parentPagePath);
+  };
+
+  const onClickCancel = () => {
+    setClickedParentPagePath('');
+    closeModal();
+  };
+
+  const onClickDone = () => {
+    const currentPageTitle = nodePath.basename(currentPage?.path ?? '') || '/';
+    const newPagePath = nodePath.resolve(clickedParentPagePath, currentPageTitle);
+
+    pagePathRenameHandler(newPagePath);
+
+    closeModal();
+  };
 
   return (
     <Modal
@@ -55,11 +88,12 @@ export const PageSelectModal: FC = () => {
           targetPath={path}
           targetPathOrId={targetPathOrId}
           targetAndAncestorsData={targetAndAncestorsData}
+          onClickTreeItem={onClickTreeItem}
         />
       </ModalBody>
       <ModalFooter>
-        <Button color="secondary" onClick={closeModal}>{t('Cancel')}</Button>{' '}
-        <Button color="primary" onClick={closeModal}>{t('Done')}</Button>{' '}
+        <Button color="secondary" onClick={onClickCancel}>{t('Cancel')}</Button>{' '}
+        <Button color="primary" onClick={onClickDone}>{t('Done')}</Button>{' '}
       </ModalFooter>
     </Modal>
   );
