42070f7c55b0d4e5809b064e12a1f3b6a7091011
Shun Miyazawa <m0zurillex@gmail.com>
2024-02-16 14:27:33 +0000

    Create template for CleanupPageCronService

---
+++ apps/app/src/server/service/page/page-cleanup-cron.ts
@@ -0,0 +1,27 @@
+import nodeCron from 'node-cron';
+
+class CleanupPageCronService {
+
+  crowi: any;
+
+  cronJob: any;
+
+  constructor(crowi) {
+    this.crowi = crowi;
+  }
+
+  startCron(): void {
+    //
+  }
+
+  async executeJob(): Promise<void> {
+    //
+  }
+
+  private generateCronJob() {
+    //
+  }
+
+}
+
+export default CleanupPageCronService;
