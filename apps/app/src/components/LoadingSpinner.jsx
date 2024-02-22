4df7fc19b857544d021489e04c415454e79093df
Tatsuya Ise <tatsuya@weseek.co.jp>
2024-02-22 12:35:57 +0000

    change loading icon from material symbols and icons to loading.io icon

--- apps/app/src/components/LoadingSpinner.jsx
+++ apps/app/src/components/LoadingSpinner.jsx
@@ -3,5 +3,7 @@ import React from 'react';
 import styles from './LoadingSpinner.module.scss';
 
 export const LoadingSpinner = () => (
-  <span className={`material-symbols-outlined pb-0 ${styles.spinner}`}>progress_activity</span>
+  // <span className={`material-symbols-outlined pb-0 ${styles.spinner}`}>progress_activity</span>
+  /** refs: https://loading.io/css/ */
+  <div className={`${styles['lds-dual-ring']}`}></div>
 );
