8f462ba197e30cd4b6f6d4482b74628d07d4c89f
Tatsuya Ise <tatsuya@weseek.co.jp>
2024-02-27 02:24:01 +0000

    Changed the implementation method of spinner

--- apps/app/src/components/LoadingSpinner.jsx
+++ apps/app/src/components/LoadingSpinner.jsx
@@ -3,7 +3,5 @@ import React from 'react';
 import styles from './LoadingSpinner.module.scss';
 
 export const LoadingSpinner = () => (
-  // <span className={`material-symbols-outlined pb-0 ${styles.spinner}`}>progress_activity</span>
-  /** refs: https://loading.io/css/ */
-  <div className={`${styles['lds-dual-ring']}`}></div>
+  <span className={`material-symbols-outlined pb-0 ${styles.spinner}`}>progress_activity</span>
 );
