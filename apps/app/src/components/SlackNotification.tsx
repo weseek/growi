a288f34e7edb3626214c28e6530a11f52460aaa5
reiji-h <reiji-h@weseek.co.jp>
2024-03-05 08:09:15 +0000

    clean code

--- apps/app/src/components/SlackNotification.tsx
+++ apps/app/src/components/SlackNotification.tsx
@@ -41,69 +41,6 @@ export const SlackNotification: FC<SlackNotificationProps> = ({
 
 
   return (
-    // <div className={`grw-slack-notification ${styles['grw-slack-notification']} w-100`}>
-    //   <div className="grw-input-group-slack-notification input-group extended-setting">
-    //     <label className="form-label input-group-addon m-0">
-    //       <div className="form-check form-switch form-switch-lg form-switch-slack">
-    //         <input
-    //           type="checkbox"
-    //           className="form-check-input border-0"
-    //           id={id}
-    //           checked={isSlackEnabled}
-    //           onChange={updateCheckboxHandler}
-    //         />
-    //         <label className="form-label form-check-label align-center" htmlFor={id}></label>
-    //       </div>
-    //     </label>
-    //     <Form>
-    //       <FormGroup switch>
-    //         <Input type="switch" role="switch">
-    //           <p>kohsei</p>
-    //         </Input>
-    //       </FormGroup>
-    //     </Form>
-    //     <input
-    //       className="grw-form-control-slack-notification form-control align-top ps-0 h-100"
-    //       id={idForSlackPopover}
-    //       type="text"
-    //       value={slackChannels}
-    //       placeholder="Input channels"
-    //       onChange={updateSlackChannelsHandler}
-    //     />
-    //     <UncontrolledPopover trigger="focus" placement="top" target={idForSlackPopover}>
-    //       <PopoverHeader>{t('slack_notification.popover_title')}</PopoverHeader>
-    //       <PopoverBody>{t('slack_notification.popover_desc')}</PopoverBody>
-    //     </UncontrolledPopover>
-    //   </div>
-    // </div>
-    // <div className={`grw-slack-notification ${styles['grw-slack-notification']} w-100`}>
-    //   <div className="grw-input-group-slack-notification input-group extended-setting">
-    //     <label className="input-group-addon">
-    //       <div className="form-check form-switch form-switch-lg form-switch-slack">
-    //         <input
-    //           type="checkbox"
-    //           className="form-check-input border-0"
-    //           id={id}
-    //           checked={isSlackEnabled}
-    //           onChange={updateCheckboxHandler}
-    //         />
-    //         <label className="form-check-label align-center" htmlFor={id}></label>
-    //       </div>
-    //     </label>
-    //     <input
-    //       className="grw-form-control-slack-notification form-control align-top pl-0"
-    //       id={idForSlackPopover}
-    //       type="text"
-    //       value={slackChannels}
-    //       placeholder="Input channels"
-    //       onChange={updateSlackChannelsHandler}
-    //     />
-    //     <UncontrolledPopover trigger="focus" placement="top" target={idForSlackPopover}>
-    //       <PopoverHeader>{t('slack_notification.popover_title')}</PopoverHeader>
-    //       <PopoverBody>{t('slack_notification.popover_desc')}</PopoverBody>
-    //     </UncontrolledPopover>
-    //   </div>
-    // </div>
     <InputGroup className={`d-flex align-items-center ${styles['grw-slack-switch']}`}>
       <InputGroupText className="rounded-end rounded-pill p-0 pe-1">
         <FormGroup switch className="position-relative pe-4 py-3 m-0 grw-slack-switch">
