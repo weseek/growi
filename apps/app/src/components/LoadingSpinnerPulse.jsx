5bb871bcac024ef1644fc46d47ecbe4b18f5b5a2
Tatsuya Ise <tatsuya@weseek.co.jp>
2024-03-07 08:17:46 +0000

    adjust spinner size to a smaller and set vertical alignment(to bottom).

--- apps/app/src/components/LoadingSpinnerPulse.jsx
+++ apps/app/src/components/LoadingSpinnerPulse.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './LoadingSpinnerPulse.module.scss';
 
 export const LoadingSpinnerPulse = () => (
-  <div className={`${styles['lds-default']}`}>
+  <div className={`align-bottom ms-1 me-1 ${styles['lds-default']}`}>
     <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
   </div>
 );
