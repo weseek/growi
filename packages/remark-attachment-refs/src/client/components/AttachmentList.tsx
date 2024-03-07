695fc665eaa0966f76dc2516957bc71d59ccbd93
Tatsuya Ise <tatsuya@weseek.co.jp>
2024-03-07 06:07:19 +0000

    rollback wrong changes

--- packages/remark-attachment-refs/src/client/components/AttachmentList.tsx
+++ packages/remark-attachment-refs/src/client/components/AttachmentList.tsx
@@ -43,7 +43,7 @@ export const AttachmentList = ({
     if (isLoading) {
       return (
         <div className="text-muted">
-          <span className="me-1"><LoadingSpinnerPulse /></span>
+          <i className="fa fa-spinner fa-pulse me-1"></i>
           <span className="attachment-refs-blink">{refsContext.toString()}</span>
         </div>
       );
