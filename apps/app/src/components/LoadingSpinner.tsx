38f303d96c7026b8ccd4ab99e35c487a0400f6fa
Tatsuya Ise <tatsuya@weseek.co.jp>
2024-03-12 07:45:31 +0000

    add default value

--- apps/app/src/components/LoadingSpinner.tsx
+++ apps/app/src/components/LoadingSpinner.tsx
@@ -2,6 +2,6 @@ import React, { type ComponentPropsWithoutRef } from 'react';
 
 import styles from './LoadingSpinner.module.scss';
 
-export const LoadingSpinner = ({ className }: ComponentPropsWithoutRef<'span'>): JSX.Element => (
+export const LoadingSpinner = ({ className = '' }: ComponentPropsWithoutRef<'span'>): JSX.Element => (
   <span className={`material-symbols-outlined pb-0 ${styles.spinner} ${className}`}>progress_activity</span>
 );
