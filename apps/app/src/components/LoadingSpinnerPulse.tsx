282c853da570e52107f69e1469acba17d477ab2d
Tatsuya Ise <tatsuya@weseek.co.jp>
2024-03-12 07:40:07 +0000

    remove LoadingSpinnerPulse component

--- apps/app/src/components/LoadingSpinnerPulse.tsx
+++
@@ -1,9 +0,0 @@
-import React, { type ComponentPropsWithoutRef } from 'react';
-
-import styles from './LoadingSpinnerPulse.module.scss';
-
-export const LoadingSpinnerPulse = ({ className }: ComponentPropsWithoutRef<'div'>): JSX.Element => (
-  <div className={`align-bottom ms-1 me-1 ${styles['lds-default']} ${className}`}>
-    <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
-  </div>
-);
