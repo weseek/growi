bce91ac6b151790e1d30148b9dbe9dbc58d4b37d
Tatsuya Ise <tatsuya@weseek.co.jp>
2024-03-19 06:50:26 +0000

    change reading styles to moved core pkg spinner

--- apps/app/src/components/LoadingSpinner.tsx
+++ apps/app/src/components/LoadingSpinner.tsx
@@ -1,7 +1,5 @@
 import React, { type ComponentPropsWithoutRef } from 'react';
 
-import styles from './LoadingSpinner.module.scss';
-
 export const LoadingSpinner = ({ className = '' }: ComponentPropsWithoutRef<'span'>): JSX.Element => (
-  <span className={`material-symbols-outlined pb-0 ${styles.spinner} ${className}`}>progress_activity</span>
+  <span className={`material-symbols-outlined pb-0 spinner ${className}`}>progress_activity</span>
 );
