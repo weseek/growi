e50ab4d280b69924694f10d0c9031b2f3f092310
satof3 <satof.design@gmail.com>
2024-03-19 17:45:08 +0900

    Change header layout

--- apps/app/src/components/Layout/NoLoginLayout.tsx
+++ apps/app/src/components/Layout/NoLoginLayout.tsx
@@ -1,4 +1,5 @@
-import React, { ReactNode } from 'react';
+import type { ReactNode } from 'react';
+import React from 'react';
 
 import { useAppTitle } from '~/stores/context';
 
@@ -32,9 +33,12 @@ export const NoLoginLayout = ({
           <div className="row">
 
             <div className="col-md-12 position-relative">
-              <div className="nologin-header mx-auto flex-column">
-                <GrowiLogo />
-                <h1 className="my-3">{ appTitle ?? 'GROWI' }</h1>
+              <div className="nologin-header mx-auto">
+                <div className="d-flex justify-content-center align-items-center">
+                  <GrowiLogo />
+                  <h1 className="growi-logo-type fs-3 my-3 ms-3">GROWI</h1>
+                </div>
+                <h2 className="fs-4 text-center">{ appTitle }</h2>
                 <div className="noLogin-form-errors px-3"></div>
               </div>
               {children}
