4517372488db920159fe92033c4944b8fafa15cd
Yuki Takei <yuki@weseek.co.jp>
2024-03-21 12:09:14 +0000

    make LoadingSpinner styles scoped scss

--- packages/ui/src/components/LoadingSpinner.tsx
+++ packages/ui/src/components/LoadingSpinner.tsx
@@ -1,5 +1,9 @@
-import React, { type ComponentPropsWithoutRef } from 'react';
+import type { ComponentPropsWithoutRef } from 'react';
+
+import styles from './LoadingSpinner.module.scss';
+
+const moduleClass = styles.spinner ?? '';
 
 export const LoadingSpinner = ({ className = '' }: ComponentPropsWithoutRef<'span'>): JSX.Element => (
-  <span className={`material-symbols-outlined pb-0 spinner ${className}`}>progress_activity</span>
+  <span className={`material-symbols-outlined pb-0 ${moduleClass} ${className}`}>progress_activity</span>
 );
