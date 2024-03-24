39ee086f4b7a2f51fc80582808a0f37ae8aa3c9b
ryoji-s <ryoji-s@weseek.co.jp>
2024-03-24 16:58:16 +0000

    apply design fb

--- apps/app/src/components/Sidebar/AppTitle/AppTitle.tsx
+++ apps/app/src/components/Sidebar/AppTitle/AppTitle.tsx
@@ -35,15 +35,19 @@ const AppTitleSubstance = memo((props: Props): JSX.Element => {
           </span>
         </div>
       </div>
-      <UncontrolledTooltip
-        className="d-none d-sm-block confidential-tooltip"
-        data-testid="confidential-tooltip"
-        placement="top"
-        target="grw-site-name"
-        fade={false}
-      >
-        {confidential}
-      </UncontrolledTooltip>
+      {!(confidential == null || confidential === '')
+      && (
+        <UncontrolledTooltip
+          className="d-none d-sm-block confidential-tooltip"
+          innerClassName="text-start"
+          data-testid="confidential-tooltip"
+          placement="top"
+          target="grw-site-name"
+          fade={false}
+        >
+          {confidential}
+        </UncontrolledTooltip>
+      )}
     </div>
   );
 });
