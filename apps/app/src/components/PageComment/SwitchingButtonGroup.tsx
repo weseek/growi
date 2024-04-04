12d684c333dc844a626941e21bb7c7f69a878f08
Yuki Takei <yuki@weseek.co.jp>
2024-04-04 13:26:21 +0000

    swap buttons

--- apps/app/src/components/PageComment/SwitchingButtonGroup.tsx
+++ apps/app/src/components/PageComment/SwitchingButtonGroup.tsx
@@ -52,20 +52,20 @@ export const SwitchingButtonGroup = (props: Props): JSX.Element => {
       role="group"
     >
       <SwitchingButton
-        active={!showPreview}
-        className="px-2"
-        onClick={() => onSelected?.(false)}
-      >
-        <span className="material-symbols-outlined me-1">edit_square</span>
-        <span className="d-none d-sm-inline">{t('Write')}</span>
-      </SwitchingButton>
-      <SwitchingButton
         active={showPreview}
         className="ps-2 pe-3"
         onClick={() => onSelected?.(true)}
       >
         <span className="material-symbols-outlined me-0">play_arrow</span>
         <span className="d-none d-sm-inline">{t('Preview')}</span>
+      </SwitchingButton>
+      <SwitchingButton
+        active={!showPreview}
+        className="px-2"
+        onClick={() => onSelected?.(false)}
+      >
+        <span className="material-symbols-outlined me-1">edit_square</span>
+        <span className="d-none d-sm-inline">{t('Write')}</span>
       </SwitchingButton>
     </div>
   );
