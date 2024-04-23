b3e66b33cc8f6202d235329754455c3b21fd2084
reiji-h <reiji-h@weseek.co.jp>
2024-04-23 06:35:18 +0000

    dont use parseFrontmatter

--- packages/presentation/src/components/Presentation.tsx
+++ packages/presentation/src/components/Presentation.tsx
@@ -3,7 +3,6 @@ import React, { useEffect } from 'react';
 import Reveal from 'reveal.js';
 
 import type { PresentationOptions } from '../consts';
-import { parseSlideFrontmatterInMarkdown } from '../services/parse-slide-frontmatter';
 
 import { Slides } from './Slides';
 
@@ -34,16 +33,13 @@ const removeAllHiddenElements = () => {
 
 export type PresentationProps = {
   options: PresentationOptions,
-  isEnabledMarp: boolean,
+  hasMarpFlag: boolean,
   children?: string,
 }
 
 export const Presentation = (props: PresentationProps): JSX.Element => {
-  const { options, isEnabledMarp, children } = props;
+  const { options, hasMarpFlag, children } = props;
   const { revealOptions } = options;
-
-  const [marp] = parseSlideFrontmatterInMarkdown(children);
-  const hasMarpFlag = isEnabledMarp && marp;
 
   useEffect(() => {
     if (children == null) {
