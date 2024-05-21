b7a5c4b2aa3202ae1e73c615bd333101d6bd5449
reiji-h <reiji-h@weseek.co.jp>
2024-05-21 04:31:53 +0000

    add setcaret after init doc

---
+++ packages/editor/src/services/codemirror-editor/use-codemirror-editor/utils/set-caret-line-init.ts
@@ -0,0 +1,53 @@
+import { useCallback } from 'react';
+
+import { Compartment, StateEffect } from '@codemirror/state';
+import { EditorView, ViewUpdate } from '@codemirror/view';
+
+export type SetCaretLineInit = (lineNumber?: number) => void;
+
+export const useSetCaretLineInit = (view?: EditorView): SetCaretLineInit => {
+
+  return useCallback((lineNumber) => {
+
+    const compartment = new Compartment();
+
+    const initDocListenerExtension = EditorView.updateListener.of((v: ViewUpdate) => {
+      if (v.docChanged && v.changes.desc.length === 0) {
+
+        const doc = v.state.doc;
+
+        try {
+          const posOfLineEnd = doc.line(lineNumber ?? 1).to;
+          view?.dispatch({
+            selection: {
+              anchor: posOfLineEnd,
+              head: posOfLineEnd,
+            },
+            scrollIntoView: true,
+            effects: EditorView.scrollIntoView(posOfLineEnd, {
+              x: 'start',
+              y: 'start',
+            }),
+          });
+        }
+        catch (_: unknown) {
+          // if posOfLineEnd is not found.
+        }
+
+        // focus
+        view?.focus();
+
+        view?.dispatch({
+          effects: compartment.reconfigure([]),
+        });
+      }
+    });
+
+    view?.dispatch({
+      effects: StateEffect.appendConfig.of(
+        compartment.of(initDocListenerExtension),
+      ),
+    });
+
+  }, [view]);
+};
