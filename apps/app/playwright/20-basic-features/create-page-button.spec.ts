5733d60745ee0dc58c8fd460f8fd65d65eb061a5
Yuki Takei <yuki@weseek.co.jp>
2024-06-03 19:35:23 +0000

    fix test

--- apps/app/playwright/20-basic-features/create-page-button.spec.ts
+++ apps/app/playwright/20-basic-features/create-page-button.spec.ts
@@ -12,16 +12,16 @@ test.describe('Create page button', () => {
 });
 
 test.describe('Create page button dropdown menu', () => {
-  test('open and ', async({ page }) => {
+  test('open and create today page', async({ page }) => {
     await page.goto('/');
 
     // open dropdown menu
     await page.getByTestId('grw-page-create-button').hover();
-    await page.getByTestId('grw-page-create-button')
-      .getByLabel('Open create page menu').click({ force: true }); // force click to prevent pointer events loop: refs https://github.com/microsoft/playwright/issues/12298#issuecomment-1170136711
+    await expect(page.getByTestId('grw-page-create-button').getByLabel('Open create page menu')).toBeVisible();
+    await page.getByTestId('grw-page-create-button').getByLabel('Open create page menu').dispatchEvent('click'); // simulate the click
     await page.getByRole('menuitem', { name: 'Create today page' }).click();
 
-    // should not be focused
-    await expect(page.getByPlaceholder('Input page name')).not.toBeFocused();
+    // should not be visible
+    await expect(page.getByPlaceholder('Input page name')).not.toBeVisible();
   });
 });
