3b281677c769fa547b043215b382ecc748ff8791
Yuki Takei <yuki@weseek.co.jp>
2024-06-03 14:46:34 +0000

    WIP: write test

--- apps/app/playwright/20-basic-features/create-page-button.spec.ts
+++ apps/app/playwright/20-basic-features/create-page-button.spec.ts
@@ -4,7 +4,24 @@ test.describe('Create page button', () => {
   test('click and autofocus to title text input', async({ page }) => {
     await page.goto('/');
 
-    await page.getByTestId('grw-page-create-button').getByLabel('Create').click();
+    await page.getByTestId('grw-page-create-button').getByRole('button', { name: 'Create' }).click();
+
+    // should be focused
     await expect(page.getByPlaceholder('Input page name')).toBeFocused();
+  });
+});
+
+test.describe('Create page button dropdown menu', () => {
+  test('open and ', async({ page }) => {
+    await page.goto('/');
+
+    // open dropdown menu
+    await page.getByTestId('grw-page-create-button').hover();
+    await page.getByTestId('grw-page-create-button')
+      .getByLabel('Open create page menu').click({ force: true }); // force click to prevent pointer events loop: refs https://github.com/microsoft/playwright/issues/12298#issuecomment-1170136711
+    await page.getByRole('menuitem', { name: 'Create today page' }).click();
+
+    // should not be focused
+    await expect(page.getByPlaceholder('Input page name')).not.toBeFocused();
   });
 });
