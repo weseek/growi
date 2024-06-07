6dbaa94a08004f693173d81c9d01d41e1dd2f434
Yuki Takei <yuki@weseek.co.jp>
2024-06-07 11:34:30 +0000

    add eslintrc

---
+++ apps/app/src/components-universal/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: '../../.eslintrc.js',
+  rules: {
+    // restrict importing from client/ and components/ directories
+    'no-restricted-imports': ['error', {
+      patterns: [
+        '~/client/',
+        '~/components/',
+        '~/stores/',
+      ],
+    }],
+  },
+};
---
+++ apps/app/src/components/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: '../../.eslintrc.js',
+  rules: {
+    // restrict importing from client/ and components/ directories
+    'no-restricted-imports': ['error', {
+      patterns: [
+        'components-universal/',
+      ],
+    }],
+  },
+};
