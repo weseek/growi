71ffbc560364db6f8f447bd56af8bbb63c2f9fb7
Yuki Takei <yuki@weseek.co.jp>
2024-06-11 10:16:31 +0000

    refactor

--- apps/app/src/services/general-xss-filter/general-xss-filter.ts
+++ apps/app/src/services/general-xss-filter/general-xss-filter.ts
@@ -1,39 +1,18 @@
 import type { IFilterXSSOptions } from 'xss';
 import { FilterXSS } from 'xss';
 
-import { uriAutolinkRegexp, emailAutolinkRegexp } from './commonmark-spec';
-import type XssOption from './xssOption';
-
-
 const REPETITIONS_NUM = 50;
 
-export class Xss {
+const option: IFilterXSSOptions = {
+  stripIgnoreTag: true,
+  stripIgnoreTagBody: false, // see https://github.com/weseek/growi/pull/505
+  css: false,
+  escapeHtml: (html) => { return html }, // resolve https://github.com/weseek/growi/issues/221
+};
 
-  myxss: FilterXSS;
+class GeneralXssFilter extends FilterXSS {
 
-  constructor(xssOption?: XssOption) {
-
-    const option: IFilterXSSOptions = {
-      stripIgnoreTag: true,
-      stripIgnoreTagBody: false, // see https://github.com/weseek/growi/pull/505
-      css: false,
-      whiteList: xssOption != null
-        ? xssOption.attrWhitelist as Record<string, string[] | undefined>
-        : {},
-      escapeHtml: (html) => { return html }, // resolve https://github.com/weseek/growi/issues/221
-      onTag: (tag, html) => {
-        // pass autolink
-        if (tag.match(uriAutolinkRegexp) || tag.match(emailAutolinkRegexp)) {
-          return html;
-        }
-      },
-    };
-
-    // create the XSS Filter instance
-    this.myxss = new FilterXSS(option);
-  }
-
-  process(document: string | undefined): string {
+  override process(document: string | undefined): string {
     let count = 0;
     let currDoc = document;
     let prevDoc = document;
@@ -46,7 +25,7 @@ export class Xss {
       }
 
       prevDoc = currDoc;
-      currDoc = this.myxss.process(currDoc ?? '');
+      currDoc = super.process(currDoc ?? '');
     }
     while (currDoc !== prevDoc);
 
@@ -54,3 +33,5 @@ export class Xss {
   }
 
 }
+
+export const generalXssFilter = new GeneralXssFilter(option);
