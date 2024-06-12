ee3326f92e9f80541039d18e8e53ba47fd97b95e
Shun Miyazawa <m0zurillex@gmail.com>
2024-06-12 02:45:25 +0000

    revert code

--- apps/app/playwright/.eslintrc.mjs
+++ apps/app/playwright/.eslintrc.mjs
@@ -1,14 +1,16 @@
 import playwright from 'eslint-plugin-playwright';
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  root: true,
-  ...playwright.configs['flat/recommended'],
-  files: ['./**'],
-  rules: {
-    // Customize Playwright rules
-    // ...
+export default [
+  {
+    ...playwright.configs['flat/recommended'],
+    files: ['./**'],
   },
-  extends: ['weseek/typescript'],
-  plugins: ['@typescript-eslint'],
-};
+  {
+    files: ['./**'],
+    rules: {
+      // Customize Playwright rules
+      // ...
+    },
+  },
+];
