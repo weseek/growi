ae7672e7fde96dffa528804ff8a9fbd4cbe49170
Shun Miyazawa <m0zurillex@gmail.com>
2024-07-02 10:49:08 +0000

    Revert "Also rewrite shared doc"
    This reverts commit c9954f75d97e5bdde6176603b30ee29614c64a3c.

--- apps/app/src/server/service/yjs-connection-manager.ts
+++ apps/app/src/server/service/yjs-connection-manager.ts
@@ -68,7 +68,7 @@ class YjsConnectionManager {
       currentYdoc.getText('codemirror').insert(0, initialValue);
     }
 
-    this.syncWithPersistedYdoc(pageId, currentYdoc, persistedYdoc);
+    await this.syncWithPersistedYdoc(pageId, currentYdoc, persistedYdoc);
 
     currentYdoc.on('update', async(update) => {
       await this.mdb.storeUpdate(pageId, update);
@@ -103,8 +103,6 @@ class YjsConnectionManager {
     if (diff.reduce((prev, curr) => prev + curr, 0) > 0) {
       await this.mdb.storeUpdate(pageId, diff);
     }
-
-    Y.applyUpdate(currentYdoc, Y.encodeStateAsUpdate(persistedYdoc));
   }
 
   public getCurrentYdoc(pageId: string): Ydoc | undefined {
