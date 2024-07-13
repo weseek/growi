863f6fe903f786b135cc42d625975ab0a9d4b200
Yuki Takei <yuki@weseek.co.jp>
2024-07-13 22:46:41 +0000

    clean code

--- apps/app/src/server/service/import/get-model-from-collection-name.ts
+++ apps/app/src/server/service/import/get-model-from-collection-name.ts
@@ -9,7 +9,7 @@ import mongoose from 'mongoose';
    * @return instance of mongoose model
    */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const getModelFromCollectionName = (collectionName: string): Model<any> => {
+export const getModelFromCollectionName = (collectionName: string): Model<any, unknown, unknown, unknown, any> | undefined => {
   const models = mongoose.modelNames().map(modelName => mongoose.model(modelName));
 
   const Model = Object.values(models).find((m) => {
