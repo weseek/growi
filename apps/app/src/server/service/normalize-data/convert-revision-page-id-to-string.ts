b75853bf1dfcc42d1d64e69a339035f5c7409f39
Shun Miyazawa <m0zurillex@gmail.com>
2024-07-18 14:17:12 +0000

    Added log of query result details

--- apps/app/src/server/service/normalize-data/convert-revision-page-id-to-string.ts
+++ apps/app/src/server/service/normalize-data/convert-revision-page-id-to-string.ts
@@ -4,21 +4,27 @@ import { type IRevisionHasId } from '@growi/core';
 import mongoose from 'mongoose';
 
 import { type IRevisionModel } from '~/server/models/revision';
+import loggerFactory from '~/utils/logger';
+
+const logger = loggerFactory('growi:service:NormalizeData:convert-revision-page-id-to-string');
 
 export const convertRevisionPageIdToString = async(): Promise<void> => {
   const Revision = mongoose.model<IRevisionHasId, IRevisionModel>('Revision');
 
   // Find and update pageId fields that are not of type string
-  await Revision.updateMany(
-    { $expr: { $not: { $eq: [{ $type: '$pageId' }, 'string'] } } },
-    [
-      {
-        $set: {
-          pageId: {
-            $toString: '$pageId',
-          },
+  const filter = { $expr: { $not: { $eq: [{ $type: '$pageId' }, 'string'] } } };
+  const update = [
+    {
+      $set: {
+        pageId: {
+          $toString: '$pageId',
         },
       },
-    ],
-  );
+    },
+  ];
+
+  await Revision.updateMany(filter, update);
+
+  const explain = await Revision.updateMany(filter, update).explain('executionStats');
+  logger.info(explain);
 };
