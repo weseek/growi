f5da1e67aa3e2ab8e75e33847f0aff7c4fa0dc7d
Shun Miyazawa <m0zurillex@gmail.com>
2024-08-02 10:58:06 +0000

    fix lint

--- packages/editor/src/client/components/CodeMirrorEditorMain.tsx
+++ packages/editor/src/client/components/CodeMirrorEditorMain.tsx
@@ -2,11 +2,13 @@ import { useEffect } from 'react';
 
 import { type Extension } from '@codemirror/state';
 import { keymap, scrollPastEnd } from '@codemirror/view';
+import { type IUserHasId } from '@growi/core';
 
 import { GlobalCodeMirrorEditorKey } from '../../consts';
 import { CodeMirrorEditor, type CodeMirrorEditorProps } from '../components-internal/CodeMirrorEditor';
 import { setDataLine } from '../services-internal';
 import { useCodeMirrorEditorIsolated } from '../stores/codemirror-editor';
+import { useCollaborativeEditorMode } from '../stores/use-collaborative-editor-mode';
 
 const additionalExtensions: Extension[] = [
   [
@@ -14,10 +16,21 @@ const additionalExtensions: Extension[] = [
     setDataLine,
   ],
 ];
-export const CodeMirrorEditorMain = (props: CodeMirrorEditorProps): JSX.Element => {
-  const { onSave, ...otherProps } = props;
+
+type Props = CodeMirrorEditorProps & {
+  isYjsEnabled?: boolean
+  user?: IUserHasId
+  pageId?: string
+  onEditorsUpdated?: (userList: IUserHasId[]) => void
+}
+export const CodeMirrorEditorMain = (props: Props): JSX.Element => {
+  const {
+    isYjsEnabled, user, pageId, onEditorsUpdated, onSave, ...otherProps
+  } = props;
 
   const { data: codeMirrorEditor } = useCodeMirrorEditorIsolated(GlobalCodeMirrorEditorKey.MAIN);
+
+  useCollaborativeEditorMode(isYjsEnabled ?? false, user, pageId, onEditorsUpdated, codeMirrorEditor);
 
   // setup additional extensions
   useEffect(() => {
