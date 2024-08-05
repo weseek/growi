a8a276166faff9bda24a86e14ea8e66fcfa96f42
WNomunomu <kohsei.nmr@gmail.com>
2024-08-05 12:11:49 +0000

    add validators

--- apps/app/src/server/routes/apiv3/announcement.ts
+++ apps/app/src/server/routes/apiv3/announcement.ts
@@ -1,10 +1,13 @@
 import type { Router } from 'express';
-
+import {
+  body, param, query, validationResult,
+} from 'express-validator';
 
 import { SupportedTargetModel, SupportedAction } from '~/interfaces/activity';
 import type { ParamsForAnnouncement } from '~/interfaces/announcement';
 import type { CrowiRequest } from '~/interfaces/crowi-request';
 import type Crowi from '~/server/crowi';
+
 
 const express = require('express');
 
@@ -17,10 +20,17 @@ module.exports = (crowi: Crowi): Router => {
   const loginRequiredStrictly = require('~/server/middlewares/login-required')(crowi);
 
   const validators = {
-
+    doAnnouncement: [
+      body('sender').exists({ checkFalsy: true }),
+      body('comment').optional({ nullable: true }).isString(),
+      body('emoji').optional({ nullable: true }).isString(),
+      body('isReadReceiptTrackingEnabled').exists().isBoolean(),
+      body('pageId').exists({ checkFalsy: true }),
+      body('receivers').exists({ checkFalsy: true }).isArray(),
+    ],
   };
 
-  router.post('/doAnnouncement', loginRequiredStrictly, async(req: CrowiRequest) => {
+  router.post('/doAnnouncement', loginRequiredStrictly, validators.doAnnouncement, async(req: CrowiRequest) => {
 
     const params: ParamsForAnnouncement = req.body;
 
