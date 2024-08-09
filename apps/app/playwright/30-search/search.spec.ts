56e3b200aa9ff2d6a73e381bdc7737603dea54f7
Shun Miyazawa <m0zurillex@gmail.com>
2024-08-09 05:35:56 +0000

    test ci

--- apps/app/playwright/30-search/search.spec.ts
+++ apps/app/playwright/30-search/search.spec.ts
@@ -12,7 +12,7 @@ import { test, expect } from '@playwright/test';
 
 test('Search page with "q" param is successfully loaded', async({ page }) => {
   // Navigate to the search page with query parameters
-  await page.goto('/_search?q=あああああああああああああああああああああああ');
+  await page.goto('/_search?q=alerts');
 
   // Confirm search result elements are visible
   await expect(page.getByTestId('search-result-base')).toBeVisible();
