cdcbadb9953fbd1a3397e889f92d288b12ad7d43
Shun Miyazawa <m0zurillex@gmail.com>
2024-08-09 01:16:49 +0000

    ipmrv test

--- apps/app/playwright/30-search/search.spec.ts
+++ apps/app/playwright/30-search/search.spec.ts
@@ -3,6 +3,8 @@ import { test, expect } from '@playwright/test';
 test('Successfully rebuild index', async({ page }) => {
   await page.goto('/admin/search');
 
+  await expect(page.getByTestId('admin-full-text-search')).toBeVisible();
+
   await page.getByTestId('rebuild-index-button').click();
 
   await expect(page.getByTestId('rebuild-status')).toContainText('Completed');
