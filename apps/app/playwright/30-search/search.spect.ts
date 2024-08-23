fd350dc3d8f52a461a390741fe574ae7533e693b
Shun Miyazawa <m0zurillex@gmail.com>
2024-08-23 07:54:41 +0000

    rm unnecessary test

--- apps/app/playwright/30-search/search.spect.ts
+++ apps/app/playwright/30-search/search.spect.ts
@@ -1,13 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-test('Successfully rebuild index', async({ page }) => {
-  await page.goto('/admin/search');
-
-  await expect(page.getByTestId('admin-full-text-search')).toBeVisible();
-
-  await expect(page.getByTestId('connection-status-label')).toHaveText('CONNECTED');
-});
-
 test('Search page with "q" param is successfully loaded', async({ page }) => {
   // Navigate to the search page with query parameters
   await page.goto('/_search?q=alerts');
