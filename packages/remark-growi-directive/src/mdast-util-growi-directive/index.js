15941d6d690e71ee7aeea07c40daea63499b6430
reiji-h <reiji-h@weseek.co.jp>
2024-08-23 09:59:37 +0000

    add export

--- packages/remark-growi-directive/src/mdast-util-growi-directive/index.js
+++ packages/remark-growi-directive/src/mdast-util-growi-directive/index.js
@@ -1,2 +1,8 @@
 export { directiveFromMarkdown, directiveToMarkdown } from './lib/index.js';
 export { DirectiveType as DirectiveTypeObject } from './lib/index.js';
+export {
+  LeafGrowiPluginDirective,
+  TextGrowiPluginDirective,
+  LeafGrowiPluginDirectiveData,
+  TextGrowiPluginDirectiveData,
+} from './index';
