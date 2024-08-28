e7b0313b064e1ec3c5a40d12c0ef37ca5d4def8b
Yuki Takei <yuki@weseek.co.jp>
2024-08-28 17:05:23 +0000

    BugFix

--- apps/app/src/server/service/openai/embeddings.ts
+++ apps/app/src/server/service/openai/embeddings.ts
@@ -5,12 +5,11 @@ import type { OpenAI } from 'openai';
 import { openaiClient } from './client';
 
 
-const hasher = crypto.createHash('sha256');
-
 export const embed = async(input: string, username?: string): Promise<OpenAI.Embedding[]> => {
   let user;
 
   if (username != null) {
+    const hasher = crypto.createHash('sha256');
     hasher.update(username);
     user = hasher.digest('hex');
   }
