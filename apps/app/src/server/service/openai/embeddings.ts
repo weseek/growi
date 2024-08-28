3743e04321f5e874479e3f490104ce14b686d4ec
Yuki Takei <yuki@weseek.co.jp>
2024-08-28 16:35:10 +0000

    hasing username

--- apps/app/src/server/service/openai/embeddings.ts
+++ apps/app/src/server/service/openai/embeddings.ts
@@ -1,13 +1,25 @@
+import crypto from 'crypto';
+
 import type { OpenAI } from 'openai';
 
 import { openaiClient } from './client';
 
+
+const hasher = crypto.createHash('sha256');
+
 export const embed = async(input: string, username?: string): Promise<OpenAI.Embedding[]> => {
+  let user;
+
+  if (username != null) {
+    hasher.update(username);
+    user = hasher.digest('hex');
+  }
+
   const result = await openaiClient.embeddings.create({
     input,
     model: 'text-embedding-3-large',
     dimensions: Number(process.env.OPENAI_DIMENSIONS),
-    user: username,
+    user,
   });
 
   return result.data;
