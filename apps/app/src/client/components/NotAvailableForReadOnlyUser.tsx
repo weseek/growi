6ed132ca4297dd1a16c911cba729cda42b8cc209
WNomunomu <kohsei.nmr@gmail.com>
2024-09-12 03:48:05 +0000

    add NotAvailableWhenReadOnlyUserNotAllowedToComment component

--- apps/app/src/client/components/NotAvailableForReadOnlyUser.tsx
+++ apps/app/src/client/components/NotAvailableForReadOnlyUser.tsx
@@ -26,3 +26,24 @@ export const NotAvailableForReadOnlyUser: React.FC<{
   );
 });
 NotAvailableForReadOnlyUser.displayName = 'NotAvailableForReadOnlyUser';
+
+
+export const NotAvailableWhenReadOnlyUserNotAllowedToComment: React.FC<{
+  children: JSX.Element
+}> = React.memo(({ children }) => {
+  const { t } = useTranslation();
+  const { data: isReadOnlyUser } = useIsReadOnlyUser();
+
+  const isDisabled = !!isReadOnlyUser;
+  const title = t('Not available for read only user');
+
+  return (
+    <NotAvailable
+      isDisabled={isDisabled}
+      title={title}
+      classNamePrefix="grw-not-available-for-read-only-user"
+    >
+      {children}
+    </NotAvailable>
+  );
+});
