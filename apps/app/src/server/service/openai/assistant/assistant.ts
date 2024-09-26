a01b1195bf50dfb83da1b14c66a0f14d6d3e47ea
abichan99911111 <shinei-a@weseek.co.jp>
2024-09-26 08:27:56 +0000

    implement default instruction

--- apps/app/src/server/service/openai/assistant/assistant.ts
+++ apps/app/src/server/service/openai/assistant/assistant.ts
@@ -1,3 +1,4 @@
+import ejs from 'ejs';
 import type OpenAI from 'openai';
 
 import { configManager } from '../../config-manager';
@@ -72,9 +73,20 @@ export const getOrCreateChatAssistant = async(): Promise<OpenAI.Beta.Assistant> 
     return chatAssistant;
   }
 
+  let instructions;
+  if (configManager.getConfig('crowi', 'app:openaiChatAssistantCustomFullInstructions')) {
+    instructions = configManager.getConfig('crowi', 'app:openaiChatAssistantCustomFullInstructions');
+  }
+  else {
+    instructions = configManager.getConfig('crowi', 'app:openaiChatAssistantInstructions').join('');
+    instructions = ejs.render(instructions, {
+      customInstruction: configManager.getConfig('crowi', 'app:openaiChatAssistantCustomInstructions'),
+    });
+  }
+
   chatAssistant = await getOrCreateAssistant(AssistantType.CHAT);
   openaiClient.beta.assistants.update(chatAssistant.id, {
-    instructions: configManager.getConfig('crowi', 'app:openaiChatAssistantInstructions'),
+    instructions,
     tools: [{ type: 'file_search' }],
   });
 
