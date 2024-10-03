a9d92ff3e7c1074c9db8aaccb62c13a667f7e5fc
Shun Miyazawa <m0zurillex@gmail.com>
2024-10-03 09:28:15 +0000

    use NotAvailableForGuest

--- apps/app/src/client/components/PageControls/RagSearchButton.tsx
+++ apps/app/src/client/components/PageControls/RagSearchButton.tsx
@@ -1,12 +1,12 @@
 import React, { useCallback } from 'react';
 
-import { useIsAiEnabled, useIsGuestUser } from '~/stores-universal/context';
+import { NotAvailableForGuest } from '~/client/components/NotAvailableForGuest';
+import { useIsAiEnabled } from '~/stores-universal/context';
 import { useRagSearchModal } from '~/stores/rag-search';
 
 import styles from './RagSearchButton.module.scss';
 
 const RagSearchButton = (): JSX.Element => {
-  const { data: isGuestUser } = useIsGuestUser();
   const { data: isAiEnabled } = useIsAiEnabled();
   const { open: openRagSearchModal } = useRagSearchModal();
 
@@ -14,19 +14,21 @@ const RagSearchButton = (): JSX.Element => {
     openRagSearchModal();
   }, [openRagSearchModal]);
 
-  if (!isAiEnabled || isGuestUser) {
+  if (!isAiEnabled) {
     return <></>;
   }
 
   return (
-    <button
-      type="button"
-      className={`btn btn-search ${styles['btn-rag-search']}`}
-      onClick={ragSearchButtonClickHandler}
-      data-testid="open-search-modal-button"
-    >
-      <span className="material-symbols-outlined">chat</span>
-    </button>
+    <NotAvailableForGuest>
+      <button
+        type="button"
+        className={`btn btn-search ${styles['btn-rag-search']}`}
+        onClick={ragSearchButtonClickHandler}
+        data-testid="open-search-modal-button"
+      >
+        <span className="material-symbols-outlined">chat</span>
+      </button>
+    </NotAvailableForGuest>
   );
 };
 
