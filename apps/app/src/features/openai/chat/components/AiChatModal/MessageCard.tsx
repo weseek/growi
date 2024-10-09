7690d46b02c668c78ec9137141b74aff778b5385
Yuki Takei <yuki@weseek.co.jp>
2024-10-09 14:00:56 +0000

    tidy up spacing

--- apps/app/src/features/openai/chat/components/AiChatModal/MessageCard.tsx
+++ apps/app/src/features/openai/chat/components/AiChatModal/MessageCard.tsx
@@ -26,22 +26,20 @@ const AssistantMessageCard = ({ children }: { children: string }): JSX.Element =
   return (
     <div className={`card border-0 ${moduleClass} ${assistantMessageCardModuleClass}`}>
       <div className="card-body d-flex">
-        <div className="me-2 me-lg-3 pt-1">
+        <div className="me-2 me-lg-3">
           <span className="growi-custom-icons grw-ai-icon rounded-pill">growi_ai</span>
         </div>
 
-        <div className="mt-1">
-          { children.length > 0
-            ? (
-              <ReactMarkdown>{children}</ReactMarkdown>
-            )
-            : (
-              <span className="text-thinking">
-                {t('modal_aichat.progress_label')} <span className="material-symbols-outlined">more_horiz</span>
-              </span>
-            )
-          }
-        </div>
+        { children.length > 0
+          ? (
+            <ReactMarkdown>{children}</ReactMarkdown>
+          )
+          : (
+            <span className="text-thinking">
+              {t('modal_aichat.progress_label')} <span className="material-symbols-outlined">more_horiz</span>
+            </span>
+          )
+        }
       </div>
     </div>
   );
