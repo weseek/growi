1b3e709d88158c7d6040079fe6d78afe9e88d730
Futa Arai <arafubeatbox@gmail.com>
2024-10-18 10:56:09 +0900

    remove dist

--- apps/pdf-converter/dist/utils/logger.d.ts
+++
@@ -1,3 +0,0 @@
-import type Logger from 'bunyan';
-declare const loggerFactory: (name: string) => Logger;
-export default loggerFactory;
--- apps/pdf-converter/dist/utils/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const universal_bunyan_1 = require("universal-bunyan");
-const loggerFactory = function (name) {
-    return (0, universal_bunyan_1.createLogger)({ name });
-};
-exports.default = loggerFactory;
