f9befdf363bc5b349a0886b257be7c77f29ea470
Futa Arai <arafubeatbox@gmail.com>
2024-11-04 20:19:31 +0900

    fix pdf-converter devcontainer

--- .devcontainer/pdf-converter/postCreateCommand.sh
+++ .devcontainer/pdf-converter/postCreateCommand.sh
@@ -3,11 +3,8 @@ sudo chown -R vscode:vscode /workspace;
 # Instal additional packages
 sudo apt update
 sudo apt-get install -y --no-install-recommends \
-  chromium locales fonts-ipafont fonts-ipaexfont fonts-ipafont-gothic fonts-ipafont-mincho
+  chromium-browser locales fonts-ipafont fonts-ipaexfont fonts-ipafont-gothic fonts-ipafont-mincho
 sudo apt-get clean -y
-
-echo "ja_JP UTF-8" > /etc/locale.gen && locale-gen
-export LANG=ja_JP.UTF-8
 
 # Setup pnpm
 SHELL=bash pnpm setup
