4ab4edf86e54d4da2f641cc37db29ca8655cb924
Futa Arai <arafubeatbox@gmail.com>
2024-11-09 21:24:55 +0900

    use image with node for base image of pdf-converter and remove features

--- .devcontainer/pdf-converter/postCreateCommand.sh
+++ .devcontainer/pdf-converter/postCreateCommand.sh
@@ -1,14 +1,13 @@
-sudo chown -R vscode:vscode /workspace;
-
 # Instal additional packages
 sudo apt update
 sudo apt-get install -y --no-install-recommends \
-  chromium-browser locales fonts-ipafont fonts-ipaexfont fonts-ipafont-gothic fonts-ipafont-mincho
+  chromium locales fonts-ipafont fonts-ipaexfont fonts-ipafont-gothic fonts-ipafont-mincho
 sudo apt-get clean -y
 
 # Setup pnpm
 SHELL=bash pnpm setup
-eval "$(cat /home/vscode/.bashrc)"
+eval "$(cat /home/node/.bashrc)"
+pnpm add -g pnpm
 
 # Install turbo
 pnpm install turbo --global
