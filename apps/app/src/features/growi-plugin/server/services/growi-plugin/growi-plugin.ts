fcdfeecca15f17e5864f06d703808166f8723ec9
reiji-h <reiji-h@weseek.co.jp>
2024-11-18 07:05:21 +0000

    dont use stream-to-promise

--- apps/app/src/features/growi-plugin/server/services/growi-plugin/growi-plugin.ts
+++ apps/app/src/features/growi-plugin/server/services/growi-plugin/growi-plugin.ts
@@ -1,6 +1,6 @@
 import fs, { readFileSync } from 'fs';
 import path from 'path';
-import { pipeline } from 'stream';
+import { pipeline } from 'stream/promises';
 
 import { GrowiPluginType } from '@growi/core';
 import type { GrowiThemeMetadata, ViteManifest } from '@growi/core';
@@ -9,7 +9,6 @@ import { importPackageJson, validateGrowiDirective } from '@growi/pluginkit/dist
 // eslint-disable-next-line no-restricted-imports
 import axios from 'axios';
 import type mongoose from 'mongoose';
-import streamToPromise from 'stream-to-promise';
 import unzipStream from 'unzip-stream';
 
 import loggerFactory from '~/utils/logger';
@@ -211,9 +210,7 @@ export class GrowiPluginService implements IGrowiPluginService {
   private async unzip(zipFilePath: fs.PathLike, destPath: fs.PathLike): Promise<void> {
     try {
       const readZipStream = fs.createReadStream(zipFilePath);
-      const unzipFileStream = pipeline(readZipStream, unzipStream.Extract({ path: destPath.toString() }));
-
-      await streamToPromise(unzipFileStream);
+      await pipeline(readZipStream, unzipStream.Extract({ path: destPath.toString() }));
     }
     catch (err) {
       logger.error(err);
