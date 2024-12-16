16f5b5ec7693f2d5b4630295ef891aa7c286dbac
Shun Miyazawa <m0zurillex@gmail.com>
2024-12-16 03:49:07 +0000

    impl KnowledgeAssistantContent

--- apps/app/src/features/openai/client/components/Sidebar/KnowledgeAssistant.tsx
+++ apps/app/src/features/openai/client/components/Sidebar/KnowledgeAssistant.tsx
@@ -1,9 +1,11 @@
-import React, { Suspense, useState } from 'react';
+import React, { Suspense } from 'react';
 
 import dynamic from 'next/dynamic';
 import { useTranslation } from 'react-i18next';
 
 import ItemsTreeContentSkeleton from '~/client/components/ItemsTree/ItemsTreeContentSkeleton';
+
+const KnowledgeAssistantContent = dynamic(() => import('./KnowledgeAssistantSubstance').then(mod => mod.KnowledgeAssistantContent), { ssr: false });
 
 export const KnowledgeAssistant = (): JSX.Element => {
   const { t } = useTranslation();
@@ -15,6 +17,9 @@ export const KnowledgeAssistant = (): JSX.Element => {
           {t('Knowledge Assistant')}
         </h3>
       </div>
+      <Suspense fallback={<ItemsTreeContentSkeleton />}>
+        <KnowledgeAssistantContent />
+      </Suspense>
     </div>
   );
 };
---
+++ apps/app/src/features/openai/client/components/Sidebar/KnowledgeAssistantSubstance.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const KnowledgeAssistantContent = (): JSX.Element => {
+  return (
+    <div>
+      <button type="button" className="btn btn-primary">
+        アシスタントを追加する
+      </button>
+    </div>
+  );
+};
