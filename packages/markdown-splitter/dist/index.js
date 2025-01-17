03eb51443892f912cc13a35f3f1dcf66e037249b
Futa Arai <arafubeatbox@gmail.com>
2025-01-17 15:07:54 +0900

    remove unnecessary dirs

--- packages/markdown-splitter/dist/index.js
+++
@@ -1,7 +0,0 @@
-import { splitMarkdownIntoFragments as t } from "./services/markdown-splitter.js";
-import { splitMarkdownIntoChunks as p } from "./services/markdown-token-splitter.js";
-export {
-  p as splitMarkdownIntoChunks,
-  t as splitMarkdownIntoFragments
-};
-//# sourceMappingURL=index.js.map
