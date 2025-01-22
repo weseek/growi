e826c415baa016324da8aa4d7d69f610f7286654
Yuki Takei <yuki@weseek.co.jp>
2025-01-22 08:36:52 +0000

    omit sidebar

--- apps/app/src/components/Layout/BasicLayout.tsx
+++ apps/app/src/components/Layout/BasicLayout.tsx
@@ -46,7 +46,7 @@ export const BasicLayout = ({ children, className }: Props): React.ReactElement 
   return (
     <RawLayout className={`${moduleClass} ${className ?? ''}`}>
       <div className="page-wrapper flex-row">
-        <div className="z-2">
+        <div className="z-2 d-print-none">
           <Sidebar />
         </div>
 
