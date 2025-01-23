33811dbd9d5e9337ea49e5b8a2584b1ae8cf5693
Shun Miyazawa <m0zurillex@gmail.com>
2025-01-23 06:58:50 +0000

    add styles

--- apps/app/src/features/openai/client/components/AiAssistant/Sidebar/AiAssistantSubstance.tsx
+++ apps/app/src/features/openai/client/components/AiAssistant/Sidebar/AiAssistantSubstance.tsx
@@ -2,15 +2,35 @@ import React from 'react';
 
 import { useAiAssistantManegementModal } from '../../../stores/ai-assistant';
 
+import styles from './AiAssistantSubstance.module.scss';
+
+const moduleClass = styles['grw-ai-assistant-substance'] ?? '';
+
 export const AiAssistantContent = (): JSX.Element => {
   const { open } = useAiAssistantManegementModal();
 
   return (
-    <div>
-      <button type="button" className="btn btn-primary" onClick={open}>
-        アシスタントを追加する
-        {/* TODO i18n */}
+    <div className={moduleClass}>
+      <button
+        type="button"
+        className="btn btn-outline-secondary px-3 d-flex align-items-center mb-3"
+        onClick={open}
+      >
+        <span className="material-symbols-outlined fs-5 me-2">add</span>
+        <span className="fw-normal">アシスタントを追加する</span>
       </button>
+
+      <div className="py-4 d-flex">
+        <h3 className="fw-bold mb-0 grw-ai-assistant-substance-header">
+          マイアシスタント
+        </h3>
+      </div>
+
+      <div className="py-4 d-flex">
+        <h3 className="fw-bold mb-0 grw-ai-assistant-substance-header">
+          チームアシスタント
+        </h3>
+      </div>
     </div>
   );
 };
