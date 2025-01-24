743aa0703a69e6fb656f797a43aa7c9245eaa9ec
satof3 <satof.design@gmail.com>
2025-01-24 10:30:05 +0000

    Add Brandlogo in editor display

--- apps/app/src/client/components/Sidebar/SidebarHead/SidebarHead.tsx
+++ apps/app/src/client/components/Sidebar/SidebarHead/SidebarHead.tsx
@@ -2,15 +2,35 @@ import React, {
   type FC, memo,
 } from 'react';
 
+import Link from 'next/link';
+
+import { useIsDefaultLogo } from '~/stores-universal/context';
+import { EditorMode, useEditorMode } from '~/stores-universal/ui';
+
+
+import { SidebarBrandLogo } from '../SidebarBrandLogo';
+
 import { ToggleCollapseButton } from './ToggleCollapseButton';
 
 import styles from './SidebarHead.module.scss';
 
 
 export const SidebarHead: FC = memo(() => {
+  const { data: editorMode } = useEditorMode();
+  const { data: isDefaultLogo } = useIsDefaultLogo();
   return (
     <div className={`${styles['grw-sidebar-head']} d-flex justify-content-end w-100`}>
-      <ToggleCollapseButton />
+      {editorMode === EditorMode.Editor ? (
+        <Link
+          href="/"
+          className="p-2"
+          data-testid="btn-brand-logo"
+        >
+          <SidebarBrandLogo isDefaultLogo={isDefaultLogo} />
+        </Link>
+      ) : (
+        <ToggleCollapseButton />
+      )}
     </div>
   );
 
