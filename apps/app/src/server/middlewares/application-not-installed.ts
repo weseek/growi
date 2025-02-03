2c9ad086751479dd1fd5a3f37f76eb8c8624eea6
Yuki Takei <yuki@weseek.co.jp>
2025-02-03 13:07:05 +0000

    refactor application-not-installed.ts

--- apps/app/src/server/middlewares/application-not-installed.ts
+++ apps/app/src/server/middlewares/application-not-installed.ts
@@ -1,14 +1,41 @@
-/** @param {import('~/server/crowi').default} crowi Crowi instance */
-module.exports = (crowi) => {
+import type { NextFunction, Request, Response } from 'express';
+import createError, { isHttpError } from 'http-errors';
+
+import type Crowi from '../crowi';
+
+export const generateCheckerMiddleware = (crowi: Crowi) => async(req: Request, res: Response, next: NextFunction): Promise<void> => {
   const { appService } = crowi;
 
-  return async(req, res, next) => {
-    const isDBInitialized = await appService.isDBInitialized(true);
+  const isDBInitialized = await appService.isDBInitialized(true);
 
-    if (isDBInitialized) {
-      return res.redirect('/');
-    }
+  if (isDBInitialized) {
+    return next(createError(409, 'Application is already installed'));
+  }
 
+  return next();
+};
+
+export const allreadyInstalledMiddleware = async(req: Request, res: Response, next: NextFunction): Promise<void> => {
+  return next(createError(409, 'Application is already installed'));
+};
+
+export const handleAsApiError = (error: Error, req: Request, res: Response, next: NextFunction): void => {
+  if (error == null) {
     return next();
-  };
+  }
+
+  if (isHttpError(error)) {
+    const httpError = error as createError.HttpError;
+    res.status(httpError.status).json({ message: httpError.message });
+    return;
+  }
+
+  next();
+};
+
+export const redirectToTopOnError = (error: Error, req: Request, res: Response, next: NextFunction): void => {
+  if (error != null) {
+    return res.redirect('/');
+  }
+  return next();
 };
