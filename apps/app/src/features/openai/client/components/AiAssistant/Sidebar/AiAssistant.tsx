fc98415ea036a06819ac0986cda2a6f2a59b735c
Shun Miyazawa <m0zurillex@gmail.com>
2025-02-06 08:47:01 +0000

    Add guest user check to AiAssistant component

--- apps/app/src/features/openai/client/components/AiAssistant/Sidebar/AiAssistant.tsx
+++ apps/app/src/features/openai/client/components/AiAssistant/Sidebar/AiAssistant.tsx
@@ -4,11 +4,13 @@ import dynamic from 'next/dynamic';
 import { useTranslation } from 'react-i18next';
 
 import ItemsTreeContentSkeleton from '~/client/components/ItemsTree/ItemsTreeContentSkeleton';
+import { useIsGuestUser } from '~/stores-universal/context';
 
 const AiAssistantContent = dynamic(() => import('./AiAssistantSubstance').then(mod => mod.AiAssistantContent), { ssr: false });
 
 export const AiAssistant = (): JSX.Element => {
   const { t } = useTranslation();
+  const { data: isGuestUser } = useIsGuestUser();
 
   return (
     <div className="px-3">
@@ -17,9 +19,19 @@ export const AiAssistant = (): JSX.Element => {
           {t('Knowledge Assistant')}
         </h3>
       </div>
-      <Suspense fallback={<ItemsTreeContentSkeleton />}>
-        <AiAssistantContent />
-      </Suspense>
+
+      { isGuestUser
+        ? (
+          <h4 className="fs-6">
+            { t('Not available for guest') }
+          </h4>
+        )
+        : (
+          <Suspense fallback={<ItemsTreeContentSkeleton />}>
+            <AiAssistantContent />
+          </Suspense>
+        )
+      }
     </div>
   );
 };
