47e74ce3a570a281fb6fc7222774ff6254c86867
Shun Miyazawa <m0zurillex@gmail.com>
2025-02-17 06:25:45 +0000

    impl updateAiAssistant()

--- apps/app/src/features/openai/client/services/ai-assistant.ts
+++ apps/app/src/features/openai/client/services/ai-assistant.ts
@@ -1,9 +1,13 @@
-import { apiv3Post, apiv3Delete } from '~/client/util/apiv3-client';
+import { apiv3Post, apiv3Put, apiv3Delete } from '~/client/util/apiv3-client';
 
 import type { UpsertAiAssistantData } from '../../interfaces/ai-assistant';
 
 export const createAiAssistant = async(body: UpsertAiAssistantData): Promise<void> => {
   await apiv3Post('/openai/ai-assistant', body);
+};
+
+export const updateAiAssistant = async(id: string, body: UpsertAiAssistantData): Promise<void> => {
+  await apiv3Put(`/openai/ai-assistant/${id}`, body);
 };
 
 export const deleteAiAssistant = async(id: string): Promise<void> => {
