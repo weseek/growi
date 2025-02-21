672ac2113333d2c87b4ffa7e1ddf8e87cae119a0
Shun Miyazawa <m0zurillex@gmail.com>
2025-02-21 11:52:22 +0000

    Refactor hooks

--- apps/app/src/features/openai/client/stores/message.tsx
+++ apps/app/src/features/openai/client/stores/message.tsx
@@ -4,10 +4,10 @@ import useSWRImmutable from 'swr/immutable';
 
 import { apiv3Get } from '~/client/util/apiv3-client';
 
-export const useSWRxMessages = (aiAssistantId: string, threadId: string): SWRResponse<OpenAI.Beta.Threads.Messages.MessagesPage, Error> => {
-  const key = [`/openai/messages/${aiAssistantId}/${threadId}`];
+export const useSWRxMessages = (aiAssistantId?: string, threadId?: string): SWRResponse<OpenAI.Beta.Threads.Messages.MessagesPage, Error> => {
+  const key = aiAssistantId != null && threadId != null ? [`/openai/messages/${aiAssistantId}/${threadId}`] : null;
   return useSWRImmutable<OpenAI.Beta.Threads.Messages.MessagesPage>(
     key,
-    ([endpoint]) => apiv3Get(endpoint).then(response => response.data.accessibleAiAssistants),
+    ([endpoint]) => apiv3Get(endpoint).then(response => response.data.messages),
   );
 };
