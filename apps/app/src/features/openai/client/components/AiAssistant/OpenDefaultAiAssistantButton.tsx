f0af0ff5f27a39a2025e5986506801e6d59c8d51
Shun Miyazawa <m0zurillex@gmail.com>
2025-03-02 07:12:28 +0000

    Enable opening DefaultAiAssistant

--- apps/app/src/features/openai/client/components/AiAssistant/OpenDefaultAiAssistantButton.tsx
+++ apps/app/src/features/openai/client/components/AiAssistant/OpenDefaultAiAssistantButton.tsx
@@ -1,15 +1,33 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 
+import { NotAvailable } from '~/client/components/NotAvailable';
 import { NotAvailableForGuest } from '~/client/components/NotAvailableForGuest';
 import { useIsAiEnabled } from '~/stores-universal/context';
+
+import { useAiAssistantChatSidebar, useSWRxAiAssistants } from '../../stores/ai-assistant';
 
 import styles from './OpenDefaultAiAssistantButton.module.scss';
 
 const OpenDefaultAiAssistantButton = (): JSX.Element => {
   const { data: isAiEnabled } = useIsAiEnabled();
+  const { data: aiAssistantData } = useSWRxAiAssistants();
+  const { open: openAiAssistantChatSidebar } = useAiAssistantChatSidebar();
+
+  const defaultAiAssistant = useMemo(() => {
+    if (aiAssistantData == null) {
+      return null;
+    }
+
+    return [...aiAssistantData.myAiAssistants, ...aiAssistantData.teamAiAssistants].find(aiAssistant => aiAssistant.isDefault);
+  }, [aiAssistantData]);
 
   const openDefaultAiAssistantButtonClickHandler = useCallback(() => {
-  }, []);
+    if (defaultAiAssistant == null) {
+      return;
+    }
+
+    openAiAssistantChatSidebar(defaultAiAssistant);
+  }, [defaultAiAssistant, openAiAssistantChatSidebar]);
 
   if (!isAiEnabled) {
     return <></>;
@@ -17,14 +35,18 @@ const OpenDefaultAiAssistantButton = (): JSX.Element => {
 
   return (
     <NotAvailableForGuest>
-      <button
-        type="button"
-        className={`btn btn-search ${styles['btn-open-default-ai-assistant']}`}
-        onClick={openDefaultAiAssistantButtonClickHandler}
-        data-testid="open-search-modal-button"
-      >
-        <span className="growi-custom-icons fs-4 align-middle lh-1">ai_assistant</span>
-      </button>
+      <NotAvailable isDisabled={defaultAiAssistant == null} title="デフォルトのAIアシスタントが設定されていません">
+        <button
+          type="button"
+          className={`btn btn-search ${styles['btn-open-default-ai-assistant']}`}
+          onClick={openDefaultAiAssistantButtonClickHandler}
+          data-testid="open-default-ai-assistant-button"
+          id="open-default-ai-assistant-button"
+          disabled={defaultAiAssistant == null}
+        >
+          <span className="growi-custom-icons fs-4 align-middle lh-1">ai_assistant</span>
+        </button>
+      </NotAvailable>
     </NotAvailableForGuest>
   );
 };
