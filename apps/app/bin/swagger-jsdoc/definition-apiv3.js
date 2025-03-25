8c73aab911287d5eed14d06baba7d11e7d914a47
Atsushi Nakatsugawa <atsushi@moongift.co.jp>
2025-03-26 08:26:55 +0900

    support(jsdoc): Add swagger document to staffs.js

--- apps/app/bin/swagger-jsdoc/definition-apiv3.js
+++ apps/app/bin/swagger-jsdoc/definition-apiv3.js
@@ -79,6 +79,7 @@ module.exports = {
         'SlackIntegrationSettings (without proxy)',
         'SlackIntegrationLegacySetting',
         'ShareLink Management',
+        'Staff',
         'UserGroupRelations',
         'UserGroups',
         'Users Management',
