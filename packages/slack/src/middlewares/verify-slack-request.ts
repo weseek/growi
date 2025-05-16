76ab30f0f50b0aef26b9b4608a39908c6eaa60af
Futa Arai <arafubeatbox@gmail.com>
2025-05-16 22:14:29 +0900

    biome ignore files

--- packages/slack/src/middlewares/verify-slack-request.ts
+++ packages/slack/src/middlewares/verify-slack-request.ts
@@ -1,4 +1,4 @@
-import { createHmac, timingSafeEqual } from 'crypto';
+import { createHmac, timingSafeEqual } from 'node:crypto';
 
 import type { NextFunction, Response } from 'express';
 import createError from 'http-errors';
