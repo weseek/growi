d1b2b01b7d868634b7c84e7015c91c71aa83ddec
Futa Arai <arafubeatbox@gmail.com>
2025-05-18 16:38:42 +0900

    undo removal of permission configuration for bulk export tmp dir

--- apps/app/docker/docker-entrypoint.sh
+++ apps/app/docker/docker-entrypoint.sh
@@ -10,4 +10,9 @@ fi
 chown -R node:node /data/uploads
 chown -h node:node ./public/uploads
 
+# Set permissions for shared directory for bulk export
+mkdir -p /tmp/page-bulk-export
+chown -R node:node /tmp/page-bulk-export
+chmod 700 /tmp/page-bulk-export
+
 exec gosu node /bin/bash -c "$@"
