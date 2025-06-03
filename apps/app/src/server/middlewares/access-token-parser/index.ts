fd1f2984732390d7c246f75bcb334311706c9a7e
Shun Miyazawa <m0zurillex@gmail.com>
2025-06-03 10:37:43 +0000

    Modify new accessTokenParser to optionally accept legacy API tokens

--- apps/app/src/server/middlewares/access-token-parser/index.ts
+++ apps/app/src/server/middlewares/access-token-parser/index.ts
@@ -6,14 +6,18 @@ import { parserForAccessToken } from './access-token';
 import { parserForApiToken } from './api-token';
 import type { AccessTokenParserReq } from './interfaces';
 
-export const accessTokenParser = (scopes?: Scope[]) => {
+export const accessTokenParser = (scopes?: Scope[], isLegacyAccessTokenEnabled = false) => {
   return async(req: AccessTokenParserReq, res: Response, next: NextFunction): Promise<void> => {
     // TODO: comply HTTP header of RFC6750 / Authorization: Bearer
-
-    if (scopes != null) {
-      await parserForAccessToken(scopes)(req, res, next);
+    if (scopes == null || scopes.length === 0) {
+      return next();
     }
-    await parserForApiToken(req, res, next);
+
+    await parserForAccessToken(scopes)(req, res, next);
+
+    if (isLegacyAccessTokenEnabled) {
+      await parserForApiToken(req, res, next);
+    }
 
     return next();
   };
