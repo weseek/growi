25392ff81074080e60832432bd60edadbaeebb14
Shun Miyazawa <m0zurillex@gmail.com>
2025-06-13 03:59:44 +0000

    rm todo

--- apps/app/src/server/routes/apiv3/personal-setting/generate-access-token.ts
+++ apps/app/src/server/routes/apiv3/personal-setting/generate-access-token.ts
@@ -79,7 +79,6 @@ export const generateAccessTokenHandlerFactory: GenerateAccessTokenHandlerFactor
   const activityEvent = crowi.event('activity');
   const addActivity = generateAddActivityMiddleware();
 
-  // TODO: 167279
   return [
     loginRequiredStrictly,
     excludeReadOnlyUser,
