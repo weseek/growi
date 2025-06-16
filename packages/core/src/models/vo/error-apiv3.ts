88f25532dd387a479139c255e97195eab1e1f8cb
Futa Arai <arafubeatbox@gmail.com>
2025-06-16 18:51:44 +0900

    fix non-autofixable biome lint errors

--- packages/core/src/models/vo/error-apiv3.ts
+++ packages/core/src/models/vo/error-apiv3.ts
@@ -1,12 +1,14 @@
 export class ErrorV3 extends Error {
   code: string;
 
+  // biome-ignore lint/suspicious/noExplicitAny: ignore
   args?: any;
 
   constructor(
     message = '',
     code = '',
     stack = undefined,
+    // biome-ignore lint/suspicious/noExplicitAny: ignore
     args: any = undefined,
   ) {
     super(); // do not provide message to the super constructor
