39e55367b32a07ecc58569aa65e9bb72ae8ac430
Shun Miyazawa <m0zurillex@gmail.com>
2025-06-25 01:44:07 +0000

    Refactor AiAssistantChatInitialView to improve structure and styling of reference pages and recent chat sections

--- apps/app/src/features/openai/client/components/AiAssistant/AiAssistantSidebar/AiAssistantChatInitialView.tsx
+++ apps/app/src/features/openai/client/components/AiAssistant/AiAssistantSidebar/AiAssistantChatInitialView.tsx
@@ -13,33 +13,33 @@ export const AiAssistantChatInitialView: React.FC<Props> = ({ description, pageP
   return (
     <>
       {description.length !== 0 && (
-        <p className="fs-6 text-body-secondary mb-0">
+        <p className="text-body-secondary mb-0">
           {description}
         </p>
       )}
+
       <div>
-        <div className="mb-3">
-          <p className="text-body-secondary mb-0">{t('sidebar_ai_assistant.reference_pages_label')}</p>
-          <div className="d-flex flex-column gap-1">
-            { pagePathPatterns.map(pagePathPattern => (
-              <a
-                key={pagePathPattern}
-                href="#"
-                className="fs-6 text-body-secondary text-decoration-none"
-              >
-                {pagePathPattern}
-              </a>
-            ))}
-          </div>
+        <p className="text-body-secondary mb-1">
+          {t('sidebar_ai_assistant.reference_pages_label')}
+        </p>
+        <div className="d-flex flex-column gap-1">
+          { pagePathPatterns.map(pagePathPattern => (
+            <a
+              key={pagePathPattern}
+              href="#"
+              className="fs-6 text-body-secondary text-decoration-none"
+            >
+              {pagePathPattern}
+            </a>
+          ))}
         </div>
+      </div>
 
-        <div className="mt-3">
-          <p className="text-body-secondary mb-0">
-            {t('sidebar_ai_assistant.recent_chat')}
-          </p>
-          <ThreadList />
-        </div>
-
+      <div>
+        <p className="text-body-secondary mb-1">
+          {t('sidebar_ai_assistant.recent_chat')}
+        </p>
+        <ThreadList />
       </div>
     </>
   );
