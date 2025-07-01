51b666dbe5bce88eeb9d27d8346e07a84e240192
Yuki Takei <yuki@weseek.co.jp>
2025-07-01 13:12:40 +0000

    WIP: insert theme line

--- apps/app/src/services/renderer/remark-plugins/plantuml.ts
+++ apps/app/src/services/renderer/remark-plugins/plantuml.ts
@@ -1,15 +1,32 @@
 import plantuml from '@akebifiky/remark-simple-plantuml';
+import type { Code } from 'mdast';
 import type { Plugin } from 'unified';
+import { visit } from 'unist-util-visit';
 import urljoin from 'url-join';
 
 type PlantUMLPluginParams = {
   plantumlUri: string,
+  isDarkMode?: boolean,
 }
 
 export const remarkPlugin: Plugin<[PlantUMLPluginParams]> = (options) => {
-  const plantumlUri = options.plantumlUri;
+  const { plantumlUri, isDarkMode } = options;
 
   const baseUrl = urljoin(plantumlUri, '/svg');
+  const simplePlantumlPlugin = plantuml.bind(this)({ baseUrl });
 
-  return plantuml.bind(this)({ baseUrl });
+  return (tree, file) => {
+    visit(tree, 'code', (node: Code) => {
+      if (node.lang === 'plantuml') {
+        const themeLine = isDarkMode
+          ? '!theme reddress-darkblue'
+          : '!theme carbon-gray';
+
+        // node.value = `${themeLine}\n${node.value}`;
+        node.value = `${''}\n${node.value}`;
+      }
+    });
+
+    simplePlantumlPlugin(tree, file);
+  };
 };
