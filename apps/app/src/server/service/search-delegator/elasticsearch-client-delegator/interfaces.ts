025877c58fdd44321b852b5870c128328682db0a
Shun Miyazawa <m0zurillex@gmail.com>
2025-07-04 07:54:01 +0000

    clean code

--- apps/app/src/server/service/search-delegator/elasticsearch-client-delegator/interfaces.ts
+++ apps/app/src/server/service/search-delegator/elasticsearch-client-delegator/interfaces.ts
@@ -1,4 +1,3 @@
-
 import type { ES7ClientDelegator } from './es7-client-delegator';
 import type { ES8ClientDelegator } from './es8-client-delegator';
 import type { ES9ClientDelegator } from './es9-client-delegator';
