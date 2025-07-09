3994459fb244b8ac4ff4c627bb8ec97a35bb682a
Shun Miyazawa <m0zurillex@gmail.com>
2025-07-09 03:31:38 +0000

    add coment

--- apps/app/src/server/service/search-delegator/elasticsearch-client-delegator/interfaces.ts
+++ apps/app/src/server/service/search-delegator/elasticsearch-client-delegator/interfaces.ts
@@ -4,6 +4,8 @@ import type { ES9ClientDelegator } from './es9-client-delegator';
 
 export type ElasticsearchClientDelegator = ES7ClientDelegator | ES8ClientDelegator | ES9ClientDelegator;
 
+
+// type guard
 export const isES7Client = (delegator: ElasticsearchClientDelegator): delegator is ES7ClientDelegator => {
   return delegator.delegetorVersion === 7;
 };
