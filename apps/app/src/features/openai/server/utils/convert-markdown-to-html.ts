1cfce7b0eb6d3ff83c776b7211bf7d3faee88599
arvid-e <arvid.edvinsson@gmail.com>
2025-07-15 04:52:15 +0000

    Find and transform code tags and backticks from html nodes to InlineCode nodes

--- apps/app/src/features/openai/server/utils/convert-markdown-to-html.ts
+++ apps/app/src/features/openai/server/utils/convert-markdown-to-html.ts
@@ -9,6 +9,8 @@ import type * as RemarkRehype from 'remark-rehype';
 import type * as Unified from 'unified';
 import type * as UnistUtilVisit from 'unist-util-visit';
 
+import { remarkPlugin as inlineCodeRemarkPlugin } from 'src/services/renderer/remark-plugins/codeblock';
+
 interface ModuleCache {
   unified?: typeof Unified.unified;
   visit?: typeof UnistUtilVisit.visit;
@@ -88,6 +90,7 @@ export const convertMarkdownToHtml = async(revisionBody: string, args: ConvertMa
 
   const processor = unified()
     .use(remarkParse)
+    .use(inlineCodeRemarkPlugin)
     .use(sanitizeMarkdown)
     .use(remarkRehype)
     .use(rehypeMeta, {
