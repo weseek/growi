7580f1d18bafb335aa1b81617b1f3c5fe2431928
arvid-e <arvid.edvinsson@gmail.com>
2025-07-16 04:53:50 +0000

    Remove unwanted files that was added during problem solving

--- apps/app/config/logger/config.prod.d.ts
+++
@@ -1,6 +0,0 @@
-declare const _exports: {
-    default: string;
-    'growi:routes:login-passport': string;
-    'growi:service:PassportService': string;
-};
-export = _exports;
--- apps/app/config/next-i18next.config.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _exports: import('next-i18next').UserConfig;
-export = _exports;
