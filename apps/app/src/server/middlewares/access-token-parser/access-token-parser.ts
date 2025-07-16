cb7c07c69e23e21e9f49d185cad96c90f451de3b
yusa-a <yusa-a@weseek.co.jp>
2025-07-16 08:15:58 +0000

    resolve conflict

--- apps/app/src/server/middlewares/access-token-parser/access-token-parser.ts
+++ apps/app/src/server/middlewares/access-token-parser/access-token-parser.ts
@@ -12,13 +12,39 @@ import type { AccessTokenParserReq } from './interfaces';
 
 const logger = loggerFactory('growi:middleware:access-token-parser');
 
-type Apiv3ErrFunction = (error: ErrorV3) => void;
 
-export const accessTokenParser = async(req: AccessTokenParserReq, res: Response & { apiv3Err: Apiv3ErrFunction }, next: NextFunction): Promise<void> => {
-  // TODO: comply HTTP header of RFC6750 / Authorization: Bearer
-  const accessToken = req.query.access_token ?? req.body.access_token;
-  if (accessToken != null && typeof accessToken !== 'string') {
-    return res.apiv3Err(new ErrorV3('The access token is invalid'));
+const extractBearerToken = (authHeader: string | undefined): string | null => {
+  if (authHeader == null) {
+    return null;
+  }
+
+  if (!authHeader.startsWith('Bearer ')) {
+    return null;
+  }
+
+  return authHeader.substring(7); // Remove 'Bearer ' prefix
+};
+
+export const accessTokenParser = async(req: AccessTokenParserReq, res: Response, next: NextFunction): Promise<void> => {
+  // Extract token from Authorization header first
+  const bearerToken = extractBearerToken(req.headers.authorization);
+
+  // Try all possible token sources in order of priority
+  const accessToken = bearerToken ?? req.query.access_token ?? req.body.access_token;
+
+  if (accessToken == null || typeof accessToken !== 'string') {
+    return next();
+  }
+
+  const User = mongoose.model<HydratedDocument<IUser>, { findUserByApiToken }>('User');
+
+  logger.debug('accessToken is', accessToken);
+
+  const user: IUserHasId = await User.findUserByApiToken(accessToken);
+
+  if (user == null) {
+    logger.debug('The access token is invalid');
+    return next();
   }
 
   const User = mongoose.model<HydratedDocument<IUser>, { findUserByApiToken }>('User');
