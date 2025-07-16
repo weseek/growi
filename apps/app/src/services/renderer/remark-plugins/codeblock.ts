6e631a6541bcefc965589a0f704f7318a09aa16c
arvid-e <arvid.edvinsson@gmail.com>
2025-07-16 05:06:28 +0000

    Fix lint error and changed import

--- apps/app/src/services/renderer/remark-plugins/codeblock.ts
+++ apps/app/src/services/renderer/remark-plugins/codeblock.ts
@@ -3,14 +3,15 @@ import type { Schema as SanitizeOption } from 'hast-util-sanitize';
 import type { InlineCode as MdastInlineCode, Html, Text } from 'mdast';
 import type { Plugin } from 'unified';
 import type { Node, Parent, Point } from 'unist';
+import type { visit as UnistUtilVisitFunction } from 'unist-util-visit';
 
+let visit: typeof UnistUtilVisitFunction;
 
-let visit: typeof import('unist-util-visit').visit;
-
-(async () => {
+(async() => {
   const mod = await import('unist-util-visit');
   visit = mod.visit;
 })();
+
 
 type InlineCode = MdastInlineCode & {
   data?: {
@@ -37,7 +38,7 @@ export const remarkPlugin: Plugin = () => {
     const defaultPoint: Point = { line: 1, column: 1, offset: 0 };
 
     if (typeof visit === 'undefined') {
-        return tree;
+      return tree;
     }
 
     visit(tree, 'html', (node: Html, index: number | undefined, parent: Parent | undefined) => {
