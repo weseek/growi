fd83f2a3ec5d4f87637e528720369b7a0c060edb
yusa-a <yusa-a@weseek.co.jp>
2025-07-17 04:22:47 +0000

    fix appsiteurl crowi type

--- apps/app/src/server/middlewares/certify-origin.ts
+++ apps/app/src/server/middlewares/certify-origin.ts
@@ -12,7 +12,7 @@ type Apiv3ErrFunction = (error: ErrorV3) => void;
 
 const certifyOrigin = (crowi: Crowi): ((req: AccessTokenParserReq, res: Response & { apiv3Err: Apiv3ErrFunction }, next: NextFunction) => void) => {
 
-  const appSiteUrl = crowi.configManager?.getConfig('crowi', 'app:siteUrl');
+  const appSiteUrl = crowi.configManager?.getConfig('app:siteUrl');
   return (req: AccessTokenParserReq, res: Response & { apiv3Err }, next: NextFunction): void => {
 
     const isSameOriginReq = req.headers.origin == null || req.headers.origin === appSiteUrl;
