dee33af18c7df9c30eb910400ea247fb5da6dab2
Shun Miyazawa <m0zurillex@gmail.com>
2025-08-05 07:38:10 +0000

    Refactor ES7SearchQuery type by removing unused properties for clarity

--- apps/app/src/server/service/search-delegator/elasticsearch-client-delegator/interfaces.ts
+++ apps/app/src/server/service/search-delegator/elasticsearch-client-delegator/interfaces.ts
@@ -27,37 +27,9 @@ export const isES9ClientDelegator = (delegator: ElasticsearchClientDelegator): d
 // Official library-derived interface
 // TODO: https://redmine.weseek.co.jp/issues/168446
 export type ES7SearchQuery = RequestParams.Search<{
-    aggregations?: Record<string, ES7types.AggregationsAggregationContainer>
-    aggs?: Record<string, ES7types.AggregationsAggregationContainer>
-    collapse?: ES7types.SearchFieldCollapse
-    explain?: boolean
-    from?: ES7types.integer
-    highlight?: ES7types.SearchHighlight
-    track_total_hits?: ES7types.SearchTrackHits
-    indices_boost?: Record<ES7types.IndexName, ES7types.double>[]
-    docvalue_fields?: (ES7types.QueryDslFieldAndFormat | ES7types.Field)[]
-    min_score?: ES7types.double
-    post_filter?: ES7types.QueryDslQueryContainer
-    profile?: boolean
-    query?: ES7types.QueryDslQueryContainer
-    rescore?: ES7types.SearchRescore | ES7types.SearchRescore[]
-    script_fields?: Record<string, ES7types.ScriptField>
-    search_after?: ES7types.SortResults
-    size?: ES7types.integer
-    slice?: ES7types.SlicedScroll
+    query: ES7types.QueryDslQueryContainer
     sort?: ES7types.Sort
-    _source?: ES7types.SearchSourceConfig
-    fields?: (ES7types.QueryDslFieldAndFormat | ES7types.Field)[]
-    suggest?: ES7types.SearchSuggester
-    terminate_after?: ES7types.long
-    timeout?: string
-    track_scores?: boolean
-    version?: boolean
-    seq_no_primary_term?: boolean
-    stored_fields?: ES7types.Fields
-    pit?: ES7types.SearchPointInTimeReference
-    runtime_mappings?: ES7types.MappingRuntimeFields
-    stats?: string[]
+    highlight?: ES7types.SearchHighlight
 }>
 export interface ES8SearchQuery {
   index: ES8types.IndexName
