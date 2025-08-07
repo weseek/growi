c8812ab54ab064abd60da34a639bd52ae306d121
Shun Miyazawa <m0zurillex@gmail.com>
2025-08-07 09:10:35 +0000

    refactor: add comment for type guard in SelectablePage definition

--- apps/app/src/features/openai/interfaces/selectable-page.ts
+++ apps/app/src/features/openai/interfaces/selectable-page.ts
@@ -4,6 +4,7 @@ import type { IPageForItem } from '~/interfaces/page';
 
 export type SelectablePage = Partial<IPageHasId> & { path: string }
 
+// type guard
 export const isSelectablePage = (page: IPageForItem): page is SelectablePage => {
   return page.path != null;
 };
