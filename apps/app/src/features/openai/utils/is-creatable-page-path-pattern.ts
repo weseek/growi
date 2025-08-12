44717b51f0ed1374556a7248aa63f49fa81ba2a3
Shun Miyazawa <m0zurillex@gmail.com>
2025-08-12 03:57:23 +0000

    refactor: fix indentation for isGlobPattern variable in isCreatablePagePathPattern function

--- apps/app/src/features/openai/utils/is-creatable-page-path-pattern.ts
+++ apps/app/src/features/openai/utils/is-creatable-page-path-pattern.ts
@@ -2,7 +2,7 @@ import { pagePathUtils } from '@growi/core/dist/utils';
 import { removeGlobPath } from './remove-glob-path';
 
 export const isCreatablePagePathPattern = (pagePath: string): boolean => {
-const isGlobPattern = pagePathUtils.isGlobPatternPath(pagePath);
+  const isGlobPattern = pagePathUtils.isGlobPatternPath(pagePath);
   if (isGlobPattern) {
     // Remove glob pattern since glob paths are non-creatable in GROWI
     const pathWithoutGlob = removeGlobPath([pagePath])[0];
