e085be052428f362cfdfb0751beef927562267af
Futa Arai <arafubeatbox@gmail.com>
2025-08-23 21:50:21 +0900

    configure biome for callout feature

--- apps/app/src/features/callout/services/consts.ts
+++ apps/app/src/features/callout/services/consts.ts
@@ -1,5 +1,13 @@
 // Ref: https://github.com/Microflash/remark-callout-directives/blob/fabe4d8adc7738469f253836f0da346591ea2a2b/themes/github/index.js
 // Ref: https://github.com/orgs/community/discussions/16925
 
-export const AllCallout = ['note', 'tip', 'important', 'info', 'warning', 'danger', 'caution'] as const;
-export type Callout = typeof AllCallout[number];
+export const AllCallout = [
+  'note',
+  'tip',
+  'important',
+  'info',
+  'warning',
+  'danger',
+  'caution',
+] as const;
+export type Callout = (typeof AllCallout)[number];
