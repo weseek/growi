635bebea84b1b284ab092f4bf4ea242a9b7e9ca6
Futa Arai <arafubeatbox@gmail.com>
2025-08-23 21:40:55 +0900

    fix tests for useSWRxRef useSWRxRefs

--- packages/remark-attachment-refs/vitest.config.ts
+++ packages/remark-attachment-refs/vitest.config.ts
@@ -4,9 +4,13 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    environment: 'happy-dom', // React testing environment
+    environment: 'node',
     clearMocks: true,
     globals: true,
-    setupFiles: ['./test/setup.ts'],
+    environmentMatchGlobs: [
+      // Use jsdom for client-side tests
+      ['**/client/**/*.spec.ts', 'jsdom'],
+      ['**/client/**/*.test.ts', 'jsdom'],
+    ],
   },
 });
