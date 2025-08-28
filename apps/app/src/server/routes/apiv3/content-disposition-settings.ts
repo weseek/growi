310ca062a723857e8f709e1c7396323b796a7071
arvid-e <arvid.edvinsson@gmail.com>
2025-08-28 08:42:00 +0000

    Add API call for setting strict disposition settings

--- apps/app/src/server/routes/apiv3/content-disposition-settings.ts
+++ apps/app/src/server/routes/apiv3/content-disposition-settings.ts
@@ -166,5 +166,46 @@ module.exports = (crowi) => {
     },
   );
 
+  router.put(
+    '/strict',
+    loginRequiredStrictly,
+    adminRequired,
+    addActivity,
+    apiV3FormValidator,
+
+    async(req, res) => {
+
+      try {
+        const strictMimeTypeSettings: Record<string, 'inline' | 'attachment'> = {
+          'application/pdf': 'attachment',
+          'application/json': 'attachment',
+          'text/csv': 'attachment',
+          'font/woff2': 'attachment',
+          'font/woff': 'attachment',
+          'font/ttf': 'attachment',
+          'font/otf': 'attachment',
+        };
+
+        await configManager.updateConfigs({ 'attachments:contentDisposition:mimeTypeOverrides': strictMimeTypeSettings });
+
+        const parameters = {
+          action: SupportedAction.ACTION_ADMIN_ATTACHMENT_DISPOSITION_UPDATE,
+          contentDispositionSettings: strictMimeTypeSettings,
+          currentMode: 'strict',
+        };
+        activityEvent.emit('update', res.locals.activity._id, parameters);
+
+        return res.apiv3({ currentMode: 'strict', contentDispositionSettings: strictMimeTypeSettings });
+      }
+      catch (err) {
+        const msg = 'Error occurred in updating content disposition for MIME types';
+        logger.error(msg, err);
+        return res.apiv3Err(
+          new ErrorV3(msg, 'update-content-disposition-failed'),
+        );
+      }
+    },
+  );
+
   return router;
 };
