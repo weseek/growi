620be95118a325a29a598865e9e96b59ec2fb844
Futa Arai <arafubeatbox@gmail.com>
2025-09-01 08:52:01 +0900

    fix non-autofixable biome error

--- apps/app/src/features/mermaid/services/mermaid.ts
+++ apps/app/src/features/mermaid/services/mermaid.ts
@@ -5,7 +5,10 @@ import { visit } from 'unist-util-visit';
 
 function rewriteNode(node: Code) {
   // replace node
-  const data = node.data ?? (node.data = {});
+  if (node.data == null) {
+    node.data = {};
+  }
+  const data = node.data;
   data.hName = 'mermaid';
   data.hProperties = {
     value: node.value,
