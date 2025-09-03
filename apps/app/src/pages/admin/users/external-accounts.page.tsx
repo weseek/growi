aa376d62462362ad170737ff2f592d3c8e162d11
Yuki Takei <yuki@weseek.co.jp>
2025-09-03 11:44:30 +0000

    refactor external accnts page

--- apps/app/src/pages/admin/users/external-accounts.page.tsx
+++ apps/app/src/pages/admin/users/external-accounts.page.tsx
@@ -1,62 +1,25 @@
-import { useEffect, useMemo } from 'react';
-
-import type {
-  NextPage, GetServerSideProps, GetServerSidePropsContext,
-} from 'next';
-import { useTranslation } from 'next-i18next';
 import dynamic from 'next/dynamic';
-import Head from 'next/head';
-import type { Container } from 'unstated';
-import { Provider } from 'unstated';
 
-import type { CommonProps } from '~/pages/utils/commons';
-import { useCurrentUser } from '~/stores-universal/context';
+import type { NextPageWithLayout } from '../../_app.page';
+import type { AdminCommonProps } from '../_shared';
+import { createAdminPageLayout, getServerSideAdminCommonProps } from '../_shared';
 
-import { retrieveServerSideProps } from '../../../utils/admin-page-util';
-
-const AdminLayout = dynamic(() => import('~/components/Layout/AdminLayout'), { ssr: false });
 const ManageExternalAccount = dynamic(() => import('~/client/components/Admin/ManageExternalAccount'), { ssr: false });
-const ForbiddenPage = dynamic(() => import('~/client/components/Admin/ForbiddenPage').then(mod => mod.ForbiddenPage), { ssr: false });
 
+type Props = AdminCommonProps;
 
-const AdminUserManagementPage: NextPage<CommonProps> = (props) => {
-  const { t } = useTranslation('admin');
-  useCurrentUser(props.currentUser ?? null);
+const AdminExternalAccountsPage: NextPageWithLayout<Props> = () => <ManageExternalAccount />;
 
-  const title = t('user_management.external_account');
-
-  const injectableContainers: Container<any>[] = useMemo(() => [], []);
-
-  useEffect(() => {
-    (async() => {
+AdminExternalAccountsPage.getLayout = createAdminPageLayout<Props>({
+  title: (_p, t) => t('user_management.external_account'),
+  containerFactories: [
+    async() => {
       const AdminExternalAccountsContainer = (await import('~/client/services/AdminExternalAccountsContainer')).default;
-      const adminExternalAccountsContainer = new AdminExternalAccountsContainer();
-      injectableContainers.push(adminExternalAccountsContainer);
-    })();
-  }, [injectableContainers]);
+      return new AdminExternalAccountsContainer();
+    },
+  ],
+});
 
-  if (props.isAccessDeniedForNonAdminUser) {
-    return <ForbiddenPage />;
-  }
+export const getServerSideProps = getServerSideAdminCommonProps;
 
-  return (
-    <Provider inject={[...injectableContainers]}>
-      <AdminLayout componentTitle={title}>
-        <Head>
-          <title>{title}</title>
-        </Head>
-        <ManageExternalAccount />
-      </AdminLayout>
-    </Provider>
-  );
-
-};
-
-
-export const getServerSideProps: GetServerSideProps = async(context: GetServerSidePropsContext) => {
-  const props = await retrieveServerSideProps(context);
-  return props;
-};
-
-
-export default AdminUserManagementPage;
+export default AdminExternalAccountsPage;
