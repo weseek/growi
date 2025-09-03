32e2ad56adb6dce92fcd28e3e6bfc2b2cf182612
Yuki Takei <yuki@weseek.co.jp>
2025-09-03 11:25:51 +0000

    refactor markdown page

--- apps/app/src/pages/admin/markdown.page.tsx
+++ apps/app/src/pages/admin/markdown.page.tsx
@@ -1,64 +1,27 @@
-import { useEffect, useMemo } from 'react';
-
-import type {
-  NextPage, GetServerSideProps, GetServerSidePropsContext,
-} from 'next';
-import { useTranslation } from 'next-i18next';
+import type { GetServerSideProps } from 'next';
 import dynamic from 'next/dynamic';
-import Head from 'next/head';
-import type { Container } from 'unstated';
-import { Provider } from 'unstated';
 
-import type { CommonProps } from '~/pages/utils/commons';
-import { generateCustomTitle } from '~/pages/utils/commons';
-import { useCurrentUser } from '~/stores-universal/context';
+import type { NextPageWithLayout } from '../_app.page';
 
-import { retrieveServerSideProps } from '../../utils/admin-page-util';
+import type { AdminCommonProps } from './_shared';
+import { createAdminPageLayout, getServerSideAdminCommonProps } from './_shared';
 
-const AdminLayout = dynamic(() => import('~/components/Layout/AdminLayout'), { ssr: false });
 const MarkDownSettingContents = dynamic(() => import('~/client/components/Admin/MarkdownSetting/MarkDownSettingContents'), { ssr: false });
-const ForbiddenPage = dynamic(() => import('~/client/components/Admin/ForbiddenPage').then(mod => mod.ForbiddenPage), { ssr: false });
 
+type Props = AdminCommonProps;
 
-const AdminMarkdownPage: NextPage<CommonProps> = (props) => {
-  const { t } = useTranslation('admin');
-  useCurrentUser(props.currentUser ?? null);
+const AdminMarkdownPage: NextPageWithLayout<Props> = () => <MarkDownSettingContents />;
 
-  const componentTitle = t('markdown_settings.markdown_settings');
-  const pageTitle = generateCustomTitle(props, componentTitle);
-
-  const injectableContainers: Container<any>[] = useMemo(() => [], []);
-
-  useEffect(() => {
-    (async() => {
+AdminMarkdownPage.getLayout = createAdminPageLayout<Props>({
+  title: (_p, t) => t('markdown_settings.markdown_settings'),
+  containerFactories: [
+    async() => {
       const AdminMarkDownContainer = (await import('~/client/services/AdminMarkDownContainer')).default;
-      const adminMarkDownContainer = new AdminMarkDownContainer();
-      injectableContainers.push(adminMarkDownContainer);
-    })();
-  }, [injectableContainers]);
+      return new AdminMarkDownContainer();
+    },
+  ],
+});
 
-  if (props.isAccessDeniedForNonAdminUser) {
-    return <ForbiddenPage />;
-  }
-
-  return (
-    <Provider inject={[...injectableContainers]}>
-      <AdminLayout componentTitle={componentTitle}>
-        <Head>
-          <title>{pageTitle}</title>
-        </Head>
-        <MarkDownSettingContents />
-      </AdminLayout>
-    </Provider>
-  );
-
-};
-
-
-export const getServerSideProps: GetServerSideProps = async(context: GetServerSidePropsContext) => {
-  const props = await retrieveServerSideProps(context);
-  return props;
-};
-
+export const getServerSideProps: GetServerSideProps = getServerSideAdminCommonProps;
 
 export default AdminMarkdownPage;
