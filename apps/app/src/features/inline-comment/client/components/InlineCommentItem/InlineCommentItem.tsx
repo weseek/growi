be49248348b810dd5c7458541752f65f70d61023
Yuki Takei <yuki@weseek.co.jp>
2026-09-11 05:11:02 +0000

    style(inline-comment): adjust button size and remove unnecessary classes

--- apps/app/src/features/inline-comment/client/components/InlineCommentItem/InlineCommentItem.tsx
+++ apps/app/src/features/inline-comment/client/components/InlineCommentItem/InlineCommentItem.tsx
@@ -170,7 +170,7 @@ export const InlineCommentItem: FC<InlineCommentItemProps> = (
                   <button
                     type="button"
                     data-testid="inline-comment-edit-button"
-                    className={`btn btn-link opacity-50 rounded-circle ${styles['icon-button']}`}
+                    className={`btn btn-link opacity-50 ${styles['icon-button']}`}
                     onClick={() => setIsEditing(true)}
                   >
                     <span className="material-symbols-outlined">edit</span>
@@ -178,7 +178,7 @@ export const InlineCommentItem: FC<InlineCommentItemProps> = (
                   <button
                     type="button"
                     data-testid="inline-comment-delete-button"
-                    className={`btn btn-link opacity-50 text-danger rounded-circle ${styles['icon-button']}`}
+                    className={`btn btn-link text-danger opacity-50 ${styles['icon-button']}`}
                     onClick={() => setIsDeleteConfirmOpen(true)}
                   >
                     <span className="material-symbols-outlined">delete</span>
